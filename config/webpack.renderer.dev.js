e7997123c34f9fe36522d06daafe9cfff6d7e9ce
govinda <govi2010@gmail.com>
2017-06-16 19:28:12 +0530

    Added Electron Build

--- config/webpack.renderer.dev.js
+++ config/webpack.renderer.dev.js
@@ -5,7 +5,7 @@
 const helpers = require('./helpers');
 const webpackMerge = require('webpack-merge'); // used to merge webpack configs
 // const webpackMergeDll = webpackMerge.strategy({plugins: 'replace'});
-const commonConfig = require('./webpack.common.js'); // the settings that are common to prod and dev
+const commonConfig = require('./webpack.renderer.js'); // the settings that are common to prod and dev
 
 /**
  * Webpack Plugins
