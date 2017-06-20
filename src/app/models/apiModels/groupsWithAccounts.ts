0f9bed0825ca12626ef5ad31ee7513e34db9012e
Kartik Upadhyay <kartik@walkover.in>
2017-06-20 20:01:01 +0530

    +kartik | Add comments for api call in models

--- src/app/models/apiModels/groupsWithAccounts.ts
+++ src/app/models/apiModels/groupsWithAccounts.ts
@@ -1,7 +1,12 @@
 import { IAccountsInfo } from '../../interfaces/accountInfo.interface';
 import { IGroupsWithAccounts } from '../../interfaces/groupsWithAccounts.interface';
 
-export class GroupsWithAccounts implements IGroupsWithAccounts {
+/**
+ * Model for getting group list api response
+ * API:: (group-list) company/companyUniqueName/groups-with-accounts
+ */
+
+export class GroupsWithAccountsResponse implements IGroupsWithAccounts {
   public synonyms: string;
   public accounts: IAccountsInfo[];
   public name: string;
