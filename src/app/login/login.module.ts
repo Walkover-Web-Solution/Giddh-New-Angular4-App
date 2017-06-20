3260e4025c3f1030d4010fe98ade9b352f9fa033
Vishal Isharani <isharanivishal@gmail.com>
2017-06-20 11:54:09 +0530

    style fixes

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Ng2BootstrapModule } from 'ngx-bootstrap';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    Ng2BootstrapModule.forRoot()
+    SharedModule
   ],
   declarations: [LoginComponent]
 })
