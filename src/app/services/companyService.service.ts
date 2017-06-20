261c0e55f41689727b713eb5e499e1569cefca38
Vishal Isharani <isharanivishal@gmail.com>
2017-06-20 20:22:04 +0530

    Vishal:- company refresh service

--- src/app/services/companyService.service.ts
+++ src/app/services/companyService.service.ts
@@ -4,11 +4,21 @@ import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
 import { Company } from '../models';
 import { COMPANY_API } from './apiurls';
+import { Store } from '@ngrx/store';
+import { AppState } from '../store/roots';
+import { UserDetails } from '../models/api-models/loginModels';
 
 @Injectable()
 export class CompanyService {
+  private user: UserDetails;
 
-  constructor(private _http: HttpWrapperService) { }
+  constructor(private _http: HttpWrapperService, private store: Store<AppState>) {
+
+    this.store.select(state => {
+      // get current user
+      this.user = state.login.user.user;
+    });
+  }
 
   /**
    * CreateCompany
@@ -21,7 +31,21 @@ export class CompanyService {
       .catch((e) => {
         return Observable.throw(e);
       });
+    }
+
+  /**
+   * CompanyList
+   */
+  public CompanyList(): Observable<Response> {
+    return this._http.get(COMPANY_API.COMPANY_LIST.replace(':uniqueName', this.user.uniqueName))
+      .map((res) => {
+        return res;
+      })
+      .catch((e) => {
+        return Observable.throw(e);
+      });
   }
+
   /**
    * DeleteCompany
    */
