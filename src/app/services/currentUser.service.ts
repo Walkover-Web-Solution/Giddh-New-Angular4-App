ee66fa2920d5119582ee8ab458dc6ee8bdd86bbe
govinda <govi2010@gmail.com>
2017-06-21 14:39:37 +0530

    Govinda: Added auth-Guard So when user is logjn then only it can access Other pages

--- src/app/services/currentUser.service.ts
+++ src/app/services/currentUser.service.ts
@@ -1,43 +1,27 @@
-import { Injectable } from '@angular/core';
-import { StorageService } from './storage.service';
-import { AuthenticationService } from './authentication.service';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+// import { AppState } from '../store/roots';
+// import { Injectable } from '@angular/core';
+// import { StorageService } from './storage.service';
+// import { AuthenticationService } from './authentication.service';
+// import { Observable } from 'rxjs/Observable';
+// import { Subject } from 'rxjs/Subject';
+// import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
-import { Domain } from '../models/domain';
+// import { Domain } from '../models/domain';
+// import { Store } from '@ngrx/store';
 
-@Injectable()
-export class CurrentUserService {
-  public appTitle = new Subject<string>();
+// @Injectable()
+// export class CurrentUserService {
+//   public appTitle = new Subject<string>();
+//   public authKey: string;
+//   constructor(public _storageService: StorageService, private store: Store<AppState>, ) {
+//   }
 
-  constructor(public _storageService: StorageService) {
+//   public get Roles(): any {
+//     let roles = this._storageService.getItem('roles');
+//     return roles;
+//   }
+//   public set Roles(roles: any) {
+//     this._storageService.setItem('roles', roles);
+//   }
 
-  }
-
-  public getTitle() {
-    let title = this._storageService.getItem('title');
-    return title;
-  }
-  public setTitle(title: string) {
-    this._storageService.setItem('title', title);
-    this.appTitle.next(title);
-  }
-
-  public get token(): string {
-    let token = this._storageService.getItem('authorizationData');
-    return token;
-  }
-  public set token(token: string) {
-    this._storageService.setItem('authorizationData', token);
-  }
-
-  public get Roles(): any {
-    let roles = this._storageService.getItem('roles');
-    return roles;
-  }
-  public set Roles(roles: any) {
-    this._storageService.setItem('roles', roles);
-  }
-
-}
+// }
