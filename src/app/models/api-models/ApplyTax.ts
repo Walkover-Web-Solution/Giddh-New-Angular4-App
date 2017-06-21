d7525c323ecd198acc45e5c5be3b848b16c652a0
Kartik Upadhyay <kartik@walkover.in>
2017-06-21 20:00:59 +0530

    +kartik | Add request models for merge and un-merge account api calls

--- src/app/models/api-models/ApplyTax.ts
+++ src/app/models/api-models/ApplyTax.ts
@@ -3,19 +3,24 @@ import { IApplyTax } from '../interfaces/applyTax.interface';
  * Model for apply tax api request
  * API:: (Apply tax) /company/{{companyname}}/tax/assign
  * Takes an array of taxes applicable on accounts or groups
+ * This request will take array of ApplyTaxRequest as payload
  */
-export class ApplyTaxRequest {
-  public taxable: IApplyTax[];
+export class ApplyTaxRequest implements IApplyTax {
+  public uniqueName: string;
+  public taxes: string[];
+  public isAccount: boolean;
 
-  constructor(taxable: IApplyTax[]) {
-    this.taxable = taxable;
+  constructor(taxable: IApplyTax) {
+    this.uniqueName = taxable.uniqueName;
+    this.taxes = taxable.taxes;
+    this.isAccount = taxable.isAccount;
   }
 }
 
 /**
  * Model for apply tax api response
  * API:: (Apply tax) /company/{{companyname}}/tax/assign
- * Response is as success message from api
+ * Response is as success message from api in body
  */
 export class ApplyTaxResponse {
 }
