d0f105bb77ee03fc4f8bccb32ec4bd4f94fd0a52
Vishal Isharani <isharanivishal@gmail.com>
2017-06-21 15:46:00 +0530

    Vishal | refresh comapny ui binding

--- src/app/services/companyService.service.ts
+++ src/app/services/companyService.service.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs/Observable';
 import { HttpWrapperService } from './httpWrapper.service';
 import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
-import { Company } from '../models';
+import { CompanyRequest } from '../models';
 import { COMPANY_API } from './apiurls';
 import { Store } from '@ngrx/store';
 import { AppState } from '../store/roots';
@@ -13,17 +13,17 @@ export class CompanyService {
   private user: UserDetails;
 
   constructor(private _http: HttpWrapperService, private store: Store<AppState>) {
-
-    this.store.select(state => {
-      // get current user
-      this.user = state.login.user.user;
+    this.store.take(1).subscribe(s => {
+      if (s.login.user) {
+        this.user = s.login.user.user;
+      }
     });
   }
 
   /**
    * CreateCompany
    */
-  public CreateCompany(company: Company): Observable<Response> {
+  public CreateCompany(company: CompanyRequest): Observable<Response> {
     return this._http.post(COMPANY_API.CREATE_COMPANY, { company })
       .map((res) => {
         return res;
