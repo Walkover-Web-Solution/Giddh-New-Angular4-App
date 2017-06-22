1d719cb63609fc6674ecda2ec6080fb2576ef1ea
govinda <govi2010@gmail.com>
2017-06-22 20:45:25 +0530

    Govinda | Added group service

--- src/app/services/apiurls/group.api.ts
+++ src/app/services/apiurls/group.api.ts
@@ -1,7 +1,7 @@
 import { Configuration } from '../../app.constant';
 
 export const GROUP_API = {
-  CREATE: Configuration.ApiUrl + 'company',
+  CREATE: Configuration.ApiUrl + 'company/:companyUniqueName/groups',
   SHARE: Configuration.ApiUrl + 'company/:companyUniqueName/groups/:groupUniqueName/share',
   SHARED_WITH: Configuration.ApiUrl + 'company/:companyUniqueName/groups/:groupUniqueName/shared-with',
 };
--- src/app/services/group.service.ts
+++ src/app/services/group.service.ts
@@ -1,3 +1,4 @@
+import { GROUP_API } from './apiurls/group.api';
 import { Injectable, EventEmitter } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
@@ -7,11 +8,10 @@ import { Router } from '@angular/router';
 import { HttpWrapperService } from './httpWrapper.service';
 import { ErrorHandlerService } from './errorhandler.service';
 import { LoaderService } from './loader.service';
-import { LOGIN_API } from './apiurls/login.api';
 import { BaseResponse } from '../models/api-models/BaseResponse';
 import { VerifyEmailModel, VerifyEmailResponseModel } from '../models/api-models/loginModels';
 import { HandleCatch } from './catchManager/catchmanger';
-import { GroupResponse, GroupCreateRequest } from '../models/api-models/Group';
+import { GroupResponse, GroupCreateRequest, ShareGroupRequest } from '../models/api-models/Group';
 
 // import { UserManager, Log, MetadataService, User } from 'oidc-client';
 @Injectable()
@@ -22,9 +22,23 @@ export class GroupService {
   ) {
   }
   public CreateGroup(modele: GroupCreateRequest): Observable<BaseResponse<GroupResponse>> {
-    return this._http.post(LOGIN_API.VerifyEmail, modele).map((res) => {
+    return this._http.post(GROUP_API.CREATE, modele).map((res) => {
       let data: BaseResponse<GroupResponse> = res.json();
       return data;
     }).catch((e) => HandleCatch<GroupResponse>(e));
   }
+  public ShareGroup(modele: ShareGroupRequest): Observable<BaseResponse<string>> {
+    return this._http.put(GROUP_API.CREATE, modele).map((res) => {
+      let data: BaseResponse<string> = res.json();
+      return data;
+    }).catch((e) => HandleCatch<string>(e));
+  }
+
+  public ShareWithGroup(modele: ShareGroupRequest): Observable<BaseResponse<string>> {
+    return this._http.put(GROUP_API.CREATE, modele).map((res) => {
+      let data: BaseResponse<string> = res.json();
+      return data;
+    }).catch((e) => HandleCatch<string>(e));
+  }
+
 }
