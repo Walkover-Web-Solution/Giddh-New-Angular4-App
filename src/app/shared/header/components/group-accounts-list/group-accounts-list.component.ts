356159e5ad1c1e44d6f4c71abcde79a5f1d24683
Vishal Isharani <isharanivishal@gmail.com>
2017-06-23 20:22:31 +0530

    Vishal | Load accounts of selected group

--- src/app/shared/header/components/group-accounts-list/group-accounts-list.component.ts
+++ src/app/shared/header/components/group-accounts-list/group-accounts-list.component.ts
@@ -1,16 +1,69 @@
 import { Component, OnInit, Input } from '@angular/core';
-// import { IAccount } from '../../../../models/interfaces/accountCreate.interface';
 import { IAccountsInfo } from '../../../../models/interfaces/accountInfo.interface';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../../store/roots';
+import { Observable } from 'rxjs/Observable';
+import { IGroupsWithAccounts } from '../../../../models/interfaces/groupsWithAccounts.interface';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'group-account-list',
   templateUrl: './group-accounts-list.component.html'
 })
 export class GroupAccountsListComponent implements OnInit {
-  @Input() public accountsList: IAccountsInfo[];
+  public accountsList$: Observable<IAccountsInfo[]>;
+  public activeGroupName$: Observable<string>;
+  // public searchLoad: Observable<boolean>;
   // tslint:disable-next-line:no-empty
-  constructor() { }
+  constructor(private store: Store<AppState>) {
+    this.accountsList$ =  this.store.select(state => {
+      let accountsList: IAccountsInfo[] = [];
+      if (state.groupwithaccounts.groupswithaccounts) {
+        if (state.groupwithaccounts.activeGroupName === null) {
+          accountsList = this.genFlatterAccounts(_.cloneDeep(state.groupwithaccounts.groupswithaccounts), []);
+        } else {
+          accountsList = this.getAccountFromGroup(_.cloneDeep(state.groupwithaccounts.groupswithaccounts), _.cloneDeep(state.groupwithaccounts.activeGroupName), []);
+        }
+      }
+      return accountsList;
+    });
+
+    this.activeGroupName$ = this.store.select(state => {
+      return state.groupwithaccounts.activeGroupName;
+    });
+    // this.searchLoad = this.store.select(state => state.groupwithaccounts.isGroupWithAccountsLoading);
+  }
 
   // tslint:disable-next-line:no-empty
-  public ngOnInit() { }
+  public ngOnInit() {
+//
+  }
+
+  public genFlatterAccounts(groupList: IGroupsWithAccounts[], result: IAccountsInfo[]): IAccountsInfo[] {
+    groupList.forEach((el) => {
+      if (el.accounts) {
+        el.accounts.forEach((e) => {
+          result.push(e);
+        });
+      }
+      if (el.groups) {
+        this.genFlatterAccounts(el.groups, result);
+      }
+    });
+    return result;
+  }
+
+  public getAccountFromGroup(groupList: IGroupsWithAccounts[], uniqueName: string, result: IAccountsInfo[]): IAccountsInfo[] {
+    groupList.forEach(el => {
+      if (el.accounts) {
+        if (el.uniqueName === uniqueName) {
+          result.push(...el.accounts);
+        }
+      }
+      if (el.groups) {
+        this.getAccountFromGroup(el.groups, uniqueName, result);
+      }
+    });
+    return result;
+  }
 }
