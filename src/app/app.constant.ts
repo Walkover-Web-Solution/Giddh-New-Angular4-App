4320037953507fae9a2f5b1c689ee4161494d297
govinda <govi2010@gmail.com>
2017-06-23 18:49:55 +0530

    Govinda | Removes Toster Service Dependancy

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -24,8 +24,17 @@ export class AuthProviders extends CustomConfig {
 export const APP_DEFAULT_TITLE = 'Giddh -';
 
 export const DEFAULT_TOASTER_OPTIONS = {
-  showClose: true,
-  timeout: 2500
+  closeButton: true, // show close button
+  timeOut: 5000, // time to live
+  enableHtml: false, // allow html in message. (UNSAFE)
+  extendedTimeOut: 1000, // time to close after a user hovers over toast
+  progressBar: true, // show progress bar
+  toastClass: 'toast', // class on toast
+  positionClass: 'toast-top-right', // class on toast
+  titleClass: 'toast-title', // class inside toast on title
+  messageClass: 'toast-message', // class inside toast on message
+  tapToDismiss: true, // close on click
+  onActivateTick: false
 };
 
 export const DEFAULT_SERVER_ERROR_MSG = 'Something went wrong! Please try again.';
