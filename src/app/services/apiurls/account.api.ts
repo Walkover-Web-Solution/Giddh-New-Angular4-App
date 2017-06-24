2498c58fefced3d6f5985ac3829956d2f0efccda
govinda <govi2010@gmail.com>
2017-06-24 15:51:31 +0530

    Govinda | Worked on Account APIS

--- src/app/services/apiurls/account.api.ts
+++ src/app/services/apiurls/account.api.ts
@@ -8,6 +8,6 @@ export const ACCOUNTS_API = {
   MOVE: Configuration.ApiUrl + 'company/:companyUniqueName/accounts/:accountUniqueName/move',
   SHARE: Configuration.ApiUrl + 'company/:companyUniqueName/accounts/:accountUniqueName/share',
   SHARED_WITH: Configuration.ApiUrl + 'company/:companyUniqueName/accounts/:accountUniqueName/shared-with',
-  FLATTEN_ACCOUNTS: Configuration.ApiUrl + 'company/:companyUniqueName/flatten-accounts?q=&refresh=', // get call
+  FLATTEN_ACCOUNTS: Configuration.ApiUrl + 'company/:companyUniqueName/flatten-accounts?q=:q&refresh=:refresh', // get call
   TAX_HIERARCHY: Configuration.ApiUrl + 'company/:companyUniqueName/accounts/:accountUniqueName/tax-hierarchy', // get call
 };
