99b8894b5e5e12ca75f68af2140ad0685121e161
Kartik Upadhyay <kartik@walkover.in>
2017-06-26 18:49:54 +0530

    WIP

--- src/app/services/apiurls/ledger.api.ts
+++ src/app/services/apiurls/ledger.api.ts
@@ -2,4 +2,6 @@ import { Configuration } from '../../app.constant';
 
 export const LEDGER_API = {
   CREATE: Configuration.ApiUrl + 'company/:companyUniqueName/accounts/:accountUniqueName/ledgers', // post call
+  MAIL_LEDGER: Configuration.ApiUrl + 'company/:companyUniqueName/accounts/:accountUniqueName/ledgers/mail', // post call
+  DOWNLOAD_INVOICE: Configuration.ApiUrl + 'company/:companyUniqueName/accounts/:accountUniqueName/invoices/download', // post call
 };
