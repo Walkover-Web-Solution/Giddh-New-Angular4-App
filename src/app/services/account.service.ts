be472594d07a1c4bc51ee22ade919f6587b5c98c
Vishal Isharani <isharanivishal@gmail.com>
2017-06-27 20:46:03 +0530

    Vishal | move account done

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -130,7 +130,6 @@ export class AccountService implements OnInit {
       })
       .catch((e) => HandleCatch<string>(e));
   }
-
   public AccountShare(model: ShareAccountRequest, accountUniqueName: string): Observable<BaseResponse<string>> {
     this.store.take(1).subscribe(s => {
       if (s.session.user) {
@@ -145,9 +144,7 @@ export class AccountService implements OnInit {
       })
       .catch((e) => HandleCatch<string>(e));
   }
-
   public AccountUnshare(userEmail: string, accountUniqueName: string): Observable<BaseResponse<string>> {
-    debugger
     this.store.take(1).subscribe(s => {
       if (s.session.user) {
         this.user = s.session.user.user;
