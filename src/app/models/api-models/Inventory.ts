edb5ada069aa97e947aeb4e4642804ef1c445eba
Kartik Upadhyay <kartik@walkover.in>
2017-06-27 14:13:24 +0530

    + kartik | Add models for unit types api

--- src/app/models/api-models/Inventory.ts
+++ src/app/models/api-models/Inventory.ts
@@ -1,6 +1,6 @@
 import { INameUniqueName } from '../interfaces/nameUniqueName.interface';
 import { IPaginatedResponse } from '../interfaces/paginatedResponse.interface';
-import { IStocksItem } from '../interfaces/stocksItem.interface';
+import { IStocksItem, IStockItem } from '../interfaces/stocksItem.interface';
 
 /*
  * Model for Create Stock Group api request
@@ -31,6 +31,7 @@ export class StockGroupResponse {
 /**
  * Model for Stocks api response
  * API:: (Stocks) company/:companyUniqueName/stock-group/stocks
+ * response will ne a hash containing StocksResponse
  */
 export class StocksResponse implements IPaginatedResponse {
   public count: number;
@@ -39,4 +40,27 @@ export class StocksResponse implements IPaginatedResponse {
   public size: number;
   public totalItems: number;
   public totalPages: number;
+}
+
+/**
+ * Model for Delete Stock-Group api response
+ * DELETE call
+ * from ui we are makingcall to delete-stockgrp api whereas from node it is directed to stock-unit api
+ * API:: (Delete Stock-Group) company/:companyUniqueName/stock-group/:stockGroupUniqueName
+ * Response will be a string message in body
+ */
+
+/**
+ * Model for units types api response
+ * from ui we are makingcall to units types api whereas from node it is directed to stock-unit api
+ * GET call
+ * API:: (units types) company/:companyUniqueName/stock-unit
+ * Response will be a array of StockUnitResponse
+ */
+export class StockUnitResponse {
+  public code: string;
+  public hierarchicalQuantity: number;
+  public name: string;
+  public parentStockUnit?: IStockItem;
+  public quantityPerUnit: number;
 }
--- src/app/services/apiurls/inventory.api.ts
+++ src/app/services/apiurls/inventory.api.ts
@@ -3,7 +3,9 @@ import { Configuration } from '../../app.constant';
 export const INVENTORY_API = {
   CREATE_STOCK_GROUP: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group', // post call
   UPDATE_STOCK_GROUP: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group', // put call
+  DELETE_STOCK_GROUP: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/:stockGroupUniqueName', // DELETE call
   STOCKS: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/stocks', // get call
   GROUPS_WITH_STOCKS_FLATTEN: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/groups-with-stocks-flatten?count=&page=1&q=', // get call
   GROUPS_WITH_STOCKS_HIERARCHY_MIN: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/groups-with-stocks-hierarchy-min', // get call
+  STOCK_UNIT: Configuration.ApiUrl + 'company/:companyUniqueName/stock-unit', // get call
 };
