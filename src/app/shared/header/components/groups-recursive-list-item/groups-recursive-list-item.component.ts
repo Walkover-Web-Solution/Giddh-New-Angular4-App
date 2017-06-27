47a8078b24b4f4da424218d707d68ed3e80b666a
Vishal Isharani <isharanivishal@gmail.com>
2017-06-27 17:33:42 +0530

    Vishal | reset active group

--- src/app/shared/header/components/groups-recursive-list-item/groups-recursive-list-item.component.ts
+++ src/app/shared/header/components/groups-recursive-list-item/groups-recursive-list-item.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { AppState } from '../../../../store/roots';
 import { Store } from '@ngrx/store';
 import { GroupWithAccountsAction } from '../../../../services/actions/groupwithaccounts.actions';
+import { AccountsAction } from '../../../../services/actions/accounts.actions';
 
 @Component({
   selector: 'groups-recursive-list-item',
@@ -15,12 +16,13 @@ export class GroupsRecursiveListItemComponent implements OnInit {
   @Input() public padLeft: number = 30;
 
   // tslint:disable-next-line:no-empty
-  constructor(private store: Store<AppState>, private groupWithAccountsAction: GroupWithAccountsAction) { }
+  constructor(private store: Store<AppState>, private groupWithAccountsAction: GroupWithAccountsAction, private accountAction: AccountsAction) { }
 
   // tslint:disable-next-line:no-empty
   public ngOnInit() {  }
 
   public onGroupClick(uniqueName: string) {
     this.store.dispatch(this.groupWithAccountsAction.getGroupDetails(uniqueName));
+    this.store.dispatch(this.accountAction.resetActiveAccount());
   }
 }
