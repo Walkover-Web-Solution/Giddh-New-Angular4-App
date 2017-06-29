1ec141968a8bb9ee5fe02d058704f441d9d7fcbb
Kartik Upadhyay <kartik@walkover.in>
2017-06-29 16:45:58 +0530

    + kartik | Add serice for iventory sidebar

--- src/app/services/apiurls/inventory.api.ts
+++ src/app/services/apiurls/inventory.api.ts
@@ -6,7 +6,7 @@ export const INVENTORY_API = {
   DELETE_STOCK_GROUP: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/:stockGroupUniqueName', // DELETE call
   STOCKS: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/stocks', // get call
   GROUPS_WITH_STOCKS_FLATTEN: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/groups-with-stocks-flatten?count=&page=1&q=', // get call
-  GROUPS_WITH_STOCKS_HIERARCHY_MIN: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/groups-with-stocks-hierarchy-min', // get call
+  GROUPS_WITH_STOCKS_HIERARCHY: Configuration.ApiUrl + 'company/:companyUniqueName/hierarchical-stock-groups?q=:q&page=:page&count=:count', // get call
   STOCK_UNIT: Configuration.ApiUrl + 'company/:companyUniqueName/stock-unit', // get call
   STOCK_REPORT: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/get-stock-report?count=:count&from=:from&page=:page&stockGroupUniqueName=stockGroupUniqueName&stockUniqueName=:stockUniqueName&to=:to', // get call
   STOCK_DETAIL: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/:stockGroupUniqueName/stock/:stockUniqueName', // get call
