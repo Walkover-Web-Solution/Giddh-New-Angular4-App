360eac1dccedfbec47b94d9ed1e6fd13ee74f7fd
Kartik Upadhyay <kartik@walkover.in>
2017-06-29 16:45:58 +0530

    + kartik | Add serice for iventory sidebar

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -6,6 +6,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../store/roots';
 import { INVENTORY_API } from './apiurls/inventory.api';
 import { StockGroupRequest, StockGroupResponse } from '../models/api-models/Inventory';
+import { GroupsWithStocksFlatten } from '../models/api-models/GroupsWithStocks';
 import { Observable } from 'rxjs/Observable';
 import { BaseResponse } from '../models/api-models/BaseResponse';
 import { UserDetails } from '../models/api-models/loginModels';
@@ -34,5 +35,20 @@ export class InventoryService {
       data.request = model;
       return data;
     }).catch((e) => HandleCatch<StockGroupResponse, StockGroupRequest>(e, model));
+  }
+
+  public GetGroupsWithStocksFlatten(q: string = '', page: number = 1, count: number = null): Observable<BaseResponse<GroupsWithStocksFlatten, string>> {
+    this.store.take(1).subscribe(s => {
+      if (s.session.user) {
+        this.user = s.session.user.user;
+      }
+      this.companyUniqueName = s.session.companyUniqueName;
+    });
+    return this._http.get(INVENTORY_API.GROUPS_WITH_STOCKS_FLATTEN.replace(':companyUniqueName', this.companyUniqueName).replace(':q', q).replace(':page', page.toString()).replace(':count', count.toString())).map((res) => {
+      let data: BaseResponse<GroupsWithStocksFlatten, string> = res.json();
+      data.request = '';
+      data.queryString = { q, page, count };
+      return data;
+    }).catch((e) => HandleCatch<GroupsWithStocksFlatten, string>(e, '', { q, page, count }));
   }
 }
