45de556290532c623cacd98b1d982833fd60536a
govinda <govi2010@gmail.com>
2017-06-29 15:19:05 +0530

    Govinda | fixed linting problems

--- src/app/inventory/inventory.routing.module.ts
+++ src/app/inventory/inventory.routing.module.ts
@@ -7,7 +7,7 @@ import { NeedsAuthentication } from '../services/decorators/needsAuthentication'
 import { InventoryAddGroupComponent } from './components/add-group-components/inventory.addgroup.component';
 import { InventoryAddStockComponent } from './components/add-stock-components/inventory.addstock.component';
 import { InventoryCustomStockComponent } from './components/custom-stock-components/inventory.customstock.component';
-import { InventoryStockReportComponent } from "./components/stock-report-component/inventory.stockreport.component";
+import { InventoryStockReportComponent } from './components/stock-report-component/inventory.stockreport.component';
 
 @NgModule({
   imports: [
