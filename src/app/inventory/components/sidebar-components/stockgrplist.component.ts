9aa804b8467f6b50984daecd78eb7ff28e9446ba
govinda <govi2010@gmail.com>
2017-06-30 17:24:51 +0530

    govinda | Sidebar component

--- src/app/inventory/components/sidebar-components/stockgrplist.component.ts
+++ src/app/inventory/components/sidebar-components/stockgrplist.component.ts
@@ -1,22 +1,23 @@
+import { AppState } from '../../../store/roots';
 import { IGroupsWithStocksHierarchyMinItem } from '../../../models/interfaces/groupsWithStocks.interface';
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
-
+import * as _ from 'lodash';
+import { SidebarAction } from '../../../services/actions/inventory/sidebar.actions';
+import { Store } from '@ngrx/store';
 @Component({
   selector: 'stockgrp-list',
   styles: [`
   `],
   template: `
   <ul class="list-unstyled stock-grp-list mrT1" >
-      <li  [routerLink]="[ 'add-group', grp.uniqueName ]" class="pdL" *ngFor="let grp of Groups">
-        <a href ng-class="{'active':  Groups.uniqueName ===groupUniqueName }">{{grp.name}}</a>
-        <i *ngIf="grp.childStockGroups.length > 0 || grp.uniqueName ===groupUniqueName" class="icon-arrow-down" [ngClass]="{'open': grp.visibleChilds}"></i>
-        <ng-template *ngFor="let item of grp.childStockGroups">
-          <stock-list [Groups]='item'>
+      <li  (click)="OpenGroup(grp,$event)"  class="pdL" *ngFor="let grp of Groups">
+        <div [routerLink]="[ 'add-group', grp.uniqueName ]" [ngClass]="{'active': grp.isActive}" >{{grp.name}}</div>
+        <i *ngIf="grp.childStockGroups.length > 0" class="icon-arrow-down" [ngClass]="{'open': grp.isOpen}"></i>
+          <stock-list [Groups]='grp' >
           </stock-list>
-        </ng-template>
-        <stockgrp-list [Groups]='grp.childStockGroups' *ngIf="grp.childStockGroups.length > 0 && grp.uniqueName === groupUniqueName">
+        <stockgrp-list [Groups]='grp.childStockGroups' *ngIf="grp.childStockGroups.length > 0 && grp.isOpen">
         </stockgrp-list>
 
       </li>
@@ -28,9 +29,10 @@ export class StockgrpListComponent implements OnInit, OnDestroy {
 
   @Input()
   public Groups: IGroupsWithStocksHierarchyMinItem[];
-  public groupUniqueName: any;
-  constructor(private route: ActivatedRoute) {
+  public groupUniqueName: string;
+  constructor(private store: Store<AppState>, private route: ActivatedRoute, private sideBarAction: SidebarAction) {
   }
+
   public ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.groupUniqueName = params['groupUniqueName'];
@@ -38,5 +40,11 @@ export class StockgrpListComponent implements OnInit, OnDestroy {
   }
   public ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  public OpenGroup(grp: IGroupsWithStocksHierarchyMinItem, e: Event) {
+    e.stopPropagation();
+    this.store.dispatch(this.sideBarAction.OpenGroup(grp.uniqueName));
+    this.store.dispatch(this.sideBarAction.GetInventoryGroup(grp.uniqueName));
   }
 }
--- src/app/store/Inventory/inventory.reducer.ts
+++ src/app/store/Inventory/inventory.reducer.ts
@@ -1,61 +1,134 @@
+import { GroupsWithStocksHierarchyMin } from '../../models/api-models/GroupsWithStocks';
+import { StockGroupResponse, StocksResponse } from '../../models/api-models/Inventory';
 import { IGroupsWithStocksHierarchyMinItem } from '../../models/interfaces/groupsWithStocks.interface';
 import { BaseResponse } from './../../models/api-models/BaseResponse';
 import { Action, ActionReducer } from '@ngrx/store';
 import * as _ from 'lodash';
+import { InventoryActionsConst } from '../../services/actions/inventory/inventory.const';
 
 /**
  * Keeping Track of the CompanyState
  */
 export interface InventoryState {
   groupsWithStocks?: IGroupsWithStocksHierarchyMinItem[];
+  activeGroup?: StockGroupResponse;
+  activeStock?: string;
 }
 const prepare = (mockData: IGroupsWithStocksHierarchyMinItem[]): IGroupsWithStocksHierarchyMinItem[] => {
   return _.orderBy(mockData.map((m) => {
     m = Object.assign({}, m, {
-      visibleChilds: false,
-      stocks: []
+      isActive: false,
+      isOpen: false
     });
-
     m.childStockGroups = prepare(m.childStockGroups);
     return m;
   }), 'name');
 };
+
 /**
  * Setting the InitialState for this Reducer's Store
  */
+// stocks: [{ uniqueName: 'sabji', name: 'Sabji' }, { uniqueName: 'kadi', name: 'Kadi' }]
 const initialState: InventoryState = {
-  groupsWithStocks: prepare([
-    { uniqueName: 'new', childStockGroups: [], name: 'new' },
-    {
-      uniqueName: 'subgroup',
-      childStockGroups: [
-        { uniqueName: 'test', childStockGroups: [], name: 'test' }
-      ],
-      name: 'Sub Group'
-    },
-    {
-      uniqueName: 'kartik',
-      childStockGroups: [
-        {
-          uniqueName: '1',
-          childStockGroups: [
-            {
-              uniqueName: 'www',
-              childStockGroups: [
-                { uniqueName: 'tttt', childStockGroups: [], name: 'ttttt' }
-              ],
-              name: 'www'
-            }],
-          name: '1'
-        }],
-      name: 'kartik'
-    }])
+  groupsWithStocks: []
 };
 
 export const InventoryReducer: ActionReducer<InventoryState> = (state: InventoryState = initialState, action: Action) => {
-
+  let groupUniqueName = '';
+  let groupArray: IGroupsWithStocksHierarchyMinItem[] = [];
+  let group: StockGroupResponse = null;
+  let activeGroupData: IGroupsWithStocksHierarchyMinItem;
   switch (action.type) {
+    case InventoryActionsConst.GetGroupsWithStocksHierarchyMinResponse:
+      if ((action.payload as BaseResponse<GroupsWithStocksHierarchyMin, string>).status === 'success') {
+        groupArray = (action.payload as BaseResponse<GroupsWithStocksHierarchyMin, string>).body.results;
+        return Object.assign({}, state, { groupsWithStocks: prepare(groupArray) });
+      }
+      return state;
+
+    case InventoryActionsConst.GetInventoryGroupResponse:
+      if ((action.payload as BaseResponse<StockGroupResponse, string>).status === 'success') {
+        groupArray = _.cloneDeep(state.groupsWithStocks);
+        group = (action.payload as BaseResponse<StockGroupResponse, string>).body;
+        for (let el of groupArray) {
+          activeGroupData = setRecursivlyStock(el.childStockGroups ? el.childStockGroups : [], group, null);
+          if (activeGroupData) {
+            break;
+          } else {
+            if (group.uniqueName === el.uniqueName) {
+              el.stocks = group.stocks;
+              break;
+            }
+          }
+          activeGroupData = null;
+        }
+        return Object.assign({}, state, { activeGroup: group, groupsWithStocks: groupArray });
+      }
+      return state;
+    case InventoryActionsConst.InventoryGroupToggleOpen:
+      groupUniqueName = action.payload;
+      groupArray = _.cloneDeep(state.groupsWithStocks);
+      if (groupUniqueName) {
+        for (let el of groupArray) {
+          activeGroupData = setRecursivlyActive(el.childStockGroups ? el.childStockGroups : [], groupUniqueName, null);
+          if (activeGroupData) {
+            el.isOpen = true;
+            el.isActive = false;
+          } else {
+            if (groupUniqueName === el.uniqueName) {
+              el.isOpen = !el.isOpen;
+              el.isActive = true;
+            } else {
+              el.isActive = false;
+            }
+          }
+          activeGroupData = null;
+        }
+      }
+      return Object.assign({}, state, {
+        groupsWithStocks: groupArray,
+      });
     default:
       return state;
   }
+};
+
+const setRecursivlyActive = (groups: IGroupsWithStocksHierarchyMinItem[], uniqueName: string, result: IGroupsWithStocksHierarchyMinItem) => {
+  for (let el of groups) {
+    if (el.uniqueName === uniqueName) {
+      el.isActive = true;
+      el.isOpen = !el.isOpen;
+      if (!result) { result = el; }
+    } else {
+      el.isActive = false;
+    }
+    if (el.childStockGroups && el.childStockGroups.length > 0 && !result) {
+      result = setRecursivlyActive(el.childStockGroups, uniqueName, result);
+      if (result) {
+        el.isOpen = true;
+        el.isActive = false;
+        result = el;
+      } else {
+        el.isActive = false;
+      }
+    }
+  }
+  return result;
+};
+
+const setRecursivlyStock = (groups: IGroupsWithStocksHierarchyMinItem[], group: StockGroupResponse, result: IGroupsWithStocksHierarchyMinItem) => {
+  for (let el of groups) {
+    if (el.uniqueName === group.uniqueName) {
+      el.stocks = group.stocks;
+      result = el;
+      return result;
+    }
+    if (el.childStockGroups && el.childStockGroups.length > 0 && !result) {
+      result = setRecursivlyActive(el.childStockGroups, group.uniqueName, result);
+      if (result) {
+        return result;
+      }
+    }
+  }
+  return result;
 };
