88b985976f291fcf7cade6a598e8d30410fda6d7
Kartik Upadhyay <kartik@walkover.in>
2017-06-30 21:32:51 +0530

    + kartik | Add Services for inventory module

--- src/app/models/interfaces/groupsWithStocks.interface.ts
+++ src/app/models/interfaces/groupsWithStocks.interface.ts
@@ -4,7 +4,7 @@ import { INameUniqueName } from './nameUniqueName.interface';
  * Interface for groups-with-stocks-flatten api response result item
  */
 export interface IGroupsWithStocksFlattenItem extends INameUniqueName {
-  stocks: INameUniqueName[];
+  stocks?: INameUniqueName[];
 }
 
 export interface IGroupsWithStocksHierarchyMinItem extends INameUniqueName {
