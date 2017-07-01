d3647b5b1dbd3a57647790ddd60cbedb72a28b71
govinda <govi2010@gmail.com>
2017-07-01 10:51:04 +0530

    Faizal | Custom Stock Unit

--- src/app/inventory/components/custom-stock-components/inventory.customstock.component.ts
+++ src/app/inventory/components/custom-stock-components/inventory.customstock.component.ts
@@ -1,13 +1,10 @@
 import { AppState } from '../../../store/roots';
-import { Actions } from '@ngrx/effects';
-
 import { Store } from '@ngrx/store';
-
-import {
-  Component,
-  OnInit
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginActions } from '../services/actions/login.action';
+import { StockUnitResponse } from '../../../models/api-models/Inventory';
+import { Observable } from 'rxjs/Observable';
+import { Select2OptionData } from '../../../shared/theme/select2/select2.interface';
 // import { Select2OptionData } from '../shared/theme/select2';
 
 @Component({
@@ -15,15 +12,48 @@ import { LoginActions } from '../services/actions/login.action';
   templateUrl: './inventory.customstock.component.html'
 })
 export class InventoryCustomStockComponent implements OnInit {
+  public stockUnitsDropDown$: Observable<Select2OptionData[]>;
+  public options: Select2Options = {
+    multiple: false,
+    width: '100%',
+    placeholder: 'Choose a parent unit'
+  };
+  public stockUnit$: Observable<StockUnitResponse[]>;
+  public editMode: boolean;
+  public customUnitObj: StockUnitResponse = new StockUnitResponse();
 
-  /**
-   * TypeScript public modifiers
-   */
   constructor(private store: Store<AppState>) {
+    this.stockUnit$ = this.store.select(p => p.inventory.stockUnits);
+    this.stockUnitsDropDown$ = this.store.select(p => {
+      let units = p.inventory.stockUnits;
+      return units.map(unit => {
+        return { text: unit.name, id: unit.code };
+      });
+    });
+
   }
+
   public ngOnInit() {
-    console.log('hello `Home` component');
+    console.log('hello `invetory-custom-stock` component');
     // this.exampleData = [
     // ];
+  }
+
+  public saveUnit(): any {
+
+  }
+
+  public deleteUnit(): any {
+
+  }
+
+  public editUnit(item: StockUnitResponse, index: number) {
+    this.customUnitObj = item;
+    this.editMode = true;
+  }
+
+  public clearFields() {
+    this.customUnitObj = new StockUnitResponse();
+    this.editMode = false;
   }
 }
