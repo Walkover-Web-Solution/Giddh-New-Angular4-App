e3a1f82dc1ed89077c2fa3956e734f10dbc24140
govinda <govi2010@gmail.com>
2017-07-04 15:01:25 +0530

    Faizal | Stock Report

--- src/app/inventory/components/stock-report-component/inventory.stockreport.component.ts
+++ src/app/inventory/components/stock-report-component/inventory.stockreport.component.ts
@@ -1,17 +1,12 @@
 import { AppState } from '../../../store/roots';
-import { Actions } from '@ngrx/effects';
 
 import { Store } from '@ngrx/store';
 
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import { LoginActions } from '../services/actions/login.action';
-// import { Select2OptionData } from '../shared/theme/select2';
+import { Component, OnInit } from '@angular/core';
 import { SidebarAction } from '../../../services/actions/inventory/sidebar.actions';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
+import { InventoryStockReportVM } from './inventory-stock-report.view-model';
 
 @Component({
   selector: 'invetory-stock-report',  // <home></home>
@@ -21,20 +16,31 @@ export class InventoryStockReportComponent implements OnInit {
   public sub: Subscription;
   public groupUniqueName: string;
   public stockUniqueName: string;
+  public vm: InventoryStockReportVM;
+
   /**
    * TypeScript public modifiers
    */
   constructor(private store: Store<AppState>, private route: ActivatedRoute, private sideBarAction: SidebarAction) {
+    this.vm = new InventoryStockReportVM();
   }
+
   public ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.groupUniqueName = params['groupUniqueName'];
       this.stockUniqueName = params['stockUniqueName'];
       if (this.groupUniqueName) {
-        debugger
         // this.store.dispatch(this.sideBarAction.OpenGroup(this.groupUniqueName));
         this.store.dispatch(this.sideBarAction.GetInventoryGroup(this.groupUniqueName, this.stockUniqueName));
       }
     });
+  }
+
+  public getStockReport() {
+    return false;
+  }
+
+  public goToManageStock() {
+    return false;
   }
 }
