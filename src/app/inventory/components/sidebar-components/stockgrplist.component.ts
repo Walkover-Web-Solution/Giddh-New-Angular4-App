a3cc79938b2c90b194d9d158bcd4c1b6930d831d
govinda <govi2010@gmail.com>
2017-07-04 15:01:25 +0530

    Faizal | Stock Report

--- src/app/inventory/components/sidebar-components/stockgrplist.component.ts
+++ src/app/inventory/components/sidebar-components/stockgrplist.component.ts
@@ -1,27 +1,28 @@
 import { AppState } from '../../../store/roots';
 import { IGroupsWithStocksHierarchyMinItem } from '../../../models/interfaces/groupsWithStocks.interface';
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
-import * as _ from 'lodash';
 import { SidebarAction } from '../../../services/actions/inventory/sidebar.actions';
 import { Store } from '@ngrx/store';
 @Component({
   selector: 'stockgrp-list',
   styles: [`
+    .active {
+      color: #d35f29 !important;
+    }
   `],
   template: `
-  <ul class="list-unstyled stock-grp-list mrT1" >
-      <li  (click)="OpenGroup(grp,$event)"  class="pdL" *ngFor="let grp of Groups">
-        <div [routerLink]="[ 'add-group', grp.uniqueName ]" [ngClass]="{'active': grp.isActive}" >{{grp.name}}</div>
+    <ul class="list-unstyled stock-grp-list mrT1">
+      <li (click)="OpenGroup(grp,$event)" class="pdL" *ngFor="let grp of Groups">
+        <div [routerLink]="[ 'add-group', grp.uniqueName ]" [ngClass]="{'active': grp.isActive}">{{grp.name}}</div>
         <i *ngIf="grp.childStockGroups.length > 0" class="icon-arrow-down" [ngClass]="{'open': grp.isOpen}"></i>
-          <stock-list [Groups]='grp' >
-          </stock-list>
+        <stock-list [Groups]='grp'>
+        </stock-list>
         <stockgrp-list [Groups]='grp.childStockGroups' *ngIf="grp.childStockGroups.length > 0 && grp.isOpen">
         </stockgrp-list>
-
       </li>
-  </ul>
+    </ul>
   `
 })
 export class StockgrpListComponent implements OnInit, OnDestroy {
@@ -31,6 +32,7 @@ export class StockgrpListComponent implements OnInit, OnDestroy {
   public Groups: IGroupsWithStocksHierarchyMinItem[];
   public groupUniqueName: string;
   public stockUniqueName: string;
+
   constructor(private store: Store<AppState>, private route: ActivatedRoute, private sideBarAction: SidebarAction) {
   }
 
@@ -40,6 +42,7 @@ export class StockgrpListComponent implements OnInit, OnDestroy {
       this.stockUniqueName = params['stockUniqueName'];
     });
   }
+
   public ngOnDestroy() {
     // this.sub.unsubscribe();
   }
