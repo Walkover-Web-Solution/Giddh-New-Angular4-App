4b52cf5a69d1ae12271c367e2f42d30536d17538
Kartik Upadhyay <kartik@walkover.in>
2017-07-06 19:18:42 +0530

    + kartik | Add response model for create ledger api

--- src/app/services/ledger.service.ts
+++ src/app/services/ledger.service.ts
@@ -9,7 +9,7 @@ import { BaseResponse } from '../models/api-models/BaseResponse';
 import { UserDetails } from '../models/api-models/loginModels';
 import { HandleCatch } from './catchManager/catchmanger';
 import { LEDGER_API } from './apiurls/ledger.api';
-import { TransactionsResponse, ReconcileResponse } from '../models/api-models/Ledger';
+import { TransactionsResponse, ReconcileResponse, LedgerResponse, LedgerRequest } from '../models/api-models/Ledger';
 
 @Injectable()
 export class LedgerService {
@@ -53,5 +53,22 @@ export class LedgerService {
       data.queryString = { accountUniqueName };
       return data;
     }).catch((e) => HandleCatch<ReconcileResponse, string>(e, '', { accountUniqueName }));
+  }
+
+  public CreateLEdger(model: LedgerRequest, accountUniqueName: string): Observable<BaseResponse<LedgerResponse, LedgerRequest>> {
+    this.store.take(1).subscribe(s => {
+      if (s.session.user) {
+        this.user = s.session.user.user;
+        this.companyUniqueName = s.session.companyUniqueName;
+      }
+    });
+    return this._http.post(LEDGER_API.CREATE.replace(':companyUniqueName', this.companyUniqueName).replace(':accountUniqueName', accountUniqueName), model)
+      .map((res) => {
+        let data: BaseResponse<LedgerResponse, LedgerRequest> = res.json();
+        data.request = model;
+        data.queryString = { accountUniqueName };
+        return data;
+      })
+      .catch((e) => HandleCatch<LedgerResponse, LedgerRequest>(e, model, { accountUniqueName }));
   }
 }
