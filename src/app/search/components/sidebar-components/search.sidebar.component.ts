e76dc210c22412f7e2e0fad8dc9d20d4d6899940
govinda <govi2010@gmail.com>
2017-07-07 19:01:07 +0530

    Govinda | Search modules

--- src/app/search/components/sidebar-components/search.sidebar.component.ts
+++ src/app/search/components/sidebar-components/search.sidebar.component.ts
@@ -8,22 +8,38 @@ import { LoginActions } from '../services/actions/login.action';
 import { Observable } from 'rxjs/Rx';
 import { SidebarAction } from '../../../services/actions/inventory/sidebar.actions';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'search-sidebar',  // <home></home>
   templateUrl: './search.sidebar.component.html'
 })
 export class SearchSidebarComponent implements OnInit, OnDestroy {
+
+  public showFromDatePicker: boolean;
+  public showToDatePicker: boolean;
+  public toDate: Date;
+  public fromDate: Date;
+  public moment = moment;
+  public searchForm: FormGroup;
   public searchLoader$: Observable<boolean>;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
   /**
    * TypeScript public modifiers
    */
-  constructor(private store: Store<AppState>, private sidebarAction: SidebarAction) {
+  constructor(private store: Store<AppState>, private _fb: FormBuilder) {
   }
 
   public ngOnInit() {
     //
+    this.searchForm = this._fb.group({
+      groupname: ['', [Validators.required]],
+      fromdate: ['', [Validators.required]],
+      todate: ['', [Validators.required]],
+      isSelfParent: [true]
+    });
   }
 
   public ngOnDestroy() {
