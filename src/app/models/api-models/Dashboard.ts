53c90987464d82cd1a146996f73245701ecd1055
Kartik Upadhyay <kartik@walkover.in>
2017-07-08 15:57:01 +0530

    + kartik | Add response models for group-history api

--- src/app/models/api-models/Dashboard.ts
+++ src/app/models/api-models/Dashboard.ts
@@ -1,4 +1,4 @@
-import { IPeriodBalances } from '../interfaces/dashboard.interface';
+import { IPeriodBalances, IGroupHistoryGroups } from '../interfaces/dashboard.interface';
 
 /**
  * Model for Audit Dashboard api request
@@ -6,10 +6,36 @@ import { IPeriodBalances } from '../interfaces/dashboard.interface';
   * In Request Payload 
  * A.   "fromDate" and "toDate" params will be sent 
  * interval values can be time unit we are seding monthly for now
+ * refresh is boolean
  * NOTE:: we are sending an extra header called 'X-Forwarded-For': res.locales.remoteIp
  * NOTE:: its response will be a hash containing a key logs
  */
 export class DashboardResponse {
   networth: IPeriodBalances[];
   profitLoss: IPeriodBalances[];
+}
+
+/**
+ * Model for group-history api request
+ * POST call
+ * API:: (group-history) company/:companyUniqueName/group-history?fromDate=:fromDate&toDate=:toDate&interval=:interval&refresh=refresh
+  * In Request Payload
+ * A. "fromDate" and "toDate" params will be sent
+ * refresh is boolean
+ * interval values can be time unit we are seding monthly for now
+ * NOTE:: its response will be a hash containing a GroupHistoryResponse
+ */
+
+export class GroupHistoryRequest {
+  groups?: string[];
+	accounts?: string[];
+	category?: string[];
+}
+
+/**
+ * NOTE:: as discussed accounts will be null always
+ */
+export class GroupHistoryResponse {
+  accounts?: any;
+  groups: IGroupHistoryGroups[];
 }
