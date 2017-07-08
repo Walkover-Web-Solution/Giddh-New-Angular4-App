96c5cdad6fc1e2851693f5c5ccd288068f926fc9
macbookpro <govi2010@gmail.com>
2017-07-08 13:12:39 +0530

    Govinda | Reload Router outlet on company Change

--- src/app/page.component.ts
+++ src/app/page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AppState } from './store/roots';
+import { Store } from '@ngrx/store';
+import { Component, AfterViewInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'page',
@@ -12,8 +16,28 @@ import { Component } from '@angular/core';
     </div>
    `
 })
-export class PageComponent {
+export class PageComponent implements AfterViewInit {
   // tslint:disable-next-line:no-empty
-  constructor() {
+  constructor(private store: Store<AppState>, private router: Router, private activatedRoute: ActivatedRoute, private location: Location) {
+  }
+  public ngAfterViewInit() {
+    // this.store.select(p => p.session.companyUniqueName).delay(2000).distinctUntilChanged().subscribe((companyName) => {
+    //   debugger;
+    //   if (this.activatedRoute.children && this.activatedRoute.children.length > 0) {
+
+    //     debugger;
+    //     this.router.navigateByUrl('/dummy', { skipLocationChange: true });
+    //     this.activatedRoute.firstChild.url.take(1).subscribe(p => {
+    //       debugger;
+    //       if (p.length > 0) {
+    //         setTimeout(() => {
+    //           this.router.navigate([p[0].path], { queryParams: p[0].parameters });
+    //         }, 1000);
+    //       }
+    //       console.log(p);
+    //     });
+
+    //   }
+    // });
   }
 }
