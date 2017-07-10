ab3843af08bc83cc3490d7d56890d848c1c12d3f
govinda <govi2010@gmail.com>
2017-07-10 19:49:00 +0530

    Faizal | Search Module - API - Link Fixes

--- src/app/services/apiurls/comapny.api.ts
+++ src/app/services/apiurls/comapny.api.ts
@@ -6,7 +6,7 @@ export const COMPANY_API = {
   COMPANY_LIST: Configuration.ApiUrl + 'users/:uniqueName/companies',
   CREATE_COMPANY: Configuration.ApiUrl + 'company',
   SEND_EMAIL: Configuration.ApiUrl + 'company/:companyUniqueName/accounts/bulk-email/?from=:from&to=:to',
-  SEND_SMS: Configuration.ApiUrl + 'company/:companyUniqueName/accounts/bulk-email',
+  SEND_SMS: Configuration.ApiUrl + 'company/:companyUniqueName/accounts/bulk-sms/?from=:from&to=:to',
   DELETE_COMPANY: Configuration.ApiUrl + 'company/:uniqueName',
   TAX: Configuration.ApiUrl + 'company/:companyUniqueName/users', // get call
   GET_COMPANY_USERS: Configuration.ApiUrl + 'company/:uniqueName',
