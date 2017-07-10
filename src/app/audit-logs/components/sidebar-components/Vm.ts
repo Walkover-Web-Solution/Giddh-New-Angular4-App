a6f672c76fc572ab13c7bb1a4b21b16b2632c751
govinda <govi2010@gmail.com>
2017-07-10 19:33:32 +0530

    Govinda | Audit Logs completed

--- src/app/audit-logs/components/sidebar-components/Vm.ts
+++ src/app/audit-logs/components/sidebar-components/Vm.ts
@@ -6,12 +6,9 @@ import * as moment from 'moment';
 import { Observable } from 'rxjs/Observable';
 export class AuditLogsSidebarVM {
   public user$: Observable<UserDetails>;
-  public selectedCompany: Observable<ComapnyResponse>;
-  public getLogsInprocess$: Observable<boolean>;
-  public showFromDatePicker: boolean = false;
-  public showToDatePicker: boolean = false;
-  public showLogDatePicker: boolean = false;
-  public canManageCompany: boolean = false;
+  public accounts$: Observable<Select2OptionData[]>;
+  public groups$: Observable<Select2OptionData[]>;
+  public users$: Observable<Select2OptionData[]>;
   public options: Select2Options = {
     multiple: false,
     width: '100%',
@@ -20,7 +17,7 @@ export class AuditLogsSidebarVM {
   };
   public moment = moment;
   public filters: any[] = [
-    { text: 'All', id: 'All' },
+    { text: 'All', id: '' },
     { text: 'create', id: 'create' },
     { text: 'delete', id: 'delete' },
     { text: 'share', id: 'share' },
@@ -35,26 +32,47 @@ export class AuditLogsSidebarVM {
     { text: 'ledger-excel-import', id: 'ledger-excel-import' }
   ];
   public entities: any[] = [
-    { text: 'All', id: 'All' },
+    { text: 'All', id: '' },
     { text: 'company', id: 'company' },
     { text: 'group', id: 'group' },
     { text: 'account', id: 'account' },
     { text: 'ledger', id: 'ledger' },
     { text: 'voucher', id: 'voucher' },
     { text: 'logs', id: 'logs' }];
+  public selectedCompany: Observable<ComapnyResponse>;
+  public getLogsInprocess$: Observable<boolean>;
+  public dateOptions: any[] = [{ text: 'Date Range', id: 1 }, { text: 'Entry/Log Date', id: 0 }];
+  public showFromDatePicker: boolean = false;
+  public showToDatePicker: boolean = false;
+  public showLogDatePicker: boolean = false;
+  public canManageCompany: boolean = false;
   public selectedOperation: string = '';
   public selectedEntity: string = '';
   public selectedUserUnq: string = '';
   public selectedAccountUnq: string = '';
-  public accounts$: Observable<Select2OptionData[]>;
-  public groups$: Observable<Select2OptionData[]>;
-  public users$: Observable<Select2OptionData[]>;
   public selectedGroupUnq: string = '';
   public selectedFromDate: Date;
   public selectedToDate: Date;
   public selectedLogDate: Date;
   public selectedEntryDate: Date;
-  public logOrEntry: string = '';
-  public dateOptions: any[] = [{ text: 'Date Range', id: 1 }, { text: 'Entry/Log Date', id: 0 }];
-  public selectedDateOption: string = '';
+  public logOrEntry: string = 'entryDate';
+  public selectedDateOption: string = '0';
+  public reset() {
+    this.showFromDatePicker = false;
+    this.showToDatePicker = false;
+    this.showLogDatePicker = false;
+    this.canManageCompany = false;
+    this.selectedOperation = '';
+    this.selectedEntity = '';
+    this.selectedUserUnq = '';
+    this.selectedAccountUnq = '';
+    this.selectedGroupUnq = '';
+    this.selectedFromDate = moment().toDate();
+    this.selectedToDate = moment().toDate();
+    this.selectedLogDate = moment().toDate();
+    this.selectedEntryDate = moment().toDate();
+    this.logOrEntry = 'entryDate';
+    this.selectedDateOption = '0';
+
+  }
 }
