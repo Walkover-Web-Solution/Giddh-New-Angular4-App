fb82be4f7e597fbc9623f2df22e90e8bcd7fd158
govinda <govi2010@gmail.com>
2017-07-11 19:53:40 +0530

    Govinda | add and manage new design

--- src/app/shared/header/components/index.ts
+++ src/app/shared/header/components/index.ts
@@ -1,5 +1,6 @@
 export * from './accounts-side-bar/accounts-side-bar.component';
-export * from './manage-groups-accounts/manage-groups-accounts.component';
+export * from './new-manage-groups-accounts/manage-groups-accounts.component';
+export * from './new-group-account-sidebar/groups-account-sidebar.component';
 export * from './company-add/company-add.component';
 export * from './account-operations/account-operations.component';
 export * from './groups-recursive-list/groups-recursive-list.component';
