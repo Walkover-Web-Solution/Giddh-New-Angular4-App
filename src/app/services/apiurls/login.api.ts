3c52efd412ad2678cf2e0e87ed94b3ffb60c4042
govinda <govi2010@gmail.com>
2017-07-15 17:48:00 +0530

    Login with Mobile

--- src/app/services/apiurls/login.api.ts
+++ src/app/services/apiurls/login.api.ts
@@ -4,9 +4,8 @@ import { Configuration } from '../../app.constant';
 export const LOGIN_API = {
   SignupWithEmail: Configuration.ApiUrl + 'signup-with-email',
   VerifyEmail: Configuration.ApiUrl + 'verify-email',
-  SignupWithMobile: Configuration.ApiUrl + 'app/api/get-login-otp',
-  VerifyOTP: Configuration.ApiUrl + 'verify-email',
+  SignupWithMobile: Configuration.ApiUrl + 'generate-otp',
+  VerifyOTP: Configuration.ApiUrl + 'verify-number',
   VerifyNumber: Configuration.ApiUrl + 'users/system_admin/verify-number',
   LoginWithNumber: Configuration.ApiUrl + 'login-with-number?countryCode=:countryCode&mobileNumber=:mobileNumber', // get call
-  VerifyLoginOTP: 'https://sendotp.msg91.com/api/verifyOTP', // post call
 };
