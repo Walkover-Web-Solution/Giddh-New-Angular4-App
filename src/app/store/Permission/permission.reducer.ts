3a3456dee431ec51a2d55707122aabb1adfe37ad
Arpit <mustafa@walkover.in>
2017-07-15 18:16:00 +0530

    + arpit: Create new role code added. and a temporary role stored in ngrx/store for new role creation

--- src/app/store/Permission/permission.reducer.ts
+++ src/app/store/Permission/permission.reducer.ts
@@ -5,29 +5,37 @@ import { BaseResponse } from '../../models/api-models/BaseResponse';
 import * as _ from 'lodash';
 
 export interface PermissionState {
-	roles: PermissionResponse[]
+	roles: PermissionResponse[],
+	newRole: object
 }
 
 export const initialState: PermissionState = {
-	roles:[]
+	roles: [],
+	newRole: {}
 };
 
 export function PermissionReducer(state = initialState, action: Action): PermissionState {
-    switch (action.type) {
+	switch (action.type) {
 
-    	case PERMISSION_ACTIONS.GET_ROLES_RESPONSE:
-    		{
-    			let newState= _.cloneDeep(state);
-    			let res = action.payload as BaseResponse<PermissionResponse[], string>;
-    			if (res.status === 'success'){
-    				newState.roles = res.body;
-    				return Object.assign({}, state, newState);
-    			}
-    			return state;
-    		}
-
-      default: {
-          return state;
-      }
-    }
+		case PERMISSION_ACTIONS.GET_ROLES_RESPONSE:
+			{
+				let newState = _.cloneDeep(state);
+				let res = action.payload as BaseResponse<PermissionResponse[], string>;
+				if (res.status === 'success') {
+					newState.roles = res.body;
+					return Object.assign({}, state, newState);
+				}
+				return state;
+			}
+		case PERMISSION_ACTIONS.CREATE_NEW_ROLE:
+			{
+				let newState = _.cloneDeep(state);
+				let res = action.payload;
+				newState.newRole = res;
+				return Object.assign({}, state, newState);
+			}
+		default: {
+			return state;
+		}
+	}
 }
