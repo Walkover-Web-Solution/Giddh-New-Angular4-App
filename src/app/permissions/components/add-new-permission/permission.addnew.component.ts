8f8b2630d07b75f15741108a8bf5eaa42c2a9dfa
Arpit <mustafa@walkover.in>
2017-07-17 19:13:32 +0530

    + arpit: Permission module second step code added.

--- src/app/permissions/components/add-new-permission/permission.addnew.component.ts
+++ src/app/permissions/components/add-new-permission/permission.addnew.component.ts
@@ -1,8 +1,16 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../store/roots';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
+
+export interface InewRole {
+  name: string,
+  copyoption: string,
+  pages: any[],
+  scopes: any[],
+}
 
 @Component({
   selector: 'permission-addnew',
@@ -13,11 +21,42 @@ export class AddNewPermissionComponent {
 
   private newRole: object = {};
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  private names: any;
+  private selectedAll: any;
 
-  constructor(private router: Router, private store: Store<AppState>) {
+  constructor(private router: Router, private store: Store<AppState>, private _location: Location) {
     this.store.select(p => p.permission.newRole).takeUntil(this.destroyed$).subscribe((role) => {
       this.newRole = role;
       console.log("++++++++ Hello the newRole information is :", this.newRole);
+      this.names = [
+        { name: 'Prashobh', selected: false },
+        { name: 'Abraham', selected: false },
+        { name: 'Anil', selected: false },
+        { name: 'Sam', selected: false },
+        { name: 'Natasha', selected: false },
+        { name: 'Marry', selected: false },
+        { name: 'Zian', selected: false },
+        { name: 'karan', selected: false },
+      ]
     });
+  }
+
+  public goToRoles() {
+    this._location.back();
+  }
+
+  public selectAll() {
+    for (var i = 0; i < this.names.length; i++) {
+      this.names[i].selected = this.selectedAll;
+    }
+  }
+  public checkIfAllSelected() {
+    this.selectedAll = this.names.every(function (item: any) {
+      return item.selected == true;
+    })
+  }
+
+  public checkPermission(page, permission) {
+    return true;
   }
 }
--- src/app/permissions/permission.routing.module.ts
+++ src/app/permissions/permission.routing.module.ts
@@ -22,11 +22,6 @@ import { AddNewPermissionComponent } from './components/add-new-permission/permi
                         path: 'permissions', component: PermissionComponent, canActivate: [NeedsAuthentication],
                         children: [
                             { path: 'add-new', pathMatch: 'full', component: AddNewPermissionComponent, canActivate: [NeedsAuthentication] },
-                            // { path: 'add-group/:groupUniqueName', component: InventoryAddGroupComponent, canActivate: [NeedsAuthentication], },
-                            // { path: 'add-group/:groupUniqueName/add-stock', component: InventoryAddStockComponent, canActivate: [NeedsAuthentication] },
-                            // { path: 'add-group/:groupUniqueName/add-stock/:stockUniqueName', component: InventoryAddStockComponent, canActivate: [NeedsAuthentication] },
-                            // { path: 'add-group/:groupUniqueName/stock-report/:stockUniqueName', component: InventoryStockReportComponent, canActivate: [NeedsAuthentication] },
-                            // { path: 'custom-stock', component: InventoryCustomStockComponent, canActivate: [NeedsAuthentication] },
                         ]
                     }
                 ]
