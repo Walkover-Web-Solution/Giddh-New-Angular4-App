5904fb2a0379a4f9bb61158839556215686ca371
Sarfaraz Ansari <sarfaraz@walkover.in>
2017-07-25 16:06:20 +0530

    fix: routing implementation in proper way

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { PageComponent } from './page.component';
-import { Routes } from '@angular/router';
+import { Routes, LoadChildren } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { PermissionComponent } from './permissions/permission.component';
 import { LoginComponent } from './login/login.component';
 import { NeedsAuthentication } from './services/decorators/needsAuthentication';
 import { UserAuthenticated } from './services/decorators/UserAuthenticated';
@@ -23,7 +22,7 @@ export const ROUTES: Routes = [
       { path: 'search', component: SearchComponent, canActivate: [NeedsAuthentication] },
       { path: 'audit-logs', component: AuditLogsComponent, canActivate: [NeedsAuthentication] },
       { path: 'dummy', component: AboutComponent },
-      { path: 'permissions', component: PermissionComponent, canActivate: [NeedsAuthentication] },
+      { path: 'permissions', loadChildren: 'app/permissions/permission.module#PermissionModule', canActivate: [NeedsAuthentication]}
     ]
   }
 ];
