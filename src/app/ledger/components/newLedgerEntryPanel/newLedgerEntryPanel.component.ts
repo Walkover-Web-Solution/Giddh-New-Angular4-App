36b3117314359548fa0ecdc7126ab3c8c9acc42b
Vishal Isharani <isharanivishal@gmail.com>
2017-07-25 19:56:25 +0530

    Vishal | ledger new entry popup

---
+++ src/app/ledger/components/newLedgerEntryPanel/newLedgerEntryPanel.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'new-ledger-entry-panel',
+  templateUrl: 'newLedgerEntryPanel.component.html'
+})
+
+export class NewLedgerEntryPanelComponent implements OnInit, OnDestroy {
+  constructor() {
+    //
+  }
+
+  public ngOnInit() {
+    //
+  }
+
+  public ngOnDestroy(): void {
+    //
+  }
+}
