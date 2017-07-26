a476d751f1eac6a0976982ea32c07759d0034b9e
Sarfaraz Ansari <sarfaraz@walkover.in>
2017-07-26 11:58:00 +0530

    fix: routing issue in permission

--- src/app/permissions/permission.component.ts
+++ src/app/permissions/permission.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 import { Location } from '@angular/common';
+
+class Dude extends NavigationStart {
+  public url: string;
+}
 
 @Component({
   template: '<router-outlet></router-outlet>'
 })
-export class PermissionComponent implements OnInit {
+export class PermissionComponent {
   constructor(private router: Router, private location: Location) {
+    this.router.events.subscribe((event: NavigationStart) => {
+      if (event.url === '/pages/permissions' || event.url === '/pages/permissions/') {
+        this.router.navigate(['/pages', 'permissions', 'list']);
+      }
+    });
   }
-  public ngOnInit() {
-    if (this.location.path() === '/pages/permissions' || this.location.path() === '/pages/permissions/') {
-      this.router.navigate(['/pages', 'permissions', 'list']);
-    }
-  }
+
+  // public this.router.setUpLocationChangeListener() {}
 }
