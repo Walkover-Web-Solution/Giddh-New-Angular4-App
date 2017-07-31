8029f221747788a6a29e95ecb83828a9f77ead99
Arpit <mustafa@walkover.in>
2017-07-31 19:20:37 +0530

    + arpit: Scrollbar max height added in header, manufacturing iten added in header list, manufacturing HTML added

--- src/app/manufacturing/manufacturing.component.ts
+++ src/app/manufacturing/manufacturing.component.ts
@@ -4,8 +4,7 @@ import { AppState } from '../store/roots';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // templateUrl: './inventory.component.html'
-  template: `<h2>Hey dude ManufacturingComponent</h2>`
+  templateUrl: './manufacturing.component.html'
 })
 
 export class ManufacturingComponent implements OnInit {
@@ -14,5 +13,28 @@ export class ManufacturingComponent implements OnInit {
 
   public ngOnInit() {
     console.log('hello ManufacturingComponent module');
+  }
+
+  private addExpense() {
+    // Add new expense
+  }
+
+  private addProduct() {
+    // Add new product
+  }
+
+  private save() {
+    // create expense in db
+    // this.store.dispatch(this.ManufacturingActions.CreateNew(data));
+  }
+
+  private update() {
+    // update expense
+    // this.store.dispatch(this.ManufacturingActions.Update(data));
+  }
+
+  private delete() {
+    // delete expense
+    // this.store.dispatch(this.ManufacturingActions.Delete(data));
   }
 }
