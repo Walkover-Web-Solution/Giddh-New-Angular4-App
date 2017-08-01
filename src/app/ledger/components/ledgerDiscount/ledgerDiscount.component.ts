4ae7717a998b43dc38b97ac196ceb87ee4de576f
Vishal Isharani <isharanivishal@gmail.com>
2017-08-01 20:03:35 +0530

    Vishal | ledger discount popup

--- src/app/ledger/components/ledgerDiscount/ledgerDiscount.component.ts
+++ src/app/ledger/components/ledgerDiscount/ledgerDiscount.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../store/roots';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { Observable } from 'rxjs/Observable';
-import { IFlattenGroupsAccountsDetail } from '../../../models/interfaces/flattenGroupsAccountsDetail.interface';
+import {
+  IFlattenGroupsAccountsDetail,
+  IFlattenGroupsAccountsDetailItem
+} from '../../../models/interfaces/flattenGroupsAccountsDetail.interface';
+import { ILedgerDiscount } from '../../../models/interfaces/ledger.interface';
 
 @Component({
   selector: 'ledger-discount',
@@ -31,8 +35,12 @@ import { IFlattenGroupsAccountsDetail } from '../../../models/interfaces/flatten
 })
 
 export class LedgerDiscountComponent implements OnInit, OnDestroy {
+  @Output() public discountUpdatedEvent: EventEmitter<ILedgerDiscount[]> = new EventEmitter();
+  @Output() public discountTotalUpdatedEvent: EventEmitter<number> = new EventEmitter();
   public discountAccountsList$: Observable<IFlattenGroupsAccountsDetail>;
+  public discountAccountsDetails: IFlattenGroupsAccountsDetailItem[] = [];
   public discountMenu: boolean = false;
+  public discountTotal: number = 0;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(private store: Store<AppState>) {
@@ -40,11 +48,70 @@ export class LedgerDiscountComponent implements OnInit, OnDestroy {
   }
 
   public ngOnInit() {
-    //
+    this.discountAccountsDetails = [];
+    this.prepareDiscountList();
+  }
+
+  /**
+   * prepare discountobj
+   */
+  public prepareDiscountList() {
+    let discountAccountsList: IFlattenGroupsAccountsDetail = null;
+    this.discountAccountsList$.take(1).subscribe(d => discountAccountsList = d);
+    discountAccountsList.accountDetails.map(acc => {
+      let disObj: IFlattenGroupsAccountsDetailItem = {
+        name: acc.name,
+        applicableTaxes: acc.applicableTaxes,
+        groupUniqueName: acc.groupUniqueName,
+        isOpen: acc.isOpen,
+        groupName: acc.groupName,
+        groupSynonyms: acc.groupSynonyms,
+        amount: acc.amount
+      };
+      this.discountAccountsDetails.push(disObj);
+    });
+  }
+
+  /**
+   * on change of discount amount
+   */
+  public change() {
+    this.discountTotal = this.generateTotal();
+    this.discountTotalUpdatedEvent.emit(this.discountTotal);
+    this.discountUpdatedEvent.emit(this.generateDiscountObject());
+    // this.discountUpdatedEvent.emit(this.discountAccountsDetails as ILedgerDiscount[]);
+  }
+
+  /**
+   * generate total of discount amount
+   * @returns {number}
+   */
+  public generateTotal() {
+    return this.discountAccountsDetails.reduce((pv, cv) => {
+      return cv.amount ?  pv + cv.amount : pv;
+    }, 0);
+  }
+
+  /**
+   * generate discount object as per ledger api require
+   * @returns {ILedgerDiscount[]}
+   */
+  public generateDiscountObject(): ILedgerDiscount[] {
+    let discountObj: ILedgerDiscount[] = [];
+    this.discountAccountsDetails.map(d => {
+      discountObj.push({
+        name: d.name,
+        uniqueName: d.groupUniqueName,
+        amount: d.amount
+      });
+    });
+    return discountObj;
   }
 
   public ngOnDestroy(): void {
     this.destroyed$.next(true);
     this.destroyed$.complete();
+    this.discountUpdatedEvent.unsubscribe();
+    this.discountTotalUpdatedEvent.unsubscribe();
   }
 }
