3f432a524b5ec9aac4ae8321d6433cf15b68ecb8
Sarfaraz Ansari <sarfaraz@walkover.in>
2017-08-01 10:44:11 +0530

    setup: manufacturing action

--- src/app/manufacturing/manufacturing.component.ts
+++ src/app/manufacturing/manufacturing.component.ts
@@ -2,13 +2,17 @@ import { Actions } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { AppState } from '../store/roots';
 import { Component, OnInit } from '@angular/core';
+import { ManufacturingActions } from '../services/actions/manufacturing/manufacturing.actions';
 
 @Component({
   templateUrl: './manufacturing.component.html'
 })
 
 export class ManufacturingComponent implements OnInit {
-  constructor(private store: Store<AppState>) {
+  constructor(
+    private store: Store<AppState>,
+    private manufacturingActions: ManufacturingActions
+  ) {
   }
 
   public ngOnInit() {
@@ -25,16 +29,16 @@ export class ManufacturingComponent implements OnInit {
 
   private save() {
     // create expense in db
-    // this.store.dispatch(this.ManufacturingActions.CreateNew(data));
+    this.store.dispatch(this.manufacturingActions.CreateMfItem(data));
   }
 
   private update() {
     // update expense
-    // this.store.dispatch(this.ManufacturingActions.Update(data));
+    this.store.dispatch(this.manufacturingActions.Update(data));
   }
 
   private delete() {
     // delete expense
-    // this.store.dispatch(this.ManufacturingActions.Delete(data));
+    this.store.dispatch(this.manufacturingActions.Delete(data));
   }
 }
