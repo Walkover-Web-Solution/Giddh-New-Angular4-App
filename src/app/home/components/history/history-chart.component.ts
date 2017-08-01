49b882de36c68cac2e8d129cd0ea1dfaa34cf412
govinda <govi2010@gmail.com>
2017-08-01 19:14:10 +0530

    Faizal | Dashboard -  Charts current : Monthly and Yearly complete...

--- src/app/home/components/history/history-chart.component.ts
+++ src/app/home/components/history/history-chart.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Options } from 'highcharts';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { IComparisionChartResponse } from '../../../models/interfaces/dashboard.interface';
+import { Observable } from 'rxjs/Observable';
+import { ActiveFinancialYear, ComapnyResponse } from '../../../models/api-models/Company';
+import { HomeActions } from '../../../services/actions/home/home.actions';
+import { AppState } from '../../../store/roots';
+import { Store } from '@ngrx/store';
+import { isNullOrUndefined } from 'util';
+import * as  moment from 'moment';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'history-chart',
@@ -8,54 +18,123 @@ import { Options } from 'highcharts';
 
 export class HistoryChartComponent implements OnInit {
   public options: Options;
+  public activeFinancialYear: ActiveFinancialYear;
+  public lastFinancialYear: ActiveFinancialYear;
+  public companies$: Observable<ComapnyResponse[]>;
+  public activeCompanyUniqueName$: Observable<string>;
+  public comparisionChartData$: Observable<IComparisionChartResponse>;
+  public requestInFlight = true;
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  private monthArray = ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar'];
+  private expenseData = [];
+  private expenseDataLY = [];
+  private revenueData = [];
+  private revenueDataLY = [];
+  private profitLossData = [];
+  private profitLossDataLY = [];
 
-  constructor() {
+  constructor(private store: Store<AppState>, private _homeActions: HomeActions) {
+//
+    this.activeCompanyUniqueName$ = this.store.select(p => p.session.companyUniqueName).takeUntil(this.destroyed$);
+    this.companies$ = this.store.select(p => p.company.companies).takeUntil(this.destroyed$);
+    this.comparisionChartData$ = this.store.select(p => p.home.comparisionChart).takeUntil(this.destroyed$);
+  }
+
+  public fetchChartData() {
+    this.requestInFlight = true;
+    this.expenseData = [];
+    this.store.dispatch(this._homeActions.getComparisionChartDataOfActiveYear(
+      this.activeFinancialYear.financialYearStarts,
+      this.activeFinancialYear.financialYearEnds, false));
+
+    this.store.dispatch(this._homeActions.getComparisionChartDataOfLastYear(
+      this.lastFinancialYear.financialYearStarts,
+      this.lastFinancialYear.financialYearEnds, false));
+
+  }
+
+  public generateCharts() {
     this.options = {
       title: {
-        text: 'Solar Employment Growth by Sector, 2010-2016'
+        text: ''
       },
-
-      subtitle: {
-        text: 'Source: thesolarfoundation.com'
-      },
-
       yAxis: {
         title: {
-          text: 'Number of Employees'
+          text: ''
         }
+      },
+      xAxis: {
+        categories: this.monthArray
       },
       legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
+        layout: 'horizontal',
+        align: 'center',
+        verticalAlign: 'bottom',
+        itemStyle: { color: '#333333', cursor: 'pointer', fontSize: '10px', fontWeight: 'bold' }
       },
-
-      plotOptions: {
-        series: {
-          pointStart: 2010
-        }
-      },
-
       series: [{
-        name: 'Installation',
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+        name: 'Expense',
+        data: this.expenseData
       }, {
-        name: 'Manufacturing',
-        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+        name: 'Revenue',
+        data: this.revenueData
       }, {
-        name: 'Sales & Distribution',
-        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+        name: 'Profit/Loss',
+        data: this.profitLossData
       }, {
-        name: 'Project Development',
-        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+        name: 'LY Expense',
+        data: this.expenseDataLY
       }, {
-        name: 'Other',
-        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+        name: 'LY Revenue',
+        data: this.revenueDataLY
+      }, {
+        name: 'LY Profit/Loss',
+        data: this.profitLossDataLY
       }]
     };
   }
 
   public ngOnInit() {
     //
+    this.comparisionChartData$
+      .skipWhile(p => (isNullOrUndefined(p) || isNullOrUndefined(p.ProfitLossActiveYear) || isNullOrUndefined(p.revenueLastYear) || isNullOrUndefined(p.revenueActiveYear)))
+      .distinctUntilChanged((p, q) => p.ExpensesActiveYearly === this.expenseData)
+      .subscribe(p => {
+        this.expenseData = (p.ExpensesActiveYearly);
+        this.expenseDataLY = (p.ExpensesLastYearYearly);
+        this.revenueData = (p.revenueActiveYearYearly);
+        this.revenueDataLY = (p.revenueLastYearYearly);
+        this.profitLossData = p.ProfitLossActiveYearYearly;
+        this.generateCharts();
+        this.requestInFlight = false;
+      });
+    this.companies$.subscribe(c => {
+      if (c) {
+        let activeCompany: ComapnyResponse;
+        let activeCmpUniqueName = '';
+        let financialYears = [];
+        this.activeCompanyUniqueName$.take(1).subscribe(a => {
+          activeCmpUniqueName = a;
+          activeCompany = c.find(p => p.uniqueName === a);
+          this.activeFinancialYear = activeCompany.activeFinancialYear;
+        });
+        if (this.activeFinancialYear) {
+          for (let cmp of c) {
+            if (cmp.uniqueName === activeCmpUniqueName) {
+              if (cmp.financialYears.length > 1) {
+                financialYears = cmp.financialYears.filter(cm => cm.uniqueName !== this.activeFinancialYear.uniqueName);
+                financialYears = _.orderBy(financialYears, (it) => {
+                  return moment(it.financialYearStarts, 'DD-MM-YYYY');
+                }, 'desc');
+                this.lastFinancialYear = financialYears[0];
+              }
+            }
+          }
+        }
+        // this.fetchChartData();
+      }
+    });
+
+
   }
 }
