c625694eb4d764a04db4e34c6c34b03536847bd3
Arpit <mustafa@walkover.in>
2017-08-02 17:46:07 +0530

    Manufacturing edit part midway

--- src/app/services/manufacturing.service.ts
+++ src/app/services/manufacturing.service.ts
@@ -127,4 +127,24 @@ export class ManufacturingService {
       })
       .catch((e) => HandleCatch<StocksResponse, IMfStockSearchRequest>(e, ''));
   }
+
+  /**
+   * get stock with rate
+   * URL:: /company/:companyUniqueName/stock/:stockUniqueName/link-with-rates
+   */
+  public GetStockWithRate(model: IManufacturingUnqItemObj): Observable<BaseResponse<ICommonResponseOfManufactureItem, string>> {
+    this.store.take(1).subscribe(s => {
+      if (s.session.user) {
+        this.user = s.session.user.user;
+      }
+      this.companyUniqueName = s.session.companyUniqueName;
+    });
+    return this._http.get(MANUFACTURING_API.GET_STOCK_WITH_RATE.replace(':companyUniqueName', this.companyUniqueName).replace(':stockUniqueName', model.stockUniqueName))
+      .map((res) => {
+        let data: BaseResponse<ICommonResponseOfManufactureItem, string> = res.json();
+        data.queryString = model;
+        return data;
+      })
+      .catch((e) => HandleCatch<ICommonResponseOfManufactureItem, string>(e, ''));
+  }
 }
