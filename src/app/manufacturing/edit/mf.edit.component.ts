062bee0713724cadb40b25b99100b19168ad7257
Arpit <mustafa@walkover.in>
2017-08-02 15:31:58 +0530

    add: Manufacturing edit page code

--- src/app/manufacturing/edit/mf.edit.component.ts
+++ src/app/manufacturing/edit/mf.edit.component.ts
@@ -3,20 +3,58 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../store/roots';
 import { Component, OnInit } from '@angular/core';
 import { ManufacturingActions } from '../../services/actions/manufacturing/manufacturing.actions';
+import { InventoryAction } from '../../services/actions/inventory/inventory.actions';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { Observable } from 'rxjs/Observable';
+import { Select2OptionData } from '../../shared/theme/select2/select2.interface';
 
 @Component({
   templateUrl: './mf.edit.component.html'
 })
 
 export class MfEditComponent implements OnInit {
+
+  public stockListDropDown$: Observable<Select2OptionData[]>;
+  public consumptionDetail = [];
+  public options: Select2Options = {
+    multiple: false,
+    width: '100%',
+    placeholder: 'Select'
+  };
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
   constructor(
     private store: Store<AppState>,
-    private manufacturingActions: ManufacturingActions
+    private manufacturingActions: ManufacturingActions,
+    private inventoryAction: InventoryAction
   ) {
   }
 
   public ngOnInit() {
     console.log('hello from MfEditComponent');
+    // dispatch stocklist request
+    this.store.select(p => p.inventory).takeUntil(this.destroyed$).subscribe((o: any) => {
+      if (!o.stocksList) {
+        this.store.dispatch(this.inventoryAction.GetStock());
+      }
+    });
+    // get all stocks
+    this.stockListDropDown$ = this.store.select(p => {
+      if (p.inventory.stocksList) {
+        console.log('The Stock List is available :', p.inventory.stocksList);
+        if (p.inventory.stocksList.results) {
+          let units = p.inventory.stocksList.results;
+
+          return units.map(unit => {
+            return { text: unit.name , id: unit.uniqueName };
+          });
+        }
+      }
+    }).takeUntil(this.destroyed$);
+  }
+
+  private checkIfLinkedStockIsUnique(ev) {
+    console.log('list updated the val is :', ev.value);
   }
 
   private addExpense() {
