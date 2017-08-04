3e77c8e8f8aec24b91d02398d5b61f034de55c48
Govinda <govi2010@gmail.com>
2017-08-04 15:01:07 +0530

    Govinda | BankAccount List

--- src/app/services/apiurls/dashboard.api.ts
+++ src/app/services/apiurls/dashboard.api.ts
@@ -3,5 +3,8 @@ import { Configuration } from '../../app.constant';
 export const DASHBOARD_API = {
   DASHBOARD: Configuration.ApiUrl + 'company/:companyUniqueName/dashboard?from=:from&to=:to&interval=:interval&refresh=:refresh', // get call
   GROUP_HISTORY: Configuration.ApiUrl + 'company/:companyUniqueName/group-history?from=:from&to=:to&interval=:interval&refresh=:refresh', // post call
-  CLOSING_BALANCE: Configuration.ApiUrl + '/company/:companyUniqueName/groups/:groupUniqueName/closing-balance?fromDate=:fromDate&toDate=:toDate&refresh=:refresh' // get call
+  CLOSING_BALANCE: Configuration.ApiUrl + '/company/:companyUniqueName/groups/:groupUniqueName/closing-balance?fromDate=:fromDate&toDate=:toDate&refresh=:refresh', // get call
+  BANK_ACCOUNTS: Configuration.ApiUrl + '/company/:companyUniqueName/ebanks', // Get call
+  REFRESH_BANK_ACCOUNT: Configuration.ApiUrl + '/company/:companyUniqueName/login/:loginId/token/refresh', // get call
+  RECONNECT_BANK_ACCOUNT: Configuration.ApiUrl + '/company/:companyUniqueName/login/:loginId/token/reconnect',
 };
