1321d246ba8421a78ba2894db520dc2d4f4a1044
Govinda <govi2010@gmail.com>
2017-08-04 16:29:45 +0530

    Govinda | Live Bankaccount

--- src/app/home/components/live-accounts/live-accounts.component.ts
+++ src/app/home/components/live-accounts/live-accounts.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { BankAccountsResponse } from '../../../models/api-models/Dashboard';
+import { HomeActions } from '../../../services/actions/home/home.actions';
+import { AppState } from '../../../store/roots';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 @Component({
   selector: 'live-accounts',
   templateUrl: 'live-accounts.component.html'
 })
 
-export class LiveAccountsComponent implements OnInit {
-  constructor() {
-  //
+export class LiveAccountsComponent implements OnInit, OnDestroy {
+  public bankAccounts$: Observable<BankAccountsResponse[]>;
+  public isGetBankAccountsInProcess$: Observable<boolean>;
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  constructor(private store: Store<AppState>, private _homeActions: HomeActions) {
+    this.isGetBankAccountsInProcess$ = this.store.select(p => p.home.isGetBankAccountsInProcess).takeUntil(this.destroyed$);
+    this.bankAccounts$ = this.store.select(p => p.home.BankAccounts).takeUntil(this.destroyed$);
   }
 
   public ngOnInit() {
-  //
+    //
+    this.reload();
+  }
+  public reload() {
+    this.store.dispatch(this._homeActions.GetBankAccount());
+  }
+  public ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 }
