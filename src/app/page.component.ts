aa3f25d4825ec5d9ea23470547cb8d98b0cc942d
Vishal Isharani <isharanivishal@gmail.com>
2017-08-05 15:58:55 +0530

    Vishal | EL-151

--- src/app/page.component.ts
+++ src/app/page.component.ts
@@ -1,26 +1,42 @@
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AppState } from './store/roots';
 import { Store } from '@ngrx/store';
-import { Component, AfterViewInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 @Component({
   selector: 'page',
-  template: `<div id="main">
-    <app-header></app-header>
-    <layout-main>
-      <router-outlet></router-outlet>
-    </layout-main>
-    <app-footer></app-footer>
-  </div>`
+  template: `
+    <div id="main">
+      <app-header></app-header>
+      <layout-main>
+        <router-outlet></router-outlet>
+      </layout-main>
+      <app-footer></app-footer>
+    </div>`
 })
-export class PageComponent implements AfterViewInit {
+export class PageComponent implements AfterViewInit, OnInit, OnDestroy {
   // tslint:disable-next-line:no-empty
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
   constructor(private store: Store<AppState>, private router: Router, private activatedRoute: ActivatedRoute, private location: Location) {
   }
+
+  public ngOnInit() {
+    this.router.events.takeUntil(this.destroyed$).subscribe((event) => {
+      window.scrollTo(0, 0);
+    });
+  }
+
   public ngAfterViewInit() {
     if (this.location.path() === '/pages') {
       this.router.navigate(['/pages', 'home']);
     }
+  }
+
+  public ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 }
