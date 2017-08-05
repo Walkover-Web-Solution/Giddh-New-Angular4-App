d58e814f1ed860c60ec00b21273736475960b8a5
Govinda <govi2010@gmail.com>
2017-08-05 20:48:21 +0530

    Govinda | Bank account refresh

--- src/app/home/components/comparision/comparision-chart.component.ts
+++ src/app/home/components/comparision/comparision-chart.component.ts
@@ -96,6 +96,9 @@ export class ComparisionChartComponent implements OnInit {
 
   public generateCharts() {
     this.options = {
+      chart: {
+        height: '320px',
+      },
       title: {
         text: ''
       },
