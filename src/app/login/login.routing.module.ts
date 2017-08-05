be7aa7b3542932f680d1432a1c77daeb485d24b8
Vishal Isharani <isharanivishal@gmail.com>
2017-08-05 12:08:45 +0530

    Vishal | EL-154

--- src/app/login/login.routing.module.ts
+++ src/app/login/login.routing.module.ts
@@ -3,12 +3,13 @@ import { PageComponent } from '../page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NeedsAuthentication } from '../services/decorators/needsAuthentication';
+import { UserAuthenticated } from '../services/decorators/UserAuthenticated';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       {
-        path: '', component: LoginComponent, canActivate: [NeedsAuthentication]
+        path: '', component: LoginComponent, canActivate: [UserAuthenticated]
       }
     ])
   ],
