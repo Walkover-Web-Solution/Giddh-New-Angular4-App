a634e6bad0fe48608eca1316d0f87dfed66ef295
Arpit <mustafa@walkover.in>
2017-08-07 17:31:46 +0530

    Setting profile midway

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { TabsModule } from 'ngx-bootstrap';
 import { SettingsComponent } from './settings.component';
 import { SettingIntegrationComponent } from './integration/setting.integration.component';
+import { SettingProfileComponent } from './profile/setting.profile.component';
 
 @NgModule({
   declarations: [
     // components here
     SettingsComponent,
-    SettingIntegrationComponent
+    SettingIntegrationComponent,
+    SettingProfileComponent
   ],
   imports: [
     CommonModule,
