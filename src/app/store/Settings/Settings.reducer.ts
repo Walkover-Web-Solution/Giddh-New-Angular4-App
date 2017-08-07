6a663ee6778a6b3faba844928c80f27aae14e3d5
Sarfaraz Ansari <sarfaraz@walkover.in>
2017-08-07 18:33:56 +0530

    setup: integration tab email and sms key get part done

--- src/app/store/Settings/Settings.reducer.ts
+++ src/app/store/Settings/Settings.reducer.ts
@@ -2,23 +2,30 @@ import * as _ from 'lodash';
 import { Action } from '@ngrx/store';
 import { BaseResponse } from '../../models/api-models/BaseResponse';
 import { SETTINGS_INTEGRATION_ACTIONS } from '../../services/actions/settings/settings.integration.const';
-import { SmsKeyClass, IntegrationPage, IntegrationPageClass } from '../../models/api-models/SettingsIntegraion';
+import { SmsKeyClass, IntegrationPage, IntegrationPageClass, EmailKeyClass } from '../../models/api-models/SettingsIntegraion';
 
 export interface SettingsState {
-    integration: IntegrationPage;
+  integration: IntegrationPage;
 }
 
 export const initialState: SettingsState = {
-    integration: new IntegrationPageClass()
+  integration: new IntegrationPageClass()
 };
 
 export function SettingsReducer(state = initialState, action: Action): SettingsState {
+  let newState = _.cloneDeep(state);
   switch (action.type) {
     case SETTINGS_INTEGRATION_ACTIONS.GET_SMS_KEY_RESPONSE:
-      let newState = _.cloneDeep(state);
       let res: BaseResponse<SmsKeyClass, string> = action.payload;
       if (res.status === 'success') {
         newState.integration.smsForm = res.body;
+        return Object.assign({}, state, newState);
+      }
+      return state;
+    case SETTINGS_INTEGRATION_ACTIONS.GET_EMAIL_KEY_RESPONSE:
+      let r: BaseResponse<EmailKeyClass, string> = action.payload;
+      if (r.status === 'success') {
+        newState.integration.emailForm = r.body;
         return Object.assign({}, state, newState);
       }
       return state;
