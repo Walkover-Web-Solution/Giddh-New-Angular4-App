03d65991326b8aaf9666da7bcb9306575548ee8f
Sarfaraz Ansari <sarfaraz@walkover.in>
2017-08-08 12:47:41 +0530

    feat: razorpay setup done

--- src/app/settings/integration/setting.integration.component.ts
+++ src/app/settings/integration/setting.integration.component.ts
@@ -6,25 +6,43 @@ import { AppState } from '../../store/roots';
 import { SettingsIntegrationActions } from '../../services/actions/settings/settings.integration.action';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import * as _ from 'lodash';
-import { SmsKeyClass, EmailKeyClass } from '../../models/api-models/SettingsIntegraion';
+import { SmsKeyClass, EmailKeyClass, RazorPayClass } from '../../models/api-models/SettingsIntegraion';
+import { Observable } from 'rxjs/Observable';
+import { Select2OptionData } from '../../shared/theme/select2/select2.interface';
+import { AccountService } from '../../services/account.service';
 
 @Component({
   selector: 'setting-integration',
-  templateUrl: './setting.integration.component.html'
+  templateUrl: './setting.integration.component.html',
+  styles: [`
+    #inlnImg img{
+      max-height:18px;
+    }
+  `]
 })
 export class SettingIntegrationComponent implements OnInit {
 
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
   private smsFormObj: SmsKeyClass = new SmsKeyClass();
   private emailFormObj: EmailKeyClass = new EmailKeyClass();
+  private razorPayObj: RazorPayClass = new RazorPayClass();
+  private accounts$: Observable<Select2OptionData[]>;
+  private select2Options: Select2Options = {
+    multiple: false,
+    width: '100%',
+    placeholder: 'Select Accounts',
+    allowClear: true
+  };
 
   constructor(
     private router: Router,
     private store: Store<AppState>,
-    private SettingsIntegrationActions: SettingsIntegrationActions
+    private SettingsIntegrationActions: SettingsIntegrationActions,
+    private accountService: AccountService
   ) {}
 
   public ngOnInit() {
+    this.setDummyData();
     this.store.dispatch(this.SettingsIntegrationActions.GetSMSKey());
     this.store.dispatch(this.SettingsIntegrationActions.GetEmailKey());
     // getting all page data of integration page
@@ -38,6 +56,24 @@ export class SettingIntegrationComponent implements OnInit {
         this.emailFormObj = o.emailForm;
       }
     });
+
+    // get accounts
+    this.accountService.GetFlattenAccounts('', '').takeUntil(this.destroyed$).subscribe(data => {
+      if (data.status === 'success') {
+        let accounts: Select2OptionData[] = [];
+        _.forEach(data.body.results, (item) => {
+          accounts.push({ text: item.name, id: item.uniqueName });
+        });
+        this.accounts$ = Observable.of(accounts);
+      }
+    });
+  }
+
+  private setDummyData() {
+    this.razorPayObj.userName = 'dude';
+    this.razorPayObj.password = 'haklg89u3430';
+    this.razorPayObj.account = {};
+    this.razorPayObj.autoCapturePayment = true;
   }
 
   private onSubmitMsgform(f: NgForm) {
@@ -56,5 +92,13 @@ export class SettingIntegrationComponent implements OnInit {
   }
   private onCancelEmailform() {
     console.log('onCancelEmailform');
+  }
+
+  private toggleCheckBox() {
+    return this.razorPayObj.autoCapturePayment = !this.razorPayObj.autoCapturePayment;
+  }
+
+  private selectAccount(event) {
+    console.log (event, 'event');
   }
 }
