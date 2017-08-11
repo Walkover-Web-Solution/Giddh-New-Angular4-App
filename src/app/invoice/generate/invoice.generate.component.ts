8420f8cbaa5dc13ed79ffe7a2e23b8686825bb30
Sarfaraz Ansari <sarfaraz_designer@live.in>
2017-08-11 17:09:23 +0530

    setup: create invoice component

--- src/app/invoice/generate/invoice.generate.component.ts
+++ src/app/invoice/generate/invoice.generate.component.ts
@@ -1,19 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import * as _ from 'lodash';
+import { NgForm } from '@angular/forms';
+const counts = [12, 25, 50, 100];
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 
 @Component({
+  styleUrls: ['./invoice.generate.component.css'],
   templateUrl: './invoice.generate.component.html'
 })
 export class InvoiceGenerateComponent implements OnInit {
 
-  // private navItems: INameUniqueName[] = INV_PAGE;
-  // private selectedPage: string = null;
+  private modalRef: BsModalRef;
+  private config = {
+    animated: true,
+    keyboard: false,
+    backdrop: true,
+    ignoreBackdropClick: true
+  };
+  private counts: number[] = counts;
 
-  constructor() {
-    console.log('jingo');
-  }
+  constructor(private modalService: BsModalService) {}
 
   public ngOnInit() {
-    console.log('bingo');
+    console.log('from InvoiceGenerateComponent');
   }
+
+  private showInvoiceModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, Object.assign({}, this.config, {class: 'gray modal-liquid'}));
+  }
+
+  private closeInvoiceModel(e) {
+    console.log(e, 'closeInvoiceModel');
+    this.modalRef.hide();
+  }
+
+  private getInvoiceByFilters(f: NgForm) {
+    console.log (f, 'onSubmitInvoiceForm');
+  }
+
+  private showNewInvoiceCreate() {
+    console.log ('showNewInvoiceCreate open modal');
+  }
+
 }
