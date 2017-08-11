f499bcf270303f575a010cb9e4880c3d2c88ad8d
Kunal Saxena <kunal.saxena@actify.de>
2017-08-11 18:18:16 +0530

    added scaffolding for content filters

--- src/app/invoice/edit-invoice/filters-container/content-filters/content.filters.component.ts
+++ src/app/invoice/edit-invoice/filters-container/content-filters/content.filters.component.ts
@@ -13,27 +13,12 @@ import {InvoiceAction} from "../../../../services/actions/invoice/invoice.action
   styleUrls: ['content.filters.component.css']
 })
 
-export class ContentFilterComponent  {
+export class ContentFilterComponent {
   @Input() public content: boolean;
   // @Input() public contentData: Content;
-  public enableheading: boolean = true;
-  public enableGSTIN: boolean = true;
-  public enablePAN: boolean = true;
-  public enableInvoiceDate: boolean = true;
-  public enableInvoiceNumber: boolean = true;
-  public dueDate: boolean = true;
-  public enableCompanyName: boolean = true;
-  public enableCompanyAddress: boolean = true;
-  public enableShipDate: boolean = true;
-  public enableShipVia: boolean = true;
-  public enableTrackingNo: boolean = true;
-  public enableCustomerName: boolean = true;
-  public enableCustomerCompanyName: boolean = true;
-  public enableCustomerEmailId: boolean = false;
-  public enableCustomerMobileNo: boolean = false;
-  public enableShippingAddress: boolean = true;
-  public enableShippingAddressState: boolean = true;
-  public enableShippingGSTIN: boolean =  true;
+  public GSTIN: string;
+  public PAN: string;
+  public companyName: string;
   public myValue: number;
   public itemSlider: string;
   public item: string;
@@ -53,165 +38,152 @@ export class ContentFilterComponent  {
   public showTemplate(id) {
     this.store.dispatch(this.invoiceAction.setTemplateId(id));
   }
+
 // -------------------GSTIN--------------------
   public onGSTINChange(data) {
     // this.store.dispatch(this.invoiceAction.setTemplateId(id));
-    this.store.dispatch(this.invoiceAction.setGSTIN(data));
+    this.store.dispatch(this.invoiceAction.updateGSTIN(data));
     console.log(data);
   }
 
-  public onCheckGSTIN(check) {
-    if (check === 'false') {
-      this.enableGSTIN = false;
-    }
-  }
-// ---------CompanyName and CompanyAddress------------
+  // public onCheckGSTIN(check) {
+  //   if (check === 'false') {
+  //     this.enableGSTIN = false;
+  //   }
+  // }
 
-  public onCheckCompanyAddress(check) {
-    if (check === 'false') {
-      this.enableCompanyAddress = false;
-    }
-  }
-
-  public onCheckCompanyName(check) {
-    if (check === 'false') {
-      this.enableCompanyName = false;
-    }
-  }
   // ----------PAN-----------------
   public onPANChange(data) {
     // this.store.dispatch(this.invoiceAction.setTemplateId(id));
-  //  this.store.dispatch(this.invoiceAction.setPAN(data));
+    this.store.dispatch(this.invoiceAction.updatePAN(data));
     console.log(data);
   }
 
-  public onCheckPAN(check) {
-    if (check === 'false') {
-      this.enablePAN = false;
-    }
-  }
 
   // -----------InvoiceDate------------
 
   public onInvoiceDateChange(data) {
     // this.store.dispatch(this.invoiceAction.setTemplateId(id));
-  //  this.store.dispatch(this.invoiceAction.setInvoiceDate(data));
+    this.store.dispatch(this.invoiceAction.updateInvoiceDate(data));
     console.log(data);
   }
 
-  public onCheckInvoiceDate(check) {
-    if (check === 'false') {
-      this.enableInvoiceDate = false;
-    }
-  }
+  //
+  // public onCheckInvoiceDate(check) {
+  //   if (check === 'false') {
+  //     this.enableInvoiceDate = false;
+  //   }
+  // }
 
   // ---------------InvoiceNumber----------
   public onInvoiceNumberChange(data) {
     // this.store.dispatch(this.invoiceAction.setTemplateId(id));
-  //  this.store.dispatch(this.invoiceAction.setInvoiceNo(data));
+    this.store.dispatch(this.invoiceAction.updateInvoiceNo(data));
     console.log(data);
   }
 
-  public onCheckInvoiceNumber(check) {
-    if (check === 'false') {
-      this.enableInvoiceNumber = false;
-    }
-  }
-
-  // -------------------ShipDate---------------
+//   public onCheckInvoiceNumber(check) {
+//     if (check === 'false') {
+//       this.enableInvoiceNumber = false;
+//     }
+//   }
+//
+//   // -------------------ShipDate---------------
   public onShipDateChange(data) {
     // this.store.dispatch(this.invoiceAction.setTemplateId(id));
-   // this.store.dispatch(this.invoiceAction.setShipDate(data));
+    this.store.dispatch(this.invoiceAction.updateShippingDate(data));
     console.log(data);
   }
-
-  public onCheckShipDAte(check) {
-    if (check === 'false') {
-      this.enableShipDate = false;
-    }
-  }
-  // -------------------ShipVia-------------------
+//
+//   public onCheckShipDAte(check) {
+//     if (check === 'false') {
+//       this.enableShipDate = false;
+//     }
+//   }
+//   // -------------------ShipVia-------------------
   public onShipViaChange(data) {
     // this.store.dispatch(this.invoiceAction.setTemplateId(id));
-    // this.store.dispatch(this.invoiceAction.setShipDate(data));
+    this.store.dispatch(this.invoiceAction.updateShippingState(data));
     console.log(data);
   }
+//
+//   public onCheckShipVia(check) {
+//     if (check === 'false') {
+//       this.enableShipVia = false;
+//     }
+//   }
+//   // -------------------TrackingNo----------------
+//   public onTrackingNoChange(data) {
+//     // this.store.dispatch(this.invoiceAction.setTemplateId(id));
+//     // this.store.dispatch(this.invoiceAction.setTrackingNo(data));
+//     console.log(data);
+//   }
+//
+//   public onCheckTrackingNo(check) {
+//     if (check === 'false') {
+//       this.enableTrackingNo = false;
+//     }
+//   }
+//
+//   // ------------------InvoiceTitleName-------------
+//   public onInvoiceChange(data) {
+//     // this.store.dispatch(this.invoiceAction.setTemplateId(id));
+//     // this.store.dispatch(this.invoiceAction.setInvoiceTitle(data));
+//     console.log(data);
+//   }
+//
+//   // --------------------CustomerDetails-----------------
+//
+//   public onCheckCustomerName(check) {
+//     if (check === 'false') {
+//       this.enableCustomerName = false;
+//     }
+//   }
+//
+//   public onCheckCustomerCompanyName(check) {
+//     if (check === 'false') {
+//       this.enableCompanyName = false;
+//     }
+//   }
+//
+//   public onCheckCustomerEmailId(check) {
+//     if (check === 'false') {
+//       this.enableCustomerEmailId = true;
+//     }
+//   }
+//
+//   public onCheckCustomerMobileNo(check) {
+//     if (check === 'false') {
+//       this.enableCustomerMobileNo = true;
+//     }
+//   }
+// // ------------------------BillingDetails-------------------
+//
+//   public onShippingAddressChange(data) {
+//     // this.store.dispatch(this.invoiceAction.setTemplateId(id));
+//     // this.store.dispatch(this.invoiceAction.setShippingAddress(data));
+//     console.log(data);
+//   }
+//
+//   public onCheckShippingAddress(check) {
+//     if (check === 'false') {
+//       this.enableShippingAddress = false;
+//     }
+//   }
+//
+//   public onCheckShippingAddressState(check) {
+//     if (check === 'false') {
+//       this.enableShippingAddressState = true;
+//     }
+//   }
+//
+//   // ----------------------InvoiceGrid-----------------------
+//   public onChangeWidth(value, colName) {
+//     this.store.dispatch(this.invoiceAction.setColumnWidth(value, colName));
+//   }
+// }
+//
+//
 
-  public onCheckShipVia(check) {
-    if (check === 'false') {
-      this.enableShipVia = false;
-    }
-  }
-  // -------------------TrackingNo----------------
-  public onTrackingNoChange(data) {
-    // this.store.dispatch(this.invoiceAction.setTemplateId(id));
-    // this.store.dispatch(this.invoiceAction.setTrackingNo(data));
-    console.log(data);
-  }
 
-  public onCheckTrackingNo(check) {
-    if (check === 'false') {
-      this.enableTrackingNo = false;
-    }
-  }
-
-  // ------------------InvoiceTitleName-------------
-  public onInvoiceChange(data) {
-    // this.store.dispatch(this.invoiceAction.setTemplateId(id));
-    // this.store.dispatch(this.invoiceAction.setInvoiceTitle(data));
-    console.log(data);
-  }
-
-  // --------------------CustomerDetails-----------------
-
-  public onCheckCustomerName(check) {
-    if (check === 'false') {
-      this.enableCustomerName = false;
-    }
-  }
-
-  public onCheckCustomerCompanyName(check) {
-    if (check === 'false') {
-      this.enableCompanyName = false;
-    }
-  }
-
-  public onCheckCustomerEmailId(check) {
-    if (check === 'false') {
-      this.enableCustomerEmailId = true;
-    }
-  }
-
-  public onCheckCustomerMobileNo(check) {
-    if (check === 'false') {
-      this.enableCustomerMobileNo = true;
-    }
-  }
-// ------------------------BillingDetails-------------------
-
-  public onShippingAddressChange(data) {
-    // this.store.dispatch(this.invoiceAction.setTemplateId(id));
-    // this.store.dispatch(this.invoiceAction.setShippingAddress(data));
-    console.log(data);
-  }
-
-  public onCheckShippingAddress(check) {
-    if (check === 'false') {
-      this.enableShippingAddress = false;
-    }
-  }
-
-  public onCheckShippingAddressState(check) {
-    if (check === 'false') {
-      this.enableShippingAddressState = true;
-    }
-  }
-
-  // ----------------------InvoiceGrid-----------------------
-  public onChangeWidth(value, colName) {
-    this.store.dispatch(this.invoiceAction.setColumnWidth(value, colName));
-  }
 }
-
-
-
