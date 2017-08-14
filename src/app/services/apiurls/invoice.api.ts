e23b8fb365260b09ed24d601ebbc3ca4bf196402
Sarfaraz Ansari <sarfaraz_designer@live.in>
2017-08-14 16:45:03 +0530

    feat: get template details and prevAndGen Invoice api done

--- src/app/services/apiurls/invoice.api.ts
+++ src/app/services/apiurls/invoice.api.ts
@@ -5,6 +5,7 @@ export const INVOICE_API = {
   GET_ALL_LEDGERS_FOR_INVOICE: COMMON_URL + 'ledgers?',
   GENERATE_BULK_INVOICE: COMMON_URL + 'invoices/bulk-generate?combined',
   PREVIEW_INVOICE: COMMON_URL + 'accounts/:accountUniqueName/invoices/generate',
-  PREVIEW_AND_GENERATE: COMMON_URL + 'accounts/:accountUniqueName/invoices/preview',
-  GET_INVOICE_TEMPLATES: COMMON_URL + 'templates/all'
+  PREVIEW_AND_GENERATE: COMMON_URL + 'v2/accounts/:accountUniqueName/invoices/preview',
+  GET_INVOICE_TEMPLATES: COMMON_URL + 'templates/all',
+  GET_INVOICE_TEMPLATE: COMMON_URL + 'templates-v2/templateUniqueName' // get call for single
 };
--- src/app/store/Invoice/invoice.reducer.ts
+++ src/app/store/Invoice/invoice.reducer.ts
@@ -2,10 +2,11 @@ import { Action } from '@ngrx/store';
 import { BaseResponse } from '../../models/api-models/BaseResponse';
 import * as _ from 'lodash';
 import { INVOICE_ACTIONS } from '../../services/actions/invoice/invoice.const';
-import { CommonPaginatedRequest, GetAllLedgersOfInvoicesResponse, GetAllInvoicesPaginatedResponse } from '../../models/api-models/Invoice';
+import { CommonPaginatedRequest, GetAllLedgersOfInvoicesResponse, GetAllInvoicesPaginatedResponse, PreviewAndGenerateInvoiceResponse, PreviewAndGenerateInvoiceRequest } from '../../models/api-models/Invoice';
 
 export class GeneratePage {
   public ledgers: GetAllLedgersOfInvoicesResponse;
+  public invoiceData: PreviewAndGenerateInvoiceResponse;
 }
 
 export class PreviewPage {
@@ -21,16 +22,13 @@ export interface InvoiceState {
 
 export const initialState: InvoiceState = {
     preview: {invoices: null},
-    generate: {ledgers: null},
+    generate: {ledgers: null, invoiceData: null},
     templates: null,
     settings: null
 };
 
 export function InvoiceReducer(state = initialState, action: Action): InvoiceState {
     switch (action.type) {
-        case INVOICE_ACTIONS.GET_ALL_INVOICES: {
-            return state;
-        }
         case INVOICE_ACTIONS.GET_ALL_INVOICES_RESPONSE: {
             let newState = _.cloneDeep(state);
             let res: BaseResponse<GetAllInvoicesPaginatedResponse, CommonPaginatedRequest> = action.payload;
@@ -38,9 +36,6 @@ export function InvoiceReducer(state = initialState, action: Action): InvoiceSta
                 newState.preview.invoices = res.body;
                 return Object.assign({}, state, newState);
             }
-            return state;
-        }
-        case INVOICE_ACTIONS.GET_ALL_LEDGERS_FOR_INVOICE: {
             return state;
         }
         case INVOICE_ACTIONS.GET_ALL_LEDGERS_FOR_INVOICE_RESPONSE: {
@@ -51,17 +46,17 @@ export function InvoiceReducer(state = initialState, action: Action): InvoiceSta
                 return Object.assign({}, state, newState);
             }
         }
-        case INVOICE_ACTIONS.GENERATE_BULK_INVOICE: {
-            return state;
-        }
         case INVOICE_ACTIONS.GENERATE_BULK_INVOICE_RESPONSE: {
             return state; // TODO: add your logic here
         }
-        case INVOICE_ACTIONS.PREVIEW_AND_GENERATE_INVOICE: {
-            return state;
-        }
         case INVOICE_ACTIONS.PREVIEW_AND_GENERATE_INVOICE_RESPONSE: {
-            return state; // TODO: add your logic here
+            let newState = _.cloneDeep(state);
+            let res: BaseResponse<PreviewAndGenerateInvoiceResponse, PreviewAndGenerateInvoiceRequest> = action.payload;
+            if (res.status === 'success') {
+                newState.generate.invoiceData = res.body;
+                return Object.assign({}, state, newState);
+            }
+            return state;
         }
         default:
         {
