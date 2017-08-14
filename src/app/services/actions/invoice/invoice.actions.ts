ff47e881f56ac7139809808d4b2bb9dc49be6c5a
Arpit Meena <arpitmeena@walkover.in>
2017-08-14 15:24:05 +0530

    Services for Generate Bulk Invoice and Generate and preview Invoices.

--- src/app/services/actions/invoice/invoice.actions.ts
+++ src/app/services/actions/invoice/invoice.actions.ts
@@ -54,6 +54,86 @@ export class InvoiceActions {
       payload: res
     }));
 
+  // Generate Bulk Invoice
+  @Effect()
+  public GenerateBulkInvoice$: Observable<Action> = this.action$
+    .ofType(INVOICE_ACTIONS.GENERATE_BULK_INVOICE)
+    .switchMap(action => this._invoiceService.GenerateBulkInvoice(action.payload.model, action.payload.data))
+    .map(response => {
+      return this.GenerateBulkInvoiceResponse(response);
+    });
+
+  @Effect()
+  public GenerateBulkInvoiceResponse$: Observable<Action> = this.action$
+    .ofType(INVOICE_ACTIONS.GENERATE_BULK_INVOICE_RESPONSE)
+    .map(response => {
+      return { type : ''};
+    });
+
+  // Action On Invoice
+  @Effect()
+  public ActionOnInvoice$: Observable<Action> = this.action$
+    .ofType(INVOICE_ACTIONS.ACTION_ON_INVOICE)
+    .switchMap(action => this._invoiceService.PerformActionOnInvoice(action.payload))
+    .map(response => {
+      return this.ActionOnInvoiceResponse(response);
+    });
+
+  @Effect()
+  public ActionOnInvoiceResponse$: Observable<Action> = this.action$
+    .ofType(INVOICE_ACTIONS.ACTION_ON_INVOICE_RESPONSE)
+    .map(response => {
+      return { type : ''};
+    });
+
+  // Get Template
+  @Effect()
+  public GetTemplte$: Observable<Action> = this.action$
+    .ofType(INVOICE_ACTIONS.GET_TEMPLATE)
+    .switchMap(action => this._invoiceService.GetTemplate())
+    .map(response => {
+      return this.GetTemplateResponse(response);
+    });
+
+  @Effect()
+  public GetTemplteResponse$: Observable<Action> = this.action$
+    .ofType(INVOICE_ACTIONS.GET_TEMPLATE_RESPONSE)
+    .map(response => {
+      return { type : ''};
+    });
+
+  // Send Mail
+  @Effect()
+  public SendMail$: Observable<Action> = this.action$
+    .ofType(INVOICE_ACTIONS.SEND_MAIL)
+    .switchMap(action => this._invoiceService.SendMail(action.payload))
+    .map(response => {
+      return this.SendMailResponse(response);
+    });
+
+  @Effect()
+  public SendMailResponse$: Observable<Action> = this.action$
+    .ofType(INVOICE_ACTIONS.SEND_MAIL_RESPONSE)
+    .map(response => {
+      return { type : ''};
+    });
+
+  // Preview and Generate Invoice
+  @Effect()
+  public PreviewAndGenerateInvoive$: Observable<Action> = this.action$
+    .ofType(INVOICE_ACTIONS.PREVIEW_AND_GENERATE_INVOICE)
+    .switchMap(action => this._invoiceService.PreviewAndGenerateInvoice(action.payload))
+    .map(response => {
+      return this.PreviewAndGenerateInvoiceResponse(response);
+    });
+
+  @Effect()
+  public PreviewAndGenerateInvoiceResponse$: Observable<Action> = this.action$
+    .ofType(INVOICE_ACTIONS.PREVIEW_AND_GENERATE_INVOICE_RESPONSE)
+    .map(response => {
+      return { type : ''};
+    });
+
   constructor(
     private action$: Actions,
     private _invoiceService: InvoiceService,
@@ -85,6 +165,75 @@ export class InvoiceActions {
   public GetAllLedgersForInvoiceResponse(model: GetAllLedgersForInvoiceResponse): Action {
     return {
       type: INVOICE_ACTIONS.GET_ALL_LEDGERS_FOR_INVOICE_RESPONSE,
+      payload: model
+    };
+  }
+
+  public GenerateBulkInvoice(model: any, data: any): Action {
+    return {
+      type: INVOICE_ACTIONS.GENERATE_BULK_INVOICE,
+      payload: {model, body: data}
+    };
+  }
+
+  public GenerateBulkInvoiceResponse(model: any): Action {
+    return {
+      type: INVOICE_ACTIONS.GENERATE_BULK_INVOICE_RESPONSE,
+      payload: model
+    };
+  }
+
+  public ActionOnInvoice(model: any): Action {
+    return {
+      type: INVOICE_ACTIONS.ACTION_ON_INVOICE,
+      payload: model
+    };
+  }
+
+  public ActionOnInvoiceResponse(model: any): Action {
+    return {
+      type: INVOICE_ACTIONS.ACTION_ON_INVOICE_RESPONSE,
+      payload: model
+    };
+  }
+
+  public GetTemplate(): Action {
+    return {
+      type: INVOICE_ACTIONS.GET_TEMPLATE
+    };
+  }
+
+  public GetTemplateResponse(model: any): Action {
+    return {
+      type: INVOICE_ACTIONS.GET_TEMPLATE_RESPONSE,
+      payload: model
+    };
+  }
+
+  public SendMail(model: any): Action {
+    return {
+      type: INVOICE_ACTIONS.SEND_MAIL,
+      payload: model
+    };
+  }
+
+  public SendMailResponse(model: any): Action {
+    return {
+      type: INVOICE_ACTIONS.SEND_MAIL_RESPONSE,
+      payload: model
+    };
+  }
+
+  public PreviewAndGenerateInvoice(model: any): Action {
+    return {
+      type: INVOICE_ACTIONS.PREVIEW_AND_GENERATE_INVOICE,
+      payload: model
+    };
+  }
+
+  public PreviewAndGenerateInvoiceResponse(model: any): Action {
+    return {
+      type: INVOICE_ACTIONS.PREVIEW_AND_GENERATE_INVOICE_RESPONSE,
       payload: model
     };
   }
