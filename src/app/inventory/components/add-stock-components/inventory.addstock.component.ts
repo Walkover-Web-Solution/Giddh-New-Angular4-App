df02388e1ca1479c410f7cd794f96230c3fe4c9a
Vishal Isharani <isharanivishal@gmail.com>
2017-08-16 15:23:11 +0530

    Vishal | EL - 179

--- src/app/inventory/components/add-stock-components/inventory.addstock.component.ts
+++ src/app/inventory/components/add-stock-components/inventory.addstock.component.ts
@@ -181,7 +181,7 @@ export class InventoryAddStockComponent implements OnInit, AfterViewInit, OnDest
       }
     });
 
-    // subscribe active stock if availabel fill form
+    // subscribe active stock if available fill form
     this.activeStock$.delay(1000).distinctUntilChanged((x, y) => {
       if (!x && !y) {
         return true;
@@ -331,18 +331,22 @@ export class InventoryAddStockComponent implements OnInit, AfterViewInit, OnDest
     });
     let val: string = this.addStockForm.controls['name'].value;
     val = uniqueNameInvalidStringReplace(val);
-    this.store.dispatch(this.inventoryAction.GetStockUniqueName(groupName, val));
+    if (val) {
+      this.store.dispatch(this.inventoryAction.GetStockUniqueName(groupName, val));
 
-    this.isStockNameAvailable$.subscribe(a => {
-      if (a !== null && a !== undefined) {
-        if (a) {
-          this.addStockForm.patchValue({uniqueName: val});
-        } else {
-          let num = 1;
-          this.addStockForm.patchValue({uniqueName: val + num});
+      this.isStockNameAvailable$.subscribe(a => {
+        if (a !== null && a !== undefined) {
+          if (a) {
+            this.addStockForm.patchValue({uniqueName: val});
+          } else {
+            let num = 1;
+            this.addStockForm.patchValue({uniqueName: val + num});
+          }
         }
-      }
-    });
+      });
+    } else {
+      this.addStockForm.patchValue({uniqueName: ''});
+    }
   }
 
   // calculate rate
