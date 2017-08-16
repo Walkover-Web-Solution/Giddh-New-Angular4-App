3ea05a27f2d2b5afd430407330b0bb60c6d8b5e9
Arpit Meena <arpitmeena@walkover.in>
2017-08-16 18:50:20 +0530

    Invoice module preview tab completed.

--- src/app/services/apiurls/invoice.api.ts
+++ src/app/services/apiurls/invoice.api.ts
@@ -7,8 +7,8 @@ export const INVOICE_API = {
   PREVIEW_AND_GENERATE: COMMON_URL + 'v2/accounts/:accountUniqueName/invoices/preview',
   GET_INVOICE_TEMPLATES: COMMON_URL + 'templates/all',
   GET_INVOICE_TEMPLATE: COMMON_URL + 'templates-v2/templateUniqueName', // get call for single
-  ACTION_ON_INVOICE: COMMON_URL + 'invoices/action',
-  DELETE_INVOICE: COMMON_URL + 'invoices/:invoiceUniqueName',
+  ACTION_ON_INVOICE: COMMON_URL + 'invoices/:invoiceUniqueName/action',
+  DELETE_INVOICE: COMMON_URL + 'invoices/:invoiceNumber',
   GENERATE_INVOICE: COMMON_URL + 'accounts/:accountUniqueName/invoices/generate',
   PREVIEW_INVOICE: COMMON_URL + 'v2/accounts/:accountUniqueName/invoices/preview',
   GET_INVOICE_TEMPLATE_DETAILS: COMMON_URL + 'templates-v2/:templateUniqueName' // get call for single template details
