30832a78e7983edf77b0fe99cb32866cf12cc50d
Sarfaraz Ansari <sarfaraz_designer@live.in>
2017-08-16 12:40:16 +0530

    midway of injecting module

--- src/app/invoice/edit-invoice/filters-container/design-filters/design.filters.component.ts
+++ src/app/invoice/edit-invoice/filters-container/design-filters/design.filters.component.ts
@@ -2,14 +2,21 @@ import {
   Component, Input, EventEmitter, Output, OnInit, OnChanges,
   SimpleChanges
 } from '@angular/core';
+import { Font } from 'ngx-font-picker';
 
 @Component({
   selector: 'design-filters',
-
   templateUrl: 'design.filters.component.html'
 })
 
 export class DesignFiltersContainerComponent implements OnInit {
+
+  public font: Font = new Font({
+    family: 'Roboto',
+    size: '14px',
+    style: 'regular',
+    styles: ['regular']
+  });
   public ifTemplateSelected: boolean = false;
   public ifLogoSelected: boolean = false;
   public ifColorSelected: boolean = false;
@@ -17,13 +24,16 @@ export class DesignFiltersContainerComponent implements OnInit {
   public ifPrintSelected: boolean = false;
   public sampleJsonString: string;
   @Input() public design: boolean;
+  private _presetFonts = ['Arial', 'Serif', 'Helvetica', 'Sans-Serif', 'Open Sans', 'Roboto Slab'];
+  private sizeSelect: boolean = true;
+  private styleSelect: boolean = true;
+  private presetFonts = this._presetFonts;
   constructor() {
     console.log('design-filters-container constructor called');
   }
 
   public ngOnInit() {
     console.log('design-filters-container initialised');
-
   }
 
   public selectTemplate() {
@@ -63,5 +73,14 @@ export class DesignFiltersContainerComponent implements OnInit {
     this.ifColorSelected = false;
     this.ifLogoSelected = false;
     this.ifTemplateSelected = false;
+  }
+
+  private togglePresetFonts() {
+    this.presetFonts = this.presetFonts.length ? [] : this._presetFonts;
+  }
+
+  private toggleExtraOptions() {
+    this.sizeSelect = !this.sizeSelect;
+    this.styleSelect = !this.styleSelect;
   }
 }
