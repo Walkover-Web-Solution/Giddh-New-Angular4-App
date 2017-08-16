465fbfef75c50905506b14949fc7b940a1da9cb5
Sarfaraz Ansari <sarfaraz_designer@live.in>
2017-08-16 12:40:16 +0530

    midway of injecting module

--- src/app/invoice/edit-invoice/edit.invoice.module.ts
+++ src/app/invoice/edit-invoice/edit.invoice.module.ts
@@ -10,10 +10,10 @@ import { SharedModule } from '../../shared/shared.module';
 
 import { EditFiltersContainersComponent } from './filters-container/edit.filters.component';
 import { OutTemplateComponent } from './out-tempate/out.template.component';
-import {MaterialModule, MdSliderModule} from "@angular/material";
-import {ContentFilterComponent} from "./filters-container/content-filters/content.filters.component";
-import {InvoiceRoutingModule} from "./edit-invoice.routing.module";
-import {PrintSettingsComponent} from "./filters-container/design-filters/select-print-settings/print.settings.component";
+import { MaterialModule, MdSliderModule } from '@angular/material';
+import { ContentFilterComponent } from './filters-container/content-filters/content.filters.component';
+import { InvoiceRoutingModule } from './edit-invoice.routing.module';
+import { PrintSettingsComponent } from './filters-container/design-filters/select-print-settings/print.settings.component';
 
 @NgModule({
   imports: [
--- src/app/invoice/edit-invoice/filters-container/design-filters/design.filters.module.ts
+++ src/app/invoice/edit-invoice/filters-container/design-filters/design.filters.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditInvoiceComponent } from './edit.invoice.component';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditInvoiceRoutingModule } from './edit.invoice.routing.module';
 import { OutTempalteComponent } from './out-tempate/out.template.component';
@@ -10,14 +9,14 @@ import { EditFiltersContainerRoutingModule } from './edit.filters.routing.module
 import { DesignFiltersContainerComponent } from './design.filters.component';
 import { SharedModule } from '../../../../shared/shared.module';
 import { SelectTemplateComponent } from './select-template/select.template.component';
-import {Actions} from "@ngrx/effects";
+import { Actions } from '@ngrx/effects';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    SharedModule,
+    SharedModule
   ],
   declarations: [],
   exports: [
