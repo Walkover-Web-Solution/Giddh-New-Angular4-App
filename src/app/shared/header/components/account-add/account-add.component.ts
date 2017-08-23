c2daf9a13ba78ff6020d3c6f58cf7a74633473b7
Vishal Isharani <isharanivishal@gmail.com>
2017-08-23 17:52:03 +0530

    Vishal | make change account-add properti's

--- src/app/shared/header/components/account-add/account-add.component.ts
+++ src/app/shared/header/components/account-add/account-add.component.ts
@@ -1,17 +1,14 @@
-import { AccountRequest, AccountResponse } from '../../../../models/api-models/Account';
+import { AccountRequest } from '../../../../models/api-models/Account';
 import { Observable } from 'rxjs';
-import { GroupResponse } from '../../../../models/api-models/Group';
 import { AppState } from '../../../../store/roots';
 import { Store } from '@ngrx/store';
 import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { AccountsAction } from '../../../../services/actions/accounts.actions';
-import { GroupWithAccountsAction } from '../../../../services/actions/groupwithaccounts.actions';
 import { digitsOnly } from '../../../helpers/customValidationHelper';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { CompanyService } from '../../../../services/companyService.service';
 import { Select2OptionData } from '../../../theme/select2/select2.interface';
-import { ModalDirective } from 'ngx-bootstrap';
 import { ColumnGroupsAccountVM } from '../new-group-account-sidebar/VM';
 import { uniqueNameInvalidStringReplace } from '../../../helpers/helperFunctions';
 
@@ -20,32 +17,22 @@ import { uniqueNameInvalidStringReplace } from '../../../helpers/helperFunctions
   templateUrl: './account-add.component.html'
 })
 export class AccountAddComponent implements OnInit, OnDestroy {
-  @ViewChild('deleteAccountModal') public deleteAccountModal: ModalDirective;
   @Input() public column: ColumnGroupsAccountVM[];
   @Input() public activeGroupUniqueName: string;
+  @Input() public fetchingAccUniqueName$: Observable<boolean>;
+  @Input() public isAccountNameAvailable$: Observable<boolean>;
+  @Input() public createAccountInProcess$: Observable<boolean>;
+  @Input() public createAccountIsSuccess$: Observable<boolean>;
   public addAccountForm: FormGroup;
-  public activeGroup$: Observable<GroupResponse>;
-  public activeAccount$: Observable<AccountResponse>;
-  public fetchingAccUniqueName$: Observable<boolean>;
-  public isAccountNameAvailable$: Observable<boolean>;
   public statesSource$: Observable<Select2OptionData[]> = Observable.of([]);
   public showGstList: boolean = false;
   public showDefaultGstListLength: number = 2;
-  public createAccountInProcess$: Observable<boolean>;
-  public createAccountIsSuccess$: Observable<boolean>;
   public isGstEnabledAcc: boolean = false;
   public isHsnSacEnabledAcc: boolean = false;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(private _fb: FormBuilder, private store: Store<AppState>, private accountsAction: AccountsAction,
-    private groupWithAccountsAction: GroupWithAccountsAction, private _companyService: CompanyService) {
-    this.activeGroup$ = this.store.select(state => state.groupwithaccounts.activeGroup).takeUntil(this.destroyed$);
-    this.activeAccount$ = this.store.select(state => state.groupwithaccounts.activeAccount).takeUntil(this.destroyed$);
-    this.fetchingAccUniqueName$ = this.store.select(state => state.groupwithaccounts.fetchingAccUniqueName).takeUntil(this.destroyed$);
-    this.isAccountNameAvailable$ = this.store.select(state => state.groupwithaccounts.isAccountNameAvailable).takeUntil(this.destroyed$);
-    this.createAccountInProcess$ = this.store.select(state => state.groupwithaccounts.createAccountInProcess).takeUntil(this.destroyed$);
-    this.createAccountIsSuccess$ = this.store.select(state => state.groupwithaccounts.createAccountIsSuccess).takeUntil(this.destroyed$);
-
+    private _companyService: CompanyService) {
     this._companyService.getAllStates().subscribe((data) => {
       let states: Select2OptionData[] = [];
       data.body.map(d => {
@@ -173,10 +160,8 @@ export class AccountAddComponent implements OnInit, OnDestroy {
   }
 
   public submit() {
-    let activeGroup = null;
     let states = null;
 
-    this.activeGroup$.take(1).subscribe(p => activeGroup = p);
     this.statesSource$.take(1).subscribe(p => states = p);
     let accountObj: AccountRequest;
     let formsValue = this.addAccountForm.value;
@@ -211,28 +196,7 @@ export class AccountAddComponent implements OnInit, OnDestroy {
     //   accountObj.gstDetails = gstDetailsArr;
     // }
     delete accountObj['gstDetails'];
-    this.store.dispatch(this.accountsAction.createAccount(activeGroup.uniqueName, accountObj));
-  }
-
-  public showDeleteAccountModal() {
-    this.deleteAccountModal.show();
-  }
-
-  public hideDeleteAccountModal() {
-    this.deleteAccountModal.hide();
-  }
-
-  public deleteAccount() {
-    let activeAccUniqueName = null;
-    this.activeAccount$.take(1).subscribe(s => activeAccUniqueName = s.uniqueName);
-    this.store.dispatch(this.accountsAction.deleteAccount(activeAccUniqueName));
-    this.hideDeleteAccountModal();
-    this.addAccountForm.reset();
-  }
-
-  public jumpToGroup(uniqueName: string) {
-    this.store.dispatch(this.accountsAction.resetActiveAccount());
-    this.store.dispatch(this.groupWithAccountsAction.getGroupDetails(uniqueName));
+    this.store.dispatch(this.accountsAction.createAccount(this.activeGroupUniqueName, accountObj));
   }
 
   public ngOnDestroy() {
