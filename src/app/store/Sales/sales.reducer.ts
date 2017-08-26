3725a237f32a67b0282260ac5b6e9f65e8cbd5f5
Sarfaraz Ansari <sarfaraz_designer@live.in>
2017-08-26 16:26:12 +0530

    feat: group dropdown on create account aside complete

--- src/app/store/Sales/sales.reducer.ts
+++ src/app/store/Sales/sales.reducer.ts
@@ -3,14 +3,17 @@ import * as _ from 'lodash';
 import { SALES_ACTIONS } from '../../services/actions/sales/sales.const';
 import { BaseResponse } from '../../models/api-models/BaseResponse';
 import { AccountResponse } from '../../models/api-models/Account';
+import { GroupsWithAccountsResponse } from '../../models/api-models/GroupsWithAccounts';
 
 export interface SalesState {
   invObj: any;
   acDtl: AccountResponse;
+  groupList: GroupsWithAccountsResponse[];
 }
 const initialState = {
   invObj: null,
-  acDtl: null
+  acDtl: null,
+  groupList: null
 };
 
 export function salesReducer(state = initialState, action: Action): SalesState {
