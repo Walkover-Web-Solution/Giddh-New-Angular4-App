fe9fa0251d3088e7a640a6fb7b7a231d5a2bcf35
Arpit Meena <arpitmeena@walkover.in>
2017-08-29 18:43:42 +0530

    Settings > Linked accounts module

--- src/app/services/settings.linked.accounts.service.ts
+++ src/app/services/settings.linked.accounts.service.ts
@@ -51,4 +51,120 @@ export class SettingsLinkedAccountsService {
         }).catch((e) => HandleCatch<IGetAllEbankAccountResponse[], string>(e));
     }
 
+    /**
+    * Refresh all ebank accounts
+    */
+   public RefreshAllEbankAccounts(): Observable<BaseResponse<IGetAllEbankAccountResponse[], string>> {
+        this.store.take(1).subscribe(s => {
+            if (s.session.user) {
+                this.user = s.session.user.user;
+            }
+            this.companyUniqueName = s.session.companyUniqueName;
+        });
+        return this._http.get(EBANKS.REFRESH_ACCOUNTS.replace(':companyUniqueName', this.companyUniqueName)).map((res) => {
+            let data: BaseResponse<IGetAllEbankAccountResponse[], string> = res.json();
+            return data;
+        }).catch((e) => HandleCatch<IGetAllEbankAccountResponse[], string>(e));
+    }
+
+    /**
+    * Reconnect account
+    */
+   public ReconnectAccount(loginId: string): Observable<BaseResponse<any, string>> {
+        this.store.take(1).subscribe(s => {
+            if (s.session.user) {
+                this.user = s.session.user.user;
+            }
+            this.companyUniqueName = s.session.companyUniqueName;
+        });
+        return this._http.get(EBANKS.RECONNECT_ACCOUNT.replace(':companyUniqueName', this.companyUniqueName).replace(':loginId', loginId)).map((res) => {
+            let data: BaseResponse<any, string> = res.json();
+            return data;
+        }).catch((e) => HandleCatch<any, string>(e));
+    }
+
+    /**
+    * Delete account
+    */
+   public DeleteBankAccount(loginId: string): Observable<BaseResponse<any, string>> {
+        this.store.take(1).subscribe(s => {
+            if (s.session.user) {
+                this.user = s.session.user.user;
+            }
+            this.companyUniqueName = s.session.companyUniqueName;
+        });
+        return this._http.delete(EBANKS.DELETE_BANK_ACCOUNT.replace(':companyUniqueName', this.companyUniqueName).replace(':loginId', loginId)).map((res) => {
+            let data: BaseResponse<any, string> = res.json();
+            data.queryString = { loginId };
+            return data;
+        }).catch((e) => HandleCatch<any, string>(e));
+    }
+
+    /**
+    * Refresh account
+    */
+   public RefreshBankAccount(loginId: string): Observable<BaseResponse<any, string>> {
+        this.store.take(1).subscribe(s => {
+            if (s.session.user) {
+                this.user = s.session.user.user;
+            }
+            this.companyUniqueName = s.session.companyUniqueName;
+        });
+        return this._http.get(EBANKS.REFREST_ACCOUNT.replace(':companyUniqueName', this.companyUniqueName).replace(':loginId', loginId)).map((res) => {
+            let data: BaseResponse<any, string> = res.json();
+            data.queryString = { loginId };
+            return data;
+        }).catch((e) => HandleCatch<any, string>(e));
+    }
+
+    /**
+    * Link bank account
+    */
+   public LinkBankAccount(dataToSend: object, accountId: string): Observable<BaseResponse<any, string>> {
+        this.store.take(1).subscribe(s => {
+            if (s.session.user) {
+                this.user = s.session.user.user;
+            }
+            this.companyUniqueName = s.session.companyUniqueName;
+        });
+        return this._http.put(EBANKS.LINK_ACCOUNT.replace(':companyUniqueName', this.companyUniqueName).replace(':accountId', accountId), dataToSend ).map((res) => {
+            let data: BaseResponse<any, string> = res.json();
+            data.queryString = { accountId };
+            return data;
+        }).catch((e) => HandleCatch<any, string>(e));
+    }
+
+    /**
+    * Unlink bank account
+    */
+   public UnlinkBankAccount(accountId: string): Observable<BaseResponse<any, string>> {
+        this.store.take(1).subscribe(s => {
+            if (s.session.user) {
+                this.user = s.session.user.user;
+            }
+            this.companyUniqueName = s.session.companyUniqueName;
+        });
+        return this._http.delete(EBANKS.UNLINK_ACCOUNT.replace(':companyUniqueName', this.companyUniqueName).replace(':accountId', accountId)).map((res) => {
+            let data: BaseResponse<any, string> = res.json();
+            data.queryString = { accountId };
+            return data;
+        }).catch((e) => HandleCatch<any, string>(e));
+    }
+
+    /**
+    * Update Date
+    */
+   public UpdateDate(date: string, accountId: string): Observable<BaseResponse<any, string>> {
+        this.store.take(1).subscribe(s => {
+            if (s.session.user) {
+                this.user = s.session.user.user;
+            }
+            this.companyUniqueName = s.session.companyUniqueName;
+        });
+        return this._http.put(EBANKS.UPDATE_DATE.replace(':companyUniqueName', this.companyUniqueName).replace(':accountId', accountId).replace(':date', date), {}).map((res) => {
+            let data: BaseResponse<any, string> = res.json();
+            data.queryString = { accountId };
+            return data;
+        }).catch((e) => HandleCatch<any, string>(e));
+    }
 }
