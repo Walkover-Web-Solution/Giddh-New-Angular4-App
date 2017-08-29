1dc83bbceecfda22fe8d86555b5161b42c079bb3
Govinda <govi2010@gmail.com>
2017-08-29 14:32:15 +0530

    Govinda |  167

--- src/app/ledger/ledger.component.ts
+++ src/app/ledger/ledger.component.ts
@@ -310,6 +310,7 @@ export class LedgerComponent implements OnInit, OnDestroy {
   }
 
   public showNewLedgerEntryPopup(trx: TransactionVM) {
+    debugger;
     this.selectBlankTxn(trx);
     this.lc.showNewLedgerPanel = true;
   }
