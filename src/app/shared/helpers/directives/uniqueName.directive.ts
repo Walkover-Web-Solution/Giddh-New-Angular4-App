2c65c2a164e9a0c6aabf86d6e75e9e9aa08272ea
Vishal Isharani <isharanivishal@gmail.com>
2017-08-31 12:17:51 +0530

    Vishal | uniqueName bug

--- src/app/shared/helpers/directives/uniqueName.directive.ts
+++ src/app/shared/helpers/directives/uniqueName.directive.ts
@@ -1,19 +1,29 @@
 import { Directive, ElementRef, HostListener, OnInit } from '@angular/core';
 import { ToasterService } from '../../../services/toaster.service';
+import { uniqueNameInvalidStringReplace } from '../helperFunctions';
 
 @Directive({
   selector: '[UniqueNameDirective]'
 })
-export class UniqueNameDirective  {
+export class UniqueNameDirective {
   public el: HTMLInputElement;
   @HostListener('keypress', ['$event'])
-  public onChange(event: any) {
-    // this.el.value = value.target.value.replace(RegExp(' ', 'g'), '');
+  public onKeyPress(event: KeyboardEvent) {
+    // debugger;
     if (event.which === 32) {
       event.preventDefault();
       this._toaster.clearAllToaster();
       this._toaster.warningToast('Space not allowed', 'Warning');
+    } else {
+      if (/[\\/(){};:"<>#?%,]/g.test(event.key)) {
+        event.preventDefault();
+      }
     }
+  }
+
+  @HostListener('input', ['$event'])
+  public onInput(event: any) {
+    this.el.value = this.el.value.toLowerCase();
   }
 
   // tslint:disable-next-line:member-ordering
