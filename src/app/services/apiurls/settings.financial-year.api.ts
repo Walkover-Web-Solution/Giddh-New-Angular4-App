43f2b47537db43cf10ffd3311553a2f046faebe1
Arpit Meena <arpitmeena@walkover.in>
2017-09-01 15:49:05 +0530

    Setting > Financial Year tab completed

--- src/app/services/apiurls/settings.financial-year.api.ts
+++ src/app/services/apiurls/settings.financial-year.api.ts
@@ -3,6 +3,8 @@ const COMMON = Configuration.ApiUrl + 'company/:companyUniqueName/';
 
 export const SETTINGS_FINANCIAL_YEAR_API = {
   GET_ALL_FINANCIAL_YEARS: COMMON + 'financial-year', // GET
-  LOCK_FINANCIAL_YEAR: 'financial-year-lock', // PATCH
-  UNLOCK_FINANCIAL_YEAR: '',
+  LOCK_FINANCIAL_YEAR: COMMON + 'financial-year-lock', // PATCH
+  UNLOCK_FINANCIAL_YEAR: COMMON + 'financial-year-unlock', // PATCH
+  SWITCH_FINANCIAL_YEAR: COMMON + 'active-financial-year', // PATCH
+  ADD_FINANCIAL_YEAR: COMMON + 'financial-year', // POST
 };
