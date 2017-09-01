29d32b286c9134c6b5202aca66e6fd5b185db867
Kunal Saxena <kunal.saxena@actify.de>
2017-09-01 13:01:59 +0530

    +kunal | added routes for purchase

---
+++ src/app/purchase/purchase.routing.module.ts
@@ -0,0 +1,40 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NeedsAuthentication } from '../services/decorators/needsAuthentication';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { PurchaseComponent } from './purchase.component';
+import { PurchaseInvoiceComponent } from './purchase-invoice/purchase.invoice.component';
+/**
+ * Created by kunalsaxena on 9/1/17.
+ */
+
+const INVOICE_ROUTES: Routes = [
+  {
+    path: '',
+    canActivate: [NeedsAuthentication],
+    component: PurchaseComponent,
+    children: [
+      { path: '', redirectTo: 'purchase', pathMatch: 'full' },
+      { path: 'invoice',  component: PurchaseInvoiceComponent  },
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    SharedModule,
+    RouterModule.forChild(INVOICE_ROUTES),
+  ],
+  exports: [
+    RouterModule,
+  ],
+  providers: []
+})
+export class PurchaseRoutingModule { }
