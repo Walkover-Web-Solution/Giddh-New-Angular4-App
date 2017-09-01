86b49e391e9ce63c5a7d9a91c092a2d8cc0d6d61
iMac <mustafa@walkover.in>
2017-09-01 18:08:15 +0530

    +mustafa | design: purchase/invoice page design complete

--- src/app/purchase/purchase-invoice/purchase.invoice.component.ts
+++ src/app/purchase/purchase-invoice/purchase.invoice.component.ts
@@ -1,28 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
-  styles: [`
-    .invoice-bg{
-      background-color: #f4f5f8;
-      padding: 20px;
-    }
-    .invoice-nav.navbar-nav>li>a{
-      padding: 6px 30px;
-      font-size:14px;
-      color:#333;
-      background-color: #e6e6e6
-    }
-    .invoice-nav.navbar-nav>li>a:hover{
-      background-color: #ff5f00;
-      color: #fff;
-    }
-    .invoice-nav.navbar-nav>li>a.active{
-      background-color: #fff;
-      color: #ff5f00;
-    }
-  `],
-  templateUrl: './purchase.invoice.component.html'
+  templateUrl: './purchase.invoice.component.html',
+  styleUrls: ['purchase.invoice.component.css']
 })
 export class PurchaseInvoiceComponent {
   constructor(
