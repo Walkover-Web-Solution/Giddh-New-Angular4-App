6c78fa765d679e133f2990f5c1a36542c4f250e5
Arpit Meena <arpitmeena@walkover.in>
2017-09-02 19:07:41 +0530

    purchase invoice midway

--- src/app/purchase/purchase.routing.module.ts
+++ src/app/purchase/purchase.routing.module.ts
@@ -7,6 +7,8 @@ import { NgModule } from '@angular/core';
 import { PurchaseComponent } from './purchase.component';
 import { PurchaseInvoiceComponent } from './purchase-invoice/purchase.invoice.component';
 import { Location } from '@angular/common';
+import { Ng2BootstrapModule } from 'ngx-bootstrap';
+import { Daterangepicker } from 'ng2-daterangepicker';
 /**
  * Created by kunalsaxena on 9/1/17.
  */
@@ -27,14 +29,21 @@ const INVOICE_ROUTES: Routes = [
   declarations: [
   ],
   imports: [
+    Daterangepicker,
     FormsModule,
     CommonModule,
     ReactiveFormsModule,
     SharedModule,
     RouterModule.forChild(INVOICE_ROUTES),
+    // Ng2BootstrapModule.forRoot(),
   ],
   exports: [
     RouterModule,
+    Ng2BootstrapModule,
+    // FormsModule,
+    CommonModule,
+    SharedModule,
+    // Daterangepicker
   ],
   providers: [Location]
 })
