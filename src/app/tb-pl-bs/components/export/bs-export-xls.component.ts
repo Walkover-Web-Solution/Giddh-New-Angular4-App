02e0348ac56bb0e01e0a43bda2aa37056e9b2899
govinda <govi2010@gmail.com>
2017-09-06 19:06:21 +0530

    Faizal | Tb-Pl-Bs | Excel Export/Download Status - Completed.

---
+++ src/app/tb-pl-bs/components/export/bs-export-xls.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AppState } from '../../../store/roots';
+import { Store } from '@ngrx/store';
+import { TBPlBsActions } from '../../../services/actions/tl-pl.actions';
+import { ProfitLossRequest } from '../../../models/api-models/tb-pl-bs';
+
+@Component({
+  selector: 'bs-export-xls',  // <home></home>
+  template: `
+    <div class="form-group xls-export">
+      <a (click)="downloadBsXls()" *ngIf="enableDownload"><img
+        src="/assets/images/xls-icon.png"/></a>
+      <!--end form-group -->
+  `
+})
+export class BsExportXlsComponent implements OnInit, OnDestroy {
+  @Input() public fy: number;
+  public enableDownload: boolean = true;
+  @Output() public plBsExportPdfEvent = new EventEmitter<boolean>();
+
+  constructor(private store: Store<AppState>, private fb: FormBuilder, private _tbPlActions: TBPlBsActions) {
+
+  }
+
+  public downloadBsXls() {
+    let request = <ProfitLossRequest>{ fy: this.fy };
+    this.store.dispatch(this._tbPlActions.DownloadBalanceSheetExcel(request));
+  }
+
+  public ngOnInit() {
+    //
+  }
+
+  public ngOnDestroy() {
+    //
+  }
+}
