dd2354c10f68b2057c63f9174ad7cb72070afe59
Vishal Isharani <isharanivishal@gmail.com>
2017-09-07 17:58:20 +0530

    Vishal | electron spcial login

--- src/app/store/Settings/Settings.reducer.ts
+++ src/app/store/Settings/Settings.reducer.ts
@@ -155,7 +155,7 @@ export function SettingsReducer(state = initialState, action: Action): SettingsS
     case SETTINGS_LINKED_ACCOUNTS_ACTIONS.REFRESH_ALL_ACCOUNTS_RESPONSE: {
         let response: BaseResponse<LinkedAccountsState[], string> = action.payload;
         if (response.status === 'success') {
-          newState.linkedAccounts = response.body;
+          newState.linkedAccounts = response.body as LinkedAccountsState;
           return Object.assign({}, state, newState);
         }
         return state;
