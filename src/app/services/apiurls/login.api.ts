6544021fd53323ca1e7f95ff5ec1bb32f22a162c
Sarfaraz Ansari <sarfaraz_designer@live.in>
2017-09-09 09:52:42 +0530

    fix: logout issue form step wizard

--- src/app/services/apiurls/login.api.ts
+++ src/app/services/apiurls/login.api.ts
@@ -7,5 +7,5 @@ export const LOGIN_API = {
   VerifyOTP: Configuration.ApiUrl + 'v2/verify-number',
   VerifyNumber: Configuration.ApiUrl + 'users/system_admin/verify-number',
   LoginWithNumber: Configuration.ApiUrl + 'login-with-number?countryCode=:countryCode&mobileNumber=:mobileNumber', // get call
-  LOGIN_WITH_GOOGLE: Configuration.ApiUrl + '/v2/login-with-google'
+  LOGIN_WITH_GOOGLE: Configuration.ApiUrl + 'v2/login-with-google'
 };
