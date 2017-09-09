59e144b655a186a04267515e5f95e92012b9e778
Vishal Isharani <isharanivishal@gmail.com>
2017-09-09 12:31:28 +0530

    Vishal | login with providers electron

--- config/webpack.renderer.prod.js
+++ config/webpack.renderer.prod.js
@@ -188,43 +188,43 @@ module.exports = function (env) {
          *
          * NOTE: To debug prod builds uncomment //debug lines and comment //prod lines
          */
-        // new UglifyJsPlugin({
-        //   // beautify: true, //debug
-        //   // mangle: false, //debug
-        //   // dead_code: false, //debug
-        //   // unused: false, //debug
-        //   // deadCode: false, //debug
-        //   // compress: {
-        //   //   screw_ie8: true,
-        //   //   keep_fnames: true,
-        //   //   drop_debugger: false,
-        //   //   dead_code: false,
-        //   //   unused: false
-        //   // }, // debug
-        //   // comments: true, //debug
+        new UglifyJsPlugin({
+          // beautify: true, //debug
+          // mangle: false, //debug
+          // dead_code: false, //debug
+          // unused: false, //debug
+          // deadCode: false, //debug
+          // compress: {
+          //   screw_ie8: true,
+          //   keep_fnames: true,
+          //   drop_debugger: false,
+          //   dead_code: false,
+          //   unused: false
+          // }, // debug
+          // comments: true, //debug
 
 
-        //   beautify: true, //prod
-        //   output: {
-        //     comments: false
-        //   }, //prod
-        //   mangle: {
-        //     screw_ie8: true
-        //   }, //prod
-        //   compress: {
-        //     screw_ie8: true,
-        //     warnings: false,
-        //     conditionals: true,
-        //     unused: true,
-        //     comparisons: false,
-        //     sequences: true,
-        //     dead_code: true,
-        //     evaluate: true,
-        //     if_return: true,
-        //     join_vars: true,
-        //     negate_iife: false // we need this for lazy v8
-        //   },
-        // }),
+          beautify: true, //prod
+          output: {
+            comments: false
+          }, //prod
+          mangle: {
+            screw_ie8: true
+          }, //prod
+          compress: {
+            screw_ie8: true,
+            warnings: false,
+            conditionals: true,
+            unused: true,
+            comparisons: false,
+            sequences: true,
+            dead_code: true,
+            evaluate: true,
+            if_return: true,
+            join_vars: true,
+            negate_iife: false // we need this for lazy v8
+          },
+        }),
 
         /**
          * Plugin: NormalModuleReplacementPlugin
