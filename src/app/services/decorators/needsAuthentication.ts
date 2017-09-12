648ae99968dbc925afe32f7a020b8d338de0a483
Govinda <govi2010@gmail.com>
2017-09-12 14:05:48 +0530

    Govinda | Fixed  authentication issue

--- src/app/services/decorators/needsAuthentication.ts
+++ src/app/services/decorators/needsAuthentication.ts
@@ -5,7 +5,8 @@ import { AppState } from '../../store/roots';
 import { CanActivate, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { StateDetailsRequest } from '../../models/api-models/Company';
+import { StateDetailsRequest, StateDetailsResponse } from '../../models/api-models/Company';
+import { BaseResponse } from "../../models/api-models/BaseResponse";
 
 @Injectable()
 export class NeedsAuthentication implements CanActivate {
@@ -13,44 +14,56 @@ export class NeedsAuthentication implements CanActivate {
   constructor(public _router: Router, private store: Store<AppState>, private _companyService: CompanyService, private companyActions: CompanyActions) {
   }
   public canActivate() {
+    // Take User from session
     this.store.take(1).subscribe(s => {
       if (s.session.user) {
         this.user = s.session.user;
       }
     });
+    // Check if user has Session
     if (this.user && this.user.session && this.user.session.id) {
+
       let cmpUniqueName = '';
       this.store.select(s => s.session.companyUniqueName).take(1).subscribe(s => {
         if (s) {
           cmpUniqueName = s;
         }
       });
+      // check if user have activeCompany
       if (cmpUniqueName === '') {
         console.log('Opps! I don\'t have company name from needsAuthentication, Let me get it');
+        // getStateDeailsAuthGuard to check if user have
         let resp = this._companyService.getStateDetailsAuthGuard('').toPromise();
         return resp.then(p => {
-          if (p.body == null) {
-            return this._companyService.CompanyList().toPromise().then(cmp => {
-              if (cmp.body.length > 0) {
-                // Set first company as acctive call Set StateDetail API
-                let stateDetails = new StateDetailsRequest();
-                stateDetails.companyUniqueName = cmp.body[0].uniqueName;
-                stateDetails.lastState = 'company.content.ledgerContent@giddh';
-                this.store.dispatch(this.companyActions.SetStateDetails(stateDetails));
+          // if (p.body == null) {
+          return this._companyService.CompanyList().toPromise().then(cmp => {
+            if (cmp.body.length > 0) {
+              if (cmp.body.findIndex(c => c.uniqueName === p.body.companyUniqueName) > -1) {
+                this.store.dispatch(this.companyActions.GetStateDetailsResponse(p));
                 return true;
               } else {
-                // Navigate to new-user for adding new comapny
-                this._router.navigate(['/dummy'], { skipLocationChange: true }).then(() => {
-                  this._router.navigate(['/new-user']);
-                });
-                return false;
+                let respState = new BaseResponse<StateDetailsResponse, string>();
+                respState.body = new StateDetailsResponse();
+                respState.body.companyUniqueName = cmp.body[0].uniqueName;
+                respState.body.lastState = 'company.content.ledgerContent@giddh';
+                respState.status = 'success';
+                respState.request = '';
+                this.store.dispatch(this.companyActions.GetStateDetailsResponse(respState));
+                return true;
               }
-            });
-          } else {
-            console.log('Got It!Let\'t Redirect-> from needsAuthentication');
-            this.store.dispatch(this.companyActions.GetStateDetailsResponse(p));
-            return true;
-          }
+            } else {
+              // Navigate to new-user for adding new comapny
+              this._router.navigate(['/dummy'], { skipLocationChange: true }).then(() => {
+                this._router.navigate(['/new-user']);
+              });
+              return false;
+            }
+          });
+          // } else {
+          //   console.log('Got It!Let\'t Redirect-> from needsAuthentication');
+          //   this.store.dispatch(this.companyActions.GetStateDetailsResponse(p));
+          //   return true;
+          // }
         }).catch((e) => {
           return this._companyService.CompanyList().toPromise().then(cmp => {
             if (cmp.body.length > 0) {
