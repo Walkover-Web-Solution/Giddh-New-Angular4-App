fa4d6c5a618b2ead9908a664e775d99d6bf9f780
Vishal Isharani <isharanivishal@gmail.com>
2017-09-13 12:15:13 +0530

    Vishal | comapny reducer changes

--- src/app/home/components/history/history-chart.component.ts
+++ src/app/home/components/history/history-chart.component.ts
@@ -37,7 +37,7 @@ export class HistoryChartComponent implements OnInit {
   private AllSeries: IndividualSeriesOptionsExtension[];
   constructor(private store: Store<AppState>, private _homeActions: HomeActions) {
     this.activeCompanyUniqueName$ = this.store.select(p => p.session.companyUniqueName).takeUntil(this.destroyed$);
-    this.companies$ = this.store.select(p => p.company.companies).takeUntil(this.destroyed$);
+    this.companies$ = this.store.select(p => p.session.companies).takeUntil(this.destroyed$);
     this.comparisionChartData$ = this.store.select(p => p.home.comparisionChart).takeUntil(this.destroyed$);
     this.AllSeries = [{
       name: 'Expense',
