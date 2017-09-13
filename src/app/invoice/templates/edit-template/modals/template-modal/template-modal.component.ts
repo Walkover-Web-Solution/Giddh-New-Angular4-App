c1856b9c5fd9fd73c7d5ac3ef284e6ced90a687c
Arpit Meena <arpitmeena@walkover.in>
2017-09-13 18:28:49 +0530

    +Arpit | Invoice module > Email tab UI part completed

--- src/app/invoice/templates/edit-template/modals/template-modal/template-modal.component.ts
+++ src/app/invoice/templates/edit-template/modals/template-modal/template-modal.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, DoCheck } from '@angular/core';
 import { ISection } from '../../../../../models/api-models/Invoice';
+import { Observable } from 'rxjs/Rx';
+import * as _ from 'lodash';
+import { InvoiceUiDataService } from '../../../../../services/invoice.ui.data.service';
+import { EmailSettingObjDefinition } from '../../../../../models/interfaces/invoice.setting.interface';
 
 @Component({
   selector: 'invoice-template-modal',
@@ -7,8 +11,23 @@ import { ISection } from '../../../../../models/api-models/Invoice';
   styleUrls: ['./template-modal.component.css']
 })
 
-export class InvoiceTemplateModalComponent {
+export class InvoiceTemplateModalComponent implements DoCheck {
   @Input() public templateId: string;
   @Input() public templateSections: ISection[];
   @Output() public downloadOrSendMailEvent: EventEmitter<object> = new EventEmitter();
+
+  public isEmailTabSelected: boolean = false;
+  public emailObject: EmailSettingObjDefinition;
+
+  constructor(private invoiceUiDataService: InvoiceUiDataService) {
+  }
+
+  public ngDoCheck() {
+    let obj = this.invoiceUiDataService.getEmailSettingObj();
+    if (obj)  {
+      this.emailObject = obj;
+      this.isEmailTabSelected = obj.isEmailTabSelected;
+    }
+  }
+
 }
