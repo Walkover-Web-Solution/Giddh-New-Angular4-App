6d4d3933a5f7d361c35269eaeab2c639458dde52
Sarfaraz Ansari <sarfaraz_designer@live.in>
2017-09-14 17:40:30 +0530

    fix: redirection loop

--- src/app/services/apiurls/login.api.ts
+++ src/app/services/apiurls/login.api.ts
@@ -8,5 +8,5 @@ export const LOGIN_API = {
   VerifyNumber: Configuration.ApiUrl + 'users/system_admin/verify-number',
   LoginWithNumber: Configuration.ApiUrl + 'login-with-number?countryCode=:countryCode&mobileNumber=:mobileNumber', // get call
   LOGIN_WITH_GOOGLE: Configuration.ApiUrl + 'v2/login-with-google',
-  LOGIN_WITH_LINKEDIN: Configuration.ApiUrl + 'v3/login-with-linkedin'
+  LOGIN_WITH_LINKEDIN: Configuration.ApiUrl + 'v3/login-with-linkedIn'
 };
