31bf55cd3f0bcd59440cb76a3c9a8f123603bb15
Vishal Isharani <isharanivishal@gmail.com>
2017-09-16 18:25:29 +0530

    Vishal | add-account-new component

---
+++ src/app/shared/header/components/account-add-new/account-add-new.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'account-add-new',
+  templateUrl: 'account-add-new.component.html'
+})
+
+export class AccountAddNewComponent implements OnInit {
+  constructor() {
+    //
+  }
+
+  public ngOnInit() {
+    //
+  }
+}
