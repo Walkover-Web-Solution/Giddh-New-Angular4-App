137ee7bdfcd5a7077a0b393aa0876eb46b4d847d
iMac <mustafa@walkover.in>
2017-09-16 18:16:34 +0530

    +mustafa | midway-unit name list in custom stock unit module

--- src/app/inventory/components/custom-stock-components/inventory.customstock.component.ts
+++ src/app/inventory/components/custom-stock-components/inventory.customstock.component.ts
@@ -10,6 +10,7 @@ import * as  _ from 'lodash';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { InventoryAction } from '../../../services/actions/inventory/inventory.actions';
 import { SidebarAction } from '../../../services/actions/inventory/sidebar.actions';
+import { StockUnits } from './stock-unit';
 // import { Select2OptionData } from '../shared/theme/select2';
 
 @Component({
@@ -31,10 +32,11 @@ export class InventoryCustomStockComponent implements OnInit, OnDestroy {
   public createCustomStockInProcess$: Observable<boolean>;
   public updateCustomStockInProcess$: Observable<boolean>;
   public deleteCustomStockInProcessCode$: Observable<any[]>;
+  public stockUnitsList = StockUnits;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(private store: Store<AppState>, private customStockActions: CustomStockUnitAction, private inventoryAction: InventoryAction,
-  private sidebarAction: SidebarAction) {
+    private sidebarAction: SidebarAction) {
     this.customUnitObj = new StockUnitRequest();
     this.stockUnit$ = this.store.select(p => p.inventory.stockUnits).takeUntil(this.destroyed$);
     this.stockUnitsDropDown$ = this.store.select(p => {
@@ -71,6 +73,7 @@ export class InventoryCustomStockComponent implements OnInit, OnDestroy {
       this.store.dispatch(this.customStockActions.CreateStockUnit(_.cloneDeep(this.customUnitObj)));
     } else {
       this.store.dispatch(this.customStockActions.UpdateStockUnit(_.cloneDeep(this.customUnitObj), this.editCode));
+      this.customUnitObj.name = '';
     }
   }
 
@@ -103,5 +106,13 @@ export class InventoryCustomStockComponent implements OnInit, OnDestroy {
   public ngOnDestroy() {
     this.destroyed$.next(true);
     this.destroyed$.complete();
+  }
+
+  public setUnitName(name) {
+    let unit = this.stockUnitsList.filter((obj) => obj.id === name.value || obj.text === name.value);
+    console.log(unit);
+    if (unit !== undefined && unit.length > 0) {
+      this.customUnitObj.code = unit[0].id;
+    }
   }
 }
