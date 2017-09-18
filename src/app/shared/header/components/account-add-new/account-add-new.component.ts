59cbf0f6fff10716b85d8d4c7dacac1efa11768f
Vishal Isharani <isharanivishal@gmail.com>
2017-09-18 17:02:21 +0530

    Vishal | add-new-account new design

--- src/app/shared/header/components/account-add-new/account-add-new.component.ts
+++ src/app/shared/header/components/account-add-new/account-add-new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { digitsOnly } from '../../../helpers/customValidationHelper';
 
 @Component({
   selector: 'account-add-new',
@@ -6,11 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AccountAddNewComponent implements OnInit {
-  constructor() {
+  public addAccountForm: FormGroup;
+  constructor(private _fb: FormBuilder) {
     //
   }
 
   public ngOnInit() {
-    //
+    this.addAccountForm = this._fb.group({
+      name: ['', Validators.compose([Validators.required, Validators.maxLength(100)])],
+      uniqueName: ['', [Validators.required]],
+      openingBalanceType: ['CREDIT', [Validators.required]],
+      openingBalance: [0, Validators.compose([digitsOnly])],
+      mobileNo: [''],
+      email: ['', Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)],
+      companyName: [''],
+      attentionTo: [''],
+      description: [''],
+      addresses: this._fb.array([
+        this.initialGstDetailsForm()
+      ]),
+      country: this._fb.group({
+        countryCode: ['']
+      }),
+      hsnOrSac: [''],
+      hsnNumber: [{ value: '', disabled: false }, []],
+      sacNumber: [{ value: '', disabled: false }, []],
+    });
+  }
+
+  public initialGstDetailsForm(): FormGroup {
+    return this._fb.group({
+      gstNumber: [''],
+      address: [''],
+      stateCode: [''],
+      isDefault: [false],
+      isComposite: [false],
+      partyType: ['']
+    });
+  }
+
+  public addGstDetailsForm() {
+    const addresses = this.addAccountForm.get('addresses') as FormArray;
+    addresses.push(this.initialGstDetailsForm());
+  }
+
+  public removeGstDetailsForm(i: number) {
+    const addresses = this.addAccountForm.get('addresses') as FormArray;
+    addresses.removeAt(i);
+  }
+
+  public isDefaultAddressSelected(val: boolean, i: number) {
+    if (val) {
+      let addresses = this.addAccountForm.get('addresses') as FormArray;
+      for (let control of addresses.controls) {
+        control.get('isDefault').patchValue(false);
+      }
+      addresses.controls[i].get('isDefault').patchValue(true);
+    }
+  }
+
+  public submit() {
+    console.log(this.addAccountForm.value);
   }
 }
