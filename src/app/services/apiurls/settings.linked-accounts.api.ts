a0c5a4ba40afb1915271f8b69280c9c3fd41cbe7
macbookpro <govi2010@gmail.com>
2017-09-19 12:48:36 +0530

    Govinda | Dashboard API // Issue

--- src/app/services/apiurls/settings.linked-accounts.api.ts
+++ src/app/services/apiurls/settings.linked-accounts.api.ts
@@ -1,6 +1,6 @@
 import { Configuration } from '../../app.constant';
 
 export const SETTINGS_LINKED_ACCOUNTS_API = {
-  BANK_ACCOUNTS: Configuration.ApiUrl + '/company/:companyUniqueName/ebanks',
-  REMOVE_ACCOUNT: Configuration.ApiUrl + '/company/:companyUniqueName/login/:loginId',
+  BANK_ACCOUNTS: Configuration.ApiUrl + 'company/:companyUniqueName/ebanks',
+  REMOVE_ACCOUNT: Configuration.ApiUrl + 'company/:companyUniqueName/login/:loginId',
 };
