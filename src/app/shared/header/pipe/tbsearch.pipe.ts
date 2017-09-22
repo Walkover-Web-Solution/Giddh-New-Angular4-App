5b7f7a1a720fce732928a860cad67df62c2814b9
Govinda <govi2010@gmail.com>
2017-09-22 16:29:12 +0530

    Govinda | Fixing Account Search

--- src/app/shared/header/pipe/tbsearch.pipe.ts
+++ src/app/shared/header/pipe/tbsearch.pipe.ts
@@ -36,7 +36,7 @@ export class TbsearchPipe implements PipeTransform {
   }
 
   public performSearch(input) {
-    return _.each(input, (grp: any) => {
+    _.each(input, (grp: any) => {
       let grpName;
       let grpUnq;
       grpName = grp.groupName.toLowerCase();
@@ -44,7 +44,7 @@ export class TbsearchPipe implements PipeTransform {
       if (!this.checkIndex(grpName, this.srch) && !this.checkIndex(grpUnq, this.srch)) {
         grp.isVisible = false;
         if (grp.childGroups.length > 0) {
-          return _.each(grp.childGroups, (sub: any) => {
+          _.each(grp.childGroups, (sub: any) => {
             let subName;
             let subUnq;
             subName = sub.groupName.toLowerCase();
@@ -52,7 +52,7 @@ export class TbsearchPipe implements PipeTransform {
             if (!this.checkIndex(subName, this.srch) && !this.checkIndex(subUnq, this.srch)) {
               sub.isVisible = false;
               if (sub.childGroups.length) {
-                return _.each(sub.childGroups, (child: any) => {
+                _.each(sub.childGroups, (child: any) => {
                   let childName;
                   let childUnq;
                   childName = child.groupName.toLowerCase();
@@ -60,7 +60,7 @@ export class TbsearchPipe implements PipeTransform {
                   if (!this.checkIndex(childName, this.srch) && !this.checkIndex(childUnq, this.srch)) {
                     child.isVisible = false;
                     if (child.childGroups.length > 0) {
-                      return _.each(child.childGroups, (subChild: any) => {
+                      _.each(child.childGroups, (subChild: any) => {
                         let subChildName;
                         let subChildUnq;
                         subChildName = subChild.groupName.toLowerCase();
@@ -68,7 +68,7 @@ export class TbsearchPipe implements PipeTransform {
                         if (!this.checkIndex(subChildName, this.srch) && !this.checkIndex(subChildUnq, this.srch)) {
                           subChild.isVisible = false;
                           if (subChild.childGroups.length > 0) {
-                            return _.each(child.childGroups, (subChild2: any) => {
+                            _.each(subChild.childGroups, (subChild2: any) => {
                               let subChild2Name;
                               let subChild2Unq;
                               subChild2Name = subChild2.groupName.toLowerCase();
@@ -76,53 +76,136 @@ export class TbsearchPipe implements PipeTransform {
                               if (!this.checkIndex(subChild2Name, this.srch) && !this.checkIndex(subChild2Unq, this.srch)) {
                                 subChild2.isVisible = false;
                                 if (subChild2.childGroups.length > 0) {
-                                  return this.performSearch(subChild.childGroups);
+                                  this.performSearch(subChild2.childGroups);
                                 }
-                              } else {
-                                grp.isVisible = true;
+                                if (subChild2.accounts.length > 0) {
+                                  _.each(subChild2.accounts, (acc: any) => {
+                                    let accName;
+                                    let accUnq;
+                                    accName = acc.name.toLowerCase();
+                                    accUnq = acc.uniqueName.toLowerCase();
+                                    if (!this.checkIndex(accName, this.srch) && !this.checkIndex(accUnq, this.srch)) {
+                                      acc.isVisible = false;
+                                    } else if (this.checkIndex(accName, this.srch) || this.checkIndex(accUnq, this.srch)) {
+                                      subChild2.isVisible = true;
+                                      subChild.isVisible = true;
+                                      child.isVisible = true;
+                                      sub.isVisible = true;
+                                      grp.isVisible = true;
+                                      acc.isVisible = true;
+                                    }
+                                  });
+                                }
+                              } else if (this.checkIndex(subChild2Name, this.srch) || this.checkIndex(subChild2Unq, this.srch)) {
+                                subChild2.isVisible = true;
+                                subChild.isVisible = true;
                                 child.isVisible = true;
                                 sub.isVisible = true;
-                                subChild.isVisible = true;
-                                return subChild2.isVisible = true;
+                                grp.isVisible = true;
                               }
                             });
                           }
-                        } else {
-                          grp.isVisible = true;
+                          if (subChild.accounts.length > 0) {
+                            _.each(subChild.accounts, (acc: any) => {
+                              let accName;
+                              let accUnq;
+                              accName = acc.name.toLowerCase();
+                              accUnq = acc.uniqueName.toLowerCase();
+                              if (!this.checkIndex(accName, this.srch) && !this.checkIndex(accUnq, this.srch)) {
+                                acc.isVisible = false;
+                              } else if (this.checkIndex(accName, this.srch) || this.checkIndex(accUnq, this.srch)) {
+                                subChild.isVisible = true;
+                                child.isVisible = true;
+                                sub.isVisible = true;
+                                grp.isVisible = true;
+                                acc.isVisible = true;
+                              }
+                            });
+                          }
+                        } else if (this.checkIndex(subChildName, this.srch) || this.checkIndex(subChildUnq, this.srch)) {
+                          subChild.isVisible = true;
+                          subChild.isVisible = true;
                           child.isVisible = true;
                           sub.isVisible = true;
-                          return subChild.isVisible = true;
+                          grp.isVisible = true;
+                        }
+                      });
+                    }
+                    if (child.accounts.length > 0) {
+                      _.each(child.accounts, (acc: any) => {
+                        let accName;
+                        let accUnq;
+                        accName = acc.name.toLowerCase();
+                        accUnq = acc.uniqueName.toLowerCase();
+                        if (!this.checkIndex(accName, this.srch) && !this.checkIndex(accUnq, this.srch)) {
+                          acc.isVisible = false;
+                        } else if (this.checkIndex(accName, this.srch) || this.checkIndex(accUnq, this.srch)) {
+                          child.isVisible = true;
+                          sub.isVisible = true;
+                          grp.isVisible = true;
+                          acc.isVisible = true;
                         }
                       });
                     }
                   } else if (this.checkIndex(childName, this.srch) || this.checkIndex(childUnq, this.srch)) {
-                    grp.isVisible = true;
                     child.isVisible = true;
-                    return sub.isVisible = true;
+                    sub.isVisible = true;
+                    grp.isVisible = true;
+                    sub.isVisible = true;
+                  }
+                });
+              }
+              if (sub.accounts.length > 0) {
+                _.each(sub.accounts, (acc: any) => {
+                  let accName;
+                  let accUnq;
+                  accName = acc.name.toLowerCase();
+                  accUnq = acc.uniqueName.toLowerCase();
+                  if (!this.checkIndex(accName, this.srch) && !this.checkIndex(accUnq, this.srch)) {
+                    acc.isVisible = false;
+                  } else if (this.checkIndex(accName, this.srch) || this.checkIndex(accUnq, this.srch)) {
+                    sub.isVisible = true;
+                    grp.isVisible = true;
+                    acc.isVisible = true;
                   }
                 });
               }
             } else if (this.checkIndex(subName, this.srch) || this.checkIndex(subUnq, this.srch)) {
+              sub.isVisible = true;
               grp.isVisible = true;
-              return sub.isVisible = true;
+              sub.isVisible = true;
+            }
+          });
+        }
+        if (grp.accounts.length > 0) {
+          _.each(grp.accounts, (acc: any) => {
+            let accName;
+            let accUnq;
+            accName = acc.name.toLowerCase();
+            accUnq = acc.uniqueName.toLowerCase();
+            if (!this.checkIndex(accName, this.srch) && !this.checkIndex(accUnq, this.srch)) {
+              acc.isVisible = false;
+            } else if (this.checkIndex(accName, this.srch) || this.checkIndex(accUnq, this.srch)) {
+              grp.isVisible = true;
+              acc.isVisible = true;
             }
           });
         }
       } else if (this.checkIndex(grpName, this.srch) || this.checkIndex(grpUnq, this.srch)) {
-        return grp.isVisible = true;
+        grp.isVisible = true;
       }
     });
   }
   public resetSearch(input) {
-    return _.each(input, (grp: any) => {
+    _.each(input, (grp: any) => {
       // grp = Object.assign(grp, { isVisible: false });
       grp.isVisible = true;
       if (grp.childGroups.length > 0) {
-        return _.each(grp.childGroups, (sub: any) => {
+        _.each(grp.childGroups, (sub: any) => {
           // sub = Object.assign(sub, { isVisible: false });
           sub.isVisible = false;
           if (sub.childGroups.length > 0) {
-            return this.resetSearch(sub.childGroups);
+            this.resetSearch(sub.childGroups);
           }
         });
       }
