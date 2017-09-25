328e665b871fd110fefa4b289eddc592f764c7d7
Arpit Meena <arpitmeena@walkover.in>
2017-09-25 15:08:10 +0530

    Manufacturing load all entries bug fixed

--- src/app/store/Manufacturing/manufacturing.reducer.ts
+++ src/app/store/Manufacturing/manufacturing.reducer.ts
@@ -76,11 +76,10 @@ export function ManufacturingReducer(state = initialState, action: Action): Manu
             let newState = _.cloneDeep(state);
             let res: BaseResponse<IStocksItem, ManufacturingItemRequest> = action.payload;
             if (res.status === 'success') {
-                // let indx = newState.reportData.results.findIndex((obj) => obj.uniqueName === res.body.uniqueName);
-                // if (indx > -1) {
-                //     newState.reportData.results[indx] = res.body;
-                // }
-                newState.reportData = null;
+                let indx = newState.reportData.results.findIndex((obj) => obj.uniqueName === res.body.uniqueName);
+                if (indx > -1) {
+                    newState.reportData.results[indx] = res.body;
+                }
                 return Object.assign({}, state, newState);
             }
             return state;
