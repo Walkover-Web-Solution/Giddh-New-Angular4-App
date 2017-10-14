542527a429f2a5f7ffcc9c6f6ba852df7a08b013
GOVINDA ALWANI <govi2010@gmail.com>
2017-10-14 16:32:04 +0530

    Govinda vishal (#55)
    * Vishal | update ledger discount entry
    
    * Vishal | update ledger discount
    
    * Visahl | add discount
    
    * Vishal | update ledger
    
    * Vishal | ng-select css changes

--- src/app/ledger/ledger.module.ts
+++ src/app/ledger/ledger.module.ts
@@ -11,6 +11,8 @@ import { LedgerDiscountComponent } from './components/ledgerDiscount/ledgerDisco
 import { UpdateLedgerEntryPanelComponent } from './components/updateLedgerEntryPanel/updateLedgerEntryPanel.component';
 import { ShareLedgerComponent } from './components/shareLedger/shareLedger.component';
 import { ExportLedgerComponent } from './components/exportLedger/exportLedger.component';
+import { UpdateLedgerTaxControlComponent } from './components/updateLedger-tax-control/updateLedger-tax-control.component';
+import { UpdateLedgerDiscountComponent } from './components/updateLedgerDiscount/updateLedgerDiscount.component';
 @NgModule({
   declarations: [
     // Components / Directives/ Pipes
@@ -19,7 +21,9 @@ import { ExportLedgerComponent } from './components/exportLedger/exportLedger.co
     LedgerDiscountComponent,
     UpdateLedgerEntryPanelComponent,
     ShareLedgerComponent,
-    ExportLedgerComponent
+    ExportLedgerComponent,
+    UpdateLedgerTaxControlComponent,
+    UpdateLedgerDiscountComponent
   ],
   exports: [
     LedgerComponent
