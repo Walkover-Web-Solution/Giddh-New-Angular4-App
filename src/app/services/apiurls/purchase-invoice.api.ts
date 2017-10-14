2b93054eb9e3c49d59870663952b855d6a2040b0
iMac <mustafa@walkover.in>
2017-10-14 16:25:11 +0530

    +mustafa | complete purchse invoice module

--- src/app/services/apiurls/purchase-invoice.api.ts
+++ src/app/services/apiurls/purchase-invoice.api.ts
@@ -6,6 +6,7 @@ export const PURCHASE_INVOICE_API = {
   // GSTR_excel_export
   GET_TAXES: COMMON + 'tax', // GET
   GENERATE_PURCHASE_INVOICE: COMMON + 'accounts/:accountUniqueName/' + 'invoices/generate-purchase',
+  UPDATE_PURCHASE_ENTRY: COMMON + 'accounts/:accountUniqueName/' + 'ledgers/:ledgerUniqueName',
   DOWNLOAD_GSTR1_SHEET: COMMON + 'gstreturn/gstr1_excel_export?monthYear=:month&gstin=:company_gstin', // GET
   DOWNLOAD_GSTR1_ERROR_SHEET: COMMON + 'gstreturn/GSTR1_error_sheet?monthYear=:month&gstin=:company_gstin', // GET
 };
