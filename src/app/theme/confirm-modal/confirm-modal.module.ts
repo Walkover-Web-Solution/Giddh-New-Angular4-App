90e0ee499957a466cfb83193084ec6eebd14e69d
Govinda Alwani <govi2010@gmail.com>
2017-10-29 13:02:49 +0530

    Vishal | theme module optimization

---
+++ src/app/theme/confirm-modal/confirm-modal.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { ConfirmModalComponent } from './confirm-modal.component';
+
+@NgModule({
+  imports: [],
+  exports: [
+    ConfirmModalComponent
+  ],
+  declarations: [ConfirmModalComponent],
+  providers: [],
+})
+export class ConfirmModalModule {
+}
