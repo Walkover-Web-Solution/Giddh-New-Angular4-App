3a84b27fe06d8ff6ca8d6fada80f9e7097692670
Govinda Alwani <govi2010@gmail.com>
2017-10-31 16:41:29 +0530

    Govinda Remove unused JS and Html

--- config/head-config.common.js
+++ config/head-config.common.js
@@ -18,15 +18,15 @@ module.exports = {
         /**
          * <link> tags for 'apple-touch-icon' (AKA Web Clips).
          */
-        { rel: 'apple-touch-icon', sizes: '57x57', href: 'assets/icon/apple-icon-57x57.png' },
-        { rel: 'apple-touch-icon', sizes: '60x60', href: 'assets/icon/apple-icon-60x60.png' },
-        { rel: 'apple-touch-icon', sizes: '72x72', href: 'assets/icon/apple-icon-72x72.png' },
-        { rel: 'apple-touch-icon', sizes: '76x76', href: 'assets/icon/apple-icon-76x76.png' },
-        { rel: 'apple-touch-icon', sizes: '114x114', href: 'assets/icon/apple-icon-114x114.png' },
-        { rel: 'apple-touch-icon', sizes: '120x120', href: 'assets/icon/apple-icon-120x120.png' },
-        { rel: 'apple-touch-icon', sizes: '144x144', href: 'assets/icon/apple-icon-144x144.png' },
-        { rel: 'apple-touch-icon', sizes: '152x152', href: 'assets/icon/apple-icon-152x152.png' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: 'assets/icon/apple-icon-180x180.png' },
+        // { rel: 'apple-touch-icon', sizes: '57x57', href: 'assets/icon/apple-icon-57x57.png' },
+        // { rel: 'apple-touch-icon', sizes: '60x60', href: 'assets/icon/apple-icon-60x60.png' },
+        // { rel: 'apple-touch-icon', sizes: '72x72', href: 'assets/icon/apple-icon-72x72.png' },
+        // { rel: 'apple-touch-icon', sizes: '76x76', href: 'assets/icon/apple-icon-76x76.png' },
+        // { rel: 'apple-touch-icon', sizes: '114x114', href: 'assets/icon/apple-icon-114x114.png' },
+        // { rel: 'apple-touch-icon', sizes: '120x120', href: 'assets/icon/apple-icon-120x120.png' },
+        // { rel: 'apple-touch-icon', sizes: '144x144', href: 'assets/icon/apple-icon-144x144.png' },
+        // { rel: 'apple-touch-icon', sizes: '152x152', href: 'assets/icon/apple-icon-152x152.png' },
+        // { rel: 'apple-touch-icon', sizes: '180x180', href: 'assets/icon/apple-icon-180x180.png' },
 
         /**
          * <link> tags for css.
@@ -42,8 +42,8 @@ module.exports = {
         { rel: 'stylesheet', href: 'assets/css/perfect-scrollbar.component.css' },
         { rel: 'stylesheet', href: 'assets/css/toastr.css' },
         { rel: 'stylesheet', href: 'assets/css/busy.css' },
-        { rel: 'stylesheet', href: 'assets/js/plugins/select2/css/select2.min.css' },
-        { rel: 'stylesheet', href: 'assets/js/plugins/tagmanager/tagmanager.css' },
+        // { rel: 'stylesheet', href: 'assets/js/plugins/select2/css/select2.min.css' },
+        // { rel: 'stylesheet', href: 'assets/js/plugins/tagmanager/tagmanager.css' },
         { rel: 'stylesheet', href: 'assets/css/mystyle.css' },
 
 
@@ -56,8 +56,8 @@ module.exports = {
         /**
          * <link> tags for favicons
          */
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'assets/icon/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '96x96', href: 'assets/icon/favicon-96x96.png' },
+        // { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'assets/icon/favicon-32x32.png' },
+        // { rel: 'icon', type: 'image/png', sizes: '96x96', href: 'assets/icon/favicon-96x96.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'assets/icon/favicon-16x16.png' },
 
         /**
@@ -73,8 +73,8 @@ module.exports = {
     script: [
         { type: 'text/javascript', src: 'assets/js/jquery-1.11.3.min.js' },
         { type: 'text/javascript', src: 'assets/js/lodash.min.js' },
-        { type: 'text/javascript', src: 'assets/js/plugins/select2/js/select2.full.min.js' },
-        { type: 'text/javascript', src: 'assets/js/plugins/tagmanager/tagmanager.js' },
+        // { type: 'text/javascript', src: 'assets/js/plugins/select2/js/select2.full.min.js' },
+        // { type: 'text/javascript', src: 'assets/js/plugins/tagmanager/tagmanager.js' },
         {type: 'text/javascript', src: 'https://checkout.razorpay.com/v1/checkout.js'}
     ]
 };
