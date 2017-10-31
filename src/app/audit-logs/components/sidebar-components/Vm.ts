7910fd80ca97f9d429dd96db332767063119b16e
Govinda Alwani <govi2010@gmail.com>
2017-10-31 12:00:03 +0530

    Vishal | audit-logs fixes

--- src/app/audit-logs/components/sidebar-components/Vm.ts
+++ src/app/audit-logs/components/sidebar-components/Vm.ts
@@ -3,13 +3,13 @@ import { CompanyResponse } from '../../../models/api-models/Company';
 
 import * as moment from 'moment/moment';
 import { Observable } from 'rxjs/Observable';
-import { Select2OptionData } from '../../../theme/select2';
+import { IOption } from '../../../theme/ng-select/option.interface';
 
 export class AuditLogsSidebarVM {
   public user$: Observable<UserDetails>;
-  public accounts$: Observable<Select2OptionData[]>;
-  public groups$: Observable<Select2OptionData[]>;
-  public users$: Observable<Select2OptionData[]>;
+  public accounts$: Observable<IOption[]>;
+  public groups$: Observable<IOption[]>;
+  public users$: Observable<IOption[]>;
   public options: Select2Options = {
     multiple: false,
     width: '100%',
@@ -18,32 +18,32 @@ export class AuditLogsSidebarVM {
   };
   public moment = moment;
   public maxDate: Date = moment().toDate();
-  public filters: any[] = [
-    { text: 'All', id: 'All' },
-    { text: 'create', id: 'create' },
-    { text: 'delete', id: 'delete' },
-    { text: 'share', id: 'share' },
-    { text: 'unshare', id: 'unshare' },
-    { text: 'move', id: 'move' },
-    { text: 'merge', id: 'merge' },
-    { text: 'unmerge', id: 'unmerge' },
-    { text: 'delete-all', id: 'delete-all' },
-    { text: 'update', id: 'update' },
-    { text: 'master-import', id: 'master-import' },
-    { text: 'daybook-import', id: 'daybook-import' },
-    { text: 'ledger-excel-import', id: 'ledger-excel-import' }
+  public filters: IOption[] = [
+    { label: 'All', value: 'All' },
+    { label: 'create', value: 'create' },
+    { label: 'delete', value: 'delete' },
+    { label: 'share', value: 'share' },
+    { label: 'unshare', value: 'unshare' },
+    { label: 'move', value: 'move' },
+    { label: 'merge', value: 'merge' },
+    { label: 'unmerge', value: 'unmerge' },
+    { label: 'delete-all', value: 'delete-all' },
+    { label: 'update', value: 'update' },
+    { label: 'master-import', value: 'master-import' },
+    { label: 'daybook-import', value: 'daybook-import' },
+    { label: 'ledger-excel-import', value: 'ledger-excel-import' }
   ];
-  public entities: any[] = [
-    { text: 'All', id: 'All' },
-    { text: 'company', id: 'company' },
-    { text: 'group', id: 'group' },
-    { text: 'account', id: 'account' },
-    { text: 'ledger', id: 'ledger' },
-    { text: 'voucher', id: 'voucher' },
-    { text: 'logs', id: 'logs' }];
+  public entities: IOption[] = [
+    { label: 'All', value: 'All' },
+    { label: 'company', value: 'company' },
+    { label: 'group', value: 'group' },
+    { label: 'account', value: 'account' },
+    { label: 'ledger', value: 'ledger' },
+    { label: 'voucher', value: 'voucher' },
+    { label: 'logs', value: 'logs' }];
   public selectedCompany: Observable<CompanyResponse>;
   public getLogsInprocess$: Observable<boolean>;
-  public dateOptions: any[] = [{ text: 'Date Range', id: 1 }, { text: 'Entry/Log Date', id: 0 }];
+  public dateOptions: IOption[] = [{ label: 'Date Range', value: '1' }, { label: 'Entry/Log Date', value: '0' }];
   public showLogDatePicker: boolean = false;
   public canManageCompany: boolean = false;
   public selectedOperation: string = '';
