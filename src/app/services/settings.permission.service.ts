845684073d6ff2a4a576daae70afe908a78c51a4
Sarfaraz Ansari <sarfaraz_designer@live.in>
2017-11-03 12:26:12 +0530

    feat: update permission completed from UI

--- src/app/services/settings.permission.service.ts
+++ src/app/services/settings.permission.service.ts
@@ -8,6 +8,7 @@ import { AppState } from '../store/roots';
 import { BaseResponse } from '../models/api-models/BaseResponse';
 import { ErrorHandler } from './catchManager/catchmanger';
 import { SETTINGS_PERMISSION_API } from './apiurls/settings.permission.api';
+import { ShareRequestForm, IUpdatePermissionResponse } from '../models/api-models/Permission';
 
 @Injectable()
 export class SettingsPermissionService {
@@ -30,6 +31,23 @@ export class SettingsPermissionService {
       data.queryString = {};
       return data;
     }).catch((e) => this.errorHandler.HandleCatch<any, string>(e));
+  }
+
+  /**
+    * Update permission
+    */
+    public UpdatePermission(model: ShareRequestForm): Observable<BaseResponse<IUpdatePermissionResponse, ShareRequestForm>> {
+      this.store.take(1).subscribe(s => {
+        if (s.session.user) {
+          this.user = s.session.user.user;
+        }
+        this.companyUniqueName = s.session.companyUniqueName;
+      });
+      return this._http.put(SETTINGS_PERMISSION_API.UPDATE_PERMISSION.replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName)).replace(':ueruniquename', model.uniqueName), model).map((res) => {
+        let data: BaseResponse<IUpdatePermissionResponse, ShareRequestForm> = res.json();
+        data.request = model;
+        return data;
+      }).catch((e) => this.errorHandler.HandleCatch<IUpdatePermissionResponse, ShareRequestForm>(e, model));
   }
 
 }
