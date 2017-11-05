90c9b830391e588305cdeebaa4c1b9ff7c6011d5
Govinda Alwani <govi2010@gmail.com>
2017-11-05 13:19:29 +0530

    Vishal | quick account api implemented

--- src/app/ledger/components/quickAccount/quickAccount.component.ts
+++ src/app/ledger/components/quickAccount/quickAccount.component.ts
@@ -6,13 +6,13 @@ import { Observable } from 'rxjs/Observable';
 import { CompanyService } from '../../../services/companyService.service';
 import { SelectComponent } from '../../../theme/ng-select/select.component';
 import { ToasterService } from '../../../services/toaster.service';
-import { AccountsAction } from '../../../services/actions/accounts.actions';
 import { AccountRequestV2 } from '../../../models/api-models/Account';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../store';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { GroupsWithAccountsResponse } from '../../../models/api-models/GroupsWithAccounts';
 import * as _ from '../../../lodash-optimized';
+import { LedgerActions } from '../../../services/actions/ledger/ledger.actions';
 
 @Component({
   selector: 'quickAccount',
@@ -24,15 +24,20 @@ export class QuickAccountComponent implements OnInit, OnDestroy {
   public groupsArrayStream$: Observable<GroupsWithAccountsResponse[]>;
   public flattenGroupsArray: IOption[] = [];
   public statesSource$: Observable<IOption[]> = Observable.of([]);
+  public isQuickAccountInProcess$: Observable<boolean>;
+  public isQuickAccountCreatedSuccessfully$: Observable<boolean>;
+  public showGstBox: boolean = false;
   public newAccountForm: FormGroup;
 
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(private _fb: FormBuilder, private _groupService: GroupService,
               private _companyService: CompanyService, private _toaster: ToasterService,
-              private accountsAction: AccountsAction, private store: Store<AppState>) {
+              private ledgerAction: LedgerActions, private store: Store<AppState>) {
+    this.isQuickAccountInProcess$ = this.store.select(p => p.ledger.isQuickAccountInProcess).takeUntil(this.destroyed$);
+    this.isQuickAccountCreatedSuccessfully$ = this.store.select(p => p.ledger.isQuickAccountCreatedSuccessfully).takeUntil(this.destroyed$);
     this.groupsArrayStream$ = this.store.select(p => p.general.groupswithaccounts).takeUntil(this.destroyed$);
-    this._groupService.GetFlattenGroupsAccounts().subscribe(result => {
+    this._groupService.GetFlattenGroupsAccounts('', 1, 5000, 'true').subscribe(result => {
       if (result.status === 'success') {
         let groupsListArray: IOption[] = [];
         result.body.results.forEach(a => {
@@ -58,10 +63,19 @@ export class QuickAccountComponent implements OnInit, OnDestroy {
     this.newAccountForm = this._fb.group({
       name: ['', Validators.compose([Validators.required, Validators.maxLength(100)])],
       uniqueName: ['', [Validators.required]],
-      gstIn: ['', Validators.compose([Validators.maxLength(15)])],
-      stateCode: [{value: '', disabled: false}],
       groupUniqueName: [''],
-      openingBalanceDate: []
+      openingBalanceDate: [],
+      addresses: this._fb.array([
+        this._fb.group({
+          gstNumber: ['', Validators.compose([Validators.maxLength(15)])],
+          stateCode: [{value: '', disabled: false}]
+        })
+      ]),
+    });
+    this.isQuickAccountCreatedSuccessfully$.subscribe(a => {
+      if (a) {
+        this.closeQuickAccountModal.emit(true);
+      }
     });
   }
 
@@ -87,62 +101,53 @@ export class QuickAccountComponent implements OnInit, OnDestroy {
     }
   }
 
-  public getStateCode(statesEle: SelectComponent) {
-    let gstVal: string = this.newAccountForm.get('gstIn').value;
+  public getStateCode(gstForm: FormGroup, statesEle: SelectComponent) {
+    let gstVal: string = gstForm.get('gstNumber').value;
     if (gstVal.length >= 2) {
       this.statesSource$.take(1).subscribe(state => {
         let s = state.find(st => st.value === gstVal.substr(0, 2));
         statesEle.disabled = true;
         if (s) {
-          this.newAccountForm.get('stateCode').patchValue(s.value);
+          gstForm.get('stateCode').patchValue(s.value);
         } else {
-          this.newAccountForm.get('stateCode').patchValue(null);
+          gstForm.get('stateCode').patchValue(null);
           this._toaster.clearAllToaster();
           this._toaster.warningToast('Invalid GSTIN.');
         }
       });
     } else {
       statesEle.disabled = false;
-      this.newAccountForm.get('stateCode').patchValue(null);
+      gstForm.get('stateCode').patchValue(null);
     }
   }
 
   public checkSelectedGroup(options: IOption) {
     this.groupsArrayStream$.subscribe(data => {
       if (data.length) {
-        let accountList = this.flattenGroup(data, []);
-        let group = accountList.find(f => f.uniqueName === options.value);
-        // this.vm.groups$ = Observable.of(groups);
+        let accountCategory = this.flattenGroup(data, options.value, null);
+        this.showGstBox = accountCategory === 'assets' || accountCategory === 'liabilities';
       }
     });
   }
 
-  public flattenGroup(rawList: any[], parents: any[] = []) {
-    let listofUN;
-    listofUN = _.map(rawList, (listItem) => {
-      let newParents;
-      let result;
-      newParents = _.union([], parents);
-      newParents.push({
-        name: listItem.name,
-        uniqueName: listItem.uniqueName
-      });
-      listItem = Object.assign({}, listItem, {parentGroups: []});
-      // listItem.parentGroups = newParents;
-      if (listItem.groups.length > 0) {
-        result = this.flattenGroup(listItem.groups, newParents);
-        result.push(_.omit(listItem, ['accounts', 'parentGroups']));
-      } else {
-        result = _.omit(listItem, ['accounts', 'parentGroups']);
+  public flattenGroup(rawList: GroupsWithAccountsResponse[], groupUniqueName: string, category: any) {
+    for (let raw of rawList) {
+      if (raw.uniqueName === groupUniqueName) {
+        return raw.category;
       }
-      return result;
-    });
-    return _.flatten(listofUN);
+
+      if (raw.groups) {
+        let AccountOfCategory = this.flattenGroup(raw.groups, groupUniqueName, raw);
+        if (AccountOfCategory) {
+          return AccountOfCategory;
+        }
+      }
+    }
   }
 
   public submit() {
     let createAccountRequest: AccountRequestV2 = _.cloneDeep(this.newAccountForm.value);
-    this.accountsAction.createAccountV2(this.newAccountForm.value.groupUniqueName, createAccountRequest);
+    this.store.dispatch(this.ledgerAction.createQuickAccountV2(this.newAccountForm.value.groupUniqueName, createAccountRequest));
   }
 
   public ngOnDestroy() {
