49e9ea7d095cccf7cb7e854823db9e32da90f943
Arpit Meena <arpitmeena@walkover.in>
2017-11-07 11:13:46 +0530

    Arpit | create  manufacturing product name in update issue solved

--- src/app/manufacturing/edit/mf.edit.component.ts
+++ src/app/manufacturing/edit/mf.edit.component.ts
@@ -69,6 +69,7 @@ export class MfEditComponent implements OnInit {
         this.isUpdateCase = true;
         let manufacturingObj = _.cloneDeep(o.reportData.results.find((stock) => stock.uniqueName === o.stockToUpdate));
         if (manufacturingObj) {
+          this.selectedProductName = manufacturingObj.stockName;
           manufacturingObj.quantity = manufacturingObj.manufacturingQuantity;
           manufacturingObj.date = moment(manufacturingObj.date, 'DD-MM-YYYY').toDate();
           manufacturingObj.multipleOf = (manufacturingObj.manufacturingQuantity / manufacturingObj.manufacturingMultipleOf);
