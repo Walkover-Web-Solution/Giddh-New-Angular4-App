32c3e3b4cf0f2f44664f17c325f4ee019f26b894
Sarfaraz Ansari <sarfaraz_designer@live.in>
2017-11-07 14:56:58 +0530

    fix: loader error

--- src/app/loader/loader.component.ts
+++ src/app/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, AfterViewInit, AfterContentInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { LoaderService } from './loader.service';
 import { LoaderState } from './loader';
@@ -10,29 +10,25 @@ import { ReplaySubject } from 'rxjs/ReplaySubject';
   styleUrls: ['./loader.component.scss']
 })
 
-export class LoaderComponent implements OnInit, AfterContentInit {
+export class LoaderComponent implements OnInit {
 
   public showLoader: boolean = false;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(
-    private loaderService: LoaderService
+    private loaderService: LoaderService,
+    private cdref: ChangeDetectorRef
   ) {
   }
 
   public ngOnInit() {
-    this.loaderService.loaderState.takeUntil(this.destroyed$).subscribe((state: LoaderState) => {
+    this.loaderService.loaderState.takeUntil(this.destroyed$).debounceTime(100).subscribe((state: LoaderState) => {
       if (state.show) {
         this.showLoader = true;
-      }
-    });
-  }
-
-  public ngAfterContentInit() {
-    this.loaderService.loaderState.takeUntil(this.destroyed$).subscribe((state: LoaderState) => {
-      if (!state.show) {
+      }else {
         this.showLoader = false;
       }
+      this.cdref.detectChanges();
     });
   }
 
