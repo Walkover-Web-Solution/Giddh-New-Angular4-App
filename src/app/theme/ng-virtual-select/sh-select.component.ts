573d854e231e767717c22d8c44222155f341b02b
Govinda Alwani <govi2010@gmail.com>
2017-11-08 11:20:30 +0530

    Vishal | nvs-select inplace of ng-select

--- src/app/theme/ng-virtual-select/sh-select.component.ts
+++ src/app/theme/ng-virtual-select/sh-select.component.ts
@@ -3,142 +3,13 @@
  */
 import { AfterViewInit, Component, ContentChild, ElementRef, EventEmitter, forwardRef, HostListener, Input, OnInit, Output, Renderer, TemplateRef, ViewChild } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-import { filter } from 'rxjs/operator/filter';
 import { IOption } from './sh-options.interface';
 
 // noinspection TsLint
 @ Component({
   selector: 'sh-select',
-  template: `
-    <div class="header" [class.sh-select-disabled]="disabled" [class.inline]="mode==='inline'"
-         (click)="show()" [class.open]="isOpen" tabindex="0" (focus)="show()" (blur)="hide($event)">
-      <input type="text"
-             #inputFilter
-             tabindex="0"
-             [hidden]="!isOpen"
-             (click)="show()"
-             [placeholder]="placeholder"
-             [(ngModel)]="filter"
-             (ngModelChange)="updateFilter($event)">
-      <div (click)="show(); $event.stopPropagation()"
-
-           *ngIf="!isOpen">
-        {{selectedValues?.length ?
-        (isMultiselect ?
-          selectedValues?.length + ' Selected'
-          : (selectedValues[0].label))
-        : placeholder}}
-      </div>
-      <i class="close icon clear"
-         *ngIf="showClear && selectedValues.length > 0"
-         (click)="clear(); $event.stopPropagation()"></i>
-    </div>
-    <sh-select-menu [isOpen]="isOpen"
-                    [rows]="rows"
-                    [selectedValues]="selectedValues"
-                    [optionTemplate]="optionTemplate"
-                    (noToggleClick)="toggleSelected($event)"></sh-select-menu>`,
-  styles: [`:host {
-    display: block;
-    position: relative;
-    width: 100%;
-    padding: 0;
-    margin: 0;
-    font-family: 'RobotoDraft', 'Roboto', 'Helvetica Neue, Helvetica, Arial', sans-serif;
-    font-style: normal;
-    font-weight: 300;
-    font-size: 1.0rem;
-    line-height: 2rem;
-    letter-spacing: 0.01rem;
-    color: #212121;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-rendering: optimizeLegibility;
-  }
-
-  .header {
-    width: 100%;
-    margin: 0 !important;
-    padding: 2px 30px 2px 10px;
-    box-sizing: border-box;
-    background-color: white;
-    font-size: 1.0em;
-    border-radius: 2px;
-    border: 1px solid rgba(34, 36, 38, .15);
-    vertical-align: middle;
-    cursor: pointer;
-  }
-
-  .header.inline {
-    background-color: transparent;
-    width: max-content;
-    border: none;
-  }
-
-  .header.open {
-    border-radius: 2px 2px 0 0;
-    box-shadow: 0 2px 3px 0 rgba(34, 36, 38, .15);
-    border-bottom: none;
-  }
-
-  div {
-    display: block;
-  }
-
-  input[type="text"] {
-    border: none !important;
-    vertical-align: middle !important;
-    width: 100%;
-    margin: 0 !important;
-    padding: 0px !important;
-    box-sizing: border-box;
-    background-color: white;
-    font-size: 1.0rem !important;
-    line-height: 2rem !important;
-    letter-spacing: 0.01rem !important;
-    font-family: 'RobotoDraft', 'Roboto', 'Helvetica Neue, Helvetica, Arial', sans-serif;
-    font-style: normal !important;
-    font-weight: 300 !important;
-    -webkit-font-smoothing: antialiased !important;
-    -moz-osx-font-smoothing: grayscale !important;;
-    text-rendering: optimizeLegibility !important;;
-  }
-
-  input[type="text"] {
-    outline: none;
-  }
-
-  [hidden] {
-    display: none;
-  }
-
-  i.close.icon.clear::after {
-    content: "✕";
-    padding-right: 8px;
-    font-weight: 800;
-    color: gray;
-  }
-
-  i.close.icon.clear:hover::after {
-    color: red;
-  }
-
-  .clear {
-    position: absolute;
-    right: 2px;
-    padding-left: 2px;
-    padding-right: 2px;
-    top: 3px;
-    cursor: pointer;
-  }
-
-  .sh-select-disabled {
-    background-color: #e3e3e3;
-    color: darkgray;
-    cursor: not-allowed;
-  }
-
-  `],
+  templateUrl: './sh-select.component.html',
+  styleUrls: [`./sh-select.component.css`],
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
@@ -170,6 +41,7 @@ export class ShSelectComponent implements ControlValueAccessor, OnInit, AfterVie
   @Output() public onClear: EventEmitter<any[]> = new EventEmitter<any[]>();
   @Output() public selected = new EventEmitter<any>();
   @Output() public noOptionsFound = new EventEmitter<boolean>();
+  @Output() public noResultsClicked = new EventEmitter<null>();
   @Output() public viewInitEvent = new EventEmitter<any>();
 
   public rows: IOption[] = [];
