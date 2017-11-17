c723660b2f0efbab98ca1de3e2073c2c122f6e07
Arpit Meena <arpitmeena@walkover.in>
2017-11-17 16:13:17 +0530

    Arpit | ledger advance search midway

--- src/app/ledger/components/advance-search/advance-search.component.ts
+++ src/app/ledger/components/advance-search/advance-search.component.ts
@@ -1,3 +1,5 @@
+import { Subscription } from 'rxjs/Rx';
+import { LedgerActions } from './../../../services/actions/ledger/ledger.actions';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ILedgerAdvanceSearchRequest, Inventory } from './../../../models/api-models/Ledger';
 import { CompanyActions } from './../../../services/actions/company.actions';
@@ -10,6 +12,7 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { IRoleCommonResponseAndRequest } from '../../../models/api-models/Permission';
 import { Observable } from 'rxjs/Observable';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
+import * as moment from 'moment';
 
 @Component({
   selector: 'advance-search-model',
@@ -18,6 +21,7 @@ import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 export class AdvanceSearchModelComponent implements OnInit {
 
+  @Input() public accountUniqueName: string;
   @Output() public closeModelEvent: EventEmitter<boolean> = new EventEmitter(true);
 
   public advanceSearchObject: ILedgerAdvanceSearchRequest = null;
@@ -29,9 +33,12 @@ export class AdvanceSearchModelComponent implements OnInit {
   // public selectedAccountForMerge: any = [];
   // public checkModel: string = 'all';
 
+  private moment = moment;
+  private fromDate: string = '';
+  private toDate: string = '';
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-  constructor(private store: Store<AppState>, private fb: FormBuilder) {
+  constructor(private store: Store<AppState>, private fb: FormBuilder, private _ledgerActions: LedgerActions) {
     this.advanceSearchForm = this.fb.group({
       uniqueNames: this.fb.array([]),
       isInvoiceGenerated: [false],
@@ -53,7 +60,9 @@ export class AdvanceSearchModelComponent implements OnInit {
   }
 
   public ngOnInit() {
-    //
+    // this.advanceSearchForm.controls['isInvoiceGenerated'].valueChanges.takeUntil(this.destroyed$).subscribe((value: boolean) => {
+    //   this.advanceSearchForm.controls90;
+    // });
   }
 
   public onCancel() {
@@ -61,9 +70,49 @@ export class AdvanceSearchModelComponent implements OnInit {
   }
 
   /**
+   * onDateRangeSelected
+   */
+  public onDateRangeSelected(data) {
+    this.fromDate = moment(data[0]).format('DD-MM-YYYY');
+    this.toDate = moment(data[1]).format('DD-MM-YYYY');
+  }
+
+  /**
    * onSearch
    */
   public onSearch() {
+    let obj = {
+      uniqueNames: [],
+      includeAmount: null,
+      amount: null,
+      amountLessThan: null,
+      amountEqualTo: null,
+      amountGreaterThan: null,
+      includeDescription: null,
+      description: null,
+      isInvoiceGenerated: false,
+      includeTag: null,
+      tags: [],
+      includeParticulars: false,
+      particulars: [],
+      chequeNumber: '',
+      dateOnCheque: '',
+      inventory: {
+        includeInventory: true,
+        inventories: ['BLUEPEN1'],
+        quantity: 1,
+        includeQuantity: true,
+        quantityLessThan: false,
+        quantityEqualTo: true,
+        quantityGreaterThan: true,
+        includeItemValue: true,
+        itemValue: 20,
+        includeItemLessThan: true,
+        includeItemEqualTo: true,
+        includeItemGreaterThan: false
+      }
+    };
     console.log('advanceSearchForm is :', this.advanceSearchForm.value);
+    this.store.dispatch(this._ledgerActions.doAdvanceSearch(obj, this.accountUniqueName, this.fromDate, this.toDate));
   }
 }
