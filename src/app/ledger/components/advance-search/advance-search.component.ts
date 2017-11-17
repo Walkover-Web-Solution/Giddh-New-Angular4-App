1a3dafdd990cb7b064cc18f4a0106caf417189bd
Arpit Meena <arpitmeena@walkover.in>
2017-11-17 10:53:46 +0530

    Arpit | ledger advance search midway

--- src/app/ledger/components/advance-search/advance-search.component.ts
+++ src/app/ledger/components/advance-search/advance-search.component.ts
@@ -1,3 +1,5 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ILedgerAdvanceSearchRequest, Inventory } from './../../../models/api-models/Ledger';
 import { CompanyActions } from './../../../services/actions/company.actions';
 import { AppState } from './../../../store/roots';
 import { Store } from '@ngrx/store';
@@ -8,6 +10,7 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { IRoleCommonResponseAndRequest } from '../../../models/api-models/Permission';
 import { Observable } from 'rxjs/Observable';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
+
 @Component({
   selector: 'advance-search-model',
   templateUrl: './advance-search.component.html'
@@ -15,29 +18,52 @@ import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 export class AdvanceSearchModelComponent implements OnInit {
 
-  @Input() public selectedRoleForDelete: IRoleCommonResponseAndRequest;
-  @Output() public confirmDeleteEvent: EventEmitter<boolean> = new EventEmitter(true);
   @Output() public closeModelEvent: EventEmitter<boolean> = new EventEmitter(true);
 
-  public activeAccount$: Observable<AccountResponseV2>;
-  public accounts$: Observable<IOption[]>;
-  public selectedaccountForMerge: any = [];
-  public checkModel: string = 'all';
+  public advanceSearchObject: ILedgerAdvanceSearchRequest = null;
+  public advanceSearchForm: FormGroup;
   public showOtherDetails: boolean = false;
+
+  // public activeAccount$: Observable<AccountResponseV2>;
+  // public accounts$: Observable<IOption[]>;
+  // public selectedAccountForMerge: any = [];
+  // public checkModel: string = 'all';
+
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-  constructor(private store: Store<AppState>) {
+  constructor(private store: Store<AppState>, private fb: FormBuilder) {
+    this.advanceSearchForm = this.fb.group({
+      uniqueNames: this.fb.array([]),
+      isInvoiceGenerated: [false],
+      amountLessThan: [false],
+      includeAmount: [false],
+      amountEqualTo: [false],
+      amountGreaterThan: [false],
+      amount: ['', Validators.required],
+      includeDescription: ['', Validators.required],
+      description: ['', Validators.required],
+      includeTag: ['', Validators.required],
+      includeParticulars: ['', Validators.required],
+      chequeNumber: ['', Validators.required],
+      dateOnCheque: ['', Validators.required],
+      tags: this.fb.array([]),
+      particulars: this.fb.array([]),
+      inventory: this.fb.array([])
+    });
   }
 
   public ngOnInit() {
     //
   }
 
-  public onConfirmation() {
-    this.confirmDeleteEvent.emit(true);
-  }
-
   public onCancel() {
     this.closeModelEvent.emit(true);
+  }
+
+  /**
+   * onSearch
+   */
+  public onSearch() {
+    console.log('advanceSearchForm is :', this.advanceSearchForm.value);
   }
 }
