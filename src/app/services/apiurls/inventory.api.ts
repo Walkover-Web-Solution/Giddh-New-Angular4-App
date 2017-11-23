ed6066b817edb53fd2b12b5beb543bcceee544cc
Arpit Meena <arpitmeena@walkover.in>
2017-11-23 11:49:45 +0530

    Arpit | hard coded refresh=true removed form manufacturing API

--- src/app/services/apiurls/inventory.api.ts
+++ src/app/services/apiurls/inventory.api.ts
@@ -5,8 +5,8 @@ export const INVENTORY_API = {
   UPDATE_STOCK_GROUP: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/:stockGroupUniqueName', // put call
   DELETE_STOCK_GROUP: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/:stockGroupUniqueName', // DELETE call
   STOCKS: Configuration.ApiUrl + 'company/:companyUniqueName/stocks', // get call
-  MANUFACTURING_STOCKS: Configuration.ApiUrl + 'company/:companyUniqueName/stocks-manufacture?refresh=true', // get call
-  CREATE_NEW_MANUFACTURING_STOCKS: Configuration.ApiUrl + 'company/:companyUniqueName/stocks?isManufactured=true&refresh=true', // get call
+  MANUFACTURING_STOCKS: Configuration.ApiUrl + 'company/:companyUniqueName/stocks-manufacture', // get call
+  CREATE_NEW_MANUFACTURING_STOCKS: Configuration.ApiUrl + 'company/:companyUniqueName/stocks?isManufactured=true', // get call
   GROUPS_STOCKS: Configuration.ApiUrl + 'company/:companyUniqueName/stock-group/:stockGroupUniqueName', // get call
   GROUPS_WITH_STOCKS_FLATTEN: Configuration.ApiUrl + 'company/:companyUniqueName/flatten-stock-groups-with-stocks?count=:count&page=:page&q=:q', // get call
   GROUPS_WITH_STOCKS: Configuration.ApiUrl + 'company/:companyUniqueName/hierarchical-stock-groups', // get call
