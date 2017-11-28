1ac9c66de0212a63be31b3f97d284bde00796ecb
Govinda Alwani <govi2010@gmail.com>
2017-11-28 15:59:25 +0530

    Vishal - decorators moved out side

--- src/app/manufacturing/manufacturing.routing.module.ts
+++ src/app/manufacturing/manufacturing.routing.module.ts
@@ -1,7 +1,7 @@
 import { ManufacturingComponent } from './manufacturing.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NeedsAuthentication } from '../services/decorators/needsAuthentication';
+import { NeedsAuthentication } from '../decorators/needsAuthentication';
 import { MfEditComponent } from './edit/mf.edit.component';
 import { MfReportComponent } from './report/mf.report.component';
 
