30f35131eac2b414cda904941caa7592f0f95704
Govinda Alwani <govi2010@gmail.com>
2017-11-28 15:59:25 +0530

    Vishal - decorators moved out side

--- src/app/decorators/needsAuthentication.ts
+++ src/app/decorators/needsAuthentication.ts
@@ -1,17 +1,13 @@
-import { CompanyActions } from '../../actions/company.actions';
-import { CompanyService } from '../companyService.service';
-import { VerifyEmailResponseModel } from '../../models/api-models/loginModels';
-import { AppState } from '../../store/roots';
+import { CompanyActions } from '../actions/company.actions';
+import { CompanyService } from '../services/companyService.service';
+import { AppState } from '../store';
 import { CanActivate, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { StateDetailsRequest, StateDetailsResponse } from '../../models/api-models/Company';
-import { BaseResponse } from '../../models/api-models/BaseResponse';
-import { userLoginStateEnum } from '../../store/authentication/authentication.reducer';
+import { userLoginStateEnum } from '../store/authentication/authentication.reducer';
 
 @Injectable()
 export class NeedsAuthentication implements CanActivate {
-  private user: VerifyEmailResponseModel;
   constructor(public _router: Router, private store: Store<AppState>, private _companyService: CompanyService, private companyActions: CompanyActions) {
   }
   public canActivate() {
