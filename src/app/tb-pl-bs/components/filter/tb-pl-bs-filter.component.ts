be19dea18c798ec73f473056bb905b19f85c3077
GOVINDA ALWANI <govi2010@gmail.com>
2017-11-29 12:30:02 +0530

    Ledger entry hover & Same date picker not have all the places (#134)
    * Govinda | 3. Ledger entry hover description not showing. For reference check on angular 1.
    
    * Govinda | 2. Same date picker not have all the places, ledger have different one and trial balance is different one.

--- src/app/tb-pl-bs/components/filter/tb-pl-bs-filter.component.ts
+++ src/app/tb-pl-bs/components/filter/tb-pl-bs-filter.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { TrialBalanceRequest } from '../../../models/api-models/tb-pl-bs';
 import { CompanyResponse } from '../../../models/api-models/Company';
 import { IOption } from '../../../theme/ng-virtual-select/sh-options.interface';
-
+import * as moment from 'moment/moment';
 @Component({
   selector: 'tb-pl-bs-filter',  // <home></home>
   templateUrl: './tb-pl-bs-filter.component.html',
@@ -15,6 +15,41 @@ export class TbPlBsFilterComponent implements OnInit, OnDestroy, OnChanges {
   public filterForm: FormGroup;
   public search: string;
   public financialOptions: IOption[] = [];
+  public datePickerOptions: any = {
+    locale: {
+      applyClass: 'btn-green',
+      applyLabel: 'Go',
+      fromLabel: 'From',
+      format: 'D-MMM-YY',
+      toLabel: 'To',
+      cancelLabel: 'Cancel',
+      customRangeLabel: 'Custom range'
+    },
+    ranges: {
+      'Last 1 Day': [
+        moment().subtract(1, 'days'),
+        moment()
+      ],
+      'Last 7 Days': [
+        moment().subtract(6, 'days'),
+        moment()
+      ],
+      'Last 30 Days': [
+        moment().subtract(29, 'days'),
+        moment()
+      ],
+      'Last 6 Months': [
+        moment().subtract(6, 'months'),
+        moment()
+      ],
+      'Last 1 Year': [
+        moment().subtract(12, 'months'),
+        moment()
+      ]
+    },
+    startDate: moment().subtract(30, 'days'),
+    endDate: moment()
+  };
   @Input() public tbExportPdf: boolean = false;
   @Input() public tbExportXLS: boolean = false;
   @Input() public tbExportCsv: boolean = false;
@@ -30,7 +65,7 @@ export class TbPlBsFilterComponent implements OnInit, OnDestroy, OnChanges {
   public expandAll: EventEmitter<boolean> = new EventEmitter<boolean>();
   public showClearSearch: boolean;
   public request: TrialBalanceRequest = {};
-  public dateOptions: IOption[] = [{label: 'Date Range', value: '1'}, {label: 'Financial Year', value: '0'}];
+  public dateOptions: IOption[] = [{ label: 'Date Range', value: '1' }, { label: 'Financial Year', value: '0' }];
 
   @Input() public showLoader: boolean = true;
 
@@ -44,7 +79,7 @@ export class TbPlBsFilterComponent implements OnInit, OnDestroy, OnChanges {
     }
     this._selectedCompany = value;
     this.financialOptions = value.financialYears.map(q => {
-      return {label: q.uniqueName, value: q.uniqueName};
+      return { label: q.uniqueName, value: q.uniqueName };
     });
     this.filterForm.patchValue({
       to: value.activeFinancialYear.financialYearEnds,
@@ -81,7 +116,7 @@ export class TbPlBsFilterComponent implements OnInit, OnDestroy, OnChanges {
   public ngOnInit() {
     //
     if (!this.showLabels) {
-      this.filterForm.patchValue({selectedDateOption: '0'});
+      this.filterForm.patchValue({ selectedDateOption: '0' });
     }
   }
 
@@ -92,7 +127,10 @@ export class TbPlBsFilterComponent implements OnInit, OnDestroy, OnChanges {
   public selectDateOption(v: IOption) {
     // this.selectedDateOption = v.value || '';
   }
-
+  public selectedDate(value: any) {
+    this.filterForm.controls['from'].setValue(moment(value.start).format('DD-MM-YYYY'));
+    this.filterForm.controls['to'].setValue(moment(value.end).format('DD-MM-YYYY'));
+  }
   public selectFinancialYearOption(v: IOption) {
     if (v.value) {
       let financialYear = this._selectedCompany.financialYears.find(p => p.uniqueName === v.value);
