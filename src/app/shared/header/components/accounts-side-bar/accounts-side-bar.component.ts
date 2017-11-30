94aeb3506a321b455547a8ca9b1b40f79a53a683
GOVINDA ALWANI <govi2010@gmail.com>
2017-11-30 17:42:28 +0530

    Ghlabs master bugfixes Account Search Optmization  (#152)
    * Govinda | If user is on Cash ledger and has selected 1st Nov - 10th Nov in date range, and user creates a ledger entry in this range (For eg: he makes Cash to Sales entry on 1st Nov) then after creating this ledger, last 30 days transactions are displayed in ledger but date range does not change. Date range is still 1st Nov - 10th Nov.
    
    This is incorrect. After creating the ledger, still all ledger entries between 1st Nov and 10th Noc only should be displayed
    Please refer to angular 1 app or contact me if you have any issues.
    
    * Govinda | EL-293 Download Invoice from Updat form.
    
    * Invoice generated flage added
    
    * Govinda Allowing cmd+v and control+ V on number directive
    
    * Govinda | EL-254 point 7
    Search Accounts – If user searches for an account that is not present, 2 Different messages are displayed and "add new" button is not available on one of them. (Click on the "+/-" button to toggle between the messages.) Also, "Manage Groups & Accounts" pop up should open up on clicking "add new" button. (Please refer to angular 1 app)
    
    * Govinda | Express has been change error in SH-select
    
    * Vishal - ledger total bug fixed
    
    * Govinda | SH select FIxing
    
    * Vishal - EL - 240 initial work done
    
    * Govinda | Ledger date range reset on open new ledger.
    
    * Vishal - two way auth bug fixed
    
    * Vishal - EL - 299
    
    * Vishal - EL - 240 front-end side done some stuff need to handled from server side
    
    * Vishal - bug fix
    
    * Govinda | Account Search Speed Inprovement
    
    * Merge conflict
    
    * API dev
    
    * Govinda | Fixing undesign

--- src/app/shared/header/components/accounts-side-bar/accounts-side-bar.component.ts
+++ src/app/shared/header/components/accounts-side-bar/accounts-side-bar.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, SimpleChanges, OnChanges, OnInit, Output, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { AppState } from '../../../../store/roots';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import { IFlattenGroupsAccountsDetail } from '../../../../models/interfaces/flattenGroupsAccountsDetail.interface';
+import { IFlattenGroupsAccountsDetail, IFlattenGroupsAccountItem, IFlattenGroupsAccountsDetailItem } from '../../../../models/interfaces/flattenGroupsAccountsDetail.interface';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { FlyAccountsActions } from '../../../../actions/fly-accounts.actions';
 import * as _ from '../../../../lodash-optimized';
@@ -10,36 +10,83 @@ import * as _ from '../../../../lodash-optimized';
 @Component({
   selector: 'accounts-side-bar',
   templateUrl: './accounts-side-bar.component.html',
-  styleUrls: ['./accounts-side-bar.component.css']
+  styleUrls: ['./accounts-side-bar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AccountsSideBarComponent implements OnInit, OnDestroy {
+export class AccountsSideBarComponent implements OnInit, OnDestroy, OnChanges {
   @Input() public flyAccounts: boolean;
+  @Input() public search: string;
+  @Input() public noGroups: boolean;
   public flatAccountWGroupsList: IFlattenGroupsAccountsDetail[];
+  public Items: IFlattenGroupsAccountItem[];
+  public ItemsSRC: IFlattenGroupsAccountItem[];
   public isFlyAccountInProcess$: Observable<boolean>;
   public companyList$: Observable<any>;
   public showAccountList: boolean = true;
   @Output() public openAddAndManage: EventEmitter<boolean> = new EventEmitter();
 
-  @Input()
-  public set noGroups(val: boolean) {
-    this._noGroups = val;
-    this.toggleAccounts(val);
-  }
-
-  public get noGroups(): boolean {
-    return this._noGroups;
-  }
-
-  private _noGroups: boolean;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-  constructor(private store: Store<AppState>, private _flyAccountActions: FlyAccountsActions) {
+  constructor(private store: Store<AppState>, private _flyAccountActions: FlyAccountsActions, private cd: ChangeDetectorRef) {
     this.store.select(state => {
       return state.flyAccounts.flattenGroupsAccounts;
     }).takeUntil(this.destroyed$).subscribe(p => {
       this.flatAccountWGroupsList = _.cloneDeep(p);
-      this.toggleAccounts(this._noGroups);
-      this._noGroups = true;
+      this.Items = _.flatten(_.map(this.flatAccountWGroupsList, (g: IFlattenGroupsAccountsDetail): IFlattenGroupsAccountItem[] => {
+        let r: IFlattenGroupsAccountItem[] = [{
+          Name: g.groupName,
+          UniqueName: g.groupUniqueName,
+          isGroup: true,
+          isOpen: false,
+          groupUniqueName: null,
+        }];
+        if (g.accountDetails) {
+          let accs = _.map(g.accountDetails, (a: IFlattenGroupsAccountsDetailItem): IFlattenGroupsAccountItem => {
+            return {
+              Name: a.name,
+              UniqueName: a.uniqueName,
+              isGroup: false,
+              isOpen: false,
+              groupUniqueName: g.groupUniqueName
+            } as IFlattenGroupsAccountItem;
+          });
+          r[0].accounts = accs;
+        } else {
+          r[0].accounts = [];
+        }
+        return r;
+      }));
+      this.ItemsSRC = _.flatten(_.map(this.flatAccountWGroupsList, (g: IFlattenGroupsAccountsDetail): IFlattenGroupsAccountItem[] => {
+        let r: IFlattenGroupsAccountItem[] = [{
+          Name: g.groupName,
+          UniqueName: g.groupUniqueName,
+          isGroup: true,
+          isOpen: false,
+          groupUniqueName: null,
+        }];
+        if (g.accountDetails) {
+          let accs = _.map(g.accountDetails, (a: IFlattenGroupsAccountsDetailItem): IFlattenGroupsAccountItem => {
+            return {
+              Name: a.name,
+              UniqueName: a.uniqueName,
+              isGroup: false,
+              isOpen: false,
+              groupUniqueName: g.groupUniqueName
+            } as IFlattenGroupsAccountItem;
+          });
+          r[0].accounts = accs;
+          r = r.concat(accs);
+        } else {
+          r[0].accounts = [];
+        }
+        return r;
+      }));
+      if (this.search && this.search.length > 0) {
+        this.toggleNoGroups(true);
+      }
+      // this.noGroups = this.noGroups;
+      // this.toggleAccounts(this._noGroups);
+      // this._noGroups = true;
     });
     this.isFlyAccountInProcess$ = this.store.select(s => s.flyAccounts.isFlyAccountInProcess).takeUntil(this.destroyed$);
 
@@ -47,16 +94,71 @@ export class AccountsSideBarComponent implements OnInit, OnDestroy {
       return state.session.companies;
     }).takeUntil(this.destroyed$);
   }
-
+  public ngOnChanges(s: SimpleChanges) {
+    if (s.noGroups && !s.noGroups.firstChange && s.noGroups.currentValue !== s.noGroups.previousValue) {
+      this.toggleNoGroups(s.noGroups.currentValue);
+    }
+    if (s.search && !s.search.firstChange && s.search.currentValue !== s.search.previousValue) {
+      // this.searchAccount(s.search.currentValue);
+    }
+  }
   public ngOnInit() {
     this.store.dispatch(this._flyAccountActions.GetflatAccountWGroups());
   }
-
-  public toggleAccounts(noGroups: boolean) {
-    this.flatAccountWGroupsList.forEach(p => {
-      p.isOpen = noGroups;
+  public searchAccount(s: string) {
+    this.Items = _.cloneDeep(this.ItemsSRC);
+    if (s === '') {
+      this.Items = _.cloneDeep(this.ItemsSRC);
+      this.cd.detectChanges();
+    } else {
+      for (let index = 0; index < this.Items.length; index++) {
+        let a = this.Items[index];
+        if (!a.isGroup) {
+          if (a.Name.indexOf(s) === -1 && a.UniqueName.indexOf(s) === -1) {
+            this.Items.splice(index, 1);
+            index = index - 1;
+          }
+        } else {
+          a.isOpen = true;
+          if (index > 0 && this.Items[index - 1].isGroup) {
+            this.Items.splice(index - 1, 1);
+            index = index - 1;
+          }
+        }
+      }
+      if (this.Items[this.Items.length - 1].isGroup) {
+        this.Items.splice(this.Items.length - 1, 1);
+      }
+      this.Items = _.cloneDeep(this.Items);
+      this.cd.detectChanges();
     }
-    );
+  }
+  public toggleNoGroups(noGroups: boolean) {
+    // this.flatAccountWGroupsList.forEach(p => {
+    //   p.isOpen = noGroups;
+    // });
+    if (noGroups) {
+      /// open all Groups
+      for (let index = 0; index < this.Items.length; index++) {
+        let a = this.Items[index];
+        if (a.isGroup && !a.isOpen) {
+          this.Items[index].isOpen = true;
+          let accs = _.cloneDeep(this.Items[index].accounts);
+          this.Items.splice(index + 1, 0, ...accs);
+        }
+      }
+    } else {
+      // close all groups
+      for (let index = 0; index < this.Items.length; index++) {
+        let a = this.Items[index];
+        if (a.isGroup && a.isOpen) {
+          this.Items[index].isOpen = false;
+          this.Items.splice(index + 1, a.accounts.length);
+        }
+      }
+    }
+    this.Items = _.cloneDeep(this.Items);
+    this.cd.detectChanges();
   }
 
   public goToManageGroups() {
@@ -67,5 +169,25 @@ export class AccountsSideBarComponent implements OnInit, OnDestroy {
     this.store.dispatch(this._flyAccountActions.ResetflatAccountWGroups());
     this.destroyed$.next(true);
     this.destroyed$.complete();
+  }
+  public getSize(item: IFlattenGroupsAccountItem, index) {
+    if (item.isGroup) {
+      return 37;
+    }
+    return 32;
+  }
+  public toggleGroup(group: IFlattenGroupsAccountItem, index, event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (group.isOpen) {
+      this.Items[index].isOpen = false;
+      this.Items.splice(index + 1, group.accounts.length);
+    } else {
+      this.Items[index].isOpen = true;
+      let accs = _.cloneDeep(this.Items[index].accounts);
+      this.Items.splice(index + 1, 0, ...accs);
+    }
+    this.Items = _.cloneDeep(this.Items);
+    this.cd.detectChanges();
   }
 }
