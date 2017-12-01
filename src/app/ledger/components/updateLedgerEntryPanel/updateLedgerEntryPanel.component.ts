8fc4fcde98722e6d18738cc0639ddced0ae79316
Mustafa bagwala <mustafa@walkover.in>
2017-12-01 15:49:53 +0530

    EL-304 & fix styling in sh-select (#154)
    * mustafa | EL-304 & fix width issue in sh-select
    
    * mustafa | minor styling fix in sh-select

--- src/app/ledger/components/updateLedgerEntryPanel/updateLedgerEntryPanel.component.ts
+++ src/app/ledger/components/updateLedgerEntryPanel/updateLedgerEntryPanel.component.ts
@@ -61,6 +61,7 @@ export class UpdateLedgerEntryPanelComponent implements OnInit, AfterViewInit, O
     }
   };
   public destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  public showAdvanced: boolean;
 
   constructor(private store: Store<AppState>, private _ledgerService: LedgerService,
               private route: ActivatedRoute, private _toasty: ToasterService, private _accountService: AccountService,
@@ -78,6 +79,7 @@ export class UpdateLedgerEntryPanelComponent implements OnInit, AfterViewInit, O
   }
 
   public ngOnInit() {
+    this.showAdvanced = false;
     this.vm = new UpdateLedgerVm();
     this.vm.selectedLedger = new LedgerResponse();
     // TODO: save backup of response for future use
