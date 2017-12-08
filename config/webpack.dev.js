1b0adbd3c28c92ddbf2508fbb040ea0301da76e4
GOVINDA ALWANI <govi2010@gmail.com>
2017-12-08 12:12:13 +0530

    Ghlabs master bugfixes (#186)
    * Vishal - El - 299 show account name in-place of account uniqueName
    
    * Vishal - El - 312 search with mergedaccount uniquename
    
    * Vishal - El - 313 calculate reckoning total in ledger
    
    * Vishal - El - 313 lint issues
    
    * Vishal - update ledger check entry category
    
    * Vishal - EL - 309
    
    * Vishal - update ledger bug fix
    
    * Vishal - El - 323
    
    * Vishal - ledger isBaseAccount bug fixed
    
    * Vishal - bug fixes
    
    * Vishal - EL - 311
    
    * Vishal - revert webpack
    
    * Vishal - El - 322 point 3 done
    
    * Govinda| Logout Issue
    
    *   Vishal - tb-pl daterangepicker bug fixed
    
    * Vishal | El - 318
    
    * Vishal | update ledger total to words bug fixes
    
    * Viashal | update ledger tax updated modal bug fix
    
    * Vishal | update ledger changes
    
    * Vishal | taxes not updating bug fixed
    
    * Vishal | EL - 325

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -23,25 +23,24 @@ const ERRLYTICS_KEY_DEV = '';
 /**
  * Webpack Constants
  */
-// const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
-// const HOST = process.env.HOST || 'localapp.giddh.com';
-// const PORT = process.env.PORT || 3000;
-// const HMR = helpers.hasProcessFlag('hot');
-// const AppUrl = 'http://dev.giddh.com/electron';
-// const ApiUrl = 'http://apidev.giddh.com/';
-// const METADATA = webpackMerge(commonConfig({ env: ENV }).metadata, {
-//     host: HOST,
-//     port: PORT,
-//     ENV: ENV,
-//     HMR: HMR,
-//     isElectron: false,
-//     errlyticsNeeded: false,
-//     errlyticsKey: ERRLYTICS_KEY_DEV,
-//     AppUrl: AppUrl,
-//     ApiUrl: ApiUrl
-// });
 
-
+const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+const HOST = process.env.HOST || 'localapp.giddh.com';
+const PORT = process.env.PORT || 3000;
+const HMR = helpers.hasProcessFlag('hot');
+const AppUrl = 'http://dev.giddh.com/electron';
+const ApiUrl = 'http://apitest.giddh.com/';
+const METADATA = webpackMerge(commonConfig({ env: ENV }).metadata, {
+    host: HOST,
+    port: PORT,
+    ENV: ENV,
+    HMR: HMR,
+    isElectron: false,
+    errlyticsNeeded: false,
+    errlyticsKey: ERRLYTICS_KEY_DEV,
+    AppUrl: AppUrl,
+    ApiUrl: ApiUrl
+});
 
 /**
  * Webpack configuration
