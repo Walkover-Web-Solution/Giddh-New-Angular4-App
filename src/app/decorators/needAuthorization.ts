ceae52fc631c38221a9938fff0d225003cfcd8d3
Arpit Meena <arpitmeena@walkover.in>
2017-12-14 18:29:23 +0530

    Arpit | Permission related bug fixed

--- src/app/decorators/needAuthorization.ts
+++ src/app/decorators/needAuthorization.ts
@@ -77,14 +77,13 @@ export class NeedsAuthorization implements CanActivate {
         if (indexOfHome !== -1) {
           this._router.navigate(['/pages/home']);
         } else {
-          if (permissions.length) {
+          if (permissions && permissions.length) {
             let firstPermittedScope = SCOPE_TO_ROUTE_MAPPING.find((scope) => scope.value === permissions[0].name);
             this._router.navigate([firstPermittedScope.key]);
           } else {
-            alert('none');
+            this._router.navigate(['/login']);
           }
         }
-
         return false;
       }
     } else {
