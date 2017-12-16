5761ba35ad2b80391274b57730db3e9c083feea5
Arpit Meena <arpitmeena@walkover.in>
2017-12-16 11:20:07 +0530

    Arpit | Universal date code refactored

--- src/app/invoice/preview/invoice.preview.component.ts
+++ src/app/invoice/preview/invoice.preview.component.ts
@@ -19,6 +19,7 @@ import { InvoiceService } from '../../services/invoice.service';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 import { GIDDH_DATE_FORMAT } from '../../shared/helpers/defaultDateFormat';
 import { ModalDirective } from 'ngx-bootstrap';
+import { createSelector } from 'reselect';
 
 const COUNTS = [
   { label: '12', value: '12' },
@@ -82,15 +83,8 @@ export class InvoicePreviewComponent implements OnInit {
     private _accountService: AccountService,
     private _invoiceService: InvoiceService,
   ) {
-    // this.startDate = new Date();
-    // this.endDate = new Date();
-    // this.startDate.setDate(this.startDate.getDate() - 30);
-    // this.endDate.setDate(this.endDate.getDate());
-    // this.invoiceSearchRequest.dateRange = [this.startDate, this.endDate];
     this.invoiceSearchRequest.page = 1;
     this.invoiceSearchRequest.count = 25;
-    // this.invoiceSearchRequest.from = moment(this.startDate).format(GIDDH_DATE_FORMAT);
-    // this.invoiceSearchRequest.to = moment(this.endDate).format(GIDDH_DATE_FORMAT);
     this.invoiceSearchRequest.entryTotalBy = '';
   }
 
@@ -142,13 +136,13 @@ export class InvoicePreviewComponent implements OnInit {
       });
 
     // Refresh report data according to universal date
-    this.store.select(p => p.session.applicationDate).distinctUntilChanged().takeUntil(this.destroyed$).subscribe((value: any) => {
-      this.universalDate = _.cloneDeep(value);
-      if (this.universalDate) {
+    this.store.select(createSelector([(state: AppState) => state.session.applicationDate], (dateObj: Date[]) => {
+      if (dateObj) {
+        this.universalDate = _.cloneDeep(dateObj);
         this.invoiceSearchRequest.dateRange = this.universalDate;
       }
       this.getInvoices();
-    });
+    })).subscribe();
   }
 
   public getInvoiceTemplateDetails(templateUniqueName: string) {
