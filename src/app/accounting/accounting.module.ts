d3b2ad4e0a39cb507a719f23e8b75c703e07e286
Arpit Meena <arpitmeena@walkover.in>
2017-12-16 12:19:32 +0530

    +Arpit | Journal routing code added

---
+++ src/app/accounting/accounting.module.ts
@@ -0,0 +1,40 @@
+import { PurchaseComponent } from './purchase/purchase.component';
+import { JournalComponent } from './journal/journal.component';
+import { AccountingRoutingModule } from './accounting-routing.module';
+import { AccountingComponent } from './accounting.component';
+import { ShSelectModule } from './../theme/ng-virtual-select/sh-select.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BsDatepickerModule, DatepickerModule } from 'ngx-bootstrap/datepicker';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { LaddaModule } from 'angular2-ladda';
+import { SelectModule } from '../theme/ng-select/ng-select';
+import { DecimalDigitsModule } from '../shared/helpers/directives/decimalDigits/decimalDigits.module';
+
+@NgModule({
+  declarations: [
+    AccountingComponent,
+    JournalComponent,
+    PurchaseComponent
+  ],
+  exports: [RouterModule],
+  providers: [],
+  imports: [
+    AccountingRoutingModule,
+    RouterModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DatepickerModule,
+    BsDatepickerModule.forRoot(),
+    ModalModule,
+    LaddaModule,
+    SelectModule,
+    DecimalDigitsModule,
+    ShSelectModule
+  ],
+})
+export class AccountingModule {
+}
