6c2b9077a7f85edb888f102f780cdf51f08c713b
Govinda Alwani <govi2010@gmail.com>
2017-12-20 10:52:04 +0530

    Vishal - daybook pagination implements

--- src/app/daybook/daybook.component.ts
+++ src/app/daybook/daybook.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'app/store';
 import * as moment from 'moment/moment';
-import { TransactionsRequest } from 'app/models/api-models/Ledger';
 import { ModalDirective } from 'ngx-bootstrap';
 import { DaybookActions } from 'app/actions/daybook/daybook.actions';
 import { DayBookResponseModel } from '../models/api-models/Daybook';
 import { Observable } from 'rxjs/Observable';
 import { ReplaySubject } from 'rxjs';
-import { createSelector } from 'reselect';
 import { DaybookQueryRequest } from '../models/api-models/DaybookRequest';
 import { DaterangePickerComponent } from '../theme/ng2-daterangepicker/daterangepicker.component';
 
@@ -21,7 +19,7 @@ export class DaybookComponent implements OnDestroy {
   public daybookQueryRequest: DaybookQueryRequest;
   public daybookData$: Observable<DayBookResponseModel>;
   @ViewChild('advanceSearchModel') public advanceSearchModel: ModalDirective;
-  @ViewChild('dateRangePickerCmp', { read: DaterangePickerComponent }) public dateRangePickerCmp: DaterangePickerComponent;
+  @ViewChild('dateRangePickerCmp', {read: DaterangePickerComponent}) public dateRangePickerCmp: DaterangePickerComponent;
   public datePickerOptions: any = {
     locale: {
       applyClass: 'btn-green',
@@ -58,10 +56,12 @@ export class DaybookComponent implements OnDestroy {
     endDate: moment()
   };
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
   constructor(private store: Store<AppState>, private _daybookActions: DaybookActions) {
     this.daybookQueryRequest = new DaybookQueryRequest();
     this.store.select(s => s.daybook.data).takeUntil(this.destroyed$).subscribe((data) => {
       if (data && data.entries.length) {
+        this.daybookQueryRequest.page = data.page;
         data.entries.map(a => {
           a.isExpanded = false;
         });
@@ -81,6 +81,7 @@ export class DaybookComponent implements OnDestroy {
 
     this.initialRequest();
   }
+
   public selectedDate(value: any) {
     this.daybookQueryRequest.from = moment(value.picker.startDate).format('DD-MM-YYYY');
     this.daybookQueryRequest.to = moment(value.picker.endDate).format('DD-MM-YYYY');
@@ -88,9 +89,11 @@ export class DaybookComponent implements OnDestroy {
 
     // this.getTransactionData();
   }
+
   public onOpenAdvanceSearch() {
     this.advanceSearchModel.show();
   }
+
   public closeAdvanceSearchPopup(obj) {
     if (!obj.cancle) {
 
@@ -104,6 +107,7 @@ export class DaybookComponent implements OnDestroy {
     }
     this.advanceSearchModel.hide();
   }
+
   public go() {
     this.store.dispatch(this._daybookActions.GetDaybook(null, this.daybookQueryRequest));
   }
@@ -111,6 +115,11 @@ export class DaybookComponent implements OnDestroy {
   public initialRequest() {
     this.daybookQueryRequest.from = this.daybookQueryRequest.from || this.datePickerOptions.startDate.format('DD-MM-YYYY');
     this.daybookQueryRequest.to = this.daybookQueryRequest.to || this.datePickerOptions.endDate.format('DD-MM-YYYY');
+    this.go();
+  }
+
+  public pageChanged(event: any): void {
+    this.daybookQueryRequest.page = event.page;
     this.go();
   }
 
