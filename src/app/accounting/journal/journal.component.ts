ca1d357c80a6740f8f1ef336365c35fb009aa97c
Arpit Meena <arpitmeena@walkover.in>
2017-12-20 19:03:54 +0530

    +Arpit | Journal part done page (1)

--- src/app/accounting/journal/journal.component.ts
+++ src/app/accounting/journal/journal.component.ts
@@ -1,3 +1,7 @@
+import { GIDDH_DATE_FORMAT } from './../../shared/helpers/defaultDateFormat';
+import { CreatedBy } from './../../models/api-models/Invoice';
+import { IParticular } from './../../models/api-models/Ledger';
+import { setTimeout } from 'timers';
 import { VsForDirective } from './../../theme/ng2-vs-for/ng2-vs-for';
 import { ToasterService } from './../../services/toaster.service';
 import { KeyboardService } from './../keyboard.service';
@@ -8,11 +12,12 @@ import { AccountService } from './../../services/account.service';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/roots';
-import { Component, OnInit, ViewChild, OnDestroy, ViewChildren, QueryList } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy, ViewChildren, QueryList, transition } from '@angular/core';
 import { Location } from '@angular/common';
 import { createSelector } from 'reselect';
 import { Observable } from 'rxjs/Observable';
 import * as _ from 'lodash';
+import * as moment from 'moment';
 
 @Component({
   templateUrl: './journal.component.html',
@@ -29,7 +34,10 @@ export class JournalComponent implements OnInit, OnDestroy {
   public showLedgerAccountList: boolean = false;
   public selectedInput: 'by' | 'to' = null;
   public journalRequestObject: any = {};
-  public isLedgerCreateSuccess$: Observable<boolean>;
+  public totalCreditAmount: number = 0;
+  public totalDebitAmount: number = 0;
+  public showSubmitButtonBox: boolean = false;
+  public moment = moment;
 
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
@@ -39,8 +47,8 @@ export class JournalComponent implements OnInit, OnDestroy {
     private store: Store<AppState>,
     private _keyboardService: KeyboardService,
     private _toaster: ToasterService) {
+    this.journalRequestObject.entryData = moment().format(GIDDH_DATE_FORMAT);
     this.journalRequestObject.transactions = [];
-    this.isLedgerCreateSuccess$ = this.store.select(p => p.ledger.ledgerCreateSuccess).takeUntil(this.destroyed$);
   }
 
   public ngOnInit() {
@@ -58,9 +66,11 @@ export class JournalComponent implements OnInit, OnDestroy {
     //   console.log('Hello the key is :', key);
     // });
 
-    this.isLedgerCreateSuccess$.subscribe((s: boolean) => {
+    this.store.select(p => p.ledger.ledgerCreateSuccess).takeUntil(this.destroyed$).subscribe((s: boolean) => {
       if (s) {
         this._toaster.successToast('Entry created successfully', 'Success');
+        this.resetEntry();
+        this.store.dispatch(this._ledgerActions.ResetLedger());
       }
     });
   }
@@ -77,19 +87,8 @@ export class JournalComponent implements OnInit, OnDestroy {
 
   public searchInList(str: string) {
     let allData = _.cloneDeep(this.allAccounts);
-    this.accounts =  _.filter(allData, (o) => _.includes(o.label, str));
+    this.accounts = _.filter(allData, (o) => _.includes(o.label, str));
   }
-
-  public createEntry() {
-    let data = {
-      transactions: [{ amount: '20', particular: 'sales', type: 'debit' }, { amount: '20', particular: 'cash', type: 'debit' }],
-      entryDate: '16-12-2017',
-      description: 'Hello this is a sample entry in Journal',
-    };
-    let accUniqueName: string = null;
-    // this.store.dispatch(this._ledgerActions.CreateBlankLedger(data, 'generalreserves'));
-  }
-
   public selectInputType(ev: KeyboardEvent) {
     if (ev.key === 'c') {
       this.selectedInput = 'to';
@@ -104,21 +103,69 @@ export class JournalComponent implements OnInit, OnDestroy {
     let target: any = _.cloneDeep(ev.target);
     console.log('enter key is :', ev);
     if (ev.keyCode === 13) {
-      if (this.selectedInput === 'to') {
+      if (this.selectedInput === 'by') {
         this.journalRequestObject.transactions.push({
           type: 'debit',
-          amount: inputAmount,
+          amount: Number(inputAmount),
           particular: selectedAccount
         });
-      } else if (this.selectedInput === 'by') {
+        let debitTransactions = _.filter(this.journalRequestObject.transactions, (o) => o.type === 'debit');
+        this.totalDebitAmount = _.sumBy(debitTransactions, (o) => o.amount);
+      } else if (this.selectedInput === 'to') {
         this.journalRequestObject.transactions.push({
           type: 'credit',
-          amount: inputAmount,
+          amount: Number(inputAmount),
           particular: selectedAccount
         });
+        let creditTransactions = _.filter(this.journalRequestObject.transactions, (o) => o.type === 'credit');
+        this.totalCreditAmount = _.sumBy(creditTransactions, (o) => o.amount);
       }
+
       this.selectedInput = null;
+      this.journalRequestObject.by = this.journalRequestObject.to = null;
     }
+  }
+
+  public detectEnterKeyOnNarration(ev: KeyboardEvent, submitBtnEle: HTMLButtonElement) {
+    if (ev.keyCode === 13) {
+      this.showSubmitButtonBox = true;
+      // Remove enter symbol form narration textarea
+      if (this.journalRequestObject.description) {
+        this.journalRequestObject.description = this.journalRequestObject.description.replace(/(?:\r\n|\r|\n)/g, '');
+      }
+      setTimeout(() => {
+        submitBtnEle.focus();
+      }, 100);
+    }
+  }
+
+  public createEntry() {
+    if (this.totalCreditAmount === this.totalDebitAmount) {
+      let data = _.cloneDeep(this.journalRequestObject);
+      _.forEach(data.transactions, element => {
+        element.particular = _.find(this.accounts, (el) => el.label === element.particular).value;
+      });
+      let accUniqueName: string = _.maxBy(data.transactions, (o: any) => o.amount).particular;
+      let indexOfMaxAmountEntry = _.findIndex(data.transactions, (o: any) => o.particular === accUniqueName);
+      delete data.by;
+      delete data.to;
+      data.transactions.splice(indexOfMaxAmountEntry, 1);
+      this.store.dispatch(this._ledgerActions.CreateBlankLedger(data, accUniqueName));
+    } else {
+      this._toaster.errorToast('Total credit amount and total debit amount should be equal.', 'Error');
+    }
+  }
+
+  public resetEntry() {
+    this.journalRequestObject.by = null;
+    this.journalRequestObject.to = null;
+    this.journalRequestObject.description = null;
+    this.journalRequestObject.transactions = [];
+    this.selectedInput = null;
+    this.showSubmitButtonBox = false;
+    this.showLedgerAccountList = false;
+    this.totalCreditAmount = 0;
+    this.totalDebitAmount = 0;
   }
 
   public ngOnDestroy() {
