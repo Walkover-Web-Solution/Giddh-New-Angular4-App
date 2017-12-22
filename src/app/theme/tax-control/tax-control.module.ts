429fb03aa80d87de2484b41a154fc6f7f45fb9f6
Sarfaraz Ansari <sarfaraz_designer@live.in>
2017-12-22 11:03:01 +0530

    fix: clickOutside event

--- src/app/theme/tax-control/tax-control.module.ts
+++ src/app/theme/tax-control/tax-control.module.ts
@@ -2,11 +2,14 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaxControlComponent } from './tax-control.component';
 import { FormsModule } from '@angular/forms';
+import { ClickOutsideModule } from 'ng-click-outside';
+
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ClickOutsideModule],
   declarations: [TaxControlComponent],
   exports: [TaxControlComponent]
 })
+
 export class TaxControlModule {
   public static forRoot(): ModuleWithProviders {
     return {
