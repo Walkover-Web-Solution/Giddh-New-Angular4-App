b59390bf47db92fd28bac0fed0874a1628661afc
Sarfaraz Ansari <sarfaraz_designer@live.in>
2017-12-23 11:09:55 +0530

    fix: EGW-99

--- src/app/permissions/permission.component.ts
+++ src/app/permissions/permission.component.ts
@@ -10,11 +10,11 @@ import { Store } from '@ngrx/store';
 })
 export class PermissionComponent implements OnInit {
   constructor(private router: Router, private store: Store<AppState>, private companyActions: CompanyActions) {
-    this.router.events.subscribe((event: NavigationStart) => {
-      if (event.url === '/pages/permissions' || event.url === '/pages/permissions/') {
-        this.router.navigate(['/pages', 'permissions', 'list']);
-      }
-    });
+    // this.router.events.subscribe((event: NavigationStart) => {
+    //   if (event.url === '/pages/permissions' || event.url === '/pages/permissions/') {
+    //     this.router.navigate(['/pages', 'permissions', 'list']);
+    //   }
+    // });
   }
   public ngOnInit(): void {
     let companyUniqueName = null;
