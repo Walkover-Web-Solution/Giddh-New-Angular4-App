caca642cc36cdfb8d2f3e8798a89e24f493520c3
Govinda Alwani <govi2010@gmail.com>
2017-12-29 16:26:44 +0530

    Updating Electron

--- src/mainprocess/index.ts
+++ src/mainprocess/index.ts
@@ -18,7 +18,11 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({
+    width: 800, height: 600, webPreferences: {
+      plugins: true,
+    }
+  });
 
   // and load the index.html of the app.
   win.loadURL(indexUrl);
