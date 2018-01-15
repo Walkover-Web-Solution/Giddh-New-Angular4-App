2ad9efef9cd59c22c6dc8d08720749bd5d988561
Govinda Alwani <govi2010@gmail.com>
2018-01-15 14:07:01 +0530

    EL-380 Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,7 +53,7 @@ import { ServiceConfig } from 'app/services/service.config';
 // Application wide providers
 const APP_PROVIDERS = [
   ...APP_RESOLVER_PROVIDERS,
-  { provide: APP_BASE_HREF, useValue: '/' }
+  { provide: APP_BASE_HREF, useValue: isElectron ? '/' : AppUrl }
 ];
 
 interface InternalStateType {
@@ -131,7 +131,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     ToastrModule.forRoot({ preventDuplicates: true, maxOpened: 3 }),
     StoreModule.forRoot(reducers, { metaReducers }),
     PerfectScrollbarModule,
-    RouterModule.forRoot(ROUTES, { useHash: true, }),
+    RouterModule.forRoot(ROUTES, { useHash: isElectron }),
     StoreRouterConnectingModule,
     // StoreDevtoolsModule.instrument({
     //   maxAge: 25
