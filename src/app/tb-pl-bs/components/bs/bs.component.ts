c4ef40541186408363024c908a075b58196e248c
Govinda Alwani <govi2010@gmail.com>
2018-01-16 16:17:21 +0530

    Minor - El-390

--- src/app/tb-pl-bs/components/bs/bs.component.ts
+++ src/app/tb-pl-bs/components/bs/bs.component.ts
@@ -32,7 +32,7 @@ import { ChildGroup, Account } from '../../../models/api-models/Search';
            <span></span>
            <span></span>
            <span></span>
-          <h1>loading trial balance</h1>
+          <h1>loading balance sheet</h1>
         </div>
     </div>
     <div *ngIf="!(showLoader | async)">
