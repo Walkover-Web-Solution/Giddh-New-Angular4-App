4b4cf4b2a7dbf8d0b31a49e9338bf1c0b991f1a0
iMac <mustafa@walkover.in>
2018-02-20 15:37:05 +0530

    mustafa | styling changes in accounting voucher accpunt/stock list:

--- src/app/accounting/ng-virtual-list/virtual-list.module.ts
+++ src/app/accounting/ng-virtual-list/virtual-list.module.ts
@@ -1,10 +1,10 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AVAccountListComponent } from './sh-select-menu.component';
-import { AVShSelectComponent } from './sh-select.component';
 import { CommonModule } from '@angular/common';
 import { ClickOutsideModule } from 'ng-click-outside';
 import { VirtualScrollModule } from '../../theme/ng-virtual-select/virtual-scroll';
+import { AVShSelectComponent } from './virtual-list.component';
+import { AVAccountListComponent } from './virtual-list-menu.component';
 
 @NgModule({
   imports: [
