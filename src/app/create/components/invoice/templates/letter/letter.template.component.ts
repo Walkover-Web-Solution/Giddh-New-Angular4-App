201c193ef047a66382d26f8c9ddd91d6d9ab03af
Arpit Meena <arpitmeena@walkover.in>
2018-02-21 15:37:45 +0530

    Arpit | HTML code converted in base64

--- src/app/create/components/invoice/templates/letter/letter.template.component.ts
+++ src/app/create/components/invoice/templates/letter/letter.template.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output, OnDestroy, ViewEncapsulation }
 import { ReplaySubject } from 'rxjs';
 import { VoucherClass } from '../../../../../models/api-models/Sales';
 import { FormControl, Form } from '@angular/forms';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'letter-template',
@@ -22,8 +23,10 @@ export class LetterTemplateComponent implements OnInit, OnDestroy  {
   public name: string = 'August 09, 2017';
   public name1: string = 'GSTIN: 11AAAAAA000A1Z0';
   public name2: string = 'PAN: AAACW9768L';
+  public base64Data: SafeResourceUrl = '';
+  public planHtml: any;
 
-  constructor() {
+  constructor(private _sanitizer: DomSanitizer) {
     console.log (`hello from LetterTemplateComponent`);
   }
 
@@ -50,8 +53,42 @@ export class LetterTemplateComponent implements OnInit, OnDestroy  {
     //
   }
 
-  public emitTemplateData(data: Form) {
-    console.log ('emitTemplateData', data);
+  public xml2string(node) {
+    if (typeof(XMLSerializer) !== 'undefined') {
+       let serializer = new XMLSerializer();
+       return serializer.serializeToString(node);
+    } else if (node.xml) {
+       return node.xml;
+    }
+ }
+
+  public stringToUtf16ByteArray(str) {
+      let bytes = [];
+    for (let i = 0; i < str.length; ++i) {
+        let charCode = str.charCodeAt(i);
+        // tslint:disable-next-line:no-bitwise
+        bytes.push((charCode & 0xFF00) >>> 8);
+        // tslint:disable-next-line:no-bitwise
+        bytes.push(charCode & 0xFF);
+    }
+      return bytes;
+  }
+
+  public _arrayBufferToBase64( buffer ) {
+    let binary = '';
+    let bytes = new Uint8Array( buffer );
+    let len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
+  }
+
+  public emitTemplateData(data: any) {
+    const a = this.xml2string(data);
+    const stringToUtf16ByteArray = this.stringToUtf16ByteArray(a);
+    const _arrayBufferToBase64 = this._arrayBufferToBase64(stringToUtf16ByteArray);
+    this.base64Data = this._sanitizer.bypassSecurityTrustResourceUrl('data:application/pdf;base64,' + _arrayBufferToBase64);
   }
 
   public doDestroy() {
