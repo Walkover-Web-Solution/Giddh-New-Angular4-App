1203e603a54363496dfefb9943bafa90a910c3c0
Arpit Meena <arpitmeena@walkover.in>
2018-02-26 17:47:18 +0530

    Arpit | daybook export file functionality midway

--- src/app/actions/daybook/daybook.actions.ts
+++ src/app/actions/daybook/daybook.actions.ts
@@ -6,6 +6,7 @@ import { CustomActions } from '../../store/customActions';
 import { ToasterService } from '../../services/toaster.service';
 import { BaseResponse } from '../../models/api-models/BaseResponse';
 import { DaybookService } from '../../services/daybook.service';
+import { saveAs } from 'file-saver';
 import { DayBookRequestModel, DaybookQueryRequest } from '../../models/api-models/DaybookRequest';
 import { DayBookResponseModel } from '../../models/api-models/Daybook';
 
@@ -35,18 +36,43 @@ export class DaybookActions {
     .ofType(DaybookActions.EXPORT_DAYBOOK_REQUEST)
     .switchMap((action: CustomActions) => {
       return this._daybookService.ExportDaybook(action.payload.request, action.payload.queryRequest)
-        .map((r) => this.validateResponse<DayBookResponseModel, DayBookRequestModel>(r, {
-          type: DaybookActions.EXPORT_DAYBOOK_RESPONSE,
-          payload: r.body
-        }, true, {
-            type: DaybookActions.EXPORT_DAYBOOK_RESPONSE,
-            payload: null
-          }));
+        .map((res: any) => {
+          if (res.status === 'success') {
+            let blob = this.base64ToBlob(res.body, res.requestType, 512);
+            let type = res.requestType === 'application/pdf' ? 'pdf' : 'xls';
+            saveAs(blob, res.fileName + type);
+          } else {
+            this._toasty.clearAllToaster();
+            this._toasty.errorToast('Something went wrong while downloading file.');
+          }
+          return { type: 'EmptyAction' };
+        });
     });
 
   constructor(private action$: Actions,
     private _toasty: ToasterService,
     private _daybookService: DaybookService) {
+  }
+
+  public base64ToBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+    let byteCharacters = atob(b64Data);
+    let byteArrays = [];
+    let offset = 0;
+    while (offset < byteCharacters.length) {
+      let slice = byteCharacters.slice(offset, offset + sliceSize);
+      let byteNumbers = new Array(slice.length);
+      let i = 0;
+      while (i < slice.length) {
+        byteNumbers[i] = slice.charCodeAt(i);
+        i++;
+      }
+      let byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+      offset += sliceSize;
+    }
+    return new Blob(byteArrays, { type: contentType });
   }
 
   public GetDaybook(request: DayBookRequestModel, queryRequest: DaybookQueryRequest): CustomActions {
@@ -63,7 +89,7 @@ export class DaybookActions {
     };
   }
 
-  private validateResponse<TResponse, TRequest>(response: BaseResponse<TResponse, TRequest>, successAction: CustomActions, showToast: boolean = false, errorAction: CustomActions = { type: 'EmptyAction' }): CustomActions {
+  private validateResponse<TResponse, TRequest>(response: BaseResponse < TResponse, TRequest > , successAction: CustomActions, showToast: boolean = false, errorAction: CustomActions = { type: 'EmptyAction' }): CustomActions {
     if (response.status === 'error') {
       if (showToast) {
         this._toasty.errorToast(response.message);
