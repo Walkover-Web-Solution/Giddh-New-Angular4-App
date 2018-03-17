dabc83918c49b785736d10f9a93d0032e623799b
Arpit Meena <arpitmeena@walkover.in>
2018-03-17 16:10:12 +0530

    Arpit | APP url changes

--- config/webpack.test-build.js
+++ config/webpack.test-build.js
@@ -69,10 +69,11 @@ function getUglifyOptions(supportES2015) {
   };
 }
 
-module.exports = function (env) {
+module.exports = function (env) { 
   const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
   const supportES2015 = buildUtils.supportES2015(buildUtils.DEFAULT_METADATA.tsConfigPath);
-  const AppUrl = 'http://test.giddh.com/';
+  // const AppUrl = 'http://test.giddh.com/';
+  const AppUrl = 'giddhnew-fs8eefokm8yjj.stackpathdns.com/';
   const ApiUrl = 'http://apitest.giddh.com/';
   const METADATA = Object.assign({}, buildUtils.DEFAULT_METADATA, {
     host: process.env.HOST || 'test.giddh.com',
@@ -84,9 +85,7 @@ module.exports = function (env) {
     errlyticsKey: ERRLYTICS_KEY_DEV,
     AppUrl: AppUrl,
     ApiUrl: ApiUrl,
-    // APP_FOLDER: '/app/'
-    APP_FOLDER: 'giddhnew-fs8eefokm8yjj.stackpathdns.com'
-
+    APP_FOLDER: 'app/'
   });
 
   // set environment suffix so these environments are loaded.
