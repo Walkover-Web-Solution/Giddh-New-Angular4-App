650fd9cbc926639941dd5d0398fd3001849701f6
Arpit Meena <arpitmeena@walkover.in>
2018-03-17 17:31:27 +0530

    Arpit | APP URL set as previous

--- config/webpack.test-build.js
+++ config/webpack.test-build.js
@@ -72,8 +72,8 @@ function getUglifyOptions(supportES2015) {
 module.exports = function (env) {
   const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
   const supportES2015 = buildUtils.supportES2015(buildUtils.DEFAULT_METADATA.tsConfigPath);
-  // const AppUrl = 'http://test.giddh.com/';
-  const AppUrl = 'http://test-fs8eefokm8yjj.stackpathdns.com/';
+  const AppUrl = 'http://test.giddh.com/';
+  // const AppUrl = 'http://test-fs8eefokm8yjj.stackpathdns.com/';
   const ApiUrl = 'http://apitest.giddh.com/';
   const METADATA = Object.assign({}, buildUtils.DEFAULT_METADATA, {
     host: process.env.HOST || 'test.giddh.com',
