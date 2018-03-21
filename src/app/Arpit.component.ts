dac53a913cee545cedd3742f4c2cc15fc05323e6
Arpit Meena <arpitmeena@walkover.in>
2018-03-21 14:06:55 +0530

    Arpit | Custom redirection code added

--- src/app/Arpit.component.ts
+++ src/app/Arpit.component.ts
@@ -13,7 +13,13 @@ export class ArpitComponent {
   constructor(
     public route: ActivatedRoute,
     private router: Router) {
-    console.log('The route in ArpitComponent is :', this.route);
-    this.router.navigate(['/login']);
+    console.log('The route in ArpitComponent is :', this.route.snapshot);
+    console.log('this.route.snapshot.url.toString() is :', this.route.snapshot.url.toString());
+    if (this.route.snapshot.url.toString() === 'create') {
+      console.log('Redirected by custom code');
+      this.router.navigate(['/create']);
+    } else {
+      this.router.navigate(['/login']);
+    }
   }
 }
