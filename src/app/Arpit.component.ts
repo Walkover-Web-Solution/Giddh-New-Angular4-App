04a1ab8780a6876511739c94221410d3206ab020
Arpit Meena <arpitmeena@walkover.in>
2018-03-21 12:38:04 +0530

    Arpit | Router log added for master branch

--- src/app/Arpit.component.ts
+++ src/app/Arpit.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'Arpit',
-  styles: [`
-  `],
-  template: `
-  `
+  styles: [``],
+  template: ``
 })
 export class ArpitComponent {
 
   public localState: any;
 
-  constructor(public route: ActivatedRoute) {
+  constructor(
+    public route: ActivatedRoute,
+    private router: Router) {
     console.log('The route in ArpitComponent is :', this.route);
+    this.router.navigate(['/login']);
   }
 }
