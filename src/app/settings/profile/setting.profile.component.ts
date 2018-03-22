bda9f7132b3c8447243e757d5acd980a2ace02bf
Arpit Meena <arpitmeena@walkover.in>
2018-03-22 14:52:12 +0530

    Arpit | Google location api replaced

--- src/app/settings/profile/setting.profile.component.ts
+++ src/app/settings/profile/setting.profile.component.ts
@@ -112,7 +112,8 @@ export class SettingProfileComponent  implements OnInit, OnDestroy {
           });
         })
         .map((res) => {
-          let data = res.map(item => item.address_components[0].long_name);
+          // let data = res.map(item => item.address_components[0].long_name);
+          let data = res.map(item => item.city);
           this.dataSourceBackup = res;
           return data;
         });
@@ -162,8 +163,6 @@ export class SettingProfileComponent  implements OnInit, OnDestroy {
         } else {
           this.companyProfileObj = profileObj;
         }
-
-        console.log('profileObj is :', profileObj);
 
         if (profileObj && profileObj.country) {
           let countryName = profileObj.country.toLocaleLowerCase();
@@ -385,27 +384,28 @@ export class SettingProfileComponent  implements OnInit, OnDestroy {
 
   public typeaheadOnSelect(e: TypeaheadMatch): void {
     this.dataSourceBackup.forEach(item => {
-      if (item.address_components[0].long_name === e.item) {
+      if (item.city === e.item) {
+        this.companyProfileObj.country = item.country;
         // set country and state values
-        try {
-          item.address_components.forEach(address => {
-            let stateIdx = _.indexOf(address.types, 'administrative_area_level_1');
-            let countryIdx = _.indexOf(address.types, 'country');
-            if (stateIdx !== -1) {
-              if (this.stateResponse) {
-                let selectedState = this.stateResponse.find((state: States) => state.name === address.long_name);
-                if (selectedState) {
-                  this.companyProfileObj.state = selectedState.code;
-                }
-              }
-            }
-            if (countryIdx !== -1) {
-              this.companyProfileObj.country = address.long_name;
-            }
-          });
-        } catch (e) {
-          console.log(e);
-        }
+        // try {
+        //   item.address_components.forEach(address => {
+        //     let stateIdx = _.indexOf(address.types, 'administrative_area_level_1');
+        //     let countryIdx = _.indexOf(address.types, 'country');
+        //     if (stateIdx !== -1) {
+        //       if (this.stateResponse) {
+        //         let selectedState = this.stateResponse.find((state: States) => state.name === address.long_name);
+        //         if (selectedState) {
+        //           this.companyProfileObj.state = selectedState.code;
+        //         }
+        //       }
+        //     }
+        //     if (countryIdx !== -1) {
+        //       this.companyProfileObj.country = address.long_name;
+        //     }
+        //   });
+        // } catch (e) {
+        //   console.log(e);
+        // }
       }
     });
   }
