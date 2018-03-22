78ee730173b12454ca91347b11b42b0c81abd8ac
Arpit Meena <arpitmeena@walkover.in>
2018-03-22 12:01:34 +0530

    Arpit | outside invoice routing code cherry picked from master branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,12 +10,11 @@ import { NewUserComponent } from './newUser.component';
 import { NewUserAuthGuard } from './decorators/newUserGuard';
 import { SocialLoginCallbackComponent } from './social-login-callback.component';
 import { CheckIfPublicPath } from './decorators/checkIfPublicPath';
-import { ArpitComponent } from './Arpit.component';
+import { PublicPageHandlerComponent } from './public-page-handler.component';
 
 export const ROUTES: Routes = [
-  // { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'create', loadChildren: './create/create.module#CreateModule'},
-  { path: '', pathMatch: 'full', redirectTo: 'login', canActivate: [CheckIfPublicPath]},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'create-invoice', loadChildren: './create/create.module#CreateModule'},
   { path: 'login', loadChildren: './login/login.module#LoginModule', canActivate: [UserAuthenticated] },
   { path: 'inventory', redirectTo: 'pages/inventory', pathMatch: 'full' },
   // { path: 'success', component: SuccessComponent },
@@ -64,5 +63,6 @@ export const ROUTES: Routes = [
       { path: '**', redirectTo: 'home', pathMatch: 'full' }
     ]
   },
-  { path: '**', redirectTo: 'login', pathMatch: 'full', canActivate: [CheckIfPublicPath] }
+  // { path: '**', redirectTo: 'login', pathMatch: 'full', canActivate: [CheckIfPublicPath] },
+  { path: '**', pathMatch: 'full', component: PublicPageHandlerComponent },
 ];
