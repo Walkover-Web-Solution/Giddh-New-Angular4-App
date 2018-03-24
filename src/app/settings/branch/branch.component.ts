4f82c2856ba89d1b2f4a31cec6817a072203e160
Arpit Meena <arpitmeena@walkover.in>
2018-03-24 17:20:24 +0530

    Arpit | Branch module initila code

--- src/app/settings/branch/branch.component.ts
+++ src/app/settings/branch/branch.component.ts
@@ -1,6 +1,7 @@
+import { createSelector } from 'reselect';
 import { IOption } from 'app/theme/ng-virtual-select/sh-options.interface';
 import { Store } from '@ngrx/store';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ComponentFactoryResolver } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppState } from '../../store/roots';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
@@ -10,6 +11,10 @@ import { ToasterService } from '../../services/toaster.service';
 import { ShSelectComponent } from 'app/theme/ng-virtual-select/sh-select.component';
 import { SettingsProfileActions } from '../../actions/settings/profile/settings.profile.action';
 import { ModalDirective, BsDropdownConfig } from 'ngx-bootstrap';
+import { CompanyAddComponent } from '../../shared/header/components';
+import { ElementViewContainerRef } from '../../shared/helpers/directives/elementViewChild/element.viewchild.directive';
+import { CompanyResponse } from '../../models/api-models/Company';
+import { Observable } from 'rxjs/Observable';
 
 export const IsyncData = [
   { label: 'Debtors', value: 'debtors' },
@@ -28,14 +33,18 @@ export const IsyncData = [
 
 export class BranchComponent implements OnInit {
   @ViewChild('branchModal') public branchModal: ModalDirective;
+  @ViewChild('addCompanyModal') public addCompanyModal: ModalDirective;
+  @ViewChild('companyadd') public companyadd: ElementViewContainerRef;
 
   public dataSyncOption = IsyncData;
   public currentBranch: string = null;
+  public companies$: Observable<CompanyResponse[]>;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(
     private store: Store<AppState>,
-    private settingsProfileActions: SettingsProfileActions
+    private settingsProfileActions: SettingsProfileActions,
+    private componentFactoryResolver: ComponentFactoryResolver,
   ) {
 
     this.store.select(p => p.settings.profile).takeUntil(this.destroyed$).subscribe((o) => {
@@ -47,13 +56,54 @@ export class BranchComponent implements OnInit {
       }
     });
     console.log('branch component');
+
+    this.companies$ = this.store.select(createSelector([(state: AppState) => state.session.companies], (companies) => {
+      if (companies && companies.length) {
+        console.log('the companies are :', companies);
+        return _.orderBy(companies, 'name');
+      }
+    })).takeUntil(this.destroyed$);
   }
 
   public ngOnInit() {
     console.log('branch component');
   }
 
+  public openCreateCompanyModal() {
+    this.loadAddCompanyComponent();
+    this.addCompanyModal.show();
+  }
+
+  public hideAddCompanyModal() {
+    this.addCompanyModal.hide();
+  }
+
+  public hideCompanyModalAndShowAddAndManage() {
+    this.addCompanyModal.hide();
+  }
+
+  public loadAddCompanyComponent() {
+    let componentFactory = this.componentFactoryResolver.resolveComponentFactory(CompanyAddComponent);
+    let viewContainerRef = this.companyadd.viewContainerRef;
+    viewContainerRef.clear();
+    let componentRef = viewContainerRef.createComponent(componentFactory);
+    (componentRef.instance as CompanyAddComponent).closeCompanyModal.subscribe((a) => {
+      this.hideAddCompanyModal();
+    });
+    (componentRef.instance as CompanyAddComponent).closeCompanyModalAndShowAddManege.subscribe((a) => {
+      this.hideCompanyModalAndShowAddAndManage();
+    });
+  }
+
   public openAddBranchModal() {
     this.branchModal.show();
+  }
+
+  public onHide() {
+    console.log('creat company modal is closed.');
+  }
+
+  public hideAddBranchModal() {
+    this.branchModal.hide();
   }
 }
