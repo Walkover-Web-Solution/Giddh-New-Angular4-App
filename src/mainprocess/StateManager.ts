a06c8bd2e74738234fc6d5aeae0fb3bdf204212d
Govinda Alwani <govi2010@gmail.com>
2018-03-28 11:28:56 +0530

    Comite

---
+++ src/mainprocess/StateManager.ts
@@ -0,0 +1,78 @@
+'use strict';
+
+import * as os from 'os';
+import * as path from 'path';
+import * as ConfigStore from 'configstore';
+import { isDev } from './util';
+import * as url from 'url';
+
+export const DEFAULT_URL = url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+});
+
+function defaultWindows() {
+    return [
+        {
+            url: DEFAULT_URL,
+            width: 800,
+            height: 600,
+            webPreferences: {
+                plugins: true,
+                webSecurity: false,
+                nodeIntegration: false
+            }
+        }
+    ];
+}
+
+export class StateManager {
+    private store = new ConfigStore('Gidhh-unofficial', { windows: defaultWindows() });
+
+    private data: Config;
+
+    constructor() {
+        if (os.platform() === 'darwin') {
+            this.store.path = path.join(os.homedir(), 'Library', 'Preferences', 'org.walkover.giddh' + (isDev() ? '-dev' : '') + '.json');
+        }
+    }
+
+    public restoreWindows(): void {
+        let data = this.getOrLoadData();
+        data.windows = defaultWindows();
+        this.store.all = data;
+    }
+
+    public getWindows(): WindowItem[] {
+        return this.getOrLoadData().windows;
+    }
+
+    public save(): void {
+        const data = this.data;
+        if (data != null) {
+            this.store.all = data;
+        }
+    }
+    private getOrLoadData(): Config {
+        let data = this.data;
+        if (data == null) {
+            data = this.store.all;
+            this.data = data;
+        }
+        return data;
+    }
+}
+
+interface Config {
+    windows: WindowItem[];
+}
+
+export interface WindowItem {
+    url: string;
+    width?: number;
+    height?: number;
+    x?: number;
+    y?: number;
+    maximized?: boolean;
+}
