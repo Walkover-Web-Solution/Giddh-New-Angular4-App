9c428fcd50048aa57da146c471c05b2d6a535559
Govinda Alwani <govi2010@gmail.com>
2018-03-28 11:28:56 +0530

    Comite

--- config/webpack.renderer.prod.js
+++ config/webpack.renderer.prod.js
@@ -11,7 +11,7 @@ const webpackMerge = require('webpack-merge');
 /**
  * The settings that are common to prod and dev
 */
-const commonConfig = require('./webpack.common.js');
+const commonConfig = require('./webpack.renderer.js');
 
 /**
  * Webpack Plugins
@@ -84,7 +84,8 @@ module.exports = function (env) {
     errlyticsNeeded: false,
     errlyticsKey: ERRLYTICS_KEY_PROD,
     AppUrl: AppUrl,
-    ApiUrl: ApiUrl
+    ApiUrl: ApiUrl,
+    APP_FOLDER:''
 
   });
 
@@ -213,6 +214,7 @@ module.exports = function (env) {
           'errlyticsKey': ERRLYTICS_KEY_PROD,
           'AppUrl': JSON.stringify(METADATA.AppUrl),
           'ApiUrl': JSON.stringify(METADATA.ApiUrl),
+          'APP_FOLDER':JSON.stringify(METADATA.APP_FOLDER),
           'process.env': {
             'ENV': JSON.stringify(METADATA.ENV),
             'NODE_ENV': JSON.stringify(METADATA.ENV),
@@ -255,6 +257,5 @@ module.exports = function (env) {
     //   clearImmediate: false,
     //   setImmediate: false
     // }
-
   });
 }
