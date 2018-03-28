d2a808cd4fcef0c8c3e6f95a0ac1c0aa8f3a7f13
Arpit Meena <arpitmeena@walkover.in>
2018-03-28 15:55:12 +0530

    Arpit | Ledger > open add and manage and close then refresh data issue SOLVED #606

--- src/app/ledger/ledger.component.ts
+++ src/app/ledger/ledger.component.ts
@@ -453,6 +453,11 @@ export class LedgerComponent implements OnInit, OnDestroy {
         this.hideEledgerWrap();
       }
     });
+
+    this.store.select(createSelector([(state: AppState) => state.general.addAndManageClosed], (yesOrNo: boolean) => {
+      this.getTransactionData();
+    })).subscribe();
+
   }
 
   public initTrxRequest(accountUnq: string) {
