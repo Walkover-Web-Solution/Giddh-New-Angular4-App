e3a02fe6df65ee5a68b91a626b3c9798379eebf1
Arpit Meena <arpitmeena@walkover.in>
2018-04-04 13:38:01 +0530

    Arpit | Tag module done in Setting and Ledger

---
+++ src/app/models/api-models/settingsTags.ts
@@ -0,0 +1,15 @@
+/**
+ * Model for Tag
+ */
+
+export class TagRequest {
+  public name: string;
+  public description: string;
+  public uniqueName?: string;
+
+  constructor() {
+    this.name = '';
+    this.description = '';
+    this.uniqueName = '';
+  }
+}
