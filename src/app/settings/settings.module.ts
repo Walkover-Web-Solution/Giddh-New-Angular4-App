f27dcc70d26cee774670293e7e7abbe4e51684a7
Arpit Meena <arpitmeena@walkover.in>
2018-04-04 13:38:01 +0530

    Arpit | Tag module done in Setting and Ledger

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -27,7 +27,7 @@ import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 import { BranchComponent } from './branch/branch.component';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { ElementViewChildModule } from '../shared/helpers/directives/elementViewChild/elementViewChild.module';
-import { TagsComponent } from './tags/tags.component';
+import { SettingsTagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +45,7 @@ import { TagsComponent } from './tags/tags.component';
     SettingPermissionComponent,
     SettingPermissionFormComponent,
     BranchComponent,
-    TagsComponent
+    SettingsTagsComponent
   ],
   imports: [
     CommonModule,
