e40f0250f9d14ca9e4f52ad8afda1a8130cc0b21
Arpit Meena <arpitmeena@walkover.in>
2018-04-06 11:55:20 +0530

    +Arpit | Reconciliation functionality done in ledger

--- src/app/ledger/ledger.component.ts
+++ src/app/ledger/ledger.component.ts
@@ -109,6 +109,7 @@ export class LedgerComponent implements OnInit, OnDestroy {
   public eDrBalAmnt: number;
   public eCrBalAmnt: number;
   public advanceSearchRequest: any;
+  public isBankOrCashAccount: boolean;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(private store: Store<AppState>, private _ledgerActions: LedgerActions, private route: ActivatedRoute,
@@ -447,8 +448,18 @@ export class LedgerComponent implements OnInit, OnDestroy {
 
     // get A/c details
     this.lc.activeAccount$.subscribe((data: AccountResponse) => {
-      if (data && data.yodleeAdded) {
-        this.getBankTransactions();
+      if (data) {
+        if (data.yodleeAdded) {
+          this.getBankTransactions();
+        }
+        if (data.parentGroups && data.parentGroups.length) {
+          let findCashOrBankIndx = data.parentGroups.findIndex((grp) => grp.uniqueName === 'bankaccounts' || grp.uniqueName === 'cash');
+          if (findCashOrBankIndx !== -1) {
+            this.isBankOrCashAccount = true;
+          } else {
+            this.isBankOrCashAccount = false;
+          }
+        }
       } else {
         this.hideEledgerWrap();
       }
@@ -819,5 +830,15 @@ export class LedgerComponent implements OnInit, OnDestroy {
     this.advanceSearchRequest = _.cloneDeep(advanceSearchRequest);
     this.advanceSearchModel.hide();
     this.advanceSearchComp.ngOnDestroy();
+  }
+
+  public getReconciliation() {
+    let dataToSend = {
+      reconcileDate: null,
+      closingBalance: 0,
+      ClosingBalanceType: null,
+      accountUniqueName: this.lc.accountUnq
+    };
+    this.store.dispatch(this._ledgerActions.GetReconciliation(dataToSend));
   }
 }
