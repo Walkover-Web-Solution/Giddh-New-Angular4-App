18abdffe793714eec846b7357f2ac6513377e133
iMac <mustafa@walkover.in>
2018-04-09 11:07:21 +0530

    mustafa | implement tax in inventory-stock

--- src/app/models/api-models/Company.ts
+++ src/app/models/api-models/Company.ts
@@ -142,7 +142,7 @@ export class TaxResponse implements ITax {
   public uniqueName: string;
   public date?: any;
   public taxValue?: any;
-  public isChecked?: any;
+  public isChecked?: boolean;
 }
 
 export class States {
