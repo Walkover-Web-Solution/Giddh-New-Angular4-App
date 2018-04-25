b8787f5b7135e7381df0ac1acbf0bbbd19d081a8
Arpit Meena <arpitmeena@walkover.in>
2018-04-25 15:27:28 +0530

    Arpit | Contact module create account midway 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, trigger, state, style, animate, transition } from '@angular/core';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
@@ -13,10 +13,11 @@ import { cloneDeep } from '../lodash-optimized';
 import { CompanyActions } from '../actions/company.actions';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { IOption } from 'app/theme/ng-virtual-select/sh-options.interface';
+import { DashboardService } from '../services/dashboard.service';
 
 const CustomerType = [
-  {label: 'Customer', value: 'customer'},
-  {label: 'Vendor', value: 'vendor'}
+  { label: 'Customer', value: 'customer' },
+  { label: 'Vendor', value: 'vendor' }
 ];
 
 @Component({
@@ -26,23 +27,95 @@ const CustomerType = [
   .dropdown-menu>li>a{
     padding: 2px 10px;
   }
-  `]
+  `],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 
 export class ContactComponent implements OnInit, OnDestroy {
   public CustomerType = CustomerType;
+  public flattenAccounts: any = [];
+  public sundryDebtorsAccountsBackup: any[] = [];
+  public sundryDebtorsAccounts$: Observable<any>;
+  public sundryCreditorsAccountsBackup: any[] = [];
+  public sundryCreditorsAccounts$: Observable<any>;
+  public activeTab: string = 'customer';
+  public accountAsideMenuState: string = 'out';
+  public asideMenuStateForProductService: string = 'out';
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-  constructor(private store: Store<AppState>, private _toasty: ToasterService, private router: Router) {
+  constructor(
+    private store: Store<AppState>,
+    private _toasty: ToasterService,
+    private router: Router,
+    private _dashboardService: DashboardService) {
 
   }
 
   public ngOnInit() {
-    //
+    this.getAccounts('sundrydebtors');
+  }
+
+  public setActiveTab(tabName: 'customer' | 'vendor') {
+    this.activeTab = tabName;
+  }
+
+  public search(ev: any) {
+    let searchStr =  ev.target.value;
+    if (this.activeTab === 'customer') {
+      this.sundryDebtorsAccounts$ = Observable.of(this.sundryDebtorsAccountsBackup.filter((acc) => acc.name.includes(searchStr)));
+    } else {
+      this.sundryCreditorsAccounts$ = Observable.of(this.sundryCreditorsAccountsBackup.filter((acc) => acc.name.includes(searchStr)));
+    }
   }
 
   public ngOnDestroy() {
     this.destroyed$.next(true);
     this.destroyed$.complete();
+  }
+
+  public openAddAndManage(openFor: 'customer' | 'vendor') {
+    //
+  }
+
+  public toggleAccountAsidePane(event?): void {
+    if (event) {
+      event.preventDefault();
+    }
+    this.accountAsideMenuState = this.accountAsideMenuState === 'out' ? 'in' : 'out';
+    this.toggleBodyClass();
+  }
+
+  public toggleBodyClass() {
+    if (this.asideMenuStateForProductService === 'in' || this.accountAsideMenuState === 'in') {
+      document.querySelector('body').classList.add('fixed');
+    }else {
+      document.querySelector('body').classList.remove('fixed');
+    }
+  }
+
+  private getAccounts(groupUniqueName: string) {
+    this._dashboardService.GetClosingBalance(groupUniqueName, '', '', false).takeUntil(this.destroyed$).subscribe(response => {
+      if (response.status === 'success') {
+        if (groupUniqueName === 'sundrydebtors') {
+          this.sundryDebtorsAccountsBackup = response.body[0].accounts;
+          this.sundryDebtorsAccounts$ = Observable.of(response.body[0].accounts);
+          this.getAccounts('sundrycreditors');
+        } else {
+          this.sundryCreditorsAccountsBackup = response.body[0].accounts;
+          this.sundryCreditorsAccounts$ = Observable.of(response.body[0].accounts);
+        }
+      }
+    });
   }
 }
