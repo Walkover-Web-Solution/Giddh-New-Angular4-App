e4c306cb703c13fb053094dea5e0068d1384b4e8
mustafa <mbagwala52@gmail.com>
2018-04-27 23:51:57 +0530

    mustafa | cherry-pick commi

--- src/app/actions/settings/settings.integration.action.ts
+++ src/app/actions/settings/settings.integration.action.ts
@@ -9,7 +9,7 @@ import { BaseResponse } from '../../models/api-models/BaseResponse';
 import { Router } from '@angular/router';
 import { SETTINGS_INTEGRATION_ACTIONS } from './settings.integration.const';
 import { SettingsIntegrationService } from '../../services/settings.integraion.service';
-import { SmsKeyClass, EmailKeyClass, RazorPayClass, RazorPayDetailsResponse } from '../../models/api-models/SettingsIntegraion';
+import { SmsKeyClass, EmailKeyClass, RazorPayClass, RazorPayDetailsResponse, CashfreeClass } from '../../models/api-models/SettingsIntegraion';
 import { CustomActions } from '../../store/customActions';
 
 @Injectable()
@@ -111,6 +111,45 @@ export class SettingsIntegrationActions {
         payload: res
       }));
 
+  @Effect()
+  public SaveCashfreeDetails$: Observable<Action> = this.action$
+    .ofType(SETTINGS_INTEGRATION_ACTIONS.SAVE_CASHFREE_DETAILS)
+    .switchMap((action: CustomActions) => this.settingsIntegrationService.SaveCashFreeDetail(action.payload))
+    .map(response => this.SaveCashfreeDetailsResponse(response));
+
+  @Effect()
+  public SaveCashfreeDetailsResponse$: Observable<Action> = this.action$
+    .ofType(SETTINGS_INTEGRATION_ACTIONS.SAVE_CASHFREE_DETAILS_RESPONSE)
+    .map((response: CustomActions) => {
+      let data: BaseResponse<any, any> = response.payload;
+      if (data.status === 'error') {
+        this.toasty.clearAllToaster();
+        this.toasty.errorToast(data.message, data.code);
+      } else {
+        this.toasty.successToast('CashFree Account has been successfully saved');
+      }
+      return { type: 'EmptyAction' };
+    });
+
+  @Effect()
+  public DeleteCashfreeDetails$: Observable<Action> = this.action$
+    .ofType(SETTINGS_INTEGRATION_ACTIONS.DELETE_CASHFREE_DETAILS)
+    .switchMap((action: CustomActions) => this.settingsIntegrationService.DeleteCashFreeDetail())
+    .map(response => this.DeleteCashfreeDetailsResponse(response));
+
+  @Effect()
+  public DeleteCashfreeDetailsResponse$: Observable<Action> = this.action$
+    .ofType(SETTINGS_INTEGRATION_ACTIONS.DELETE_CASHFREE_DETAILS_RESPONSE)
+    .map((response: CustomActions) => {
+      let data: BaseResponse<string, string> = response.payload;
+      if (data.status === 'error') {
+        this.toasty.errorToast(data.message, data.code);
+      } else {
+        this.toasty.successToast(data.body, '');
+      }
+      return { type: 'EmptyAction' };
+    });
+
   constructor(private action$: Actions,
     private toasty: ToasterService,
     private router: Router,
@@ -167,6 +206,74 @@ export class SettingsIntegrationActions {
     return {
       type: SETTINGS_INTEGRATION_ACTIONS.UPDATE_RAZOR_PAY_DETAILS,
       payload: value
+    };
+  }
+
+  public SaveCashfreeDetails(value: CashfreeClass): CustomActions {
+    return {
+      type: SETTINGS_INTEGRATION_ACTIONS.SAVE_RAZOR_PAY_DETAILS,
+      payload: value
+    };
+  }
+
+  public SaveCashfreeDetailsResponse(res): CustomActions {
+    return {
+      type: SETTINGS_INTEGRATION_ACTIONS.SAVE_CASHFREE_DETAILS_RESPONSE,
+      payload: res
+    };
+  }
+
+  public DeleteCashfreeDetails(): CustomActions {
+    return {
+      type: SETTINGS_INTEGRATION_ACTIONS.DELETE_CASHFREE_DETAILS,
+    };
+  }
+
+  public DeleteCashfreeDetailsResponse(res): CustomActions {
+    return {
+      type: SETTINGS_INTEGRATION_ACTIONS.DELETE_CASHFREE_DETAILS_RESPONSE,
+      payload: res
+    };
+  }
+
+  public AddAutoCollectUser(value: CashfreeClass): CustomActions {
+    return {
+      type: SETTINGS_INTEGRATION_ACTIONS.ADD_AUTOCOLLECT_USER,
+      payload: value
+    };
+  }
+
+  public AddAutoCollectUserResponse(res): CustomActions {
+    return {
+      type: SETTINGS_INTEGRATION_ACTIONS.ADD_AUTOCOLLECT_USER_RESPONSE,
+      payload: res
+    };
+  }
+
+  public UpdateAutoCollectUser(value: CashfreeClass): CustomActions {
+    return {
+      type: SETTINGS_INTEGRATION_ACTIONS.UPDATE_AUTOCOLLECT_USER,
+      payload: value
+    };
+  }
+
+  public UpdateAutoCollectUserResponse(res): CustomActions {
+    return {
+      type: SETTINGS_INTEGRATION_ACTIONS.UPDATE_AUTOCOLLECT_USER_RESPONSE,
+      payload: res
+    };
+  }
+
+  public DeleteAutoCollectUser(): CustomActions {
+    return {
+      type: SETTINGS_INTEGRATION_ACTIONS.DELETE_AUTOCOLLECT_USER,
+    };
+  }
+
+  public DeleteAutoCollectUserResponse(res): CustomActions {
+    return {
+      type: SETTINGS_INTEGRATION_ACTIONS.DELETE_AUTOCOLLECT_USER_RESPONSE,
+      payload: res
     };
   }
 
