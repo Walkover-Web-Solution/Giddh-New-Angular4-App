abc4395bb7b7d6f1abb96a0fe0b6fc8b56f7f0c7
Govinda Alwani <govi2010@gmail.com>
2018-04-28 15:26:15 +0530

    Vishal | inventory entry,report and user actions

--- src/app/models/api-models/Inventory.ts
+++ src/app/models/api-models/Inventory.ts
@@ -204,7 +204,6 @@ interface IGroupStockReport {
   stockName: string;
 }
 
-
 export interface Stock {
   uniqueName: string;
 }
@@ -241,4 +240,36 @@ export interface InventoryFilter {
   senders?: any[];
   includeReceivers?: string;
   receivers?: any[];
+}
+
+export interface InventoryReportTransactions {
+  uniqueName: string;
+  date: string;
+  quantity: number;
+  description: string;
+  stockUnit: {
+    name: string;
+    code: string;
+  };
+  sender: INameUniqueName;
+  receiver: INameUniqueName;
+  closingQuantity: number;
+  stock: INameUniqueName;
+}
+
+export interface InventoryReport {
+  page: number;
+  count: number;
+  totalPages: number;
+  totalItems: number;
+  fromDate: string;
+  toDate: string;
+  stock: INameUniqueName;
+  stockUnit: {
+    name: string;
+    code: string;
+  };
+  openingQuantity: number;
+  closingQuantity: number;
+  transactions: InventoryReportTransactions[];
 }
