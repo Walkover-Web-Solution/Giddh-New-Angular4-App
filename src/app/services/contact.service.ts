6020c08301058f269cf4a23872430060c92d036e
Arpit Meena <arpitmeena@walkover.in>
2018-05-03 13:29:38 +0530

    Arpit | Contact module done

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -29,9 +29,10 @@ export class ContactService {
     }).catch((e) => this.errorHandler.HandleCatch<any, any>(e, body, ''));
   }
 
-  public GetContacts(): Observable<BaseResponse<any, string>> {
+  public GetContacts(groupUniqueName: string, pageNumber: number): Observable<BaseResponse<any, string>> {
     this.companyUniqueName = this._generalService.companyUniqueName;
-    return this._http.get(this.config.apiUrl + 'v2/company/:companyUniqueName/groups/customer-vendor-report'.replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName))).map((res) => {
+    return this._http.get(this.config.apiUrl + 'v2/company/:companyUniqueName/groups/:groupUniqueName/account-balances?page=:page&count=10'.replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName)).replace(':groupUniqueName', encodeURIComponent(groupUniqueName)).replace(':page', pageNumber.toString())).map((res) => {
+      // return this._http.get(this.config.apiUrl + 'v2/company/:companyUniqueName/groups/customer-vendor-report'.replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName))).map((res) => {
       let data: BaseResponse<any, string> = res;
       data.request = '';
       return data;
