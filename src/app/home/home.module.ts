679be7473d917d7ba5b0441b2091b4ba987a3547
iMac <mustafa@walkover.in>
2018-05-04 10:18:51 +0530

    mustafa | midway dashboard new design

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -14,6 +14,7 @@ import { ChartModule } from 'angular2-highcharts';
 import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 import { LaddaModule } from 'angular2-ladda';
 import { PerfectScrollbarModule, PERFECT_SCROLLBAR_CONFIG, PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import { RatioAnalysisChartComponent } from './components/ratio-analysis/ratio-analysis-chart.component';
 
 export function highchartsFactory() {
   const hc = require('highcharts');
@@ -37,7 +38,8 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     RevenueChartComponent,
     ComparisionChartComponent,
     HistoryChartComponent,
-    NetworthChartComponent
+    NetworthChartComponent,
+    RatioAnalysisChartComponent
   ],
   exports: [HomeComponent],
   providers: [
