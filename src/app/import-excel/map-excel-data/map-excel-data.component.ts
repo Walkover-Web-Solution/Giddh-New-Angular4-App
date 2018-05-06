8e5ed24b2ab77ccafa45100b2e068ce9b6e136b5
govinda <govi2010@gmail.com>
2018-05-06 11:14:46 +0530

    Faizal | Import-Excel Module -> Mapping Step Complete

--- src/app/import-excel/map-excel-data/map-excel-data.component.ts
+++ src/app/import-excel/map-excel-data/map-excel-data.component.ts
@@ -2,6 +2,12 @@ import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Outpu
 import { ImportExcelData, MapHeader } from '../../models/api-models/import-excel';
 import { IOption } from '../../theme/ng-select/option.interface';
 
+interface DataModel {
+  field: string;
+  options: IOption[];
+  selected: string;
+}
+
 @Component({
   selector: 'map-excel-data',  // <home></home>
   styleUrls: ['./map-excel-data.component.scss'],
@@ -9,9 +15,19 @@ import { IOption } from '../../theme/ng-select/option.interface';
 })
 
 export class MapExcelDataComponent implements OnInit, OnDestroy, AfterViewInit {
-  @Input() public importData: ImportExcelData;
   @Output() public onNext = new EventEmitter();
   @Output() public onBack = new EventEmitter();
+  public dataModel: DataModel[];
+  private _importData: ImportExcelData;
+  public get importData(): ImportExcelData {
+    return this._importData;
+  }
+
+  @Input()
+  public set importData(value: ImportExcelData) {
+    this.prepareDataModel(value);
+    this._importData = value;
+  }
 
   constructor() {
     //
@@ -29,11 +45,19 @@ export class MapExcelDataComponent implements OnInit, OnDestroy, AfterViewInit {
     //
   }
 
-  public getMappings() {
-    return Object.keys(this.importData.mappings.mappingInfo);
+  public columnSelected(val: IOption, data: DataModel) {
+    console.log(val);
+    const mapping: MapHeader[] = this._importData.mappings.mappingInfo[data.field];
+    mapping.find(p => p.columnNumber === +val.value).isSelected = true;
   }
 
-  public getOptionsFromMap(maps: MapHeader[]): IOption[] {
-    return maps.map(p => ({value: p.columnHeader, label: p.columnHeader}));
+  private prepareDataModel(value: ImportExcelData) {
+    this.dataModel = Object.keys(value.mappings.mappingInfo)
+      .map(field => ({
+        field,
+        options: value.mappings.mappingInfo[field]
+          .map(p => ({value: p.columnNumber, label: p.columnHeader})),
+        selected: value.mappings.mappingInfo[field].find(p => p.isSelected)
+      }));
   }
 }
