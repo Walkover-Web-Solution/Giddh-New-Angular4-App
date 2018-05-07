72ad5daa628a338ce41d693f0bad9e5bc10087e2
Arpit Meena <arpitmeena@walkover.in>
2018-05-07 14:52:33 +0530

    Arpit | Mac Electron App quit issue fixed

--- src/mainprocess/WindowManager.ts
+++ src/mainprocess/WindowManager.ts
@@ -33,7 +33,8 @@ export default class WindowManager {
             // to stay active until the user quits explicitly with Cmd + Q
             if (process.platform === 'darwin') {
                 // reopen initial window
-                this.openWindows();
+                // this.openWindows();
+                app.quit();
             } else {
                 app.quit();
             }
