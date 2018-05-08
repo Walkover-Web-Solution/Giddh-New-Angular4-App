a4e9864a8b2f5482aa056198ff5ad584937306a4
Arpit Meena <arpitmeena@walkover.in>
2018-05-08 16:09:21 +0530

    Arpit | Invoice template edit and preview functionality midway

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -31,7 +31,7 @@ export class ContactService {
 
   public GetContacts(groupUniqueName: string, pageNumber: number): Observable<BaseResponse<any, string>> {
     this.companyUniqueName = this._generalService.companyUniqueName;
-    return this._http.get(this.config.apiUrl + 'v2/company/:companyUniqueName/groups/:groupUniqueName/account-balances?page=:page&count=10'.replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName)).replace(':groupUniqueName', encodeURIComponent(groupUniqueName)).replace(':page', pageNumber.toString())).map((res) => {
+    return this._http.get(this.config.apiUrl + 'v2/company/:companyUniqueName/groups/:groupUniqueName/account-balances?page=:page&count=15'.replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName)).replace(':groupUniqueName', encodeURIComponent(groupUniqueName)).replace(':page', pageNumber.toString())).map((res) => {
       // return this._http.get(this.config.apiUrl + 'v2/company/:companyUniqueName/groups/customer-vendor-report'.replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName))).map((res) => {
       let data: BaseResponse<any, string> = res;
       data.request = '';
