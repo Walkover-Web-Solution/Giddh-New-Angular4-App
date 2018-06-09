d161c990ae3f90e9950893e6c0af91777c62c038
Govinda Alwani <govi2010@gmail.com>
2018-06-09 19:25:37 +0530

    Vishal | warehouese transafer destination

---
+++ src/app/inventory/components/warehouse/warehouseProducts/warehouse.products.component.ts
@@ -0,0 +1,21 @@
+import { AppState } from '../store/roots';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'warehouse-products',
+  templateUrl: './warehouse.products.component.html'
+})
+export class WarehouseProductsComponent implements OnInit, OnDestroy {
+
+  constructor() {
+    //
+  }
+
+  public ngOnInit() {
+    //
+  }
+
+  public ngOnDestroy() {
+    //
+  }
+}
