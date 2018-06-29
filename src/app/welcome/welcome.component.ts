5d270484667f79a9b44613df19667ca95e8785bc
govi2010 <govi2010@gmail.com>
2018-06-29 18:38:47 +0530

    new create component design

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'welcome-component',
+  templateUrl: './welcome.component.html'
+})
+
+export class WelcomeComponent implements OnInit {
+  constructor() {
+    //
+  }
+
+  public ngOnInit() {
+    //
+  }
+}
