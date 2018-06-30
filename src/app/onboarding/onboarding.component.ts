4950e4280456796f8a89f42a6b8ac8186be0d542
Nisha Gupta <nishagupta@Nishas-iMac.local>
2018-06-30 18:36:10 +0530

    onboarding screen html

--- src/app/onboarding/onboarding.component.ts
+++ src/app/onboarding/onboarding.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'onboarding-component',
-  templateUrl: './onboarding.component.html'
+  templateUrl: './onboarding.component.html',
+  styleUrls:['./onboarding.component.css']
+  
 })
 
 export class OnboardingComponent implements OnInit {
