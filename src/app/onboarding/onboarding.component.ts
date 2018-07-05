990ed7217d15d154001559de807a270834d33db2
iMac <mustafa@walkover.in>
2018-07-05 17:03:47 +0530

    mustafa | superform & schedule modal added

--- src/app/onboarding/onboarding.component.ts
+++ src/app/onboarding/onboarding.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { WindowRef } from '../shared/helpers/window.object';
+import { ModalDirective, TabsetComponent } from 'ngx-bootstrap';
 
 @Component({
   selector: 'onboarding-component',
@@ -10,12 +11,19 @@ import { WindowRef } from '../shared/helpers/window.object';
 })
 
 export class OnboardingComponent implements OnInit {
+  @ViewChild('talkSalesModal') public talkSalesModal: ModalDirective;
+  @ViewChild('supportTab') public supportTab: TabsetComponent;
+  public loadAPI: Promise<any>;
   constructor(private _router: Router, private _window: WindowRef) {
-    //
+      this._window.nativeWindow.superformIds = ['Jkvq'];
   }
 
   public ngOnInit() {
     //
+  this.loadAPI = new Promise((resolve) => {
+      this.loadScript();
+      resolve(true);
+  });
   }
 
   public goTo(path: string) {
@@ -30,6 +38,37 @@ export class OnboardingComponent implements OnInit {
       return false;
   }
 
+  public openScheduleModal() {
+    this.talkSalesModal.show();
+  }
+
+public loadScript() {
+    let isFound = false;
+    let scripts = document.getElementsByTagName('script');
+    for (let i = 0; i < scripts.length; ++i) {
+        if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes('loader')) {
+            isFound = true;
+        }
+    }
+
+    if (!isFound) {
+        let dynamicScripts = ['https://random-scripts.herokuapp.com/superform/superform.js'];
+
+        for (let i = 0; i < dynamicScripts .length; i++) {
+            let node = document.createElement('script');
+            node.src = dynamicScripts [i];
+            node.type = 'text/javascript';
+            node.async = false;
+            node.charset = 'utf-8';
+            document.getElementsByTagName('head')[0].appendChild(node);
+        }
+
+    }
+}
+
+public closeModal() {
+  this.talkSalesModal.hide();
+}
   // public downloadPlugin() {
   //   window.location = 'https://s3.ap-south-1.amazonaws.com/giddhbuildartifacts/Walkover+Prod.tcp';
   // }
