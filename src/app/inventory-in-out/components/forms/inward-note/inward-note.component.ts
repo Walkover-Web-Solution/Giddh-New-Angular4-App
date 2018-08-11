156d54d11fac75aa9077deed69465600a52b72c6
govi2010 <govi2010@gmail.com>
2018-08-11 13:03:57 +0530

    structure changes

--- src/app/inventory-in-out/components/forms/inward-note/inward-note.component.ts
+++ src/app/inventory-in-out/components/forms/inward-note/inward-note.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
 import { InventoryEntry, InventoryUser } from '../../../../models/api-models/Inventory-in-out';
 
 import { IStocksItem } from '../../../../models/interfaces/stocksItem.interface';
 import { IOption } from '../../../../theme/ng-virtual-select/sh-options.interface';
 import * as moment from 'moment';
+import { Observable } from 'rxjs/Observable';
+import { StockUnitRequest } from '../../../../models/api-models/Inventory';
 
 @Component({
   selector: 'inward-note',
@@ -20,42 +22,70 @@ export class InwardNoteComponent implements OnInit, OnChanges {
   @Output() public onSave = new EventEmitter<InventoryEntry>();
 
   @Input() public stockList: IStocksItem[];
+  @Input() public stockUnits: StockUnitRequest[];
   @Input() public userList: InventoryUser[];
 
   @Input() public isLoading: boolean;
   public stockListOptions: IOption[];
+  public stockUnitsOptions: IOption[];
   public userListOptions: IOption[];
   public form: FormGroup;
-  public config: Partial<BsDatepickerConfig> = { dateInputFormat: 'DD-MM-YYYY' };
+  public config: Partial<BsDatepickerConfig> = {dateInputFormat: 'DD-MM-YYYY'};
   public mode: 'sender' | 'product' = 'sender';
   public today = new Date();
-  public get inventoryEntryDate(): FormControl { debugger; return this.form.get('inventoryEntryDate') as FormControl; }
-  public get transactions(): FormArray { return this.form.get('transactions') as FormArray; }
+
+  public get inventoryEntryDate(): FormControl {
+    return this.form.get('inventoryEntryDate') as FormControl;
+  }
+
+  public get transactions(): FormArray {
+    return this.form.get('transactions') as FormArray;
+  }
 
   constructor(private _fb: FormBuilder) {
-    this.form = this._fb.group({
-      inventoryEntryDate: [moment().format('DD-MM-YYYY'), Validators.required],
-      transactions: this._fb.array([], Validators.required),
-      description: ['']
-    });
-    this.addTransactionItem();
+    this.initializeForm(true);
   }
 
   public ngOnInit() {
     //
   }
 
-  public ngOnChanges(changes: SimpleChanges): void {
-    if (changes.stockList && this.stockList) {
-      this.stockListOptions = this.stockList.map(p => ({ label: p.name, value: p.uniqueName }));
-    }
-    if (changes.userList && this.userList) {
-      this.userListOptions = this.userList.map(p => ({ label: p.name, value: p.uniqueName }));
+  public initializeForm(initialRequest: boolean = false) {
+    this.form = this._fb.group({
+      inventoryEntryDate: [moment().format('DD-MM-YYYY'), Validators.required],
+      transactions: this._fb.array([], Validators.required),
+      description: ['']
+    });
+    if (initialRequest) {
+      this.addTransactionItem();
     }
   }
 
-  public addTransactionItem() {
-    const items = this.form.get('transactions') as FormArray;
+  public modeChanged(mode: 'sender' | 'product') {
+    this.mode = mode;
+    this.inventoryEntryDate.reset();
+    this.initializeForm();
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.stockList && this.stockList) {
+      this.stockListOptions = this.stockList.map(p => ({label: p.name, value: p.uniqueName}));
+    }
+    if (changes.stockUnits && this.stockUnits) {
+      this.stockUnitsOptions = this.stockUnits.map(p => ({label: `${p.name} (${p.code})`, value: p.code}));
+    }
+    if (changes.userList && this.userList) {
+      this.userListOptions = this.userList.map(p => ({label: p.name, value: p.uniqueName}));
+    }
+  }
+
+  public addTransactionItem(control?: AbstractControl) {
+
+    if (control && control.invalid) {
+      return;
+    }
+
+    const items = this.transactions;
     const value = items.length > 0 ? items.at(0).value : {
       type: '',
       quantity: '',
@@ -68,10 +98,8 @@ export class InwardNoteComponent implements OnInit, OnChanges {
       type: ['SENDER', Validators.required],
       quantity: ['', Validators.required],
       inventoryUser: [value.inventoryUser, Validators.required],
-
-
       stock: [value.stock, Validators.required],
-      stockUnit: [value.stockUnit, Validators.required]
+      stockUnit: [this.mode === 'sender' ? value.stockUnit : '', Validators.required]
     });
     items.push(transaction);
   }
@@ -84,7 +112,7 @@ export class InwardNoteComponent implements OnInit, OnChanges {
   public userChanged(option: IOption, index: number) {
     const items = this.form.get('transactions') as FormArray;
     const user = this.userList.find(p => p.uniqueName === option.value);
-    const inventoryUser = user ? { uniqueName: user.uniqueName } : null;
+    const inventoryUser = user ? {uniqueName: user.uniqueName} : null;
 
     if (index >= 0) {
       const control = items.at(index);
@@ -94,28 +122,28 @@ export class InwardNoteComponent implements OnInit, OnChanges {
 
       });
     } else {
-      items.controls.forEach(c => c.patchValue({ ...c.value, inventoryUser }));
+      items.controls.forEach(c => c.patchValue({...c.value, inventoryUser}));
 
     }
   }
 
   public stockChanged(option: IOption, index: number) {
-    const items = this.form.get('transactions') as FormArray;
+    const items = this.transactions;
     const stockItem = this.stockList.find(p => p.uniqueName === option.value);
-    const stock = stockItem ? { uniqueName: stockItem.uniqueName } : null;
-    const stockUnit = stockItem ? { code: stockItem.stockUnit.code } : null;
+    const stock = stockItem ? {uniqueName: stockItem.uniqueName} : null;
+    const stockUnit = stockItem ? stockItem.stockUnit.code : null;
     if (index >= 0) {
       const control = items.at(index);
-      control.patchValue({ ...control.value, stock, stockUnit });
+      control.patchValue({...control.value, stock, stockUnit});
     } else {
-      items.controls.forEach(c => c.patchValue({ ...c.value, stock, stockUnit }));
+      items.controls.forEach(c => c.patchValue({...c.value, stock, stockUnit}));
     }
   }
 
   public save() {
     if (this.form.valid) {
       const inventoryEntryDate = moment(this.form.value.transferDate).format('DD-MM-YYYY');
-      this.onSave.emit({ ...this.form.value, inventoryEntryDate });
+      this.onSave.emit({...this.form.value, inventoryEntryDate});
     }
   }
 }
