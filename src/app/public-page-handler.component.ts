b03768faee695e57ed58b3c3478e407157ac586c
Arpit Meena <arpitmeena@walkover.in>
2018-08-21 10:52:40 +0530

    Arpit | 404 logic updated

--- src/app/public-page-handler.component.ts
+++ src/app/public-page-handler.component.ts
@@ -13,6 +13,10 @@ export class PublicPageHandlerComponent {
   constructor(
     public route: ActivatedRoute,
     private router: Router) {
+    console.log('the.route is :', route);
+    console.log('the.router is :', router);
+    console.log('this.router.routerState.snapshot.url is :', this.router.routerState.snapshot.url);
+
     if (this.router.routerState.snapshot.url.includes('create-invoice')) {
       this.router.navigate(['/create-invoice']);
     } else if (this.router.routerState.snapshot.url.includes('signup')) {
@@ -23,8 +27,9 @@ export class PublicPageHandlerComponent {
       url = url.replace('/app', '');
       this.router.navigateByUrl(url);
     } else {
-      // this.router.navigate(['login']);
-      this.router.navigateByUrl('404');
+      console.log('last else');
+      this.router.navigate(['login']);
+      // this.router.navigateByUrl('404');
     }
   }
 }
