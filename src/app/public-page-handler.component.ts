fb80f5f3a69d148350449fe1ab89c38423004e7e
Arpit Meena <arpitmeena@walkover.in>
2018-08-21 11:57:42 +0530

    Arpit | 404 logic updated

--- src/app/public-page-handler.component.ts
+++ src/app/public-page-handler.component.ts
@@ -20,7 +20,6 @@ export class PublicPageHandlerComponent {
     if (this.router.routerState.snapshot.url.includes('create-invoice')) {
       this.router.navigate(['/create-invoice']);
     } else if (this.router.routerState.snapshot.url.includes('signup')) {
-      // this.router.navigate(['signup']);
       this.router.navigateByUrl('signup');
     } else if (this.router.routerState.snapshot.url.includes('app/pages/settings')) {
       let url = this.router.routerState.snapshot.url;
@@ -29,13 +28,23 @@ export class PublicPageHandlerComponent {
     } else {
       console.log('the last else');
       let url = this.router.routerState.snapshot.url;
-      url = url.replace('/app', '');
+      url = url.replace('/app/', '');
+      console.log('the formatted url is :', url);
+      let existingRouteIndx = this.router.config.findIndex((r) => r.redirectTo === url);
+      console.log('existingRouteIndx is :', existingRouteIndx);
+      if (existingRouteIndx > -1) {
+        console.log('YES');
+        this.router.navigateByUrl(url);
+      } else {
+        console.log('NO');
+        this.router.navigateByUrl('404');
+      }
 
-      this.router.navigateByUrl(url).then((res) => {
-        console.log('the res then success is :', res);
-      }).catch((err) => {
-        console.log('the err from catch is :', err);
-      });
+      // this.router.navigateByUrl(url).then((res) => {
+      //   console.log('the res then success is :', res);
+      // }).catch((err) => {
+      //   console.log('the err from catch is :', err);
+      // });
 
       // console.log('last else');
       // this.router.navigate(['login']);
