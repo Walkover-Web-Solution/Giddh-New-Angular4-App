6cce637c2e95cd6d1babddb0eb82625b24021d3c
govinda <govi2010@gmail.com>
2018-08-24 19:17:00 +0530

    carriedsales

--- src/app/services/apiurls/carried-over-sales.api.ts
+++ src/app/services/apiurls/carried-over-sales.api.ts
@@ -1,4 +1,4 @@
-const COMMON_URL_FOR_CARRIED_OVER_SALE = 'company/c1/new-old-sales?q=:q&type=:type&value=:value';
+const COMMON_URL_FOR_CARRIED_OVER_SALE = 'company/c1/new-old-sales?type=:type&value=:value';
 
 export const CARRIEDOVERSALES_API = {
   GET: COMMON_URL_FOR_CARRIED_OVER_SALE
