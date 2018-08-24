f91400f094f21bf901ca0a6c6c30a0e9d081a83e
iMac <mustafa@walkover.in>
2018-08-24 13:59:51 +0530

    mustafa | midway template design

--- src/app/invoice/templates/edit-template/out-tempate/templates/gst-template-e/gst-template-e.component.ts
+++ src/app/invoice/templates/edit-template/out-tempate/templates/gst-template-e/gst-template-e.component.ts
@@ -59,9 +59,9 @@ export class GstTemplateEComponent implements OnInit, OnDestroy, OnChanges {
         if (changes.fieldsAndVisibility.currentValue.table.sNo && changes.fieldsAndVisibility.currentValue.table.sNo.display) {
           this.columnsVisibled++;
         }
-        if (changes.fieldsAndVisibility.currentValue.table.date && changes.fieldsAndVisibility.currentValue.table.date.display) {
-          this.columnsVisibled++;
-        }
+        // if (changes.fieldsAndVisibility.currentValue.table.date && changes.fieldsAndVisibility.currentValue.table.date.display) {
+        //   this.columnsVisibled++;
+        // }
         if (changes.fieldsAndVisibility.currentValue.table.item && changes.fieldsAndVisibility.currentValue.table.item.display) {
           this.columnsVisibled++;
         }
@@ -77,9 +77,9 @@ export class GstTemplateEComponent implements OnInit, OnDestroy, OnChanges {
         if (changes.fieldsAndVisibility.currentValue.table.discount && changes.fieldsAndVisibility.currentValue.table.discount.display) {
           this.columnsVisibled++;
         }
-        if (changes.fieldsAndVisibility.currentValue.table.taxableValue && changes.fieldsAndVisibility.currentValue.table.taxableValue.display) {
-          this.columnsVisibled++;
-        }
+        // if (changes.fieldsAndVisibility.currentValue.table.taxableValue && changes.fieldsAndVisibility.currentValue.table.taxableValue.display) {
+        //   this.columnsVisibled++;
+        // }
         if (changes.fieldsAndVisibility.currentValue.table.taxes && changes.fieldsAndVisibility.currentValue.table.taxes.display) {
           this.columnsVisibled++;
         }
