af2defcfa8542c45c24d9acdaf9d3463b0959569
govinda <govi2010@gmail.com>
2018-08-27 11:55:39 +0530

    carried over sales changes

--- src/app/store/CarriedOverSales/carried-over-sales.reducer.ts
+++ src/app/store/CarriedOverSales/carried-over-sales.reducer.ts
@@ -1,18 +1,19 @@
 import { CarriedOverSalesResponse } from '../../models/api-models/carried-over-sales';
 import { CustomActions } from '../customActions';
 import { CarriedOverSalesActions } from '../../actions/carried-over-sales.actions';
-import { DaybookActions } from '../../actions/daybook/daybook.actions';
 
 export interface CarriedOverSalesState {
   data?: CarriedOverSalesResponse;
   showLoader: boolean;
   noData: boolean;
+  requestInSuccess: boolean;
 }
 
 export const initialState: CarriedOverSalesState = {
   data: null,
   noData: true,
-  showLoader: false
+  showLoader: false,
+  requestInSuccess: false
 };
 
 export function carriedOverSalesReduce(state = initialState, action: CustomActions) {
@@ -22,16 +23,16 @@ export function carriedOverSalesReduce(state = initialState, action: CustomActio
         let data: CarriedOverSalesResponse = _.cloneDeep(action.payload) as CarriedOverSalesResponse;
         let noData = false;
         let showLoader = false;
-        if (data && data.totalSales.length < 1 && data.newSales.length < 1 && data.carriedSales.length < 1) {
+        if (data && data.totalSales && data.newSales && data.carriedSales.length < 1) {
           noData = true;
         }
-        return {...state, data, noData, showLoader};
+        return {...state, data, noData, showLoader, requestInSuccess: true};
       } else {
-        return {...state, showLoader: false, data: null, noData: true};
+        return {...state, showLoader: false, data: null, noData: true, requestInSuccess: true};
       }
     }
     case CarriedOverSalesActions.GET_CARRIED_SALES_REQUEST: {
-      return {...state, showLoader: true};
+      return {...state, showLoader: true, requestInSuccess: false};
     }
     default: {
       return state;
