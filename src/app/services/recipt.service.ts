fb1e76a85178397fcbea7b5f8c409bfdda77b1ea
govinda <govi2010@gmail.com>
2018-08-29 19:23:23 +0530

    create Recipt services

--- src/app/services/recipt.service.ts
+++ src/app/services/recipt.service.ts
@@ -1,13 +1,12 @@
 import { Injectable, OnInit } from '@angular/core';
 import { GeneralService } from './general.service';
-import { ReciptRequest } from '../models/api-models/recipt';
+import { ReciptDeleteRequest, ReciptRequest, ReciptRequestParams, ReciptResponse } from '../models/api-models/recipt';
 import { Observable } from 'rxjs/Observable';
 import { BaseResponse } from '../models/api-models/BaseResponse';
 import { HttpWrapperService } from './httpWrapper.service';
 import { HttpClient } from '@angular/common/http';
 import { IServiceConfigArgs } from './service.config';
 import { RECIPT_API } from './apiurls/recipt.api';
-import { GenerateInvoiceRequestClass } from '../models/api-models/Invoice';
 import { ErrorHandler } from './catchManager/catchmanger';
 
 @Injectable()
@@ -23,20 +22,47 @@ export class ReciptService implements OnInit {
     //
   }
 
-  // public UpdateRecipt(accountUniqueName: string, model: ReciptRequest): Observable<BaseResponse<string, ReciptRequest>> {
-  //   this.companyUniqueName = this._generalService.companyUniqueName;
-  //   return this._http(this.config.apiUrl + RECIPT_API.PUT.replace(':companyUniqueName', this.companyUniqueName).replace(':accountUniqueName', accountUniqueName), model)
-  //     .map((res) => {
-  //       let data: BaseResponse<string, ReciptRequest> = res;
-  //       data.request = model;
-  //       data.queryString = {accountUniqueName};
-  //       return data;
-  //     })
-  //     .catch((e) => this.errorHandler.HandleCatch<string, GenerateInvoiceRequestClass>(e, model));
-  // }
+  public UpdateRecipt(accountUniqueName: string, model: ReciptRequest): Observable<BaseResponse<string, ReciptRequest>> {
+    this.companyUniqueName = this._generalService.companyUniqueName;
+    return this._http.put(this.config.apiUrl + RECIPT_API.PUT
+      .replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName))
+      .replace(':accountUniqueName', encodeURIComponent(accountUniqueName)), model)
+      .map((res) => {
+        let data: BaseResponse<string, ReciptRequest> = res;
+        data.request = model;
+        data.queryString = {accountUniqueName};
+        return data;
+      })
+      .catch((e) => this.errorHandler.HandleCatch<string, ReciptRequest>(e, model));
+  }
 
-  public GetAllRecipt() {
-  //
+  public GetAllRecipt(queryRequest: ReciptRequestParams): Observable<BaseResponse<ReciptResponse, string>> {
+    this.companyUniqueName = this._generalService.companyUniqueName;
+    return this._http.get(this.config.apiUrl + RECIPT_API.GET
+      .replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName))
+      .replace(':page', queryRequest.page.toString())
+      .replace(':count', queryRequest.count.toString())
+      .replace(':from', queryRequest.from.toString())
+      .replace(':to', queryRequest.to.toString())
+      .replace(':type', queryRequest.type.toString()))
+      .map((res) => {
+        let data: BaseResponse<ReciptResponse, string> = res;
+        data.queryString = queryRequest;
+        return data;
+      })
+      .catch((e) => this.errorHandler.HandleCatch<ReciptResponse, string>(e, null, queryRequest));
+  }
+
+  public DeleteRecipt(accountUniqueName: string, querRequest: ReciptDeleteRequest): Observable<BaseResponse<string, ReciptDeleteRequest>> {
+    this.companyUniqueName = this._generalService.companyUniqueName;
+    return this._http.delete(this.config.apiUrl + RECIPT_API.DELETE
+      .replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName))
+      .replace(':accountUniqueName', encodeURIComponent(accountUniqueName)))
+      .map((res) => {
+        let data: BaseResponse<any, any> = res;
+        data.request = querRequest;
+        return data;
+      }).catch((e) => this.errorHandler.HandleCatch<any, any>(e, accountUniqueName));
   }
 
 }
