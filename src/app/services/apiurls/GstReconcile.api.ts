6984b301dbaf67a6491d4482af1c6c9051d8cf4d
govi2010 <govi2010@gmail.com>
2018-09-01 12:57:57 +0530

    gst reconcile ui fixes

--- src/app/services/apiurls/GstReconcile.api.ts
+++ src/app/services/apiurls/GstReconcile.api.ts
@@ -1,7 +1,7 @@
 const COMMON_URL_FOR_GST_RECONCILE = 'company/:companyUniqueName/gstr-reconcile';
 
 export const GST_RECONCILE_API = {
-  GET: COMMON_URL_FOR_GST_RECONCILE + '/invoices?period=:period',
-  GET_USERNAME: COMMON_URL_FOR_GST_RECONCILE + '/auth?userName=:userName',
-  POST: COMMON_URL_FOR_GST_RECONCILE + '/verify-otp'
+  GET_INVOICES: COMMON_URL_FOR_GST_RECONCILE + '/invoices?period=:period',
+  GENERATE_OTP: COMMON_URL_FOR_GST_RECONCILE + '/auth?userName=:userName',
+  VERIFY_OTP: COMMON_URL_FOR_GST_RECONCILE + '/verify-otp'
 };
