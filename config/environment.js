a2fbbd48505f6328129c0e628d1f58a3d6be8fba
Govinda Alwnai <govi2010@live.in>
2018-09-06 15:52:26 +0530

    prod and dev commands with local and test

--- config/environment.js
+++ config/environment.js
@@ -1,27 +1,27 @@
 const local = {
-  errlyticsNeeded: 'false',
-  errlyticsKey: '""',
-  isElectron:'false',
-  AppUrl: '"http://localapp.giddh.com:3000/"',
-  ApiUrl: '"http://apitest.giddh.com/"',
-  APP_FOLDER: '""'
+  errlyticsNeeded: JSON.stringify(false),
+  errlyticsKey: '',
+  isElectron: JSON.stringify(false),
+  AppUrl: 'http://localapp.giddh.com:3000/',
+  ApiUrl: 'http://apitest.giddh.com/',
+  APP_FOLDER: ''
 };
 const test = {
   errlyticsNeeded: 'false',
   errlyticsKey: '""',
-  isElectron:'false',
-  AppUrl: '"http://test.giddh.com/"',
-  ApiUrl: '"http://apitest.giddh.com/"',
-  APP_FOLDER: '"app/"'
+  isElectron: 'false',
+  AppUrl: 'http://test.giddh.com/',
+  ApiUrl: 'http://apitest.giddh.com/',
+  APP_FOLDER: 'app/'
 };
 const prod = {
   errlyticsNeeded: 'true',
-  errlyticsKey: '"eTrTpSiedQC4tLUYVDup3RJpc_wFL2QhCaIc0vzpsQA"',
-  isElectron:'false',
-  AppUrl: '"https://giddh.com/"',
-  ApiUrl: '"https://api.giddh.com/"',
-  APP_FOLDER: '"app/"'
+  errlyticsKey: JSON.stringify('eTrTpSiedQC4tLUYVDup3RJpc_wFL2QhCaIc0vzpsQA'),
+  isElectron: 'false',
+  AppUrl: 'https://giddh.com/"',
+  ApiUrl: 'https://api.giddh.com/',
+  APP_FOLDER: 'app/'
 };
 
 
-module.exports = { local, test, prod };
+module.exports = {local, test, prod};
