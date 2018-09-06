49a42860e72b04b060c50dc857fc45ee15870037
govinda <govi2010@gmail.com>
2018-09-06 17:52:21 +0530

    handling gst auth error

--- src/app/store/GstReconcile/GstReconcile.reducer.ts
+++ src/app/store/GstReconcile/GstReconcile.reducer.ts
@@ -1,5 +1,7 @@
 import { CustomActions } from '../customActions';
 import { GST_RECONCILE_ACTIONS } from '../../actions/gst-reconcile/GstReconcile.const';
+import { BaseResponse } from '../../models/api-models/BaseResponse';
+import { GstReconcileInvoiceDetails, GstReconcileInvoiceResponse } from '../../models/api-models/GstReconcile';
 
 export interface GstReconcileState {
   isGenerateOtpInProcess: boolean;
@@ -8,7 +10,29 @@ export interface GstReconcileState {
   isGstReconcileInvoiceSuccess: boolean;
   isGstReconcileVerifyOtpInProcess: boolean;
   isGstReconcileVerifyOtpSuccess: boolean;
+  gstAuthenticated: boolean;
+  gstReconcileData: GstReconcileDataState;
 }
+
+class ReconcileActionState {
+  public count: number;
+  public data: GstReconcileInvoiceDetails;
+}
+
+interface GstReconcileDataState {
+  notFoundOnGiddh: ReconcileActionState;
+  notFoundOnPortal: ReconcileActionState;
+  matched: ReconcileActionState;
+  partiallyMatched: ReconcileActionState;
+}
+
+const gstReconcileDataInitialState: GstReconcileDataState = {
+  notFoundOnGiddh: new ReconcileActionState(),
+  matched: new ReconcileActionState(),
+  notFoundOnPortal: new ReconcileActionState(),
+  partiallyMatched: new ReconcileActionState(),
+
+};
 
 const initialState: GstReconcileState = {
   isGenerateOtpInProcess: false,
@@ -17,6 +41,8 @@ const initialState: GstReconcileState = {
   isGstReconcileInvoiceSuccess: false,
   isGstReconcileVerifyOtpInProcess: false,
   isGstReconcileVerifyOtpSuccess: false,
+  gstAuthenticated: true,
+  gstReconcileData: gstReconcileDataInitialState
 };
 
 export function GstReconcileReducer(state: GstReconcileState = initialState, action: CustomActions): GstReconcileState {
@@ -50,19 +76,59 @@ export function GstReconcileReducer(state: GstReconcileState = initialState, act
         isGstReconcileVerifyOtpSuccess: action.payload.status === 'success'
       };
 
-    case GST_RECONCILE_ACTIONS.GST_RECONCILE_INVOICE_PERIOD_REQUEST:
+    case GST_RECONCILE_ACTIONS.GST_RECONCILE_INVOICE_REQUEST:
       return {
         ...state,
         isGstReconcileInvoiceInProcess: true,
         isGstReconcileInvoiceSuccess: false
       };
 
-    case GST_RECONCILE_ACTIONS.GST_RECONCILE_INVOICE_PERIOD_RESPONSE:
-      return {
-        ...state,
-        isGstReconcileInvoiceInProcess: false,
-        isGstReconcileInvoiceSuccess: action.payload.status === 'success'
-      };
+    case GST_RECONCILE_ACTIONS.GST_RECONCILE_INVOICE_RESPONSE:
+      let response: BaseResponse<GstReconcileInvoiceResponse, string> = action.payload;
+      let newState = _.cloneDeep(state);
+      newState.isGenerateOtpInProcess = false;
+      if (response.status === 'success') {
+        newState.isGstReconcileInvoiceSuccess = true;
+        newState.gstAuthenticated = true;
+        let gstData = newState.gstReconcileData;
+        switch (response.queryString.action) {
+          case 'NOT_ON_GIDDH':
+            gstData.notFoundOnGiddh = {
+              count: response.body.notFoundOnGiddh,
+              data: response.body.details
+            };
+          case 'NOT_ON_PORTAL':
+            gstData.notFoundOnPortal = {
+              count: response.body.notFoundOnPortal,
+              data: response.body.details
+            };
+          case 'MATCHED':
+            gstData.matched = {
+              count: response.body.matched,
+              data: response.body.details
+            };
+          case 'PARTIALLY_MATCHED':
+            gstData.partiallyMatched = {
+              count: response.body.partiallyMatched,
+              data: response.body.details
+            };
+        }
+      } else {
+        if (response.code === 'GST_AUTH_ERROR') {
+          newState.isGstReconcileInvoiceSuccess = false;
+          newState.gstAuthenticated = false;
+          //  false
+        } else {
+          newState.isGstReconcileInvoiceSuccess = true;
+          newState.gstAuthenticated = true;
+          // true
+        }
+      }
+      return newState;
+    // {
+    //     // isGstReconcileInvoiceInProcess: false,
+    //     // isGstReconcileInvoiceSuccess: action.payload.status === 'success'
+    //   };
     default:
       return state;
   }
