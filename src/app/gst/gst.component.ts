345eb71a2bfdc1a118ce10b97d18e1ef5305469e
Arpit Meena <arpitmeena@walkover.in>
2018-09-06 15:51:55 +0530

    Arpit | New GST module > Blank page added

---
+++ src/app/gst/gst.component.ts
@@ -0,0 +1,27 @@
+/**
+ * Created by kunalsaxena on 9/1/17.
+ */
+import { Component, OnInit } from '@angular/core';
+import { CompanyActions } from '../actions/company.actions';
+import { AppState } from '../store/roots';
+import { Store } from '@ngrx/store';
+import { StateDetailsRequest } from '../models/api-models/Company';
+
+@Component({
+  templateUrl: './gst.component.html'
+})
+export class GstComponent implements OnInit {
+  constructor(private store: Store<AppState>, private _companyActions: CompanyActions) {
+    //
+  }
+  public ngOnInit(): void {
+    let companyUniqueName = null;
+    this.store.select(c => c.session.companyUniqueName).take(1).subscribe(s => companyUniqueName = s);
+    let stateDetailsRequest = new StateDetailsRequest();
+    stateDetailsRequest.companyUniqueName = companyUniqueName;
+    stateDetailsRequest.lastState = 'gst';
+
+    this.store.dispatch(this._companyActions.SetStateDetails(stateDetailsRequest));
+  }
+
+}
