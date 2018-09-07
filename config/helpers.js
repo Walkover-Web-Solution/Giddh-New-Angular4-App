dac8eea9d0dd6fb8b8e7fd7d675b1591e81b113f
Govinda Alwani <govi2010@gmail.com>
2018-09-07 05:56:43 -0700

    remove un nessasory files

--- config/helpers.js
+++ config/helpers.js
@@ -11,7 +11,7 @@ const EVENT = process.env.npm_lifecycle_event || '';
 var ROOT = path.resolve(__dirname, '..');
 
 function hasProcessFlag(flag) {
-  console.log('Govinda Flag :', flag)
+  // console.log('Govinda Flag :', flag)
   return process.argv.join('').indexOf(flag) > -1;
 }
 
