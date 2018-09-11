882a0529946cc6e90d8d24d3c15d6ec8d2e56208
Arpit Meena <arpitmeena@walkover.in>
2018-09-11 12:36:51 +0530

    Arpit | New GST blank page added for SYNRAM

---
+++ src/app/gst/gst-page-c/gst-page-c.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+declare var jquery: any;
+declare var $: any;
+
+@Component({
+  selector: 'gst-page-c',
+  templateUrl: './gst-page-c.component.html',
+  styleUrls: ['gst-page-c.component.css'],
+})
+export class GstPageCComponent {
+  constructor() {
+    //
+  }
+
+  public ngOnInit() {
+
+	}
+
+}
