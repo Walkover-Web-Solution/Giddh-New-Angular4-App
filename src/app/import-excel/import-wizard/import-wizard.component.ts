e4a3da2ea781ec82664d39314799dc04d226cd34
govinda <govi2010@gmail.com>
2018-09-12 12:48:22 +0530

    bug fix on import-wizard

--- src/app/import-excel/import-wizard/import-wizard.component.ts
+++ src/app/import-excel/import-wizard/import-wizard.component.ts
@@ -27,7 +27,6 @@ export class ImportWizardComponent implements OnInit, OnDestroy, AfterViewInit {
   public mappedData: ImportExcelResponseData;
   public dataModel: DataModel[];
   public dataChanged = (excelState: ImportExcelState) => {
-    debugger;
     this.excelState = excelState;
     if (excelState.requestState === ImportExcelRequestStates.UploadFileSuccess) {
       this.step++;
