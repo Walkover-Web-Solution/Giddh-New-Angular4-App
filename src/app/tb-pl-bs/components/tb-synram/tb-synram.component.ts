e4e69f226326f4c67dc1d4ca439b6a0d07a88722
Arpit Meena <arpitmeena@walkover.in>
2018-09-19 10:14:20 +0530

    Arpit | New trial balance page added for design

---
+++ src/app/tb-pl-bs/components/tb-synram/tb-synram.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tb-synram',
+  templateUrl: './tb-synram.component.html',
+  styleUrls: ['./tb-synram.component.css']
+})
+export class TbSynramComponent {
+  //
+}
