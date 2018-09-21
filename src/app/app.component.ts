216e8e8efe20dec608e2f10dc580034d7135e35b
Govinda Alwnai <govi2010@live.in>
2018-09-21 12:01:06 +0530

    Adding Loader to application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from './store/roots';
 import { GeneralService } from './services/general.service';
 import { pick } from './lodash-optimized';
+import { BehaviorSubject } from 'rxjs';
 
 /**
  * App Component
@@ -23,10 +24,12 @@ import { pick } from './lodash-optimized';
     <noscript>
       <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K2L9QG" height="0" width="0" style="display:none;visibility:hidden"></iframe>
     </noscript>
+    <div id="loader-1" *ngIf="!(IAmLoaded | async)" class="giddh-spinner vertical-center-spinner"></div>
     <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements AfterViewInit {
+  public IAmLoaded: BehaviorSubject<boolean>;
 
   constructor(private store: Store<AppState>, private router: Router, private activatedRoute: ActivatedRoute, private _generalService: GeneralService) {
     this.store.select(s => s.session).subscribe(ss => {
@@ -43,6 +46,7 @@ export class AppComponent implements AfterViewInit {
       }
       this._generalService.companyUniqueName = ss.companyUniqueName;
     });
+    this.IAmLoaded = this._generalService.IAmLoaded;
   }
 
   public ngAfterViewInit() {
