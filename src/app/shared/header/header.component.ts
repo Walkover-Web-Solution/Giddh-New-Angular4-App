9427944c8a45d8d4a9aac61accb1fc906beb7775
Arpit Meena <arpitmeena@walkover.in>
2018-09-29 12:26:41 +0530

    Arpit | PR number 1271 code replaced again

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -65,7 +65,7 @@ export const NAVIGATION_ITEM_LIST: IUlist[] = [
   { type: 'MENU', name: 'Onboarding', uniqueName: '/onboarding' },
   { type: 'MENU', name: 'Purchase Invoice ', uniqueName: '/pages/purchase/create' },
   { type: 'MENU', name: 'Company Import/Export', uniqueName: '/pages/company-import-export' },
-  { type: 'MENU', name: 'New V/S Old Invoices', uniqueName: '/pages/carriedoversales' },
+  { type: 'MENU', name: 'New V/S Old Invoices', uniqueName: '/pages/new-vs-old-invoices' },
   { type: 'MENU', name: 'GST Module', uniqueName: '/pages/gst/gst' },
   { type: 'MENU', name: 'GST Module Page 2', uniqueName: '/pages/gst/gst-page-b' },
   { type: 'MENU', name: 'GST Module Page 3', uniqueName: '/pages/gst/gst-page-c' },
