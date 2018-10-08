4d8234b49e12472557ae782fd5929aea2d46ec8c
Arpit Meena <arpitmeena@walkover.in>
2018-10-08 15:40:07 +0530

    Arpit | JV fixes

--- src/app/accounting/keyboard.directive.ts
+++ src/app/accounting/keyboard.directive.ts
@@ -10,23 +10,12 @@ export class OnReturnDirective {
   private activeIndx: number = null;
   private fieldToActivate: any = null;
   private selectedField;
+  private isOtherKeyPressed: boolean = false;
 
   constructor(private _el: ElementRef) {
     this.el = this._el;
   }
 
-  // @HostListener('keydown', ['$event']) public onKeyDown(e) {
-  //     if ((e.which === 13 || e.keyCode === 13)) {
-  //         console.log('e.srcElement.nextElementSibling is :', e.srcElement.nextElementSibling);
-  //         e.preventDefault();
-  //         if (e.srcElement.nextElementSibling) {
-  //             e.srcElement.nextElementSibling.focus();
-  //         } else {
-  //             console.log('close keyboard');
-  //         }
-  //         return;
-  //     }
-  // }
   @HostListener('keydown', ['$event'])
   public onKeyDown(e: any) {
 
@@ -60,48 +49,31 @@ export class OnReturnDirective {
           return setTimeout(() => {
             target.focus();
           }, 0);
+        } else if (allElements[indx] && allElements[indx].classList.contains('select-stock-in-invoice')) {
+          if (this.activeIndx === indx) {
+            target = allElements[indx + 1];
+            if (target.disabled) {
+              target = allElements[indx + 4];
+            }
+            this.activeIndx = null;
+            return target.focus();
+          } else {
+            return this.activeIndx = indx;
+          }
+        } else if (allElements[indx] && allElements[indx].classList.contains('invoice-account-field')) {
+          if (this.activeIndx === indx) {
+            target = allElements[indx + 1];
+            if (target.disabled) {
+              return document.getElementById('invoice-narration').focus();
+            }
+            this.activeIndx = null;
+            return target.focus();
+          } else {
+            return this.activeIndx = indx;
+          }
         }
 
-        // let attrArray = [];
-        // for (let i = 0, atts = target.attributes, n = atts.length, arr = []; i < n; i++) {
-        //   attrArray.push(atts[i].nodeName);
-        // }
-        // console.log('attrArray is :', attrArray);
-        // if (attrArray.indexOf('onreturn') > -1) {
-        //   target.focus();
-        // } else {
-        //   alert('ele without directive');
-        // }
-
         if (target) {
-          // if (this.activeIndx === indx) {
-          //   this.clickCount++;
-          //   if (this.clickCount > 1) {
-          //     const activatedRow: any = window.document.querySelectorAll('tr.activeRow');
-          //     const rowEntryType =  activatedRow[0].children[0].children[0].value;
-          //     let fieldToActivate;
-          //     if (rowEntryType === 'by') {
-          //       fieldToActivate = activatedRow[0].children[2].children[0];
-          //     } else {
-          //       fieldToActivate = activatedRow[0].children[3].children[0];
-          //     }
-          //     if (this.fieldToActivate === fieldToActivate) {
-          //       target.focus();
-          //     } else {
-          //       if (fieldToActivate.disabled) {
-          //         const disabledFieldIndx = nodeList.findIndex((ele) => ele === fieldToActivate);
-          //         allElements[disabledFieldIndx + 1].focus();
-          //       }
-          //       fieldToActivate.focus();
-          //       this.clickCount = 0;
-          //       this.fieldToActivate = fieldToActivate;
-          //     }
-          //     // console.log('lastChild is :', activatedRow[0].lastChild());
-          //   }
-          // } else {
-          //   this.activeIndx = indx;
-          //   this.clickCount = 0;
-          // }
 
           if (target.disabled) {
            target = allElements[indx + 2];
@@ -123,26 +95,8 @@ export class OnReturnDirective {
             if (allElements[indx] && allElements[indx].classList.contains('from-or-to-acc')) {
               this.clickCount++;
             }
-            // if (allElements[indx] && allElements[indx].classList.contains('select-stock-in-invoice')) {
-            //   console.log('Yesssss...');
-            //   console.log('this.clickCount is :', this.clickCount);
-            //   if (this.clickCount < 1) {
-            //     this.clickCount = 0;
-            //     target = allElements[indx + 4];
-            //   } else {
-            //     if (allElements[indx + 1].disabled) {
-            //       console.log('yes it is disabled');
-            //       this.clickCount++;
-            //     }
-            //   }
-            // }
             target.focus();
           }
-
-          // if (target.disabled) {
-          //   const disabledFieldIndx = nodeList.findIndex((ele) => ele === target);
-          //   allElements[disabledFieldIndx + 1].focus();
-          // }
         }
 
       } else if (e.which === 8 || e.keyCode === 8) {
@@ -169,7 +123,7 @@ export class OnReturnDirective {
             target.focus();
           }, 0);
         }
-
+        // && !this.isOtherKeyPressed && this.selectedField !== target
         if (target && e.target.value.length === e.target.selectionEnd) {
           e.preventDefault();
           if (target.disabled) {
@@ -178,44 +132,24 @@ export class OnReturnDirective {
             target.focus();
           }
         }
-
-        // if (selectedEle.value === '') {
-        // e.preventDefault();
-        // if (target) {
-        //   target.focus();
-        // }
-        // }
       } else if (e.which === 32 || e.keyCode === 32) {
         const target = allElements[indx];
         if (target) {
           target.value = '';
         }
       }
-
-      // console.log('currentEle is :', nextEle);
-      // alert(indx);
-
-      // console.log('event is :', e);
-      // e.preventDefault();
-      // let control: any;
-      // control = e.srcElement.nextSibling;
-      // while (true) {
-      //   if (control) {
-      //     if ((!control.hidden) &&
-      //       (control.nodeName === 'INPUT' ||
-      //         control.nodeName === 'SELECT' ||
-      //         control.nodeName === 'BUTTON' ||
-      //         control.nodeName === 'TEXTAREA')) {
-      //       control.focus();
-      //       return;
-      //     } else {
-      //       control = control.nextElementSibling;
-      //     }
-      //   } else {
-      //     console.log('close keyboard', window.document.querySelectorAll('input'));
-      //     return;
-      //   }
-      // }
     }
+    // else {
+    //   const selectedEle = e.target;
+    //   const allElements: any = window.document.querySelectorAll('input[onReturn][type="text"]');
+    //   const nodeList = Array.from(allElements);
+    //   const indx = nodeList.findIndex((ele) => ele === selectedEle);
+    //   if (this.selectedField === allElements[indx]) {
+    //     this.isOtherKeyPressed = true;
+    //   } else {
+    //     this.isOtherKeyPressed = false;
+    //     this.selectedField = allElements[indx];
+    //   }
+    // }
   }
 }
