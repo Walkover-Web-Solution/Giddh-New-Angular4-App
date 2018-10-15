d9469f84e7725b61366f9d996ac306f0b312a5c7
Arpit Meena <arpitmeena@walkover.in>
2018-10-15 14:08:44 +0530

    Arpit | #1379 fixed

--- src/app/services/apiurls/settings.integration.api.ts
+++ src/app/services/apiurls/settings.integration.api.ts
@@ -10,5 +10,5 @@ export const SETTINGS_INTEGRATION_API = {
   AMAZON_SELLER: COMMON + 'amazon/seller', // GET, POST
   AMAZON_SELLER_OPERATION: COMMON + 'amazon/seller/:sellerId', // DELETE, PUT
   // GET_GMAIL_INTEGRATION_STATUS: 'users/gmail-token' // GET
-  GET_GMAIL_INTEGRATION_STATUS: 'gmail-token' // GET
+  GET_GMAIL_INTEGRATION_STATUS: COMMON + 'gmail-token' // GET
 };
