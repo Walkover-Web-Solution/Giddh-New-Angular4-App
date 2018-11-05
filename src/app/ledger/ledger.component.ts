a618a52a1c4e5084a53fb63648e428ae34a3d4c3
iMac <mustafa@walkover.in>
2018-11-05 14:59:09 +0530

    mustafa | fix recent txns in ledger

--- src/app/ledger/ledger.component.ts
+++ src/app/ledger/ledger.component.ts
@@ -547,9 +547,10 @@ export class LedgerComponent implements OnInit, OnDestroy {
     this.ledgerBulkActionSuccess$.subscribe((yes: boolean) => {
       if (yes) {
         this.entryUniqueNamesForBulkAction = [];
-        this.store.dispatch(this._ledgerActions.doAdvanceSearch(_.cloneDeep(this.advanceSearchRequest.dataToSend), this.advanceSearchRequest.accountUniqueName,
-          moment(this.advanceSearchRequest.dataToSend.bsRangeValue[0]).format('DD-MM-YYYY'), moment(this.advanceSearchRequest.dataToSend.bsRangeValue[1]).format('DD-MM-YYYY'),
-          this.advanceSearchRequest.page, this.advanceSearchRequest.count, this.advanceSearchRequest.q));
+        this.getTransactionData();
+        // this.store.dispatch(this._ledgerActions.doAdvanceSearch(_.cloneDeep(this.advanceSearchRequest.dataToSend), this.advanceSearchRequest.accountUniqueName,
+        //   moment(this.advanceSearchRequest.dataToSend.bsRangeValue[0]).format('DD-MM-YYYY'), moment(this.advanceSearchRequest.dataToSend.bsRangeValue[1]).format('DD-MM-YYYY'),
+        //   this.advanceSearchRequest.page, this.advanceSearchRequest.count, this.advanceSearchRequest.q));
       }
     });
 
