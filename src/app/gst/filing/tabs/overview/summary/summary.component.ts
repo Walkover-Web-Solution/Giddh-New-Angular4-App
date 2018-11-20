4acc1b78645beb39cffbf46deb267085f79a1a59
iMac <mustafa@walkover.in>
2018-11-20 15:29:04 +0530

    mustafa | changes & minor fixes in GSTR module

--- src/app/gst/filing/tabs/overview/summary/summary.component.ts
+++ src/app/gst/filing/tabs/overview/summary/summary.component.ts
@@ -6,6 +6,31 @@ import { Observable, ReplaySubject, of } from 'rxjs';
 import { GstRReducerState, GstOverViewResponse } from 'app/store/GstR/GstR.reducer';
 import { takeUntil } from 'rxjs/operators';
 import { Router, ActivatedRoute } from '@angular/router';
+import * as _ from 'lodash';
+import { ReconcileActionState } from 'app/store/GstReconcile/GstReconcile.reducer';
+
+export const GstR1SummarySequencing = [
+   { name: 'B2B Invoices', gstReturnType: 'b2b', index: 1 },
+   { name: 'B2C (Large) Invoices', gstReturnType: 'b2cl', index: 2 },
+   { name: 'Export Invoices', gstReturnType: 'export', index: 3 },
+   { name: 'B2C (Small) Invoices', gstReturnType: 'b2cs', index: 4 },
+   { name: 'Exempt', gstReturnType: 'nil', index: 5 },
+   { name: 'Credit / Debit Notes / Refund Vouchers', gstReturnType: 'CreditNote/DebitNote/RefundVouchers', index: 6 },
+   { name: 'Advance Payments', gstReturnType: 'advance-payments', index: 7 },
+   { name: 'Tax Paid', gstReturnType: 'taxPaid', index: 8 },
+   { name: 'HSN Summary', gstReturnType: 'hsnsac', index: 9 }
+];
+
+export const GstR2SummarySequencing = [
+   { name: 'B2B Invoices', gstReturnType: 'b2b', index: 1 },
+   { name: 'B2BUR Invoices', gstReturnType: 'b2bur', index: 2 },
+   { name: 'Credit/Debit Notes (Registered)', gstReturnType: 'cdnr', index: 3 },
+   { name: 'Credit/Debit Notes (Unregistered)', gstReturnType: 'cdnUr', index: 4 },
+   { name: 'Import of Goods / Capital Goods', gstReturnType: 'impg', index: 5 },
+   { name: 'Import of Services', gstReturnType: 'imps', index: 6 },
+   { name: 'Nil Rated Invoices', gstReturnType: 'nil', index: 7 },
+   { name: 'HSN / SAC Summary', gstReturnType: 'hsnsac', index: 8 },
+];
 
 @Component({
   selector: 'overview-summary',
@@ -23,6 +48,12 @@ export class OverviewSummaryComponent implements OnInit, OnChanges, AfterViewIni
   public companyGst$: Observable<string> = of('');
   public gstOverviewDataInProgress$: Observable<boolean>;
   public imgPath: string = '';
+  public summaryResponse: any = [];
+  public gstFoundOnGiddh$: Observable<boolean>;
+  public gstNotFoundOnGiddhData$: Observable<ReconcileActionState>;
+  public gstNotFoundOnPortalData$: Observable<ReconcileActionState>;
+  public gstMatchedData$: Observable<ReconcileActionState>;
+  public gstPartiallyMatchedData$: Observable<ReconcileActionState>;
 
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
@@ -30,7 +61,11 @@ export class OverviewSummaryComponent implements OnInit, OnChanges, AfterViewIni
     this.gstOverviewData$ = this._store.select(p => p.gstR.overViewData).pipe(takeUntil(this.destroyed$));
     this.companyGst$ = this._store.select(p => p.gstR.activeCompanyGst).pipe(takeUntil(this.destroyed$));
     this.gstOverviewDataInProgress$ = this._store.select(p => p.gstR.overViewDataInProgress).pipe(takeUntil(this.destroyed$));
-
+    this.gstFoundOnGiddh$ = this._store.select(p => p.gstReconcile.gstFoundOnGiddh).pipe(takeUntil(this.destroyed$));
+    this.gstNotFoundOnGiddhData$ = this._store.select(p => p.gstReconcile.gstReconcileData.notFoundOnGiddh).pipe(takeUntil(this.destroyed$));
+    this.gstNotFoundOnPortalData$ = this._store.select(p => p.gstReconcile.gstReconcileData.notFoundOnPortal).pipe(takeUntil(this.destroyed$));
+    this.gstMatchedData$ = this._store.select(p => p.gstReconcile.gstReconcileData.matched).pipe(takeUntil(this.destroyed$));
+    this.gstPartiallyMatchedData$ = this._store.select(p => p.gstReconcile.gstReconcileData.partiallyMatched).pipe(takeUntil(this.destroyed$));
   }
 
   public ngOnInit() {
@@ -40,27 +75,37 @@ export class OverviewSummaryComponent implements OnInit, OnChanges, AfterViewIni
         this.activeCompanyGstNumber = a;
       }
     });
+    this.gstOverviewData$.subscribe((a) => {
+      if (a && a.transactionSummary) {
+        if (this.selectedGst === 'gstr1') {
+          this.mapResponseData(a, GstR1SummarySequencing);
+        } else {
+          this.mapResponseData(a, GstR2SummarySequencing);
+        }
+      }
+    });
   }
 
   /**
    * viewTransactions
    */
   public viewTransactions(obj) {
-    if (obj && !obj.type) {
+    if (obj.gstReturnType === 'hsnsac' || obj.gstReturnType === 'CreditNote/DebitNote/RefundVouchers') {
       return;
     }
     let param = {
       page: 1,
       count: 20,
-      entity: obj.type,
+      entityType: obj.type,
       gstin: this.activeCompanyGstNumber,
       type: obj.gstReturnType,
       from: this.currentPeriod.from,
       to: this.currentPeriod.to,
-      status: ''
+      status: 'all'
     };
     this._store.dispatch(this.gstAction.GetSummaryTransaction(this.selectedGst, param));
-    this._route.navigate(['pages', 'gstfiling', 'filing-return', this.selectedGst, this.currentPeriod.from, this.currentPeriod.to , 'transaction', param.entity]);
+    this._store.dispatch(this.gstAction.RequestTransactions(param));
+    this._route.navigate(['pages', 'gstfiling', 'filing-return', this.selectedGst, this.currentPeriod.from, this.currentPeriod.to , 'transaction', param.entityType]);
   }
 
   public ngOnChanges(s: SimpleChanges) {
@@ -73,5 +118,21 @@ export class OverviewSummaryComponent implements OnInit, OnChanges, AfterViewIni
 
   public ngOnDestroy() {
     this.destroyed$.next(true);
+  }
+
+  public mapResponseData(data, sequencingList) {
+    let manipulatedData = _.cloneDeep(data);
+    _.forEach(manipulatedData.transactionSummary.results, function(obj) {
+      let selectedObj =  _.find(sequencingList, function(o) {
+        return o.gstReturnType  === obj.gstReturnType;
+      });
+
+      if (selectedObj) {
+        obj.name  = selectedObj.name;
+        obj.index = selectedObj.index;
+      }
+    });
+    manipulatedData.transactionSummary.results = _.sortBy(manipulatedData.transactionSummary.results, (o) => o.index);
+    this.gstOverviewData$ = of(manipulatedData);
   }
 }
