6716983f3a021bafa4c0517044aca4b1e7b0a3f0
rohit <rohit@walkover.in>
2018-12-27 17:26:37 +0530

    default note and imageSignature radio button implemented

--- src/app/invoice/templates/edit-template/filters-container/content-filters/content.filters.component.ts
+++ src/app/invoice/templates/edit-template/filters-container/content-filters/content.filters.component.ts
@@ -1,12 +1,18 @@
+
+import { ToasterService } from './../../../../../services/toaster.service';
 import { ActivatedRoute } from '@angular/router';
-import { take } from 'rxjs/operators';
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { take, takeUntil } from 'rxjs/operators';
+import { Component, Input, OnDestroy, OnInit, EventEmitter } from '@angular/core';
 import { InvoiceUiDataService, TemplateContentUISectionVisibility } from '../../../../../services/invoice.ui.data.service';
 import { CustomTemplateResponse } from '../../../../../models/api-models/Invoice';
 import * as _ from '../../../../../lodash-optimized';
-import { ReplaySubject } from 'rxjs';
+import { ReplaySubject, Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../../../store/roots';
+import { Configuration } from './../../../../../app.constant';
+import { UploaderOptions, UploadOutput, UploadInput, UploadFile, humanizeBytes } from 'ngx-uploader';
+import { ViewChild, ElementRef } from '@angular/core';
+import { INVOICE_API } from 'app/services/apiurls/invoice';
 
 @Component({
   selector: 'content-selector',
@@ -24,23 +30,41 @@ export class ContentFilterComponent implements OnInit, OnDestroy {
   public showCompanyName: boolean;
   public fieldsAndVisibility: any;
   public voucherType = '';
+  public formData: FormData;
+  public signatureSrc: string;
+  public fileUploadOptions: UploaderOptions;
+  public signatureImgAttached: boolean = false;
+  public uploadInput: EventEmitter<UploadInput>;
+  public files: UploadFile[] = [];
+  public humanizeBytes: any;
+  public dragOver: boolean;
+  public imagePreview: any;
+  public isFileUploaded: boolean = false;
+  public isFileUploadInProgress: boolean = false;
+  public companyUniqueName = null;
+  public sessionId$: Observable<string>;
+  public companyUniqueName$: Observable<string>;
+  @ViewChild('signatureImg') public signatureImgzRef: ElementRef<HTMLInputElement>;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
   constructor(private store: Store<AppState>, private _invoiceUiDataService: InvoiceUiDataService,
-    private _activatedRoute: ActivatedRoute
+    private _activatedRoute: ActivatedRoute, private _toasty: ToasterService
     ) {
-    let companyUniqueName = null;
     let companies = null;
     let defaultTemplate = null;
 
     this.store.select(s => s.session).pipe(take(1)).subscribe(ss => {
-      companyUniqueName = ss.companyUniqueName;
+      this.companyUniqueName = ss.companyUniqueName;
       companies = ss.companies;
     });
 
     this.store.select(s => s.invoiceTemplate).pipe(take(1)).subscribe(ss => {
       defaultTemplate = ss.defaultTemplate;
     });
-    this._invoiceUiDataService.initCustomTemplate(companyUniqueName, companies, defaultTemplate);
+    this._invoiceUiDataService.initCustomTemplate(this.companyUniqueName, companies, defaultTemplate);
+
+    this.sessionId$ = this.store.select(p => p.session.user.session.id).pipe(takeUntil(this.destroyed$));
+    this.companyUniqueName$ = this.store.select(p => p.session.companyUniqueName).pipe(takeUntil(this.destroyed$));
   }
 
   public ngOnInit() {
@@ -54,6 +78,7 @@ export class ContentFilterComponent implements OnInit, OnDestroy {
     });
     this._invoiceUiDataService.customTemplate.subscribe((template: CustomTemplateResponse) => {
       this.customTemplate = _.cloneDeep(template);
+
     });
 
     this._invoiceUiDataService.selectedSection.subscribe((info: TemplateContentUISectionVisibility) => {
@@ -67,6 +92,11 @@ export class ContentFilterComponent implements OnInit, OnDestroy {
     this._invoiceUiDataService.fieldsAndVisibility.subscribe((obj) => {
       this.fieldsAndVisibility = _.cloneDeep(obj);
     });
+
+    this.fileUploadOptions = {concurrency: 1, allowedContentTypes: ['image/png', 'image/jpeg']};
+    this.files = []; // local uploading files array
+    this.uploadInput = new EventEmitter<UploadInput>(); // input events, we use this to emit data to ngx-uploader
+    this.humanizeBytes = humanizeBytes;
   }
 
   /**
@@ -114,7 +144,6 @@ export class ContentFilterComponent implements OnInit, OnDestroy {
     //   template.sections[0].content[17].display = false;
     //   template.sections[0].content[18].display = false;
     // }
-
     this._invoiceUiDataService.setCustomTemplate(template);
   }
 
@@ -132,4 +161,113 @@ export class ContentFilterComponent implements OnInit, OnDestroy {
     // this.destroyed$.complete();
   }
 
+  public onUploadFileOutput(output: UploadOutput): void {
+    if (output.type === 'allAddedToQueue') {
+      // this.logoAttached = true;
+      this.signatureImgAttached = true;
+      this.signatureImgAttached = true;
+       this.previewFile(output.file);
+       this.startUpload();
+
+    } else if (output.type === 'start') {
+      //
+    } else if (output.type === 'done') {
+      // this.isFileUploadInProgress = false;
+      this.signatureImgAttached = true;
+      if (output.file.response.status === 'success') {
+        this.customTemplate.sections.footer.data.imageSignature.label = output.file.response.body.uniqueName;
+        this.signatureSrc = ApiUrl + 'company/' + this.companyUniqueName + '/image/' + output.file.response.body.uniqueName;
+        // this.customTemplate.sections.footer.data.imageSignature.label = this.signatureSrc;
+        this.onChangeFieldVisibility(null, null, null);
+        // this.isFileUploaded = true;
+        this._toasty.successToast('file uploaded successfully.');
+        this.startUpload();
+      } else {
+        this._toasty.errorToast(output.file.response.message, output.file.response.code);
+      }
+    }
+  }
+
+
+  public startUpload(): void {
+    let sessionId = null;
+    let companyUniqueName = null;
+     this.sessionId$.pipe(take(1)).subscribe(a => sessionId = a);
+   this.companyUniqueName$.pipe(take(1)).subscribe(a => companyUniqueName = a);
+    const event: UploadInput = {
+      type: 'uploadAll',
+      url: Configuration.ApiUrl + INVOICE_API.UPLOAD_LOGO.replace(':companyUniqueName', encodeURIComponent(companyUniqueName)),
+      method: 'POST',
+      headers: {'Session-Id': sessionId},
+    };
+
+    this.uploadInput.emit(event);
+  }
+
+  public previewFile(files: any) {
+    let preview: any = document.getElementById('signatureImage');
+    let a: any = document.querySelector('input[type=file]');
+    let file = a.files[0];
+    let reader = new FileReader();
+
+    reader.onloadend = () => {
+      preview.src = reader.result;
+      this._invoiceUiDataService.setLogoPath(preview.src);
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+      // this.logoAttached = true;
+    } else {
+      preview.src = '';
+      // this.logoAttached = false;
+      this._invoiceUiDataService.setLogoPath('');
+    }
+  }
+
+  public cancelUpload(id: string): void {
+    this.uploadInput.emit({type: 'cancel', id});
+  }
+
+  public removeFile(id: string): void {
+    this.uploadInput.emit({type: 'remove', id});
+  }
+
+  public removeAllFiles(): void {
+    this.uploadInput.emit({type: 'removeAll'});
+  }
+
+  
+
+  /**
+   * deleteLogo
+   */
+  public deleteLogo() {
+    // this.onValueChange('logoUniqueName', null);
+    this._invoiceUiDataService.setLogoPath('');
+    this.files = []; // local uploading files array
+    this.uploadInput = new EventEmitter<UploadInput>(); // input events, we use this to emit data to ngx-uploader
+    this.humanizeBytes = humanizeBytes;
+    // this.logoAttached = false;
+    this.isFileUploaded = false;
+    this.isFileUploadInProgress = false;
+    this.removeAllFiles();
+  }
+
+  /**
+   * chooseSigntureType
+   */
+  public chooseSigntureType(val) {
+    console.log('chooseSigntureType...', val);
+    if (val === 'slogan') {
+      this.customTemplate.sections.footer.data.slogan.display = true;
+      this.customTemplate.sections.footer.data.imageSignature.display = false;
+     this.signatureImgAttached = false;
+     this.signatureSrc = '';
+    this.signatureImgzRef.nativeElement.value = null;
+      } else {
+      this.customTemplate.sections.footer.data.imageSignature.display = true;
+      this.customTemplate.sections.footer.data.slogan.display = false;
+     
+    }
+  }
 }
