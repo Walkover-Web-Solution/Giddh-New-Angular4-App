74ca86ce6c4be3e04690d7d2f2aca73afa843506
sadik-walkover <sadik@walkover.in>
2018-12-29 15:29:00 +0530

    mustafa | remove existing taxes in update ledger to remove duplicay of entries

--- src/app/ledger/components/updateLedgerEntryPanel/updateLedgerEntryPanel.component.ts
+++ src/app/ledger/components/updateLedgerEntryPanel/updateLedgerEntryPanel.component.ts
@@ -556,6 +556,10 @@ export class UpdateLedgerEntryPanelComponent implements OnInit, AfterViewInit, O
     if (isThereUpdatedEntry && requestObj.taxes && requestObj.taxes.length) {
       this.showUpdateTaxModal();
     } else {
+      // remove existing taxes
+      _.remove(requestObj.taxes, (n) => {
+          return this.vm.selectedLedger.taxes.indexOf(n) !== -1;
+      });
       // if their's no change fire action straightaway
       this.store.dispatch(this._ledgerAction.updateTxnEntry(requestObj, this.accountUniqueName, this.entryUniqueName));
     }
