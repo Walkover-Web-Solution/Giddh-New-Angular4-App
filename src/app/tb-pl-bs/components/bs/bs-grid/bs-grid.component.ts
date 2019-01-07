004a670c85b25a5d24f9fe664fa50477a7ca9199
govinda <govi2010@gmail.com>
2019-01-07 14:42:16 +0530

    redesign for search

--- src/app/tb-pl-bs/components/bs/bs-grid/bs-grid.component.ts
+++ src/app/tb-pl-bs/components/bs/bs-grid/bs-grid.component.ts
@@ -9,20 +9,6 @@ import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'bs-grid',  // <home></home>
-  animations: [
-    trigger(
-      'enterAnimation', [
-        transition(':enter', [
-          style({transform: 'translateX(100%)', opacity: 0}),
-          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
-        ]),
-        transition(':leave', [
-          style({transform: 'translateX(0)', opacity: 1}),
-          animate('0ms', style({transform: 'translateX(100%)', opacity: 0}))
-        ])
-      ]
-    )
-  ],
   templateUrl: './bs-grid.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
--- src/app/tb-pl-bs/components/tb/tb-grid/tb-grid.component.ts
+++ src/app/tb-pl-bs/components/tb/tb-grid/tb-grid.component.ts
@@ -8,20 +8,6 @@ import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'tb-grid',
-  animations: [
-    trigger(
-      'enterAnimation', [
-        transition(':enter', [
-          style({transform: 'translateX(100%)', opacity: 0}),
-          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
-        ]),
-        transition(':leave', [
-          style({transform: 'translateX(0)', opacity: 1}),
-          animate('0ms', style({transform: 'translateX(100%)', opacity: 0}))
-        ])
-      ]
-    )
-  ],
   templateUrl: './tb-grid.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
