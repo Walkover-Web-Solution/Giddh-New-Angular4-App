09e6167dc453c92d338d72055054dfb8e0190c44
mustafa-walkover <mustafa@walkover.in>
2019-01-10 14:09:15 +0530

    mustafa | resolving multiple API calls in ledger

--- src/app/ledger/ledger.component.ts
+++ src/app/ledger/ledger.component.ts
@@ -681,6 +681,7 @@ export class LedgerComponent implements OnInit, OnDestroy {
   }
 
   public getTransactionData() {
+    this.accountInprogress$.subscribe(a => a);
     // this.isAdvanceSearchImplemented = false;
     // this.advanceSearchComp.resetAdvanceSearchModal();
     // this.advanceSearchRequest = null;
