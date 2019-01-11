5cd31907a6608e2ac43409091b6313f8c27bd97f
mustafa-walkover <mustafa@walkover.in>
2019-01-11 14:42:39 +0530

    mustafa | remove existing Tax txn in updateLedger

--- src/app/ledger/components/updateLedgerEntryPanel/updateLedgerEntryPanel.component.ts
+++ src/app/ledger/components/updateLedgerEntryPanel/updateLedgerEntryPanel.component.ts
@@ -66,6 +66,7 @@ export class UpdateLedgerEntryPanelComponent implements OnInit, AfterViewInit, O
   public currentAccountApplicableTaxes: string[] = [];
   public isMultiCurrencyAvailable: boolean = false;
   public baseCurrency: string = null;
+  public existingTaxTxn: any[] = [];
 
   constructor(private store: Store<AppState>, private _ledgerService: LedgerService,
               private _toasty: ToasterService, private _accountService: AccountService,
@@ -261,6 +262,7 @@ export class UpdateLedgerEntryPanelComponent implements OnInit, AfterViewInit, O
           this.vm.generatePanelAmount();
           this.vm.generateGrandTotal();
           this.vm.generateCompoundTotal();
+          this.existingTaxTxn = _.filter(this.vm.selectedLedger.transactions, (o) => o.isTax );
           //#endregion
         }
       });
@@ -558,7 +560,12 @@ export class UpdateLedgerEntryPanelComponent implements OnInit, AfterViewInit, O
     } else {
       // remove taxes entry
       _.remove(requestObj.transactions, (obj) => {
-          return obj.isTax;
+          if (obj.isTax) {
+            let taxTxn = _.find(this.existingTaxTxn, (o) => obj.particular.uniqueName === o.particular.uniqueName);
+            if (taxTxn) {
+              return obj;
+            }
+          }
       });
       // if their's no change fire action straightaway
       this.store.dispatch(this._ledgerAction.updateTxnEntry(requestObj, this.accountUniqueName, this.entryUniqueName));
