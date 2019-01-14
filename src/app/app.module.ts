a97f831f2d6984c7b1e66f343c38918fa2fc9abb
mustafa-walkover <mustafa@walkover.in>
2019-01-14 18:55:18 +0530

    mustafa | search module issue resolve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,7 +40,7 @@ import { PopoverModule } from 'ngx-bootstrap/popover';
 import { LaddaModule } from 'angular2-ladda/module/module';
 import { ShSelectModule } from './theme/ng-virtual-select/sh-select.module';
 import { LoaderComponent } from './loader/loader.component';
-import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTypeaheadModule, NgbTypeaheadConfig } from '@ng-bootstrap/ng-bootstrap';
 import { localStorageSync } from 'ngrx-store-localstorage';
 import { ActionModule } from './actions/action.module';
 import { DecoratorsModule } from './decorators/decorators.module';
@@ -94,6 +94,10 @@ if (ENV === 'development') {
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true
 };
+
+function getTypeaheadConfig(): NgbTypeaheadConfig {
+ return Object.assign(new NgbTypeaheadConfig(), {hideResultsOnBlur: false});
+}
 
 /**
  * `AppModule` is the main entry point into Angular2's bootstraping process
@@ -185,7 +189,11 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
       useClass: GiddhHttpInterceptor,
       multi: true
     },
-    CustomPreloadingStrategy
+    CustomPreloadingStrategy,
+    {
+      provide: NgbTypeaheadConfig,
+      useFactory: getTypeaheadConfig
+    }
   ]
 })
 export class AppModule {
