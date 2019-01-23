89cfd1df8b164170dc29e56c66e7d3c7cd705e4d
Faiz <faiz2rock@gmail.com>
2019-01-23 18:11:35 +0530

    Faizal | Alt+C to create new account/customer in Journal,Sales and Ledger

--- src/app/shared/helpers/directives/keyboardShortcut/keyboardShortcut.directive.ts
+++ src/app/shared/helpers/directives/keyboardShortcut/keyboardShortcut.directive.ts
@@ -5,6 +5,42 @@ const keyMaps = {
   tab: 9,
   enter: 13,
   esc: 27,
+  0: 48,
+  1: 49,
+  2: 50,
+  3: 51,
+  4: 52,
+  5: 53,
+  6: 54,
+  7: 55,
+  8: 56,
+  9: 57,
+  a: 65,
+  b: 66,
+  c: 67,
+  d: 68,
+  e: 69,
+  f: 70,
+  g: 71,
+  h: 72,
+  i: 73,
+  j: 74,
+  k: 75,
+  l: 76,
+  m: 77,
+  n: 78,
+  o: 79,
+  p: 80,
+  q: 81,
+  r: 82,
+  s: 83,
+  t: 84,
+  u: 85,
+  v: 86,
+  w: 87,
+  x: 88,
+  y: 89,
+  z: 90,
 };
 const controlKeyMaps = {
   shift: 'shiftKey',
@@ -41,21 +77,20 @@ export class KeyboardShortcutDirective {
 
   private matchArray(event: KeyboardEvent, shortcuts: string[]) {
     for (let key of shortcuts) {
-      if (key.includes('+')) {
-        let keys = key.split('+');
-        const match = keys.every(k => (controlKeyMaps[k] && event[controlKeyMaps[k]]) || keyMaps[k] === event.which);
-        if (match) {
-          this.onMatch(key);
-          return;
-        }
-      } else if (this.matchSingle(event, key)) {
-        return;
-      }
+      this.matchSingle(event, key);
     }
   }
 
   private matchSingle(event: KeyboardEvent, key: string) {
-    if (event.which === keyMaps[key] && !event.shiftKey && !event.ctrlKey && !event.altKey) {
+    if (key.includes('+')) {
+      let keys = key.split('+');
+      const match = keys.every(k => (controlKeyMaps[k] && event[controlKeyMaps[k]]) || keyMaps[k] === event.which);
+      console.log(key, event, match);
+      if (match) {
+        this.onMatch(key);
+        return;
+      }
+    } else if (event.which === keyMaps[key] && !event.shiftKey && !event.ctrlKey && !event.altKey) {
       this.onMatch(key);
       return true;
     }
--- src/app/theme/ng-virtual-select/sh-select.component.ts
+++ src/app/theme/ng-virtual-select/sh-select.component.ts
@@ -35,6 +35,7 @@ export class ShSelectComponent implements ControlValueAccessor, OnInit, AfterVie
   @Input() public notFoundMsg: string = 'No results found';
   @Input() public notFoundLinkText: string = 'Create New';
   @Input() public notFoundLink: boolean = false;
+  @ContentChild('notFoundLinkTemplate') public notFoundLinkTemplate: TemplateRef<any>;
   @Input() public isFilterEnabled: boolean = true;
   @Input() public width: string = 'auto';
   @Input() public ItemHeight: number = 41;
