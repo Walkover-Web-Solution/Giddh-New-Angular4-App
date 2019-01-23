d83d86e2998bc266d1921cace24ccecb9309f0dd
Faiz <faiz2rock@gmail.com>
2019-01-23 12:45:56 +0530

    Faizal | Tab to switch tabs (in settings) and shift+tab for reverse

--- src/app/shared/helpers/directives/keyboardShortcut/keyboardShortcut.directive.ts
+++ src/app/shared/helpers/directives/keyboardShortcut/keyboardShortcut.directive.ts
@@ -4,27 +4,31 @@ const keyMaps = {
   backspace: 8,
   tab: 9,
   enter: 13,
-  shift: 16,
-  ctrl: 17,
-  alt: 18,
   esc: 27,
+};
+const controlKeyMaps = {
+  shift: 'shiftKey',
+  ctrl: 'ctrlKey',
+  alt: 'altKey',
 };
 
 @Directive({
   selector: '[keyboardShortcut]'
 })
 export class KeyboardShortcutDirective {
-  @Input() public keyboardShortcut: string | { [key: string]: boolean };
-  @Output() public onShortcutPress = new EventEmitter<any>();
+  @Input() public keyboardShortcut: string | string[] | { [key: string]: boolean };
+  @Output() public onShortcutPress = new EventEmitter<string>();
 
   constructor(private _el: ElementRef) {
   }
 
   @HostListener('window:keydown', ['$event, ElementRef'])
   public handleKeyDown(event: KeyboardEvent) {
-    console.log('event', this._el);
-    let key;
-    if (typeof this.keyboardShortcut === 'string') {
+    let key: string;
+    if (Array.isArray(this.keyboardShortcut)) {
+      this.matchArray(event, this.keyboardShortcut);
+      return;
+    } else if (typeof this.keyboardShortcut === 'string') {
       key = this.keyboardShortcut;
     } else {
       key = Object.keys(this.keyboardShortcut)[0];
@@ -32,11 +36,36 @@ export class KeyboardShortcutDirective {
         return;
       }
     }
-    if (event.which === keyMaps[key]) {
-      event.preventDefault();
-      event.stopImmediatePropagation();
-      event.stopPropagation();
-      this.onShortcutPress.emit();
+    this.matchSingle(event, key);
+  }
+
+  private matchArray(event: KeyboardEvent, shortcuts: string[]) {
+    for (let key of shortcuts) {
+      if (key.includes('+')) {
+        let keys = key.split('+');
+        const match = keys.every(k => (controlKeyMaps[k] && event[controlKeyMaps[k]]) || keyMaps[k] === event.which);
+        if (match) {
+          this.onMatch(key);
+          return;
+        }
+      } else if (this.matchSingle(event, key)) {
+        return;
+      }
     }
+  }
+
+  private matchSingle(event: KeyboardEvent, key: string) {
+    if (event.which === keyMaps[key] && !event.shiftKey && !event.ctrlKey && !event.altKey) {
+      this.onMatch(key);
+      return true;
+    }
+    return false;
+  }
+
+  private onMatch(key: string) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    event.stopPropagation();
+    this.onShortcutPress.emit(key);
   }
 }
