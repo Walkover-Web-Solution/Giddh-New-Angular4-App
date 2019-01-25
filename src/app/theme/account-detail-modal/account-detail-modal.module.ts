c17250810249fe02bec29c980287f627f1461749
govi2010 <govi2010@gmail.com>
2019-01-25 11:29:48 +0530

    account details popup with go to ledger done

--- src/app/theme/account-detail-modal/account-detail-modal.module.ts
+++ src/app/theme/account-detail-modal/account-detail-modal.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { AccountDetailModalComponent } from './account-detail-modal.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ModalModule],
   exports: [AccountDetailModalComponent],
   declarations: [AccountDetailModalComponent],
   providers: [],
