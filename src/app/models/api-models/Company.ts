cf39fa7497d19d359ff2d8c0e31d69776fbe9056
mustafa-walkover <mustafa@walkover.in>
2019-02-25 15:07:59 +0530

    feat | in setting profile added new branch alias

--- src/app/models/api-models/Company.ts
+++ src/app/models/api-models/Company.ts
@@ -12,6 +12,7 @@ export class CompanyRequest {
   public contactNo: string;
   public email: string;
   public isBranch?: boolean;
+  public nameAlias?: string;
 }
 
 export class StateDetailsRequest {
@@ -64,6 +65,7 @@ export class CompanyResponse {
   public panNumber?: string;
   public isMultipleCurrency?: boolean;
   public userEntityRoles?: UserEntityRole[];
+  public nameAlias?: string;
 }
 
 export interface UserEntityRole {
