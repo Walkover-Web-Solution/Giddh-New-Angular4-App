71110c4071baf850c1edb818662f85862eafcd69
GOVINDA ALWANI <govi2010@gmail.com>
2019-03-16 17:14:17 +0530

    Master dev BUG - 255 (#1802)
    * css bug fixes in changes
    
    * css bug fixes in changes
    
    * lager tabbing issues fixes on new ledge panel
    
    * update ledger tabindex bug fixes
    
    * branch hover work is done

--- src/app/ledger/components/updateLedger-tax-control/updateLedger-tax-control.component.ts
+++ src/app/ledger/components/updateLedger-tax-control/updateLedger-tax-control.component.ts
@@ -36,6 +36,7 @@ export class UpdateLedgerTaxControlComponent implements OnInit, OnDestroy, OnCha
   @Output() public isApplicableTaxesEvent: EventEmitter<boolean> = new EventEmitter();
   @Output() public taxAmountSumEvent: EventEmitter<number> = new EventEmitter();
   @Output() public selectedTaxEvent: EventEmitter<UpdateLedgerTaxData[]> = new EventEmitter();
+  @Output() public hideOtherPopups: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   public sum: number = 0;
   public formattedTotal: string;
@@ -146,6 +147,30 @@ export class UpdateLedgerTaxControlComponent implements OnInit, OnDestroy, OnCha
     } else {
       this.isApplicableTaxesEvent.emit(true);
     }
+  }
+
+  public onFocusLastDiv(el) {
+    el.stopPropagation();
+    el.preventDefault();
+    if (!this.showTaxPopup) {
+      this.showTaxPopup = true;
+      this.hideOtherPopups.emit(true);
+      return;
+    }
+    let focussableElements = '.entrypanel input[type=text]:not([disabled]),.entrypanel [tabindex]:not([disabled]):not([tabindex="-1"])';
+    // if (document.activeElement && document.activeElement.form) {
+    let focussable = Array.prototype.filter.call(document.querySelectorAll(focussableElements),
+      (element) => {
+        // check for visibility while always include the current activeElement
+        return element.offsetWidth > 0 || element.offsetHeight > 0 || element === document.activeElement
+      });
+    let index = focussable.indexOf(document.activeElement);
+    if (index > -1) {
+      let nextElement = focussable[index + 1] || focussable[0];
+      nextElement.focus();
+    }
+    this.toggleTaxPopup(false);
+    return false;
   }
 
   private isTaxApplicable(tax): boolean {
