e6ddfc8e6ec02e00bb1b86bf33ff8b2cf08a86f6
govi2010 <govi2010@gmail.com>
2019-06-11 10:44:35 +0530

    update proforma invoice

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -1731,8 +1731,8 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
           if (this.isUpdateMode) {
             // we need to remove # from account uniqueName because we are appending # to stock for uniqueNess
             if (txn.stockList && txn.stockList.length) {
-              txn.accountUniqueName = txn.accountUniqueName.slice(0, txn.accountUniqueName.indexOf('#'));
-              txn.fakeAccForSelect2 = txn.fakeAccForSelect2.slice(0, txn.fakeAccForSelect2.indexOf('#'));
+              txn.accountUniqueName = txn.accountUniqueName.indexOf('#') > -1 ? txn.accountUniqueName.slice(0, txn.accountUniqueName.indexOf('#')) : txn.accountUniqueName;
+              txn.fakeAccForSelect2 = txn.accountUniqueName.indexOf('#') > -1 ? txn.fakeAccForSelect2.slice(0, txn.fakeAccForSelect2.indexOf('#')) : txn.fakeAccForSelect2;
             }
           }
           // will get errors of string and if not error then true boolean
