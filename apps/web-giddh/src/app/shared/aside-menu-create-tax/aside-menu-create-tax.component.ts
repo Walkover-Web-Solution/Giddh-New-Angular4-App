860ce130b931f4d6f3996d7e701149bdb607593e
govi2010 <govi2010@gmail.com>
2019-06-22 12:05:04 +0530

    aside pane

--- apps/web-giddh/src/app/shared/aside-menu-create-tax/aside-menu-create-tax.component.ts
+++ apps/web-giddh/src/app/shared/aside-menu-create-tax/aside-menu-create-tax.component.ts
@@ -1,6 +1,10 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { IOption } from '../../theme/ng-select/option.interface';
 import { TaxResponse } from '../../models/api-models/Company';
+import { ReplaySubject } from 'rxjs';
+import { AppState } from '../../store';
+import { select, Store } from '@ngrx/store';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'aside-menu-create-tax-component',
@@ -21,11 +25,43 @@ export class AsideMenuCreateTaxComponent implements OnInit {
     {label: 'Others', value: 'others'},
 
   ];
+  public duration: IOption[] = [
+    {label: 'Monthly', value: 'MONTHLY'},
+    {label: 'Quarterly', value: 'QUARTERLY'},
+    {label: 'Half-Yearly', value: 'HALFYEARLY'},
+    {label: 'Yearly', value: 'YEARLY'}
+  ];
+  public days: IOption[] = [];
   public newTaxObj: TaxResponse = new TaxResponse();
+  public flattenAccountsOptions: IOption[] = [];
 
-  constructor() {
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
+  constructor(private store: Store<AppState>) {
+    for (let i = 1; i <= 31; i++) {
+      this.days.push({label: i.toString(), value: i.toString()});
+    }
   }
 
   ngOnInit() {
+    this.store
+      .pipe(select(p => p.general.flattenAccounts), takeUntil(this.destroyed$))
+      .subscribe(data => {
+        if (data && data.length) {
+          let arr: IOption[] = [];
+          data.forEach(f => {
+            arr.push({label: `${f.name} - (${f.uniqueName})`, value: f.uniqueName});
+          });
+          this.flattenAccountsOptions = arr;
+        }
+      });
+  }
+
+  public customAccountFilter(term: string, item: IOption) {
+    return (item.label.toLocaleLowerCase().indexOf(term) > -1 || item.value.toLocaleLowerCase().indexOf(term) > -1);
+  }
+
+  public customDateSorting(a: IOption, b: IOption) {
+    return (parseInt(a.label) - parseInt(b.label));
   }
 }
--- apps/web-giddh/src/app/shared/aside-menu-create-tax/aside-menu-create-tax.module.ts
+++ apps/web-giddh/src/app/shared/aside-menu-create-tax/aside-menu-create-tax.module.ts
@@ -5,11 +5,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ShSelectModule } from '../../theme/ng-virtual-select/sh-select.module';
 import { LaddaModule } from 'angular2-ladda';
 import { DecimalDigitsModule } from '../helpers/directives/decimalDigits/decimalDigits.module';
+import { BsDatepickerModule } from 'ngx-bootstrap';
 
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule, ReactiveFormsModule, ShSelectModule, LaddaModule, DecimalDigitsModule
+    CommonModule, FormsModule, ReactiveFormsModule, ShSelectModule, LaddaModule, DecimalDigitsModule, BsDatepickerModule
   ],
   exports: [AsideMenuCreateTaxComponent],
   declarations: [AsideMenuCreateTaxComponent],
