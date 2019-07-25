527b75290ddc3fff283039d34a7141c2408adf76
Pradeep Kumar <pradeep@msg91.com>
2019-07-25 09:34:44 +0530

    If one page and no record

--- apps/web-giddh/src/app/invoice/preview/invoice.preview.component.ts
+++ apps/web-giddh/src/app/invoice/preview/invoice.preview.component.ts
@@ -307,11 +307,13 @@ export class InvoicePreviewComponent implements OnInit, OnChanges, OnDestroy {
           this.showExportButton = this.voucherData.items.every(s => s.account.uniqueName === this.voucherData.items[0].account.uniqueName);
         } else {
           // this.totalSale = 0;
-          if(this.voucherData.page>0) {
+          if(this.voucherData.page>1) {
             this.voucherData.totalItems = this.voucherData.count * (this.voucherData.page - 1);
             this.advanceSearchFilter.page = Math.ceil(this.voucherData.totalItems / this.voucherData.count);
             this.invoiceSearchRequest.page = Math.ceil(this.voucherData.totalItems / this.voucherData.count);
-            this.getVoucher(false);
+            if(this.voucherData.page!==0){
+              this.getVoucher(false);
+            }            
           }
           this.showExportButton = false;
         }
