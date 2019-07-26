0c3a8cf0c80214d5d989ad7342c40244a2ae69f0
rohit-walkover <rohit@walkover.in>
2019-07-26 14:54:23 +0530

    export invoice integrated  API changed for Dev

--- apps/web-giddh/src/app/services/invoice.service.ts
+++ apps/web-giddh/src/app/services/invoice.service.ts
@@ -722,7 +722,7 @@ public UpdateGeneratedTransporter(transporterId: string, model: IEwayBillTranspo
    public exportCsvInvoiceDownload(model: any): Observable<BaseResponse<string, any>> {
     this.user = this._generalService.user;
     this.companyUniqueName = this._generalService.companyUniqueName;
-    return this._http.get(this.config.apiUrl + INVOICE_API.DOWNLOAD_INVOICE_EXPORT_CSV.replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName)).replace(':from', encodeURIComponent(model.from)).replace(':to', encodeURIComponent(model.to))).pipe(
+    return this._http.post(this.config.apiUrl + INVOICE_API.DOWNLOAD_INVOICE_EXPORT_CSV.replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName)).replace(':from', encodeURIComponent(model.from)).replace(':to', encodeURIComponent(model.to)), model.dataToSend).pipe(
       map((res) => {
         let data: BaseResponse<string, any> = res;
         data.request = model;
