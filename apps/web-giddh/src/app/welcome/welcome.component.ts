df6d3dbb660a686667a30b28e74e351ac7e43aab
rohit-walkover <rohit@walkover.in>
2019-08-07 18:29:55 +0530

    welcome page object issue and gst state issue fixed

--- apps/web-giddh/src/app/welcome/welcome.component.ts
+++ apps/web-giddh/src/app/welcome/welcome.component.ts
@@ -1,6 +1,6 @@
-import { Observable, of as observableOf, ReplaySubject } from 'rxjs';
+import { Observable, of as observableOf, ReplaySubject, Subject } from 'rxjs';
 
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, debounceTime, distinctUntilChanged, take } from 'rxjs/operators';
 import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { IOption } from '../theme/ng-select/option.interface';
 import { States, CompanyRequest, CompanyCreateRequest } from '../models/api-models/Company';
@@ -11,6 +11,9 @@ import { contriesWithCodes } from '../shared/helpers/countryWithCodes';
 import { SettingsProfileActions } from '../actions/settings/profile/settings.profile.action';
 import { Router } from '@angular/router';
 import { GeneralService } from '../services/general.service';
+import { ToasterService } from '../services/toaster.service';
+import { FormGroup } from '@angular/forms';
+import { ShSelectComponent } from '../theme/ng-virtual-select/sh-select.component';
 
 @Component({
   selector: 'welcome-component',
@@ -22,12 +25,15 @@ export class WelcomeComponent implements OnInit, OnDestroy, AfterViewInit {
   public companyProfileObj: any = null;
   public countryCodeList: IOption[] = [];
   public company: any = {};
-  public createNewCompany: any;
+  public createNewCompany: any = {};
   public statesSource$: Observable<IOption[]> = observableOf([]);
   public stateStream$: Observable<States[]>;
   public states: IOption[] = [];
   public countryIsIndia: boolean = false;
+  public businesstype: string = '';
   public selectedCountry = '';
+  //public gstKeyDownSubject$: Subject<any> = new Subject<any>();
+  public isGstValid: boolean;
   public industrialList: IOption[] = [{
     label: 'Agriculture',
     value: 'Agriculture'
@@ -97,22 +103,29 @@ export class WelcomeComponent implements OnInit, OnDestroy, AfterViewInit {
   }
   ];
   public updateProfileSuccess$: Observable<boolean>;
-  public businessptions: IOption[] = [
-    { label: 'Unregister Business', value: 'Unregister Business' },
-    { label: 'Register Business', value: 'Register Business' }
+  public businessType: IOption[] = [
+    { label: 'Unregister', value: 'unregistered' },
+    { label: 'Register', value: 'registered' }
   ];
 
   public taxesoptions: IOption[] = [
     { label: 'TCS', value: 'TCS Tax' },
     { label: 'TDS', value: 'TDS Tax' },
   ];
+  public BusinessOptions: IOption[] = [
+    { label: 'Food', value: 'food' },
+    { label: 'Service', value: 'service' },
+     { label: 'Manufacturing', value: 'manufacturing' },
+    { label: 'Retail', value: 'retail' },
+  ];
+
 
   public hideTextarea = true;
   public collapseTextarea = false;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(private store: Store<AppState>, private settingsProfileActions: SettingsProfileActions,
-    private _router: Router, private _generalService: GeneralService) {
+    private _router: Router, private _generalService: GeneralService, private _toasty: ToasterService) {
     this.companyProfileObj = {};
 
     contriesWithCodes.map(c => {
@@ -123,7 +136,7 @@ export class WelcomeComponent implements OnInit, OnDestroy, AfterViewInit {
     this.stateStream$.subscribe((data) => {
       if (data) {
         data.map(d => {
-          this.states.push({ label: `${d.name}`, value: `${d.name}` });
+          this.states.push({label: `${d.code} - ${d.name}`, value: d.code});
         });
       }
       this.statesSource$ = observableOf(this.states);
@@ -158,6 +171,16 @@ export class WelcomeComponent implements OnInit, OnDestroy, AfterViewInit {
         this._router.navigate(['/select-plan']);
       }
     });
+
+    // this.gstKeyDownSubject$
+    //   .pipe(debounceTime(3000)
+    //     , distinctUntilChanged()
+    //     , takeUntil(this.destroyed$))
+    //   .subscribe((event: any) => {
+    //     if (this.isGstValid) {
+    //     this.patchProfile({gstDetails: this.companyProfileObj.gstDetails});
+    //     }
+    //   });
   }
 
   public ngAfterViewInit() {
@@ -193,6 +216,79 @@ export class WelcomeComponent implements OnInit, OnDestroy, AfterViewInit {
       }
     }
 
+  }
+    public checkGstNumValidation(ele: HTMLInputElement) {
+    let isInvalid: boolean = false;
+    if (ele.value) {
+      if (ele.value.length !== 15 || (Number(ele.value.substring(0, 2)) < 1) || (Number(ele.value.substring(0, 2)) > 37)) {
+        this._toasty.errorToast('Invalid GST number');
+        ele.classList.add('error-box');
+        this.isGstValid = false;
+      } else {
+        ele.classList.remove('error-box');
+        this.isGstValid = true;
+        // this.checkGstDetails();
+      }
+    } else {
+      ele.classList.remove('error-box');
+    }
+  }
+  public getStateCode(gstNo: HTMLInputElement, statesEle: ShSelectComponent) {
+    let gstVal: string = gstNo.value;
+
+    if (gstVal.length >= 2) {
+      this.statesSource$.pipe(take(1)).subscribe(state => {
+        let s = state.find(st => st.value === gstVal.substr(0, 2));
+        statesEle.setDisabledState(false);
+        this.companyProfileObj.state = s.value;
+        if (s) {
+           this.companyProfileObj.state = s.value;
+           statesEle.setDisabledState(true);
+         
+        } else {
+           this.companyProfileObj.state = '';
+          statesEle.setDisabledState(false);
+          this._toasty.clearAllToaster();
+          this._toasty.warningToast('Invalid GSTIN.');
+        }
+      });
+    } else {
+      statesEle.setDisabledState(false);
+      this.companyProfileObj.state = '';
+    }
+  }
+
+  public selectedbusinessType(event) {
+    //
+    if(event) {
+    this.businessType = event.value;
+    }
+    console.log(event);
+  }
+  public selectedbusinessOptions(event) {
+    if(event) {
+   //
+    }
+    console.log(event);
+
+  }
+  public selectedApplicabeTaxes(event) {
+    console.log(event);
+
+  }
+
+    public setChildState(ele: HTMLInputElement, index: number) {
+    let stateCode: any = Number(ele.value.substring(0, 2));
+    if (stateCode <= 37) {
+      if (stateCode < 10 && stateCode !== 0) {
+        stateCode = (stateCode < 10) ? '0' + stateCode.toString() : stateCode.toString();
+      } else if (stateCode === 0) {
+        stateCode = '';
+      }
+      this.companyProfileObj.gstDetails[index].addressList[0].stateCode = stateCode.toString();
+    } else {
+      this.companyProfileObj.gstDetails[index].addressList[0].stateCode = '';
+    }
   }
 
   public ngOnDestroy() {
