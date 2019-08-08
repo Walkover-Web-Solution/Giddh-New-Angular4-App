ca66b482446d22f9e7639b341be4a87545cf5ee0
shalinee <shalinee@msg91.com>
2019-08-08 14:27:07 +0530

    add billing-detail component

---
+++ apps/web-giddh/src/app/billing-detail/billingDetail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'billing-details',
+  templateUrl: 'billingDetail.component.html',
+  styleUrls: ['billingDetail.component.scss']
+})
+export class BillingDetailComponent {
+  constructor() {
+
+  }
+}
