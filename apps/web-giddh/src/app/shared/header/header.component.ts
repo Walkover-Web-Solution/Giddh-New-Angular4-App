67e615499e2f080333804a77b3d3af59dcf8a40e
Govinda <govi2010@gmail.com>
2019-08-14 17:44:58 +0530

    issue fixed

--- apps/web-giddh/src/app/shared/header/header.component.ts
+++ apps/web-giddh/src/app/shared/header/header.component.ts
@@ -402,7 +402,7 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
           return;
         }
       } else {
-        const lastStateName = NAVIGATION_ITEM_LIST.find((page) => page.uniqueName.substring(7, page.uniqueName.length).includes(lastState));
+        const lastStateName = NAVIGATION_ITEM_LIST.find((page) => page.uniqueName.substring(7, page.uniqueName.length).includes(lastState.replace('pages/', '')));
         if (lastStateName) {
           return this.selectedPage = lastStateName.name;
         } else if (lastState.includes('ledger/')) {
