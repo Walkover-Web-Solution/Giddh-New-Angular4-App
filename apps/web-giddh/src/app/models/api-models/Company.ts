fd60675801c974b21649f6f09a44a8fcfdbee159
rohit-walkover <rohit@walkover.in>
2019-09-04 12:25:04 +0530

    razorpay buy plan changed issue fixed

--- apps/web-giddh/src/app/models/api-models/Company.ts
+++ apps/web-giddh/src/app/models/api-models/Company.ts
@@ -261,6 +261,9 @@ export class CreateCompanyUsersPlan {
   balance?: number;
   expiry?: string;
   startedAt?: string;
+  companiesWithTransactions?: any;
+  companyTotalTransactions?: any;
+  totalTransactions?: number;
 }
 
 export class PlanDetails {
