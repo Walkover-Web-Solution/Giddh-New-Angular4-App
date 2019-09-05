02cc66fdafe32f29dc9b0bd1d7f6fd9d658533a3
sadik-walkover <sadik@walkver.in>
2019-09-05 17:35:11 +0530

    sadique | multy currency responsive issues

--- apps/web-giddh/src/app/app.component.ts
+++ apps/web-giddh/src/app/app.component.ts
@@ -89,13 +89,13 @@ export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
       this.IAmLoaded = s;
     });
 
-    this.tagManagerUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.googletagmanager.com/ns.html?id=GTM-K2L9QG');
-
-    this.breakpointObserver.observe([
-      '(max-width: 1024px)'
-    ]).subscribe(result => {
-      this.changeOnMobileView(result.matches);
-    });
+    
+    // this.tagManagerUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.googletagmanager.com/ns.html?id=GTM-K2L9QG');
+    // this.breakpointObserver.observe([
+    //   '(max-width: 1024px)'
+    // ]).subscribe(result => {
+    //   this.changeOnMobileView(result.matches);
+    // });
 
   }
 
