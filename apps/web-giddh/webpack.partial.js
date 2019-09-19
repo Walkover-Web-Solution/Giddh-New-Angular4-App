91e4c6a30d0584d52cfc294a05a4301685c25619
shehbaz <shehbaz@walkover.in>
2019-09-19 19:24:33 +0530

    apidev changes

--- apps/web-giddh/webpack.partial.js
+++ apps/web-giddh/webpack.partial.js
@@ -17,7 +17,7 @@ module.exports = {
       'process.env.errlyticsNeeded': JSON.stringify(false),
       'process.env.errlyticsKey': JSON.stringify(''),
       'process.env.AppUrl': JSON.stringify('http://localapp.giddh.com:3000/'),
-      'process.env.ApiUrl': JSON.stringify('http://apitest.giddh.com/'),
+      'process.env.ApiUrl': JSON.stringify('http://apidev.giddh.com/'),
       'process.env.APP_FOLDER': JSON.stringify('')
     }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
