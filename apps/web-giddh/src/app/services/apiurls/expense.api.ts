c04f6f861aec6c187337cd5fe19489a436f02244
rohit-walkover <rohit@walkover.in>
2019-10-15 16:06:00 +0530

    multiple file upload on petty cash approve case is completed

--- apps/web-giddh/src/app/services/apiurls/expense.api.ts
+++ apps/web-giddh/src/app/services/apiurls/expense.api.ts
@@ -3,4 +3,5 @@ export const EXPENSE_API = {
   // get call
   GET: UNIVERSAL_URI_EXPENSE + 'report',
   ACTION: UNIVERSAL_URI_EXPENSE + ':accountUniqueName?action=:actionType',
+  GETEntry: UNIVERSAL_URI_EXPENSE + 'ledger/:accountUniqueName',
 };
