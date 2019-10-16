f2df959169bff32653a3738eabbb501eda0def19
Noopur <noopurchhajed@msg91.com>
2019-10-16 10:42:24 +0530

    update bank details

--- apps/web-giddh/src/app/shared/header/components/account-update-new/account-update-new.component.ts
+++ apps/web-giddh/src/app/shared/header/components/account-update-new/account-update-new.component.ts
@@ -474,16 +474,16 @@ export class AccountUpdateNewComponent implements OnInit, OnDestroy {
     //   this._toaster.errorToast('Mobile no. & email Id is mandatory');
     //   return;
     // }
-    if (this.showBankDetail) {
-      if (accountRequest.accountBankDetails && accountRequest.accountBankDetails.length > 0) {
-        if (!accountRequest['accountBankDetails'][0].bankAccountNo || !accountRequest['accountBankDetails'][0].ifsc) {
-        //  accountRequest['accountBankDetails'] = [];
-        this.accountDetails.accountBankDetails;
-        }
+    // if (this.showBankDetail) {
+    //   if (accountRequest.accountBankDetails && accountRequest.accountBankDetails.length > 0) {
+    //     if (!accountRequest['accountBankDetails'][0].bankAccountNo || !accountRequest['accountBankDetails'][0].ifsc) {
+    //       accountRequest['accountBankDetails'] = [];
+    //     }
+    //   }
+    // } else {
+    if (!this.showBankDetail) { 
+        delete accountRequest['accountBankDetails'];
       }
-    } else {
-      delete accountRequest['accountBankDetails'];
-    }
 
     this.submitClicked.emit({
       value: {groupUniqueName: this.activeGroupUniqueName, accountUniqueName: this.activeAccountName},
