35afa224bfec3cd36389b5db67717959255cc41b
sadik-walkover <sadik@walkver.in>
2019-10-21 10:30:22 +0530

    add tooltip in add ledger

--- apps/web-giddh/src/app/theme/tax-control/tax-control.module.ts
+++ apps/web-giddh/src/app/theme/tax-control/tax-control.module.ts
@@ -4,9 +4,10 @@ import { TaxControlComponent } from './tax-control.component';
 import { FormsModule } from '@angular/forms';
 import { ClickOutsideModule } from 'ng-click-outside';
 import { VirtualScrollModule }  from 'apps/web-giddh/src/app/theme/ng-virtual-select/virtual-scroll';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ClickOutsideModule, VirtualScrollModule],
+  imports: [CommonModule, FormsModule, ClickOutsideModule, VirtualScrollModule, TooltipModule.forRoot(),],
   declarations: [TaxControlComponent],
   exports: [TaxControlComponent]
 })
