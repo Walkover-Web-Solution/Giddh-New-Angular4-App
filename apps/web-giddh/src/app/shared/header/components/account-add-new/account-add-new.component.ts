1b5ff7cb5383d5d0f13e64682fa4adbb721b6643
rohit-walkover <rohit@walkover.in>
2019-10-21 18:45:41 +0530

    country select phone code auto selected issue fixed

--- apps/web-giddh/src/app/shared/header/components/account-add-new/account-add-new.component.ts
+++ apps/web-giddh/src/app/shared/header/components/account-add-new/account-add-new.component.ts
@@ -69,10 +69,10 @@ export class AccountAddNewComponent implements OnInit, OnChanges, OnDestroy {
 
   public showOtherDetails: boolean = false;
   public partyTypeSource: IOption[] = [
-    {value: 'NOT APPLICABLE', label: 'NOT APPLICABLE'},
-    {value: 'DEEMED EXPORT', label: 'DEEMED EXPORT'},
-    {value: 'GOVERNMENT ENTITY', label: 'GOVERNMENT ENTITY'},
-    {value: 'SEZ', label: 'SEZ'}
+    { value: 'NOT APPLICABLE', label: 'NOT APPLICABLE' },
+    { value: 'DEEMED EXPORT', label: 'DEEMED EXPORT' },
+    { value: 'GOVERNMENT ENTITY', label: 'GOVERNMENT ENTITY' },
+    { value: 'SEZ', label: 'SEZ' }
   ];
   public countrySource: IOption[] = [];
   public stateStream$: Observable<States[]>;
@@ -91,7 +91,7 @@ export class AccountAddNewComponent implements OnInit, OnChanges, OnDestroy {
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(private _fb: FormBuilder, private store: Store<AppState>, private accountsAction: AccountsAction,
-              private _companyService: CompanyService, private _toaster: ToasterService, private companyActions: CompanyActions) {
+    private _companyService: CompanyService, private _toaster: ToasterService, private companyActions: CompanyActions) {
     this.companiesList$ = this.store.select(s => s.session.companies).pipe(takeUntil(this.destroyed$));
     this.stateStream$ = this.store.select(s => s.general.states).pipe(takeUntil(this.destroyed$));
     this.stateStream$.subscribe((data) => {
@@ -99,7 +99,7 @@ export class AccountAddNewComponent implements OnInit, OnChanges, OnDestroy {
       let states: IOption[] = [];
       if (data) {
         data.map(d => {
-          states.push({label: `${d.code} - ${d.name}`, value: d.code});
+          states.push({ label: `${d.code} - ${d.name}`, value: d.code });
         });
       }
       this.statesSource$ = observableOf(states);
@@ -111,19 +111,19 @@ export class AccountAddNewComponent implements OnInit, OnChanges, OnDestroy {
       let currencies: IOption[] = [];
       if (data) {
         data.map(d => {
-          currencies.push({label: d.code, value: d.code});
+          currencies.push({ label: d.code, value: d.code });
         });
       }
       this.currencySource$ = observableOf(currencies);
     });
 
     contriesWithCodes.map(c => {
-      this.countrySource.push({value: c.countryflag, label: `${c.countryflag} - ${c.countryName}`});
+      this.countrySource.push({ value: c.countryflag, label: `${c.countryflag} - ${c.countryName}`, additional: c.value });
     });
 
     // Country phone Code
     contriesWithCodes.map(c => {
-      this.countryPhoneCode.push({value: c.value, label: c.value});
+      this.countryPhoneCode.push({ value: c.value, label: c.value });
     });
 
     this.store.select(s => s.settings.profile).pipe(takeUntil(this.destroyed$)).subscribe((profile) => {
@@ -132,8 +132,7 @@ export class AccountAddNewComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   public ngOnInit() {
-    if(this.activeGroupUniqueName == 'discount')
-    {
+    if (this.activeGroupUniqueName === 'discount') {
       this.isDiscount = true;
     }
     this.initializeNewForm();
@@ -220,15 +219,15 @@ export class AccountAddNewComponent implements OnInit, OnChanges, OnDestroy {
         this.isAccountNameAvailable$.subscribe(a => {
           if (a !== null && a !== undefined) {
             if (a) {
-              this.addAccountForm.patchValue({uniqueName: val});
+              this.addAccountForm.patchValue({ uniqueName: val });
             } else {
               let num = 1;
-              this.addAccountForm.patchValue({uniqueName: val + num});
+              this.addAccountForm.patchValue({ uniqueName: val + num });
             }
           }
         });
       } else {
-        this.addAccountForm.patchValue({uniqueName: ''});
+        this.addAccountForm.patchValue({ uniqueName: '' });
       }
     });
   }
@@ -252,7 +251,7 @@ export class AccountAddNewComponent implements OnInit, OnChanges, OnDestroy {
       foreignOpeningBalance: [0],
       openingBalance: [0],
       mobileNo: [''],
-      mobileCode: ['91'],
+      mobileCode: [''],
       email: ['', Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)],
       companyName: [''],
       attentionTo: [''],
@@ -263,8 +262,8 @@ export class AccountAddNewComponent implements OnInit, OnChanges, OnDestroy {
       }),
       hsnOrSac: [''],
       currency: [''],
-      hsnNumber: [{value: '', disabled: false}],
-      sacNumber: [{value: '', disabled: false}],
+      hsnNumber: [{ value: '', disabled: false }],
+      sacNumber: [{ value: '', disabled: false }],
       accountBankDetails: this._fb.array([
         this._fb.group({
           bankName: [''],
@@ -281,7 +280,7 @@ export class AccountAddNewComponent implements OnInit, OnChanges, OnDestroy {
     let gstFields = this._fb.group({
       gstNumber: ['', Validators.compose([Validators.maxLength(15)])],
       address: ['', Validators.maxLength(120)],
-      stateCode: [{value: '', disabled: false}],
+      stateCode: [{ value: '', disabled: false }],
       isDefault: [false],
       isComposite: [false],
       partyType: ['NOT APPLICABLE']
@@ -443,8 +442,7 @@ export class AccountAddNewComponent implements OnInit, OnChanges, OnDestroy {
       delete accountRequest['cashFreeVirtualAccountData'];
     }
 
-    if(this.activeGroupUniqueName === 'discount')
-    {
+    if (this.activeGroupUniqueName === 'discount') {
       delete accountRequest['addresses'];
     }
     // if (this.showVirtualAccount && (!accountRequest.mobileNo || !accountRequest.email)) {
@@ -478,6 +476,12 @@ export class AccountAddNewComponent implements OnInit, OnChanges, OnDestroy {
     this.resetAddAccountForm();
     this.destroyed$.next(true);
     this.destroyed$.complete();
+  }
+  public selectCountry(event: IOption) {
+    if (event) {
+      let phoneCode = event.additional;
+      this.addAccountForm.get('mobileCode').setValue(phoneCode);
+    }
   }
 
 }
