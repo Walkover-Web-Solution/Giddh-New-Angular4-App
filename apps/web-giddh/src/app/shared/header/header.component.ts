4aedf268d1f499a7647e619e2087f9d3225874cd
Govinda <govi2010@gmail.com>
2019-10-30 17:33:22 +0530

    changes

--- apps/web-giddh/src/app/shared/header/header.component.ts
+++ apps/web-giddh/src/app/shared/header/header.component.ts
@@ -76,8 +76,8 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
   public flyAccounts: ReplaySubject<boolean> = new ReplaySubject<boolean>();
   public noGroups: boolean;
   public languages: any[] = [
-    {name: 'ENGLISH', value: 'en'},
-    {name: 'DUTCH', value: 'nl'}
+    { name: 'ENGLISH', value: 'en' },
+    { name: 'DUTCH', value: 'nl' }
   ];
   public activeFinancialYear: ActiveFinancialYear;
   public datePickerOptions: any = {
@@ -130,8 +130,8 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
     endDate: moment()
   };
 
-  public sideMenu: { isopen: boolean } = {isopen: false};
-  public companyMenu: { isopen: boolean } = {isopen: false};
+  public sideMenu: { isopen: boolean } = { isopen: false };
+  public companyMenu: { isopen: boolean } = { isopen: false };
   public isCompanyRefreshInProcess$: Observable<boolean>;
   public isCompanyCreationSuccess$: Observable<boolean>;
   public isLoggedInWithSocialAccount$: Observable<boolean>;
@@ -588,7 +588,7 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
         if (!this.isDateRangeSelected) {
           this.datePickerOptions.startDate = moment(dateObj[0]);
           this.datePickerOptions.endDate = moment(dateObj[1]);
-          this.datePickerOptions = {...this.datePickerOptions, startDate: moment(dateObj[0]), endDate: moment(dateObj[1])};
+          this.datePickerOptions = { ...this.datePickerOptions, startDate: moment(dateObj[0]), endDate: moment(dateObj[1]) };
           this.isDateRangeSelected = true;
           const from: any = moment().subtract(30, 'days').format(GIDDH_DATE_FORMAT);
           const to: any = moment().format(GIDDH_DATE_FORMAT);
@@ -661,7 +661,7 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
       }
     });
     if (o) {
-      menu = {...menu, ...o};
+      menu = { ...menu, ...o };
     } else {
       try {
         menu.name = pageName.split('/pages/')[1].toLowerCase();
@@ -682,7 +682,7 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
     this.doEntryInDb('menus', menu);
 
     if (menu.additional) {
-      this.router.navigate([pageName], {queryParams: menu.additional});
+      this.router.navigate([pageName], { queryParams: menu.additional });
     } else {
       this.router.navigate([pageName]);
     }
@@ -971,7 +971,7 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
     } else {
       this.isTodaysDateSelected = true;
       let today = _.cloneDeep([moment(), moment()]);
-      this.datePickerOptions = {...this.datePickerOptions, startDate: today[0], endDate: today[1]};
+      this.datePickerOptions = { ...this.datePickerOptions, startDate: today[0], endDate: today[1] };
       let dates = {
         fromDate: null,
         toDate: null,
@@ -1043,7 +1043,7 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
         if (item.uniqueName.includes('?')) {
           item.uniqueName = item.uniqueName.split('?')[0];
         }
-        this.router.navigate([item.uniqueName], {queryParams: {tab: item.additional.tab, tabIndex: item.additional.tabIndex}});
+        this.router.navigate([item.uniqueName], { queryParams: { tab: item.additional.tab, tabIndex: item.additional.tabIndex } });
       } else {
         this.router.navigate([item.uniqueName]);
       }
@@ -1104,7 +1104,7 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
   public goToSelectPlan(template: TemplateRef<any>) {
     this.modalService.hide(1);
     // this.router.navigate(['billing-detail']);
-    this.router.navigate(['pages', 'user-details'], {queryParams: {tab: 'subscriptions', tabIndex: 3, isPlanPage: true}});
+    this.router.navigate(['pages', 'user-details'], { queryParams: { tab: 'subscriptions', tabIndex: 3, isPlanPage: true } });
     this.modelRefExpirePlan = this.modalService.show(template);
   }
 
@@ -1264,7 +1264,7 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
     );
 
     this.subscriptions.push(_combine);
-    let config: ModalOptions = {class: 'universal_modal', show: true, keyboard: true, animated: false};
+    let config: ModalOptions = { class: 'universal_modal', show: true, keyboard: true, animated: false };
     this.modelRef = this.modalService.show(this.navigationModal, config);
   }
 
@@ -1295,4 +1295,5 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
     }
     return name;
   }
+
 }
