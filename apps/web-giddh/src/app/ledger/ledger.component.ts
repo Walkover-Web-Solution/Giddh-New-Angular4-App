365ebb01b875720333e483eb80c67dea58c5a1b5
rohit-walkover <rohit@walkover.in>
2019-11-02 15:07:34 +0530

    ledger bank account reconcile issue fixed

--- apps/web-giddh/src/app/ledger/ledger.component.ts
+++ apps/web-giddh/src/app/ledger/ledger.component.ts
@@ -529,6 +529,10 @@ export class LedgerComponent implements OnInit, OnDestroy {
 
     this.lc.transactionData$.subscribe((lt: any) => {
       if (lt) {
+        if (lt.closingBalance) {
+          this.closingBalanceBeforeReconcile = lt.closingBalance;
+          this.closingBalanceBeforeReconcile.type = this.closingBalanceBeforeReconcile.type === 'CREDIT' ? 'Cr' : 'Dr';;
+        }
         if (lt.closingBalanceForBank) {
           this.reconcileClosingBalanceForBank = lt.closingBalanceForBank;
           this.reconcileClosingBalanceForBank.type = this.reconcileClosingBalanceForBank.type === 'CREDIT' ? 'Cr' : 'Dr';
@@ -1283,7 +1287,9 @@ export class LedgerComponent implements OnInit, OnDestroy {
     this.lc.transactionData$.pipe(take(1)).subscribe((val) => {
       if (val) {
         this.closingBalanceBeforeReconcile = val.closingBalance;
-        this.closingBalanceBeforeReconcile.type = this.closingBalanceBeforeReconcile.type === 'CREDIT' ? 'Cr' : 'Dr';
+        if (this.closingBalanceBeforeReconcile) {
+          this.closingBalanceBeforeReconcile.type = this.closingBalanceBeforeReconcile.type === 'CREDIT' ? 'Cr' : 'Dr';
+        }
       }
     });
     let dataToSend = {
@@ -1546,5 +1552,10 @@ export class LedgerComponent implements OnInit, OnDestroy {
       this.keydownClassAdded = false;
     }
 
+  }
+  public getReconciledBack() {
+    this.closingBalanceBeforeReconcile = null;
+    this.getBankTransactions();
+    this.getTransactionData();
   }
 }
