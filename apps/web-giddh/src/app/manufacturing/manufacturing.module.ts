167e2a79e8d5d05710c020d1183252bbfd888bdc
prateek-giddh <prateekmishra@giddh.com>
2019-11-12 13:28:59 +0530

    fix: Issue #G0-2236

--- apps/web-giddh/src/app/manufacturing/manufacturing.module.ts
+++ apps/web-giddh/src/app/manufacturing/manufacturing.module.ts
@@ -1,45 +1,24 @@
-import { ShSelectModule } from './../theme/ng-virtual-select/sh-select.module';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ManufacturingComponent } from './manufacturing.component';
-import { MfReportComponent } from './report/mf.report.component';
+
 import { MfEditComponent } from './edit/mf.edit.component';
-import { ManufacturingRoutingModule } from './manufacturing.routing.module';
 import { DeleteManufacturingConfirmationModelComponent } from './edit/modal/confirmation.model.component';
-import { BsDatepickerModule, DatepickerModule } from 'ngx-bootstrap/datepicker';
-import { PaginationModule } from 'ngx-bootstrap/pagination';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { LaddaModule } from 'angular2-ladda';
-import { SelectModule } from '../theme/ng-select/ng-select';
-import { DecimalDigitsModule } from '../shared/helpers/directives/decimalDigits/decimalDigits.module';
-import { ClickOutsideModule } from 'ng-click-outside';
+import { ManufacturingComponent } from './manufacturing.component';
+import { ManufacturingRoutingModule } from './manufacturing.routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ManufacturingComponent,
-    MfReportComponent,
     MfEditComponent,
     DeleteManufacturingConfirmationModelComponent
   ],
   exports: [RouterModule],
   providers: [],
   imports: [
-    RouterModule,
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    ManufacturingRoutingModule,
-    PaginationModule,
-    DatepickerModule,
-    BsDatepickerModule.forRoot(),
-    ModalModule,
-    LaddaModule,
-    SelectModule,
-    DecimalDigitsModule,
-    ShSelectModule,
-    ClickOutsideModule
+      ManufacturingRoutingModule,
+      SharedModule,
+      RouterModule
   ],
 })
 export class ManufacturingModule {
