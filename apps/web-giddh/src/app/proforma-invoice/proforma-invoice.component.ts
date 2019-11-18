8b6c214ff17c78dee0ce453bd5dd444429c3f777
prateek-giddh <prateekmishra@giddh.com>
2019-11-18 20:37:25 +0530

    fix: Type of cash invoice

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -2986,7 +2986,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
         this.originalExchangeRate = this.exchangeRate;
 
         this.invoiceUniqueName = result.uniqueName;
-        this.invoiceType = result.type;
+        // this.invoiceType = result.type;
         this.prepareInvoiceTypeFlags();
         if (result.cashInvoice) {
             this.isCashInvoice = true;
