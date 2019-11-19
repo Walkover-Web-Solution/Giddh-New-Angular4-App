669eebbb3f1c8c11043ea995e2f8c1c26c5cbd41
sadik-walkover <sadik@walkver.in>
2019-11-19 10:58:57 +0530

    vat report

--- apps/web-giddh/src/app/vat-report/vatReport.module.ts
+++ apps/web-giddh/src/app/vat-report/vatReport.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { VatReportRoutingModule } from './vatReport.routing.module';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { VatReportComponent } from './vatReport.component';
-import { VatReportListComponent } from './components/test/vat-report-list.component';
 import { CommonModule } from '@angular/common';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
   declarations: [
     VatReportComponent,
-    VatReportListComponent
   ],
   imports: [
     VatReportRoutingModule,
