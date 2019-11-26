30f067a029483dacbb48010ea9027fd6ee57a914
shalinee <shalinee@msg91.com>
2019-11-26 14:56:01 +0530

    added img dynamic path

--- apps/web-giddh/src/app/settings/warehouse/warehouse.component.ts
+++ apps/web-giddh/src/app/settings/warehouse/warehouse.component.ts
@@ -15,56 +15,58 @@ import { CompanyActions } from '../../actions/company.actions';
 import { SettingsBranchActions } from '../../actions/settings/branch/settings.branch.action';
 
 export const IsyncData = [
-  { label: 'Debtors', value: 'debtors' },
-  { label: 'Creditors', value: 'creditors' },
-  { label: 'Inventory', value: 'inventory' },
-  { label: 'Taxes', value: 'taxes' },
-  { label: 'Bank', value: 'bank' }
+    { label: 'Debtors', value: 'debtors' },
+    { label: 'Creditors', value: 'creditors' },
+    { label: 'Inventory', value: 'inventory' },
+    { label: 'Taxes', value: 'taxes' },
+    { label: 'Bank', value: 'bank' }
 ];
 
 @Component({
-  selector: 'setting-warehouse',
-  templateUrl: './warehouse.component.html',
-  styleUrls: ['./warehouse.component.scss'],
-  providers: [{ provide: BsDropdownConfig, useValue: { autoClose: false } }]
+    selector: 'setting-warehouse',
+    templateUrl: './warehouse.component.html',
+    styleUrls: ['./warehouse.component.scss'],
+    providers: [{ provide: BsDropdownConfig, useValue: { autoClose: false } }]
 })
 
 export class WarehouseComponent implements OnInit {
-  @ViewChild('companyadd') public companyadd: ElementViewContainerRef;
-  @ViewChild('addCompanyModal') public addCompanyModal: ModalDirective;
-  constructor(
-    private store: Store<AppState>,
-    private settingsBranchActions: SettingsBranchActions,
-    private componentFactoryResolver: ComponentFactoryResolver,
-    private companyActions: CompanyActions,
-    private settingsProfileActions: SettingsProfileActions
-  ) { }
-  public ngOnInit() {
-  }
-  public openCreateCompanyModal() {
-    this.loadAddCompanyComponent();
-    this.addCompanyModal.show();
-  }
-  public hideAddCompanyModal() {
-    this.addCompanyModal.hide();
-  }
+    public imgPath: string = '';
+    @ViewChild('companyadd') public companyadd: ElementViewContainerRef;
+    @ViewChild('addCompanyModal') public addCompanyModal: ModalDirective;
+    constructor(
+        private store: Store<AppState>,
+        private settingsBranchActions: SettingsBranchActions,
+        private componentFactoryResolver: ComponentFactoryResolver,
+        private companyActions: CompanyActions,
+        private settingsProfileActions: SettingsProfileActions
+    ) { }
+    public ngOnInit() {
+        this.imgPath = isElectron ? 'assets/images/' : AppUrl + APP_FOLDER + 'assets/images/';
+    }
+    public openCreateCompanyModal() {
+        this.loadAddCompanyComponent();
+        this.addCompanyModal.show();
+    }
+    public hideAddCompanyModal() {
+        this.addCompanyModal.hide();
+    }
 
 
-  public loadAddCompanyComponent() {
-    let componentFactory = this.componentFactoryResolver.resolveComponentFactory(CompanyAddComponent);
-    let viewContainerRef = this.companyadd.viewContainerRef;
-    viewContainerRef.clear();
-    let componentRef = viewContainerRef.createComponent(componentFactory);
-    (componentRef.instance as CompanyAddComponent).createBranch = true;
-    (componentRef.instance as CompanyAddComponent).closeCompanyModal.subscribe((a) => {
-      this.hideAddCompanyModal();
-    });
-    (componentRef.instance as CompanyAddComponent).closeCompanyModalAndShowAddManege.subscribe((a) => {
-      this.hideCompanyModalAndShowAddAndManage();
-    });
-  }
-  public hideCompanyModalAndShowAddAndManage() {
-    this.addCompanyModal.hide();
-  }
-  public warehouseDetails = false;
+    public loadAddCompanyComponent() {
+        let componentFactory = this.componentFactoryResolver.resolveComponentFactory(CompanyAddComponent);
+        let viewContainerRef = this.companyadd.viewContainerRef;
+        viewContainerRef.clear();
+        let componentRef = viewContainerRef.createComponent(componentFactory);
+        (componentRef.instance as CompanyAddComponent).createBranch = true;
+        (componentRef.instance as CompanyAddComponent).closeCompanyModal.subscribe((a) => {
+            this.hideAddCompanyModal();
+        });
+        (componentRef.instance as CompanyAddComponent).closeCompanyModalAndShowAddManege.subscribe((a) => {
+            this.hideCompanyModalAndShowAddAndManage();
+        });
+    }
+    public hideCompanyModalAndShowAddAndManage() {
+        this.addCompanyModal.hide();
+    }
+    public warehouseDetails = false;
 }
