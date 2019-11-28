f19c98a1bcf7dfcd49f709ec9b2a2921100fe5d5
prateek-giddh <prateekmishra@giddh.com>
2019-11-28 19:04:55 +0530

    refactor: Remove redundant code

--- apps/web-giddh/src/app/settings/warehouse/warehouse.component.ts
+++ apps/web-giddh/src/app/settings/warehouse/warehouse.component.ts
@@ -1,15 +1,17 @@
-import { Component, ComponentFactoryResolver, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { BsDropdownConfig, BsModalService, ModalDirective, ModalOptions } from 'ngx-bootstrap';
+import { Component, ComponentFactoryResolver, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { BsDropdownConfig, BsModalRef, BsModalService, ModalDirective, ModalOptions } from 'ngx-bootstrap';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
-import { CompanyActions } from '../../actions/company.actions';
 import { ItemOnBoardingActions } from '../../actions/item-on-boarding/item-on-boarding.action';
-import { SettingsBranchActions } from '../../actions/settings/branch/settings.branch.action';
-import { SettingsProfileActions } from '../../actions/settings/profile/settings.profile.action';
 import { OnBoardingType } from '../../app.constant';
 import { OnBoardingComponent } from '../../shared/header/components';
 import { ElementViewContainerRef } from '../../shared/helpers/directives/elementViewChild/element.viewchild.directive';
+import { ItemOnBoarding } from '../../store/item-on-boarding/item-on-boarding.reducer';
 import { AppState } from '../../store/roots';
+import { SettingsUtilityService } from '../services/settings-utility.service';
+import { WarehouseActions } from './action/warehouse.action';
 
 /**
  * Warehouse component
@@ -17,6 +19,7 @@ import { AppState } from '../../store/roots';
  * @export
  * @class WarehouseComponent
  * @implements {OnInit}
+ * @implements {OnDestroy}
  */
 @Component({
     selector: 'setting-warehouse',
@@ -24,27 +27,33 @@ import { AppState } from '../../store/roots';
     styleUrls: ['./warehouse.component.scss'],
     providers: [{ provide: BsDropdownConfig, useValue: { autoClose: false } }]
 })
+export class WarehouseComponent implements OnInit, OnDestroy {
 
-export class WarehouseComponent implements OnInit {
-
+    /** Image path relative to app environment */
     public imgPath: string = '';
+    /** Stores item on boarding details */
+    public itemOnBoardingDetails: ItemOnBoarding;
 
-    @ViewChild('companyadd') public companyadd: ElementViewContainerRef;
-
-    @ViewChild('addCompanyModal') public addCompanyModal: ModalDirective;
-
+    /** View container to carry out on boarding */
+    @ViewChild('onBoardingContainer') public onBoardingContainer: ElementViewContainerRef;
+    /** Warehouse on boarding modal viewchild */
+    @ViewChild('warehouseOnBoardingModal') public warehouseOnBoardingModal: ModalDirective;
     /** Welcome component template ref for second step of warehouse on boarding */
     @ViewChild('welcomeComponent') public welcomeComponentTemplate: TemplateRef<any>;
+
+    /** Observable to unsubscribe all the store listeners to avoid memory leaks */
+    private destroyed$: Subject<boolean> = new Subject();
+    /** Stores the current visible on boarding modal instance */
+    private welcomePageModalInstance: BsModalRef;
 
     /** @ignore */
     constructor(
         private store: Store<AppState>,
         private itemOnBoardingActions: ItemOnBoardingActions,
-        private settingsBranchActions: SettingsBranchActions,
         private bsModalService: BsModalService,
         private componentFactoryResolver: ComponentFactoryResolver,
-        private companyActions: CompanyActions,
-        private settingsProfileActions: SettingsProfileActions
+        private settingsUtilityService: SettingsUtilityService,
+        private warehouseActions: WarehouseActions
     ) { }
 
     /**
@@ -54,6 +63,19 @@ export class WarehouseComponent implements OnInit {
      */
     public ngOnInit(): void {
         this.imgPath = isElectron ? 'assets/images/' : AppUrl + APP_FOLDER + 'assets/images/';
+        this.store.pipe(select(state => state.itemOnboarding), takeUntil(this.destroyed$)).subscribe((itemOnBoardingDetails: ItemOnBoarding) => {
+            this.itemOnBoardingDetails = itemOnBoardingDetails;
+        });
+    }
+
+    /**
+     * Releases all the observables to avoid memory leaks
+     *
+     * @memberof WarehouseComponent
+     */
+    public ngOnDestroy(): void {
+        this.destroyed$.next(true);
+        this.destroyed$.complete();
     }
 
     /**
@@ -62,8 +84,67 @@ export class WarehouseComponent implements OnInit {
      * @memberof WarehouseComponent
      */
     public openCreateCompanyModal(): void {
+        this.startOnBoarding();
         this.prepareNewCompanyModal();
-        this.addCompanyModal.show();
+        this.warehouseOnBoardingModal.show();
+    }
+
+    /**
+     * Hides the welcome page modal once the user presses 'back' button
+     * on Welcome page
+     *
+     * @memberof WarehouseComponent
+     */
+    public hideWelcomePage(): void {
+        if (this.welcomePageModalInstance) {
+            this.welcomePageModalInstance.hide();
+            this.openCreateCompanyModal();
+        }
+    }
+
+    /**
+     * Form submit handler responsible for creation of warehouse
+     *
+     * @param {*} formData
+     * @memberof WarehouseComponent
+     */
+    public handleFormSubmit(formData: any): void {
+        if (formData) {
+            const { controls: formControls } = formData.welcomeForm;
+            if (formControls && formData.otherData) {
+                const requestParamter = this.settingsUtilityService.getWarehouseRequestObject(formControls, formData.otherData.taxName);
+                console.log('Request: ', requestParamter);
+                this.store.dispatch(this.warehouseActions.createWarehouse(requestParamter));
+            }
+        }
+    }
+
+    public onModalDismiss(event: ModalDirective) {
+        if (event.dismissReason) {
+            this.endOnBoarding();
+        }
+    }
+
+    /**
+     * Ends the on boarding process
+     *
+     * @private
+     * @memberof WarehouseComponent
+     */
+    private endOnBoarding(): void {
+        this.store.dispatch(this.itemOnBoardingActions.getOnBoardingStatusAction(false));
+        this.store.dispatch(this.itemOnBoardingActions.getOnBoardingTypeAction(null));
+    }
+
+    /**
+     * Starts the on boarding process
+     *
+     * @private
+     * @memberof WarehouseComponent
+     */
+    private startOnBoarding(): void {
+        this.store.dispatch(this.itemOnBoardingActions.getOnBoardingStatusAction(true));
+        this.store.dispatch(this.itemOnBoardingActions.getOnBoardingTypeAction(OnBoardingType.Warehouse));
     }
 
     /**
@@ -73,7 +154,7 @@ export class WarehouseComponent implements OnInit {
      * @memberof WarehouseComponent
      */
     private hideAddCompanyModal(): void {
-        this.addCompanyModal.hide();
+        this.warehouseOnBoardingModal.hide();
     }
 
     /**
@@ -85,15 +166,15 @@ export class WarehouseComponent implements OnInit {
      */
     private prepareNewCompanyModal(): void {
         let componentFactory = this.componentFactoryResolver.resolveComponentFactory(OnBoardingComponent);
-        let viewContainerRef = this.companyadd.viewContainerRef;
+        let viewContainerRef = this.onBoardingContainer.viewContainerRef;
         viewContainerRef.clear();
         let componentRef = viewContainerRef.createComponent(componentFactory);
         (componentRef.instance as OnBoardingComponent).onboardingType = OnBoardingType.Warehouse;
         (componentRef.instance as OnBoardingComponent).closeCompanyModal.subscribe((data: any) => {
             if (data && data.isFirstStepCompleted) {
-                this.store.dispatch(this.itemOnBoardingActions.getOnBoardingStatusAction(true));
-                this.store.dispatch(this.itemOnBoardingActions.getOnBoardingTypeAction(OnBoardingType.Warehouse));
                 this.showWelcomePage();
+            } else {
+                this.endOnBoarding();
             }
             this.hideAddCompanyModal();
         });
@@ -106,13 +187,14 @@ export class WarehouseComponent implements OnInit {
      * @memberof WarehouseComponent
      */
     private showWelcomePage(): void {
-        const modalConfig: ModalOptions = {
-            class: 'warehouse-welcome-modal',
-            animated: false,
-            keyboard: false,
-            backdrop: false
-        };
-        this.bsModalService.show(this.welcomeComponentTemplate, modalConfig);
+        if (this.itemOnBoardingDetails && this.itemOnBoardingDetails.isOnBoardingInProgress) {
+            const modalConfig: ModalOptions = {
+                class: 'warehouse-welcome-modal',
+                animated: false,
+                keyboard: false,
+                backdrop: false
+            };
+            this.welcomePageModalInstance = this.bsModalService.show(this.welcomeComponentTemplate, modalConfig);
+        }
     }
-
 }
