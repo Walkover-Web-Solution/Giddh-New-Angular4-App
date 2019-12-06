851483a759cf46549253338c9b8cf4d2f24d442e
ravindersingh <ravinder@walkover.in>
2019-12-06 12:37:39 +0530

    Added specific heading for import select page

--- apps/web-giddh/src/app/import-excel/import-type-select/import-type-select.component.ts
+++ apps/web-giddh/src/app/import-excel/import-type-select/import-type-select.component.ts
@@ -3,23 +3,35 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppState } from '../../store';
 import { ImportExcelActions } from '../../actions/import-excel/import-excel.actions';
+import { CurrentPage } from '../../models/api-models/Common';
+import { GeneralActions } from '../../actions/general/general.actions';
 
 @Component({
-  selector: 'import-type-select',  // <home></home>
-  styleUrls: ['./import-type-select.component.scss'],
-  templateUrl: './import-type-select.component.html'
+    selector: 'import-type-select',
+    styleUrls: ['./import-type-select.component.scss'],
+    templateUrl: './import-type-select.component.html'
 })
 
 export class ImportTypeSelectComponent implements OnInit {
 
-  constructor(
-    private store: Store<AppState>,
-    private _router: Router,
-    private _importExcelActions: ImportExcelActions
-  ) {
-  }
+    constructor(
+        private store: Store<AppState>,
+        private _router: Router,
+        private _importExcelActions: ImportExcelActions,
+        private _generalActions: GeneralActions
+    ) {
+        this.setCurrentPageTitle("Import Data");
+    }
 
-  public ngOnInit() {
-    this.store.dispatch(this._importExcelActions.resetImportExcelState());
-  }
+    public ngOnInit() {
+        this.store.dispatch(this._importExcelActions.resetImportExcelState());
+    }
+
+    public setCurrentPageTitle(title) {
+        let currentPageObj = new CurrentPage();
+        currentPageObj.name = title;
+        currentPageObj.url = this._router.url;
+        currentPageObj.additional = "";
+        this.store.dispatch(this._generalActions.setPageTitle(currentPageObj));
+    }
 }
