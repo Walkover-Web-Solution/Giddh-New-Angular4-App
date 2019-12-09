2a62df316a39dfa2ade4bc0ba226683055798659
prateek-giddh <prateekmishra@giddh.com>
2019-12-09 18:45:05 +0530

    fix: Issue #3775

--- apps/web-giddh/src/app/settings/warehouse/constants/warehouse.constant.ts
+++ apps/web-giddh/src/app/settings/warehouse/constants/warehouse.constant.ts
@@ -1,6 +1,6 @@
 export const WAREHOUSE_API = {
     CREATE: 'company/:companyUniqueName/warehouse',
     UPDATE: 'company/:companyUniqueName/warehouse/:warehouseUniqueName',
-    FETCH: 'company/:companyUniqueName/warehouse?page=:page',
+    FETCH: 'company/:companyUniqueName/warehouse?page=:page&refresh=true',
     SET_DEFAULT_WAREHOUSE: 'company/:companyUniqueName/warehouse/:warehouseUniqueName/default'
 }
