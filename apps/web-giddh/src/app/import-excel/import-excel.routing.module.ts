6a4843593114c215b37c844c84fbc8b18e153e7a
Govinda <govi2010@gmail.com>
2019-12-16 18:24:15 +0530

    import report navigation issue fixed

--- apps/web-giddh/src/app/import-excel/import-excel.routing.module.ts
+++ apps/web-giddh/src/app/import-excel/import-excel.routing.module.ts
@@ -10,8 +10,8 @@ import { ImportReportComponent } from './import-report/import-report.component';
         RouterModule.forChild([
             {
                 path: '', component: ImportComponent, children: [
-                    { path: '', redirectTo: 'select' },
-                    { path: 'select', component: ImportTypeSelectComponent },
+                    { path: '', redirectTo: 'select-type' },
+                    { path: 'select-type', component: ImportTypeSelectComponent },
                     { path: ':type', component: ImportWizardComponent },
                     { path: 'report/import-report', component: ImportReportComponent }
                 ]
