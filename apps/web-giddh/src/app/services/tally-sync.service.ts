529b33ee4baff0c433e6507f132f22989ca0f51f
rohit-walkover <rohit@walkover.in>
2020-01-29 17:57:54 +0530

    new API for error log download

--- apps/web-giddh/src/app/services/tally-sync.service.ts
+++ apps/web-giddh/src/app/services/tally-sync.service.ts
@@ -5,7 +5,7 @@ import { ErrorHandler } from './catchManager/catchmanger';
 import { GeneralService } from './general.service';
 import { IServiceConfigArgs, ServiceConfig } from './service.config';
 import { TALLY_SYNC_API } from "./apiurls/tally-sync";
-import { TallySyncResponseData } from "../models/api-models/tally-sync";
+import { TallySyncResponseData, DownloadTallyErrorLogRequest } from "../models/api-models/tally-sync";
 
 @Injectable()
 export class TallySyncService {
@@ -43,12 +43,10 @@ export class TallySyncService {
         }), catchError((e) => this.errorHandler.HandleCatch<TallySyncResponseData, string>(e)));
     }
 
-    public getErrorLog(id: any, companyUniqueName: string) {
+    public getErrorLog(companyUniqueName: string, model: DownloadTallyErrorLogRequest) {
         const url = this.config.apiUrl + TALLY_SYNC_API.ERROR_LOG
-            .replace(':companyUniqueName', companyUniqueName)
-            .replace(':tally_logs_id', id)
-            ;
-        return this._http.get(url).pipe(map((res) => {
+            .replace(':companyUniqueName', companyUniqueName);
+        return this._http.get(url, model).pipe(map((res) => {
             return res;
         }), catchError((e) =>
             this.errorHandler.HandleCatch<TallySyncResponseData, string>(e))
