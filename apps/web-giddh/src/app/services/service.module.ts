d5d4916d351a1e7096d848bd5b49aca8ec37ad58
Govinda Mac <govi2010@gmail.com>
2020-02-22 17:01:44 +0530

    Release Android. 2.3.2

--- apps/web-giddh/src/app/services/service.module.ts
+++ apps/web-giddh/src/app/services/service.module.ts
@@ -1,75 +1,75 @@
-import { MagicLinkService } from './magic-link.service';
-import { PermissionDataService } from '../permissions/permission-data.service';
-import { LogsService } from './logs.service';
-import { ErrorHandler } from './catchManager/catchmanger';
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { MagicLinkService } from "./magic-link.service";
+import { PermissionDataService } from "../permissions/permission-data.service";
+import { LogsService } from "./logs.service";
+import { ErrorHandler } from "./catchManager/catchmanger";
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
-import { AuthenticationService } from './authentication.service';
-import { GroupService } from './group.service';
-import { StorageService } from './storage.service';
-import { HttpWrapperService } from './httpWrapper.service';
-import { ToasterService } from './toaster.service';
+import { AuthenticationService } from "./authentication.service";
+import { GroupService } from "./group.service";
+import { StorageService } from "./storage.service";
+import { HttpWrapperService } from "./httpWrapper.service";
+import { ToasterService } from "./toaster.service";
 
-import { CompanyService } from './companyService.service';
-import { LocationService } from './location.service';
-import { AccountService } from './account.service';
-import { InventoryService } from './inventory.service';
-import { PermissionService } from './permission.service';
-import { ManufacturingService } from './manufacturing.service';
+import { CompanyService } from "./companyService.service";
+import { LocationService } from "./location.service";
+import { AccountService } from "./account.service";
+import { InventoryService } from "./inventory.service";
+import { PermissionService } from "./permission.service";
+import { ManufacturingService } from "./manufacturing.service";
 /**
  * Home Module
  */
-import { SearchService } from './search.service';
-import { TlPlService } from './tl-pl.service';
-import { LedgerService } from './ledger.service';
-import { DaybookService } from './daybook.service';
-import { DashboardService } from './dashboard.service';
-import { SettingsIntegrationService } from './settings.integraion.service';
-import { SettingsProfileService } from './settings.profile.service';
-import { SettingsTaxesService } from './settings.taxes.service';
-import { SalesService } from './sales.service';
-import { InvoiceService } from './invoice.service';
-import { InvoiceTemplatesService } from './invoice.templates.service';
-import { SettingsLinkedAccountsService } from './settings.linked.accounts.service';
-import { PurchaseInvoiceService } from './purchase-invoice.service';
-import { SettingsFinancialYearService } from './settings.financial-year.service';
-import { SettingsPermissionService } from './settings.permission.service';
-import { LoaderService } from '../loader/loader.service';
-import { GeneralService } from './general.service';
-import { SettingsBranchService } from './settings.branch.service';
-import { SettingsTagService } from './settings.tag.service';
-import { ContactService } from './contact.service';
-import { SettingsTriggersService } from './settings.triggers.service';
-import { RecurringVoucherService } from './recurring-voucher.service';
-import { ImportExcelService } from './import-excel.service';
-import { SettingsBunchService } from './settings.bunch.service';
-import { SettingsDiscountService } from './settings.discount.service';
-import { NewVsOldInvoicesService } from './new-vs-old-invoices.service';
-import { CompanyImportExportService } from './companyImportExportService';
-import { AgingreportingService } from './agingreporting.service';
-import { GstReconcileService } from './GstReconcile.service';
-import { ReceiptService } from './receipt.service';
-import { DbService } from './db.service';
-import { VersionCheckService } from 'apps/web-giddh/src/app/version-check.service';
-import { SubscriptionsService } from './subscriptions.service';
-import { ProformaService } from './proforma.service';
-import { ExpenseService } from './expences.service';
-import { CommonService } from './common.service';
-import { VatService } from './vat.service';
-import { CommandKService } from './commandk.service';
-import { PurchaseRecordService } from './purchase-record.service';
-import { InvoiceBulkUpdateService } from './invoice.bulkupdate.service';
+import { SearchService } from "./search.service";
+import { TlPlService } from "./tl-pl.service";
+import { LedgerService } from "./ledger.service";
+import { DaybookService } from "./daybook.service";
+import { DashboardService } from "./dashboard.service";
+import { SettingsIntegrationService } from "./settings.integraion.service";
+import { SettingsProfileService } from "./settings.profile.service";
+import { SettingsTaxesService } from "./settings.taxes.service";
+import { SalesService } from "./sales.service";
+import { InvoiceService } from "./invoice.service";
+import { InvoiceTemplatesService } from "./invoice.templates.service";
+import { SettingsLinkedAccountsService } from "./settings.linked.accounts.service";
+import { PurchaseInvoiceService } from "./purchase-invoice.service";
+import { SettingsFinancialYearService } from "./settings.financial-year.service";
+import { SettingsPermissionService } from "./settings.permission.service";
+import { LoaderService } from "../loader/loader.service";
+import { GeneralService } from "./general.service";
+import { SettingsBranchService } from "./settings.branch.service";
+import { SettingsTagService } from "./settings.tag.service";
+import { ContactService } from "./contact.service";
+import { SettingsTriggersService } from "./settings.triggers.service";
+import { RecurringVoucherService } from "./recurring-voucher.service";
+import { ImportExcelService } from "./import-excel.service";
+import { SettingsBunchService } from "./settings.bunch.service";
+import { SettingsDiscountService } from "./settings.discount.service";
+import { NewVsOldInvoicesService } from "./new-vs-old-invoices.service";
+import { CompanyImportExportService } from "./companyImportExportService";
+import { AgingreportingService } from "./agingreporting.service";
+import { GstReconcileService } from "./GstReconcile.service";
+import { ReceiptService } from "./receipt.service";
+import { DbService } from "./db.service";
+import { VersionCheckService } from "apps/web-giddh/src/app/version-check.service";
+import { SubscriptionsService } from "./subscriptions.service";
+import { ProformaService } from "./proforma.service";
+import { ExpenseService } from "./expences.service";
+import { CommonService } from "./common.service";
+import { VatService } from "./vat.service";
+import { CommandKService } from "./commandk.service";
+import { PurchaseRecordService } from "./purchase-record.service";
+import { InvoiceBulkUpdateService } from "./invoice.bulkupdate.service";
+import { UserAgent } from "@ionic-native/user-agent/ngx";
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
  */
 
 @NgModule({
-    imports: [CommonModule, RouterModule
-    ],
+    imports: [CommonModule, RouterModule],
     exports: [CommonModule, FormsModule, RouterModule]
 })
 export class ServiceModule {
@@ -82,6 +82,7 @@ export class ServiceModule {
                 LoaderService,
                 StorageService,
                 ErrorHandler,
+                UserAgent,
                 HttpWrapperService,
                 AuthenticationService,
                 ToasterService,
