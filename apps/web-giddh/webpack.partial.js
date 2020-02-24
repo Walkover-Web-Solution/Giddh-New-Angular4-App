d46f66c68e5d14c930208b6a8436e4c9626b04c3
ravindersingh <ravinder@walkover.in>
2020-02-24 18:26:48 +0530

    Displayed HSN/SAC in Stock always and created HSN/SAC fields in stock group

--- apps/web-giddh/webpack.partial.js
+++ apps/web-giddh/webpack.partial.js
@@ -9,7 +9,7 @@ module.exports = {
             'errlyticsNeeded': JSON.stringify(false),
             'errlyticsKey': JSON.stringify(''),
             'AppUrl': JSON.stringify('http://localhost:3000/'),
-            'ApiUrl': JSON.stringify('https://apitest.giddh.com/'),
+            'ApiUrl': JSON.stringify('http://localhost:8080/'),
             'APP_FOLDER': JSON.stringify(''),
             'PRODUCTION_ENV': JSON.stringify(false),
             'STAGING_ENV': JSON.stringify(false),
@@ -21,7 +21,7 @@ module.exports = {
             'process.env.errlyticsNeeded': JSON.stringify(false),
             'process.env.errlyticsKey': JSON.stringify(''),
             'process.env.AppUrl': JSON.stringify('http://localhost:3000/'),
-            'process.env.ApiUrl': JSON.stringify('https://apitest.giddh.com/'),
+            'process.env.ApiUrl': JSON.stringify('http://localhost:8080/'),
             'process.env.APP_FOLDER': JSON.stringify('')
         }),
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
