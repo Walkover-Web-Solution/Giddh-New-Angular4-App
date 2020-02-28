9befa30830bcc6b1643e8ca8272f0d8161b46399
sadik-walkover <sadik@walkver.in>
2020-02-28 16:25:59 +0530

    add dropdown

--- apps/web-giddh/src/app/create-advance-receipt/create-advance-receipt.component.ts
+++ apps/web-giddh/src/app/create-advance-receipt/create-advance-receipt.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAdvanceReceiptComponent implements OnInit {
 
+    public tempDateParams: any = {};
+
     constructor() {
     }
 
