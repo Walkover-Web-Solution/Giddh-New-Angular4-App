eb3465d35c66995c3a0c403628d81b6ce6a559cb
ravindersingh <ravinder@walkover.in>
2020-03-31 12:53:20 +0530

    updated changes in columnar report

--- apps/web-giddh/src/app/theme/ng-virtual-select/sh-select.component.ts
+++ apps/web-giddh/src/app/theme/ng-virtual-select/sh-select.component.ts
@@ -51,6 +51,7 @@ export class ShSelectComponent implements ControlValueAccessor, OnInit, AfterVie
     @Input() public defaultValue: string = "";
     @Input() public readonlyInput: boolean;
     @Input() public showCheckbox: boolean = false;
+    @Input() public fixedValue: string = "";
 
     @ViewChild('inputFilter') public inputFilter: ElementRef;
     @ViewChild('mainContainer') public mainContainer: ElementRef;
@@ -129,6 +130,10 @@ export class ShSelectComponent implements ControlValueAccessor, OnInit, AfterVie
                 this.propagateChange(this.filter);
             } else {
                 this.clearFilter();
+
+                if (this.fixedValue) {
+                    this.filter = this.fixedValue;
+                }
             }
             this.onHide.emit();
         }
@@ -426,6 +431,10 @@ export class ShSelectComponent implements ControlValueAccessor, OnInit, AfterVie
             if (this.forceClearReactive.status) {
                 this.filter = '';
                 this.clear();
+
+                if (this.fixedValue) {
+                    this.filter = this.fixedValue;
+                }
             }
         }
 
@@ -433,6 +442,12 @@ export class ShSelectComponent implements ControlValueAccessor, OnInit, AfterVie
             if (this.defaultValue) {
                 this.defaultValueUpdated = true;
                 this.filter = changes.defaultValue.currentValue;
+            }
+        }
+
+        if ('fixedValue' in changes) {
+            if (changes.fixedValue && changes.fixedValue.currentValue) {
+                this.filter = changes.fixedValue.currentValue;
             }
         }
     }
