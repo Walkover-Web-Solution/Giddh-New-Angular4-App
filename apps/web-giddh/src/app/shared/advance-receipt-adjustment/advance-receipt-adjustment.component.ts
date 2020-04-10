a7888a21f231d550724410f50b1b921e0d5290dc
rohit-walkover <rohit@walkover.in>
2020-04-10 12:17:40 +0530

    have change return type

--- apps/web-giddh/src/app/shared/advance-receipt-adjustment/advance-receipt-adjustment.component.ts
+++ apps/web-giddh/src/app/shared/advance-receipt-adjustment/advance-receipt-adjustment.component.ts
@@ -414,7 +414,7 @@ export class AdvanceReceiptAdjustmentComponent implements OnInit {
      *
      * @memberof AdvanceReceiptAdjustmentComponent
      */
-    public clickSelectVoucher(index: number, form: NgForm): void {
+    public clickSelectVoucher(index: number, form: NgForm): any {
         form.controls[`voucherName${index}`].markAsTouched();
         this.adjustVoucherOptions = this.getAdvanceReceiptUnselectedVoucher();
         if (this.adjustVoucherForm.adjustments.length && this.adjustVoucherForm.adjustments[index] && this.adjustVoucherForm.adjustments[index].voucherNumber) {
