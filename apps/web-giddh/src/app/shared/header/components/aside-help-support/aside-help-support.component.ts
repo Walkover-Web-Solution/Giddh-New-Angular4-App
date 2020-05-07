edc6b4d659b42ae0afb2299fa29a39f9dac47a33
shalinee-walkover <shalinee@msg91.com>
2020-05-07 14:09:51 +0530

    subscription notification added and image path makes dynamice

--- apps/web-giddh/src/app/shared/header/components/aside-help-support/aside-help-support.component.ts
+++ apps/web-giddh/src/app/shared/header/components/aside-help-support/aside-help-support.component.ts
@@ -1,17 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { AppState } from '../../store';
-import { AccountsAction } from '../../actions/accounts.actions';
-import { CompanyActions } from "../../actions/company.actions";
-import { takeUntil, take } from 'rxjs/operators';
-import { Observable, of as observableOf, ReplaySubject } from "rxjs";
-import { VerifyEmailResponseModel, VerifyMobileModel } from "../../models/api-models/loginModels";
-import { AccountResponseV2 } from "../../models/api-models/Account";
-import { CompanyService } from "../../services/companyService.service";
-import { BankTransferRequest } from "../../models/api-models/Company";
-import { IRegistration } from "../../models/interfaces/registration.interface";
-import { ToasterService } from "../../services/toaster.service";
-
 @Component({
     selector: 'aside-help-support',
     templateUrl: './aside-help-support.component.html',
@@ -19,16 +8,12 @@ import { ToasterService } from "../../services/toaster.service";
 })
 
 export class AsideHelpSupportComponent implements OnInit {
-
     //Event emitter to close the Aside panel
     @Output() public closeAsideEvent: EventEmitter<boolean> = new EventEmitter(true);
-
     public closeAsidePane(event?) {
         this.closeAsideEvent.emit(event);
     }
-    
     public ngOnInit() {
-
     }
 
 }
