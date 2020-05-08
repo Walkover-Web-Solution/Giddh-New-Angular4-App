1085e13281c217618f2f0cbf6975d0f1a41445d5
ravindersingh <ravinder@walkover.in>
2020-05-08 14:53:36 +0530

    fixed bugs

--- apps/web-giddh/src/app/theme/ngx-date-range-picker/ngx-daterangepicker.module.ts
+++ apps/web-giddh/src/app/theme/ngx-date-range-picker/ngx-daterangepicker.module.ts
@@ -6,7 +6,6 @@ import { NgxDaterangepickerComponent } from './ngx-daterangepicker.component';
 import { NgxDaterangepickerDirective } from './ngx-daterangepicker.directive';
 import { LOCALE_CONFIG, LocaleConfig } from './ngx-daterangepicker.config';
 import { NgxDaterangepickerLocaleService } from './ngx-daterangepicker-locale.service';
-import { ScrollComponent } from './virtual-scroll';
 import { PERFECT_SCROLLBAR_CONFIG, PerfectScrollbarConfigInterface, PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
@@ -17,8 +16,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
 @NgModule({
     declarations: [
         NgxDaterangepickerComponent,
-        NgxDaterangepickerDirective,
-        ScrollComponent
+        NgxDaterangepickerDirective
     ],
     imports: [
         CommonModule,
@@ -33,12 +31,10 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     }],
     exports: [
         NgxDaterangepickerComponent,
-        NgxDaterangepickerDirective,
-        ScrollComponent
+        NgxDaterangepickerDirective
     ],
     entryComponents: [
-        NgxDaterangepickerComponent,
-        ScrollComponent
+        NgxDaterangepickerComponent
     ]
 })
 
