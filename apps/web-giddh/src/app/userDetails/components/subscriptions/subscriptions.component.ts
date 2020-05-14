ba7b1670ae12310c4b00573235336bdce0d02906
ravindersingh <ravinder@walkover.in>
2020-05-14 09:01:38 +0530

    dynamic subscription page

--- apps/web-giddh/src/app/userDetails/components/subscriptions/subscriptions.component.ts
+++ apps/web-giddh/src/app/userDetails/components/subscriptions/subscriptions.component.ts
@@ -13,6 +13,7 @@ import { CompanyResponse, SubscriptionRequest, CreateCompanyUsersPlan } from '..
 import { GeneralService } from '../../../services/general.service';
 import { SettingsProfileActions } from '../../../actions/settings/profile/settings.profile.action';
 import { CompanyActions } from '../../../actions/company.actions';
+import { GIDDH_DATE_FORMAT_UI } from '../../../shared/helpers/defaultDateFormat';
 
 @Component({
     selector: 'subscriptions',
@@ -26,7 +27,7 @@ export class SubscriptionsComponent implements OnInit, AfterViewInit, OnDestroy 
     public seletedUserPlans: SubscriptionsUser;
     public selectedPlanCompanies: CompaniesWithTransaction[];
     public isPlanShow: boolean = false;
-    public srchString = '';
+    public searchString = '';
     public transactions: any;
     public companies: any;
     public companyTransactions: any;
@@ -41,15 +42,6 @@ export class SubscriptionsComponent implements OnInit, AfterViewInit, OnDestroy 
     public currentCompanyPlan: any = '';
     public activityLogAsideMenuState: string = 'out';
     public companyDetailsAsideMenuState: string = 'out';
-    public associatedCompanies = [
-        { companyName: 'ABC Limited' },
-        { companyName: 'ABC Limited' },
-        { companyName: 'ABC Limited' },
-        { companyName: 'ABC Limited' },
-        { companyName: 'ABC Limited' },
-        { companyName: 'ABC Limited' },
-        { companyName: 'ABC Limited' },
-    ];
     public subscriptionRequestObj: SubscriptionRequest = {
         planUniqueName: '',
         subscriptionId: '',
@@ -58,6 +50,9 @@ export class SubscriptionsComponent implements OnInit, AfterViewInit, OnDestroy 
     };
     public loggedInUser: UserDetails;
     public subscriptionPlan: CreateCompanyUsersPlan;
+    public giddhDateFormatUI: string = GIDDH_DATE_FORMAT_UI;
+    public subscriptionDates: any = { startedAt: '', expiry: '' };
+    public companyListForFilter: CompanyResponse[] = [];
 
     constructor(private store: Store<AppState>, private _subscriptionsActions: SubscriptionsActions, private modalService: BsModalService, private _route: Router, private activeRoute: ActivatedRoute, private subscriptionService: SubscriptionsService, private generalService: GeneralService, private settingsProfileActions: SettingsProfileActions, private companyActions: CompanyActions) {
         this.store.dispatch(this._subscriptionsActions.SubscribedCompanies());
@@ -71,10 +66,12 @@ export class SubscriptionsComponent implements OnInit, AfterViewInit, OnDestroy 
             this.loggedInUser = this.generalService.user;
         }
 
-        this.companies$.subscribe(company => {
-            if (company) {
+        this.companies$.subscribe(companies => {
+            if (companies) {
+                let orderedCompanies = _.orderBy(companies, 'name');
+                this.companyListForFilter = orderedCompanies;
                 this.activeCompanyUniqueName$.pipe(take(1)).subscribe(active => {
-                    this.activeCompany = company.find(cmp => cmp.uniqueName === active);
+                    this.activeCompany = companies.find(cmp => cmp.uniqueName === active);
                     this.showCurrentCompanyPlan();
                 });
             }
@@ -133,14 +130,14 @@ export class SubscriptionsComponent implements OnInit, AfterViewInit, OnDestroy 
         this.destroyed$.complete();
     }
 
-    public filterCompanyList(ev) {
-        let companies: CompaniesWithTransaction[] = [];
-        companies = this.seletedUserPlans.companiesWithTransactions;
-        let filterd = companies.filter((cmp) => {
-            return cmp.name.toLowerCase().includes(ev.toLowerCase());
-        });
-        this.selectedPlanCompanies = filterd;
-    }
+    // public filterCompanyList(ev) {
+    //     let companies: CompaniesWithTransaction[] = [];
+    //     companies = this.seletedUserPlans.companiesWithTransactions;
+    //     let filterd = companies.filter((cmp) => {
+    //         return cmp.name.toLowerCase().includes(ev.toLowerCase());
+    //     });
+    //     this.selectedPlanCompanies = filterd;
+    // }
 
     /**
      * This function will set the current company plan
@@ -168,6 +165,15 @@ export class SubscriptionsComponent implements OnInit, AfterViewInit, OnDestroy 
             }
         }
 
+        if (this.seletedUserPlans) {
+            if (this.seletedUserPlans.startedAt) {
+                this.subscriptionDates.startedAt = moment(this.seletedUserPlans.startedAt.split("-").reverse().join("-"));
+            }
+            if (this.seletedUserPlans.expiry) {
+                this.subscriptionDates.expiry = moment(this.seletedUserPlans.expiry.split("-").reverse().join("-"));
+            }
+        }
+        console.log(this.subscriptions);
         console.log(this.seletedUserPlans);
     }
 
@@ -210,7 +216,7 @@ export class SubscriptionsComponent implements OnInit, AfterViewInit, OnDestroy 
     }
 
     public renewPlan(): void {
-        if(this.seletedUserPlans && this.seletedUserPlans.planDetails && this.seletedUserPlans.planDetails.amount > 0) {
+        if (this.seletedUserPlans && this.seletedUserPlans.planDetails && this.seletedUserPlans.planDetails.amount > 0) {
 
             this.subscriptionPlan = {
                 companies: this.seletedUserPlans.companies,
@@ -252,6 +258,33 @@ export class SubscriptionsComponent implements OnInit, AfterViewInit, OnDestroy 
 
     public patchProfile(obj): void {
         this.store.dispatch(this.settingsProfileActions.PatchProfile(obj));
+    }
+
+    public formatDate(date: string): moment.Moment {
+        return moment(date.split("-").reverse().join("-"));
+    }
+
+    public calculateRemainingDays(expiry: string): any {
+        if (expiry) {
+            let currentDate = moment();
+            let expiryDate = moment(expiry.split("-").reverse().join("-"));
+            return expiryDate.diff(currentDate, 'days');
+        } else {
+            return "-";
+        }
+    }
+
+    public filterCompanyList(event) {
+        let companies: CompanyResponse[] = [];
+        this.companies$.pipe(take(1)).subscribe(cmps => companies = cmps);
+
+        this.companyListForFilter = companies.filter((cmp) => {
+            if (!cmp.nameAlias) {
+                return cmp.name.toLowerCase().includes(event.toLowerCase());
+            } else {
+                return cmp.name.toLowerCase().includes(event.toLowerCase()) || cmp.nameAlias.toLowerCase().includes(event.toLowerCase());
+            }
+        });
     }
 
     // public getSubscriptionList() {
