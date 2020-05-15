d3512b9d354dc68619b9c3e592bfe24116daf54c
rohit-walkover <rohit@walkover.in>
2020-05-15 16:23:12 +0530

    date issue resolved

--- apps/web-giddh/src/app/manufacturing/edit/mf.edit.component.ts
+++ apps/web-giddh/src/app/manufacturing/edit/mf.edit.component.ts
@@ -22,6 +22,7 @@ import { createSelector } from 'reselect';
 import { IForceClear } from 'apps/web-giddh/src/app/models/api-models/Sales';
 import { CurrentPage } from '../../models/api-models/Common';
 import { GeneralActions } from '../../actions/general/general.actions';
+import { GIDDH_DATE_FORMAT } from '../../shared/helpers/defaultDateFormat';
 
 @Component({
     templateUrl: './mf.edit.component.html'
@@ -333,7 +334,7 @@ export class MfEditComponent implements OnInit {
         let dataToSave = _.cloneDeep(this.manufacturingDetails);
         dataToSave.stockUniqueName = this.selectedProduct;
         if (dataToSave.date && !dataToSave.date.includes('-')) {
-            dataToSave.date = (dataToSave.date).format('DD-MM-YYYY');
+            dataToSave.date = (dataToSave.date).format(GIDDH_DATE_FORMAT);
         }
         dataToSave.linkedStocks.forEach((obj) => {
             obj.manufacturingUnit = obj.stockUnitCode;
@@ -350,7 +351,7 @@ export class MfEditComponent implements OnInit {
     public updateEntry() {
         let dataToSave = _.cloneDeep(this.manufacturingDetails);
         if (dataToSave.date && !dataToSave.date.includes('-')) {
-            dataToSave.date = (dataToSave.date).format('DD-MM-YYYY');
+            dataToSave.date = (dataToSave.date).format(GIDDH_DATE_FORMAT);
         }
         // dataToSave.grandTotal = this.getTotal('otherExpenses', 'amount') + this.getTotal('linkedStocks', 'amount');
         // dataToSave.multipleOf = dataToSave.quantity;
