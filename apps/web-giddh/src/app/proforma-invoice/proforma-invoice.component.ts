e7202ad8b5c1c31e3d9495703289edb1630b6044
prateek-giddh <prateekmishra@giddh.com>
2020-05-19 13:21:30 +0530

    fix: Field not updating based on selection of country

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -1218,6 +1218,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
         this.store.pipe(select(s => s.common.onboardingform), takeUntil(this.destroyed$)).subscribe(res => {
             if (res) {
                 if (res.fields) {
+                    this.formFields = [];
                     Object.keys(res.fields).forEach(key => {
                         if (res.fields[key]) {
                             this.formFields[res.fields[key].name] = [];
