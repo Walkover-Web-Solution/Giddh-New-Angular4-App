bcd6869d30189fead39fdf25b53da8db96d3b3ec
ravindersingh <ravinder@walkover.in>
2020-05-28 12:02:08 +0530

    formatting changes

--- apps/web-giddh/src/app/userDetails/components/company-details-sidebar/company-details-sidebar.component.ts
+++ apps/web-giddh/src/app/userDetails/components/company-details-sidebar/company-details-sidebar.component.ts
@@ -2,21 +2,21 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import * as moment from 'moment';
 
 @Component({
-	selector: 'company-details-sidebar',
-	styleUrls: ['./company-details-sidebar.component.scss'],
-	templateUrl: './company-details-sidebar.component.html'
+    selector: 'company-details-sidebar',
+    styleUrls: ['./company-details-sidebar.component.scss'],
+    templateUrl: './company-details-sidebar.component.html'
 })
 
 export class CompanyDetailsSidebarComponent implements OnInit {
     @Input() public selectedCompany: any;
-	@Output() public closeEvent: EventEmitter<boolean> = new EventEmitter();
+    @Output() public closeEvent: EventEmitter<boolean> = new EventEmitter();
     public moment = moment;
 
-	constructor() {
+    constructor() {
 
     }
 
     public ngOnInit() {
-        
+
     }
 }
