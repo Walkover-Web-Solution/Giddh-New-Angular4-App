9651aa7b59302502643a4f92ddc2dd3a4950ee7a
ravindersingh <ravinder@walkover.in>
2020-06-03 14:29:22 +0530

    fixed issues

--- apps/web-giddh/src/app/mobile-home/mobile-home.component.ts
+++ apps/web-giddh/src/app/mobile-home/mobile-home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef, HostListener, OnDestroy, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { CommandKRequest } from '../models/api-models/Common';
-import { Subject, ReplaySubject } from 'rxjs';
+import { Subject, ReplaySubject, of as observableOf } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 import { AppState } from '../store';
 import { GeneralService } from '../services/general.service';
@@ -17,7 +17,7 @@ import { remove } from '../lodash-optimized';
 
 export class MobileHomeComponent implements OnInit, OnDestroy, AfterViewInit {
     @ViewChild('searchEle') public searchEle: ElementRef;
-    
+
     public allowLoadMore: boolean = false;
     public isLoading: boolean = false;
     public activeCompanyUniqueName: any = '';
@@ -28,13 +28,18 @@ export class MobileHomeComponent implements OnInit, OnDestroy, AfterViewInit {
         totalPages: 1
     };
     public visibleItems: number = 10;
-    private searchSubject: Subject<string> = new Subject();
-    private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
     public searchedItems: any[] = [];
     public listOfSelectedGroups: any[] = [];
     public noResultsFound: boolean = false;
+    public hasMenus: boolean = false;
+    public hasAccounts: boolean = false;
+    public hasGroups: boolean = false;
+    public companyInitials: any = '';
+    public searchString: any = "";
+    private searchSubject: Subject<string> = new Subject();
+    private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-    constructor(private store: Store<AppState>, private _generalService: GeneralService, private _commandKService: CommandKService, private _cdref: ChangeDetectorRef) {
+    constructor(private store: Store<AppState>, private generalService: GeneralService, private commandKService: CommandKService, private cdref: ChangeDetectorRef) {
         document.querySelector('body').classList.add('mobile-home');
 
         this.store.pipe(select(p => p.session.companyUniqueName), takeUntil(this.destroyed$)).subscribe(res => {
@@ -53,15 +58,41 @@ export class MobileHomeComponent implements OnInit, OnDestroy, AfterViewInit {
             this.commandKRequestParams.page = 1;
             this.commandKRequestParams.q = term;
             this.searchCommandK(true);
-            this._cdref.markForCheck();
+            this.cdref.markForCheck();
         });
 
         this.searchSubject.next("");
+
+        this.store.pipe(select((state: AppState) => state.session.companies), takeUntil(this.destroyed$)).subscribe(companies => {
+            if (companies) {
+                let selectedCmp = companies.find(cmp => {
+                    if (cmp && cmp.uniqueName) {
+                        return cmp.uniqueName === this.generalService.companyUniqueName;
+                    } else {
+                        return false;
+                    }
+                });
+
+                if (selectedCmp) {
+                    let selectedCompanyArray = selectedCmp.name.split(" ");
+                    let companyInitials = [];
+                    for (let loop = 0; loop < selectedCompanyArray.length; loop++) {
+                        if (loop <= 1) {
+                            companyInitials.push(selectedCompanyArray[loop][0]);
+                        } else {
+                            break;
+                        }
+                    }
+
+                    this.companyInitials = companyInitials.join(" ");
+                }
+            }
+        });
     }
 
     /**
-     * This function gets called after view initializes and will
-     * set focus in search box and will call function to adjust the width of container
+     * This function gets called after view initializes and will set focus in search box
+     * 
      * @memberof CommandKComponent
      */
     public ngAfterViewInit(): void {
@@ -100,13 +131,13 @@ export class MobileHomeComponent implements OnInit, OnDestroy, AfterViewInit {
         this.isLoading = true;
 
         if (this.listOfSelectedGroups && this.listOfSelectedGroups.length > 0) {
-            let lastGroup = this._generalService.getLastElement(this.listOfSelectedGroups);
+            let lastGroup = this.generalService.getLastElement(this.listOfSelectedGroups);
             this.commandKRequestParams.group = lastGroup.uniqueName;
         } else {
             this.commandKRequestParams.group = "";
         }
 
-        this._commandKService.searchCommandK(this.commandKRequestParams, this.activeCompanyUniqueName).subscribe((res) => {
+        this.commandKService.searchCommandK(this.commandKRequestParams, this.activeCompanyUniqueName).subscribe((res) => {
             this.isLoading = false;
 
             if (res && res.body && res.body.results && res.body.results.length > 0) {
@@ -114,17 +145,25 @@ export class MobileHomeComponent implements OnInit, OnDestroy, AfterViewInit {
                 res.body.results.forEach((key, index) => {
                     key.loop = length + index;
                     this.searchedItems.push(key);
+
+                    if (key.type === "MENU") {
+                        this.hasMenus = true;
+                    } else if (key.type === "GROUP") {
+                        this.hasGroups = true;
+                    } else if (key.type === "ACCOUNT") {
+                        this.hasAccounts = true;
+                    }
                 });
                 this.noResultsFound = false;
                 this.allowLoadMore = true;
                 this.commandKRequestParams.totalPages = res.body.totalPages;
-                this._cdref.detectChanges();
+                this.cdref.detectChanges();
             } else {
                 if (this.searchedItems.length === 0) {
                     this.noResultsFound = true;
                     this.allowLoadMore = false;
                 }
-                this._cdref.detectChanges();
+                this.cdref.detectChanges();
             }
         });
     }
@@ -250,5 +289,16 @@ export class MobileHomeComponent implements OnInit, OnDestroy, AfterViewInit {
             this.focusInSearchBox();
             this.searchCommandK(true);
         }
+    }
+
+    /**
+     * This will clear the search string
+     *
+     * @memberof MobileHomeComponent
+     */
+    public clearSearch(): void {
+        this.searchString = "";
+        this.searchEle.nativeElement.value = null;
+        this.searchSubject.next("");
     }
 }
