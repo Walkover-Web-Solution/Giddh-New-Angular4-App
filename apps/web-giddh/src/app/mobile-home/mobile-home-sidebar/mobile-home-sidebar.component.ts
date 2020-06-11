5c1548f11ff69373d45640e3b6d88540f1f6021b
shalinee-walkover <shalinee@msg91.com>
2020-06-11 15:17:19 +0530

    Design sideadbar and comany search page for mobile

---
+++ apps/web-giddh/src/app/mobile-home/mobile-home-sidebar/mobile-home-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, NgModule, OnDestroy, AfterViewInit } from '@angular/core';
+@Component({
+    selector: 'mobile-home-sidebar',
+    templateUrl: './mobile-home-sidebar.component.html',
+    styleUrls: ['./mobile-home-sidebar.component.scss']
+})
+export class MobileHomeSidebarComponent implements OnInit {
+
+    public companyInitials: any = '';
+    public ngOnInit(){
+    }
+}
