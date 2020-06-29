8bb486ada4ac5e7a1ad8f8d7b021972d2fead7ba
shalinee-walkover <shalinee@msg91.com>
2020-06-29 13:42:48 +0530

    added static code or create purchase order

---
+++ apps/web-giddh/src/app/purchase/create-purchase-order/create-purchase-order.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, ViewChild, ElementRef, TemplateRef } from '@angular/core';
+import {
+    BsDropdownDirective,
+    BsModalRef,
+    BsModalService,
+    ModalDirective,
+    ModalOptions,
+    TabsetComponent
+    , PopoverDirective
+} from 'ngx-bootstrap'
+import { GeneralService } from 'apps/web-giddh/src/app/services/general.service';
+@Component({
+    selector: 'create-purchase-order',
+    templateUrl: './create-purchase-order.component.html',
+    styleUrls: ['./create-purchase-order.component.scss']
+})
+export class CreatePurchaseOrderComponent implements OnInit {
+    public modelRef: BsModalRef;
+    public modalRef: BsModalRef;
+    public  isMulticurrencyAccount : true;
+    constructor(
+        private modalService: BsModalService,
+        private generalService: GeneralService,
+    ) { }
+    ngOnInit() {
+    }
+
+
+
+}
