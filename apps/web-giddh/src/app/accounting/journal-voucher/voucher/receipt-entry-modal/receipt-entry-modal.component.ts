3be4bf9c1da2fd45b7866daf0794c16d6ca43c53
Ravinder Singh <ravinderwalkover>
2020-07-08 09:49:33 +0530

    Resolved conflict

--- apps/web-giddh/src/app/accounting/journal-voucher/voucher/receipt-entry-modal/receipt-entry-modal.component.ts
+++ apps/web-giddh/src/app/accounting/journal-voucher/voucher/receipt-entry-modal/receipt-entry-modal.component.ts
@@ -8,6 +8,8 @@ import { takeUntil } from 'rxjs/operators';
 import { select, Store } from '@ngrx/store';
 import { AppState } from 'apps/web-giddh/src/app/store';
 import { ReplaySubject, of as observableOf, Observable } from 'rxjs';
+import { SalesService } from 'apps/web-giddh/src/app/services/sales.service';
+
 @Component({
     selector: 'receipt-entry',
     templateUrl: './receipt-entry-modal.component.html',
@@ -17,13 +19,15 @@ import { ReplaySubject, of as observableOf, Observable } from 'rxjs';
 export class ReceiptEntryModalComponent implements OnInit, OnDestroy {
     @Input() public transaction: any;
     @Input() public activeCompany: any;
-    @Input() public receiptEntries: any[] = [];
+    @Input() public voucherDate: any;
     @Output() public entriesList: EventEmitter<any> = new EventEmitter();
 
     @ViewChild('referenceType') public referenceType: ShSelectComponent;
 
+    public receiptEntries: any[] = [];
     public modalRef: BsModalRef;
-    public selectInvNoDateAmount: IOption[] = [];
+    public pendingInvoiceList: any[] = [];
+    public pendingInvoiceListSource$: Observable<IOption[]> = observableOf([]);
     public selectRef: IOption[] = [
         { label: "Receipt", value: "receipt" },
         { label: "Advance Receipt", value: "advanceReceipt" },
@@ -32,15 +36,27 @@ export class ReceiptEntryModalComponent implements OnInit, OnDestroy {
     public totalEntries: number = 0;
     public isValidForm: boolean = false;
     public amountErrorMessage: string = "Amount can't be greater than Credit Amount.";
+    public invoiceAmountErrorMessage: string = "Amount can't be greater than Invoice Balance Due.";
     private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
     public taxList: IOption[] = [];
     public taxListSource$: Observable<IOption[]> = observableOf([]);
+    public pendingInvoicesList: any[] = [];
+    public pendingInvoicesListParams: any = {
+        accountUniqueNames: [],
+        voucherType: "receipt"
+    };
 
-    constructor(private toaster: ToasterService, private settingsTaxesActions: SettingsTaxesActions, private store: Store<AppState>) {
+    constructor(private toaster: ToasterService, private settingsTaxesActions: SettingsTaxesActions, private store: Store<AppState>,
+        private salesService: SalesService) {
 
     }
 
     public ngOnInit(): void {
+        if (this.transaction && this.transaction.selectedAccount) {
+            this.pendingInvoicesListParams.accountUniqueNames.push(this.transaction.selectedAccount.UniqueName);
+            this.getInvoiceListForReceiptVoucher();
+        }
+
         if (this.activeCompany && this.activeCompany.countryV2 && this.activeCompany.countryV2.alpha2CountryCode) {
             this.getTaxList(this.activeCompany.countryV2.alpha2CountryCode);
         }
@@ -49,32 +65,48 @@ export class ReceiptEntryModalComponent implements OnInit, OnDestroy {
     }
 
     public addNewEntry(): void {
-        this.receiptEntries[this.totalEntries] = {
-            type: '',
-            note: '',
-            tax: '',
-            invoiceNo: '',
-            amount: 0
+        if (this.totalEntries === 0 || (this.receiptEntries[this.totalEntries - 1] && this.receiptEntries[this.totalEntries - 1] !== undefined && Number(this.receiptEntries[this.totalEntries - 1].amount) > 0)) {
+            this.receiptEntries[this.totalEntries] = {
+                type: '',
+                note: '',
+                tax: '',
+                invoiceNo: '',
+                amount: 0
+            }
+            this.totalEntries++;
         }
     }
 
-    public validateAmount(event: KeyboardEvent): void {
+    public validateAmount(event: KeyboardEvent, entry: any): void {
         if ((event.keyCode === 9 || event.keyCode === 13) && this.transaction && this.transaction.amount) {
-            let receiptTotal = 0;
+            this.validateEntry(entry);
+        }
+    }
 
-            this.receiptEntries.forEach(receipt => {
-                receiptTotal += parseFloat(receipt.amount);
-            });
+    public validateEntry(entry: any): void {
+        let receiptTotal = 0;
 
-            if (receiptTotal < this.transaction.amount) {
-                this.totalEntries++;
-                this.addNewEntry();
-            } else if (receiptTotal > this.transaction.amount) {
-                this.toaster.clearAllToaster();
-                this.toaster.errorToast(this.amountErrorMessage);
+        this.receiptEntries.forEach(receipt => {
+            receiptTotal += Number(receipt.amount);
+        });
+
+        if (receiptTotal < this.transaction.amount) {
+            if (entry.type === "againstReference") {
+                let invoiceBalanceDue = Number(this.pendingInvoiceList[entry.invoiceNo].balanceDue.amountForAccount);
+                if (invoiceBalanceDue >= entry.amount) {
+                    this.addNewEntry();
+                } else if (invoiceBalanceDue < entry.amount) {
+                    this.toaster.clearAllToaster();
+                    this.toaster.errorToast(this.invoiceAmountErrorMessage);
+                }
             } else {
-                this.isValidForm = true;
+                this.addNewEntry();
             }
+        } else if (receiptTotal > this.transaction.amount) {
+            this.toaster.clearAllToaster();
+            this.toaster.errorToast(this.amountErrorMessage);
+        } else {
+            this.isValidForm = true;
         }
     }
 
@@ -82,7 +114,7 @@ export class ReceiptEntryModalComponent implements OnInit, OnDestroy {
         let receiptTotal = 0;
 
         this.receiptEntries.forEach(receipt => {
-            receiptTotal += parseFloat(receipt.amount);
+            receiptTotal += Number(receipt.amount);
         });
 
         if (receiptTotal < this.transaction.amount) {
@@ -92,7 +124,8 @@ export class ReceiptEntryModalComponent implements OnInit, OnDestroy {
             this.toaster.clearAllToaster();
             this.toaster.errorToast(this.amountErrorMessage);
         } else {
-            this.entriesList.emit(this.receiptEntries);
+            this.transaction.voucherAdjustments = this.receiptEntries;
+            this.entriesList.emit(this.transaction);
         }
     }
 
@@ -112,5 +145,21 @@ export class ReceiptEntryModalComponent implements OnInit, OnDestroy {
     public ngOnDestroy(): void {
         this.destroyed$.next(true);
         this.destroyed$.complete();
+    }
+
+    public getInvoiceListForReceiptVoucher(): void {
+        this.salesService.getInvoiceListForReceiptVoucher(this.voucherDate, this.pendingInvoicesListParams).subscribe(response => {
+            if (response && response.status === "success" && response.body && response.body.length > 0) {
+                let pendingInvoiceList: IOption[] = [];
+
+                Object.keys(response.body).forEach(key => {
+                    this.pendingInvoiceList[response.body[key].invoiceUniqueName] = [];
+                    this.pendingInvoiceList[response.body[key].invoiceUniqueName] = response.body[key];
+
+                    pendingInvoiceList.push({ label: response.body[key].invoiceNumber + ", " + response.body[key].invoiceDate + ", " + response.body[key].balanceDue.amountForAccount + " cr.", value: response.body[key].invoiceUniqueName });
+                });
+                this.pendingInvoiceListSource$ = observableOf(pendingInvoiceList);
+            }
+        });
     }
 }
--- apps/web-giddh/src/app/services/apiurls/sales.api.ts
+++ apps/web-giddh/src/app/services/apiurls/sales.api.ts
@@ -4,7 +4,8 @@ export const SALES_API_V2 = {
     GENERATE_GENERIC_ITEMS: `company/:companyUniqueName/accounts/:accountUniqueName/vouchers/generate`,
     GET_VOUCHER_INVOICE_LIST: 'company/:companyUniqueName/vouchers/invoice-list?voucherDate=:voucherDate&adjustmentRequest=:adjustmentRequest',
     UPDATE_VOUCHER: URL + 'vouchers',
-    GET_ACCOUNTS_OF_GROUP_WITH_CURRENCY: 'company/:companyUniqueName/brief-accounts'
+    GET_ACCOUNTS_OF_GROUP_WITH_CURRENCY: 'company/:companyUniqueName/brief-accounts',
+    GET_INVOICE_LIST_FOR_RECEIPT_VOUCHER: '/company/:companyUniqueName/vouchers/invoice-list?voucherDate=:voucherDate'
 };
 export const SALES_API_V4 = {
     GENERATE_SALES: `v4/${URL}invoices/generate-sales`,
