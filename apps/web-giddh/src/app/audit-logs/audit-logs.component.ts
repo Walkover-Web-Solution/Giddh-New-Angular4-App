70a9b9054f8b4fb55a8d19de2c0924e7c73e6c7c
rohit-walkover <rohit@walkover.in>
2020-07-13 18:32:37 +0530

    Implemented separate module routing for new audit log

--- apps/web-giddh/src/app/audit-logs/audit-logs.component.ts
+++ apps/web-giddh/src/app/audit-logs/audit-logs.component.ts
@@ -5,18 +5,32 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { StateDetailsRequest } from '../models/api-models/Company';
 import { CompanyActions } from '../actions/company.actions';
 import { ReplaySubject } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'audit-logs',
     templateUrl: './audit-logs.component.html'
 })
 export class AuditLogsComponent implements OnInit, OnDestroy {
+    /** To check module for new version  */
+    public isNewVersion: boolean = false;
     private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-    constructor(private store: Store<AppState>, private companyActions: CompanyActions) {
+    constructor(private store: Store<AppState>, private companyActions: CompanyActions, private route: ActivatedRoute) {
     }
 
     public ngOnInit() {
+        this.route.params.subscribe(response => {
+            if (response) {
+                this.isNewVersion = false;
+                if (response.version && response.version && String(response.version).toLocaleLowerCase() === 'new') {
+                    this.isNewVersion = true;
+                }
+            } else {
+                this.isNewVersion = false;
+            }
+            console.log('params:', response);
+        });
         let companyUniqueName = null;
         this.store.select(c => c.session.companyUniqueName).pipe(take(1)).subscribe(s => companyUniqueName = s);
         let stateDetailsRequest = new StateDetailsRequest();
