beea6b71a48ef3412d3a6ec7bb82af9be7a7d5ee
rohit-walkover <rohit@walkover.in>
2020-07-15 16:02:00 +0530

    Custom sorting implemented for approver name in bank integration

--- apps/web-giddh/src/app/settings/integration/setting.integration.component.ts
+++ apps/web-giddh/src/app/settings/integration/setting.integration.component.ts
@@ -291,7 +291,9 @@ export class SettingIntegrationComponent implements OnInit, AfterViewInit {
                         });
                     });
                 }
-                this.addBankForm.reset();
+                if (this.addBankForm) {
+                    this.addBankForm.reset();
+                }
                 this.isBankUpdateInEdit = null;
             }
         });
@@ -311,7 +313,7 @@ export class SettingIntegrationComponent implements OnInit, AfterViewInit {
             this.inputMaskFormat = profile.balanceDisplayFormat ? profile.balanceDisplayFormat.toLowerCase() : '';
             if (profile && profile.countryV2 && profile.countryV2.alpha2CountryCode) {
                 this.isIndianCompany = profile.countryV2.alpha2CountryCode === 'IN' ? true : false;
-                if(!this.isIndianCompany && this.selectedTabParent === 4) {
+                if (!this.isIndianCompany && this.selectedTabParent === 4) {
                     this.selectedTabParent = 0;
                     this.selectTab(this.selectedTabParent);
                 }
@@ -654,7 +656,7 @@ export class SettingIntegrationComponent implements OnInit, AfterViewInit {
     }
 
     public selectTab(id: number) {
-        if(this.integrationTab.tabs[id] && this.integrationTab.tabs[id] !== undefined) {
+        if (this.integrationTab.tabs[id] && this.integrationTab.tabs[id] !== undefined) {
             this.integrationTab.tabs[id].active = true;
         }
     }
@@ -1277,5 +1279,17 @@ export class SettingIntegrationComponent implements OnInit, AfterViewInit {
         }
         this.isUpdateBankFormValid$ = of(valid);
         return valid;
+    }
+
+    /** Note We have to use param 'b' as this need mapping on sh-select custome filter params
+     * To apply custom sorting on approver list 
+     *
+     * @param {IOption} a Params to sort selected item
+     * @param {IOption} b Params to sort selected item
+     * @returns
+     * @memberof SettingIntegrationComponent
+     */
+    public customApproverNameListSorting(a: IOption, b: IOption) {
+        return (parseInt(a.label));
     }
 }
