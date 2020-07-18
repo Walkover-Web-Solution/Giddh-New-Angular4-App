1e3d7d4b2a8b0dcedb5186b3e61cb52c3d9506ed
govi2010 <govi2010@gmail.com>
2020-07-18 19:32:19 +0530

    commit
    remove redundant checks

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -4489,7 +4489,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
         if (this.invFormData.entries && this.invFormData.entries.length) {
             let validLineItem;
             for (let i = 0; i < this.invFormData.entries.length; i++) {
-                validLineItem = this.invFormData.entries[i].transactions.find(transaction => (transaction.accountUniqueName && transaction.accountUniqueName !== '' && transaction.amount > 0));
+                validLineItem = this.invFormData.entries[i].transactions.find(transaction => (transaction.accountUniqueName && transaction.amount > 0));
                 if (validLineItem) {
                     break;
                 }
