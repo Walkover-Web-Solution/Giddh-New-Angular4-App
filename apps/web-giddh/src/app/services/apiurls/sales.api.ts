19653588c93d364651abf6d86cd4fb908dcbae7d
prateek-giddh <prateekmishra@giddh.com>
2020-07-21 11:44:47 +0530

    fix: minor fixes and refactoring

--- apps/web-giddh/src/app/services/apiurls/sales.api.ts
+++ apps/web-giddh/src/app/services/apiurls/sales.api.ts
@@ -2,7 +2,7 @@ let URL = 'company/:companyUniqueName/accounts/:accountUniqueName/';
 export const SALES_API_V2 = {
     GENERATE_SALES: `v2/${URL}invoices/generate-sales`,
     GENERATE_GENERIC_ITEMS: `company/:companyUniqueName/accounts/:accountUniqueName/vouchers/generate`,
-    GET_VOUCHER_INVOICE_LIST: 'company/:companyUniqueName/vouchers/invoice-list?voucherDate=:voucherDate',
+    GET_VOUCHER_INVOICE_LIST: 'company/:companyUniqueName/vouchers/invoice-list?voucherDate=:voucherDate&adjustmentRequest=:adjustmentRequest',
     UPDATE_VOUCHER: URL + 'vouchers'
 };
 export const SALES_API_V4 = {
