b91ddb2b901b890b3512728e9a8cc35d5ea9eb97
prateek-giddh <prateekmishra@giddh.com>
2020-07-22 16:06:37 +0530

    refactor: Resolve code review comments

--- apps/web-giddh/src/app/ledger/components/newLedgerEntryPanel/newLedgerEntryPanel.component.ts
+++ apps/web-giddh/src/app/ledger/components/newLedgerEntryPanel/newLedgerEntryPanel.component.ts
@@ -1217,7 +1217,7 @@ export class NewLedgerEntryPanelComponent implements OnInit, OnDestroy, OnChange
             } else {
                 this.isAdjustReceiptSelected = false;
             }
-            this.isAdjustVoucherSelected = false
+            this.isAdjustVoucherSelected = false;
             this.blankLedger.generateInvoice = false;
         }
         this.adjustPaymentModal.hide();
