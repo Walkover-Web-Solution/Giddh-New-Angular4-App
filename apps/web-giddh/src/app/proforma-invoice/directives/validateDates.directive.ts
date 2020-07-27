f5dc93a9c5ef08b0d7fd854b5d4cabf13eb3d6bf
govi2010 <govi2010@gmail.com>
2020-07-27 12:03:21 +0530

    Yogesh | GIDK-757_date_validations | GIDK-757 | pending (design and testing)

--- apps/web-giddh/src/app/proforma-invoice/directives/validateDates.directive.ts
+++ apps/web-giddh/src/app/proforma-invoice/directives/validateDates.directive.ts
@@ -22,41 +22,45 @@ export function validateDate(invoiceDate: string, invoiceDueDate: string, vouche
         const entryLength = parseInt(voucherItems);
         const type = invoiceType as VoucherTypeEnum;
         // return null if controls haven't initialised yet
-        if (!invoiceDateControl || !invoiceDueDateControl) {
+        if ((!invoiceDateControl)) {
             return null;
         }
-
+        if (!invoiceDueDateControl && (type === VoucherTypeEnum.purchase || type === VoucherTypeEnum.sales ||
+                type === VoucherTypeEnum.generateProforma || type === VoucherTypeEnum.generateEstimate)) {
+            return null
+        }
         // return null if another validator has already found an error on the invoiceDueDateControl
-        if (invoiceDueDateControl.errors && !invoiceDueDateControl.errors.mustGrater) {
+        if (invoiceDueDateControl && invoiceDueDateControl.errors && !invoiceDueDateControl.errors.mustGrater) {
             return null;
         }
 
+        // set errors on entry dates
         if (entryLength && !isNaN(entryLength)) {
             let invalidIndex: number[] = [];
-                if( type === VoucherTypeEnum.generateProforma ||
-                    type === VoucherTypeEnum.generateEstimate ||
-                    type === VoucherTypeEnum.sales ||
-                    type === VoucherTypeEnum.cash ||
-                    type === VoucherTypeEnum.creditNote ||
-                    type === VoucherTypeEnum.debitNote ) {
-                    for (let i = 0; i < entryLength; i++) {
-                        if (formGroup.controls['transaction.date_' + i.toString()]) {
-                            const entryDate = formGroup.controls['transaction.date_' + i.toString()].value;
-                            if (moment(invoiceDateControl.value).isBefore(moment(entryDate))) {
-                                invalidIndex = [...invalidIndex, i];
-                            }
-                        }
-                    }
-                } else if( type === VoucherTypeEnum.purchase) {
-                    for (let i = 0; i < entryLength; i++) {
-                        if (formGroup.controls['transaction.date_' + i.toString()]) {
-                            const entryDate = formGroup.controls['transaction.date_' + i.toString()].value;
-                            if ((moment(invoiceDateControl.value).get('month') !== moment(entryDate).get('month')) || (moment(invoiceDateControl.value).get('year') !== moment(entryDate).get('year'))) {
-                                invalidIndex = [...invalidIndex, i];
-                            }
+            if (type === VoucherTypeEnum.generateProforma ||
+                type === VoucherTypeEnum.generateEstimate ||
+                type === VoucherTypeEnum.sales ||
+                type === VoucherTypeEnum.cash ||
+                type === VoucherTypeEnum.creditNote ||
+                type === VoucherTypeEnum.debitNote) {
+                for (let i = 0; i < entryLength; i++) {
+                    if (formGroup.controls['transaction.date_' + i.toString()]) {
+                        const entryDate = formGroup.controls['transaction.date_' + i.toString()].value;
+                        if (moment(invoiceDateControl.value).isBefore(moment(entryDate))) {
+                            invalidIndex = [...invalidIndex, i];
                         }
                     }
                 }
+            } else if (type === VoucherTypeEnum.purchase) {
+                for (let i = 0; i < entryLength; i++) {
+                    if (formGroup.controls['transaction.date_' + i.toString()]) {
+                        const entryDate = formGroup.controls['transaction.date_' + i.toString()].value;
+                        if ((moment(invoiceDateControl.value).get('month') !== moment(entryDate).get('month')) || (moment(invoiceDateControl.value).get('year') !== moment(entryDate).get('year'))) {
+                            invalidIndex = [...invalidIndex, i];
+                        }
+                    }
+                }
+            }
             if (invalidIndex && invalidIndex.length) {
                 invalidIndex.forEach(index => {
                     if (type !== VoucherTypeEnum.purchase) {
@@ -74,10 +78,12 @@ export function validateDate(invoiceDate: string, invoiceDueDate: string, vouche
             }
         }
         // set error on invoiceDueDateControl if validation fails
-        if (moment(invoiceDateControl.value).isSameOrBefore(moment(invoiceDueDateControl.value), 'day')) {
-            invoiceDueDateControl.setErrors(null);
-        } else {
-            invoiceDueDateControl.setErrors({mustGrater: true});
+        if(invoiceDueDateControl && invoiceDueDateControl.value) {
+            if (moment(invoiceDateControl.value).isSameOrBefore(moment(invoiceDueDateControl.value), 'day')) {
+                invoiceDueDateControl.setErrors(null);
+            } else {
+                invoiceDueDateControl.setErrors({mustGrater: true});
+            }
         }
     }
 }
