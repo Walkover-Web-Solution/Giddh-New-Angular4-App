461f67bd9c59d79cd7cbd405a44a55ebf60f040a
Ravinder Singh <ravinderwalkover>
2020-08-05 12:15:04 +0530

    Working on PO Preview

---
+++ apps/web-giddh/src/app/purchase/purchase-order-preview/purchase-order-preview.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, TemplateRef, Input, Output, EventEmitter } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap'
+
+@Component({
+    selector: 'purchase-order-preview',
+    templateUrl: './purchase-order-preview.component.html',
+    styleUrls: ['./purchase-order-preview.component.scss']
+})
+
+export class PurchaseOrderPreviewComponent implements OnInit {
+    @Input() public purchaseOrders: any;
+    @Output() public hidePreview: EventEmitter<boolean> = new EventEmitter();
+
+    public modalRef: BsModalRef;
+    public orderHistoryAsideState: string = 'out';
+
+    constructor(private modalService: BsModalService) {
+
+    }
+
+    public ngOnInit(): void {
+
+    }
+
+    public toggleOrderHistoryAsidePane(event?: any): void {
+        if (event) {
+            event.preventDefault();
+        }
+        this.orderHistoryAsideState = this.orderHistoryAsideState === 'out' ? 'in' : 'out';
+        this.toggleBodyClass();
+    }
+
+    public toggleBodyClass(): void {
+        if (this.orderHistoryAsideState === 'in') {
+            document.querySelector('body').classList.add('fixed');
+        } else {
+            document.querySelector('body').classList.remove('fixed');
+        }
+    }
+
+    public openModal(template: TemplateRef<any>): void {
+        this.modalRef = this.modalService.show(template);
+    }
+
+    public closePreview(): void {
+        this.hidePreview.emit(true);
+    }
+
+}
--- apps/web-giddh/src/app/services/apiurls/purchase-order.api.ts
+++ apps/web-giddh/src/app/services/apiurls/purchase-order.api.ts
@@ -2,6 +2,7 @@
 export const PURCHASE_ORDER = 'company/:companyUniqueName/';
 /** APIs for purchase order */
 export const PURCHASE_ORDER_API = {
-    GET_ALL: `${PURCHASE_ORDER}purchase-order/get-all?from=:from&to=:to&page=:page&count=:count&q=:q&sort=:sort&sortBy=:sortBy`,
-    CREATE: `${PURCHASE_ORDER}accounts/:accountUniqueName/purchase-order/generate`
+    GET_ALL: `${PURCHASE_ORDER}purchase-order/get-all?from=:from&to=:to&page=:page&count=:count&vendorName=:q&sort=:sort&sortBy=:sortBy`,
+    CREATE: `${PURCHASE_ORDER}accounts/:accountUniqueName/purchase-order/generate`,
+    GET: `${PURCHASE_ORDER}accounts/:accountUniqueName/purchase-order/:purchaseOrderUniqueName`
 }
