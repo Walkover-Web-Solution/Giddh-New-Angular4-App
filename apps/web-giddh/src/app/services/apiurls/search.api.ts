15a667e33e0d0f8d3bcec2a06878e4f5492b426e
prateek-giddh <prateekmishra@giddh.com>
2020-08-08 01:01:23 +0530

    feat: Move API URL path TEST

--- apps/web-giddh/src/app/services/apiurls/search.api.ts
+++ apps/web-giddh/src/app/services/apiurls/search.api.ts
@@ -1,4 +1,5 @@
 export const SEARCH_API = {
 	// SEARCH: 'company/:companyUniqueName/groups/:groupName/closing-balance', // get call
-	SEARCH: 'v2/company/:companyUniqueName/groups/:groupName/account-closing-balance?count=:count&from=:from&to=:to&refresh=:refresh&page=:page', // POST call
+    SEARCH: 'v2/company/:companyUniqueName/groups/:groupName/account-closing-balance?count=:count&from=:from&to=:to&refresh=:refresh&page=:page', // POST call
+    ACCOUNT_SEARCH: 'company/:companyUniqueName/account-search'
 };
