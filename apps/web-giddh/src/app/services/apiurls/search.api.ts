18d148a111816a75647f721eac3ced629025bd1c
prateek-giddh <prateekmishra@giddh.com>
2020-08-12 20:12:26 +0530

    feat: Integrate new detail API in ledger CREATE/UPDATE

--- apps/web-giddh/src/app/services/apiurls/search.api.ts
+++ apps/web-giddh/src/app/services/apiurls/search.api.ts
@@ -2,5 +2,5 @@ export const SEARCH_API = {
 	// SEARCH: 'company/:companyUniqueName/groups/:groupName/closing-balance', // get call
     SEARCH: 'v2/company/:companyUniqueName/groups/:groupName/account-closing-balance?count=:count&from=:from&to=:to&refresh=:refresh&page=:page', // POST call
     ACCOUNT_SEARCH: 'company/:companyUniqueName/account-search',
-    ACCOUNT_DETAIL: 'company/:companyUniqueName/accounts/:accountUniqueName',
+    ACCOUNT_DETAIL: 'v2/company/:companyUniqueName/particular/:accountUniqueName',
 };
