92b6f2a789c4c0d4eff1df8a63d25d2e4ceeea8a
Ravinder Singh <ravinderwalkover>
2020-08-21 11:14:57 +0530

    Added JS Docs

--- apps/web-giddh/src/app/purchase/purchase-setting/purchase-setting.component.ts
+++ apps/web-giddh/src/app/purchase/purchase-setting/purchase-setting.component.ts
@@ -11,7 +11,7 @@ import { InvoiceService } from '../../services/invoice.service';
 import { PurchaseOrderService } from '../../services/purchase-order.service';
 import { GeneralService } from '../../services/general.service';
 import { AuthenticationService } from '../../services/authentication.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'purchase-setting',
@@ -20,18 +20,25 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 
 export class PurchaseSettingComponent implements OnInit, OnDestroy {
-    public isInvalidfield: boolean = true;
-    public isMulticurrencyAccount: true;
+    /* This will hold the invoice settings */
     public invoiceSettings: any = { purchaseBillSettings: {} };
+    /* This will hold the PB lock date */
     public lockDate: Date = new Date();
+    /* This will hold if email updated */
     public isEmailChanged: boolean = false;
+    /* This will hold the original email*/
     public originalEmail: string;
+    /* This will hold if gmail is integrated */
     public isGmailIntegrated: boolean;
     /* Active company unique name */
     public activeCompanyUniqueName$: Observable<string>;
+    /* This will hold company uniquename */
     public companyUniqueName: any;
+    /* Observable for gmail auth code url */
     public gmailAuthCodeUrl$: Observable<string> = null;
+    /* This holds gmail auth code url */
     private gmailAuthCodeStaticUrl: string = 'https://accounts.google.com/o/oauth2/auth?redirect_uri=:redirect_url&response_type=code&client_id=:client_id&scope=https://www.googleapis.com/auth/gmail.send&approval_prompt=force&access_type=offline';
+    /* Observable to unsubscribe all the store listeners to avoid memory leaks */
     private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
     constructor(private store: Store<AppState>, private toaster: ToasterService, private settingsIntegrationActions: SettingsIntegrationActions, private invoiceService: InvoiceService, public purchaseOrderService: PurchaseOrderService, private generalService: GeneralService, public authenticationService: AuthenticationService, private route: ActivatedRoute) {
@@ -41,6 +48,11 @@ export class PurchaseSettingComponent implements OnInit, OnDestroy {
         this.gmailAuthCodeUrl$ = observableOf(this.gmailAuthCodeStaticUrl);
     }
 
+    /**
+     * Initializes the component
+     *
+     * @memberof PurchaseSettingComponent
+     */
     public ngOnInit(): void {
         this.activeCompanyUniqueName$.subscribe(response => {
             this.companyUniqueName = response;
@@ -56,15 +68,27 @@ export class PurchaseSettingComponent implements OnInit, OnDestroy {
         this.initSettings();
     }
 
+    /**
+     * Releases the memory
+     *
+     * @memberof PurchaseSettingComponent
+     */
     public ngOnDestroy(): void {
         this.destroyed$.next(true);
         this.destroyed$.complete();
     }
 
+    /**
+     * This initializes the settings
+     *
+     * @memberof PurchaseSettingComponent
+     */
     public initSettings(): void {
         this.invoiceService.GetInvoiceSetting().subscribe(response => {
             if (response && response.status === "success" && response.body) {
                 this.invoiceSettings = _.cloneDeep(response.body);
+
+                this.originalEmail = _.cloneDeep(this.invoiceSettings.purchaseBillSettings.email);
 
                 if (this.invoiceSettings.purchaseBillSettings.lockDate) {
                     this.lockDate = moment(this.invoiceSettings.purchaseBillSettings.lockDate, GIDDH_DATE_FORMAT).toDate();
@@ -77,10 +101,20 @@ export class PurchaseSettingComponent implements OnInit, OnDestroy {
         });
     }
 
+    /**
+     * This will reset the form with default settings
+     *
+     * @memberof PurchaseSettingComponent
+     */
     public resetForm(): void {
         this.initSettings();
     }
 
+    /**
+     * This will update the form settings
+     *
+     * @memberof PurchaseSettingComponent
+     */
     public updateForm(): void {
         let formToSave = _.cloneDeep(this.invoiceSettings);
 
@@ -97,6 +131,13 @@ export class PurchaseSettingComponent implements OnInit, OnDestroy {
         });
     }
 
+    /**
+     * This will verify the email and will save the email
+     *
+     * @param {*} emailId
+     * @returns {boolean}
+     * @memberof PurchaseSettingComponent
+     */
     public verifyEmail(emailId: any): boolean {
         let email = new RegExp(/[a-z0-9!#$%&'*+=?^_{|}~-]+(?:.[a-z0-9!#$%&’*+=?^_{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g);
         if (email.test(emailId)) {
@@ -108,6 +149,13 @@ export class PurchaseSettingComponent implements OnInit, OnDestroy {
         }
     }
 
+    /**
+     * This will delete the email
+     *
+     * @param {*} emailId
+     * @returns {boolean}
+     * @memberof PurchaseSettingComponent
+     */
     public deleteEmail(emailId: any): boolean {
         if (!emailId) {
             return false;
@@ -117,10 +165,22 @@ export class PurchaseSettingComponent implements OnInit, OnDestroy {
         }
     }
 
+    /**
+     * This will update the variable to hold if email changed
+     *
+     * @param {string} email
+     * @memberof PurchaseSettingComponent
+     */
     public onChangeEmail(email: string): void {
         this.isEmailChanged = email !== this.originalEmail;
     }
 
+    /**
+     * This will update the settings email
+     *
+     * @param {*} emailAddress
+     * @memberof PurchaseSettingComponent
+     */
     public updateSettingsEmail(emailAddress: any): void {
         let getRequestObject = {
             companyUniqueName: this.companyUniqueName
@@ -140,6 +200,13 @@ export class PurchaseSettingComponent implements OnInit, OnDestroy {
         });
     }
 
+    /**
+     * This will update the gmail integration
+     *
+     * @private
+     * @param {string} authCode
+     * @memberof PurchaseSettingComponent
+     */
     private saveGmailAuthCode(authCode: string): void {
         const dataToSave = {
             code: authCode,
@@ -159,6 +226,12 @@ export class PurchaseSettingComponent implements OnInit, OnDestroy {
         });
     }
 
+    /**
+     * This will return page url
+     *
+     * @returns {string}
+     * @memberof PurchaseSettingComponent
+     */
     public getRedirectUrl(): string {
         return AppUrl + 'pages/purchase-management/purchase-settings';
     }
