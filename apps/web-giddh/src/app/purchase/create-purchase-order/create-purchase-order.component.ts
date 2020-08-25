e5390431438a43d684b0aa75e233bd6163b5d09b
Ravinder Singh <ravinderwalkover>
2020-08-25 19:23:06 +0530

    Fixed PR Issue

--- apps/web-giddh/src/app/purchase/create-purchase-order/create-purchase-order.component.ts
+++ apps/web-giddh/src/app/purchase/create-purchase-order/create-purchase-order.component.ts
@@ -2441,7 +2441,7 @@ export class CreatePurchaseOrderComponent implements OnInit, OnDestroy {
                     this.purchaseOrderDetails = response.body;
                     let entriesUpdated = false;
 
-                    if (this.purchaseOrderDetails.roundOffTotal && this.purchaseOrderDetails.roundOffTotal.amountForAccount === 0 && this.purchaseOrderDetails.roundOffTotal.amountForCompany === 0) {
+                    if (this.purchaseOrderDetails && this.purchaseOrderDetails.roundOffTotal && this.purchaseOrderDetails.roundOffTotal.amountForAccount === 0 && this.purchaseOrderDetails.roundOffTotal.amountForCompany === 0) {
                         this.applyRoundOff = false;
                     }
 
