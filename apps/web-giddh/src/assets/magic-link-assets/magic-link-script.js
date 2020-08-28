d9de731399f00df20b81a533b2dbacfa0f75043f
prateek-giddh <prateekmishra@giddh.com>
2020-08-28 11:29:56 +0530

    feat: Point to PROD

--- apps/web-giddh/src/assets/magic-link-assets/magic-link-script.js
+++ apps/web-giddh/src/assets/magic-link-assets/magic-link-script.js
@@ -194,7 +194,7 @@ var app = new Vue({
                         if((from && to) || (e && e.response && e.response.data && e.response.data.code !== "NOT_FOUND")) {
                             this.$toaster.error(msg);
                         }
-                    });    
+                    });
             } else {
                 this.$toaster.error('Magic link ID not found.');
             }
@@ -367,14 +367,14 @@ var app = new Vue({
                 case 'dev.giddh.com':
                 case 'test.giddh.com':
                 case 'stage.giddh.com':
-                    apiBaseUrl = 'https://apitest.giddh.com/';
+                    apiBaseUrl = 'https://api.giddh.com/';
                     break;
                 case 'giddh.com':
                 case 'app.giddh.com':
                     apiBaseUrl = 'https://api.giddh.com/';
                     break;
                 default:
-                    apiBaseUrl = 'https://apitest.giddh.com/';
+                    apiBaseUrl = 'https://api.giddh.com/';
             }
             return apiBaseUrl;
         }
