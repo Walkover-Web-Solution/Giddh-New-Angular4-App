cb7256f5f59ef4f778ca72905619a935eb9bb89d
Ravinder Singh <ravinderwalkover>
2020-09-01 13:28:47 +0530

    Ravinder | Development | Completed revision history

--- apps/web-giddh/src/app/purchase/revision-history/revision-history.component.ts
+++ apps/web-giddh/src/app/purchase/revision-history/revision-history.component.ts
@@ -3,14 +3,14 @@ import { PurchaseOrderService } from '../../services/purchase-order.service';
 import { ToasterService } from '../../services/toaster.service';
 import { PAGINATION_LIMIT } from '../../app.constant';
 import { GeneralService } from '../../services/general.service';
-import { GIDDH_DATE_FORMAT } from '../../shared/helpers/defaultDateFormat';
 
 @Component({
-    selector: 'aside-order-history',
-    templateUrl: './order-history.component.html',
-    styleUrls: ['./order-history.component.scss']
+    selector: 'aside-revision-history',
+    templateUrl: './revision-history.component.html',
+    styleUrls: ['./revision-history.component.scss']
 })
-export class OrderHistoryComponent implements OnInit {
+
+export class RevisionHistoryComponent implements OnInit {
     /* Taking PO details as input */
     @Input() public purchaseOrder: any;
     /* Emitting the close popup event */
@@ -28,28 +28,46 @@ export class OrderHistoryComponent implements OnInit {
     };
     /* This will hold purchase order versions */
     public purchaseOrderVersions: any = {};
-    /* This will hold giddh date format */
-    public giddhDateFormat: string = GIDDH_DATE_FORMAT;
 
     constructor(public purchaseOrderService: PurchaseOrderService, private toaster: ToasterService, private generalService: GeneralService) {
 
     }
 
+    /**
+     * Initializes the component
+     *
+     * @memberof RevisionHistoryComponent
+     */
     public ngOnInit(): void {
         this.purchaseOrderVersionsGetRequest.companyUniqueName = this.purchaseOrder.company.uniqueName;
         this.purchaseOrderVersionsGetRequest.accountUniqueName = this.purchaseOrder.account.uniqueName;
         this.purchaseOrderVersionsPostRequest.purchaseNumber = this.purchaseOrder.number;
 
-        this.getPurchaseOrderVersions();
+        this.getPurchaseOrderVersions(true);
     }
 
+    /**
+     * This will send the event to close the aside pan
+     *
+     * @memberof RevisionHistoryComponent
+     */
     public closeAsidePane(): void {
         this.closeAsideEvent.emit(true);
     }
 
-    public getPurchaseOrderVersions(): void {
+    /**
+     * This will get the revision history
+     *
+     * @param {boolean} resetPage
+     * @memberof RevisionHistoryComponent
+     */
+    public getPurchaseOrderVersions(resetPage: boolean): void {
         if (this.purchaseOrderVersionsGetRequest.companyUniqueName && this.purchaseOrderVersionsPostRequest.purchaseNumber) {
             this.purchaseOrderVersions = {};
+
+            if (resetPage) {
+                this.purchaseOrderVersionsGetRequest.page = 1;
+            }
 
             this.purchaseOrderService.getAllVersions(this.purchaseOrderVersionsGetRequest, this.purchaseOrderVersionsPostRequest).subscribe((res) => {
                 if (res) {
@@ -78,6 +96,14 @@ export class OrderHistoryComponent implements OnInit {
         }
     }
 
+    /**
+     * This will return the version message based on change
+     *
+     * @param {string} type
+     * @param {*} change
+     * @returns {string}
+     * @memberof RevisionHistoryComponent
+     */
     public getVersionMessage(type: string, change: any): string {
         let message = "";
         let revisionField = this.generalService.getRevisionField(change.type);
@@ -93,5 +119,18 @@ export class OrderHistoryComponent implements OnInit {
         }
 
         return message;
+    }
+
+    /**
+     * Callback for page change event
+     *
+     * @param {*} event
+     * @memberof RevisionHistoryComponent
+     */
+    public pageChanged(event: any): void {
+        if (this.purchaseOrderVersionsGetRequest.page !== event.page) {
+            this.purchaseOrderVersionsGetRequest.page = event.page;
+            this.getPurchaseOrderVersions(false);
+        }
     }
 }
