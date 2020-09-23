6ab93332669ded788f65be66ff0c9d8694e35873
prateek-giddh <prateekmishra@giddh.com>
2020-09-23 16:08:54 +0530

    fix: Remove min character check

--- apps/web-giddh/src/app/theme/ng-virtual-select/sh-select.component.ts
+++ apps/web-giddh/src/app/theme/ng-virtual-select/sh-select.component.ts
@@ -466,7 +466,7 @@ export class ShSelectComponent implements ControlValueAccessor, OnInit, AfterVie
             this.stopDynamicSearch$ = new ReplaySubject(1);
             this.dynamicSearchQueryChanged = new Subject();
             this.dynamicSearchQueryChanged.pipe(debounceTime(700), distinctUntilChanged(), takeUntil(this.stopDynamicSearch$)).subscribe((query: string) => {
-                if (query && query.length > 1) {
+                if (query) {
                     this.dynamicSearchedQuery.emit(query);
                 }
             });
@@ -508,7 +508,7 @@ export class ShSelectComponent implements ControlValueAccessor, OnInit, AfterVie
                 this.placeholder = changes.placeholder.currentValue;
             }
         }
-        
+
         if ('options' in changes) {
             if (changes.options && changes.options.currentValue) {
                 this.refreshList();
