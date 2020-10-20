17fa6d84cf60c40935656c93e9fba2435bb5cf63
ravinderwalkover <ravinder@walkover.in>
2020-10-20 18:42:38 +0530

    Ravinder | Hotfix - Vendor sorting issue after bulk payment

--- apps/web-giddh/src/app/contact/contact.component.ts
+++ apps/web-giddh/src/app/contact/contact.component.ts
@@ -1261,12 +1261,11 @@ export class ContactComponent implements OnInit, OnDestroy, OnChanges {
     public closeBulkPaymentModel(event: any): void {
         //  if bulk paymemt success then clear all selected contacts lists
         if (event) {
-            this.clearSelectedContacts();
+            this.clearSelectedContacts(false);
         }
         this.isBulkPaymentShow = false;
         this.selectedAccForPayment = null;
         this.bulkPaymentModalRef.hide();
-        this.getAccounts(this.fromDate, this.toDate, this.activeTab === 'customer' ? 'sundrydebtors' : 'sundrycreditors', this.checkboxInfo.selectedPage, 'true', PAGINATION_LIMIT, this.searchStr);
     }
 
     /**
@@ -1358,14 +1357,16 @@ export class ContactComponent implements OnInit, OnDestroy, OnChanges {
      *
      * @memberof ContactComponent
      */
-    public clearSelectedContacts(): void {
+    public clearSelectedContacts(resetPage:boolean = true): void {
         this.searchStr = '';
         this.selectedCheckedContacts = [];
         this.selectedAccountsList = [];
         this.allSelectionModel = false;
-        this.checkboxInfo = {
-            selectedPage: 1
-        };
-        this.getAccounts(this.fromDate, this.toDate, this.activeTab === 'customer' ? 'sundrydebtors' : 'sundrycreditors', this.checkboxInfo.selectedPage, 'true', PAGINATION_LIMIT, this.searchStr);
+        if(resetPage) {
+            this.checkboxInfo = {
+                selectedPage: 1
+            };
+        }
+        this.getAccounts(this.fromDate, this.toDate, this.activeTab === 'customer' ? 'sundrydebtors' : 'sundrycreditors', this.checkboxInfo.selectedPage, 'true', PAGINATION_LIMIT, this.searchStr, this.key, this.order);
     }
 }
