12b620e8e553470e6e2fa5525ad4b2797a4b3c14
rohit-walkover <rohit@walkover.in>
2020-11-04 18:52:57 +0530

    PR issues resolved

--- apps/web-giddh/src/app/shared/header/header.component.ts
+++ apps/web-giddh/src/app/shared/header/header.component.ts
@@ -2004,7 +2004,7 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
      * @memberof HeaderComponent
      */
     private getElectronMacAppVersion(): void {
-        this.authService.GetElectronMacAppVersion().subscribe((res: string) => {
+        this.authService.getElectronMacAppVersion().subscribe((res: string) => {
             if (res && typeof res === 'string') {
                 let version = res.split('files')[0];
                 let versNum = version.split(' ')[1];
