be9b6b0f23a2bf8230f1c8bba22a55776266fc32
Ravinder Singh <ravinderwalkover>
2020-11-06 13:36:59 +0530

    Ravinder | Fixed Search Retain issue

--- apps/web-giddh/src/app/store/purchase-order/purchase-order.reducer.ts
+++ apps/web-giddh/src/app/store/purchase-order/purchase-order.reducer.ts
@@ -4,17 +4,21 @@ import { PURCHASE_ORDER_ACTIONS } from '../../actions/purchase-order/purchase-or
 export interface PurchaseOrderState {
     listFilters: any;
     selectedPo: [];
+    poSearch: any;
 }
 
 export const initialState: PurchaseOrderState = {
     listFilters: null,
-    selectedPo: null
+    selectedPo: null,
+    poSearch: null
 }
 
 export function purchaseOrderReducer(state: PurchaseOrderState = initialState, action: CustomActions): PurchaseOrderState {
     switch (action.type) {
         case PURCHASE_ORDER_ACTIONS.SET_FILTERS:
             return { ...state, listFilters: {getRequest: action.payload.getRequest, postRequest: action.payload.postRequest}, selectedPo: action.payload.selectedPo };
+            case PURCHASE_ORDER_ACTIONS.PO_PREVIEW_SEARCH:
+                return { ...state, poSearch: action.payload };    
         default: return state;
     }
 }
