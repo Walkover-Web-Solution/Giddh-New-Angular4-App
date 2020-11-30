d306344b583a71d586937d03fd25ecd23197057c
rohit-walkover <rohit@walkover.in>
2020-11-30 17:35:11 +0530

    New datepicker implemented on import company daybook modules

--- apps/web-giddh/src/app/companyImportExport/component/form/company-import-export-form.ts
+++ apps/web-giddh/src/app/companyImportExport/component/form/company-import-export-form.ts
@@ -1,12 +1,16 @@
-import { takeUntil } from 'rxjs/operators';
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { IOption } from '../../../theme/ng-select/option.interface';
 import * as moment from 'moment';
 import { CompanyImportExportFileTypes } from '../../../models/interfaces/companyImportExport.interface';
 import { AppState } from '../../../store';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { CompanyImportExportActions } from '../../../actions/company-import-export/company-import-export.actions';
 import { Observable, ReplaySubject } from 'rxjs';
+import { GIDDH_DATE_FORMAT, GIDDH_NEW_DATE_FORMAT_UI } from '../../../shared/helpers/defaultDateFormat';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { GIDDH_DATE_RANGE_PICKER_RANGES } from '../../../app.constant';
+import { GeneralService } from '../../../services/general.service';
 
 @Component({
 	selector: 'company-import-export-form-component',
@@ -22,56 +26,47 @@ export class CompanyImportExportFormComponent implements OnInit, OnDestroy {
 		{ label: 'Master Except Accounting Entries', value: CompanyImportExportFileTypes.MASTER_EXCEPT_ACCOUNTS.toString() },
 	];
 	public fileType: string = '';
-	public datePickerOptions: any = {
-		locale: {
-			applyClass: 'btn-green',
-			applyLabel: 'Go',
-			fromLabel: 'From',
-			format: 'D-MMM-YY',
-			toLabel: 'To',
-			cancelLabel: 'Cancel',
-			customRangeLabel: 'Custom range'
-		},
-		ranges: {
-			'Last 1 Day': [
-				moment().subtract(1, 'days'),
-				moment()
-			],
-			'Last 7 Days': [
-				moment().subtract(6, 'days'),
-				moment()
-			],
-			'Last 30 Days': [
-				moment().subtract(29, 'days'),
-				moment()
-			],
-			'Last 6 Months': [
-				moment().subtract(6, 'months'),
-				moment()
-			],
-			'Last 1 Year': [
-				moment().subtract(12, 'months'),
-				moment()
-			]
-		},
-		startDate: moment().subtract(30, 'days'),
-		endDate: moment()
-	};
-	public from: string = moment().subtract(30, 'days').format('DD-MM-YYYY');
-	public to: string = moment().format('DD-MM-YYYY');
+	public from: string = moment().subtract(30, 'days').format(GIDDH_DATE_FORMAT);
+	public to: string = moment().format(GIDDH_DATE_FORMAT);
 	public selectedFile: File = null;
 
 	public isExportInProcess$: Observable<boolean>;
 	public isExportSuccess$: Observable<boolean>;
 	public isImportInProcess$: Observable<boolean>;
 	public isImportSuccess$: Observable<boolean>;
+    /** Date format type */
+    public giddhDateFormat: string = GIDDH_DATE_FORMAT;
+    /** directive to get reference of element */
+    @ViewChild('datepickerTemplate') public datepickerTemplate: ElementRef;
+    /* This will store modal reference */
+    public modalRef: BsModalRef;
+    /* This will store selected date range to use in api */
+    public selectedDateRange: any;
+    /* This will store selected date range to show on UI */
+    public selectedDateRangeUi: any;
+    /* This will store available date ranges */
+    public datePickerOption: any = GIDDH_DATE_RANGE_PICKER_RANGES;
+    /* Moment object */
+    public moment = moment;
+    /* Selected from date */
+    public fromDate: string;
+    /* Selected to date */
+    public toDate: string;
+    /* Selected range label */
+    public selectedRangeLabel: any = "";
+    /* This will store the x/y position of the field to show datepicker under it */
+    public dateFieldPosition: any = { x: 0, y: 0 };
+    /** Universal date observer */
+    public universalDate$: Observable<any>;
 	private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-	constructor(private _store: Store<AppState>, private _companyImportExportActions: CompanyImportExportActions) {
-		this.isExportInProcess$ = this._store.select(s => s.companyImportExport.exportRequestInProcess).pipe(takeUntil(this.destroyed$));
-		this.isExportSuccess$ = this._store.select(s => s.companyImportExport.exportRequestSuccess).pipe(takeUntil(this.destroyed$));
-		this.isImportInProcess$ = this._store.select(s => s.companyImportExport.importRequestInProcess).pipe(takeUntil(this.destroyed$));
-		this.isImportSuccess$ = this._store.select(s => s.companyImportExport.importRequestSuccess).pipe(takeUntil(this.destroyed$));
+	constructor(private _store: Store<AppState>, private _companyImportExportActions: CompanyImportExportActions, private generalService: GeneralService, private modalService: BsModalService, private changeDetectorRef: ChangeDetectorRef,) {
+		this.isExportInProcess$ = this._store.pipe(select(s => s.companyImportExport.exportRequestInProcess), takeUntil(this.destroyed$));
+		this.isExportSuccess$ = this._store.pipe(select(s => s.companyImportExport.exportRequestSuccess), takeUntil(this.destroyed$));
+		this.isImportInProcess$ = this._store.pipe(select(s => s.companyImportExport.importRequestInProcess), takeUntil(this.destroyed$));
+		this.isImportSuccess$ = this._store.pipe(select(s => s.companyImportExport.importRequestSuccess), takeUntil(this.destroyed$));
+        this.universalDate$ = this._store.pipe(select(sessionStore => sessionStore.session.applicationDate), distinctUntilChanged(), takeUntil(this.destroyed$));
+
 	}
 
 	public ngOnInit() {
@@ -86,11 +81,16 @@ export class CompanyImportExportFormComponent implements OnInit, OnDestroy {
 				this.backButtonPressed();
 			}
 		});
-	}
-
-	public selectedDate(value: any) {
-		this.from = moment(value.picker.startDate, 'DD-MM-YYYY').format('DD-MM-YYYY');
-		this.to = moment(value.picker.endDate, 'DD-MM-YYYY').format('DD-MM-YYYY');
+         this.universalDate$.subscribe((dateObj) => {
+            if (dateObj) {
+                let universalDate = _.cloneDeep(dateObj);
+                this.selectedDateRange = { startDate: moment(universalDate[0]), endDate: moment(universalDate[1]) };
+                this.selectedDateRangeUi = moment(universalDate[0]).format(GIDDH_NEW_DATE_FORMAT_UI) + " - " + moment(universalDate[1]).format(GIDDH_NEW_DATE_FORMAT_UI);
+                this.fromDate = moment(universalDate[0]).format(GIDDH_DATE_FORMAT);
+                this.toDate = moment(universalDate[1]).format(GIDDH_DATE_FORMAT);
+            }
+            this.changeDetectorRef.detectChanges();
+        });
 	}
 
 	public fileSelected(file: File) {
@@ -118,4 +118,54 @@ export class CompanyImportExportFormComponent implements OnInit, OnDestroy {
 		this.destroyed$.next(true);
 		this.destroyed$.complete();
 	}
+
+    /**
+     * To show the datepicker
+     *
+     * @param {*} element
+     * @memberof CompanyImportExportFormComponent
+     */
+    public showGiddhDatepicker(element: any): void {
+        if (element) {
+            this.dateFieldPosition = this.generalService.getPosition(element.target);
+        }
+        this.modalRef = this.modalService.show(
+            this.datepickerTemplate,
+            Object.assign({}, { class: 'modal-lg giddh-datepicker-modal', backdrop: false, ignoreBackdropClick: false })
+        );
+    }
+
+    /**
+     * This will hide the datepicker
+     *
+     * @memberof CompanyImportExportFormComponent
+     */
+    public hideGiddhDatepicker(): void {
+        this.modalRef.hide();
+    }
+
+    /**
+     * Call back function for date/range selection in datepicker
+     *
+     * @param {*} value
+     * @memberof CompanyImportExportFormComponent
+     */
+    public dateSelectedCallback(value?: any): void {
+        if(value && value.event === "cancel") {
+            this.hideGiddhDatepicker();
+            return;
+        }
+        this.selectedRangeLabel = "";
+
+        if (value && value.name) {
+            this.selectedRangeLabel = value.name;
+        }
+        this.hideGiddhDatepicker();
+        if (value && value.startDate && value.endDate) {
+            this.selectedDateRange = { startDate: moment(value.startDate), endDate: moment(value.endDate) };
+            this.selectedDateRangeUi = moment(value.startDate).format(GIDDH_NEW_DATE_FORMAT_UI) + " - " + moment(value.endDate).format(GIDDH_NEW_DATE_FORMAT_UI);
+            this.from = moment(value.startDate).format(GIDDH_DATE_FORMAT);
+            this.to = moment(value.endDate).format(GIDDH_DATE_FORMAT);
+        }
+    }
 }
