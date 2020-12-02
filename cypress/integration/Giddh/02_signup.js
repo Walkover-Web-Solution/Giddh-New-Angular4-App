cc03d223b27b84576469594e40534e20d2f97c12
chirag gupta77 <chiraggupta77@gmail.com>
2020-12-02 10:55:44 +0530

    +Chirag | remove unwanted code and updated cypress version and commented failed TC

--- cypress/integration/Giddh/02_signup.js
+++ cypress/integration/Giddh/02_signup.js
@@ -11,7 +11,7 @@ describe('This is SignUp Test', function() {
     })
 
 
-    xit('SingUp with Email', () => {
+    it('SingUp with Email', () => {
         let r = "giddhautomation_signup"+ Math.random().toString(36).substring(7)+ "@gmail.com";
         console.log("random", r);
         cy.SignUp(r, testData.Password)
