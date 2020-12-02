ff2a5b7fda5a66aeceb6f9c5e4b6109aa009bc9b
prateek-giddh <prateekmishra@giddh.com>
2020-12-02 15:38:31 +0530

    feat: Hide the import options except entries in branch mode

--- apps/web-giddh/src/app/import-excel/import-type-select/import-type-select.component.ts
+++ apps/web-giddh/src/app/import-excel/import-type-select/import-type-select.component.ts
@@ -5,6 +5,8 @@ import { AppState } from '../../store';
 import { ImportExcelActions } from '../../actions/import-excel/import-excel.actions';
 import { CurrentPage } from '../../models/api-models/Common';
 import { GeneralActions } from '../../actions/general/general.actions';
+import { GeneralService } from '../../services/general.service';
+import { OrganizationType } from '../../models/user-login-state';
 
 @Component({
     selector: 'import-type-select',
@@ -14,15 +16,20 @@ import { GeneralActions } from '../../actions/general/general.actions';
 
 export class ImportTypeSelectComponent implements OnInit {
 
+    /** True if current organization is branch */
+    public isBranch: boolean;
+
     constructor(
         private store: Store<AppState>,
         private _router: Router,
         private _importExcelActions: ImportExcelActions,
-        private _generalActions: GeneralActions
+        private _generalActions: GeneralActions,
+        private generalService: GeneralService
     ) {
         let currentPageObj = new CurrentPage();
         currentPageObj.name = "Import Data";
         currentPageObj.url = this._router.url;
+        this.isBranch = this.generalService.currentOrganizationType === OrganizationType.Branch;
         this.store.dispatch(this._generalActions.setPageTitle(currentPageObj));
     }
 
