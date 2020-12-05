7393e84a5abcbb2a616be68b21008420ee235a1c
chirag gupta77 <chiraggupta77@gmail.com>
2020-12-05 10:27:47 +0530

    +Chirag | added viewport to remove tc flakiness

--- cypress/integration/Giddh/02_signup.js
+++ cypress/integration/Giddh/02_signup.js
@@ -3,6 +3,7 @@ describe('This is SignUp Test', ()=> {
 
     let testData = "";
     before(()=> {
+        cy.viewport(1366, 768)
         cy.fixture('example.json')
             .then((data) => {
                 // "this" is still the test context object
