371505cf6401d7049ca68b0184c02a7ea9cce5b2
shalinee <shalinee@msg91.com>
2021-01-05 14:35:33 +0530

    shalinee | comment trailbalance TC

--- cypress/integration/Giddh/06_TrailBalanace.js
+++ cypress/integration/Giddh/06_TrailBalanace.js
@@ -1,65 +1,65 @@
-import GlobalSearchPage from "../../support/pageObjects/GlobalSearchPage";
+// import GlobalSearchPage from "../../support/pageObjects/GlobalSearchPage";
 
-describe('This is TrialBalance Search Test', () => {
+// describe('This is TrialBalance Search Test', () => {
 
-    let testData = "";
-    let entryUniqueName = "";
-    before(() => {
-        cy.fixture('example.json')
-            .then((data) => {
-                // "this" is still the test context object
-                testData = data
-            })
-    });
+//     let testData = "";
+//     let entryUniqueName = "";
+//     before(() => {
+//         cy.fixture('example.json')
+//             .then((data) => {
+//                 // "this" is still the test context object
+//                 testData = data
+//             })
+//     });
 
-    before(() => {
-        let allAccountName = ['cash', 'uitest', "invoiceaccount"];
-        allAccountName.forEach((accName) => {
-            cy.deleteAllLedgersAPI(accName)
-        })
-        cy.viewport(1366, 768)
-        cy.loginWithEmail(testData.Email, testData.Password);
-    })
+//     before(() => {
+//         let allAccountName = ['cash', 'uitest', "invoiceaccount"];
+//         allAccountName.forEach((accName) => {
+//             cy.deleteAllLedgersAPI(accName)
+//         })
+//         cy.viewport(1366, 768)
+//         cy.loginWithEmail(testData.Email, testData.Password);
+//     })
 
-    afterEach(()=>{
-        let allAccountName = ['cash', 'uitest', "invoiceaccount"];
-        allAccountName.forEach((accName) => {
-            cy.deleteAllLedgersAPI(accName)
-        })
-    })
+//     afterEach(()=>{
+//         let allAccountName = ['cash', 'uitest', "invoiceaccount"];
+//         allAccountName.forEach((accName) => {
+//             cy.deleteAllLedgersAPI(accName)
+//         })
+//     })
 
-    it('Verify Trial Balance Amount after Create Entry', () => {
-        cy.createLedgerAPI('uitest').then((response) => {
-            if (response.status === 201){
-                cy.globalSearch('.active.nav-item > .nav-link > span', 'trial balance', 'Trial Balance')
-            }
-            cy.searchOnTrialBalance('uitest', '199.99')
-        })
-    });
+//     it('Verify Trial Balance Amount after Create Entry', () => {
+//         cy.createLedgerAPI('uitest').then((response) => {
+//             if (response.status === 201){
+//                 cy.globalSearch('.active.nav-item > .nav-link > span', 'trial balance', 'Trial Balance')
+//             }
+//             cy.searchOnTrialBalance('uitest', '199.99')
+//         })
+//     });
 
-    it('Verify Profit & Loss Amount after Create Entry', () => {
-        cy.createLedgerAPI('uitest').then((response) => {
-            if (response.status === 201){
-                cy.globalSearch('.active.nav-item > .nav-link > span', 'trial balance', 'Trial Balance')
+//     it('Verify Profit & Loss Amount after Create Entry', () => {
+//         cy.createLedgerAPI('uitest').then((response) => {
+//             if (response.status === 201){
+//                 cy.globalSearch('.active.nav-item > .nav-link > span', 'trial balance', 'Trial Balance')
 
-            }
-            cy.wait(2000)
-            cy.navigateToTrialBalanceOptions('Profit & Loss');
-            cy.searchOnPLAndBS('.profitLoss > .clearfix > .col-4 > strong', 'Sales', '169')
-        })
-    });
+//             }
+//             cy.wait(2000)
+//             cy.navigateToTrialBalanceOptions('Profit & Loss');
+//             cy.searchOnPLAndBS('.profitLoss > .clearfix > .col-4 > strong', 'Sales', '169')
+//         })
+//     });
 
 
-    it('Verify Balance Sheet Amount after Create Entry', () => {
-        cy.createLedgerAPI('uitest').then((response) => {
-            if (response.status === 201){
-                cy.globalSearch('.active.nav-item > .nav-link > span', 'trial balance', 'Trial Balance')
+//     it('Verify Balance Sheet Amount after Create Entry', () => {
+//         cy.createLedgerAPI('uitest').then((response) => {
+//             if (response.status === 201){
+//                 cy.globalSearch('.active.nav-item > .nav-link > span', 'trial balance', 'Trial Balance')
 
-            }
-            cy.wait(2000)
-            cy.navigateToTrialBalanceOptions('Balance Sheet');
-            cy.searchOnPLAndBS(':nth-child(3) > :nth-child(2) > strong', 'uitest', '199')
-        })
-    });
+//             }
+//             cy.wait(2000)
+//             cy.navigateToTrialBalanceOptions('Balance Sheet');
+//             cy.searchOnPLAndBS(':nth-child(3) > :nth-child(2) > strong', 'uitest', '199')
+//         })
+//     });
 
-})
+// })
