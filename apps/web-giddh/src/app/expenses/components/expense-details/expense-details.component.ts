8853f6b805912b55f63309ce4f36a20c17cb33f7
ravindersingh <ravinder@walkover.in>
2021-01-28 19:56:45 +0530

    Resolved conflict

--- apps/web-giddh/src/app/expenses/components/expense-details/expense-details.component.ts
+++ apps/web-giddh/src/app/expenses/components/expense-details/expense-details.component.ts
@@ -45,7 +45,10 @@ import { AccountService } from '../../../services/account.service';
 })
 
 export class ExpenseDetailsComponent implements OnInit, OnChanges, OnDestroy {
-
+    /* This will hold local JSON data */
+    @Input() public localeData: any = {};
+    /* This will hold common JSON data */
+    @Input() public commonLocaleData: any = {};
     public modalRef: BsModalRef;
     public approveEntryModalRef: BsModalRef;
     public message: string;
@@ -150,8 +153,10 @@ export class ExpenseDetailsComponent implements OnInit, OnChanges, OnDestroy {
         totalPages: 0,
         query: ''
     };
-
     private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+    /** This will hold creator name */
+    public byCreator: string = '';
+
     /** True if account belongs to cash/bank account */
     private cashOrBankEntry: any;
     /** Stores the petty cash entry type */
@@ -198,6 +203,7 @@ export class ExpenseDetailsComponent implements OnInit, OnChanges, OnDestroy {
         this.store.pipe(select(s => s.company && s.company.taxes), takeUntil(this.destroyed$)).subscribe(res => {
             this.companyTaxesList = res || [];
         });
+        this.buildCreatorString();
     }
 
     public preFillData(res: PettyCashResonse) {
@@ -280,7 +286,9 @@ export class ExpenseDetailsComponent implements OnInit, OnChanges, OnDestroy {
 
     public approveEntry() {
         if (this.entryAgainstObject.base && !this.entryAgainstObject.model) {
-            this._toasty.errorToast('Please Select ' + this.entryAgainstObject.base + '  for entry..');
+            let errorMessage = this.localeData.entry_against_error;
+            errorMessage = errorMessage.replace("[ENTRY_AGAINST]", this.entryAgainstObject.base);
+            this._toasty.errorToast(errorMessage);
             this.hideApproveConfirmPopup(false);
             return;
         }
@@ -366,6 +374,7 @@ export class ExpenseDetailsComponent implements OnInit, OnChanges, OnDestroy {
             this.selectedItem = changes['selectedRowItem'].currentValue;
             this.store.dispatch(this._expenceActions.getPettycashEntryRequest(this.selectedItem.uniqueName));
             this.store.dispatch(this._ledgerActions.setAccountForEdit(this.selectedItem.baseAccount.uniqueName || null));
+            this.buildCreatorString();
         }
     }
 
@@ -414,7 +423,7 @@ export class ExpenseDetailsComponent implements OnInit, OnChanges, OnDestroy {
                 this.imageURL.push(img);
                 this.imgAttachedFileName = output.file.response.body.name;
                 // this.customTemplate.sections.footer.data.imageSignature.label = output.file.response.body.uniqueName;
-                this._toasty.successToast('file uploaded successfully.');
+                this._toasty.successToast(this.localeData.file_upload_success);
                 // this.startUpload();
             } else {
                 this._toasty.errorToast(output.file.response.message, output.file.response.code);
@@ -899,5 +908,19 @@ export class ExpenseDetailsComponent implements OnInit, OnChanges, OnDestroy {
     public ngOnDestroy(): void {
         this.destroyed$.next(true);
         this.destroyed$.complete();
+    }
+
+    /**
+     * This will build the creator name string
+     *
+     * @memberof ExpenseDetailsComponent
+     */
+    public buildCreatorString(): void {
+        if(this.selectedItem && this.selectedItem.createdBy) {
+            this.byCreator = this.localeData.by_creator;
+            this.byCreator = this.byCreator.replace("[CREATOR_NAME]", this.selectedItem.createdBy.name);
+        } else {
+            this.byCreator = "";
+        }
     }
 }
