1ee6dd5f124b1fe3e7ef293d15e5c164f849bfa1
shalinee <shalinee@msg91.com>
2021-01-28 17:19:59 +0530

    get current page url

--- apps/web-giddh/src/app/invoice/download-bulk-invoice/download-bulk-invoice.component.ts
+++ apps/web-giddh/src/app/invoice/download-bulk-invoice/download-bulk-invoice.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
     selector: 'download-bulk-invoice',
     templateUrl: './download-bulk-invoice.component.html',
     styleUrls: ['./download-bulk-invoice.component.scss'],
 })
 export class DownloadBulkInvoiceComponent implements OnInit {
+    /* it will store current page url */
+    public currentPageUrl: string = '';
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+    ){}
 
     public ngOnInit() {
+        this.currentPageUrl = this.router.url;
     }
 }
