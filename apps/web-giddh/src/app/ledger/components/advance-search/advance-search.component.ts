07124a52ebd8ddef92901667ea160e818dc45dda
prateek-giddh <prateekmishra@giddh.com>
2021-01-30 17:16:02 +0530

    perf: Remove flatten accounts from Search module

--- apps/web-giddh/src/app/ledger/components/advance-search/advance-search.component.ts
+++ apps/web-giddh/src/app/ledger/components/advance-search/advance-search.component.ts
@@ -772,6 +772,9 @@ export class AdvanceSearchModelComponent implements OnInit, OnDestroy, OnChanges
                     this.groupsSearchResultsPaginationData.totalPages = data.body.totalPages;
                     if (successCallback) {
                         successCallback(data.body.results);
+                    } else {
+                        this.defaultGroupPaginationData.page = this.groupsSearchResultsPaginationData.page;
+                        this.defaultGroupPaginationData.totalPages = this.groupsSearchResultsPaginationData.totalPages;
                     }
                 }
             });
