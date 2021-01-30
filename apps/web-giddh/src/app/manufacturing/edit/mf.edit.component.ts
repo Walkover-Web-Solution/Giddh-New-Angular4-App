3014081abab10734b929ffb8563c5b8c18b15779
prateek-giddh <prateekmishra@giddh.com>
2021-01-30 16:29:17 +0530

    perf: Remove flatten accounts from Manufacturing and fix dynamic search API issue

--- apps/web-giddh/src/app/manufacturing/edit/mf.edit.component.ts
+++ apps/web-giddh/src/app/manufacturing/edit/mf.edit.component.ts
@@ -12,17 +12,15 @@ import { InventoryAction } from '../../actions/inventory/inventory.actions';
 import { IStockItemDetail } from '../../models/interfaces/stocksItem.interface';
 import * as _ from '../../lodash-optimized';
 import * as moment from 'moment/moment';
-import { GroupService } from '../../services/group.service';
 import { ManufacturingItemRequest } from '../../models/interfaces/manufacturing.interface';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { InventoryService } from '../../services/inventory.service';
-import { AccountService } from '../../services/account.service';
-import { GroupsWithAccountsResponse } from '../../models/api-models/GroupsWithAccounts';
 import { createSelector } from 'reselect';
 import { IForceClear } from 'apps/web-giddh/src/app/models/api-models/Sales';
 import { CurrentPage } from '../../models/api-models/Common';
 import { GeneralActions } from '../../actions/general/general.actions';
 import { GIDDH_DATE_FORMAT } from '../../shared/helpers/defaultDateFormat';
+import { SearchService } from '../../services/search.service';
 
 @Component({
     templateUrl: './mf.edit.component.html',
@@ -34,7 +32,6 @@ export class MfEditComponent implements OnInit {
 
     public stockListDropDown$: Observable<IOption[]>;
     public allStocksDropDown$: Observable<IOption[]>;
-    public groupsList$: Observable<GroupsWithAccountsResponse[]>;
     public consumptionDetail = [];
     public isUpdateCase: boolean = false;
     public manufacturingDetails: ManufacturingItemRequest;
@@ -74,23 +71,59 @@ export class MfEditComponent implements OnInit {
     private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
     /** This holds giddh date format */
     public giddhDateFormat: string = GIDDH_DATE_FORMAT;
+    /** Stores the search results pagination details */
+    public expenseAccountsSearchResultsPaginationData = {
+        page: 0,
+        totalPages: 0,
+        query: ''
+    };
+    /** Default search suggestion list to be shown for search */
+    public defaultExpenseAccountSuggestions: Array<IOption> = [];
+    /** True, if API call should be prevented on default scroll caused by scroll in list */
+    public preventExpenseDefaultScrollApiCall: boolean = false;
+    /** Stores the default search results pagination details */
+    public defaultExpenseAccountPaginationData = {
+        page: 0,
+        totalPages: 0,
+        query: ''
+    };
+    /** Stores the list of accounts */
+    public expenseAccounts: IOption[];
+    /** Stores the search results pagination details */
+    public liabilitiesAssetAccountsSearchResultsPaginationData = {
+        page: 0,
+        totalPages: 0,
+        query: ''
+    };
+    /** Default search suggestion list to be shown for search */
+    public defaultLiabilitiesAssetAccountSuggestions: Array<IOption> = [];
+    /** True, if API call should be prevented on default scroll caused by scroll in list */
+    public preventLiabilitiesAssetDefaultScrollApiCall: boolean = false;
+    /** Stores the default search results pagination details */
+    public defaultLiabilitiesAssetAccountPaginationData = {
+        page: 0,
+        totalPages: 0,
+        query: ''
+    };
+    /** Stores the list of accounts */
+    public liabilitiesAssetAccounts: IOption[];
 
-    constructor(private store: Store<AppState>,
+    constructor(
+        private store: Store<AppState>,
         private manufacturingActions: ManufacturingActions,
         private inventoryAction: InventoryAction,
         private _location: Location,
         private _inventoryService: InventoryService,
-        private _accountService: AccountService,
         private generalAction: GeneralActions,
         private router: Router,
-        private _toasty: ToasterService) {
+        private _toasty: ToasterService,
+        private searchService: SearchService
+    ) {
 
         this.store.dispatch(this.inventoryAction.GetManufacturingCreateStock());
         this.store.dispatch(this.inventoryAction.GetStock());
         this.isGetManufactureStockInProgress$ = this.store.pipe(select(state => state.inventory.isGetManufactureStockInProgress), takeUntil(this.destroyed$));
         this.isStockWithRateInprogress$ = this.store.pipe(select(state => state.manufacturing.isStockWithRateInprogress), takeUntil(this.destroyed$));
-
-        this.groupsList$ = this.store.pipe(select(p => p.general.groupswithaccounts), takeUntil(this.destroyed$));
 
         this.manufacturingDetails = new ManufacturingItemRequest();
         this.initializeOtherExpenseObj();
@@ -108,6 +141,10 @@ export class MfEditComponent implements OnInit {
                     manufacturingObj.linkedStocks.forEach((item) => {
                         item.quantity = (item.manufacturingQuantity / manufacturingObj.manufacturingMultipleOf);
                     });
+                    manufacturingObj.otherExpenses.forEach(expense => {
+                        expense.baseAccount.defaultName = `${expense.baseAccount.name} (${expense.baseAccount.uniqueName})`;
+                        expense.transactions[0].account.defaultName = `${expense.transactions[0]?.account?.name} (${expense.transactions[0]?.account?.uniqueName})`;
+                    });
                     if (!this.initialQuantityObj.length) {
                         this.initialQuantityObj = manufacturingObj.linkedStocks;
                     }
@@ -121,38 +158,6 @@ export class MfEditComponent implements OnInit {
             }
         });
 
-        // Get group with accounts
-        this.groupsList$.subscribe(data => {
-            if (data && data.length) {
-                let GroupWithAccResponse = _.cloneDeep(data);
-                this._accountService.getFlattenAccounts('', '').pipe(takeUntil(this.destroyed$)).subscribe(response => {
-                    if (response.status === 'success') {
-                        let flattenGroupResponse = _.cloneDeep(response.body.results);
-
-                        this.liabilityGroupAccounts = [];
-                        this.expenseGroupAccounts = [];
-
-                        _.forEach(GroupWithAccResponse, (d: any) => {
-                            _.forEach(flattenGroupResponse, acc => {
-                                if (d.category === 'expenses' || d.category === 'liabilities' || d.category === 'assets') {
-                                    let matchedAccIndex = acc.parentGroups.findIndex((account) => account.uniqueName === d.uniqueName);
-                                    if (matchedAccIndex > -1) {
-                                        if (d.category === 'expenses') {
-                                            this.expenseGroupAccounts.push({ label: `${acc.name} (${acc.uniqueName})`, value: acc.uniqueName });
-                                        }
-                                        if (d.category === 'liabilities' || d.category === 'assets') {
-                                            this.liabilityGroupAccounts.push({ label: `${acc.name} (${acc.uniqueName})`, value: acc.uniqueName });
-                                        }
-                                    }
-                                }
-                            });
-                        });
-                        this.expenseGroupAccounts$ = observableOf(this.expenseGroupAccounts);
-                        this.liabilityGroupAccounts$ = observableOf(this.liabilityGroupAccounts);
-                    }
-                });
-            }
-        });
 
         this.manufacturingDetails.quantity = 1;
         this.setCurrentPageTitle();
@@ -236,6 +241,9 @@ export class MfEditComponent implements OnInit {
                 this.onQuantityChange(1);
             }
         });
+
+        this.loadExpenseAccounts();
+        this.loadAssetsLiabilitiesAccounts();
     }
 
     public getStocksWithRate(data) {
@@ -556,5 +564,200 @@ export class MfEditComponent implements OnInit {
         currentPageObj.url = "";
         currentPageObj.additional = "";
         this.store.dispatch(this.generalAction.setPageTitle(currentPageObj));
+    }
+
+    /**
+     * Loads the default list of expense accounts
+     *
+     * @memberof MfEditComponent
+     */
+    public loadExpenseAccounts(): void {
+        this.onExpenseAccountSearchQueryChanged('');
+    }
+
+    /**
+     * Loads the default list of assets and liabilities accounts
+     *
+     * @memberof MfEditComponent
+     */
+    public loadAssetsLiabilitiesAccounts(): void {
+        this.onLiabilitiesAssetAccountSearchQueryChanged('', 1, (response) => {
+            this.defaultLiabilitiesAssetAccountSuggestions = response.map(result => {
+                return {
+                    value: result.uniqueName,
+                    label: `${result.name} (${result.uniqueName})`
+                }
+            }) || [];
+            this.defaultLiabilitiesAssetAccountPaginationData.page = this.liabilitiesAssetAccountsSearchResultsPaginationData.page;
+            this.defaultLiabilitiesAssetAccountPaginationData.totalPages = this.liabilitiesAssetAccountsSearchResultsPaginationData.totalPages;
+        });
+    }
+
+    /**
+     * Search query change handler for expense account
+     *
+     * @param {string} query Search query
+     * @param {number} [page=1] Page to request
+     * @param {boolean} withStocks True, if search should include stocks in results
+     * @param {Function} successCallback Callback to carry out further operation
+     * @memberof MfEditComponent
+     */
+    public onExpenseAccountSearchQueryChanged(query: string, page: number = 1, successCallback?: Function): void {
+        this.expenseAccountsSearchResultsPaginationData.query = query;
+        if (!this.preventExpenseDefaultScrollApiCall &&
+            (query || (this.defaultExpenseAccountSuggestions && this.defaultExpenseAccountSuggestions.length === 0) || successCallback)) {
+            // Call the API when either query is provided, default suggestions are not present or success callback is provided
+            const requestObject: any = {
+                q: encodeURIComponent(query),
+                page,
+                group: encodeURIComponent('operatingcost, indirectexpenses')
+            };
+            this.searchService.searchAccountV2(requestObject).subscribe(data => {
+                if (data && data.body && data.body.results) {
+                    const searchResults = data.body.results.map(result => {
+                        return {
+                            value: result.uniqueName,
+                            label: `${result.name} (${result.uniqueName})`
+                        }
+                    }) || [];
+                    if (page === 1) {
+                        this.expenseAccounts = searchResults;
+                    } else {
+                        this.expenseAccounts = [
+                            ...this.expenseAccounts,
+                            ...searchResults
+                        ];
+                    }
+                    this.expenseGroupAccounts$ = observableOf(this.expenseAccounts);
+                    this.expenseAccountsSearchResultsPaginationData.page = data.body.page;
+                    this.expenseAccountsSearchResultsPaginationData.totalPages = data.body.totalPages;
+                    if (successCallback) {
+                        successCallback(data.body.results);
+                    } else {
+                        this.defaultExpenseAccountPaginationData.page = this.expenseAccountsSearchResultsPaginationData.page;
+                        this.defaultExpenseAccountPaginationData.totalPages = this.expenseAccountsSearchResultsPaginationData.totalPages;
+                    }
+                }
+            });
+        } else {
+            this.expenseAccounts = [...this.defaultExpenseAccountSuggestions];
+            this.expenseAccountsSearchResultsPaginationData.page = this.defaultExpenseAccountPaginationData.page;
+            this.expenseAccountsSearchResultsPaginationData.totalPages = this.defaultExpenseAccountPaginationData.totalPages;
+            this.preventExpenseDefaultScrollApiCall = true;
+            setTimeout(() => {
+                this.preventExpenseDefaultScrollApiCall = false;
+            }, 500);
+        }
+    }
+
+    /**
+     * Scroll end handler for expense account
+     *
+     * @returns null
+     * @memberof MfEditComponent
+     */
+    public handleExpenseAccountScrollEnd(): void {
+        if (this.expenseAccountsSearchResultsPaginationData.page < this.expenseAccountsSearchResultsPaginationData.totalPages) {
+            this.onExpenseAccountSearchQueryChanged(
+                this.expenseAccountsSearchResultsPaginationData.query,
+                this.expenseAccountsSearchResultsPaginationData.page + 1,
+                (response) => {
+                    if (!this.expenseAccountsSearchResultsPaginationData.query) {
+                        const results = response.map(result => {
+                            return {
+                                value: result.uniqueName,
+                                label: `${result.name} (${result.uniqueName})`
+                            }
+                        }) || [];
+                        this.defaultExpenseAccountSuggestions = this.defaultExpenseAccountSuggestions.concat(...results);
+                        this.defaultExpenseAccountPaginationData.page = this.expenseAccountsSearchResultsPaginationData.page;
+                        this.defaultExpenseAccountPaginationData.totalPages = this.expenseAccountsSearchResultsPaginationData.totalPages;
+                    }
+            });
+        }
+    }
+
+    /**
+     * Search query change handler on liabilities and assets account
+     *
+     * @param {string} query Search query
+     * @param {number} [page=1] Page to request
+     * @param {boolean} withStocks True, if search should include stocks in results
+     * @param {Function} successCallback Callback to carry out further operation
+     * @memberof MfEditComponent
+     */
+    public onLiabilitiesAssetAccountSearchQueryChanged(query: string, page: number = 1, successCallback?: Function): void {
+        this.liabilitiesAssetAccountsSearchResultsPaginationData.query = query;
+        if (!this.preventLiabilitiesAssetDefaultScrollApiCall &&
+            (query || (this.defaultLiabilitiesAssetAccountSuggestions && this.defaultLiabilitiesAssetAccountSuggestions.length === 0) || successCallback)) {
+            // Call the API when either query is provided, default suggestions are not present or success callback is provided
+            const requestObject: any = {
+                q: encodeURIComponent(query),
+                page,
+                group: encodeURIComponent('noncurrentassets, currentassets, fixedassets, currentliabilities, noncurrentliabilities, shareholdersfunds')
+            }
+            this.searchService.searchAccountV2(requestObject).subscribe(data => {
+                if (data && data.body && data.body.results) {
+                    const searchResults = data.body.results.map(result => {
+                        return {
+                            value: result.uniqueName,
+                            label: `${result.name} (${result.uniqueName})`
+                        }
+                    }) || [];
+                    if (page === 1) {
+                        this.liabilitiesAssetAccounts = searchResults;
+                    } else {
+                        this.liabilitiesAssetAccounts = [
+                            ...this.liabilitiesAssetAccounts,
+                            ...searchResults
+                        ];
+                    }
+                    this.liabilityGroupAccounts$ = observableOf(this.liabilitiesAssetAccounts);
+                    this.liabilitiesAssetAccountsSearchResultsPaginationData.page = data.body.page;
+                    this.liabilitiesAssetAccountsSearchResultsPaginationData.totalPages = data.body.totalPages;
+                    if (successCallback) {
+                        successCallback(data.body.results);
+                    } else {
+                        this.defaultLiabilitiesAssetAccountPaginationData.page = this.liabilitiesAssetAccountsSearchResultsPaginationData.page;
+                        this.defaultLiabilitiesAssetAccountPaginationData.totalPages = this.liabilitiesAssetAccountsSearchResultsPaginationData.totalPages;
+                    }
+                }
+            });
+        } else {
+            this.liabilitiesAssetAccounts = [...this.defaultLiabilitiesAssetAccountSuggestions];
+            this.liabilitiesAssetAccountsSearchResultsPaginationData.page = this.defaultLiabilitiesAssetAccountPaginationData.page;
+            this.liabilitiesAssetAccountsSearchResultsPaginationData.totalPages = this.defaultLiabilitiesAssetAccountPaginationData.totalPages;
+            this.preventLiabilitiesAssetDefaultScrollApiCall = true;
+            setTimeout(() => {
+                this.preventLiabilitiesAssetDefaultScrollApiCall = false;
+            }, 500);
+        }
+    }
+
+    /**
+     * Scroll end handler for liabilities and assets account
+     *
+     * @returns null
+     * @memberof MfEditComponent
+     */
+    public handleLiabilitiesAssetAccountScrollEnd(): void {
+        if (this.defaultLiabilitiesAssetAccountPaginationData.page < this.defaultLiabilitiesAssetAccountPaginationData.totalPages) {
+            this.onLiabilitiesAssetAccountSearchQueryChanged(
+                this.defaultLiabilitiesAssetAccountPaginationData.query,
+                this.defaultLiabilitiesAssetAccountPaginationData.page + 1,
+                (response) => {
+                    if (!this.defaultLiabilitiesAssetAccountPaginationData.query) {
+                        const results = response.map(result => {
+                            return {
+                                value: result.uniqueName,
+                                label: `${result.name} (${result.uniqueName})`
+                            }
+                        }) || [];
+                        this.defaultLiabilitiesAssetAccountSuggestions = this.defaultLiabilitiesAssetAccountSuggestions.concat(...results);
+                        this.defaultLiabilitiesAssetAccountPaginationData.page = this.liabilitiesAssetAccountsSearchResultsPaginationData.page;
+                        this.defaultLiabilitiesAssetAccountPaginationData.totalPages = this.liabilitiesAssetAccountsSearchResultsPaginationData.totalPages;
+                    }
+            });
+        }
     }
 }
