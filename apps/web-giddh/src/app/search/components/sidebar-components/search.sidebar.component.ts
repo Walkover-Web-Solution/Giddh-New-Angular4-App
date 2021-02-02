7c08cc696684583a86295c7224af685c2a7edb04
prateek-giddh <prateekmishra@giddh.com>
2021-02-02 15:55:08 +0530

    fix: Nullity issue

--- apps/web-giddh/src/app/search/components/sidebar-components/search.sidebar.component.ts
+++ apps/web-giddh/src/app/search/components/sidebar-components/search.sidebar.component.ts
@@ -178,7 +178,7 @@ export class SearchSidebarComponent implements OnInit, OnChanges, OnDestroy {
                     // branches are loaded
                     if (this.generalService.currentOrganizationType === OrganizationType.Branch) {
                         currentBranchUniqueName = this.generalService.currentBranchUniqueName;
-                        this.currentBranch = _.cloneDeep(response.find(branch => branch.uniqueName === currentBranchUniqueName));
+                        this.currentBranch = _.cloneDeep(response.find(branch => branch.uniqueName === currentBranchUniqueName)) || this.currentBranch;
                     } else {
                         currentBranchUniqueName = this.activeCompany ? this.activeCompany.uniqueName : '';
                         this.currentBranch = {
