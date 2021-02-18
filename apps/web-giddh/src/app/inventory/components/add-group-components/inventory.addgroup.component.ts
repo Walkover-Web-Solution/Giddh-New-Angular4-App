6bfe00a94462f90deec17821add7d04835c76f14
ravindersingh <ravinder@walkover.in>
2021-02-18 11:59:28 +0530

    Fixed PR Issue

--- apps/web-giddh/src/app/inventory/components/add-group-components/inventory.addgroup.component.ts
+++ apps/web-giddh/src/app/inventory/components/add-group-components/inventory.addgroup.component.ts
@@ -266,7 +266,7 @@ export class InventoryAddGroupComponent implements OnInit, OnDestroy, AfterViewI
             stockRequest.parentStockGroupUniqueName = uniqName.value;
         }
 
-        if(stockRequest.taxes?.length <= 0) {
+        if(!stockRequest.taxes) {
             stockRequest.taxes = [];
         }
 
