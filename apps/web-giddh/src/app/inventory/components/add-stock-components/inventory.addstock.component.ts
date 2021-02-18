31724bd7f75beb15ac2dbc25284b8d64f2160156
ravindersingh <ravinder@walkover.in>
2021-02-18 11:59:28 +0530

    Fixed PR Issue

--- apps/web-giddh/src/app/inventory/components/add-stock-components/inventory.addstock.component.ts
+++ apps/web-giddh/src/app/inventory/components/add-stock-components/inventory.addstock.component.ts
@@ -807,7 +807,7 @@ export class InventoryAddStockComponent implements OnInit, AfterViewInit, OnDest
         }
 
         stockObj.isFsStock = formObj.isFsStock;
-        stockObj.taxes = (formObj.taxes?.length > 0) ? formObj.taxes : [];
+        stockObj.taxes = (formObj.taxes) ? formObj.taxes : [];
 
         if (stockObj.isFsStock) {
             formObj.manufacturingDetails.linkedStocks = this.removeBlankLinkedStock(formObj.manufacturingDetails.linkedStocks);
@@ -878,7 +878,7 @@ export class InventoryAddStockComponent implements OnInit, AfterViewInit, OnDest
         stockObj.openingQuantity = formObj.openingQuantity || 0;
         stockObj.hsnNumber = formObj.hsnNumber;
         stockObj.sacNumber = formObj.sacNumber;
-        stockObj.taxes = (formObj.taxes?.length > 0) ? formObj.taxes : [];
+        stockObj.taxes = (formObj.taxes) ? formObj.taxes : [];
         stockObj.skuCode = formObj.skuCode;
         stockObj.skuCode = formObj.skuCode;
         stockObj.skuCodeHeading = formObj.skuCodeHeading;
