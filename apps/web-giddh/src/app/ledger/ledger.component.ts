b8c0407bed1e81caa7eec8a177be6bf051230e62
ravindersingh <ravinder@walkover.in>
2021-03-12 12:11:56 +0530

    Fixed exception

--- apps/web-giddh/src/app/ledger/ledger.component.ts
+++ apps/web-giddh/src/app/ledger/ledger.component.ts
@@ -1669,10 +1669,10 @@ export class LedgerComponent implements OnInit, OnDestroy {
         }
         switch (actionType) {
             case 'delete':
-                this.bulkActionConfirmationModal.show();
+                this.bulkActionConfirmationModal?.show();
                 break;
             case 'generate':
-                this.bulkActionGenerateVoucherModal.show();
+                this.bulkActionGenerateVoucherModal?.show();
                 break;
             case 'upload':
                 fileInput.click();
