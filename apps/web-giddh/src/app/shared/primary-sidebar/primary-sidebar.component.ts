110b4b416fff4c9e6809fdd63469355028d6c01d
shalinee <shalinee@msg91.com>
2021-03-12 18:00:12 +0530

    shalinee | TEST | sidebar and main item page designed

--- apps/web-giddh/src/app/shared/primary-sidebar/primary-sidebar.component.ts
+++ apps/web-giddh/src/app/shared/primary-sidebar/primary-sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store, select } from '@ngrx/store';
+import { AppState } from 'apps/web-giddh/src/app/store';
+import { GeneralActions } from 'apps/web-giddh/src/app/actions/general/general.actions';
 @Component({
     selector: 'primary-sidebar',
     templateUrl: './primary-sidebar.component.html',
@@ -9,7 +11,25 @@ import { Component, OnInit } from '@angular/core';
 
 export class PrimarySidebarComponent implements OnInit {
 
-    public ngOnInit() {
+    /* This will show sidebar is open */
+    public sideMenu: { isopen: boolean } = { isopen: true };
+    constructor(private store: Store<AppState>, private generalActions: GeneralActions) {
 
+    }
+    public ngOnInit(): void {
+
+
+    }
+    /**
+     * This will toggle the side menu
+     *
+     * @param {boolean} openSideMenu
+     * @memberof HamburgerMenuComponent
+     */
+    public sideBarStateChange(openSideMenu: boolean): void {
+        if (this.sideMenu) {
+            this.sideMenu.isopen = openSideMenu;
+        }
+        this.store.dispatch(this.generalActions.openSideMenu(openSideMenu));
     }
 }
