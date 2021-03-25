7de869067fa9a26a0ec2870034321962ccc59458
shalinee-walkover <shalinee@msg91.com>
2021-03-25 16:56:18 +0530

    shalinee | TEST | New header and sidebar design

--- apps/web-giddh/src/app/shared/helpers/allItems.ts
+++ apps/web-giddh/src/app/shared/helpers/allItems.ts
@@ -3,6 +3,7 @@ export interface AllItem {
     link: string;
     icon: string;
     description: string;
+    additional?: any;
 }
 
 export interface AllItems {
@@ -10,20 +11,73 @@ export interface AllItems {
 }
 
 export const allItems: AllItems[] = [
-    { 
+    {
         ["Customer"]: [
-            { label: 'Invoice', link: '/pages/invoice/preview/sales', icon: 'icon-invoice-new', description: 'Lorem' },
-            { label: 'Estimate', link: '/pages/invoice/preview/estimates', icon: 'icon-invoice-new', description: 'Lorem' },
-            { label: 'Sales Order', link: '/pages/invoice/preview/sales', icon: 'icon-sales-order1', description: 'Lorem' },
-            { label: 'Customer', link: '/pages/contact/customer', icon: 'icon-invoice-new', description: 'Lorem' },
+            { label: 'Invoice', link: '/pages/invoice/preview/sales', icon: 'icon-invoice-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Estimate', link: '/pages/invoice/preview/estimates', icon: 'icon-estimate-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Sales Order', link: '/pages/invoice/preview/sales', icon: 'icon-sales-order1', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Customer', link: '/pages/contact/customer', icon: 'icon-customer', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Aging Report', link: '/pages/contact/aging-report', icon: 'icon-aging-report-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Sales Register', link: '/pages/reports/sales-register', icon: 'icon-register-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Template', link: '/pages/invoice/preview/templates/sales', icon: 'icon-template-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Credit Note', link: '/pages/invoice/preview/credit%20note', icon: 'icon-credit-note-new', description: 'Lorem ipsum dolor sit amet, consectetur' }
         ]
     },
     {
         ["Vendor"]: [
-            { label: 'Invoice5', link: '/pages/invoice/preview/sales', icon: 'icon-invoice-new', description: 'Lorem' },
-            { label: 'Invoice6', link: '/pages/invoice/preview/sales', icon: 'icon-invoice-new', description: 'Lorem' },
-            { label: 'Invoice2', link: '/pages/invoice/preview/sales', icon: 'icon-invoice-new', description: 'Lorem' },
-            { label: 'Invoice8', link: '/pages/invoice/preview/sales', icon: 'icon-invoice-new', description: 'Lorem' },
+            { label: 'Purchase', link: '/pages/proforma-invoice/invoice/purchase', icon: 'icon-invoice-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Purchase Order', link: '/pages/purchase-management/purchase/order', icon: 'icon-purchase-order', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Vendor', link: '/pages/contact/vendor', icon: 'icon-vendor', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Aging Report', link: '/pages/contact/aging-report', icon: 'icon-aging-report-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Purchase Register', link: '/pages/reports/purchase-register', icon: 'icon-register-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Template', link: '/pages/invoice/preview/templates/sales', icon: 'icon-template-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Debit Note', link: '/pages/invoice/preview/debit%20note', icon: 'icon-debit-note-new', description: 'Lorem ipsum dolor sit amet, consectetur' }
         ]
-    }
+    },
+    {
+        ["Other Voucher"]: [
+            { label: 'Credit Note', link: '/pages/invoice/preview/credit%20note', icon: 'icon-credit-note-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Debit Note', link: '/pages/invoice/preview/debit%20note', icon: 'icon-debit-note-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Recipt', link: '/pages/reports/receipt', icon: 'icon-reciept-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Template', link: '/pages/invoice/preview/templates/sales', icon: 'icon-template-new', description: 'Lorem ipsum dolor sit amet, consectetur' }
+        ]
+    },
+    {
+        ["Inventory"]: [  ]
+    },
+    {
+        ["Reports"]: [
+            { label: 'Balance Sheet', link: '/pages/trial-balance-and-profit-loss', icon: 'icon-balance-sheet-new', description: 'Lorem ipsum dolor sit amet, consectetur', additional: { tab: 'balance-sheet', tabIndex: 2 } },
+            { label: 'Profit and Loss', link: '/pages/trial-balance-and-profit-loss', icon: 'icon-proft-loss-new', description: 'Lorem ipsum dolor sit amet, consectetur', additional: { tab: 'profit-and-loss', tabIndex: 1 } },
+            { label: 'Trial balance', link: '/pages/trial-balance-and-profit-losst', icon: 'icon-trial-balance-new', description: 'Lorem ipsum dolor sit amet, consectetur', additional: { tab: 'trial-balance', tabIndex: 0 } },
+            { label: 'Sales Bifurcation ', link: '/pages/new-vs-old-invoices', icon: 'icon-sale-bifurcation', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Daybook', link: '/pages/daybook', icon: 'icon-daybook-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'TAX (GST)', link: '/pages/settings/taxes', icon: 'icon-tax-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'E-way bill', link: '/pages/invoice/ewaybill', icon: 'icon-eway-bill-new', description: 'Lorem ipsum dolor sit amet, consectetur' }
+        ]
+    },
+    {
+        ["Import"]: [
+            { label: 'Master', link: '/pages/trial-balance-and-profit-loss', icon: 'icon-master-new1', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Accounts', link: '/pages/import/account', icon: 'icon-proft-loss-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Invoice', link: '/pages/import/entries', icon: 'icon-invoice-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Group', link: '/pages/import/group', icon: 'icon-group-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Trial Balance', link: '/pages/trial-balance-and-profit-loss', icon: 'icon-trial-balance-new', description: 'Lorem ipsum dolor sit amet, consectetur' }
+        ]
+    },
+    {
+        ["Other"]: [
+            { label: 'Petty Cash', link: '/pages/expenses-manager?tab=pending&tabIndex=0', icon: 'icon-petty-cash', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Dashboard', link: '/pages/home', icon: 'icon-dashboard-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Cash Flow', link: '/pages/reports/cash-flow-statement', icon: 'icon-cash-flow-statement', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Columnar Report', link: '/pages/reports/monthly-columnar-report', icon: 'icon-monthly-columnar-report', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Reverse Charge', link: '/pages/reports/reverse-charge', icon: 'icon-reverse-charge', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Audit Log', link: '/pages/audit-logs', icon: 'icon-audit-log', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Search', link: '/pages/search', icon: 'icon-search-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Onboarding', link: '/pages/onboarding', icon: 'icon-onboarding-new', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Tally Import', link: '/pages/tallysync', icon: 'icon-tally-import', description: 'Lorem ipsum dolor sit amet, consectetur' },
+            { label: 'Company Impex', link: '/pages/company-import-export', icon: 'icon-company-impex', description: 'Lorem ipsum dolor sit amet, consectetur' }
+        ]
+    },
+
 ];
