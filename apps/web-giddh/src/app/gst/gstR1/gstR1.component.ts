904fcb642f03d2aa043d401d1db4c4ef328744af
shalinee-walkover <shalinee@msg91.com>
2021-04-12 17:15:34 +0530

    shalinee | test  | remove unneccesary code and done css nesting

--- apps/web-giddh/src/app/gst/gstR1/gstR1.component.ts
+++ apps/web-giddh/src/app/gst/gstR1/gstR1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 declare var jquery: any;
 declare var $: any;
