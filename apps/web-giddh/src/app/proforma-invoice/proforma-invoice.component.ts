f1e44a1ded1e0b56c3e0d2a038c3964aaef357b5
prateek-giddh <prateekmishra@giddh.com>
2021-04-22 20:52:15 +0530

    fix: Nullity issue and PO shipping auto fill PIN code issue

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -1965,7 +1965,15 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
         // If currency of item is null or undefined then treat it to be equivalent of company currency
         item.currency = item.currency || this.companyCurrency;
         this.isMulticurrencyAccount = item.currency !== this.companyCurrency;
-
+        if (item.addresses && item.addresses.length > 0) {
+            item.addresses.forEach(address => {
+                if (address && address.isDefault) {
+                    const defaultAddress: any = address;
+                    this.invFormData.accountDetails.billingDetails.pincode = defaultAddress.pincode;
+                    this.invFormData.accountDetails.shippingDetails.pincode = defaultAddress.pincode;
+                }
+            });
+        }
         if (this.isMulticurrencyAccount) {
             this.customerCurrencyCode = item.currency;
             this.companyCurrencyName = item.currency;
@@ -6613,7 +6621,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
                         this.purchaseBillCompany.billingDetails.stateCode = defaultAddress.stateCode;
                         this.purchaseBillCompany.billingDetails.stateName = defaultAddress.stateName;
                         this.purchaseBillCompany.billingDetails.gstNumber = defaultAddress.taxNumber;
-                        this.purchaseBillCompany.billingDetails.pincode = defaultAddress.pincode;
+                        this.purchaseBillCompany.billingDetails.pincode = defaultAddress.taxNumber;
                         this.isDeliverAddressFilled = true;
                     }
                 }
@@ -6640,6 +6648,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
                     this.purchaseBillCompany.shippingDetails.stateCode = defaultAddress.stateCode;
                     this.purchaseBillCompany.shippingDetails.stateName = defaultAddress.stateName;
                     this.purchaseBillCompany.shippingDetails.gstNumber = defaultAddress.taxNumber;
+                    this.purchaseBillCompany.shippingDetails.pincode = defaultAddress.pincode;
                 } else {
                     this.resetShippingAddress();
                 }
