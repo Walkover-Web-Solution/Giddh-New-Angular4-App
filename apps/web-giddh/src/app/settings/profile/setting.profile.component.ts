e5c6557568a59b09e1ea9ad990f78f49bda86a8d
ravindersingh <ravinder@walkover.in>
2021-05-04 18:24:16 +0530

    Working on settings module

--- apps/web-giddh/src/app/settings/profile/setting.profile.component.ts
+++ apps/web-giddh/src/app/settings/profile/setting.profile.component.ts
@@ -152,7 +152,11 @@ export class SettingProfileComponent implements OnInit, OnDestroy {
     public imgPath: string = '';
 
     private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
-
+    /* This will hold local JSON data */
+    public localeData: any = {};
+    /* This will hold common JSON data */
+    public commonLocaleData: any = {};
+    
     constructor(
         private commonService: CommonService,
         private companyService: CompanyService,
