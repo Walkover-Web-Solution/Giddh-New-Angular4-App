38cc606f8f9fa3c1d4649a0355a612f8eb4887f9
Sagar Prasad <sagarprasad@Sagars-MacBook-Air.local>
2021-05-21 13:56:59 +0530

    new updates

--- apps/web-giddh/src/app/ledger/ledger.component.ts
+++ apps/web-giddh/src/app/ledger/ledger.component.ts
@@ -2279,6 +2279,7 @@ export class LedgerComponent implements OnInit, OnDestroy {
     public hideUploadBankStatementModal(): void {
         if(this.importStatementModal) {
             this.importStatementModal.hide();
+            this.getBankTransactions();
         }
     }
 
