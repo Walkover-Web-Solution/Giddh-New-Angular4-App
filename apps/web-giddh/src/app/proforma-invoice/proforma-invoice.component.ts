2db80c729c0f5c7534b7da174ca0def7aeab500b
prateek-giddh <prateekmishra@giddh.com>
2021-06-09 12:03:27 +0530

    fix: Prod - Cash/bank accounts are not coming in payment mode while generating invoice from invoice>pending page.

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -1358,6 +1358,11 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
                         if (!obj.accountDetails.currencySymbol) {
                             obj.accountDetails.currencySymbol = '';
                         }
+                        if (this.isPendingVoucherType) {
+                            obj.accountDetails.name = results[0].account.name;
+                            obj.voucherDetails.customerName = results[0].account.name;
+                            this.loadBankCashAccounts(obj?.accountDetails?.currency?.code);
+                        }
                         this.invFormData = obj;
                         this.buildBulkData(this.invFormData.entries.length, 0);
                         this.checkVoucherEntries();
@@ -1418,7 +1423,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
                     if (this.isMultiCurrencyModule() || this.isPurchaseInvoice) {
                         this.initializeWarehouse();
                     }
-                    if (this.isSalesInvoice) {
+                    if (this.isSalesInvoice || this.isPendingVoucherType) {
                         this.loadBankCashAccounts(tempSelectedAcc.currency);
                     }
                     this.store.dispatch(this.accountActions.resetActiveAccount());
