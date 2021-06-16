7dd8c5a3d5a9ec2829225f6e4474b6fd028087cc
shalinee-walkover <shalinee@msg91.com>
2021-06-16 11:38:40 +0530

    shalinee | stage | remove sideBarStateChange

--- apps/web-giddh/src/app/shared/header/header.component.ts
+++ apps/web-giddh/src/app/shared/header/header.component.ts
@@ -1389,7 +1389,6 @@ export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy, AfterV
 
         setTimeout(() => {
             if (document.getElementsByClassName("setting-data") && document.getElementsByClassName("setting-data").length > 0) {
-                this.sideBarStateChange(true);
                 document.querySelector('body').classList.add('on-setting-page');
                 document.querySelector('body').classList.remove('page-has-tabs');
                 document.querySelector('body').classList.remove('on-user-page');
