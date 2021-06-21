1071e72aa1dd22a9c3e24905cd3d37aa35adbfd1
prateek-giddh <prateekmishra@giddh.com>
2021-06-21 23:45:34 +0530

    feat: Add Brotli compression

--- apps/web-giddh/webpack.stage.partial.js
+++ apps/web-giddh/webpack.stage.partial.js
@@ -1,4 +1,6 @@
 const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+const zlib = require("zlib");
 module.exports = {
     plugins: [
         new webpack.DefinePlugin({
@@ -27,5 +29,18 @@ module.exports = {
             'process.env.APP_FOLDER': JSON.stringify('')
         }),
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new CompressionPlugin({
+            filename: "[path][base].br",
+            algorithm: "brotliCompress",
+            test: /\.(js|css|html|svg)$/,
+            compressionOptions: {
+                params: {
+                    [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+                },
+            },
+            threshold: 0,
+            minRatio: 0.8,
+            deleteOriginalAssets: false
+        })
     ]
 }
