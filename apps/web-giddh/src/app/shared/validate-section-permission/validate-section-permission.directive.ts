c82c40447f23583ad83c1b84323542861a04b454
ravindersingh <ravinder@walkover.in>
2021-06-24 23:11:59 +0530

    Ravinder | Test | Displayed error message if there is no permission

--- apps/web-giddh/src/app/shared/validate-section-permission/validate-section-permission.directive.ts
+++ apps/web-giddh/src/app/shared/validate-section-permission/validate-section-permission.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, OnChanges } from "@angular/core";
+import { Directive, ElementRef, Input, OnChanges, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[validateSectionPermission]'
@@ -8,21 +8,29 @@ export class ValidateSectionPermissionDirective implements OnChanges {
     @Input() public hasPermission: boolean = true;
     @Input() public errorMessage: string = "You don't have permission to view this";
 
-    constructor(private elementRef: ElementRef) {
-
+    constructor(
+        private elementRef: ElementRef, 
+        private renderer: Renderer2) {
     }
 
     /**
-     * Initializes the directive
+     * Checks permissions and show/hide element
      *
      * @memberof ValidateSectionPermissionDirective
      */
     public ngOnChanges(): void {
         if(!this.hasPermission) {
-            //this.elementRef.nativeElement.innerHTML = this.errorMessage;
-            this.elementRef.nativeElement.classList.add('giddh-no-permissions');
+            if(!this.elementRef.nativeElement?.parentElement?.parentElement?.querySelector(".giddh-permission-error-message")) {
+                const errorDiv = this.renderer.createElement('div');
+                const errorText = this.renderer.createText(this.errorMessage);
+                errorDiv?.classList?.add("giddh-permission-error-message");
+                this.renderer.appendChild(errorDiv, errorText);
+
+                this.renderer.appendChild(this.elementRef.nativeElement?.parentElement, errorDiv);
+            }
+            this.elementRef.nativeElement?.classList?.add('giddh-no-permissions');
         } else {
-            this.elementRef.nativeElement.classList.remove('giddh-no-permissions');
+            this.elementRef.nativeElement?.classList?.remove('giddh-no-permissions');
         }
     }
 }
