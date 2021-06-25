064a66f8cbae6153dcfbedf1eacfdc0a4c9cff54
ravindersingh <ravinder@walkover.in>
2021-06-25 10:58:06 +0530

    Added translation for no permission error message

--- apps/web-giddh/src/app/shared/validate-section-permission/validate-section-permission.directive.ts
+++ apps/web-giddh/src/app/shared/validate-section-permission/validate-section-permission.directive.ts
@@ -1,16 +1,26 @@
-import { Directive, ElementRef, Input, OnChanges, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, Input, OnChanges, OnDestroy, Renderer2 } from "@angular/core";
+import { select, Store } from "@ngrx/store";
+import { ReplaySubject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import { AppState } from "../../store";
 
 @Directive({
     selector: '[validateSectionPermission]'
 })
 
-export class ValidateSectionPermissionDirective implements OnChanges {
+export class ValidateSectionPermissionDirective implements OnChanges, OnDestroy {
+    /** This defines if user has permission or not */
     @Input() public hasPermission: boolean = true;
-    @Input() public errorMessage: string = "You don't have permission to view this";
+    /** Taking error message as input */
+    @Input() public errorMessage: string = "";
+    /** Subject to release subscriptions */
+    private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
     constructor(
         private elementRef: ElementRef, 
-        private renderer: Renderer2) {
+        private renderer: Renderer2,
+        private store: Store<AppState>) {
+
     }
 
     /**
@@ -20,18 +30,39 @@ export class ValidateSectionPermissionDirective implements OnChanges {
      */
     public ngOnChanges(): void {
         if(!this.hasPermission) {
-            if(!this.elementRef.nativeElement?.parentElement?.parentElement?.querySelector(".giddh-no-permissions-error-message")) {
-                const errorDiv = this.renderer.createElement('div');
-                const errorText = this.renderer.createText(this.errorMessage);
-                errorDiv?.classList?.add("giddh-no-permissions-error-message");
-                this.renderer.appendChild(errorDiv, errorText);
+            this.store.pipe(select(state => state.session.commonLocaleData), takeUntil(this.destroyed$)).subscribe((response) => {
+                if (response) {
+                    let commonLocaleData = response;
+                    if(!this.errorMessage) {
+                        this.errorMessage = commonLocaleData?.app_giddh_no_permissions;
+                    }
 
-                this.renderer.appendChild(this.elementRef.nativeElement?.parentElement, errorDiv);
-            }
-            this.elementRef.nativeElement?.classList?.add('giddh-no-permissions');
+                    if(!this.elementRef.nativeElement?.parentElement?.parentElement?.querySelector(".giddh-no-permissions-error-message")) {
+                        const errorDiv = this.renderer.createElement('div');
+                        const errorText = this.renderer.createText(this.errorMessage);
+                        errorDiv?.classList?.add("giddh-no-permissions-error-message");
+                        errorDiv?.classList?.add("ml-3");
+                        errorDiv?.classList?.add("mt-2");
+                        this.renderer.appendChild(errorDiv, errorText);
+
+                        this.renderer.appendChild(this.elementRef.nativeElement?.parentElement, errorDiv);
+                    }
+                    this.elementRef.nativeElement?.classList?.add('giddh-no-permissions');
+                }
+            });
         } else {
             this.elementRef.nativeElement?.classList?.remove('giddh-no-permissions');
             this.elementRef.nativeElement?.parentElement?.parentElement?.querySelector(".giddh-no-permissions-error-message")?.remove();
         }
+    }
+
+    /**
+     * Unsubscribes from the listeners
+     *
+     * @memberof ValidateSectionPermissionDirective
+     */
+     public ngOnDestroy(): void {
+        this.destroyed$.next(true);
+        this.destroyed$.complete();
     }
 }
