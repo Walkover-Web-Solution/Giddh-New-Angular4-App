5f064c876037c01557eb58f60992ffc6fb69fe9d
Sagar Prasad <sagarprasad@Sagars-MacBook-Air.local>
2021-07-01 18:41:06 +0530

    update new

--- apps/web-giddh/src/app/invoice/preview/models/bulk-export-modal/bulk-export.component.ts
+++ apps/web-giddh/src/app/invoice/preview/models/bulk-export-modal/bulk-export.component.ts
@@ -64,14 +64,13 @@ export class BulkExportModal implements OnInit, OnDestroy {
             { label: this.localeData?.invoice_copy_options?.customer, value: 'CUSTOMER' },
             { label: this.localeData?.invoice_copy_options?.transport, value: 'TRANSPORT' }
         ];
-        this.getEmail();
     }
     
     /**
      * Get company email
-     * @returns updated recipient value to company email
+     * update recipient value to company email
      */
-    public getEmail(): any {
+    public getRecipientEmail(): void {
         this.store.pipe(select(appState => appState.session.user), take(1)).subscribe(result => {
             if (result && result.user) {
                 this.recipients = result.user.email;
