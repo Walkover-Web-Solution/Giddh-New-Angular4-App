116be20fc11670e483855c9e9977ec863e59c8d9
prateek-giddh <prateekmishra@giddh.com>
2021-07-02 19:24:19 +0530

    fix: Ledger issues

--- apps/web-giddh/src/app/ledger/ledger.component.ts
+++ apps/web-giddh/src/app/ledger/ledger.component.ts
@@ -866,11 +866,29 @@ export class LedgerComponent implements OnInit, OnDestroy {
         this.lc.showBankLedgerPanel = true;
     }
 
-    public hideBankLedgerPopup(e?: boolean) {
+    public hideBankLedgerPopup(event?: any) {
         // cuando se emita falso en caso de éxito del mapa de cuenta
-        if (!e) {
+        if (!event) {
             this.getBankTransactions();
             this.getTransactionData();
+        }
+        if (event && event.path) {
+            let classList = event.path.map(m => {
+                return m.classList;
+            });
+
+            if (classList && classList instanceof Array) {
+                const shouldNotClose = classList.some((className: DOMTokenList) => {
+                    if (!className) {
+                        return;
+                    }
+                    return className.contains('entry-picker') || className.contains('currencyToggler') || className.contains('bs-datepicker');
+                });
+
+                if (shouldNotClose) {
+                    return;
+                }
+            }
         }
         if (this.lc.currentBlankTxn) {
             this.lc.currentBlankTxn.showDropdown = false;
@@ -1267,6 +1285,7 @@ export class LedgerComponent implements OnInit, OnDestroy {
 
     public resetAdvanceSearch() {
         this.searchText = "";
+        this.isAdvanceSearchImplemented = false;
         if (this.advanceSearchComp) {
             this.advanceSearchComp.resetAdvanceSearchModal();
         }
