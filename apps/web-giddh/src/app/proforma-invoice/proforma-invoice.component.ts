7e1f03d5681bf4b8bcf2e50f18187e02b765a2f8
prateek-giddh <prateekmishra@giddh.com>
2021-07-15 14:12:05 +0530

    fix: Revert unwanted changes

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -3746,7 +3746,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
                     voucher: data,
                     entries: [],
                     date: data.voucherDetails.voucherDate,
-                    type: this.invoiceType,
+                    type: this.proformaInvoiceUtilityService.parseVoucherType(this.invoiceType),
                     exchangeRate: exRate,
                     dueDate: data.voucherDetails.dueDate,
                     number: this.invoiceNo,
