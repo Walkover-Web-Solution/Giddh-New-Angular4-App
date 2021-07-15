8de7403cab121cca2cbce394cbc2e2c9aa4ee652
prateek-giddh <prateekmishra@giddh.com>
2021-07-15 14:54:50 +0530

    fix: Remove unwanted code

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -1919,8 +1919,6 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
         if (this.container) {
             this.container.clear();
         }
-        this.customerCountryName = '';
-        this.customerCountryCode = '';
         this.showSwitchedCurr = false;
         this.autoSaveIcon = false;
         this.autoFillShipping = true;
