7e803d140d061361a7f6d03e48c493682172ce47
prateek-giddh <prateekmishra@giddh.com>
2021-07-17 17:01:32 +0530

    PROD- Sales Invoice is not getting created for multi-currency, if customer is created from Invoice page.

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -1268,7 +1268,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
                         this.isMulticurrencyAccount = tempSelectedAcc.currencySymbol !== this.baseCurrencySymbol;
                         this.customerCountryName = tempSelectedAcc.country.countryName;
                         this.countryCode = tempSelectedAcc?.country?.countryCode || 'IN';
-
+                        this.checkIfNeedToExcludeTax(tempSelectedAcc);
                         this.getUpdatedStateCodes(tempSelectedAcc.country.countryCode).then(() => {
                             this.invFormData.accountDetails = new AccountDetailsClass(tempSelectedAcc);
                         });
