837d57564c29c3969948b9cc7cf24d53f29b10bb
prateek-giddh <prateekmishra@giddh.com>
2021-07-19 12:54:43 +0530

    refactor: Resolve code review comments

---
+++ apps/web-giddh/src/app/shared/no-data/no-data.component.ts
@@ -0,0 +1,38 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit} from "@angular/core";
+import { LocaleService } from "../../services/locale.service";
+
+@Component({
+    selector: 'no-data',
+    templateUrl: './no-data.component.html',
+    styleUrls: ['./no-data.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NoDataComponent implements OnInit {
+    /** Primary message for no data */
+    @Input() primaryMessage: string;
+    /** Primary message for no data */
+    @Input() primaryMessageClass: string;
+    /** Secondary message for no data */
+    @Input() secondaryMessage: string;
+    /** Secondary message for no data */
+    @Input() secondaryMessageClass: string;
+
+    /** @ignore */
+    constructor(
+        private localeService: LocaleService
+    ) {}
+
+    /**
+     * Initializes the component message
+     *
+     * @memberof NoDataComponent
+     */
+    ngOnInit(): void {
+        if (!this.primaryMessage) {
+            this.primaryMessage = this.localeService.translate("app_no_entries_found");
+        }
+        if (!this.secondaryMessage) {
+            this.secondaryMessage = this.localeService.translate("app_search_suggestion");
+        }
+    }
+}
