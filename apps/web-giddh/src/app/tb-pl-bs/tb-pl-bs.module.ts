1f5a61d88909c98ff93f2e677ee68de6eb5b3168
prateek-giddh <prateekmishra@giddh.com>
2021-07-27 15:29:16 +0530

    feat: Remove dependency from SharedModule

--- apps/web-giddh/src/app/tb-pl-bs/tb-pl-bs.module.ts
+++ apps/web-giddh/src/app/tb-pl-bs/tb-pl-bs.module.ts
@@ -1,40 +1,42 @@
-import { CurrencyModule } from './../shared/helpers/pipes/currencyPipe/currencyType.module';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { TbPlBsComponent } from './tb-pl-bs.component';
-import { TbGridComponent } from './components/tb/tb-grid/tb-grid.component';
-import { TbPlBsFilterComponent } from './components/filter/tb-pl-bs-filter.component';
-import { TbPlBsRoutingModule } from './tb-pl-bs.routing.module';
-import { TlPlGridRowComponent } from './components/tb-pl-bs-grid-row.component';
-import { TrialAccordionDirective } from './components/trial-accordion.directive';
-import { TbComponent } from './components/tb/tb.component';
-import { PlComponent } from './components/pl/pl.component';
-import { PlGridComponent } from './components/pl/pl-grid/pl-grid.component';
-import { PlGridRowComponent } from './components/pl/pl-grid/pl-grid-row.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LaddaModule } from 'angular2-ladda';
+import { ClickOutsideModule } from 'ng-click-outside';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
+import { HamburgerMenuModule } from '../shared/header/components/hamburger-menu/hamburger-menu.module';
+import { TbsearchPipe } from '../shared/header/pipe/tbsearch.pipe';
+import { HighlightModule } from '../shared/helpers/pipes/highlightPipe/highlight.module';
+import { RecTypeModule } from '../shared/helpers/pipes/recType/recType.module';
+import { AccountDetailModalModule } from '../theme/account-detail-modal/account-detail-modal.module';
+import { ShSelectModule } from '../theme/ng-virtual-select/sh-select.module';
 import { Daterangepicker } from '../theme/ng2-daterangepicker/daterangepicker.module';
-import { BsComponent } from './components/bs/bs.component';
-import { BsGridComponent } from './components/bs/bs-grid/bs-grid.component';
+import { TranslateDirectiveModule } from '../theme/translate/translate.directive.module';
+import { CurrencyModule } from './../shared/helpers/pipes/currencyPipe/currencyType.module';
 import { BsGridRowComponent } from './components/bs/bs-grid/bs-grid-row.component';
+import { BsGridComponent } from './components/bs/bs-grid/bs-grid.component';
+import { BsComponent } from './components/bs/bs.component';
+import { BsExportXlsComponent } from './components/export/bs-export-xls.component';
 import { PlExportXlsComponent } from './components/export/pl-export-xls.component';
 import { TbExportCsvComponent } from './components/export/tb-export-csv.component';
 import { TbExportPdfComponent } from './components/export/tb-export-pdf.component';
 import { TbExportXlsComponent } from './components/export/tb-export-xls.component';
-import { BsExportXlsComponent } from './components/export/bs-export-xls.component';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { TbsearchPipe } from '../shared/header/pipe/tbsearch.pipe';
-import { LaddaModule } from 'angular2-ladda';
-import { HighlightModule } from '../shared/helpers/pipes/highlightPipe/highlight.module';
-import { RecTypeModule } from '../shared/helpers/pipes/recType/recType.module';
-import { ShSelectModule } from '../theme/ng-virtual-select/sh-select.module';
-import { AccountDetailModalModule } from '../theme/account-detail-modal/account-detail-modal.module';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { ClickOutsideModule } from 'ng-click-outside';
-import { SharedModule } from '../shared/shared.module';
-import { ScrollingModule } from '@angular/cdk/scrolling';
+import { TbPlBsFilterComponent } from './components/filter/tb-pl-bs-filter.component';
+import { PlGridRowComponent } from './components/pl/pl-grid/pl-grid-row.component';
+import { PlGridComponent } from './components/pl/pl-grid/pl-grid.component';
+import { PlComponent } from './components/pl/pl.component';
+import { TlPlGridRowComponent } from './components/tb-pl-bs-grid-row.component';
+import { TbGridComponent } from './components/tb/tb-grid/tb-grid.component';
+import { TbComponent } from './components/tb/tb.component';
+import { TrialAccordionDirective } from './components/trial-accordion.directive';
 import { AccountsFilterPipe } from './pipes/accounts-filter.pipe';
+import { TbPlBsComponent } from './tb-pl-bs.component';
+import { TbPlBsRoutingModule } from './tb-pl-bs.routing.module';
 
 @NgModule({
     declarations: [
@@ -79,8 +81,9 @@ import { AccountsFilterPipe } from './pipes/accounts-filter.pipe';
         CurrencyModule,
         TooltipModule,
         AccountDetailModalModule,
-        SharedModule,
-        ScrollingModule
+        ScrollingModule,
+        TranslateDirectiveModule,
+        HamburgerMenuModule
     ],
 })
 export class TBPlBsModule {
