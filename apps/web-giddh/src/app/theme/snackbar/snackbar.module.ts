b444e1957104d6bbd2dba9cb6ca502aa65efdb82
ravindersingh <ravinder@walkover.in>
2021-08-23 10:23:39 +0530

    Ravinder | Test | Fixed Bugs

--- apps/web-giddh/src/app/theme/snackbar/snackbar.module.ts
+++ apps/web-giddh/src/app/theme/snackbar/snackbar.module.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { SnackBarComponent } from "./snackbar.component";
 
 @NgModule({
     declarations: [
         SnackBarComponent
+    ],
+    imports: [
+        CommonModule
     ],
     exports: [
         SnackBarComponent
