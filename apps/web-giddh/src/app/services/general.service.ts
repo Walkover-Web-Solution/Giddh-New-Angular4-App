373d98e55c860dde9b2e7fd8fc61d16d54754070
ravindersingh <ravinder@walkover.in>
2021-08-23 21:48:20 +0530

    Ravinder | Test | IN UAE BASED COMPANY GSTR-1,2,3B and E-way Bill MODULE SHOWN

--- apps/web-giddh/src/app/services/general.service.ts
+++ apps/web-giddh/src/app/services/general.service.ts
@@ -778,19 +778,20 @@ export class GeneralService {
     /**
      * Returns the visible menu items to be shown for menu panel (as per permission)
      *
-     * @param {string} module name
-     * @param {Array<any>} apiItems List of permissible items obtained from API
-     * @param {Array<AllItems>} itemList List of all the items of menu
-     * @returns {Array<AllItems>} Array of permissible menu items
+     * @param {string} module
+     * @param {Array<any>} apiItems
+     * @param {Array<AllItems>} itemList
+     * @param {*} countryCode
+     * @returns {Array<AllItems>}
      * @memberof GeneralService
      */
-    public getVisibleMenuItems(module: string, apiItems: Array<any>, itemList: Array<AllItems>): Array<AllItems> {
+    public getVisibleMenuItems(module: string, apiItems: Array<any>, itemList: Array<AllItems>, countryCode: any = ""): Array<AllItems> {
         const visibleMenuItems = cloneDeep(itemList);
         itemList?.forEach((menuItem, menuIndex) => {
             visibleMenuItems[menuIndex].items = [];
             menuItem.items?.forEach(item => {
                 const isValidItem = apiItems.find(apiItem => apiItem.uniqueName === item.link);
-                if ((isValidItem && item.hide !== module) || (item.alwaysPresent && item.hide !== module)) {
+                if (((isValidItem && item.hide !== module) || (item.alwaysPresent && item.hide !== module)) && (!item.additional?.countrySpecific?.length || item.additional?.countrySpecific?.indexOf(countryCode) > -1)) {
                     // If items returned from API have the current item which can be shown in branch/company mode, add it
                     visibleMenuItems[menuIndex].items.push(item);
                 }
