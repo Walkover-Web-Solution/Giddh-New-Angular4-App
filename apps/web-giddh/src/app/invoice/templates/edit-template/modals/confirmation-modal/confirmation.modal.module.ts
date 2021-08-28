310f158a06534b99ba29c07908616c09d09f5a32
ravindersingh <ravinder@walkover.in>
2021-08-28 17:24:06 +0530

    Ravinder | Test | Fixed Invoice Close popup blank message issue

--- apps/web-giddh/src/app/invoice/templates/edit-template/modals/confirmation-modal/confirmation.modal.module.ts
+++ apps/web-giddh/src/app/invoice/templates/edit-template/modals/confirmation-modal/confirmation.modal.module.ts
@@ -1,7 +1,11 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { DeleteTemplateConfirmationModelComponent } from "./confirmation.modal.component";
 
 @NgModule({
+    imports: [
+        CommonModule
+    ],
     declarations: [
         DeleteTemplateConfirmationModelComponent
     ],
