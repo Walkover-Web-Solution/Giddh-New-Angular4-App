c657ae106c948b527a3ead3f7051747f7a3f149f
ravindersingh <ravinder@walkover.in>
2021-09-02 18:56:49 +0530

    Fixed PR Issue

--- apps/web-giddh/src/app/actions/ledger/ledger.actions.ts
+++ apps/web-giddh/src/app/actions/ledger/ledger.actions.ts
@@ -161,6 +161,11 @@ export class LedgerActions {
                 } else if (response.status === 'no-network') {
                     this.ResetUpdateLedger();
                     return { type: 'EmptyAction' };
+                } else if(response.status === 'confirm') {
+                    return {
+                        type: LEDGER.SHOW_DUPLICATE_VOUCHER_CONFIRMATION,
+                        payload: response
+                    }
                 } else {
                     this.toaster.showSnackBar("success", this.localeService.translate("app_messages.entry_updated"));
                     if (action && action.payload && action.payload.request && action.payload.request.refreshLedger) {
@@ -662,14 +667,14 @@ export class LedgerActions {
         };
     }
 
-    private validateResponse<TResponse, TRequest>(response: BaseResponse<TResponse, TRequest>, successAction: CustomActions, showToast: boolean = false, errorAction: CustomActions = { type: 'EmptyAction' }, isCreateUpdateLedger?: boolean): CustomActions {
+    private validateResponse<TResponse, TRequest>(response: BaseResponse<TResponse, TRequest>, successAction: CustomActions, showToast: boolean = false, errorAction: CustomActions = { type: 'EmptyAction' }, isCreateLedger?: boolean): CustomActions {
         if (response.status === 'error') {
             if (showToast) {
                 this.toaster.showSnackBar("error", response.message);
             }
             return errorAction;
         } else if(response.status === "confirm") {
-            if(isCreateUpdateLedger) {
+            if(isCreateLedger) {
                 return {
                     type: LEDGER.SHOW_DUPLICATE_VOUCHER_CONFIRMATION,
                     payload: response
