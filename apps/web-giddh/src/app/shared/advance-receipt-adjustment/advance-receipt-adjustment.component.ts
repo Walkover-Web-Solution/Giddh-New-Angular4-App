fe269ab4cf472d417930ec1b89c812f1c94f01ab
ravindersingh <ravinder@walkover.in>
2021-09-06 11:49:40 +0530

    Fixed PR Issue

--- apps/web-giddh/src/app/shared/advance-receipt-adjustment/advance-receipt-adjustment.component.ts
+++ apps/web-giddh/src/app/shared/advance-receipt-adjustment/advance-receipt-adjustment.component.ts
@@ -169,8 +169,8 @@ export class AdvanceReceiptAdjustmentComponent implements OnInit, OnDestroy {
                 }
             }
             this.salesService.getInvoiceList(requestObject, this.invoiceFormDetails.voucherDetails.voucherDate).pipe(takeUntil(this.destroyed$)).subscribe((response) => {
-                if (response?.body?.results?.length > 0) {
-                    this.allAdvanceReceiptResponse = response.body.results.map(result => ({ ...result, adjustmentAmount: { amountForAccount: result.balanceDue.amountForAccount, amountForCompany: result.balanceDue.amountForCompany } }));
+                if (response && response.body) {
+                    this.allAdvanceReceiptResponse = response.body.results?.map(result => ({ ...result, adjustmentAmount: { amountForAccount: result.balanceDue.amountForAccount, amountForCompany: result.balanceDue.amountForCompany } }));
                     this.adjustVoucherOptions = [];
                     if (this.allAdvanceReceiptResponse && this.allAdvanceReceiptResponse.length) {
                         this.allAdvanceReceiptResponse.forEach(item => {
