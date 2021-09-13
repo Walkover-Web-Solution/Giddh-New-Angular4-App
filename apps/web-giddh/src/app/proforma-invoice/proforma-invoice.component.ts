942d671abae1a0575b5c4f1960dc8700c6d8edd4
ravindersingh <ravinder@walkover.in>
2021-09-13 12:58:52 +0530

    Fixed PR Issue

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -2544,9 +2544,6 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
                 }
                 if (this.isCreditNote || this.isDebitNote) {
                     updatedData['invoiceNumberAgainstVoucher'] = this.invFormData.voucherDetails.voucherNumber;
-                    if(this.voucherApiVersion === 2) {
-                        updatedData['number'] = this.invFormData.voucherDetails.voucherNumber;
-                    }
                     updatedData['invoiceLinkingRequest'] = data.voucherDetails.invoiceLinkingRequest;
                 }
                 if (this.isCreditNote) {
@@ -3947,9 +3944,6 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
                 } as GenericRequestForGenerateSCD;
                 if (this.isCreditNote || this.isDebitNote) {
                     requestObject['invoiceNumberAgainstVoucher'] = this.invFormData.voucherDetails.voucherNumber;
-                    if(this.voucherApiVersion === 2) {
-                        requestObject['number'] = this.invFormData.voucherDetails.voucherNumber;
-                    }
                 }
                 if (((this.isCreditNote || this.isDebitNote) && this.selectedInvoice) || this.isSalesInvoice) {
                     if (this.isSalesInvoice) {
