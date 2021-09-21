b5dff6e69ffa4ab4c59d6b8a61cfc688793dea20
ravindersingh <ravinder@walkover.in>
2021-09-21 12:35:41 +0530

    Ravinder | Test | On invoice page, select all checkbox is not coming when closing bulk update pop up

--- apps/web-giddh/src/app/invoice/preview/invoice.preview.component.ts
+++ apps/web-giddh/src/app/invoice/preview/invoice.preview.component.ts
@@ -539,6 +539,7 @@ export class InvoicePreviewComponent implements OnInit, OnChanges, OnDestroy {
 
         this.store.pipe(select(state => state.invoice.isGenerateBulkInvoiceCompleted), takeUntil(this.destroyed$)).subscribe(response => {
             if (response) {
+                this.updateSelectedItems = true;
                 this.getVoucher(this.isUniversalDateApplicable);
                 this.store.dispatch(this.invoiceActions.resetBulkEInvoice());
             }
