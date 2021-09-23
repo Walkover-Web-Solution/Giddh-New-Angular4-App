e40c7abfeaf12279720fb2a05f27398d119e8088
ravindersingh <ravinder@walkover.in>
2021-09-23 19:26:05 +0530

    Updated nedb integration code

--- apps/electron-giddh/src/offline-api/services/local/companies.ts
+++ apps/electron-giddh/src/offline-api/services/local/companies.ts
@@ -1,3 +1,7 @@
+import Datastore from "nedb";
+import { findAsync, insertAsync, updateAsync } from "../../helpers/nedb_async";
+import { createDbFile, getPath } from "../../helpers/general";
+
 /**
  * This will save the companies list
  *
@@ -7,6 +11,17 @@
 export async function saveCompaniesLocal(request): Promise<any> {
     if (request && request.status === "success") {
         const companiesList = request.body;
+        const filename = getPath() + "companies.db";
+        createDbFile(filename);
+
+        const db = new Datastore({ filename: filename, autoload: true });
+        const response = await findAsync(db, {});
+
+        if (response?.length > 0) {
+            updateAsync(db, {}, { $set: companiesList }, { multi: false });
+        } else {
+            insertAsync(db, companiesList);
+        }
 
         return { status: "success", body: companiesList };
     } else {
@@ -21,10 +36,14 @@ export async function saveCompaniesLocal(request): Promise<any> {
  * @returns
  */
 export async function getCompaniesLocal(request: any): Promise<any> {
-    const companies = {};
+    const filename = getPath() + "companies.db";
+    createDbFile(filename);
 
-    if (companies) {
-        return { status: "success", body: companies };
+    const db = new Datastore({ filename: filename, autoload: true });
+    const response = await findAsync(db, {});
+
+    if (response?.length > 0) {
+        return { status: "success", body: response };
     } else {
         return { status: "error", message: "Companies list unavailable." };
     }
