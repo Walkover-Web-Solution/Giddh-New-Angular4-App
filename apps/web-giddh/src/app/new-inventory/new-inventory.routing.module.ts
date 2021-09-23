2315c2f8ec58f04e99afce5ac279382672588074
Govinda Alwnai <govi2010@live.in>
2021-09-23 16:53:26 +0530

    new inventory in group wise design fixed

--- apps/web-giddh/src/app/new-inventory/new-inventory.routing.module.ts
+++ apps/web-giddh/src/app/new-inventory/new-inventory.routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { NewInventoryComponent } from "./new-inventory.component";
 import { InventoryCreateUpdateGroupComponent } from "./component/stock-group/create-update-group/create-update-group.component";
 import { AboutGroupDetailComponent } from "./component/about-group-detail/about-group-detail.component";
@@ -13,69 +13,99 @@ import { AdjustProductServiceComponent } from "./component/adjust-product-servic
 import { CreateNewInventoryComponent } from "./component/create-new-inventory-component/create-new-inventory.component";
 import { InventoryDashboardComponent } from "./component/inventory-dashboard/inventory-dashboard.component";
 import { DashboardSellingReport } from "./component/inventory-dashboard/dashboard-selling-report/dashboard-selling-report.component";
-@NgModule({
-    imports: [
-        RouterModule.forChild([
-            {
-                path: "",
-                component: NewInventoryComponent,
-            },
-            {
-                path: "stock-group/create",
-                component: InventoryCreateUpdateGroupComponent,
-            },
-            {
-                path: "stock-group/edit/:groupUniqueName",
-                component: InventoryCreateUpdateGroupComponent,
-            },
-            {
-                path: "about-group-detail",
-                component: AboutGroupDetailComponent,
+import { NewInventoryGroupComponent } from "./component/inventory-group/inventory-group.component";
+import { ProductServiceListComponent } from "./component/inventory-product-service-list/inventory-product-service-list.component";
+import { InventoryTransactionListComponent } from "./component/inventory-transaction-list/inventory-transaction-list.component";
+import { InventoryCustomFieldComponent } from "./component/inventory-custom-field/inventory-custom-field.component";
 
+const routes: Routes = [
+    {
+        path: "", component: NewInventoryComponent,
+        children: [
+            { path: "", redirectTo: "inventory-group", pathMatch: "full" },
+
+            {
+                path: "inventory-group",
+                component: NewInventoryGroupComponent,
             },
             {
-                path: "about-product-service-detail",
-                component: AboutProductServiceDetailComponent,
+                path: "inventory-product-service-list",
+                component: ProductServiceListComponent,
             },
             {
                 path: "inventory-combo-list",
                 component: InventoryComboListComponent,
             },
             {
-                path: "about-combo-detail",
-                component: AboutComboDetailComponent
+                path: "inventory-transaction-list",
+                component: InventoryTransactionListComponent,
             },
             {
-                path: "create-custom-field",
-                component: CreateCustomFieldComponent
+                path: "inventory-custom-field",
+                component: InventoryCustomFieldComponent,
             },
-            {
-                path: "adjust-inventory",
-                component: AdjustInventoryComponent
-            },
-            {
-                path: "adjust-inventory-group",
-                component: AdjustGroupComponent
-            },
-            {
-                path: "adjust-product-and-service",
-                component: AdjustProductServiceComponent
-            },
-            {
-                path: "create-new-inventory",
-                component: CreateNewInventoryComponent,
-            },
-            {
-                path: "inventory-dashboard",
-                component: InventoryDashboardComponent,
-            },
-            {
-                path: "top-selling-report",
-                component: DashboardSellingReport,
-            }
+        ],
+    },
+    {
+        path: "stock-group/create",
+        component: InventoryCreateUpdateGroupComponent,
+    },
+    {
+        path: "stock-group/edit/:groupUniqueName",
+        component: InventoryCreateUpdateGroupComponent,
+    },
+    {
+        path: "about-group-detail",
+        component: AboutGroupDetailComponent,
 
-        ])
-    ],
-    exports: [RouterModule]
+    },
+    {
+        path: "about-product-service-detail",
+        component: AboutProductServiceDetailComponent,
+    },
+    {
+        path: "inventory-combo-list",
+        component: InventoryComboListComponent,
+    },
+    {
+        path: "about-combo-detail",
+        component: AboutComboDetailComponent,
+    },
+    {
+        path: "create-custom-field",
+        component: CreateCustomFieldComponent,
+    },
+    {
+        path: "adjust-inventory",
+        component: AdjustInventoryComponent,
+    },
+    {
+        path: "adjust-inventory-group",
+        component: AdjustGroupComponent,
+    },
+    {
+        path: "adjust-product-and-service",
+        component: AdjustProductServiceComponent,
+    },
+    {
+        path: "create-new-inventory",
+        component: CreateNewInventoryComponent,
+    },
+    {
+        path: "inventory-dashboard",
+        component: InventoryDashboardComponent,
+    },
+    {
+        path: "top-selling-report",
+        component: DashboardSellingReport,
+    },
+];
+
+
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class NewInventoryRoutingModule { }
+export class NewInventoryRoutingModule {
+}
