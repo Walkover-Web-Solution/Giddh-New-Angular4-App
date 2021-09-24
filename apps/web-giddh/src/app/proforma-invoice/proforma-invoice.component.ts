1e2dcd9c64f1dcf08bb8d786ed7bc53169731c10
deepGiddh <deep@whozzat.com>
2021-09-24 14:13:27 +0530

    Deep|Test|Added the SafeNavigation Operator

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -721,7 +721,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
      */
     public toggleRcmCheckbox(event: any): void {
         event.preventDefault();
-        this.rcmConfiguration = this.generalService.getRcmConfiguration(event.target.checked, this.commonLocaleData);
+        this.rcmConfiguration = this.generalService.getRcmConfiguration(event?.target?.checked, this.commonLocaleData);
     }
 
     /**
@@ -3542,11 +3542,11 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
 
     @HostListener('document:click', ['$event'])
     public clickedOutside(event) {
-        if (this.copyPreviousEstimate && this.copyPreviousEstimate.nativeElement && !this.copyPreviousEstimate.nativeElement.contains(event.target)) {
+        if (this.copyPreviousEstimate && this.copyPreviousEstimate.nativeElement && !this.copyPreviousEstimate.nativeElement.contains(event?.target)) {
             this.showLastEstimateModal = false;
         }
 
-        if (this.unregisteredBusiness && this.unregisteredBusiness.nativeElement && !this.unregisteredBusiness.nativeElement.contains(event.target)) {
+        if (this.unregisteredBusiness && this.unregisteredBusiness.nativeElement && !this.unregisteredBusiness.nativeElement.contains(event?.target)) {
             this.showGstTreatmentModal = false;
         }
     }
@@ -5487,7 +5487,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
             }, () => {
                 this.startLoader(false);
                 this._toasty.errorToast(this.commonLocaleData?.app_something_went_wrong);
-            });   
+            });
         }
     }
 
