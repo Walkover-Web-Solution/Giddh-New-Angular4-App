b234d6b9ddfd90024026bf15c36aacb285c4013a
ravindersingh <ravinder@walkover.in>
2021-09-24 12:18:53 +0530

    Ravinder | Test | discount column issue in debit note for customer

--- apps/web-giddh/src/app/material-ledger/components/new-ledger-entry-panel/new-ledger-entry-panel.component.ts
+++ apps/web-giddh/src/app/material-ledger/components/new-ledger-entry-panel/new-ledger-entry-panel.component.ts
@@ -368,7 +368,7 @@ export class NewLedgerEntryPanelComponent implements OnInit, OnDestroy, OnChange
     }
 
     public ngOnChanges(changes: SimpleChanges): void {
-        if (this.currentTxn && this.currentTxn.selectedAccount) {
+        if (changes?.currentTxn?.currentValue?.selectedAccount) {
             this.currentTxn.advanceReceiptAmount = giddhRoundOff(this.currentTxn.amount, this.giddhBalanceDecimalPlaces);
             if (!this.currentTxn.selectedAccount.stock) {
                 this.selectedWarehouse = String(this.defaultWarehouse);
