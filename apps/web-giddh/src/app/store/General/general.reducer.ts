1ec28e11056c40e02e031cc9b64510d356c3b50b
deepGiddh <deep@whozzat.com>
2021-10-09 11:30:08 +0530

    Deep|Test|Fixed the Formatting

--- apps/web-giddh/src/app/store/General/general.reducer.ts
+++ apps/web-giddh/src/app/store/General/general.reducer.ts
@@ -296,7 +296,7 @@ export function GeneRalReducer(state: GeneralState = initialState, action: Custo
             }
         }
         case GENERAL_ACTIONS.UPDATE_CURRENT_LIABILITIES: {
-            if(state && state.flattenAccounts){
+            if(state?.flattenAccounts){
                let flattenAccountsArray = [...state.flattenAccounts];
                flattenAccountsArray = flattenAccountsArray.filter(account => account.uniqueName !== action.payload);
                return {
