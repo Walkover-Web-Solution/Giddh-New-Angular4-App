d1d6b96089eb1f3312975ac3797bc1078ddc69f4
deepGiddh <deep@whozzat.com>
2021-10-09 10:34:13 +0530

    Deep|Test|Fixed the issue Cannot read property 'length' of null

--- apps/web-giddh/src/app/store/General/general.reducer.ts
+++ apps/web-giddh/src/app/store/General/general.reducer.ts
@@ -296,11 +296,13 @@ export function GeneRalReducer(state: GeneralState = initialState, action: Custo
             }
         }
         case GENERAL_ACTIONS.UPDATE_CURRENT_LIABILITIES: {
-            let flattenAccountsArray = [...(state.flattenAccounts ||[])];
-            flattenAccountsArray = flattenAccountsArray.filter(account => account.uniqueName !== action.payload)
-            return {
+            if(state && state.flattenAccounts){
+               let flattenAccountsArray = [...state.flattenAccounts];
+               flattenAccountsArray = flattenAccountsArray.filter(account => account.uniqueName !== action.payload)
+               return {
                 ...state,
                 flattenAccounts: flattenAccountsArray
+               }
             }
         }
         case GENERAL_ACTIONS.UPDATE_INDEX_DB: {
