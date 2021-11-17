e45404ee641967d0db69c19acd1aa1c78bb182de
ravindersingh <ravinder@walkover.in>
2021-11-17 18:40:36 +0530

    Ravinder | Test | Change detection issue after deleting file

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -7450,6 +7450,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
                 this.selectedFileName = '';
                 this.invFormData.entries[0].attachedFile = '';
                 this._toasty.successToast(response?.body);
+                this._cdr.detectChanges();
             } else {
                 this._toasty.errorToast(response?.message)
             }
