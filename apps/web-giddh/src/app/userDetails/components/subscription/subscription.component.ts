a0cb5b2dcd1feeb18d672856f505fee4f60ea14e
Dilpreet Singh <dilpreet.dang99@gmail..com>
2021-11-30 19:24:34 +0530

    integrate code with data binding searching

--- apps/web-giddh/src/app/userDetails/components/subscription/subscription.component.ts
+++ apps/web-giddh/src/app/userDetails/components/subscription/subscription.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit, ViewChild, Input } from "@angular/core";
+import { Component, OnInit, ViewChild, Input, OnDestroy, ChangeDetectorRef, ChangeDetectionStrategy, SimpleChanges, OnChanges } from "@angular/core";
 import { BreakpointObserver, BreakpointState } from "@angular/cdk/layout";
 import { MatAccordion } from "@angular/material/expansion";
 import { MatDialog } from "@angular/material/dialog";
 import { SubscriptionsActions } from '../../../actions/userSubscriptions/subscriptions.action';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { AppState } from '../../../store/roots';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { SubscriptionsUser } from "../../../models/api-models/Subscriptions";
+import { Observable, ReplaySubject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import { SubscriptionsService } from "../../../services/subscriptions.service";
+import { uniqBy } from "../../../lodash-optimized";
+import * as moment from "moment";
+import { GIDDH_DATE_FORMAT } from "../../../shared/helpers/defaultDateFormat";
+import { FormControl } from "@angular/forms";
 
 
 export interface PeriodicElement {
@@ -23,10 +31,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
 @Component({
     selector: 'subscription',
     templateUrl: './subscription.component.html',
-    styleUrls: ['./subscription.component.scss']
+    styleUrls: ['./subscription.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class SubscriptionComponent implements OnInit {
+export class SubscriptionComponent implements OnInit, OnDestroy, OnChanges {
 
     // This will change the search bar height dynamically
     public menuBarHeight: Number = 40;
@@ -49,10 +58,82 @@ export class SubscriptionComponent implements OnInit {
     @ViewChild("moveCompany", { static: false }) public moveCompany: any;
     public modalRef: BsModalRef;
 
-    constructor(public dialog: MatDialog, public breakpointObserver: BreakpointObserver, private subscriptionsActions: SubscriptionsActions, private store: Store<AppState>) { }
+    // This will store the subscriptions in blank array
+    public subscriptions: SubscriptionsUser[] = [];
+    // This will get the subscriptions data 
+    public subscriptions$: Observable<SubscriptionsUser[]>;
+    // This will selected the subscriptions data 
+    public selectedSubscription: any;
+    // This will hold the plan list data by unique name 
+    public plansList: any[] = [];
+
+    // This will store the static data in expiry list 
+    public expiringList: any[] = [
+        { name: '7 Days', value: 7 },
+        { name: '15 Days', value: 15 },
+        { name: '1 Month', value: 31 },
+        { name: '6 Months', value: 180 },
+        { name: '1 Year', value: 365 }
+    ];
+
+    // This will store the static data in transaction balance list 
+    public transactionBalanceList: any[] = [
+        { name: 'Less than 1,000', value: 1000 },
+        { name: 'Less than 5,000', value: 5000 },
+        { name: 'Less than 10,000', value: 10000 },
+        { name: 'Less than 50,000', value: 50000 }
+    ];
+
+    // This will search the subscriptions and plans in subscription list 
+    public searchSubscription: FormControl = new FormControl();
+
+    private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
+    // This will filter the plan dropdwon , expiry dropdown and transaction balance dropdown in subscription list 
+    public filters: any = { plan: '', expiration: '', transactionBalance: '' };
+
+    constructor(
+        public dialog: MatDialog,
+        public breakpointObserver:
+            BreakpointObserver,
+        private subscriptionsActions: SubscriptionsActions,
+        private store: Store<AppState>,
+        private subscriptionService: SubscriptionsService,
+        private changeDetectionRef: ChangeDetectorRef) {
+
+        this.subscriptions$ = this.store.pipe(select(state => state.subscriptions.subscriptions), takeUntil(this.destroyed$));
+    }
 
     public imgPath: string = '';
+
     public ngOnInit(): void {
+
+        //This service will use for get subscribed companies
+        this.subscriptionService.getSubScribedCompanies().pipe(takeUntil(this.destroyed$)).subscribe((res) => {
+            if (res?.status === "success" && res?.body) {
+                this.store.dispatch(this.subscriptionsActions.SubscribedCompaniesResponse(res));
+            }
+        });
+
+        // This subscription subscribes and get the response
+        this.subscriptions$.subscribe(response => {
+            if (response) {
+                this.subscriptions = response.map(subscription => {
+                    subscription.remainingDays = Number(moment(subscription.expiry, GIDDH_DATE_FORMAT).diff(moment(), 'days'));
+                    subscription.startedAt = moment(subscription.startedAt, GIDDH_DATE_FORMAT).format("D MMM, y");
+                    subscription.expiry = moment(subscription.expiry, GIDDH_DATE_FORMAT).format("D MMM, y");
+                    return subscription;
+                });
+                this.plansList = uniqBy(response.map(subscription => { return { name: subscription.planDetails?.name, uniqueName: subscription.planDetails?.uniqueName } }), "uniqueName");
+                this.changeDetectionRef.detectChanges();
+            }
+        });
+
+        // listen for search field value changes
+        this.searchSubscription.valueChanges.pipe(takeUntil(this.destroyed$)).subscribe(() => {
+            this.filterSubscriptions();
+        });
+
         this.imgPath = (isElectron || isCordova) ? 'assets/images/' : AppUrl + APP_FOLDER + 'assets/images/';
 
         this.breakpointObserver.observe([
@@ -62,7 +143,7 @@ export class SubscriptionComponent implements OnInit {
         ]).subscribe((state: BreakpointState) => {
 
             if (state.breakpoints['(min-width: 768px)']) {
-                        this.rowLength = 120;
+                this.rowLength = 120;
                 //         this.menuBarHeight = 5.5;
                 //         this.sideBarBoxLength = 13;
                 this.sideBarBoxWidth = 5;
@@ -116,5 +197,54 @@ export class SubscriptionComponent implements OnInit {
             this.store.dispatch(this.subscriptionsActions.SubscribedCompanies());
         }
         // this.modalRef.hide();
+    }
+
+    // This function will use for destroy on next and complete
+    public ngOnDestroy(): void {
+        this.destroyed$.next(true);
+        this.destroyed$.complete();
+    }
+
+    // This function will use for select subscriptions
+    public selectSubscription(subscription: any): void {
+        this.selectedSubscription = subscription;
+    }
+
+    // This function will use for on changes
+    public ngOnChanges(changes: SimpleChanges): void { }
+
+    // This function will use for filter plans , withing and expiry in dropdown and searching subscriptions
+    public filterSubscriptions(): void {
+        let subscriptions = [];
+        this.subscriptions = [];
+        this.subscriptions$.subscribe(response => {
+            if (response) {
+                response.forEach(subscription => {
+                    let flag = true;
+                    if (
+                        (this.searchSubscription?.value && subscription?.planDetails?.name?.toLowerCase()?.indexOf(this.searchSubscription?.value?.toLowerCase()) === -1) ||
+                        (this.filters?.plan && subscription?.planDetails?.uniqueName !== this.filters?.plan) ||
+                        (this.filters?.expiration && (subscription?.remainingDays < 0 || subscription?.remainingDays > this.filters?.expiration)) ||
+                        (this.filters?.transactionBalance && (subscription?.remainingTransactions < 0 || subscription?.remainingTransactions > this.filters?.transactionBalance))
+                    ) {
+                        flag = false;
+                    }
+                    if (flag) {
+                        subscriptions.push(subscription);
+                    }
+                });
+                this.subscriptions = subscriptions;
+                this.changeDetectionRef.detectChanges();
+            }
+        });
+    }
+
+    // This function will use for reset filter plans , withing and expiry in dropdown and searching subscriptions
+    public clearFilters(): void {
+        this.searchSubscription = new FormControl();
+        this.filters.plan = '';
+        this.filters.expiration = '';
+        this.filters.transactionBalance = '';
+        this.filterSubscriptions();
     }
 }
