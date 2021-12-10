b0999a159672a6f54a769faccc5e4d41fe1dc38c
ravindersingh <ravinder@walkover.in>
2021-12-10 16:40:30 +0530

    Created attachment component

---
+++ apps/web-giddh/src/app/theme/attachments/attachments.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnChanges, OnDestroy, OnInit } from "@angular/core";
+
+@Component({
+    selector: "attachments",
+    templateUrl: "./attachments.component.html",
+    styleUrls: ["./attachments.component.scss"]
+})
+export class AttachmentsComponent implements OnInit, OnChanges, OnDestroy {
+
+    constructor(
+
+    ) {
+
+    }
+
+    public ngOnInit(): void {
+
+    }
+
+    public ngOnChanges(): void {
+
+    }
+
+    public ngOnDestroy(): void {
+
+    }
+}
---
+++ apps/web-giddh/src/app/theme/attachments/attachments.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { AttachmentsComponent } from "./attachments.component";
+
+@NgModule({
+    declarations: [
+        AttachmentsComponent
+    ],
+    imports: [
+
+    ],
+    exports: [
+        AttachmentsComponent
+    ]
+})
+export class AttachmentsModule {
+
+}
