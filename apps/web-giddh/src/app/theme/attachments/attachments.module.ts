f65f97c87ec183a0e5847f7622f884a0bb20f859
Shivansh <shivansh@whozzat.com>
2021-12-12 19:21:50 +0530

    Shivansh | Test | Attachment + Voucher download and preview doc

--- apps/web-giddh/src/app/theme/attachments/attachments.module.ts
+++ apps/web-giddh/src/app/theme/attachments/attachments.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from "@angular/core";
 import { AttachmentsComponent } from "./attachments.component";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from "@angular/common";
 
 @NgModule({
     declarations: [
         AttachmentsComponent
     ],
     imports: [
-
+        MatIconModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatListModule,
+        CommonModule
     ],
     exports: [
         AttachmentsComponent
