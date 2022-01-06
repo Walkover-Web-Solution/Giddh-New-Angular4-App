8dcf85894f64e9341bf768188d854a1ff8911537
Ravinder Singh <57168459+ravinderwalkover@users.noreply.github.com>
2022-01-06 19:27:06 +0530

    Revert "Dilpreet | Test | Fixed responsive design issues"

--- apps/web-giddh/src/app/contact/contact.component.ts
+++ apps/web-giddh/src/app/contact/contact.component.ts
@@ -9,7 +9,6 @@ import {
     OnDestroy,
     OnInit,
     Output,
-    Renderer2,
     TemplateRef,
     ViewChild,
 } from "@angular/core";
@@ -245,13 +244,34 @@ export class ContactComponent implements OnInit, OnDestroy {
     public availableColumnsCount: any[] = [];
     /** True if we should select all checkbox */
     public showSelectAll: boolean = false;
+    /** True if custom fields finished loading */
+    public customFieldsLoaded: boolean = false;
 
-    constructor(public dialog: MatDialog, private store: Store<AppState>, private router: Router, private companyServices: CompanyService, private commonActions: CommonActions, private toaster: ToasterService,
-        private contactService: ContactService, private settingsIntegrationActions: SettingsIntegrationActions, private companyActions: CompanyActions, private componentFactoryResolver: ComponentFactoryResolver,
-        private groupWithAccountsAction: GroupWithAccountsAction, private cdRef: ChangeDetectorRef, private generalService: GeneralService, private route: ActivatedRoute, private generalAction: GeneralActions,
-        private breakPointObservar: BreakpointObserver, private modalService: BsModalService, private settingsProfileActions: SettingsProfileActions, private groupService: GroupService,
-        private settingsBranchAction: SettingsBranchActions, public currencyPipe: GiddhCurrencyPipe, private lightbox: Lightbox ,private renderer: Renderer2) {
-        this.searchLoader$ = this.store.pipe(select(p => p.search.searchLoader), takeUntil(this.destroyed$));
+    constructor(
+        public dialog: MatDialog, 
+        private store: Store<AppState>, 
+        private router: Router, 
+        private companyServices: CompanyService, 
+        private commonActions: CommonActions, 
+        private toaster: ToasterService,
+        private contactService: ContactService, 
+        private settingsIntegrationActions: SettingsIntegrationActions, 
+        private companyActions: CompanyActions, 
+        private componentFactoryResolver: ComponentFactoryResolver,
+        private groupWithAccountsAction: GroupWithAccountsAction, 
+        private cdRef: ChangeDetectorRef, 
+        private generalService: GeneralService, 
+        private route: ActivatedRoute, 
+        private generalAction: GeneralActions,
+        private breakPointObservar: BreakpointObserver, 
+        private modalService: BsModalService, 
+        private settingsProfileActions: SettingsProfileActions, 
+        private groupService: GroupService,
+        private settingsBranchAction: SettingsBranchActions, 
+        public currencyPipe: GiddhCurrencyPipe,
+        private lightbox: Lightbox) {
+
+        this.searchLoader$ = this.store.pipe(select(state => state.search.searchLoader), takeUntil(this.destroyed$));
         this.dueAmountReportRequest = new DueAmountReportQueryRequest();
         this.createAccountIsSuccess$ = this.store.pipe(select(state => state.groupwithaccounts.createAccountIsSuccess), takeUntil(this.destroyed$));
 
@@ -276,8 +296,7 @@ export class ContactComponent implements OnInit, OnDestroy {
     }
 
     public ngOnInit() {
-        this.renderer.addClass(document.body, 'contact-body');
-        this.imgPath = (isElectron || isCordova) ? "assets/images/" : AppUrl + APP_FOLDER + "assets/images/";
+        this.imgPath = isElectron ? 'assets/images/' : AppUrl + APP_FOLDER + 'assets/images/';
         this.store.dispatch(this.companyActions.getAllRegistrations());
         this.store.dispatch(this.settingsProfileActions.GetProfileInfo());
         this.currentOrganizationType = this.generalService.currentOrganizationType;
@@ -575,7 +594,6 @@ export class ContactComponent implements OnInit, OnDestroy {
     }
 
     public ngOnDestroy() {
-        this.renderer.removeClass(document.body, 'contact-body');
         localStorage.removeItem(this.localStorageKeysForFilters[this.activeTab === "vendor" ? "vendor" : "customer"]);
         this.destroyed$.next(true);
         this.destroyed$.complete();
