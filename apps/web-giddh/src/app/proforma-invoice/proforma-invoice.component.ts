de9ae118d2586783fc369b62d1cf5cbaab15dcf3
ravindersingh <ravinder@walkover.in>
2022-01-17 11:28:51 +0530

    Wrong voucher types is getting passed in invoice list api in update purchase voucher flow

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -6032,7 +6032,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
             } else {
                 const requestObject = {
                     accountUniqueName: customerUniqueName,
-                    voucherType: this.selectedVoucherType
+                    voucherType: (this.isPurchaseInvoice) ? VoucherTypeEnum.purchase : this.selectedVoucherType
                 }
                 apiCallObservable = this.salesService.getInvoiceList(requestObject, voucherDate);
             }
