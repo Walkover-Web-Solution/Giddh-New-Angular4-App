e16b8c759ebf83e5ac2bc108e65f36c0af89afca
Dilpreet Singh Dang <dilpreet.dang99@gmail.com>
2022-02-14 16:43:15 +0530

    PR suggestions fixed

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -7195,10 +7195,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
         let invoiceType = this.voucherTypeToNamePipe.transform(this.invoiceType);
         invoiceType = this.titleCasePipe.transform(invoiceType);
         this.copyPreviousInvoiceText = this.copyPreviousInvoiceText?.replace("[INVOICE_TYPE]", invoiceType);
-         if (this.isCreditNote) {
-            this.copyPreviousInvoiceText = this.localeData?.copy_previous_invoices;
-            this.copyPreviousInvoiceText = this.copyPreviousInvoiceText?.replace(this.localeData?.invoices, invoiceType);
-        } else if (this.isDebitNote) {
+        if (this.isCreditNote || this.isDebitNote) {
             this.copyPreviousInvoiceText = this.localeData?.copy_previous_invoices;
             this.copyPreviousInvoiceText = this.copyPreviousInvoiceText?.replace(this.localeData?.invoices, invoiceType);
         }
