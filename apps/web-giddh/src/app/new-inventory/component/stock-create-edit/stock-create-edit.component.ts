77e19a424887b36bb710060cbc2e7124229ab080
ravindersingh <ravinder@walkover.in>
2022-03-10 15:26:48 +0530

    Removed form tags

--- apps/web-giddh/src/app/new-inventory/component/stock-create-edit/stock-create-edit.component.ts
+++ apps/web-giddh/src/app/new-inventory/component/stock-create-edit/stock-create-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormControl } from "@angular/forms";
-import { ReplaySubject,Observable } from "rxjs";
-import { takeUntil,map,startWith } from "rxjs/operators";
+import { ReplaySubject, Observable } from "rxjs";
+import { takeUntil, map, startWith } from "rxjs/operators";
 import { COMMA, ENTER } from "@angular/cdk/keycodes";
 import { MatChipInputEvent } from "@angular/material/chips";
 
@@ -49,7 +49,50 @@ export class StockCreateEditComponent implements OnInit, OnDestroy {
 
     private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
     public stockForm: any = {
-        type: 'PRODUCT'
+        type: 'PRODUCT',
+        name: null,
+        uniqueName: null,
+        stockUnitCode: null,
+        hsnNumber: null,
+        sacNumber: null,
+        taxes: null,
+        skuCode: null,
+        skuCodeHeading: null,
+        customField1Heading: null,
+        customField1Value: null,
+        customField2Heading: null,
+        customField2Value: null,
+        purchaseAccountDetails: {
+            accountUniqueName: null,
+            unitRates: [
+                {
+                    rate: null,
+                    stockUnitCode: null
+                }
+            ]
+        },
+        salesAccountDetails: {
+            accountUniqueName: null,
+            unitRates: [
+                {
+                    rate: null,
+                    stockUnitCode: null
+                }
+            ]
+        },
+        isFsStock: null,
+        manufacturingDetails: null,
+        accountGroup: null,
+        lowStockAlertCount: 0,
+        outOfStockSelling: true,
+        variants: null,
+        options: [
+            {
+                name: null,
+                values: null,
+                order: 1
+            }
+        ]
     };
 
     constructor(
@@ -61,43 +104,43 @@ export class StockCreateEditComponent implements OnInit, OnDestroy {
         this.imgPath = isElectron ? 'assets/images/' : AppUrl + APP_FOLDER + 'assets/images/';
 
         this.productUnitFilteredOptions = this.productUnitControl.valueChanges.pipe(
-          startWith(''),
-          map(value => this._productUnitfilter(value)),
+            startWith(''),
+            map(value => this._productUnitfilter(value)),
         );
 
         this.productUnderGroupFilteredOptions = this.productUnderGroupControl.valueChanges.pipe(
-          startWith(''),
-          map(value => this._productUnderGroupfilter(value)),
+            startWith(''),
+            map(value => this._productUnderGroupfilter(value)),
         );
 
         this.serviceUnitGroupFilteredOptions = this.serviceUnitGroupControl.valueChanges.pipe(
-          startWith(''),
-          map(value => this._serviceUnitGroupfilter(value)),
+            startWith(''),
+            map(value => this._serviceUnitGroupfilter(value)),
         );
 
         this.serviceUnderGroupFilteredOptions = this.serviceUnderGroupControl.valueChanges.pipe(
-          startWith(''),
-          map(value => this._serviceUnderGroupfilter(value)),
+            startWith(''),
+            map(value => this._serviceUnderGroupfilter(value)),
         );
 
         this.productPurchaseLinkedGroupFilteredOptions = this.productPurchaseLinkedGroupControl.valueChanges.pipe(
-          startWith(''),
-          map(value => this._productPurchaseLinkedGroupfilter(value)),
+            startWith(''),
+            map(value => this._productPurchaseLinkedGroupfilter(value)),
         );
 
         this.productSalesLinkedFilteredOptions = this.productSalesLinkedControl.valueChanges.pipe(
-          startWith(''),
-          map(value => this._productSalesLinkedfilter(value)),
+            startWith(''),
+            map(value => this._productSalesLinkedfilter(value)),
         );
 
         this.servicePurchaseLinkedGroupFilteredOptions = this.servicePurchaseLinkedGroupControl.valueChanges.pipe(
-          startWith(''),
-          map(value => this._servicePurchaseLinkedfilter(value)),
+            startWith(''),
+            map(value => this._servicePurchaseLinkedfilter(value)),
         );
 
         this.serviceSalesLinkedGroupFilteredOptions = this.serviceSalesLinkedGroupControl.valueChanges.pipe(
-          startWith(''),
-          map(value => this._serviceSalesLinkedfilter(value)),
+            startWith(''),
+            map(value => this._serviceSalesLinkedfilter(value)),
         );
 
     }
@@ -124,42 +167,42 @@ export class StockCreateEditComponent implements OnInit, OnDestroy {
     }
 
     private _productUnitfilter(value: string): string[] {
-      const filterValue = value.toLowerCase();
-      return this.productUnitOptions.filter(productUnitOption => productUnitOption.toLowerCase().includes(filterValue));
+        const filterValue = value.toLowerCase();
+        return this.productUnitOptions.filter(productUnitOption => productUnitOption.toLowerCase().includes(filterValue));
     }
 
     private _productUnderGroupfilter(value: string): string[] {
-      const filterValue = value.toLowerCase();
-      return this.productUnderGroupOptions.filter(productUnderGroupOption => productUnderGroupOption.toLowerCase().includes(filterValue));
+        const filterValue = value.toLowerCase();
+        return this.productUnderGroupOptions.filter(productUnderGroupOption => productUnderGroupOption.toLowerCase().includes(filterValue));
     }
 
     private _serviceUnitGroupfilter(value: string): string[] {
-      const filterValue = value.toLowerCase();
-      return this.serviceUnitGroupOptions.filter(serviceUnitGroupOption => serviceUnitGroupOption.toLowerCase().includes(filterValue));
+        const filterValue = value.toLowerCase();
+        return this.serviceUnitGroupOptions.filter(serviceUnitGroupOption => serviceUnitGroupOption.toLowerCase().includes(filterValue));
     }
 
     private _serviceUnderGroupfilter(value: string): string[] {
-      const filterValue = value.toLowerCase();
-      return this.serviceUnderGroupOptions.filter(serviceUnderGroupOption => serviceUnderGroupOption.toLowerCase().includes(filterValue));
+        const filterValue = value.toLowerCase();
+        return this.serviceUnderGroupOptions.filter(serviceUnderGroupOption => serviceUnderGroupOption.toLowerCase().includes(filterValue));
     }
 
     private _productPurchaseLinkedGroupfilter(value: string): string[] {
-      const filterValue = value.toLowerCase();
-      return this.productPurchaseLinkedGroupOptions.filter(productPurchaseLinkedGroupOption => productPurchaseLinkedGroupOption.toLowerCase().includes(filterValue));
+        const filterValue = value.toLowerCase();
+        return this.productPurchaseLinkedGroupOptions.filter(productPurchaseLinkedGroupOption => productPurchaseLinkedGroupOption.toLowerCase().includes(filterValue));
     }
 
     private _productSalesLinkedfilter(value: string): string[] {
-      const filterValue = value.toLowerCase();
-      return this.productSalesLinkedOptions.filter(productSalesLinkedGroupOption => productSalesLinkedGroupOption.toLowerCase().includes(filterValue));
+        const filterValue = value.toLowerCase();
+        return this.productSalesLinkedOptions.filter(productSalesLinkedGroupOption => productSalesLinkedGroupOption.toLowerCase().includes(filterValue));
     }
 
     private _servicePurchaseLinkedfilter(value: string): string[] {
-      const filterValue = value.toLowerCase();
-      return this.servicePurchaseLinkedGroupOptions.filter(servicePurchaseLinkedGroupOption => servicePurchaseLinkedGroupOption.toLowerCase().includes(filterValue));
+        const filterValue = value.toLowerCase();
+        return this.servicePurchaseLinkedGroupOptions.filter(servicePurchaseLinkedGroupOption => servicePurchaseLinkedGroupOption.toLowerCase().includes(filterValue));
     }
 
     private _serviceSalesLinkedfilter(value: string): string[] {
-      const filterValue = value.toLowerCase();
-      return this.serviceSalesLinkedGroupOptions.filter(serviceSalesLinkedGroupOption => serviceSalesLinkedGroupOption.toLowerCase().includes(filterValue));
+        const filterValue = value.toLowerCase();
+        return this.serviceSalesLinkedGroupOptions.filter(serviceSalesLinkedGroupOption => serviceSalesLinkedGroupOption.toLowerCase().includes(filterValue));
     }
 }
