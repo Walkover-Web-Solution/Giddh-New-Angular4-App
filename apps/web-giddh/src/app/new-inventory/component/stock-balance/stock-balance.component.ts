8501a8c31cae4f2ad8e9edd90c171fc8f596013c
Shivansh <shivansh@whozzat.com>
2022-03-24 14:45:44 +0530

    Shivansh | Test | designing Stock Balance page

---
+++ apps/web-giddh/src/app/new-inventory/component/stock-balance/stock-balance.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'stock-balance',
+    templateUrl: './stock-balance.component.html',
+    styleUrls: ['./stock-balance.component.scss'],
+})
+
+export class StockBalanceComponent{}
