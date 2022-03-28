54807985f555b98772c6d04bc4a653bc7bcd0685
Shivansh <shivansh@whozzat.com>
2022-03-28 15:25:25 +0530

    Shivansh | Test | designing Stock Balance page

--- apps/web-giddh/src/app/new-inventory/component/stock-balance/stock-balance.component.ts
+++ apps/web-giddh/src/app/new-inventory/component/stock-balance/stock-balance.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl } from "@angular/forms";
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
     selector: 'stock-balance',
     templateUrl: './stock-balance.component.html',
@@ -11,12 +16,29 @@ export class StockBalanceComponent implements OnInit{
     
     /** Image path variable */
     public imgPath: string = '';
+
+    public stockListBoxIndex: number;
+    public stockListBox = document.getElementsByClassName("stock-list-box");
     
     toppings = new FormControl();
     toppingList: string[] = ['Warehouse 1', 'Warehouse 2', 'Warehouse 3', 'Warehouse 4', 'Warehouse 5', 'Warehouse 6'];
 
+    foods: Food[] = [
+    {value: 'small-0', viewValue: 'Small'},
+    {value: 'medius-1', viewValue: 'Medium'},
+    {value: 'large-2', viewValue: 'Large'},
+    {value: 'extra-large-3', viewValue: 'Extra Large'},
+    {value: 'extra-extra-large-4', viewValue: 'Extra Extra Large'},
+  ];
+
+
     ngOnInit() {
         /** This will use for image format */
         this.imgPath = isElectron ? 'assets/images/' : AppUrl + APP_FOLDER + 'assets/images/';
+        for(this.stockListBoxIndex = 0 ; this.stockListBoxIndex<this.stockListBox.length; this.stockListBoxIndex++ ){
+            this.stockListBox[this.stockListBoxIndex].addEventListener('click',function(){
+                this.classList.toggle("active");
+            })
+        }
     }
 }
