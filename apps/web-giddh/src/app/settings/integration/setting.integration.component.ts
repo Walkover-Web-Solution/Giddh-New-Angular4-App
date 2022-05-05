672ffc2f2940615a85cd476ebc856333990a9a68
Shivansh Rawat <shivansh@whozzat.com>
2022-05-05 17:54:55 +0530

    fixed PR issues

--- apps/web-giddh/src/app/settings/integration/setting.integration.component.ts
+++ apps/web-giddh/src/app/settings/integration/setting.integration.component.ts
@@ -144,6 +144,7 @@ export class SettingIntegrationComponent implements OnInit, AfterViewInit {
     public activeCompany: any;
     /** Holds image path */
     public imgPath: string = '';
+    /** This will hold toggle buttons value and size */
     public bootstrapToggleSwitch = BOOTSTRAP_TOGGLE_SWITCH;
 
     constructor(
