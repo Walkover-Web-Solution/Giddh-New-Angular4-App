2a51c378aed49ed7cac644be09f86615df874e7c
Dilpreet Singh Dang <dilpreet.dang99@gmail.com>
2022-05-07 12:41:49 +0530

    code suggestion fixed

--- apps/web-giddh/src/app/financial-reports/components/export/balance-sheet/export-xls/export-xls.component.ts
+++ apps/web-giddh/src/app/financial-reports/components/export/balance-sheet/export-xls/export-xls.component.ts
@@ -8,10 +8,10 @@ import { AppState } from 'apps/web-giddh/src/app/store';
     templateUrl: './export-xls.component.html'
 })
 export class BalanceSheetExportXlsComponent implements OnInit {
+    /** This will hold expandAll values for balance sheet export */
+    @Input() public expandAll: boolean;
     @Input() public fy: number;
     @Input() public filters: any = {};
-    /** This will hold expandAll values for balance sheet export */
-    @Input() public expandAll : boolean;
     public enableDownload: boolean = true;
     public imgPath: string = '';
     @Output() public plBsExportPdfEvent = new EventEmitter<boolean>();
@@ -21,20 +21,11 @@ export class BalanceSheetExportXlsComponent implements OnInit {
     constructor(
         private store: Store<AppState>,
         private tbPlActions: TBPlBsActions) {
-
     }
 
     public downloadBsXls() {
-        let expanded = this.expandAll;
-        if(expanded === true){
-            const expand = "expanded"
-            let request = { from: this.filters.from, to: this.filters.to, branchUniqueName: this.filters.branchUniqueName, filename: this.localeData?.xls.balance_sheet.download_filename, view: expand };
-            this.store.dispatch(this.tbPlActions.DownloadBalanceSheetExcel(request));
-        }else if(expanded === false) {
-            const collapse = "collapsed"
-            let request = { from: this.filters.from, to: this.filters.to, branchUniqueName: this.filters.branchUniqueName, filename: this.localeData?.xls.balance_sheet.download_filename, view: collapse };
-            this.store.dispatch(this.tbPlActions.DownloadBalanceSheetExcel(request));
-        }
+        let request = { from: this.filters.from, to: this.filters.to, branchUniqueName: this.filters.branchUniqueName, filename: this.localeData?.xls.balance_sheet.download_filename, view: this.expandAll ? "expanded" : "collpased" };
+        this.store.dispatch(this.tbPlActions.DownloadBalanceSheetExcel(request));
     }
 
     public ngOnInit() {
