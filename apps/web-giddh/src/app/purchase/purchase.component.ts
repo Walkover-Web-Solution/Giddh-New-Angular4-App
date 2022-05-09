88e533df5c61b50f86893d3ea0566261dfbc0f6f
ravindersingh <ravinder@walkover.in>
2022-05-09 11:01:52 +0530

    reverted code

--- apps/web-giddh/src/app/purchase/purchase.component.ts
+++ apps/web-giddh/src/app/purchase/purchase.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { PurchaseOrderActions } from '../actions/purchase-order/purchase-order.action';
+import { AppState } from '../store';
 
 @Component({
     styleUrls: [`./purchase.component.scss`],
     templateUrl: './purchase.component.html'
 })
-export class PurchaseComponent {
-    constructor() {
+export class PurchaseComponent implements OnDestroy {
+    constructor(private store: Store<AppState>, public purchaseOrderActions: PurchaseOrderActions) {
 
+    }
+
+    public ngOnDestroy(): void {
+        this.store.dispatch(this.purchaseOrderActions.setPurchaseOrderFilters({}));
     }
 }
