2ea870b499fbf015ad7f10e7e2fe4456118fe9ba
ravindersingh <ravinder@walkover.in>
2022-05-19 11:14:00 +0530

    Removed spacing

--- apps/web-giddh/src/app/material-ledger/components/new-ledger-entry-panel/new-ledger-entry-panel.component.ts
+++ apps/web-giddh/src/app/material-ledger/components/new-ledger-entry-panel/new-ledger-entry-panel.component.ts
@@ -587,7 +587,6 @@ export class NewLedgerEntryPanelComponent implements OnInit, OnDestroy, OnChange
                 }
             }
             if (this.discountControl) {
-
                 this.discountControl.change();
             }
 
