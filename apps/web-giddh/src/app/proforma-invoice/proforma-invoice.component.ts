334cd5ca3ecb6ab0d020daad03963e6b814aab7d
ravindersingh <ravinder@walkover.in>
2022-05-24 13:30:55 +0530

    Fixed issue

--- apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
+++ apps/web-giddh/src/app/proforma-invoice/proforma-invoice.component.ts
@@ -201,7 +201,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
     /** Billing state field instance */
     @ViewChild('statesShipping', { static: false }) statesShipping: SalesShSelectComponent;
     public showAdvanceReceiptAdjust: boolean = false;
-
+    @Output() public reloadFiles: EventEmitter<boolean> = new EventEmitter<boolean>();
     @Output() public cancelVoucherUpdate: EventEmitter<boolean> = new EventEmitter<boolean>();
 
     public editCurrencyInputField: boolean = false;
@@ -3811,7 +3811,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
      * @memberof ProformaInvoiceComponent
      */
     public cancelUpdate(): void {
-        if(this.callFromOutside) {
+        if (this.callFromOutside) {
             this.location?.back();
         } else {
             if (this.invoiceForm) {
@@ -7593,6 +7593,7 @@ export class ProformaInvoiceComponent implements OnInit, OnDestroy, AfterViewIni
                 this.selectedFileName = '';
                 this.invFormData.entries[0].attachedFile = '';
                 this._toasty.successToast(response?.body);
+                this.reloadFiles.emit(true);
                 this._cdr.detectChanges();
             } else {
                 this._toasty.errorToast(response?.message)
