bf6efb60372663dd7e1e005ec548350264b0cdea
Dilpreet Singh Dang <dilpreet.dang99@gmail.com>
2022-07-07 13:33:58 +0530

    fix PR suggestions

--- apps/web-giddh/src/app/material-ledger/components/new-ledger-entry-panel/new-ledger-entry-panel.component.ts
+++ apps/web-giddh/src/app/material-ledger/components/new-ledger-entry-panel/new-ledger-entry-panel.component.ts
@@ -743,15 +743,14 @@ export class NewLedgerEntryPanelComponent implements OnInit, OnDestroy, OnChange
                 return;
             }
         }
+        // Taxes checkbox will be false in case of receipt and payment voucher 
         if (this.voucherApiVersion === 2) {
             if (this.blankLedger.voucherType === 'rcpt' || this.blankLedger.voucherType === 'pay') {
                 if (!this.isAdvanceReceipt) {
                     this.currentTxn?.taxes === [];
                     this.currentTxn?.taxesVm?.filter(p => {
-                        if (p.isChecked) {
-                            p.isChecked = false
-                        }
-                    })
+                        p.isChecked = false;
+                    });
                 }
             }
         }
