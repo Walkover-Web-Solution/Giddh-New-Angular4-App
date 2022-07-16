c41fd0ec7b7b36a1710baeebbc7548f77301a3fb
Dilpreet Singh Dang <dilpreet.dang99@gmail.com>
2022-07-16 12:42:30 +0530

    fixed condition

--- apps/web-giddh/src/app/services/general.service.ts
+++ apps/web-giddh/src/app/services/general.service.ts
@@ -727,30 +727,6 @@ export class GeneralService {
     }
 
     /**
-     * Validates the bank details: Bank Name, Account number, IFSC code.
-     * If either of them is provided then the rest two fields are also mandatory
-     * as all the 3 values are required for payment purpose. If none of them is provided,
-     * then also it is valid. It is invalid when anyone of them is missing and rest
-     * are provided
-     *
-     * @returns {boolean} True, if bank details are valid
-     * @memberof GeneralService
-     */
-    public checkForValidBankDetails(bankDetails: any, countryCode: string): boolean {
-        const fieldsWithValue = bankDetails;
-        const keys = countryCode === 'AE' ?
-            ['beneficiaryName', 'bankName', 'branchName', 'bankAccountNo', 'swiftCode'] :
-            ['bankName', 'bankAccountNo', 'ifsc'];
-        let isValid = true;
-        if (fieldsWithValue) {
-            isValid = keys.every(key => Boolean(fieldsWithValue[key])) || keys.every(key => !Boolean(fieldsWithValue[key]));
-            return isValid;
-        } else {
-            return isValid;
-        }
-    }
-
-    /**
      * Returns the string initials upto 2 letters/characters
      *
      * @param {string} name String whose intials are required
@@ -805,6 +781,30 @@ export class GeneralService {
     }
 
     /**
+     * Validates the bank details: Bank Name, Account number, IFSC code.
+     * If either of them is provided then the rest two fields are also mandatory
+     * as all the 3 values are required for payment purpose. If none of them is provided,
+     * then also it is valid. It is invalid when anyone of them is missing and rest
+     * are provided
+     *
+     * @returns {boolean} True, if bank details are valid
+     * @memberof GeneralService
+     */
+    public checkForValidBankDetails(bankDetails: any, countryCode: string): boolean {
+        const fieldsWithValue = bankDetails;
+        const keys = countryCode === 'AE' ?
+            ['beneficiaryName', 'bankName', 'branchName', 'bankAccountNo', 'swiftCode'] :
+            ['bankName', 'bankAccountNo', 'ifsc'];
+        let isValid = true;
+        if (fieldsWithValue) {
+            isValid = keys.every(key => Boolean(fieldsWithValue[key])) || keys.every(key => !Boolean(fieldsWithValue[key]));
+            return isValid;
+        } else {
+            return isValid;
+        }
+    }
+
+    /**
      * Navigates to the route provided
      *
      * @param {*} route Route to navigate to
@@ -843,6 +843,39 @@ export class GeneralService {
     }
 
     /**
+     * This will give multi-lingual current voucher label
+     *
+     * @param {string} voucherCode Voucher code
+     * @param {*} commonLocaleData Global context of multi-lingual keys
+     * @return {string} Multi-lingual current voucher label
+     * @memberof GeneralService
+     */
+    public getCurrentVoucherLabel(voucherCode: string, commonLocaleData: any): string {
+        switch (voucherCode) {
+            case AdjustedVoucherType.Sales: case AdjustedVoucherType.SalesInvoice: return commonLocaleData?.app_voucher_types.sales;
+            case AdjustedVoucherType.Purchase: case AdjustedVoucherType.PurchaseInvoice: return commonLocaleData?.app_voucher_types.purchase;
+            case AdjustedVoucherType.CreditNote: return commonLocaleData?.app_voucher_types.credit_note;
+            case AdjustedVoucherType.DebitNote: return commonLocaleData?.app_voucher_types.debit_note;
+            case AdjustedVoucherType.Payment: return commonLocaleData?.app_voucher_types.payment;
+            default: return '';
+        }
+    }
+
+    /**
+     * Determines if an element is child element to another element
+     *
+     * @param {*} child Element received as child
+     * @param {*} parent Element received as parent
+     * @return {boolean} True, if element is child of another element
+     * @memberof GeneralService
+     */
+    public childOf(child: any, parent: any): boolean {
+        while ((child = child.parentNode) && child !== parent) {
+        }
+        return !!child;
+    }
+
+    /**
      * This will sort branches by alias
      *
      * @param {*} branchA
@@ -877,39 +910,7 @@ export class GeneralService {
     }
 
     /**
-     * This will give multi-lingual current voucher label
-     *
-     * @param {string} voucherCode Voucher code
-     * @param {*} commonLocaleData Global context of multi-lingual keys
-     * @return {string} Multi-lingual current voucher label
-     * @memberof GeneralService
-     */
-    public getCurrentVoucherLabel(voucherCode: string, commonLocaleData: any): string {
-        switch (voucherCode) {
-            case AdjustedVoucherType.Sales: case AdjustedVoucherType.SalesInvoice: return commonLocaleData?.app_voucher_types.sales;
-            case AdjustedVoucherType.Purchase: case AdjustedVoucherType.PurchaseInvoice: return commonLocaleData?.app_voucher_types.purchase;
-            case AdjustedVoucherType.CreditNote: return commonLocaleData?.app_voucher_types.credit_note;
-            case AdjustedVoucherType.DebitNote: return commonLocaleData?.app_voucher_types.debit_note;
-            case AdjustedVoucherType.Payment: return commonLocaleData?.app_voucher_types.payment;
-            default: return '';
-        }
-    }
-
-    /**
-     * Determines if an element is child element to another element
-     *
-     * @param {*} child Element received as child
-     * @param {*} parent Element received as parent
-     * @return {boolean} True, if element is child of another element
-     * @memberof GeneralService
-     */
-    public childOf(child: any, parent: any): boolean {
-        while ((child = child.parentNode) && child !== parent) {
-        }
-        return !!child;
-    }
-
-    /* This will expand left sidebar
+     * This will expand left sidebar
      *
      * @memberof GeneralService
      */
@@ -955,7 +956,7 @@ export class GeneralService {
         return amount?.replace(/,/g, "")?.replace(/ /g, "")?.replace(/'/g, "").trim();
     }
 
-    /*
+    /**
      * This will return available themes
      *
      * @returns {*}
@@ -968,7 +969,7 @@ export class GeneralService {
         ];
     }
 
-    /**
+    /*
      * Adds tooltip text for grand total and total due amount
      * to item supplied (for Cash/Sales Invoice and CR/DR note)
      *
@@ -1070,18 +1071,16 @@ export class GeneralService {
                 }
             });
         }
-        if (entryAccount?.parentGroups && entryAccount?.name && entryAccount?.category) {
-            if (
-                this.voucherApiVersion === 2
-                &&
-                ((ledgerAccount?.parentGroups[1]?.uniqueName === 'sundrydebtors' || ledgerAccount?.parentGroups[1]?.uniqueName === 'sundrycreditors') && (entryAccount?.parentGroups[1]?.uniqueName === VoucherTypeEnum.cash || entryAccount?.parentGroups[1]?.uniqueName === 'bankaccounts'))
+        if (
+            this.voucherApiVersion === 2
+            && entryAccount?.parentGroups?.length > 0 && ledgerAccount?.parentGroups?.length > 0 &&
+            (((ledgerAccount?.parentGroups[1]?.uniqueName === 'sundrydebtors' || ledgerAccount?.parentGroups[1]?.uniqueName === 'sundrycreditors') && (entryAccount?.parentGroups[1]?.uniqueName === VoucherTypeEnum.cash || entryAccount?.parentGroups[1]?.uniqueName === 'bankaccounts'))
                 ||
-                ((ledgerAccount?.parentGroups[1]?.uniqueName === VoucherTypeEnum.cash || ledgerAccount?.parentGroups[1]?.uniqueName === 'bankaccounts') && (entryAccount?.parentGroups[1]?.uniqueName === 'sundrydebtors' || entryAccount?.parentGroups[1]?.uniqueName === 'sundrycreditors'))
-                &&
-                (!voucherType || (["rcpt", "pay", "advance-receipt"].includes(voucherType)))
-            ) {
-                return true;
-            }
+                ((ledgerAccount?.parentGroups[1]?.uniqueName === VoucherTypeEnum.cash || ledgerAccount?.parentGroups[1]?.uniqueName === 'bankaccounts') && (entryAccount?.parentGroups[1]?.uniqueName === 'sundrydebtors' || entryAccount?.parentGroups[1]?.uniqueName === 'sundrycreditors')))
+            &&
+            (!voucherType || (["rcpt", "pay", "advance-receipt"].includes(voucherType)))
+        ) {
+            return true;
         }
 
         return false;
