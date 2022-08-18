aea34b74d5f26ea4bb86865326f248416266f985
Dilpreet Singh Dang <dilpreet.dang99@gmail.com>
2022-08-18 13:53:01 +0530

    remove old code

--- apps/web-giddh/src/app/contact/contact.component.ts
+++ apps/web-giddh/src/app/contact/contact.component.ts
@@ -558,7 +558,7 @@ export class ContactComponent implements OnInit, OnDestroy {
         }
         if (isElectron) {
             let ipcRenderer = (window as any).require('electron').ipcRenderer;
-            url = location.origin + location.pathname + '?returnUrl=ledger/' + accUniqueName;
+            url = location.origin + location.pathname + `#./pages/${part}/${accUniqueName}`;
             console.log(ipcRenderer.send('open-url', url));
         } else {
             (window as any).open(url);
