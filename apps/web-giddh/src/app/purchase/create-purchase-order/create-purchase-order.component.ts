021af1653116bef8f1cc5950a0c451066ca715fc
Dilpreet Singh Dang <dilpreet.dang99@gmail.com>
2022-09-02 15:20:29 +0530

    fixed tax issue in single item and bulk item in purchase order

--- apps/web-giddh/src/app/purchase/create-purchase-order/create-purchase-order.component.ts
+++ apps/web-giddh/src/app/purchase/create-purchase-order/create-purchase-order.component.ts
@@ -1293,15 +1293,16 @@ export class CreatePurchaseOrderComponent implements OnInit, OnDestroy, AfterVie
                 };
             }
             if (isBulkItem) {
-                txn = this.calculateItemValues(selectedAcc, txn, entry);
+                txn = this.calculateItemValues(selectedAcc, txn, entry, false, true);
             } else {
                 this.searchService.loadDetails(selectedAcc.additional.uniqueName, requestObject).pipe(takeUntil(this.destroyed$)).subscribe(data => {
                     if (data && data.body) {
                         // Take taxes of parent group and stock's own taxes
-                        const taxes = data.body.taxes || [];
-                        if (data.body.stock) {
-                            taxes.push(...data.body.stock.taxes);
-                        }
+                        const taxes = this.generalService.fetchTaxesOnPriority(
+                            data.body.stock?.taxes ?? [],
+                            data.body.stock?.groupTaxes ?? [],
+                            data.body.taxes ?? [],
+                            data.body.groupTaxes ?? []);
 
                         // directly assign additional property
                         selectedAcc.additional = {
@@ -3421,6 +3422,8 @@ export class CreatePurchaseOrderComponent implements OnInit, OnDestroy, AfterVie
         this.focusOnDescription();
         this.calculateStockEntryAmount(transaction);
         this.calculateWhenTrxAltered(entry, transaction);
+        console.log(transaction);
+
         return transaction;
     }
 
