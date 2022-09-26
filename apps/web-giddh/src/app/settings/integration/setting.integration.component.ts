13e9f49da17a9fa7077e88957653e351df782123
Dilpreet Singh Dang <dilpreet.dang99@gmail.com>
2022-09-26 19:13:05 +0530

    Dilpreet | tally suggestions and imports in sidebar

--- apps/web-giddh/src/app/settings/integration/setting.integration.component.ts
+++ apps/web-giddh/src/app/settings/integration/setting.integration.component.ts
@@ -1032,7 +1032,7 @@ export class SettingIntegrationComponent implements OnInit, AfterViewInit {
      *
      * @memberof SettingIntegrationComponent
      */
-    public toggleIsCopied() : void {
+    public urlCopied(): void {
         this.isCopied = true;
         setTimeout(() => {
             this.isCopied = false;
