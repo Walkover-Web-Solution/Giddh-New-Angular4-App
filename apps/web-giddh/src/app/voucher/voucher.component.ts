795b0f11c8b4487ad5337d5f9edb6171f81b3970
Dilpreet Singh Dang <dilpreet@whozzat.com>
2022-12-05 19:25:53 +0530

    fixed gst number blur issue and linkedPO

--- apps/web-giddh/src/app/voucher/voucher.component.ts
+++ apps/web-giddh/src/app/voucher/voucher.component.ts
@@ -3714,6 +3714,10 @@ export class VoucherComponent implements OnInit, OnDestroy, AfterViewInit, OnCha
         if (this.isCustomerSelected) {
             this.toggleAccountSelectionDropdown(false);
         }
+        if(this.isPurchaseInvoice){
+            this.fieldFilteredOptions = [];
+            this.linkedPo = [];
+        }
     }
 
     public onSelectBankCash(item: IOption) {
