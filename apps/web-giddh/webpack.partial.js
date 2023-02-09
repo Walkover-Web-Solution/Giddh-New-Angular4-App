6402ffd03c78b657b4371ba4ab8a5cf21ad57ac0
ravindersingh <ravinder@walkover.in>
2023-02-09 14:22:37 +0530

    Reverted changes

--- apps/web-giddh/webpack.partial.js
+++ apps/web-giddh/webpack.partial.js
@@ -12,7 +12,7 @@ module.exports = {
             'errlyticsNeeded': JSON.stringify(false),
             'errlyticsKey': JSON.stringify(''),
             'AppUrl': JSON.stringify('http://localhost:3000/'),
-            'ApiUrl': JSON.stringify('https://api.giddh.com/'),
+            'ApiUrl': JSON.stringify('https://apitest.giddh.com/'),
             'APP_FOLDER': JSON.stringify(''),
             'PRODUCTION_ENV': JSON.stringify(false),
             'STAGING_ENV': JSON.stringify(false),
@@ -44,7 +44,7 @@ module.exports = {
             'process.env.errlyticsNeeded': JSON.stringify(false),
             'process.env.errlyticsKey': JSON.stringify(''),
             'process.env.AppUrl': JSON.stringify('http://localhost:3000/'),
-            'process.env.ApiUrl': JSON.stringify('https://api.giddh.com/'),
+            'process.env.ApiUrl': JSON.stringify('https://apitest.giddh.com/'),
             'process.env.APP_FOLDER': JSON.stringify('')
         }),
         new CompressionPlugin({
