da3872e65f8de6f57c575646f6a3585a2c6a9d54
ravindersingh <ravinder@walkover.in>
2023-02-09 11:48:56 +0530

    Fixed mobile number issue in electreon

--- apps/web-giddh/src/app/voucher/voucher.component.ts
+++ apps/web-giddh/src/app/voucher/voucher.component.ts
@@ -8129,7 +8129,7 @@ export class VoucherComponent implements OnInit, OnDestroy, AfterViewInit, OnCha
         const errorMsg = document.querySelector("#init-contact-proforma-error-msg");
         const validMsg = document.querySelector("#init-contact-proforma-valid-msg");
         let errorMap = [this.localeData?.invalid_contact_number, this.commonLocaleData?.app_invalid_country_code, this.commonLocaleData?.app_invalid_contact_too_short, this.commonLocaleData?.app_invalid_contact_too_long, this.localeData?.invalid_contact_number];
-        const intlTelInput = !isElectron ? window['intlTelInput'] : window['intlTelInputGlobals.electron'];
+        const intlTelInput = !isElectron ? window['intlTelInput'] : window['intlTelInputGlobals']['electron'];
         if (intlTelInput && input) {
             this.intl = intlTelInput(input, {
                 nationalMode: true,
