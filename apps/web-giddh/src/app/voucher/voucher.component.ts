b58b413a55f7493c218291cdd732fb1537a60489
Dilpreet Singh Dang <dilpreet@whozzat.com>
2023-03-07 19:08:50 +0530

    fixed unit issues also in purchase order

--- apps/web-giddh/src/app/voucher/voucher.component.ts
+++ apps/web-giddh/src/app/voucher/voucher.component.ts
@@ -4964,7 +4964,7 @@ export class VoucherComponent implements OnInit, OnDestroy, AfterViewInit, OnCha
                         if (stock.unitRates && stock.unitRates.length) {
                             newTrxObj.stockList = this.prepareUnitArr(stock.unitRates);
                         } else {
-                            newTrxObj.stockList.push({ id: newTrxObj.stockDetails.stockUnit.code, text: newTrxObj.stockDetails.stockUnit.uniqueName, rate: newTrxObj.rate });
+                            newTrxObj.stockList.push({ id: newTrxObj.stockDetails.stockUnit.code, text: newTrxObj.stockDetails.stockUnit.uniqueName });
                         }
                     }
 
