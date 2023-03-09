524f416370fb2b035bd9f2fde2136e7afe0e5a0b
Dilpreet Singh Dang <dilpreet@whozzat.com>
2023-03-09 13:56:02 +0530

    fixed PR issues

--- apps/web-giddh/src/app/purchase/create-purchase-order/create-purchase-order.component.ts
+++ apps/web-giddh/src/app/purchase/create-purchase-order/create-purchase-order.component.ts
@@ -2723,7 +2723,7 @@ export class CreatePurchaseOrderComponent implements OnInit, OnDestroy, AfterVie
                     if (stock.unitRates && stock.unitRates.length) {
                         salesTransactionItemClass.stockList = this.prepareUnitArr(stock.unitRates);
                     } else {
-                        salesTransactionItemClass.stockList.push({ id: salesTransactionItemClass.stockDetails.stockUnit.code, text: salesTransactionItemClass.stockDetails.stockUnit.code ?? (salesTransactionItemClass.stockDetails.stockUnit.code === salesTransactionItemClass.stockUnit ? salesTransactionItemClass.stockUnit : unitRate) });
+                        salesTransactionItemClass.stockList.push({ id: salesTransactionItemClass.stockDetails.stockUnit.code, text: (salesTransactionItemClass.stockDetails.stockUnit.code === salesTransactionItemClass.stockUnit ? salesTransactionItemClass.stockDetails.stockUnit.code : salesTransactionItemClass.stockUnit) });
                     }
                 }
                 salesEntryClass.transactions.push(salesTransactionItemClass);
