b2f14c73d52c7e700b4d9dff471cf23d0326c3af
Dilpreet Singh Dang <dilpreet@whozzat.com>
2023-03-09 16:32:41 +0530

    change stockUnit to code

--- apps/web-giddh/src/app/purchase/create-purchase-order/create-purchase-order.component.ts
+++ apps/web-giddh/src/app/purchase/create-purchase-order/create-purchase-order.component.ts
@@ -2718,7 +2718,7 @@ export class CreatePurchaseOrderComponent implements OnInit, OnDestroy, AfterVie
                     stock.unitRates = stock.unitRates || [];
                     let stockUnit: IStockUnit = {
                         id: salesTransactionItemClass.stockDetails.stockUnit.uniqueName,
-                        text: salesTransactionItemClass.stockUnit
+                        text: salesTransactionItemClass.stockDetails.stockUnit.code
                     };
                     salesTransactionItemClass.stockList = [];
                     if (stock.unitRates && stock.unitRates.length) {
