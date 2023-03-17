d61f0069881aa825759e27e628273d792d77770e
ravindersingh <ravinder@walkover.in>
2023-03-17 16:13:45 +0530

    fixed menu open logic

--- apps/web-giddh/src/app/new-inventory/component/inventory-sidebar/inventory-sidebar.component.ts
+++ apps/web-giddh/src/app/new-inventory/component/inventory-sidebar/inventory-sidebar.component.ts
@@ -1,6 +1,6 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, EventEmitter, Output, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { ReplaySubject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { FlatTreeControl } from '@angular/cdk/tree';
@@ -11,154 +11,179 @@ import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree'
  * Each node has a name and an optional list of children.
  */
 interface SidebarNode {
-  icons?: string;
-  name: string;
-  link?: string;
-  children?: SidebarNode[];
+    icons?: string;
+    name: string;
+    link?: string;
+    children?: SidebarNode[];
 }
 
 const TREE_DATA: SidebarNode[] = [
-  {
-    name: 'Stock',
-    icons: 'stock.svg',
-    children: [{ name: 'Create New', icons: 'create-new.svg', link: '/pages/new-inventory/stock/product/create'}, { name: 'Item-wise', icons:'item-wise.svg', link: '/pages/new-inventory/item-wise' }, { name: 'Group-wise' , icons: 'group-wise.svg', link: '/pages/new-inventory/group-wise' }, { name: 'Variant-wise', icons: 'varient-wise.svg', link: '/pages/new-inventory/variant-wise' }, { name: 'Transactions', icons: 'transactions.svg',link: '/pages/new-inventory/inventory-transaction-list' }],
-  },
-  {
-    name: 'Services',
-    icons: 'service.svg',
-    children: [{ name: 'Create New', icons: 'create-new.svg', link: '/pages/new-inventory/stock/service/create'}, { name: 'Item-wise', icons:'item-wise.svg', link: '/pages/new-inventory/item-wise' }, { name: 'Group-wise', icons: 'group-wise.svg', link: '/pages/new-inventory/group-wise' }, { name: 'Variant-wise',icons: 'varient-wise.svg', link: '/pages/new-inventory/variant-wise' }, { name: 'Transactions', icons: 'transactions.svg' }],
-  },
-  {
-    name: 'Fixed Assets',
-    icons: 'fixed-assets.svg',
-    children: [{ name: 'Create New', icons: 'create-new.svg', link: '/pages/new-inventory/stock/product/create' }, { name: 'Item-wise', icons:'item-wise.svg', link: '/pages/new-inventory/item-wise' }, { name: 'Group-wise', icons: 'group-wise.svg', link: '/pages/new-inventory/group-wise' }, { name: 'Variant-wise',icons: 'varient-wise.svg', link: '/pages/new-inventory/variant-wise' }, { name: 'Transactions', icons: 'transactions.svg' }],
-  },
-  {
-    name: 'Branch Transfer',
-    icons: 'branch-transfer.svg'
-  },
-  {
-    name: 'Manufacturing',
-    icons: 'manufacturing.svg'
-  },
-  {
-    name: 'Warehouse Opening Balance',
-    link: '/pages/new-inventory/stock-balance',
-    icons: 'warehouse-opening-balance.svg'
-  },
+    {
+        name: 'Stock',
+        icons: 'stock.svg',
+        children: [{ name: 'Create New', icons: 'create-new.svg', link: '/pages/new-inventory/stock/product/create' }, { name: 'Item-wise', icons: 'item-wise.svg', link: '/pages/new-inventory/item-wise' }, { name: 'Group-wise', icons: 'group-wise.svg', link: '/pages/new-inventory/group-wise' }, { name: 'Variant-wise', icons: 'varient-wise.svg', link: '/pages/new-inventory/variant-wise' }, { name: 'Transactions', icons: 'transactions.svg', link: '/pages/new-inventory/inventory-transaction-list' }],
+    },
+    {
+        name: 'Services',
+        icons: 'service.svg',
+        children: [{ name: 'Create New', icons: 'create-new.svg', link: '/pages/new-inventory/stock/service/create' }, { name: 'Item-wise', icons: 'item-wise.svg', link: '/pages/new-inventory/item-wise' }, { name: 'Group-wise', icons: 'group-wise.svg', link: '/pages/new-inventory/group-wise' }, { name: 'Variant-wise', icons: 'varient-wise.svg', link: '/pages/new-inventory/variant-wise' }, { name: 'Transactions', icons: 'transactions.svg' }],
+    },
+    {
+        name: 'Fixed Assets',
+        icons: 'fixed-assets.svg',
+        children: [{ name: 'Create New', icons: 'create-new.svg', link: '/pages/new-inventory/stock/product/create' }, { name: 'Item-wise', icons: 'item-wise.svg', link: '/pages/new-inventory/item-wise' }, { name: 'Group-wise', icons: 'group-wise.svg', link: '/pages/new-inventory/group-wise' }, { name: 'Variant-wise', icons: 'varient-wise.svg', link: '/pages/new-inventory/variant-wise' }, { name: 'Transactions', icons: 'transactions.svg' }],
+    },
+    {
+        name: 'Branch Transfer',
+        icons: 'branch-transfer.svg'
+    },
+    {
+        name: 'Manufacturing',
+        icons: 'manufacturing.svg'
+    },
+    {
+        name: 'Warehouse Opening Balance',
+        link: '/pages/new-inventory/stock-balance',
+        icons: 'warehouse-opening-balance.svg'
+    },
 ];
 
 /** Flat node with expandable and level information */
 interface SidebarFlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
+    expandable: boolean;
+    name: string;
+    level: number;
 }
 
 @Component({
-  selector: 'inventory-sidebar',
-  templateUrl: './inventory-sidebar.component.html',
-  styleUrls: [`./inventory-sidebar.component.scss`],
+    selector: 'inventory-sidebar',
+    templateUrl: './inventory-sidebar.component.html',
+    styleUrls: [`./inventory-sidebar.component.scss`],
 })
 export class InventorySidebarComponent implements OnDestroy {
-  /* Event emitter for close sidebar popup event */
-  @Output() public closeAsideEvent: EventEmitter<boolean> = new EventEmitter(true);
-  /** True if mobile screen */
-  public isMobileScreen: boolean = true;
-  /** Observable to unsubscribe all the store listeners to avoid memory leaks */
-  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
-  /** Holding data in SidebarNode Array */
-  dataList: any[] = TREE_DATA;
-  /** Holds images folder path */
-  public imgPath: string = "";
-public selectedNode : string = '';
-  private transformer = (node: SidebarNode, level: number) => {
-    return {
-      expandable: !!node.children && node.children.length > 0,
-      name: node.name,
-      level: level,
-      icons: node.icons,
-      link: node.link
+    /* Event emitter for close sidebar popup event */
+    @Output() public closeAsideEvent: EventEmitter<boolean> = new EventEmitter(true);
+    /** True if mobile screen */
+    public isMobileScreen: boolean = true;
+    /** Observable to unsubscribe all the store listeners to avoid memory leaks */
+    private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+    /** Holding data in SidebarNode Array */
+    dataList: any[] = TREE_DATA;
+    /** Holds images folder path */
+    public imgPath: string = "";
+    public selectedNode: string = '';
+    private transformer = (node: SidebarNode, level: number) => {
+        return {
+            expandable: !!node.children && node.children.length > 0,
+            name: node.name,
+            level: level,
+            icons: node.icons,
+            link: node.link
+        };
     };
-  };
 
-  treeControl = new FlatTreeControl<SidebarFlatNode>(
-    node => node.level,
-    node => node.expandable,
-  );
+    treeControl = new FlatTreeControl<SidebarFlatNode>(
+        node => node.level,
+        node => node.expandable,
+    );
 
-  treeFlattener = new MatTreeFlattener(
-    this.transformer,
-    node => node.level,
-    node => node.expandable,
-    node => node.children,
-  );
+    treeFlattener = new MatTreeFlattener(
+        this.transformer,
+        node => node.level,
+        node => node.expandable,
+        node => node.children,
+    );
 
-  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+    dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
-  constructor(
-    private router: Router,
-    private breakPointObserver: BreakpointObserver
-  ) {
-    this.breakPointObserver.observe([
-      '(max-width: 767px)'
-    ]).pipe(takeUntil(this.destroyed$)).subscribe(result => {
-      this.isMobileScreen = result.matches;
-    });
-    this.dataSource.data = TREE_DATA;
-  }
-
-  hasChild = (_: number, node: SidebarFlatNode) => node.expandable;
-
-  /**
-   * Initializes the component
-   *
-   * @memberof InventorySidebarComponent
-  */
-  public ngOnInit(): void {
-    this.imgPath = isElectron ? 'assets/images/' : AppUrl + APP_FOLDER + 'assets/images/';
-  }
-
-  /**
-   * Releases the memory
-   *
-   * @memberof InventorySidebarComponent
-   */
-  public ngOnDestroy(): void {
-    this.destroyed$.next(true);
-    this.destroyed$.complete();
-  }
-
-  /**
-   * This will close the aside panel
-   *
-   * @param {*} [event]
-   * @memberof InventorySidebarComponent
-   */
-  public closeAsidePane(event?: any): void {
-    this.closeAsideEvent.emit(event);
-  }
-
-  /**
-   * This will navigate the user to previous page
-   *
-   * @memberof InventorySidebarComponent
-   */
-  public goToPreviousPage(): void {
-    this.router.navigate(['/pages/inventory']);
-  }
-
-  /**
-   * This will close the settings popup if clicked outside and is mobile screen
-   *
-   * @param {*} [event]
-   * @memberof InventorySidebarComponent
-   */
-  public closeAsidePaneIfMobile(event?: any): void {
-    if (this.isMobileScreen && event?.target?.className !== "icon-bar") {
-      this.closeAsideEvent.emit(event);
+    constructor(
+        private router: Router,
+        private breakPointObserver: BreakpointObserver
+    ) {
+        this.breakPointObserver.observe([
+            '(max-width: 767px)'
+        ]).pipe(takeUntil(this.destroyed$)).subscribe(result => {
+            this.isMobileScreen = result.matches;
+        });
+        this.dataSource.data = TREE_DATA;
     }
-  }
+
+    hasChild = (_: number, node: SidebarFlatNode) => node.expandable;
+
+    /**
+     * Initializes the component
+     *
+     * @memberof InventorySidebarComponent
+    */
+    public ngOnInit(): void {
+        this.imgPath = isElectron ? 'assets/images/' : AppUrl + APP_FOLDER + 'assets/images/';
+
+        this.openActiveMenu(this.router.url);
+
+        this.router.events.pipe(takeUntil(this.destroyed$)).subscribe(event => {
+            console.log(event);
+            if (event instanceof NavigationEnd) {
+                this.openActiveMenu(event.url);
+            }
+        });
+    }
+
+    public openActiveMenu(url: string): void {
+        let activeNodeIndex = null;
+        TREE_DATA.forEach((tree, index) => {
+            if(activeNodeIndex === null) {
+                let activeNode = tree.children?.filter(node => node.link === url);
+                if(activeNode?.length) {
+                    activeNodeIndex = index;
+                }
+            }
+        });
+
+        if(activeNodeIndex !== null) {
+            this.treeControl.expand(this.treeControl.dataNodes[activeNodeIndex]);
+        }
+    }
+
+    /**
+     * Releases the memory
+     *
+     * @memberof InventorySidebarComponent
+     */
+    public ngOnDestroy(): void {
+        this.destroyed$.next(true);
+        this.destroyed$.complete();
+    }
+
+    /**
+     * This will close the aside panel
+     *
+     * @param {*} [event]
+     * @memberof InventorySidebarComponent
+     */
+    public closeAsidePane(event?: any): void {
+        this.closeAsideEvent.emit(event);
+    }
+
+    /**
+     * This will navigate the user to previous page
+     *
+     * @memberof InventorySidebarComponent
+     */
+    public goToPreviousPage(): void {
+        this.router.navigate(['/pages/inventory']);
+    }
+
+    /**
+     * This will close the settings popup if clicked outside and is mobile screen
+     *
+     * @param {*} [event]
+     * @memberof InventorySidebarComponent
+     */
+    public closeAsidePaneIfMobile(event?: any): void {
+        if (this.isMobileScreen && event?.target?.className !== "icon-bar") {
+            this.closeAsideEvent.emit(event);
+        }
+    }
 
 }
 
