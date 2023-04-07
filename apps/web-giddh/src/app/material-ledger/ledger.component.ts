df1f6acc171eace00c6fc58c31e97d563f043e7c
ravindersingh <ravinder@walkover.in>
2023-04-07 11:06:57 +0530

    fixed bug

--- apps/web-giddh/src/app/material-ledger/ledger.component.ts
+++ apps/web-giddh/src/app/material-ledger/ledger.component.ts
@@ -1909,6 +1909,7 @@ export class LedgerComponent implements OnInit, OnDestroy {
         }
         this.asideMenuState = this.asideMenuState === 'out' ? 'in' : 'out';
         this.toggleBodyClass();
+        this.cdRf.detectChanges();
     }
 
     public toggleOtherTaxesAsidePane(updateLedgerModalVm: any): void {
