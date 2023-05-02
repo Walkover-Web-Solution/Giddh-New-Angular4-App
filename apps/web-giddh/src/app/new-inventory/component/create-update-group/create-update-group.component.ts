c0ee0171f953c6cf6feebc7389b7e41ea775e579
Dilpreet Singh Dang <dilpreet@whozzat.com>
2023-05-02 20:45:44 +0530

    fixed issue

--- apps/web-giddh/src/app/new-inventory/component/create-update-group/create-update-group.component.ts
+++ apps/web-giddh/src/app/new-inventory/component/create-update-group/create-update-group.component.ts
@@ -136,8 +136,8 @@ export class CreateUpdateGroupComponent implements OnInit, OnDestroy {
             if (response?.length > 0) {
                 this.taxes = response || [];
             }
+            this.changeDetection.detectChanges();
         });
-        this.changeDetection.detectChanges();
     }
 
 
@@ -216,6 +216,7 @@ export class CreateUpdateGroupComponent implements OnInit, OnDestroy {
         }
         // this.groupForm.get('taxes')?.patchValue(this.taxTempArray?.map(taxTemp => taxTemp?.uniqueName));
         this.selectedTaxes = this.taxTempArray.map(tax => tax?.uniqueName);
+        this.changeDetection.detectChanges();
     }
 
     /**
@@ -225,37 +226,40 @@ export class CreateUpdateGroupComponent implements OnInit, OnDestroy {
      */
     public saveGroup(): void {
         this.groupForm.controls['parentStockGroupUniqueName'].setValue(this.stockGroupUniqueName);
-        this.toggleLoader(true);
+        this.groupForm.controls['type'].setValue(this.stockType);
         if (this.groupUniqueName) {
-            this.groupForm.controls['type'].setValue(this.stockType);
+            this.toggleLoader(true);
             this.inventoryService.UpdateStockGroup(this.groupForm?.value, this.groupUniqueName).pipe(takeUntil(this.destroyed$)).subscribe(response => {
                 if (response?.status === "success") {
                     this.toggleLoader(false);
                     this.getStockGroups();
-                    this.getTaxes();
+                    this.selectedGroupTaxes();
                     this.toaster.clearAllToaster();
                     this.toaster.successToast(this.localeData?.stock_group_update);
                 } else {
-                    this.toaster.clearAllToaster();
                     this.toggleLoader(false);
+                    this.toaster.clearAllToaster();
                     this.toaster.errorToast(response?.message);
                 }
+                this.changeDetection.detectChanges();
             });
+
         } else {
-            this.groupForm.controls['type'].setValue(this.stockType);
+            this.toggleLoader(true);
             this.inventoryService.CreateStockGroup(this.groupForm?.value).pipe(takeUntil(this.destroyed$)).subscribe(response => {
                 if (response?.status === "success") {
                     this.toggleLoader(false);
                     this.getStockGroups();
-                    this.getTaxes();
+                    this.resetTaxes();
                     this.resetGroupForm();
                     this.toaster.clearAllToaster();
                     this.toaster.successToast(this.localeData?.stock_group_create);
                 } else {
-                    this.toaster.clearAllToaster();
                     this.toggleLoader(false);
+                    this.toaster.clearAllToaster();
                     this.toaster.errorToast(response?.message);
                 }
+                this.changeDetection.detectChanges();
             });
         }
     }
@@ -334,6 +338,30 @@ export class CreateUpdateGroupComponent implements OnInit, OnDestroy {
     }
 
     /**
+     * This will reset the taxes list
+     *
+     * @memberof CreateUpdateGroupComponent
+     */
+    public resetTaxes(): void {
+        this.taxes.forEach(tax => {
+            tax.isChecked = false;
+            tax.isDisabled = false;
+            return tax;
+        });
+        this.changeDetection.detectChanges();
+    }
+
+    public selectedGroupTaxes(): void {
+        this.taxes.forEach(tax => {
+            if (tax?.isChecked) {
+                this.selectTax(tax);
+            }
+            return tax;
+        });
+        this.changeDetection.detectChanges();
+    }
+
+    /**
     * This will use for set hsn/sac value default
     *
     * @param {*} transaction
@@ -387,11 +415,12 @@ export class CreateUpdateGroupComponent implements OnInit, OnDestroy {
                     taxes: response.body.taxes
                 });
                 this.groupForm.updateValueAndValidity();
+                this.changeDetection.detectChanges();
             } else {
                 this.toggleLoader(false);
                 this.toaster.showSnackBar("error", response?.message);
+                this.changeDetection.detectChanges();
             }
-            this.changeDetection.detectChanges();
         });
     }
 
