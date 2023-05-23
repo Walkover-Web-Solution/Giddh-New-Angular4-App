2bd0eb407f71a50afbc386b578cbdf287b93b76f
nisha <nisha@walkover.in>
2023-05-23 18:15:49 +0530

    Inventory Master-sidebar-page

--- apps/web-giddh/src/app/new-inventory/component/inventory-master/inventory-master.component.ts
+++ apps/web-giddh/src/app/new-inventory/component/inventory-master/inventory-master.component.ts
@@ -7,22 +7,22 @@ import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from "@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InventoryMasterComponent implements OnInit, OnDestroy {
-  public asideCreateUnitMenuState: string = 'out';
-  public asideCreateGroupMenuState: string = 'out';
+  public createUpdateStock: string = 'out';
+  public createUpdateGroup: string = 'out';
 
   /* Create unit aside pane open function */
   public createUnitToggleAsidePane(): void {
-    this.asideCreateUnitMenuState = this.asideCreateUnitMenuState === 'out' ? 'in' : 'out';
+    this.createUpdateStock = this.createUpdateStock === 'out' ? 'in' : 'out';
     this.toggleBodyClass();
   }
   /* Create unit aside pane open function */
   public createGroupToggleAsidePane(): void {
-    this.asideCreateGroupMenuState = this.asideCreateGroupMenuState === 'out' ? 'in' : 'out';
+    this.createUpdateGroup = this.createUpdateGroup === 'out' ? 'in' : 'out';
     this.toggleBodyClass();
   }
   /* Aside pane toggle fixed class */
   public toggleBodyClass(): void {
-    if (this.asideCreateUnitMenuState === 'in' || this.asideCreateGroupMenuState === 'in') {
+    if (this.createUpdateStock === 'in' || this.createUpdateGroup === 'in') {
       document.querySelector('body').classList.add('fixed');
     } else {
       document.querySelector('body').classList.remove('fixed');
