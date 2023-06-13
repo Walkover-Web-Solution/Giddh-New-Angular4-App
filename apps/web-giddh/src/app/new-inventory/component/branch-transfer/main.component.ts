50d11f00b8dfd46ffa952cb12e8f4a7a8b3e1ad9
Divyanshu <divyanshushrivastava123@gmail.com>
2023-06-13 18:25:45 +0530

    Remove unused comment

--- apps/web-giddh/src/app/new-inventory/component/branch-transfer/main.component.ts
+++ apps/web-giddh/src/app/new-inventory/component/branch-transfer/main.component.ts
@@ -5,5 +5,5 @@ import { Component } from "@angular/core";
     templateUrl: './main.component.html'
 })
 export class MainComponent {
-    /* This will hold the value out/in to open/close setting sidebar popup */
+  
 }
