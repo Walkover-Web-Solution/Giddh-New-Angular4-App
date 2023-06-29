c59a9b57f4f8b81ae64587bf09bca1fdf1c7949e
Divyanshu <divyanshushrivastava123@gmail.com>
2023-06-29 17:38:30 +0530

    datepicker disabled background issue fix

--- apps/web-giddh/src/app/theme/giddh-datepicker/giddh-datepicker.component.ts
+++ apps/web-giddh/src/app/theme/giddh-datepicker/giddh-datepicker.component.ts
@@ -37,6 +37,8 @@ export class GiddhDatepickerComponent implements ControlValueAccessor, OnInit, O
     @Input() public disabled: boolean = false;
     /** True if we need to show error */
     @Input() public showError: boolean = false;
+    /** Hold Mat Label Text*/
+    @Input() public label: any;
     /** Emitting selected date object as output */
     @Output() public dateSelected: EventEmitter<any> = new EventEmitter<any>();
     /** Emitting the state of datepicker (open/close) */
