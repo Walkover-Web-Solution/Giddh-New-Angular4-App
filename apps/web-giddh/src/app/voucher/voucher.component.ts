d00c46f220f29f76ca3890d26f148baea08f4afe
prateek-giddh <prateekmishra@giddh.com>
2023-07-01 11:25:27 +0530

    fix: Purchase order details are not coming when user link the PO in PB voucher page

--- apps/web-giddh/src/app/voucher/voucher.component.ts
+++ apps/web-giddh/src/app/voucher/voucher.component.ts
@@ -3437,7 +3437,7 @@ export class VoucherComponent implements OnInit, OnDestroy, AfterViewInit, OnCha
                 };
                 if (selectedAcc.additional.stock) {
                     txn.isStockTxn = true;
-                    this.loadStockVariants(selectedAcc.additional.stock.uniqueName);
+                    this.loadStockVariants(selectedAcc.additional.stock.uniqueName, isLinkedPoItem ? entryIndex : undefined);
                 } else {
                     this.loadDetails(this.currentTxnRequestObject[this.activeIndx]);
                 }
@@ -3472,7 +3472,14 @@ export class VoucherComponent implements OnInit, OnDestroy, AfterViewInit, OnCha
      * @memberof VoucherComponent
      */
 
-    public calculateItemValues(selectedAcc: any, transaction: SalesTransactionItemClass, entry: SalesEntryClass, calculateTransaction: boolean = true, isBulkItem?: boolean): SalesTransactionItemClass {
+    public calculateItemValues(
+        selectedAcc: any,
+        transaction: SalesTransactionItemClass,
+        entry: SalesEntryClass,
+        calculateTransaction: boolean = true,
+        isBulkItem?: boolean,
+        isLinkedPoItem?: boolean
+    ): SalesTransactionItemClass {
         let o = cloneDeep(selectedAcc.additional);
         const variant = o.stock?.variant;
         const isInclusiveEntry = (variant?.purchaseTaxInclusive && o.category === 'expenses') ||
@@ -3574,6 +3581,8 @@ export class VoucherComponent implements OnInit, OnDestroy, AfterViewInit, OnCha
             transaction.isStockTxn = true;
             if (isBulkItem) {
                 transaction.variant = selectedAcc.variant;
+            } else if (isLinkedPoItem) {
+                transaction.variant = o.stock.variant;
             }
             // Stock item, show the warehouse drop down if it is hidden
             if ((this.isMultiCurrencyModule()) && !this.shouldShowWarehouse) {
@@ -8617,7 +8626,7 @@ export class VoucherComponent implements OnInit, OnDestroy, AfterViewInit, OnCha
                     uNameStr: selectedAcc.additional && selectedAcc.additional.parentGroups ? selectedAcc.additional.parentGroups.map(parent => parent?.uniqueName).join(', ') : data.body.parentGroups.join(', '),
                     category: data.body.category
                 };
-                txn = this.calculateItemValues(selectedAcc, txn, entry, !isLinkedPoItem);
+                txn = this.calculateItemValues(selectedAcc, txn, entry, !isLinkedPoItem, false, isLinkedPoItem);
 
                 if (isLinkedPoItem) {
                     txn.applicableTaxes = entry.taxList;
