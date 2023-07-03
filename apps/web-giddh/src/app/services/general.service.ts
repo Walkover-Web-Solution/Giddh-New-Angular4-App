f5843ad05e91b51b1107e88213cf1257dc67fc6a
Dilpreet Singh Dang <dilpreet@whozzat.com>
2023-07-03 18:43:07 +0530

    Dilpreet | Stage | TEST- Reverse charge button is not in material

--- apps/web-giddh/src/app/services/general.service.ts
+++ apps/web-giddh/src/app/services/general.service.ts
@@ -207,12 +207,10 @@ export class GeneralService {
      */
     public getRcmConfiguration(isRcmSelected: boolean, commonLocaleData?: any): ConfirmationModalConfiguration {
         const buttons: Array<ConfirmationModalButton> = [{
-            text: (commonLocaleData) ? commonLocaleData?.app_yes : 'Yes',
-            cssClass: 'btn btn-success'
+            text: (commonLocaleData) ? commonLocaleData?.app_yes : 'Yes'
         },
         {
-            text: (commonLocaleData) ? commonLocaleData?.app_no : 'No',
-            cssClass: 'btn btn-danger'
+            text: (commonLocaleData) ? commonLocaleData?.app_no : 'No'
         }];
         const headerText: string = (commonLocaleData) ? commonLocaleData?.app_rc_heading : 'Reverse Charge Confirmation';
         const headerCssClass: string = 'd-inline-block mr-1';
@@ -634,12 +632,10 @@ export class GeneralService {
      */
     public getDateChangeConfiguration(localeData: any, commonLocaleData: any, isVoucherDateSelected: boolean): ConfirmationModalConfiguration {
         const buttons: Array<ConfirmationModalButton> = [{
-            text: commonLocaleData?.app_yes,
-            cssClass: 'btn btn-success'
+            text: commonLocaleData?.app_yes
         },
         {
-            text: commonLocaleData?.app_no,
-            cssClass: 'btn btn-danger'
+            text: commonLocaleData?.app_no
         }];
         const headerText: string = localeData?.date_change_confirmation_heading;
         const headerCssClass: string = 'd-inline-block mr-1';
@@ -675,12 +671,10 @@ export class GeneralService {
      */
     public getAttachmentDeleteConfiguration(localeData: any, commonLocaleData: any): ConfirmationModalConfiguration {
         const buttons: Array<ConfirmationModalButton> = [{
-            text: commonLocaleData?.app_yes,
-            cssClass: 'btn btn-success'
+            text: commonLocaleData?.app_yes
         },
         {
-            text: commonLocaleData?.app_no,
-            cssClass: 'btn btn-danger'
+            text: commonLocaleData?.app_no
         }];
         const headerText: string = commonLocaleData?.app_confirmation;
         const headerCssClass: string = 'd-inline-block mr-1';
