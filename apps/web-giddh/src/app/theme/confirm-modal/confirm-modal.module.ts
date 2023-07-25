9c3d1f58a3e894ce52c234e316f1bd4c9805011d
nisha <nisha@walkover.in>
2023-07-25 10:01:26 +0530

    modal in material

--- apps/web-giddh/src/app/theme/confirm-modal/confirm-modal.module.ts
+++ apps/web-giddh/src/app/theme/confirm-modal/confirm-modal.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { ConfirmModalComponent } from './confirm-modal.component';
 import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
     declarations: [ConfirmModalComponent],
-    imports: [CommonModule, MatButtonModule],
+    imports: [
+        CommonModule, 
+        MatButtonModule,
+        MatDialogModule
+    ],
     exports: [
         ConfirmModalComponent
     ],
