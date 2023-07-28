ac3814a26a3321531cfec2d361c0069bf36b65da
Dilpreet Singh Dang <dilpreet@whozzat.com>
2023-07-28 16:03:41 +0530

    Dilpreet | Stage | Create Company Prod Bugs

--- apps/web-giddh/src/app/add-company/add-company-module.ts
+++ apps/web-giddh/src/app/add-company/add-company-module.ts
@@ -12,6 +12,7 @@ import { FormFieldsModule } from "../theme/form-fields/form-fields.module";
 import { TranslateDirectiveModule } from "../theme/translate/translate.directive.module";
 import { AddCompanyComponent } from "./add-company.component";
 import { AddCompanyRoutingModule } from "./add-company.routing.module";
+import { ConfirmModalModule } from '../theme/confirm-modal';
 
 @NgModule({
     declarations: [
@@ -30,7 +31,8 @@ import { AddCompanyRoutingModule } from "./add-company.routing.module";
         FormFieldsModule,
         HamburgerMenuModule,
         TranslateDirectiveModule,
-        GiddhPageLoaderModule
+        GiddhPageLoaderModule,
+        ConfirmModalModule
     ],
     providers: [
 
