f270e259e4deac0bfda73d9cdb14184727a1d0bf
ravindersingh <ravinder@walkover.in>
2023-08-25 18:24:44 +0530

    Reverted plain env

--- apps/web-giddh/src/app/settings/integration/setting.integration.component.ts
+++ apps/web-giddh/src/app/settings/integration/setting.integration.component.ts
@@ -147,7 +147,7 @@ export class SettingIntegrationComponent implements OnInit, AfterViewInit {
     private plaidLinkHandler: PlaidLinkHandler;
     /** This will hold plaid configuration */
     private plaidConfig: LegacyPlaidConfig = {
-        env: "sandbox",
+        env: "development",
         token: null,
         product: ["auth","transactions"],
         onSuccess: undefined,
