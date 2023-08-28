89ec2ba825d9d942da9581f5aecfbab5a43686f9
ravindersingh <ravinder@walkover.in>
2023-08-28 13:05:02 +0530

    Reverted team id from notarize.js

--- electron-sign/notarize.js
+++ electron-sign/notarize.js
@@ -21,9 +21,7 @@ module.exports = async function (params) {
             appBundleId: appId,
             appPath: appPath,
             appleId: process.env.NOTARIZE_EMAIL, // enter Credential to generate mac's electron build
-            appleIdPassword: process.env.NOTARIZE_PASS,
-            tool: 'notarytool',
-            teamId: "F3U6Z5L2EJ"
+            appleIdPassword: process.env.NOTARIZE_PASS
         });
     } catch (error) {
         console.error(error);
