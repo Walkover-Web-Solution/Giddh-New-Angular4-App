2f458a3b3990a253ba0f06e3eed58b38e4a47768
ravindersingh <ravinder@walkover.in>
2023-09-02 12:46:00 +0530

    Reverted package name

--- electron-sign/notarize.js
+++ electron-sign/notarize.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const electronNotarize = require('electron-notarize');
+const electronNotarize = require('@electron/notarize');
 module.exports = async function (params) {
     // Only notarize the app on Mac OS only.
     if (process.platform !== 'darwin') {
