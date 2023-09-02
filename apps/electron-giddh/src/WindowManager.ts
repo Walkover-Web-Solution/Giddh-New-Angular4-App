25d870578258b285878adfc66d5a626f5dc8df2a
ravindersingh <ravinder@walkover.in>
2023-09-02 13:11:53 +0530

    Updated user agent

--- apps/electron-giddh/src/WindowManager.ts
+++ apps/electron-giddh/src/WindowManager.ts
@@ -98,8 +98,9 @@ export default class WindowManager {
                 window.maximize();
             }
             setTimeout(() => {
+                window.webContents.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36";
                 window.loadURL(descriptor.url,
-                    { userAgent: 'Chrome' }
+                    { userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' }
                 );
                 window.show();
                 this.registerWindowEventHandlers(window, descriptor);
