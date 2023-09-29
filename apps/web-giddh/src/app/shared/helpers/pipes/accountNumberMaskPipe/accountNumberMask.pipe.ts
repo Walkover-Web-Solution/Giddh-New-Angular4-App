98b8bc8c119d1619916e85e73017a97d8cfdbc0d
Dilpreet Singh Dang <dilpreet@whozzat.com>
2023-09-29 13:44:40 +0530

    PR changes

--- apps/web-giddh/src/app/shared/helpers/pipes/accountNumberMaskPipe/accountNumberMask.pipe.ts
+++ apps/web-giddh/src/app/shared/helpers/pipes/accountNumberMaskPipe/accountNumberMask.pipe.ts
@@ -6,25 +6,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AccountNumberMaskPipe implements PipeTransform {
     public constructor() { }
-/**
- * This will be use for convert account number to mask format.
- *
- * @param {string} accountNumber
- * @return {*}  {string}
- * @memberof AccountNumberMaskPipe
- */
-public transform(accountNumber: string): string {
+    /**
+     * This will be use for convert account number to mask format.
+     *
+     * @param {string} accountNumber
+     * @return {*}  {string}
+     * @memberof AccountNumberMaskPipe
+     */
+    public transform(accountNumber: string, defaultAccountNumberLength:number = 4): string {
         if (!accountNumber) {
             return '';
         }
 
-        // Check if the input contains at least four digits
-        if (accountNumber.length < 4) {
-            throw new Error('Account number should contain at least four digits');
-        }
-
         // Mask all characters except the last four digits
-        const maskedAccount = '*'.repeat(accountNumber.length - 4) + accountNumber.slice(-4);
+        const maskedAccount = '*'.repeat(accountNumber.length - defaultAccountNumberLength) + accountNumber.slice(-defaultAccountNumberLength);
         return maskedAccount;
 
     }
