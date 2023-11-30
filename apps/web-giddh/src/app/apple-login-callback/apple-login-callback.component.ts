060d17d679c6fb803c97e794a3f6907fd7e0d420
ravindersingh <ravinder@walkover.in>
2023-11-30 18:59:58 +0530

    debugging

--- apps/web-giddh/src/app/apple-login-callback/apple-login-callback.component.ts
+++ apps/web-giddh/src/app/apple-login-callback/apple-login-callback.component.ts
@@ -1,20 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "apple-login-callback",
     templateUrl: "./apple-login-callback.component.html",
     styleUrls: ["./apple-login-callback.component.scss"]
 })
-export class AppleLoginCallbackComponent implements OnInit {
+export class AppleLoginCallbackComponent {
 
     constructor (
 
     ) {
 
-    }
-
-    public ngOnInit(): void {
-        //window.opener.postMessage({ origin: 'giddh', data: "" }, "*");
-        //window.close();
     }
 }
