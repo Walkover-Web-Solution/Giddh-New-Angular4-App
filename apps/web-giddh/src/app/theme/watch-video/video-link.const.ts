6919b86959705605449314f6e7c7b864b10279a1
Divyanshu <divyanshushrivastava123@gmail.com>
2023-12-08 13:17:46 +0530

    working on video link for cash and sales-preview invoice

--- apps/web-giddh/src/app/theme/watch-video/video-link.const.ts
+++ apps/web-giddh/src/app/theme/watch-video/video-link.const.ts
@@ -1,3 +1,5 @@
 export const VIDEOLINK = {
-    VOUCHER: 'https://www.youtube.com/embed/TDEl-daOySU?si=NJovgqKIUEwxG54M&amp;start=82',
+    VOUCHER_SALES: 'https://www.youtube.com/embed/TDEl-daOySU?si=NJovgqKIUEwxG54M&amp;start=82',
+    VOUCHER_CASH: 'https://www.youtube.com/embed/TDEl-daOySU?si=NJovgqKIUEwxG54M&amp',
+    SALES_PREVIEW: 'https://www.youtube.com/embed/y9at0bOnHcw?si=FjqP45U0hWvIcFIM'
 };
