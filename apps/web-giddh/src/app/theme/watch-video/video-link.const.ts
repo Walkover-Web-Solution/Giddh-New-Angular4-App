59431b9dbbf3bba9ac6ba69eb6efa7d22fed5b53
Divyanshu <divyanshushrivastava123@gmail.com>
2023-12-14 18:38:51 +0530

    pr changes

--- apps/web-giddh/src/app/theme/watch-video/video-link.const.ts
+++ apps/web-giddh/src/app/theme/watch-video/video-link.const.ts
@@ -1,6 +1,6 @@
 export const VIDEOLINK = {
     VOUCHER_SALES: 'https://www.youtube.com/embed/TDEl-daOySU?si=NJovgqKIUEwxG54M&amp;start=82',
-    VOUCHER_CASH: 'https://www.youtube.com/embed/TDEl-daOySU?si=NJovgqKIUEwxG54M&amp',
+    VOUCHER_CASH: 'https://www.youtube.com/embed/TDEl-daOySU?si=NJovgqKIUEwxG54M',
     SALES_PREVIEW: 'https://www.youtube.com/embed/y9at0bOnHcw?si=FjqP45U0hWvIcFIM',
     INVENTORY_MASTER: "https://www.youtube.com/embed/SCymMiHtG30?si=7_jYkM8LBwFz_v3r",
     INVENTORY_MASTER_GROUP: "https://www.youtube.com/embed/SCymMiHtG30?si=xPtwoa7nD55bJg2C&amp;start=41",
