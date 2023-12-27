800f24c40414289a7c90d3e68bcc1f44dc5e7402
Divyanshu <divyanshushrivastava123@gmail.com>
2023-12-27 17:48:37 +0530

    video link add in daybook and other video link design issue fixes

--- apps/web-giddh/src/app/theme/watch-video/video-link.const.ts
+++ apps/web-giddh/src/app/theme/watch-video/video-link.const.ts
@@ -5,7 +5,7 @@ export const VIDEOLINK = {
     INVENTORY_MASTER: "https://www.youtube.com/embed/SCymMiHtG30?si=7_jYkM8LBwFz_v3r",
     INVENTORY_MASTER_GROUP: "https://www.youtube.com/embed/SCymMiHtG30?si=xPtwoa7nD55bJg2C&amp;start=41",
     INVENTORY_CREATE_STOCK: "https://www.youtube.com/embed/BpQNwfVqvto?si=RMDls_K39f9gKp-7",
-    ECOMM_INTEGRATION: "https://www.youtube.com/embed/XQaJt4cZPRA?si=ogoyFlJNThTO4MhP",
+    ECOMM_INTEGRATION: "https://www.youtube.com/embed/VztCB0KeG70?si=5KnjTJbnQjEXFLhD",
     GST_FILLING_DASHBOARD: "https://www.youtube.com/embed/7NcUnWRlq6Q?si=yucjgvQ89N-nmuXa",
     GSTR3B: "https://www.youtube.com/embed/DMNqDuIqK0Q?si=a2uzS1Lgg14v4RJ1",
     GSTR1: "https://www.youtube.com/embed/WfdcKZs7VbU?si=foAEswap_4mxQhb5",
@@ -18,7 +18,4 @@ export const VIDEOLINK = {
     IMPORT_MASTER: "https://www.youtube.com/embed/Ij_kNKOzuqo?si=KLLx0XolqYBYB_U9",
     BARCODE: "https://www.youtube.com/embed/x1OJlwia3OY?si=1LBbTy-hxJI4bfKe",
     DAYBOOK: "https://www.youtube.com/embed/zYFouv7uaEo?si=34xOTDVT1-fC27H_",
-    SHOPIFY_INTEGRATION: "https://www.youtube.com/embed/VztCB0KeG70?si=5KnjTJbnQjEXFLhD"
-
-
 };
