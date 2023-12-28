8d43d4f7c30b0cc0d226b9c8295f72ea60829138
Dilpreet Singh Dang <dilpreet@whozzat.com>
2023-12-28 19:46:43 +0530

    Dilpreet | Stage | Prod Bugs

--- apps/web-giddh/src/app/shared/tax-sidebar/tax-sidebar.component.ts
+++ apps/web-giddh/src/app/shared/tax-sidebar/tax-sidebar.component.ts
@@ -100,6 +100,7 @@ export class TaxSidebarComponent implements OnInit, OnDestroy {
                     this.showGstMenus = false;
                 }
             }
+            this.changeDetectionRef.detectChanges();
         });
 
         this.getCurrentPeriod$.subscribe(period => {
