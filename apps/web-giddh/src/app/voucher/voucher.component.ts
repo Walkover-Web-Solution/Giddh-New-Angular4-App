8fdc3f3311bdc30d7aaccf7bde41f108ca761c23
Dilpreet Singh Dang <dilpreet@whozzat.com>
2024-01-04 17:49:21 +0530

    code ref

--- apps/web-giddh/src/app/voucher/voucher.component.ts
+++ apps/web-giddh/src/app/voucher/voucher.component.ts
@@ -3166,7 +3166,7 @@ export class VoucherComponent implements OnInit, OnDestroy, AfterViewInit, OnCha
 
                     trx?.stockDetails?.variant?.unitRates?.forEach(unitRate => {
                         let matchedUnit = unitRate?.stockUnitUniqueName === trx?.stockUnit;
-                        if (matchedUnit) {
+                        if (matchedUnit && this.isBulkEntryInProgress) {
                             trx.rate = Number((unitRate.rate / this.exchangeRate).toFixed(this.highPrecisionRate));
                             trx.quantity = trx?.stockDetails?.variant?.variantDiscount?.quantity || trx.quantity;
                         }
@@ -3826,8 +3826,8 @@ export class VoucherComponent implements OnInit, OnDestroy, AfterViewInit, OnCha
 
         const isMrpDiscountInclusive = !variant?.discountExclusive && variant?.unitRates?.filter(ur => ur.stockUnitUniqueName === transaction.stockUnit);
         if (isMrpDiscountInclusive?.length) {
-            transaction.rate = Number((isMrpDiscountInclusive[0].rate / this.exchangeRate).toFixed(this.highPrecisionRate));
             if (!isBulkItem) {
+                transaction.rate = Number((isMrpDiscountInclusive[0].rate / this.exchangeRate).toFixed(this.highPrecisionRate));
                 transaction.quantity = variant?.variantDiscount?.quantity || transaction.quantity;
             }
             transaction.taxInclusive = true;
