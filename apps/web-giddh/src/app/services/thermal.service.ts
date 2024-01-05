8094daf09ca800e63e5168799b0454be3c4cb1e5
ravindersingh <ravinder@walkover.in>
2024-01-05 15:32:47 +0530

    added demo thermal certificate

--- apps/web-giddh/src/app/services/thermal.service.ts
+++ apps/web-giddh/src/app/services/thermal.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { PrinterFormatService } from './printer.format.service';
+import { KJUR, KEYUTIL, stob64, hextorstr } from 'jsrsasign';
 import * as qz from "qz-tray";
+
 @Injectable()
 export class ThermalService {
 
@@ -517,7 +519,7 @@ export class ThermalService {
 
                 if (itemName?.length < productName?.length) {
                     let lastIndex = productName.length - itemName.length;
-    
+
                     for (var itemNameIndex = itemName?.length; itemNameIndex < lastIndex; itemNameIndex = itemNameIndex + 11) {
                         items += this.printerFormat.formatCenter(this.printerFormat.leftAlign + productName?.substr(itemNameIndex, 11));
                     }
@@ -678,67 +680,71 @@ export class ThermalService {
                 this.printerFormat.lineBreak + this.printerFormat.lineBreak +
                 this.printerFormat.leftAlign + this.justifyText(firmNameField, "");
 
-            qz.security.setCertificatePromise(function (resolve, reject) {
+            qz.security.setCertificatePromise((resolve, reject) => {
                 resolve("-----BEGIN CERTIFICATE-----\n" +
-                "MIIFAzCCAuugAwIBAgICEAIwDQYJKoZIhvcNAQEFBQAwgZgxCzAJBgNVBAYTAlVT\n" +
-                "MQswCQYDVQQIDAJOWTEbMBkGA1UECgwSUVogSW5kdXN0cmllcywgTExDMRswGQYD\n" +
-                "VQQLDBJRWiBJbmR1c3RyaWVzLCBMTEMxGTAXBgNVBAMMEHF6aW5kdXN0cmllcy5j\n" +
-                "b20xJzAlBgkqhkiG9w0BCQEWGHN1cHBvcnRAcXppbmR1c3RyaWVzLmNvbTAeFw0x\n" +
-                "NTAzMTkwMjM4NDVaFw0yNTAzMTkwMjM4NDVaMHMxCzAJBgNVBAYTAkFBMRMwEQYD\n" +
-                "VQQIDApTb21lIFN0YXRlMQ0wCwYDVQQKDAREZW1vMQ0wCwYDVQQLDAREZW1vMRIw\n" +
-                "EAYDVQQDDAlsb2NhbGhvc3QxHTAbBgkqhkiG9w0BCQEWDnJvb3RAbG9jYWxob3N0\n" +
-                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtFzbBDRTDHHmlSVQLqjY\n" +
-                "aoGax7ql3XgRGdhZlNEJPZDs5482ty34J4sI2ZK2yC8YkZ/x+WCSveUgDQIVJ8oK\n" +
-                "D4jtAPxqHnfSr9RAbvB1GQoiYLxhfxEp/+zfB9dBKDTRZR2nJm/mMsavY2DnSzLp\n" +
-                "t7PJOjt3BdtISRtGMRsWmRHRfy882msBxsYug22odnT1OdaJQ54bWJT5iJnceBV2\n" +
-                "1oOqWSg5hU1MupZRxxHbzI61EpTLlxXJQ7YNSwwiDzjaxGrufxc4eZnzGQ1A8h1u\n" +
-                "jTaG84S1MWvG7BfcPLW+sya+PkrQWMOCIgXrQnAsUgqQrgxQ8Ocq3G4X9UvBy5VR\n" +
-                "CwIDAQABo3sweTAJBgNVHRMEAjAAMCwGCWCGSAGG+EIBDQQfFh1PcGVuU1NMIEdl\n" +
-                "bmVyYXRlZCBDZXJ0aWZpY2F0ZTAdBgNVHQ4EFgQUpG420UhvfwAFMr+8vf3pJunQ\n" +
-                "gH4wHwYDVR0jBBgwFoAUkKZQt4TUuepf8gWEE3hF6Kl1VFwwDQYJKoZIhvcNAQEF\n" +
-                "BQADggIBAFXr6G1g7yYVHg6uGfh1nK2jhpKBAOA+OtZQLNHYlBgoAuRRNWdE9/v4\n" +
-                "J/3Jeid2DAyihm2j92qsQJXkyxBgdTLG+ncILlRElXvG7IrOh3tq/TttdzLcMjaR\n" +
-                "8w/AkVDLNL0z35shNXih2F9JlbNRGqbVhC7qZl+V1BITfx6mGc4ayke7C9Hm57X0\n" +
-                "ak/NerAC/QXNs/bF17b+zsUt2ja5NVS8dDSC4JAkM1dD64Y26leYbPybB+FgOxFu\n" +
-                "wou9gFxzwbdGLCGboi0lNLjEysHJBi90KjPUETbzMmoilHNJXw7egIo8yS5eq8RH\n" +
-                "i2lS0GsQjYFMvplNVMATDXUPm9MKpCbZ7IlJ5eekhWqvErddcHbzCuUBkDZ7wX/j\n" +
-                "unk/3DyXdTsSGuZk3/fLEsc4/YTujpAjVXiA1LCooQJ7SmNOpUa66TPz9O7Ufkng\n" +
-                "+CoTSACmnlHdP7U9WLr5TYnmL9eoHwtb0hwENe1oFC5zClJoSX/7DRexSJfB7YBf\n" +
-                "vn6JA2xy4C6PqximyCPisErNp85GUcZfo33Np1aywFv9H+a83rSUcV6kpE/jAZio\n" +
-                "5qLpgIOisArj1HTM6goDWzKhLiR/AeG3IJvgbpr9Gr7uZmfFyQzUjvkJ9cybZRd+\n" +
-                "G8azmpBBotmKsbtbAU/I/LVk8saeXznshOVVpDRYtVnjZeAneso7\n" +
-                "-----END CERTIFICATE-----\n" +
-                "--START INTERMEDIATE CERT--\n" +
-                "-----BEGIN CERTIFICATE-----\n" +
-                "MIIFEjCCA/qgAwIBAgICEAAwDQYJKoZIhvcNAQELBQAwgawxCzAJBgNVBAYTAlVT\n" +
-                "MQswCQYDVQQIDAJOWTESMBAGA1UEBwwJQ2FuYXN0b3RhMRswGQYDVQQKDBJRWiBJ\n" +
-                "bmR1c3RyaWVzLCBMTEMxGzAZBgNVBAsMElFaIEluZHVzdHJpZXMsIExMQzEZMBcG\n" +
-                "A1UEAwwQcXppbmR1c3RyaWVzLmNvbTEnMCUGCSqGSIb3DQEJARYYc3VwcG9ydEBx\n" +
-                "emluZHVzdHJpZXMuY29tMB4XDTE1MDMwMjAwNTAxOFoXDTM1MDMwMjAwNTAxOFow\n" +
-                "gZgxCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJOWTEbMBkGA1UECgwSUVogSW5kdXN0\n" +
-                "cmllcywgTExDMRswGQYDVQQLDBJRWiBJbmR1c3RyaWVzLCBMTEMxGTAXBgNVBAMM\n" +
-                "EHF6aW5kdXN0cmllcy5jb20xJzAlBgkqhkiG9w0BCQEWGHN1cHBvcnRAcXppbmR1\n" +
-                "c3RyaWVzLmNvbTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBANTDgNLU\n" +
-                "iohl/rQoZ2bTMHVEk1mA020LYhgfWjO0+GsLlbg5SvWVFWkv4ZgffuVRXLHrwz1H\n" +
-                "YpMyo+Zh8ksJF9ssJWCwQGO5ciM6dmoryyB0VZHGY1blewdMuxieXP7Kr6XD3GRM\n" +
-                "GAhEwTxjUzI3ksuRunX4IcnRXKYkg5pjs4nLEhXtIZWDLiXPUsyUAEq1U1qdL1AH\n" +
-                "EtdK/L3zLATnhPB6ZiM+HzNG4aAPynSA38fpeeZ4R0tINMpFThwNgGUsxYKsP9kh\n" +
-                "0gxGl8YHL6ZzC7BC8FXIB/0Wteng0+XLAVto56Pyxt7BdxtNVuVNNXgkCi9tMqVX\n" +
-                "xOk3oIvODDt0UoQUZ/umUuoMuOLekYUpZVk4utCqXXlB4mVfS5/zWB6nVxFX8Io1\n" +
-                "9FOiDLTwZVtBmzmeikzb6o1QLp9F2TAvlf8+DIGDOo0DpPQUtOUyLPCh5hBaDGFE\n" +
-                "ZhE56qPCBiQIc4T2klWX/80C5NZnd/tJNxjyUyk7bjdDzhzT10CGRAsqxAnsjvMD\n" +
-                "2KcMf3oXN4PNgyfpbfq2ipxJ1u777Gpbzyf0xoKwH9FYigmqfRH2N2pEdiYawKrX\n" +
-                "6pyXzGM4cvQ5X1Yxf2x/+xdTLdVaLnZgwrdqwFYmDejGAldXlYDl3jbBHVM1v+uY\n" +
-                "5ItGTjk+3vLrxmvGy5XFVG+8fF/xaVfo5TW5AgMBAAGjUDBOMB0GA1UdDgQWBBSQ\n" +
-                "plC3hNS56l/yBYQTeEXoqXVUXDAfBgNVHSMEGDAWgBQDRcZNwPqOqQvagw9BpW0S\n" +
-                "BkOpXjAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQAJIO8SiNr9jpLQ\n" +
-                "eUsFUmbueoxyI5L+P5eV92ceVOJ2tAlBA13vzF1NWlpSlrMmQcVUE/K4D01qtr0k\n" +
-                "gDs6LUHvj2XXLpyEogitbBgipkQpwCTJVfC9bWYBwEotC7Y8mVjjEV7uXAT71GKT\n" +
-                "x8XlB9maf+BTZGgyoulA5pTYJ++7s/xX9gzSWCa+eXGcjguBtYYXaAjjAqFGRAvu\n" +
-                "pz1yrDWcA6H94HeErJKUXBakS0Jm/V33JDuVXY+aZ8EQi2kV82aZbNdXll/R6iGw\n" +
-                "2ur4rDErnHsiphBgZB71C5FD4cdfSONTsYxmPmyUb5T+KLUouxZ9B0Wh28ucc1Lp\n" +
-                "rbO7BnjW\n" +
-                "-----END CERTIFICATE-----\n");
+                    "MIIECzCCAvOgAwIBAgIGAYzI7gY2MA0GCSqGSIb3DQEBCwUAMIGiMQswCQYDVQQG\n" +
+                    "EwJVUzELMAkGA1UECAwCTlkxEjAQBgNVBAcMCUNhbmFzdG90YTEbMBkGA1UECgwS\n" +
+                    "UVogSW5kdXN0cmllcywgTExDMRswGQYDVQQLDBJRWiBJbmR1c3RyaWVzLCBMTEMx\n" +
+                    "HDAaBgkqhkiG9w0BCQEWDXN1cHBvcnRAcXouaW8xGjAYBgNVBAMMEVFaIFRyYXkg\n" +
+                    "RGVtbyBDZXJ0MB4XDTI0MDEwMTA2NDgxMVoXDTQ0MDEwMTA2NDgxMVowgaIxCzAJ\n" +
+                    "BgNVBAYTAlVTMQswCQYDVQQIDAJOWTESMBAGA1UEBwwJQ2FuYXN0b3RhMRswGQYD\n" +
+                    "VQQKDBJRWiBJbmR1c3RyaWVzLCBMTEMxGzAZBgNVBAsMElFaIEluZHVzdHJpZXMs\n" +
+                    "IExMQzEcMBoGCSqGSIb3DQEJARYNc3VwcG9ydEBxei5pbzEaMBgGA1UEAwwRUVog\n" +
+                    "VHJheSBEZW1vIENlcnQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDg\n" +
+                    "1hu1xltGCrJ9rXTVtyaFWM7ml80M3bZGaaEu6mNjcuEZpE30/nbErigpZxJ7zbvO\n" +
+                    "ZyBXbVyNZs7RIJDLfW1J9YY22IjBHqh1gum8sZzzE8JtL/4jiFnp20GwMn8QYouO\n" +
+                    "AE+4zwzbIUFzZ8ZVRpMozfEI1l5YrevryU/Q+aEPn237vSpvRfPzhYqg5UpDGq+W\n" +
+                    "LXDJ9ng/HShcT71Lez/SgWfSzF6Pp2xSOvUWmGrdOYEEBNwVWKs5LgbgBB8zjcgB\n" +
+                    "2VvxeSACxYvaW6oRc/cpX2bAxUWYtxi9TwQkYtaSST5V4lXzCuZ0o4Hm8IXdbtXf\n" +
+                    "Smi1HQ8Q7pxNNRNNfq/LAgMBAAGjRTBDMBIGA1UdEwEB/wQIMAYBAf8CAQEwDgYD\n" +
+                    "VR0PAQH/BAQDAgEGMB0GA1UdDgQWBBRZejvu15Y6iPGscwNUxCAE2a3NozANBgkq\n" +
+                    "hkiG9w0BAQsFAAOCAQEAAv55d6BsMS/wbxcdj7Pmz4bsUvqmGyBaGTIuwoTmPcpI\n" +
+                    "8he4XnT7Pywn1LgiFtX3UXeCD9VnxbMJoC6qTKscxkvprIq1LxfsmQSrHN/YkpKf\n" +
+                    "+VVZc+0rx6x95NF06XqS0UOLX1LLSQvtN6ZSC3kHJ7TLb8TtR4/5rqtpM4LDtvJn\n" +
+                    "H2A6+VPi7OlHLPt8umwhKB4j70qnpDXYXrSVskOlBgV/pyCY76J4g12/P4V8Le7X\n" +
+                    "DjTQdepEARmE2xkpfJ2dHH3eBR7a/J79ofr3nj67qL5bxPmQ6fPKMwuSpR99rRst\n" +
+                    "T4cbjWzfzAVX6WDT0Nmii1BeksCJCwEoydtwI/BZQQ==\n" +
+                    "-----END CERTIFICATE-----\n");
+            });
+
+            qz.security.setSignatureAlgorithm("SHA512");
+            qz.security.setSignaturePromise(hash => {
+                return (resolve, reject) => {
+                    var pk = KEYUTIL.getKey("-----BEGIN PRIVATE KEY-----\n" +
+                        "MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDg1hu1xltGCrJ9\n" +
+                        "rXTVtyaFWM7ml80M3bZGaaEu6mNjcuEZpE30/nbErigpZxJ7zbvOZyBXbVyNZs7R\n" +
+                        "IJDLfW1J9YY22IjBHqh1gum8sZzzE8JtL/4jiFnp20GwMn8QYouOAE+4zwzbIUFz\n" +
+                        "Z8ZVRpMozfEI1l5YrevryU/Q+aEPn237vSpvRfPzhYqg5UpDGq+WLXDJ9ng/HShc\n" +
+                        "T71Lez/SgWfSzF6Pp2xSOvUWmGrdOYEEBNwVWKs5LgbgBB8zjcgB2VvxeSACxYva\n" +
+                        "W6oRc/cpX2bAxUWYtxi9TwQkYtaSST5V4lXzCuZ0o4Hm8IXdbtXfSmi1HQ8Q7pxN\n" +
+                        "NRNNfq/LAgMBAAECggEAHqJdpS9RKWJ0vnX/WM07JOkcS8P8LtgYqVBkI1HRV/AD\n" +
+                        "pqxB6PMw+IxoMXf3R0IZaD6Z4/+lICkVXd4rjGZbGTwoXNm1DGciedxr/4dNxLr4\n" +
+                        "/0qOPIYsBhaXRSwkLvXWYPM+6YXgkQzfb5uApQbYLzOcN2GJ+lg8MGS+CNGydb+4\n" +
+                        "aIG9XBZ36gshGJ11d8os9Xfx0uZI/tqYLyUJUQCeAE0+ojThvLy7d1uB+T1lICIQ\n" +
+                        "35EqbLaFctX93mqS0Ew32RWfwux+ZG5etSH7yOel9vqasBiWYNRGu5e7Gs5tYGVN\n" +
+                        "2jCNaxyWMfBtZANhHDkT9L9RrSQ9jN5p+2I7WdDajQKBgQDzhRyF85rqlEDHv+s5\n" +
+                        "r6Ozp11U5Nsd0ztz764d3n1EQFCZxjy27bCosV8l4Nk3LeA/gJzkqpwLvihiiw5d\n" +
+                        "HunJC1oYnVcAAKUFn69Tn1JpRevIPZQPC2D3ihBkjpcgKWg89urLZV1v/kPrCUgF\n" +
+                        "Fem/ELLlxkdRc0F1bOfvghXNdwKBgQDsW+AE6pCW4c/NTq1fbEx9T9/pWx6nNRez\n" +
+                        "qGK1r6ZFDVTUUIyeFR0pFd850qunSr5bZco3JtJKyDNkTWDx6yfXMLXabWXRXxCC\n" +
+                        "OtNNHgShheq/npPdKhvJVVjzQS2gVvD3hflQy4Q6V7yuuF3NaKJl5T8vNMoq0CYy\n" +
+                        "XSnBalv1TQKBgQDaIVH7aspJdaLRSkgZ1Ap+0lv3v0fG339Ryk/c0X7DtBh+peOc\n" +
+                        "uCyV7sub0ly/4IgOWdYvveMpYZwrKQQkxZwESfbaNZXImss2zmLuNLhNNCSPqRkG\n" +
+                        "oNqq4TDt4gvc8zo74bFPn+yidM/BqP6qD4bhbsPQrlsTynnIBVV8ZLnC4QKBgQDr\n" +
+                        "NG5UVhv2mdXRiAbNW5cuaIr4zF1PpWzlsp4wERBTzPVqppGYSoiPNx3Bi9yXiLCv\n" +
+                        "gTbe9Tn1gw5pNPtRImKuKOH+8DiPRpvGDMXzCvkux3YYo7SfNOUb0FvJDkZaNcpr\n" +
+                        "dx3oAe6epGq2L+/glcxtu9jJoC3CtwobukvwFmhWTQKBgQDgwyDYPLPw6w5NcK21\n" +
+                        "IKBHo46zgtFNgmpetbZH8kpn+xrZPtXf988/aqhaxolZZeSBoKecG2Cb9FMhzABA\n" +
+                        "cJDmEvqSvPC7rbO+8CqDVFtX3dAcQ0qtp5ODIAefmBypIeuscqqZIJavLetaYeQq\n" +
+                        "gcqzoBWrZ4cyzdjG2SKrYSgb+w==\n" +
+                        "-----END PRIVATE KEY-----\n"
+                    );
+                    let sig = new KJUR.crypto.Signature({ "alg": "SHA512withRSA" }); // Use "SHA1withRSA" for QZ Tray 2.0 and older
+                    sig.init(pk);
+                    sig.updateString(hash);
+                    let hex = sig.sign();
+                    resolve(stob64(hextorstr(hex)));
+                };
             });
 
             if (!qz.websocket.isActive()) {
