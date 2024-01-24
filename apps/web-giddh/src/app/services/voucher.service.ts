24e0fc3c992538391e770b0a39cbce63b7e56456
Ravinder <ravinder@walkover.in>
2024-01-24 18:49:34 +0530

    code ref

---
+++ apps/web-giddh/src/app/services/voucher.service.ts
@@ -0,0 +1,160 @@
+import { Inject, Injectable, Optional } from "@angular/core";
+import { GiddhErrorHandler } from "./catchManager/catchmanger";
+import { HttpWrapperService } from "./http-wrapper.service";
+import { GeneralService } from "./general.service";
+import { IServiceConfigArgs, ServiceConfig } from "./service.config";
+import { Observable, map, catchError } from "rxjs";
+import { BaseResponse } from "../models/api-models/BaseResponse";
+import { InvoiceSetting } from "../models/interfaces/invoice.setting.interface";
+import { INVOICE_API } from "./apiurls/invoice.api";
+import { ProformaFilter, ProformaResponse } from "../models/api-models/proforma";
+import { PROFORMA_API } from "./apiurls/proforma.api";
+import { InvoiceReceiptFilter, ReciptResponse } from "../models/api-models/recipt";
+import { VoucherTypeEnum } from "../models/api-models/Sales";
+import { RECEIPT_API } from "./apiurls/receipt.api";
+import { PURCHASE_RECORD_DATE_OPERATION, PURCHASE_RECORD_DUE_DATE_OPERATION, PURCHASE_RECORD_GRAND_TOTAL_OPERATION, PurchaseRecordAdvanceSearch } from "../purchase/purchase-record/constants/purchase-record.interface";
+
+@Injectable()
+export class VoucherService {
+    private companyUniqueName: string;
+
+    constructor(
+        private errorHandler: GiddhErrorHandler, 
+        private http: HttpWrapperService, 
+        private generalService: GeneralService, 
+        @Optional() @Inject(ServiceConfig) private config: IServiceConfigArgs
+    ) {
+
+    }
+
+    private createQueryString(url: string, model: any): string {
+        if ((model.from)) {
+            url = url + 'from=' + model.from + '&';
+        }
+        if ((model.to)) {
+            url = url + 'to=' + model.to + '&';
+        }
+        if ((model.page)) {
+            url = url + 'page=' + model.page + '&';
+        }
+        if ((model.count)) {
+            url = url + 'count=' + model.count;
+        }
+        if ((model.type)) {
+            url = url + '&type=' + model.type;
+        }
+        if ((model.sort)) {
+            url = url + '&sort=' + model.sort;
+        }
+        if ((model.sortBy)) {
+            url = url + '&sortBy=' + model.sortBy;
+        }
+        if ((model.q)) {
+            url = url + '&q=' + model.q;
+        }
+        return url;
+    }
+
+    public getInvoiceSettings(): Observable<BaseResponse<InvoiceSetting, string>> {
+        this.companyUniqueName = this.generalService.companyUniqueName;
+        return this.http.get(this.config.apiUrl + INVOICE_API.SETTING_INVOICE?.replace(':companyUniqueName', this.companyUniqueName)).pipe(
+            map((res) => {
+                let data: BaseResponse<InvoiceSetting, string> = res;
+                return data;
+            }),
+            catchError((e) => this.errorHandler.HandleCatch<InvoiceSetting, string>(e)));
+    }
+
+    public getAllProformaEstimate(request: ProformaFilter, voucherType: string): Observable<BaseResponse<ProformaResponse, ProformaFilter>> {
+        this.companyUniqueName = this.generalService.companyUniqueName;
+        let url = this.generalService.createQueryString(this.config.apiUrl + PROFORMA_API.getAll, {
+            page: request.page, count: request.count, from: request.from, to: request.to, q: request.q, sort: request.sort, sortBy: request.sortBy
+        });
+
+        return this.http.post(url
+            ?.replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName))
+            ?.replace(':vouchers', voucherType), request)
+            .pipe(
+                map((res) => {
+                    let data: BaseResponse<ProformaResponse, ProformaFilter> = res;
+                    data.queryString = { page: request.page, count: request.count, from: request.from, to: request.to, type: 'pdf' };
+                    data.request = request;
+                    return data;
+                }),
+                catchError((e) => this.errorHandler.HandleCatch<ProformaResponse, ProformaFilter>(e, request, { page: request.page, count: request.count, from: request.from, to: request.to, type: 'pdf' })));
+    }
+
+    public getAllVouchers(body: InvoiceReceiptFilter, type: string): Observable<BaseResponse<ReciptResponse, InvoiceReceiptFilter>> {
+        this.companyUniqueName = this.generalService.companyUniqueName;
+        const requestPayload = (type === VoucherTypeEnum.purchase && this.generalService.voucherApiVersion !== 2) ? this.getPurchaseRecordPayload(body) : body;
+        const contextPath = (type === VoucherTypeEnum.purchase && this.generalService.voucherApiVersion !== 2) ? RECEIPT_API.GET_ALL_PURCHASE_RECORDS : RECEIPT_API.GET_ALL;
+        const requestParameter = {
+            page: body?.page, count: body?.count, from: body?.from, to: body?.to, q: (body?.q) ? encodeURIComponent(body?.q) : body?.q, sort: body?.sort, sortBy: body?.sortBy
+        };
+
+        if (this.generalService.voucherApiVersion === 2) {
+            delete body.from;
+            delete body.to;
+        }
+
+        let url = this.createQueryString(this.config.apiUrl + contextPath, (type === VoucherTypeEnum.purchase && this.generalService.voucherApiVersion !== 2) ? requestParameter : { ...requestParameter, type });
+        if (this.generalService.voucherApiVersion === 2) {
+            url = this.generalService.addVoucherVersion(url, this.generalService.voucherApiVersion);
+        }
+        return this.http.post(url
+            ?.replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName)), requestPayload).pipe(
+                map((res) => {
+                    let data: BaseResponse<ReciptResponse, InvoiceReceiptFilter> = res;
+                    data.queryString = { page: body?.page, count: body?.count, from: body?.from, to: body?.to, type: 'pdf' };
+                    data.request = body;
+                    return data;
+                }),
+                catchError((e) => this.errorHandler.HandleCatch<ReciptResponse, InvoiceReceiptFilter>(e, body, { page: body?.page, count: body?.count, from: body?.from, to: body?.to, type: 'pdf' })));
+    }
+
+    /**
+     * Mapper to map advance search request for Purchase Record
+     *
+     * @private
+     * @param {InvoiceReceiptFilter} request Request to be mapped
+     * @returns {PurchaseRecordAdvanceSearch} Request payload for purchase record advance search
+     * @memberof ReceiptService
+     */
+    private getPurchaseRecordPayload(request: InvoiceReceiptFilter): PurchaseRecordAdvanceSearch {
+        let advanceSearchRequest: PurchaseRecordAdvanceSearch = new PurchaseRecordAdvanceSearch();
+        advanceSearchRequest.purchaseDate = request.invoiceDate;
+        if (request.invoiceDateEqual) {
+            advanceSearchRequest.purchaseDateOperation = PURCHASE_RECORD_DATE_OPERATION.ON;
+        } else if (request.invoiceDateAfter) {
+            advanceSearchRequest.purchaseDateOperation = PURCHASE_RECORD_DATE_OPERATION.AFTER;
+        } else if (request.invoiceDateBefore) {
+            advanceSearchRequest.purchaseDateOperation = PURCHASE_RECORD_DATE_OPERATION.BEFORE;
+        }
+        advanceSearchRequest.dueDate = request.dueDate;
+        if (request.dueDateEqual) {
+            advanceSearchRequest.dueDateOperation = PURCHASE_RECORD_DUE_DATE_OPERATION.ON;
+        } else if (request.dueDateAfter) {
+            advanceSearchRequest.dueDateOperation = PURCHASE_RECORD_DUE_DATE_OPERATION.AFTER;
+        } else if (request.dueDateBefore) {
+            advanceSearchRequest.dueDateOperation = PURCHASE_RECORD_DUE_DATE_OPERATION.BEFORE;
+        }
+        advanceSearchRequest.grandTotal = request.total;
+        if (request.totalEqual && request.totalMoreThan) {
+            advanceSearchRequest.grandTotalOperation = PURCHASE_RECORD_GRAND_TOTAL_OPERATION.GREATER_THAN_OR_EQUALS;
+        } else if (request.totalEqual && request.totalLessThan) {
+            advanceSearchRequest.grandTotalOperation = PURCHASE_RECORD_GRAND_TOTAL_OPERATION.LESS_THAN_OR_EQUALS;
+        } else if (request.totalEqual) {
+            advanceSearchRequest.grandTotalOperation = PURCHASE_RECORD_GRAND_TOTAL_OPERATION.EQUALS;
+        } else if (request.totalMoreThan) {
+            advanceSearchRequest.grandTotalOperation = PURCHASE_RECORD_GRAND_TOTAL_OPERATION.GREATER_THAN;
+        } else if (request.totalLessThan) {
+            advanceSearchRequest.grandTotalOperation = PURCHASE_RECORD_GRAND_TOTAL_OPERATION.LESS_THAN;
+        }
+
+        if (request.purchaseOrderNumber) {
+            advanceSearchRequest.purchaseOrderNumber = request.purchaseOrderNumber;
+        }
+
+        return advanceSearchRequest;
+    }
+}
--- apps/web-giddh/src/app/vouchers/create/create.component.ts
+++ apps/web-giddh/src/app/vouchers/create/create.component.ts
@@ -3,10 +3,18 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { VoucherComponentStore } from "../vouchers.store";
 import { AppState } from "../../store";
 import { Store } from "@ngrx/store";
-import { Observable } from "rxjs";
-import { InvoiceActions } from "../../actions/invoice/invoice.actions";
+import { Observable, ReplaySubject, delay, takeUntil } from "rxjs";
 import { GeneralService } from "../../services/general.service";
 import { VoucherTypeEnum } from "../../models/api-models/Sales";
+import { VouchersUtilityService } from "../vouchers.utility.service";
+import { OnboardingFormRequest } from "../../models/api-models/Common";
+import { CommonActions } from "../../actions/common.actions";
+import { CompanyActions } from "../../actions/company.actions";
+import { TaxResponse } from "../../models/api-models/Company";
+import { WarehouseActions } from "../../settings/warehouse/action/warehouse.action";
+import { SettingsUtilityService } from "../../settings/services/settings-utility.service";
+import { SettingsBranchActions } from "../../actions/settings/branch/settings.branch.action";
+import { OrganizationType } from "../../models/user-login-state";
 
 @Component({
     selector: "create",
@@ -18,9 +26,27 @@ export class VoucherCreateComponent implements OnInit, OnDestroy {
     /** Holds current voucher type */
     public voucherType: string = "";
     /** Holds images folder path */
-    public imgPath: string = "";
+    public imgPath: string = isElectron ? "assets/images/" : AppUrl + APP_FOLDER + "assets/images/";
+    /** Loading Observable */
+    public isLoading$: Observable<any> = this.componentStore.isLoading$;
     /** Invoice settings Observable */
     public invoiceSettings$: Observable<any> = this.componentStore.invoiceSettings$;
+    /** Discounts list Observable */
+    public discountsList$: Observable<any> = this.componentStore.discountsList$;
+    /** Company profile Observable */
+    public companyProfile$: Observable<any> = this.componentStore.companyProfile$;
+    /** Active company Observable */
+    public activeCompany$: Observable<any> = this.componentStore.activeCompany$;
+    /** Onboarding form Observable */
+    public onboardingForm$: Observable<any> = this.componentStore.onboardingForm$;
+    /** Company taxes Observable */
+    public companyTaxes$: Observable<any> = this.componentStore.companyTaxes$;
+    /** Warehouses Observable */
+    public warehouseList$: Observable<any> = this.componentStore.warehouseList$;
+    /** Branches Observable */
+    public branchList$: Observable<any> = this.componentStore.branchList$;
+    /** Holds boolean of TCS/TDS Applicable Observable */
+    public isTcsTdsApplicable$: Observable<any> = this.componentStore.isTcsTdsApplicable$;
     public dummyOptions: any[] = [
         { label: "Option 1", value: 1 },
         { label: "Option 2", value: 2 },
@@ -33,28 +59,95 @@ export class VoucherCreateComponent implements OnInit, OnDestroy {
     public invoiceSettings: any;
     /** True if round off will be applicable */
     public applyRoundOff: boolean = true;
+    /** Holds company specific data */
+    public company: any = {
+        countryName: '',
+        countryCode: '',
+        baseCurrency: '',
+        baseCurrencySymbol: '',
+        inputMaskFormat: '',
+        taxType: '',
+        isTcsTdsApplicable: false,
+        isActive: false,
+        branch: null
+    };
+    /** Holds account specific data */
+    public account: any = {
+        countryName: '',
+        countryCode: '',
+        baseCurrency: '',
+        baseCurrencySymbol: ''
+    };
+    /** Invoice Settings */
+    public activeCompany: any;
+    /** This will hold onboarding api form request */
+    public onboardingFormRequest: OnboardingFormRequest = { formName: '', country: '' };
+    /** Onboarding form fields */
+    public formFields: any[] = [];
+    /** Holds company tax list  */
+    public companyTaxes: TaxResponse[] = [];
+    /** Holds company discounts */
+    public discountsList: any[] = [];
+    /** Holds company warehouses */
+    public warehouses: Array<any>;
+    /** Holds company branches */
+    public branches: Array<any>;
+    /** Observable to unsubscribe all the store listeners to avoid memory leaks */
+    private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
     constructor(
         private activatedRoute: ActivatedRoute,
         private router: Router,
         private componentStore: VoucherComponentStore,
         private store: Store<AppState>,
-        private invoiceActions: InvoiceActions,
-        private generalService: GeneralService
+        private generalService: GeneralService,
+        private vouchersUtilityService: VouchersUtilityService,
+        private commonActions: CommonActions,
+        private companyActions: CompanyActions,
+        private warehouseActions: WarehouseActions,
+        private settingsUtilityService: SettingsUtilityService,
+        private settingsBranchAction: SettingsBranchActions
     ) {
-        this.imgPath = isElectron ? "assets/images/" : AppUrl + APP_FOLDER + "assets/images/";
+        
     }
 
     public ngOnInit(): void {
-        this.voucherApiVersion = this.generalService.voucherApiVersion;
-
+        this.getVoucherType();
+        this.getVoucherVersion();
+        this.getIsTcsTdsApplicable();
+        this.getActiveCompany();
         this.getInvoiceSettings();
+        this.getDiscountsList();
+        this.getCompanyProfile();
+        this.getOnboardingFormData();
+        this.getCompanyTaxes();
+        this.getWarehouses();
+        this.getCompanyBranches();
+    }
+
+    private getVoucherType(): void {
+        this.activatedRoute.params.pipe(delay(0), takeUntil(this.destroyed$)).subscribe(params => {
+            if (params) {
+                this.voucherType = params.voucherType;
+                console.log(params);
+            }
+        });
+    }
+
+    private getVoucherVersion(): void {
+        this.voucherApiVersion = this.generalService.voucherApiVersion;
+    }
+
+    private getIsTcsTdsApplicable(): void {
+        this.isTcsTdsApplicable$.subscribe(response => {
+            this.company.isTcsTdsApplicable = response;
+        });
     }
 
     private getInvoiceSettings(): void {
         this.invoiceSettings$.subscribe(settings => {
             if (!settings) {
-                this.store.dispatch(this.invoiceActions.getInvoiceSetting());
+                this.componentStore.getInvoiceSettings();
             } else {
                 this.invoiceSettings = settings;
 
@@ -77,12 +170,116 @@ export class VoucherCreateComponent implements OnInit, OnDestroy {
         });
     }
 
-    public selectDropdown(event:any): void{
+    private getDiscountsList(): void {
+        this.discountsList$.subscribe(discountsList => {
+            if (!discountsList) {
+                this.componentStore.getDiscountsList();
+            } else {
+                this.discountsList = discountsList;
+            }
+        });
+    }
+
+    private getCompanyProfile(): void {
+        this.companyProfile$.subscribe(profile => {
+            if (profile && Object.keys(profile).length) {
+                this.company.countryName = profile.country;
+                this.company.countryCode = profile.countryCode || profile.countryV2.alpha2CountryCode;
+                this.company.baseCurrency = profile.baseCurrency;
+                this.company.baseCurrencySymbol = profile.baseCurrencySymbol;
+                this.company.inputMaskFormat = profile.balanceDisplayFormat?.toLowerCase() || '';
+
+                this.showTaxTypeByCountry(this.company.countryCode);
+            }
+        });
+    }
+
+    private getActiveCompany(): void {
+        this.activeCompany$.subscribe(activeCompany => {
+            this.activeCompany = activeCompany;
+        })
+    }
+
+    private showTaxTypeByCountry(countryCode: string): void {
+        this.company.taxType = this.vouchersUtilityService.showTaxTypeByCountry(countryCode, this.activeCompany?.countryV2?.alpha2CountryCode);
+        if (this.company.taxType) {
+            this.getOnboardingForm(countryCode);
+        }
+    }
+
+    private getOnboardingForm(countryCode: string): void {
+        if (this.onboardingFormRequest.country !== countryCode) {
+            this.onboardingFormRequest.formName = 'onboarding';
+            this.onboardingFormRequest.country = countryCode;
+            this.store.dispatch(this.commonActions.GetOnboardingForm(this.onboardingFormRequest));
+        }
+    }
+
+    private getOnboardingFormData(): void {
+        this.onboardingForm$.subscribe(response => {
+            if (response) {
+                this.formFields = [];
+                Object.keys(response.fields).forEach(key => {
+                    if (response?.fields[key]) {
+                        this.formFields[response.fields[key]?.name] = [];
+                        this.formFields[response.fields[key]?.name] = response.fields[key];
+                    }
+                });
+            }
+        })
+    }
+
+    private getCompanyTaxes(): void {
+        this.companyTaxes$.subscribe(response => {
+            if (!response) {
+                this.store.dispatch(this.companyActions.getTax());
+            } else {
+                this.companyTaxes = response;
+            }
+        });
+    }
+
+    private getWarehouses(): void {
+        this.warehouseList$.subscribe(response => {
+            if (!response) {
+                this.store.dispatch(this.warehouseActions.fetchAllWarehouses({ page: 1, count: 0 }));
+            } else {
+                let warehouseResults = response.results?.filter(wh => !wh.isArchived);
+                const warehouseData = this.settingsUtilityService.getFormattedWarehouseData(warehouseResults);
+                this.warehouses = warehouseData.formattedWarehouses;
+            }
+        });
+    }
+
+    private getCompanyBranches(): void {
+        this.branchList$.subscribe(response => {
+            if (!response) {
+                this.store.dispatch(this.settingsBranchAction.GetALLBranches({ from: '', to: '' }));
+            } else {
+                this.branches = response;
+                this.company.isActive = this.generalService.currentOrganizationType !== OrganizationType.Branch && this.branches?.length > 1;
+
+                if (this.generalService.currentOrganizationType === OrganizationType.Branch) {
+                    // Find the current checked out branch
+                    this.company.branch = response.find(branch => branch?.uniqueName === this.generalService.currentBranchUniqueName);
+                } else {
+                    // Find the HO branch
+                    this.company.branch = response.find(branch => !branch.parentBranch);
+                }
+            }
+        });
+    }
+
+    public getPreviousVouchers(): void {
+        
+    }
+
+    public selectDropdown(event: any): void {
 
     }
 
     public ngOnDestroy(): void {
-
+        this.destroyed$.next(true);
+        this.destroyed$.complete();
     }
-
 }
