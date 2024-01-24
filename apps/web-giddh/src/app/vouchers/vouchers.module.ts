039d102f2bf86e86e0a0d445bc1c641b71ab9e4e
nisha <nisha@walkover.in>
2024-01-24 18:27:52 +0530

    voucher invoic page design

--- apps/web-giddh/src/app/vouchers/vouchers.module.ts
+++ apps/web-giddh/src/app/vouchers/vouchers.module.ts
@@ -7,11 +7,28 @@ import { VouchersRoutingModule } from "./vouchers.routing.module";
 import { VoucherListComponent } from "./list/list.component";
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatTableModule } from "@angular/material/table";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { FormFieldsModule } from "../theme/form-fields/form-fields.module";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDialogModule } from "@angular/material/dialog";
+import { AdvanceSearchComponent } from "./advance-search/advance-search.component";
+import { GiddhDatepickerModule } from "../theme/giddh-datepicker/giddh-datepicker.module";
+import { BulkExportComponent } from "./bulk-export/bulk-export.component";
+import { MatSelectModule } from "@angular/material/select";
+import { PaymentDialogComponent } from "./payment-dialog/payment-dialog.component";
+import { AdjustPaymentDialogComponent } from "./adjust-payment-dialog/adjust-payment-dialog.component";
 
 @NgModule({
     declarations: [
         MainComponent,
-        VoucherListComponent
+        VoucherListComponent,
+        AdvanceSearchComponent,
+        BulkExportComponent,
+        PaymentDialogComponent,
+        AdjustPaymentDialogComponent
     ],
     imports: [
         CommonModule,
@@ -19,7 +36,16 @@ import { MatTableModule } from "@angular/material/table";
         FormsModule,
         VouchersRoutingModule,
         MatTabsModule,
-        MatTableModule
+        MatTableModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatInputModule,
+        FormFieldsModule,
+        MatTooltipModule,
+        MatCheckboxModule,
+        MatDialogModule,
+        GiddhDatepickerModule,
+        MatSelectModule
     ],
     exports: [
         
