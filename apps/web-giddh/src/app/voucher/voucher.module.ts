00f18e99704f50952ce52f91f3acf7b9fdd7792e
Ravinder <ravinder@walkover.in>
2024-02-13 13:21:12 +0530

    conflict resolved

--- apps/web-giddh/src/app/voucher/voucher.module.ts
+++ apps/web-giddh/src/app/voucher/voucher.module.ts
@@ -39,6 +39,7 @@ import { VoucherComponent } from "./voucher.component";
 import { VoucherRoutingModule } from "./voucher.routing.module";
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 import { WatchVideoModule } from "../theme/watch-video/watch-video.module";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
     imports: [
@@ -83,7 +84,8 @@ import { WatchVideoModule } from "../theme/watch-video/watch-video.module";
         MatRadioModule,
         FormFieldsModule,
         NgxMatSelectSearchModule,
-        WatchVideoModule
+        WatchVideoModule,
+        MatCardModule
     ],
     exports: [VoucherComponent],
     declarations: [
