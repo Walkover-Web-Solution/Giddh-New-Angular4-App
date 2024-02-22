e27f7c94769275e2251905213dc74f82fea871cb
Dilpreet Singh Dang <dilpreet@whozzat.com>
2024-02-22 17:36:47 +0530

    conflict resolved

--- apps/web-giddh/src/app/voucher/directive/onload.directive.ts
+++ apps/web-giddh/src/app/voucher/directive/onload.directive.ts
@@ -1,18 +1,22 @@
-import { Directive, EventEmitter, OnInit, Output } from "@angular/core";
+import { Directive, ElementRef, EventEmitter, OnInit, Output } from "@angular/core";
 
 @Directive({
     selector: "[onLoad]"
 })
 export class OnloadDirective implements OnInit {
     /** This will emit the blank event */
-    @Output() public elementLoaded: EventEmitter<void> = new EventEmitter();
+    @Output('elementLoaded') public elementLoaded: EventEmitter<any> = new EventEmitter();
 
+
+    constructor(private el: ElementRef) {
+    }
+    
     /**
      * Hook cycle for component initilization
      *
      * @memberof OnloadDirective
      */
     public ngOnInit(): void {
-        this.elementLoaded.emit();
+        this.elementLoaded.emit(this.el);
     }
 }
