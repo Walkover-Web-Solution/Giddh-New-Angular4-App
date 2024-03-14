164e4a021c2c3a4c262c7c79f4e2abdc223e4a49
nisha <nisha@walkover.in>
2024-03-14 13:14:10 +0530

    subscription buy now table in material

--- apps/web-giddh/src/app/subscription/buy-plan/buy-plan.component.ts
+++ apps/web-giddh/src/app/subscription/buy-plan/buy-plan.component.ts
@@ -3,11 +3,27 @@ import { FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivateDialogComponent } from '../activate-dialog/activate-dialog.component';
 
+export interface PeriodicElement {
+  name: string;
+  premium: string;
+  popular: number;
+  starter: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: '1', premium: 'Hydrogen', popular: 1.0079, starter: 'H'},
+  {name: '1', premium: 'Hydrogen', popular: 1.0079, starter: 'H'},
+  {name: '1', premium: 'Hydrogen', popular: 1.0079, starter: 'H'},
+  {name: '1', premium: 'Hydrogen', popular: 1.0079, starter: 'H'},
+  {name: '1', premium: 'Hydrogen', popular: 1.0079, starter: 'H'},
+  {name: '1', premium: 'Hydrogen', popular: 1.0079, starter: 'H'},
+];
 @Component({
   selector: 'app-buy-plan',
   templateUrl: './buy-plan.component.html',
   styleUrls: ['./buy-plan.component.scss']
 })
+
 export class BuyPlanComponent implements OnInit {
   firstStepForm: FormGroup;
   secondStepForm: FormGroup;
@@ -18,6 +34,16 @@ export class BuyPlanComponent implements OnInit {
   constructor(
     public dialog: MatDialog
   ) { }
+  displayedColumns = [
+    'name',
+    'premium',
+    'popular',
+    'starter',
+    'premium',
+    'popular',
+    'starter',
+  ];
+  dataSource = ELEMENT_DATA;
 
   ngOnInit() {
   }
