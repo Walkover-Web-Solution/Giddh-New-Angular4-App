c1b9a10b6122f9c88571cb5d056731df32a11c80
Ravinder <ravinder@walkover.in>
2024-04-16 14:25:49 +0530

    code ref

--- apps/web-giddh/src/app/theme/form-fields/reactive-dropdown-field/reactive-dropdown-field.component.ts
+++ apps/web-giddh/src/app/theme/form-fields/reactive-dropdown-field/reactive-dropdown-field.component.ts
@@ -212,7 +212,6 @@ export class ReactiveDropdownFieldComponent implements ControlValueAccessor, OnI
             this.value = value;
         } else {
             this.value = '';
-            this.searchFormControl.next('');
         }
         this.onChange(value);
     }
