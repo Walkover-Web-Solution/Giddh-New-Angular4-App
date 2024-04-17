6bf05744914524d739d98f792ccdfd8ac9083ebc
Dilpreet Singh Dang <dilpreet@whozzat.com>
2024-04-17 19:49:36 +0530

    change doc

--- apps/web-giddh/src/app/services/general.service.ts
+++ apps/web-giddh/src/app/services/general.service.ts
@@ -1712,7 +1712,7 @@ export class GeneralService {
     }
 
     /**
-     * This will return the system current utc time zone
+     * This will return the system current user time zone
      *
      * @return {*}
      * @memberof GeneralService
@@ -1723,7 +1723,7 @@ export class GeneralService {
     }
 
     /**
-     *This will be return governmenr client time zone header
+     *This will be return government client time zone header
      *
      * @param {*} timezone
      * @return {*}  {*}
