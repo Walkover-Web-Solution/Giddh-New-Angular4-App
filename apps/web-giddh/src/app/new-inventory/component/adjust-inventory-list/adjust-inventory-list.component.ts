5037063eb1f402dd6b2db53caa849d8fcd552c54
Dilpreet Singh Dang <dilpreet@whozzat.com>
2024-07-05 19:36:44 +0530

    working on get all inventory searching

--- apps/web-giddh/src/app/new-inventory/component/adjust-inventory-list/adjust-inventory-list.component.ts
+++ apps/web-giddh/src/app/new-inventory/component/adjust-inventory-list/adjust-inventory-list.component.ts
@@ -5,7 +5,7 @@ import { API_COUNT_LIMIT, GIDDH_DATE_RANGE_PICKER_RANGES } from '../../../app.co
 import * as dayjs from 'dayjs';
 import { GIDDH_DATE_FORMAT, GIDDH_NEW_DATE_FORMAT_UI } from '../../../shared/helpers/defaultDateFormat';
 import { AdjustmentInventoryListComponentStore } from './utility/adjust-inventory-list.store';
-import { ReplaySubject, takeUntil } from 'rxjs';
+import { debounceTime, distinctUntilChanged, ReplaySubject, takeUntil } from 'rxjs';
 import { AdjustmentInventoryListResponse, InventorytAdjustmentReportQueryRequest } from '../../../models/api-models/Inventory';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -30,7 +30,7 @@ export class AdjustInventoryLisComponent implements OnInit {
     /** Observable to unsubscribe all the store listeners to avoid memory leaks */
     private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
     /** This will use for table heading */
-    public displayedColumns: string[] = ['referenceNo', 'name', 'reason', 'status', 'ADJUSTED_BY', 'TYPE', 'DATE'];
+    public displayedColumns: string[] = ['referenceNo', 'name', 'reason', 'status', 'ADJUSTED_BY', 'TYPE', 'DATE','action'];
     /** Hold the data of subscriptions */
     public dataSource: any;
     /** True if translations loaded */
@@ -76,15 +76,15 @@ export class AdjustInventoryLisComponent implements OnInit {
     /** True, if custom date filter is selected or custom searching or sorting is performed */
     public showClearFilter: boolean = false;
     /* True if show Reason show */
-    public reason: boolean = false;
+    public showReason: boolean = false;
     /* True if show Reference No show */
-    public referenceNo: boolean = false;
+    public showReferenceNo: boolean = false;
     /* True if show Adjusted by show */
-    public adjustedBy: boolean = false;
+    public showAdjustedBy: boolean = false;
     /* True if show Status by show */
-    public status: boolean = false;
+    public showStatus: boolean = false;
     /* True if show Stock/Group Name show */
-    public name: boolean = false;
+    public showName: boolean = false;
     public inventoryType: 'stock' | 'group';
     public showData: boolean = true;
 
@@ -142,6 +142,90 @@ export class AdjustInventoryLisComponent implements OnInit {
                 this.adjustInventoryListRequest.totalItems = 0;
             }
         });
+
+        this.adjustInventoryListForm?.controls['referenceNo'].valueChanges.pipe(
+            debounceTime(700),
+            distinctUntilChanged(),
+            takeUntil(this.destroyed$),
+        ).subscribe(searchedText => {
+            if (searchedText !== null && searchedText !== undefined) {
+                this.showClearFilter = true;
+                this.adjustInventoryListRequest.q = searchedText;
+                this.adjustInventoryListRequest.searchBy ='refNo' ;
+                this.getAllAdjusmentReports(true);
+            }
+            if (searchedText === null || searchedText === "") {
+                this.showClearFilter = false;
+                this.showReferenceNo = false;
+            }
+        });
+        this.adjustInventoryListForm?.controls['name'].valueChanges.pipe(
+            debounceTime(700),
+            distinctUntilChanged(),
+            takeUntil(this.destroyed$),
+        ).subscribe(searchedText => {
+            if (searchedText !== null && searchedText !== undefined) {
+                this.showClearFilter = true;
+                this.adjustInventoryListRequest.q = searchedText;
+                this.adjustInventoryListRequest.searchBy = 'entityName';
+                this.getAllAdjusmentReports(true);
+            }
+            if (searchedText === null || searchedText === "") {
+                this.showClearFilter = false;
+                this.showName = false;
+            }
+        });
+        this.adjustInventoryListForm?.controls['reason'].valueChanges.pipe(
+            debounceTime(700),
+            distinctUntilChanged(),
+            takeUntil(this.destroyed$),
+        ).subscribe(searchedText => {
+            if (searchedText !== null && searchedText !== undefined) {
+                this.showClearFilter = true;
+                this.adjustInventoryListRequest.q = searchedText;
+                this.adjustInventoryListRequest.searchBy = 'reason';
+                this.getAllAdjusmentReports(true);
+            }
+            if (searchedText === null || searchedText === "") {
+                this.showClearFilter = false;
+                this.showReason = false;
+            }
+        });
+
+        this.adjustInventoryListForm?.controls['status'].valueChanges.pipe(
+            debounceTime(700),
+            distinctUntilChanged(),
+            takeUntil(this.destroyed$),
+        ).subscribe(searchedText => {
+            if (searchedText !== null && searchedText !== undefined) {
+                this.showClearFilter = true;
+                this.adjustInventoryListRequest.q = searchedText;
+                this.adjustInventoryListRequest.searchBy = 'requestStatus';
+                this.getAllAdjusmentReports(true);
+            }
+            if (searchedText === null || searchedText === "") {
+                this.showClearFilter = false;
+                this.showStatus = false;
+            }
+        });
+
+    }
+
+    /**
+ * Clears the filters and resets the form in the SubscriptionComponent.
+ *
+ * @memberof SubscriptionComponent
+ */
+    public clearFilter(): void {
+        this.showClearFilter = false;
+        this.showName = false;
+        this.showReason = false;
+        this.showReferenceNo = false;
+        this.showAdjustedBy = false;
+        this.showStatus = false;
+        this.adjustInventoryListForm.reset();
+        this.inlineSearch = '';
+        this.getAllAdjusmentReports(true);
     }
 
     /**
@@ -269,6 +353,19 @@ export class AdjustInventoryLisComponent implements OnInit {
         return searchField;
     }
 
+/**
+ * Handle page change
+ *
+ * @param {*} event
+ * @memberof SubscriptionComponent
+ */
+    public handlePageChange(event: any): void {
+        this.pageIndex = event.pageIndex;
+        this.adjustInventoryListRequest.count = event.pageSize;
+        this.adjustInventoryListRequest.page = event.pageIndex + 1;
+        this.getAllAdjusmentReports(false);
+    }
+
     /**
      * Handles clicks outside the specified element for filtering in the SubscriptionComponent.
      *
@@ -278,20 +375,20 @@ export class AdjustInventoryLisComponent implements OnInit {
      * @memberof SubscriptionComponent
      */
     public handleClickOutside(event: any, element: any, searchedFieldName: string): void {
-        console.log(event, element, searchedFieldName);
-        if (searchedFieldName === 'name') {
+        console.log('handleClickOutside',searchedFieldName);
+        if (searchedFieldName === 'Stock Name') {
             if (this.adjustInventoryListForm?.controls['name'].value !== null && this.adjustInventoryListForm?.controls['name'].value !== '') {
                 return;
             }
-        } else if (searchedFieldName === 'reason') {
+        } else if (searchedFieldName === 'Reason') {
             if (this.adjustInventoryListForm?.controls['reason'].value !== null && this.adjustInventoryListForm?.controls['reason'].value !== '') {
                 return;
             }
-        } else if (searchedFieldName === 'status') {
+        } else if (searchedFieldName === 'Request Status') {
             if (this.adjustInventoryListForm?.controls['status'].value !== null && this.adjustInventoryListForm?.controls['status'].value !== '') {
                 return;
             }
-        } else if (searchedFieldName === 'referenceNo') {
+        } else if (searchedFieldName === 'Reference No') {
             if (this.adjustInventoryListForm?.controls['referenceNo'].value !== null && this.adjustInventoryListForm?.controls['referenceNo'].value !== '') {
                 return;
             }
@@ -300,16 +397,14 @@ export class AdjustInventoryLisComponent implements OnInit {
         if (this.generalService.childOf(event?.target, element)) {
             return;
         } else {
-            if (searchedFieldName === 'reason') {
-                this.reason = false;
-            } else if (searchedFieldName === 'referenceNo') {
-                this.referenceNo = false;
-            } else if (searchedFieldName === 'adjustedBy') {
-                this.adjustedBy = false;
-            } else if (searchedFieldName === 'name') {
-                this.name = false;
-            } else if (searchedFieldName === 'status') {
-                this.status = false;
+            if (searchedFieldName === 'Reason') {
+                this.showReason = false;
+            } else if (searchedFieldName === 'Reference No') {
+                this.showReferenceNo = false;
+            } else if (searchedFieldName === 'Stock Name') {
+                this.showName = false;
+            } else if (searchedFieldName === 'Request Status') {
+                this.showStatus = false;
             }
         }
     }
@@ -322,22 +417,28 @@ export class AdjustInventoryLisComponent implements OnInit {
      * @memberof SubscriptionComponent
      */
     public toggleSearch(fieldName: string): void {
-        console.log(fieldName);
-        if (fieldName === 'reason') {
-            this.reason = true;
+        console.log('toggleSearch', fieldName);
+
+        if (fieldName === 'Reason') {
+            this.showReason = true;
         }
-        if (fieldName === 'referenceNo') {
-            this.referenceNo = true;
+        if (fieldName === 'Reference No') {
+            this.showReferenceNo = true;
         }
-        if (fieldName === 'adjustedBy') {
-            this.adjustedBy = true;
+        if (fieldName === 'Stock Name') {
+            this.showName = true;
         }
-        if (fieldName === 'name') {
-            this.name = true;
+        if (fieldName === 'Request Status') {
+            this.showStatus = true;
         }
-        if (fieldName === 'status') {
-            this.status = true;
-        }
+    }
+
+    public editInventoryAdjustment(row: any): void {
+
+    }
+
+    public deleteInventoryAdjustment(row: any): void {
+
     }
 
 }
