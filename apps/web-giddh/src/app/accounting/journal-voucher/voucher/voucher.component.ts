ffa8754a4df13dafa8097c1268c60d4711f92cb1
Divyanshu <divyanshushrivastava123@gmail.com>
2024-08-03 11:58:50 +0530

    remove jv decimal code

--- apps/web-giddh/src/app/accounting/journal-voucher/voucher/voucher.component.ts
+++ apps/web-giddh/src/app/accounting/journal-voucher/voucher/voucher.component.ts
@@ -803,27 +803,17 @@ export class AccountAsVoucherComponent implements OnInit, OnDestroy, AfterViewIn
      * @memberof AccountAsVoucherComponent
      */
     public updateTransactionActualAmount(transaction: FormGroup): void {
+        if (+transaction.get('amount')?.value > 9 && transaction.get('amount')?.value?.startsWith('0')) {
+            transaction.get('amount')?.patchValue(+transaction.get('amount')?.value?.replace(/^0+/, ''));
+        }
+        const amount = Number(transaction.get('amount')?.value);
+        transaction.get('amount')?.patchValue(amount);
+        transaction.get('actualAmount')?.patchValue(amount);
+        transaction.get('total')?.patchValue(amount);
+
         const totalCreditAndDebit = this.calculateTotalCreditAndDebit();
         this.totalCreditAmount = totalCreditAndDebit.totalCredit;
         this.totalDebitAmount = totalCreditAndDebit.totalDebit;
-    }
-
-    /**
-     * Update amount in number and remove 0 from starting
-     *
-     * @param {FormGroup} transaction
-     * @memberof AccountAsVoucherComponent
-     */
-    public updateValidNumber(transaction: FormGroup): void {
-        if (transaction) {
-            if (+transaction.get('amount')?.value > 9 && transaction.get('amount')?.value?.startsWith('0')) {
-                transaction.get('amount')?.patchValue(+transaction.get('amount')?.value?.replace(/^0+/, ''));
-            }
-            const amount = parseFloat(transaction.get('amount')?.value);
-            transaction.get('amount')?.patchValue(amount);
-            transaction.get('actualAmount')?.patchValue(amount);
-            transaction.get('total')?.patchValue(amount);
-        }
     }
 
     /**
@@ -1391,7 +1381,7 @@ export class AccountAsVoucherComponent implements OnInit, OnDestroy, AfterViewIn
     }
 
     /**
-     * In case of sales ( i.e To ) row update manually then 
+     * In case of sales ( i.e To ) row update manually then
      * calculate and update cash ( i.e. By ) value
      *
      * @private
