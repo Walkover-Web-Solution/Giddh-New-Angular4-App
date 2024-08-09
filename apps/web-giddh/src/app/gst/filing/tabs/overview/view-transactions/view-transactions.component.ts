d00569886f338911d281c41839eab195a8186832
Ankit Dubey <ankitdubey@whozzat.com>
2024-08-09 16:48:53 +0530

    Ankit | test | By Default, all the invoices, credit notes and debit notes should be shown in the GSTR 1 report in the respective sections

--- apps/web-giddh/src/app/gst/filing/tabs/overview/view-transactions/view-transactions.component.ts
+++ apps/web-giddh/src/app/gst/filing/tabs/overview/view-transactions/view-transactions.component.ts
@@ -101,6 +101,7 @@ export class ViewTransactionsComponent implements OnInit, OnDestroy {
 
     public ngOnInit() {
         this.gstr1entityType = [
+            { label: this.localeData?.all_transactions, value: '' },
             { label: this.commonLocaleData?.app_invoices, value: 'invoices' },
             { label: this.commonLocaleData?.app_credit_notes, value: 'credit-notes' },
             { label: this.commonLocaleData?.app_debit_notes, value: 'debit-notes' },
@@ -142,7 +143,9 @@ export class ViewTransactionsComponent implements OnInit, OnDestroy {
 
         this.activatedRoute.firstChild.queryParams.pipe(takeUntil(this.destroyed$)).subscribe(params => {
             this.selectedGstNumber = params.selectedGst;
-            this.filterParam.entityType = params.entityType;
+        //  this.filterParam.entityType = params.entityType;
+        //  give default value select all Transaction 
+            this.filterParam.entityType = '';
             this.filterParam.type = params.type;
             this.filterParam.status = params?.status;
             this.viewFilteredTxn('page', 1);
