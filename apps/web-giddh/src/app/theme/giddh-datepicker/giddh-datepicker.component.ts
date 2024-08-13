fbb5f5e0a5a0bbe0bfd831e503874c68f5df51ba
Ankit Dubey <ankitdubey@whozzat.com>
2024-08-13 18:48:20 +0530

    Ankit | test | Prod - Invalid date error is cominug when changing the entry date on ledger for imported transaction

--- apps/web-giddh/src/app/theme/giddh-datepicker/giddh-datepicker.component.ts
+++ apps/web-giddh/src/app/theme/giddh-datepicker/giddh-datepicker.component.ts
@@ -55,6 +55,8 @@ export class GiddhDatepickerComponent implements ControlValueAccessor, OnInit, O
     @Output() public focusOut: EventEmitter<any> = new EventEmitter<any>();
     /** This will hold if datepicker is open */
     public isDatepickerOpen: boolean = false;
+     /** This is used to show change date */
+     public inputChange : any ='';
     /** Internal data model */
     private innerValue: any = '';
     /** This is used to show default date */
@@ -97,13 +99,23 @@ export class GiddhDatepickerComponent implements ControlValueAccessor, OnInit, O
     }
     
     /**
+     * get current value on input 
+     * 
+     * @param event 
+     */
+    public dateInputChange(event: Event): void {
+        const inputElement = event.target as HTMLInputElement;
+        const inputValue = inputElement.value;
+        this.inputChange = inputValue;
+    } 
+    /**
      * Callback for date change
      *
      * @param {MatDatepickerInputEvent<Date>} event
      * @memberof GiddhDatepickerComponent
      */
     public dateChange(event: MatDatepickerInputEvent<Date>): void {
-        let selectedDate = (typeof(event?.value) === "object") ? dayjs(event?.value).toDate() : dayjs(event?.value, GIDDH_DATE_FORMAT).toDate();
+        let selectedDate = (typeof(event?.value) === "object" && (event?.value) !== null) ? dayjs(event?.value).toDate() : dayjs(this.inputChange, GIDDH_DATE_FORMAT).toDate();
         this.onChangeCallback(selectedDate);
         this.dateSelected.emit(selectedDate);
     }
@@ -166,8 +178,8 @@ export class GiddhDatepickerComponent implements ControlValueAccessor, OnInit, O
             this.calendarDate = (typeof(value) === "object") ? dayjs(value).toDate() : dayjs(value, GIDDH_DATE_FORMAT).toDate();
             this.changeDetectorRef.detectChanges();
         } else {
-            this.innerValue = "";
-            this.calendarDate = "";
+            this.innerValue = this.inputChange;
+            this.calendarDate = dayjs(this.inputChange , GIDDH_DATE_FORMAT).toDate();
             this.changeDetectorRef.detectChanges();
         }
     }
