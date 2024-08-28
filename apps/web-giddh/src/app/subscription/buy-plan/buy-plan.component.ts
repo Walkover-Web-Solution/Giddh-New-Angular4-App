bc2adadaae4a53db89c211813ef9acf86ea10e8f
Ankit Dubey <ankitdubey@whozzat.com>
2024-08-28 20:06:39 +0530

    remove space

--- apps/web-giddh/src/app/subscription/buy-plan/buy-plan.component.ts
+++ apps/web-giddh/src/app/subscription/buy-plan/buy-plan.component.ts
@@ -1381,8 +1381,6 @@ export class BuyPlanComponent implements OnInit, OnDestroy {
             customer_id: request.razorpayCustomerId,
             recurring: "1",
             handler: (response: any) => {
-                console.log(response);
-                console.log(request.razorpayCustomerId);
                 that.updateSubscriptionPayment(response, false, request);
             },
             theme: {
