ecd9e0c05e443472daa7fdcd89250f735ab2828f
Ankit Dubey <ankitdubey@whozzat.com>
2024-09-05 17:33:05 +0530

    Ankit | change

--- apps/web-giddh/src/app/vat-report/utility/vat.report.store.ts
+++ apps/web-giddh/src/app/vat-report/utility/vat.report.store.ts
@@ -6,15 +6,15 @@ import { VatService } from "../../services/vat.service";
 import { GstReconcileService } from "../../services/gst-reconcile.service";
 
 export interface VatReportState {
-    isLoading: boolean;
+    liabilityPaymentListInProgress: boolean;
     liabilityPaymentList: any;
     taxNumber: any;
 }
 
 const DEFAULT_STATE: VatReportState = {
-    isLoading: false,
+    liabilityPaymentListInProgress: false,
     liabilityPaymentList: null,
-    taxNumber: null,
+    taxNumber: null
 };
 
 @Injectable()
@@ -28,32 +28,28 @@ export class VatReportComponentStore extends ComponentStore<VatReportState> {
         super(DEFAULT_STATE);
     }
 
-    public isLoading$ = this.select((state) => state.isLoading);
-
     /**
-     *   save list of Payment Liability
-     *
-     * @memberof VatReportComponentStore
-     */
+    *   Save list of Payment Liability
+    *
+    * @memberof VatReportComponentStore
+    */
     readonly getLiabilityPaymentList = this.effect((data: Observable<any>) => {
         return data.pipe(
             switchMap((req) => {
-                this.patchState({ liabilityPaymentList: null, isLoading: true });
+                this.patchState({ liabilityPaymentList: null, liabilityPaymentListInProgress: true });
                 return this.vatService.getPaymentLiabilityList(req.payload, req.searchForm, req.isPaymentMode).pipe(
                     tapResponse(
                         (res: any) => {
                             if (res?.status === "success") {
-                                return this.patchState({ liabilityPaymentList: res, isLoading: false });
+                                return this.patchState({ liabilityPaymentList: res, liabilityPaymentListInProgress: false });
                             } else {
-                                if (res?.message) {
-                                    this.toaster.showSnackBar("error", res.message);
-                                }
-                                return this.patchState({ liabilityPaymentList: null, isLoading: false });
+                                res?.message && this.toaster.showSnackBar("error", res.message);
+                                return this.patchState({ liabilityPaymentList: null, liabilityPaymentListInProgress: false });
                             }
                         },
                         (error: any) => {
                             this.toaster.showSnackBar("error", error);
-                            return this.patchState({ liabilityPaymentList: null, isLoading: false });
+                            return this.patchState({ liabilityPaymentList: null, liabilityPaymentListInProgress: false });
                         }
                     ),
                     catchError((err) => EMPTY)
@@ -63,29 +59,27 @@ export class VatReportComponentStore extends ComponentStore<VatReportState> {
     });
 
     /**
-     *   get Tax Number
-     *
-     * @memberof VatReportComponentStore
-     */
+    *   Get Tax Number
+    *
+    * @memberof VatReportComponentStore
+    */
     readonly getTaxNumber = this.effect((data: Observable<void>) => {
         return data.pipe(
             switchMap(() => {
-                this.patchState({ taxNumber: null, isLoading: true });
+                this.patchState({ taxNumber: null, liabilityPaymentListInProgress: true });
                 return this.gstReconcileService.getTaxDetails().pipe(
                     tapResponse(
                         (res: any) => {
                             if (res?.status === "success") {
-                                return this.patchState({ taxNumber: res, isLoading: false });
+                                return this.patchState({ taxNumber: res, liabilityPaymentListInProgress: false });
                             } else {
-                                if (res?.message) {
-                                    this.toaster.showSnackBar("error", res.message);
-                                }
-                                return this.patchState({ taxNumber: null, isLoading: false });
+                                res?.message && this.toaster.showSnackBar("error", res.message);
+                                return this.patchState({ taxNumber: null, liabilityPaymentListInProgress: false });
                             }
                         },
                         (error: any) => {
                             this.toaster.showSnackBar("error", error);
-                            return this.patchState({ taxNumber: null, isLoading: false });
+                            return this.patchState({ taxNumber: null, liabilityPaymentListInProgress: false });
                         }
                     ),
                     catchError((err) => EMPTY)
