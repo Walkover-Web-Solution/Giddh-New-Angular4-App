176c0e1428264ba5bbaa83412d9e06408fd47232
Dilpreet Singh Dang <dilpreet@whozzat.com>
2024-09-06 15:05:02 +0530

    remove console

--- apps/web-giddh/src/app/settings/integration/setting.integration.component.ts
+++ apps/web-giddh/src/app/settings/integration/setting.integration.component.ts
@@ -381,7 +381,6 @@ export class SettingIntegrationComponent implements OnInit, AfterViewInit {
         };
         window.addEventListener('message', event => {
             if (this.router.url === '/pages/settings/integration/payment') {
-                console.log(event);
                 if (event && event.data === "GOCARDLESS") {
                     this.createEndUserAgreementSuccess$.pipe(takeUntil(this.destroyed$)).subscribe(response => {
                         if (response?.reference) {
