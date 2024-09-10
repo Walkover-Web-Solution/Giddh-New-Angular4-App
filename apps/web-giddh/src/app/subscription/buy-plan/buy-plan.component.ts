99cdb9b624c1bc7e1da680a29208f20ad3252682
Divyanshu <divyanshushrivastava123@gmail.com>
2024-09-10 17:53:21 +0530

    pr changes

--- apps/web-giddh/src/app/subscription/buy-plan/buy-plan.component.ts
+++ apps/web-giddh/src/app/subscription/buy-plan/buy-plan.component.ts
@@ -202,6 +202,8 @@ export class BuyPlanComponent implements OnInit, OnDestroy {
     public razorpaySuccess$ = this.componentStore.select((state) => state.razorpaySuccess);
     /** Hold calculation amount response*/
     public calculationResponse: any;
+    /** Hold new user selected country value */
+    public newUserSelectedCountryValue: string = '';
 
     constructor(
         public dialog: MatDialog,
@@ -1217,6 +1219,7 @@ export class BuyPlanComponent implements OnInit, OnDestroy {
         if (event?.value) {
             this.componentStore.getAllPlans({ params: { regionCode: event?.value } });
             this.newUserSelectedCountry = event.label;
+            this.newUserSelectedCountryValue = event.value;
             setTimeout(() => {
                 if (this.isSubscriptionRegion) {
                     this.currentCountry.patchValue(this.countrySource.find(country => country.label === this.newUserSelectedCountry));
