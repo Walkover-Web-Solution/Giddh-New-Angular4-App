8083831c7e290d1f3dc1edbb13a8ccfcd26e9aef
Dilpreet Singh Dang <dilpreet@whozzat.com>
2024-09-24 18:22:54 +0530

    revert url

--- apps/web-giddh/webpack.stage.partial.js
+++ apps/web-giddh/webpack.stage.partial.js
@@ -11,7 +11,7 @@ module.exports = {
             'errlyticsKey': JSON.stringify(''),
             'AppUrl': JSON.stringify('https://stage.giddh.com/'),
             'ApiUrl': JSON.stringify('https://apitest.giddh.com/'),
-            'UkApiUrl': JSON.stringify('https://routes.msg91.com/api/proxy/117230/33scfei2/'),
+            'UkApiUrl': JSON.stringify('https://gbapi.giddh.com/'),
             'PORTAL_URL': JSON.stringify('https://master.d2n1i21e52r793.amplifyapp.com/'),
             'APP_FOLDER': JSON.stringify(''),
             'PRODUCTION_ENV': JSON.stringify(false),
@@ -39,7 +39,7 @@ module.exports = {
             'process.env.errlyticsKey': JSON.stringify(''),
             'process.env.AppUrl': JSON.stringify('https://stage.giddh.com/'),
             'process.env.ApiUrl': JSON.stringify('https://apitest.giddh.com/'),
-            'process.env.UkApiUrl': JSON.stringify('https://routes.msg91.com/api/proxy/117230/33scfei2/'),
+            'process.env.UkApiUrl': JSON.stringify('https://gbapi.giddh.com/'),
             'process.PORTAL_URL': JSON.stringify('https://master.d2n1i21e52r793.amplifyapp.com/'),
             'process.env.APP_FOLDER': JSON.stringify('')
         }),
