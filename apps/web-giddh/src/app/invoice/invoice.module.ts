fe69c5996a5ae224b74366ca850243885c6079f8
Divyanshu <divyanshushrivastava123@gmail.com>
2024-10-04 15:23:17 +0530

    revert Invoice module changes

--- apps/web-giddh/src/app/invoice/invoice.module.ts
+++ apps/web-giddh/src/app/invoice/invoice.module.ts
@@ -168,6 +168,9 @@ import { FroalaTemplateEditorModule } from '../shared/template-froala/template-f
         TooltipModule,
         DownloadOrSendInvoiceOnMailComponent,
         InvoicePreviewComponent
+    ],
+    providers: [
+        InvoiceUiDataService
     ]
 })
 export class InvoiceModule {
