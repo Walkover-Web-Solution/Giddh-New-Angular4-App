e7cb827db20cdc197f142d9483abb99368a2d71f
Ankit <walkover@walkovers-iMac.local>
2024-10-09 11:30:34 +0530

    handle response

--- apps/web-giddh/src/app/vouchers/list/list.component.ts
+++ apps/web-giddh/src/app/vouchers/list/list.component.ts
@@ -622,6 +622,7 @@ export class VoucherListComponent implements OnInit, OnDestroy {
         if (response && response.voucherType === this.voucherType) {
             this.dataSource = [];
             this.totalResults = response?.totalItems;
+            this.selectAllVouchers({ checked: false });
             response.items?.forEach((item: any, index: number) => {
                 item.index = index + 1;
 
@@ -1462,7 +1463,6 @@ export class VoucherListComponent implements OnInit, OnDestroy {
         const tempKeysInAdvanceFiltersForm = ['dueAmount', 'dateRange', 'grandTotalOperation', 'invoiceTotalAmount', 'invoiceDateRange'];
         this.advanceSearchDialogRef?.close();
         this.advanceFiltersApplied = true;
-        this.selectAllVouchers({ checked: false });
         let advanceFilters = {
             sortBy: this.advanceFilters.sortBy,
             sort: this.advanceFilters.sort,
