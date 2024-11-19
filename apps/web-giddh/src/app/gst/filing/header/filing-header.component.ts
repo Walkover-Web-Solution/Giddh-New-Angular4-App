142e71fb2f3d07daf289f512bdfe210523a818d1
Dilpreet Singh Dang <dilpreet@whozzat.com>
2024-11-19 18:35:34 +0530

    Implementation according to PR issues from Ravinder sir

--- apps/web-giddh/src/app/gst/filing/header/filing-header.component.ts
+++ apps/web-giddh/src/app/gst/filing/header/filing-header.component.ts
@@ -18,6 +18,7 @@ import { GstReport } from '../../constants/gst.constant';
 import { GstReconcileService } from '../../../services/gst-reconcile.service';
 import { GeneralService } from '../../../services/general.service';
 import { saveAs } from 'file-saver';
+import { RestrictedModules } from '../../../app.constant';
 
 @Component({
     // tslint:disable-next-line:component-selector
@@ -95,6 +96,8 @@ export class FilingHeaderComponent implements OnInit, OnChanges, OnDestroy {
     public dayjs = dayjs;
     /** Active company details */
     public activeCompany: any = null;
+    /** Enum for restricted modules */
+    public restrictedModules = RestrictedModules;
 
     constructor(
         private store: Store<AppState>,
--- apps/web-giddh/src/app/shared/header/components/share-group-modal/share-group-modal.component.ts
+++ apps/web-giddh/src/app/shared/header/components/share-group-modal/share-group-modal.component.ts
@@ -12,6 +12,7 @@ import { GIDDH_EMAIL_REGEX } from '../../../helpers/defaultDateFormat';
 import { clone, cloneDeep } from 'apps/web-giddh/src/app/lodash-optimized';
 import { Router } from '@angular/router';
 import { SettingsProfileActions } from 'apps/web-giddh/src/app/actions/settings/profile/settings.profile.action';
+import { RestrictedModules } from 'apps/web-giddh/src/app/app.constant';
 
 @Component({
     selector: 'share-group-modal',
@@ -37,6 +38,8 @@ export class ShareGroupModalComponent implements OnInit, OnDestroy {
     public remainingUsers: number = 0;
     /** Active company details */
     public activeCompany: any;
+    /** Enum for restricted modules */
+    public restrictedModules = RestrictedModules;
 
 
     @Output() public closeShareGroupModal: EventEmitter<any> = new EventEmitter();
