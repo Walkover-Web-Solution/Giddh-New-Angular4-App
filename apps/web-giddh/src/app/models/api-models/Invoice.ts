a3b8e6a631466b1c5908ffe12299ab910d3c874b
Divyanshu <divyanshushrivastava123@gmail.com>
2024-11-20 20:04:26 +0530

    conflict resolved

--- apps/web-giddh/src/app/models/api-models/Invoice.ts
+++ apps/web-giddh/src/app/models/api-models/Invoice.ts
@@ -533,7 +533,7 @@ export class UpdateEwayVehicle {
     public reasonRem: string;
 
     public transDocNo?: string;
-    public transDocDate?: string;
+    public transDocDate?: any;
     public transMode: string;
     public vehicleType: string;
 }
