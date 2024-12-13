0e0af193e0ec7e1bcf4324441f1ce92d53cef752
Dilpreet Singh Dang <dilpreet@whozzat.com>
2024-12-13 13:07:15 +0530

    Dilpreet | Test | Branch WIse PR Issue Fixed

--- apps/web-giddh/src/app/invoice/proforma/proforma-list.component.ts
+++ apps/web-giddh/src/app/invoice/proforma/proforma-list.component.ts
@@ -234,10 +234,10 @@ export class ProformaListComponent implements OnInit, OnDestroy, OnChanges {
                 this.branches = response || [];
                 this.isCompany = this.generalService.currentOrganizationType !== OrganizationType.Branch && this.branches?.length > 1;
                 this.store.pipe(select(select => select.branchConsolidated), takeUntil(this.destroyed$)).subscribe(response => {
-            if (response) {
-                this.isConsolidatedBranch = response.isBranchConsolidated;
-            }
-        });
+                    if (response) {
+                        this.isConsolidatedBranch = response.isBranchConsolidated;
+                    }
+                });
             }
         });
 
