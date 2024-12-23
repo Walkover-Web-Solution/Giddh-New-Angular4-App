6d94147bdf22ea42304d98a2e16c2ff8c2bd6917
Divyanshu <divyanshushrivastava123@gmail.com>
2024-12-23 18:00:40 +0530

    intl function exception fix

--- apps/web-giddh/src/assets/js/intl-tel-input.js
+++ apps/web-giddh/src/assets/js/intl-tel-input.js
@@ -6,7 +6,7 @@
 
 // wrap in UMD
 (function(factory) {
-    if (typeof module === "object" && module.exports) module.exports = factory(); else window.intlTelInput = factory();
+    if (typeof module === "object" && module.exports) module.exports = factory(); else window?.intlTelInput = factory();
 })(function(undefined) {
     "use strict";
     return function() {
