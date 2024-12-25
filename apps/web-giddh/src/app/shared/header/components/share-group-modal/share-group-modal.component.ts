79d140ee4276a0351675dfaa2bec4cb06d6240d3
Ankit <walkover@walkovers-iMac.local>
2024-12-25 14:15:16 +0530

    remove null

--- apps/web-giddh/src/app/shared/header/components/share-group-modal/share-group-modal.component.ts
+++ apps/web-giddh/src/app/shared/header/components/share-group-modal/share-group-modal.component.ts
@@ -57,7 +57,7 @@ export class ShareGroupModalComponent implements OnInit, OnDestroy {
         this.activeCompany$.pipe(takeUntil(this.destroyed$)).subscribe(activeCompany => {
             if (activeCompany) {
                 this.activeCompany = activeCompany;
-                if (Object.hasOwn(activeCompany?.subscription?.planDetails?.restrictedModules, this.restrictedModules.Users) && activeCompany?.moduleRestrictionStatus) {
+                if (Object.hasOwn(activeCompany.subscription.planDetails.restrictedModules, this.restrictedModules.Users) && activeCompany.moduleRestrictionStatus) {
                     let module = activeCompany.moduleRestrictionStatus.find(
                         (module) => module?.moduleName === this.restrictedModules.Users
                     );
