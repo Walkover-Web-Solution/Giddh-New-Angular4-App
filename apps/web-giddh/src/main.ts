80456024952db96fa014d26f918280c3e86f1883
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-01-08 15:36:56 +0530

    working on load modulaes

--- apps/web-giddh/src/main.ts
+++ apps/web-giddh/src/main.ts
@@ -4,11 +4,19 @@ import { AppModule } from './app/app.module';
 
 
 import { environment } from './environments/environment';
+import { EnvironmentService } from './app/services/enviroment.service';
+// Initialize the environment service before bootstrapping the app
+const environmentService = new EnvironmentService();
+environmentService.initializeEnvironment().then(() => {
+    console.log(';');
 
-if (environment.production) {
-    enableProdMode();
-}
+    if (environment.production) {
+        enableProdMode();
+    }
 
-platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .catch(err => console.log(err));
+    platformBrowserDynamic()
+        .bootstrapModule(AppModule)
+        .catch(err => console.log(err));
+}).catch(err => {
+    console.error('Environment initialization failed:', err);
+});
