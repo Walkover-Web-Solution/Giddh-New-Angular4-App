7bba758a38300dc3c83283c942fc2db404831749
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-01-22 11:30:09 +0530

    add POST BUILD code

--- tools/web/postbuild.js
+++ tools/web/postbuild.js
@@ -23,9 +23,19 @@ for (let i = 0; i < process.argv.length; i++) {
     }
 }
 
+if (!rootDirectiory) {
+    console.error('Error: Missing --path argument. Please provide the dist folder path.');
+    process.exit(1);
+}
+
 const versionFilePath = path.join(__dirname, rootDirectiory, 'version.json');
 const indexFilePath = path.join(__dirname, rootDirectiory, 'index.html');
 const newIndexFilePath = path.join(__dirname, rootDirectiory, 'index.php');
+const phpConfPath = path.join(
+    __dirname,
+    rootDirectiory,
+    '.platform/nginx/conf.d/elasticbeanstalk/php.conf'
+);
 
 let mainHash = '';
 let mainBundleFile = '';
@@ -51,8 +61,7 @@ const phpScript = `<?php
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
     curl_close($ch);
-?>
-`;
+?>`;
 
 // JavaScript to append to index.html
 const whiteLabelScript = `
@@ -61,9 +70,59 @@ const whiteLabelScript = `
     if (response) {
         localStorage.setItem('whiteLabel', response.slice(1,-1));
     }
-</script>
-`;
+</script>`;
 
+// Nginx configuration for php.conf
+const nginxConfig = `
+# This file is managed by Elastic Beanstalk, Pass the PHP scripts to FastCGI server
+
+root /var/www/html;
+
+index index.php index.html index.htm;
+
+# Exclude Angular files (static assets or specific routes)
+location ~* \.(?:js|css|png|jpg|jpeg|gif|ico|woff|woff2|ttf|otf|eot|svg|mp4|webm|html|json|map|pdf|txt|br)$ {
+    try_files $uri =404;
+}
+
+# Serve Angular application paths directly
+location /assets/ {
+    try_files $uri $uri/ =404;
+}
+
+# Redirect all other requests to PHP
+location / {
+    rewrite ^ /index.php$is_args$args;
+}
+
+location ~ \.(php|phar)(/.*)?$ {
+    fastcgi_split_path_info ^(.+\.(?:php|phar))(/.*)$;
+    fastcgi_intercept_errors on;
+    fastcgi_index  index.php;
+    fastcgi_param  QUERY_STRING       $query_string;
+    fastcgi_param  REQUEST_METHOD     $request_method;
+    fastcgi_param  CONTENT_TYPE       $content_type;
+    fastcgi_param  CONTENT_LENGTH     $content_length;
+    fastcgi_param  SCRIPT_NAME        $fastcgi_script_name;
+    fastcgi_param  REQUEST_URI        $request_uri;
+    fastcgi_param  DOCUMENT_URI       $document_uri;
+    fastcgi_param  DOCUMENT_ROOT      $document_root;
+    fastcgi_param  SERVER_PROTOCOL    $server_protocol;
+    fastcgi_param  REQUEST_SCHEME     $scheme;
+    fastcgi_param  HTTPS              $https if_not_empty;
+    fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
+    fastcgi_param  SERVER_SOFTWARE    nginx/$nginx_version;
+    fastcgi_param  REMOTE_ADDR        $remote_addr;
+    fastcgi_param  REMOTE_PORT        $remote_port;
+    fastcgi_param  SERVER_ADDR        $server_addr;
+    fastcgi_param  SERVER_PORT        $server_port;
+    fastcgi_param  SERVER_NAME        $server_name;
+    # PHP only, required if PHP was built with --enable-force-cgi-redirect
+    fastcgi_param  REDIRECT_STATUS    200;
+    fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
+    fastcgi_param  PATH_INFO $fastcgi_path_info;
+    fastcgi_pass   php-fpm;
+}`;
 
 // Function to append script to index.html
 const appendScriptToIndex = (indexPath, phpScriptContent, jsScriptContent) => {
@@ -131,7 +190,15 @@ readDir(path.join(__dirname, rootDirectiory))
     })
     .then(() => {
         console.log('Ensuring directories for php.conf...');
-        // return ensureDirectoriesExist(phpConfPath);
+        return ensureDirectoriesExist(phpConfPath); // Ensures directory exists before writing
+    })
+    .then(() => {
+        console.log('Writing nginx configuration to php.conf...');
+        return writeFile(phpConfPath, nginxConfig);
+    })
+    .then(() => {
+        console.log('Creating .platform folder ');
+        return mkdir(path.join(__dirname, rootDirectiory, '.platform', 'nginx', 'conf.d'), { recursive: true });
     })
     .then(() => {
         console.log('Post-build tasks completed successfully.');
