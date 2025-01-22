6c73d859b0a8ca226c6a8f215df0e29f56bb4515
Ankit <walkover@walkovers-iMac.local>
2025-01-22 18:53:23 +0530

    set decimal value

--- apps/web-giddh/src/app/store/settings/Settings.reducer.ts
+++ apps/web-giddh/src/app/store/settings/Settings.reducer.ts
@@ -234,9 +234,15 @@ export function SettingsReducer(state = initialState, action: CustomActions): Se
         case SETTINGS_PROFILE_ACTIONS.GET_PROFILE_RESPONSE: {
             let response: BaseResponse<CompanyResponse, string> = action.payload;
             if (response?.status === 'success') {
+                localStorage.setItem('currencyDecimalType', response.body?.balanceDecimalPlaces);
+                localStorage.setItem('currencyNumberType', response.body?.balanceDisplayFormat);
                 newState.profile = response.body;
                 newState.profileRequest = true;
                 newState.getProfileInProgress = false;
+                const currentCompanyIndx = _.findIndex(newState.companies, (company) => company?.uniqueName === response.body?.uniqueName);
+                if (currentCompanyIndx !== -1) {
+                    newState.companies[currentCompanyIndx].country = response.body?.country;
+                }
                 return Object.assign({}, state, newState);
             } else if (response?.status === 'error' && response.statusCode === UNAUTHORISED) {
                 return { ...state, updateProfileInProgress: false, getProfileInProgress: false };
