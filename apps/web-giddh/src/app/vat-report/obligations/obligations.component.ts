8e24474f4247e9116d26b3e4bcbae3dc690d227a
Ankit <walkover@walkovers-iMac.local>
2025-01-30 15:59:32 +0530

    Prod - Authenticate button on GSTR2 and connect HMRC button on VAT report page is not disable even when plan has tax filing restriction

--- apps/web-giddh/src/app/vat-report/obligations/obligations.component.ts
+++ apps/web-giddh/src/app/vat-report/obligations/obligations.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { merge, Observable, ReplaySubject, take, takeUntil } from 'rxjs';
-import { GIDDH_DATE_RANGE_PICKER_RANGES } from '../../app.constant';
+import { GIDDH_DATE_RANGE_PICKER_RANGES, RestrictedModules } from '../../app.constant';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { GIDDH_DATE_FORMAT, GIDDH_NEW_DATE_FORMAT_UI } from '../../shared/helpers/defaultDateFormat';
 import * as dayjs from 'dayjs';
@@ -83,6 +83,10 @@ export class ObligationsComponent implements OnInit, OnDestroy {
     public connectToHMRCUrl$ = this.componentStore.select(state => state.connectToHMRCUrl);
     /** Observable to store the data of obligation */
     public obligationList$ = this.componentStore.select(state => state.obligationList);
+    /** Stores the current company */
+    public activeCompany: any = {};
+    /** Enum for restricted modules */
+    public restrictedModules: any = RestrictedModules;
 
     constructor(
         private formBuilder: UntypedFormBuilder,
@@ -98,6 +102,7 @@ export class ObligationsComponent implements OnInit, OnDestroy {
         this.store.pipe(select(state => state.session.activeCompany), takeUntil(this.destroyed$)).subscribe(activeCompany => {
             if (activeCompany) {
                 this.companyUniqueName = activeCompany.uniqueName;
+                this.activeCompany = activeCompany;
             }
         });
     }
@@ -156,7 +161,9 @@ export class ObligationsComponent implements OnInit, OnDestroy {
                 if (this.isCompanyMode || this.isConsolidatedBranch) {
                     this.hasTaxNumber = true;
                 }
-                this.getURLHMRCAuthorization();
+                if (!this.activeCompany?.subscription?.planDetails?.restrictedModules.hasOwnProperty(this.restrictedModules.TaxFilling)) {
+                    this.getURLHMRCAuthorization();
+                }
             }
         });
 
@@ -204,6 +211,17 @@ export class ObligationsComponent implements OnInit, OnDestroy {
                 }
             }
         });
+    }
+
+    /**
+     * Navigates to the page for buy plan.
+     * @param subscriptionId
+     * @memberof  ObligationsComponent
+     */
+    public buyPlan(subscriptionId: string): void {
+        if (subscriptionId) {
+            this.route.navigate(['pages', 'user-details', 'subscription', 'buy-plan', subscriptionId]);
+        }
     }
 
     /**
--- apps/web-giddh/src/app/vat-report/vat-report-filters/vat-report-filters.component.ts
+++ apps/web-giddh/src/app/vat-report/vat-report-filters/vat-report-filters.component.ts
@@ -7,7 +7,7 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { SettingsFinancialYearService } from '../../services/settings.financial-year.service';
 import { Observable, ReplaySubject, take, takeUntil } from 'rxjs';
 import { IOption } from '../../theme/ng-virtual-select/sh-options.interface';
-import { BranchHierarchyType, GIDDH_DATE_RANGE_PICKER_RANGES, SALES_TAX_SUPPORTED_COUNTRIES, TRN_SUPPORTED_COUNTRIES, VAT_SUPPORTED_COUNTRIES } from '../../app.constant';
+import { BranchHierarchyType, GIDDH_DATE_RANGE_PICKER_RANGES, RestrictedModules, SALES_TAX_SUPPORTED_COUNTRIES, TRN_SUPPORTED_COUNTRIES, VAT_SUPPORTED_COUNTRIES } from '../../app.constant';
 import * as dayjs from 'dayjs';
 import { GIDDH_DATE_FORMAT, GIDDH_DATE_FORMAT_YYYY_MM_DD, GIDDH_NEW_DATE_FORMAT_UI } from '../../shared/helpers/defaultDateFormat';
 import { OrganizationType } from '../../models/user-login-state';
@@ -15,7 +15,7 @@ import { cloneDeep } from '../../lodash-optimized';
 import { GstReconcileService } from '../../services/gst-reconcile.service';
 import { CommonService } from '../../services/common.service';
 import { ToasterService } from '../../services/toaster.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SettingsTaxesActions } from '../../actions/settings/taxes/settings.taxes.action';
 import { SalesTaxReport } from '../../theme/tax-authority/utility/tax-authority.const';
 import { CompanyActions } from '../../actions/company.actions';
@@ -179,6 +179,8 @@ export class VatReportFiltersComponent implements OnInit, OnChanges {
     }
     /** True if consolidated branch */
     public isConsolidatedBranch: boolean;
+    /** Enum for restricted modules */
+    public restrictedModules: any = RestrictedModules;
 
     constructor(
         private store: Store<AppState>,
@@ -192,7 +194,8 @@ export class VatReportFiltersComponent implements OnInit, OnChanges {
         private route: ActivatedRoute,
         private settingsTaxesActions: SettingsTaxesActions,
         private companyActions: CompanyActions,
-        private componentStore: TaxAuthorityComponentStore
+        private componentStore: TaxAuthorityComponentStore,
+        private router: Router
     ) {
         this.getFinancialYears();
     }
@@ -272,6 +275,16 @@ export class VatReportFiltersComponent implements OnInit, OnChanges {
         }
     }
 
+    /**
+     * Navigates to the page for buy plan.
+     * @param subscriptionId
+     * @memberof  ShareGroupModalComponent
+     */
+    public buyPlan(subscriptionId: string): void {
+        if (subscriptionId) {
+            this.router.navigate(['pages', 'user-details', 'subscription', 'buy-plan', subscriptionId]);
+        }
+    }
     /**
     * On Change of input properties
     *
