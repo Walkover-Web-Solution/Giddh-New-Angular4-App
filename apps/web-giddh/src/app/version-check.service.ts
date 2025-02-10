a92bebac3094901b6bc8d7c99c90f0e808eb0b98
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-02-10 15:29:35 +0530

    PR changes

--- apps/web-giddh/src/app/version-check.service.ts
+++ apps/web-giddh/src/app/version-check.service.ts
@@ -20,8 +20,6 @@ export class VersionCheckService {
      * @param {number} frequency - in milliseconds, defaults to 30 minutes
      */
     public initVersionCheck(url, frequency = 1000 * 60 * 1) { // will check in every 1 minutes
-        console.log(url);
-
         this.checkVersion(url);
 
         setTimeout(() => {
