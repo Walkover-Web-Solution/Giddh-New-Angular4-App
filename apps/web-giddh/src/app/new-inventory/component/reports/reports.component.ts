a04bbdfd743a74fdc31ee2a4c061e2e9ab14a565
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-02-19 16:58:10 +0530

    fixed reprot issue

--- apps/web-giddh/src/app/new-inventory/component/reports/reports.component.ts
+++ apps/web-giddh/src/app/new-inventory/component/reports/reports.component.ts
@@ -668,6 +668,7 @@ export class ReportsComponent implements OnInit {
             this.displayedColumns = event
                 .filter(item => item?.checked)
                 .map(item => item?.value);
+            this.getReport(false);
         }
     }
 
