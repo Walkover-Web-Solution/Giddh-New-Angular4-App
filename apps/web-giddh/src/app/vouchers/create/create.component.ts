e4cc5f2363ff7444a1cfb6f6c1ea40eec8dbb67a
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-02-28 13:36:41 +0530

    Dilpreet | Stage | Prod Bugs

--- apps/web-giddh/src/app/vouchers/create/create.component.ts
+++ apps/web-giddh/src/app/vouchers/create/create.component.ts
@@ -3706,7 +3706,7 @@ export class VoucherCreateComponent implements OnInit, OnDestroy, AfterViewInit 
         invoiceForm = this.vouchersUtilityService.formatVoucherObject(invoiceForm);
 
         if (invoiceForm.account.mobileNumber != this.account.mobileNumber) {
-            invoiceForm.account.mobileNumber = this.intlClass.selectedCountryData.dialCode + invoiceForm.account.mobileNumber;
+            invoiceForm.account.mobileNumber = invoiceForm.account.mobileNumber ? this.intlClass.selectedCountryData.dialCode + invoiceForm.account.mobileNumber : '';
         }
 
         if (!this.currentVoucherFormDetails?.depositAllowed) {
