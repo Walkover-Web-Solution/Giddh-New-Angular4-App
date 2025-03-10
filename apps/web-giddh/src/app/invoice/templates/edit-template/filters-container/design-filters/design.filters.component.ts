0a4a09d955d5b4774b7d58d04ac6c51d1904a5f5
Divyanshu Shrivastava <69809432+dvCodeWorld@users.noreply.github.com>
2025-03-10 16:01:22 +0530

    Revert "Divyanshu | Test | Prepare and create template same as tally template for GIDDH - Version 1"

--- apps/web-giddh/src/app/invoice/templates/edit-template/filters-container/design-filters/design.filters.component.ts
+++ apps/web-giddh/src/app/invoice/templates/edit-template/filters-container/design-filters/design.filters.component.ts
@@ -376,6 +376,7 @@ export class DesignFiltersContainerComponent implements OnInit, OnDestroy {
                 if (this.customTemplate.templateType === 'tally_template') {
                     this.presetFonts = [
                         { label: 'Open Sans', value: 'Open Sans' },
+                        { label: 'Sans-Serif', value: 'Sans-Serif' },
                         { label: 'Roboto', value: 'Roboto' }
                     ];
                 }
