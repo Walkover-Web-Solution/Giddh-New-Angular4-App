61e6ba78081e55e8943634d376add5c53a971a40
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-03-11 17:15:51 +0530

    Dilpreet | Test |  Autopay setting issue

--- apps/web-giddh/src/app/ledger/components/update-ledger-tax-control/update-ledger-tax-control.component.ts
+++ apps/web-giddh/src/app/ledger/components/update-ledger-tax-control/update-ledger-tax-control.component.ts
@@ -122,8 +122,6 @@ export class UpdateLedgerTaxControlComponent implements OnDestroy, OnChanges {
      * prepare taxObject as per needed
      */
     public prepareTaxObject() {
-        console.log(this.taxes);
-        
         if (this.customTaxTypesForTaxFilter && this.customTaxTypesForTaxFilter.length) {
             this.taxes = this.taxes?.filter(f => this.customTaxTypesForTaxFilter.includes(f.taxType));
         }
