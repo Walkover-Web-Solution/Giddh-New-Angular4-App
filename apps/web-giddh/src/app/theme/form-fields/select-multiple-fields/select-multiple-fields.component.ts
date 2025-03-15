246069c2b5407df541461659109229e2b93f4876
shivani vani <shivani@walkover.in>
2025-03-15 15:20:20 +0530

    adjust the width of select-multiple-fields for email template

--- apps/web-giddh/src/app/theme/form-fields/select-multiple-fields/select-multiple-fields.component.ts
+++ apps/web-giddh/src/app/theme/form-fields/select-multiple-fields/select-multiple-fields.component.ts
@@ -42,6 +42,7 @@ export class SelectMultipleFieldsComponent implements OnInit, OnDestroy, OnChang
     @Input() public isPurchaseOrder: string = '';
     /** Holds module translation data */
     @Input() public localeData: any = {};
+    @Input() public isSmallWidth: string;
     /** Callback for option selected */
     @Output() public selectedOption: EventEmitter<any> = new EventEmitter<any>();
     /** List of chips based on selected values */
