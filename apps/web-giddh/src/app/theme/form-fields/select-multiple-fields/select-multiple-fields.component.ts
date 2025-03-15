641c2873cb066c949dc4628710e45e9031030097
shivani vani <shivani@walkover.in>
2025-03-15 15:38:24 +0530

    added documentations

--- apps/web-giddh/src/app/theme/form-fields/select-multiple-fields/select-multiple-fields.component.ts
+++ apps/web-giddh/src/app/theme/form-fields/select-multiple-fields/select-multiple-fields.component.ts
@@ -42,6 +42,7 @@ export class SelectMultipleFieldsComponent implements OnInit, OnDestroy, OnChang
     @Input() public isPurchaseOrder: string = '';
     /** Holds module translation data */
     @Input() public localeData: any = {};
+    /**  Holds width of select-multiple-fields for email template */
     @Input() public isSmallWidth: string;
     /** Callback for option selected */
     @Output() public selectedOption: EventEmitter<any> = new EventEmitter<any>();
