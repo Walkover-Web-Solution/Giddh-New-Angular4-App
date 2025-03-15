80214284180583ed1026e8b3df410b3a3fb9d79d
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-03-15 12:23:08 +0530

    fixed warehouse issue

--- apps/web-giddh/src/app/ledger/components/update-ledger-entry-panel/update-ledger-entry-panel.component.ts
+++ apps/web-giddh/src/app/ledger/components/update-ledger-entry-panel/update-ledger-entry-panel.component.ts
@@ -2437,6 +2437,7 @@ export class UpdateLedgerEntryPanelComponent implements OnInit, AfterViewInit, O
                         uniqueName: t.particular?.uniqueName
                     }
                 });
+                this.isStockPresent = false;
             }
         });
         initialAccounts.push(...this.defaultSuggestions);
