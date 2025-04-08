9d4233ffc442c4ccf63ac30ea202ab713c388af0
Divyanshu <divyanshushrivastava123@gmail.com>
2025-04-08 10:58:16 +0530

    Prevent close other tax dailog

--- apps/web-giddh/src/app/ledger/components/new-ledger-entry-panel/new-ledger-entry-panel.component.ts
+++ apps/web-giddh/src/app/ledger/components/new-ledger-entry-panel/new-ledger-entry-panel.component.ts
@@ -1259,7 +1259,8 @@ export class NewLedgerEntryPanelComponent implements OnInit, OnDestroy, OnChange
                 maxWidth: '760px',
                 width: '100%',
                 height: '100vh',
-                maxHeight: '100vh'
+                maxHeight: '100vh',
+                disableClose: true
             });
         }
     }
