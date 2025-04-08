572fa1f6319bb3fb6675e4208078cc867b417520
Divyanshu <divyanshushrivastava123@gmail.com>
2025-04-08 10:47:15 +0530

    Pr changes

--- apps/web-giddh/src/app/ledger/components/new-ledger-entry-panel/new-ledger-entry-panel.component.ts
+++ apps/web-giddh/src/app/ledger/components/new-ledger-entry-panel/new-ledger-entry-panel.component.ts
@@ -1230,6 +1230,11 @@ export class NewLedgerEntryPanelComponent implements OnInit, OnDestroy, OnChange
      */
     public closeAsideMenuStateForOtherTax(): void {
         this.asideMenuStateForOtherTaxesDialogRef.close();
+        if (this.blankLedger.otherTaxesSum > 0) {
+            this.blankLedger.isOtherTaxesApplicable = true;
+        } else {
+            this.blankLedger.isOtherTaxesApplicable = false;
+        }
     }
 
     public toggleOtherTaxesAsidePane(modalBool: boolean) {
