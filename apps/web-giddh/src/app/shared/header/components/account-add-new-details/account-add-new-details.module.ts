3ee0bd492e521f71062e0a3f20e0186a6e680225
Ankit <walkover@walkovers-iMac.local>
2025-04-18 16:27:14 +0530

    use panel class

--- apps/web-giddh/src/app/shared/header/components/account-add-new-details/account-add-new-details.module.ts
+++ apps/web-giddh/src/app/shared/header/components/account-add-new-details/account-add-new-details.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterModule } from '@angular/router';
-import { LaddaModule } from 'angular2-ladda';
-import { SelectModule } from 'apps/web-giddh/src/app/theme/ng-select/ng-select';
-import { ShSelectModule } from 'apps/web-giddh/src/app/theme/ng-virtual-select/sh-select.module';
 import { TranslateDirectiveModule } from 'apps/web-giddh/src/app/theme/translate/translate.directive.module';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { AccountAddNewDetailsComponent } from './account-add-new-details.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { BulkAddDialogComponent } from '../bulk-add-dialog/bulk-add-dialog.component';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatTabsModule } from '@angular/material/tabs';
 import { FormFieldsModule } from 'apps/web-giddh/src/app/theme/form-fields/form-fields.module';
@@ -22,10 +18,6 @@ import { FormFieldsModule } from 'apps/web-giddh/src/app/theme/form-fields/form-
     exports: [AccountAddNewDetailsComponent],
     imports: [
         CommonModule,
-        LaddaModule.forRoot({
-            style: 'slide-left',
-            spinnerSize: 30
-        }),
         ReactiveFormsModule,
         TabsModule.forRoot(),
         TranslateDirectiveModule,
@@ -36,7 +28,6 @@ import { FormFieldsModule } from 'apps/web-giddh/src/app/theme/form-fields/form-
         FormsModule,
         MatRadioModule,
         MatTabsModule,
-        TooltipModule.forRoot(),
         FormFieldsModule
     ]
 })
