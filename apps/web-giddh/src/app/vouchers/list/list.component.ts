054bbf79ae9422e16920ffd736f63788125eaa04
Divyanshu <divyanshushrivastava123@gmail.com>
2025-04-30 18:44:09 +0530

    Excel export design and key changes

--- apps/web-giddh/src/app/vouchers/list/list.component.ts
+++ apps/web-giddh/src/app/vouchers/list/list.component.ts
@@ -1547,7 +1547,7 @@ export class VoucherListComponent implements OnInit, OnDestroy {
                 allVouchersSelected: this.allVouchersSelected,
                 localeData: this.localeData
             },
-            panelClass: ['mat-dialog-md'],
+            maxHeight: '80vh',
             disableClose: true
         });
 
