6d14b32b8e36695bb71c6b6beb0bcdf7f56d20df
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-05-06 12:25:43 +0530

    Dilpreet | Stage | Prod - Amount to pay on pop up is coming wrong in renew plan (active expired subscription)

--- apps/web-giddh/src/app/subscription/buy-plan/buy-plan.component.ts
+++ apps/web-giddh/src/app/subscription/buy-plan/buy-plan.component.ts
@@ -1309,6 +1309,11 @@ export class BuyPlanComponent implements OnInit, OnDestroy {
             duration: this.firstStepForm.get('duration').value,
             countryCode: this.isNewUserLoggedIn ? this.selectedPlan?.entityCode : this.secondStepForm.get('country').value?.value
         }
+
+        if (this.isChangePlan || this.isRenewPlan) {
+            reqObj['subscriptionId'] = this.subscriptionId;
+        }
+
         if (this.selectedPlan?.uniqueName && reqObj?.countryCode) {
             this.componentStore.getCalculationData(reqObj);
         }
