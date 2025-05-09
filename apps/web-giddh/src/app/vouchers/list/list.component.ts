0f407f085f7032167dc642b8dba28cb11a6ee94b
Ankit <walkover@walkovers-iMac.local>
2025-05-09 18:37:47 +0530

    conflict resolve

--- apps/web-giddh/src/app/vouchers/list/list.component.ts
+++ apps/web-giddh/src/app/vouchers/list/list.component.ts
@@ -1692,19 +1692,19 @@ export class VoucherListComponent implements OnInit, OnDestroy {
                         this.poBulkAction('delete');
                     }
                 } else if (this.voucherType === VoucherTypeEnum.generateEstimate || this.voucherType === VoucherTypeEnum.generateProforma) {
-                    const voucher = this.selectedVouchers[0];
+                    const selectedVoucher = voucher ?? this.selectedVouchers[0];
                     const payload = {
-                        accountUniqueName: voucher.customerUniqueName
+                        accountUniqueName: selectedVoucher.customerUniqueName
                     }
                     if (this.voucherType === VoucherTypeEnum.generateEstimate) {
-                        payload['estimateNumber'] = voucher?.estimateNumber;
+                        payload['estimateNumber'] = selectedVoucher?.estimateNumber;
                     } else {
-                        payload['proformaNumber'] = voucher?.proformaNumber;
+                        payload['proformaNumber'] = selectedVoucher?.proformaNumber;
                     }
                     this.componentStore.deleteEstimsteProformaVoucher({ payload: payload, voucherType: this.voucherType });
                 } else {
                     const payload = {
-                        voucherUniqueNames: this.selectedVouchers?.map(voucher => { return voucher?.uniqueName }),
+                        voucherUniqueNames: voucher?.uniqueName ? [voucher.uniqueName] : this.selectedVouchers?.map(voucher => { return voucher?.uniqueName }),
                         voucherType: this.voucherType
                     };
                     this.componentStore.bulkUpdateInvoice({ payload: payload, actionType: 'delete' });
@@ -1994,6 +1994,27 @@ export class VoucherListComponent implements OnInit, OnDestroy {
     }
 
     /**
+     * Handle cancel Voucher Dialog
+     *
+     * @param {*} voucher
+     * @memberof VoucherListComponent
+     */
+    public cancelVoucherDialog(voucher: any): void {
+        const dialogRef = this.dialog.open(NewConfirmationModalComponent, {
+            panelClass: ['mat-dialog-md'],
+            data: {
+                configuration: this.generalService.deleteConfiguration(this.localeData?.cancel_voucher_confirmation_message, this.commonLocaleData)
+            }
+        });
+
+        dialogRef.afterClosed().pipe(takeUntil(this.destroyed$)).subscribe(response => {
+            if (response && response === this.commonLocaleData?.app_yes) {
+                this.actionVoucher(voucher, 'cancel');
+            }
+        });
+    }
+
+    /**
      * Handle Voucher Actions API Call
      *
      * @param {*} voucher
@@ -2001,7 +2022,7 @@ export class VoucherListComponent implements OnInit, OnDestroy {
      * @memberof VoucherListComponent
      */
     public actionVoucher(voucher: any, action: string): void {
-        this.componentStore.actionVoucher({ voucherUniqueName: voucher?.uniqueName, payload: { action: action, voucherType: voucher?.voucherType ?? this.voucherType } });
+        this.componentStore.actionVoucher({ voucherUniqueName: voucher?.uniqueName, payload: { action: action, voucherType: voucher?.voucherType ?? this.voucherType }});
     }
 
     /**
