42314fa28db66f5170bd68d4e863f4e383bb6b34
Ankit <walkover@walkovers-iMac.local>
2025-05-24 17:48:27 +0530

    Change PDF library for template A

--- apps/web-giddh/src/app/invoice/templates/edit-template/filters-container/design-filters/design.filters.component.ts
+++ apps/web-giddh/src/app/invoice/templates/edit-template/filters-container/design-filters/design.filters.component.ts
@@ -41,7 +41,8 @@ export class DesignFiltersContainerComponent implements OnInit, OnDestroy {
     public _presetFonts = [
         { label: 'Open Sans', value: 'Open Sans' },
         { label: 'Sans-Serif', value: 'Sans-Serif' },
-        { label: 'opensans-regular', value: 'opensans-regular' }
+        { label: 'opensans-regular', value: 'opensans-regular' },
+        { label: 'Roboto', value: 'Roboto' }
     ];
     public _presetFontsSize = [
         { label: '16px', value: 16 },
@@ -425,7 +426,7 @@ export class DesignFiltersContainerComponent implements OnInit, OnDestroy {
     /**
      * * This is used when the user changes the template.
      */
-    public showMessage(): void{
+    public showMessage(): void {
         this._toasty.showSnackBar("warning", 'You can not change the template type in update mode.');
     }
 }
