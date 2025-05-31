7c1f15cb47d818366e2c4d1d7c3406f4c5608a33
Ankit <ankitdubey@whozzat.com>
2025-05-31 16:13:59 +0530

    add document

--- apps/web-giddh/src/app/ledger/components/import-statement/import-statement.const.ts
+++ apps/web-giddh/src/app/ledger/components/import-statement/import-statement.const.ts
@@ -8,6 +8,7 @@ export enum ImportStatementType {
     BankStatement = 'Bank_statement'
 };
 
+// This enum is used to differentiate between the types of vouchers that can be imported
 export enum VoucherType {
     AccountWise = "account-wise",
     VoucherWise = "voucher-wise"
