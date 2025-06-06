cdef5c5b9d5d7ad42d85dd9e20c63bb33d2f1cf6
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-06-06 13:37:29 +0530

    revert code

--- apps/web-giddh/src/app/invoice/templates/edit-template/filters-container/design-filters/design.filters.component.ts
+++ apps/web-giddh/src/app/invoice/templates/edit-template/filters-container/design-filters/design.filters.component.ts
@@ -40,8 +40,9 @@ export class DesignFiltersContainerComponent implements OnInit, OnDestroy {
     public selectedTemplateUniqueName: string = 'gst_template_a';
     public _presetFonts = [
         { label: 'Open Sans', value: 'Open Sans' },
-        { label: 'Sans-Serif', value: 'Sans-Serif' },
-        { label: 'opensans-regular', value: 'opensans-regular' }
+        { label: 'Roboto', value: 'Roboto' },
+        { label: 'Lato', value: 'Lato' },
+        { label: 'Inter', value: 'Inter' }
     ];
     public _presetFontsSize = [
         { label: '16px', value: 16 },
@@ -425,7 +426,7 @@ export class DesignFiltersContainerComponent implements OnInit, OnDestroy {
     /**
      * * This is used when the user changes the template.
      */
-    public showMessage(): void{
+    public showMessage(): void {
         this._toasty.showSnackBar("warning", 'You can not change the template type in update mode.');
     }
 }
