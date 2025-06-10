4f278b1180efdc89a32c38f374fceb32a0d0fb69
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-06-10 19:14:48 +0530

    Dilpreet | Stage | Export ledger T-view

--- apps/web-giddh/src/app/ledger/ledger.component.ts
+++ apps/web-giddh/src/app/ledger/ledger.component.ts
@@ -1820,7 +1820,9 @@ export class LedgerComponent implements OnInit, OnDestroy {
             data: {
                 accountUniqueName: this.lc.accountUnq,
                 advanceSearchRequest: this.advanceSearchRequest,
-                selectEntryUniqueName: this.checkedTrxWhileHovering.map(((entry) => { return entry.uniqueName }))
+                selectEntryUniqueName: this.checkedTrxWhileHovering.map(((entry) => { return entry.uniqueName })),
+                currencyTogglerModel: this.currencyTogglerModel,
+                isLedgerAccountAllowsMultiCurrency: this.isLedgerAccountAllowsMultiCurrency
             },
             role: 'alertdialog',
             ariaLabel: 'export'
