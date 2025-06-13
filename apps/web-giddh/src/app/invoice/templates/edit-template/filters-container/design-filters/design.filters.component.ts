3b31834df5e686d8872484cc48dc25df5516c371
Divyanshu <divyanshushrivastava123@gmail.com>
2025-06-13 15:24:26 +0530

    Feat: Add Header and footer  custom company name support

--- apps/web-giddh/src/app/invoice/templates/edit-template/filters-container/design-filters/design.filters.component.ts
+++ apps/web-giddh/src/app/invoice/templates/edit-template/filters-container/design-filters/design.filters.component.ts
@@ -69,6 +69,8 @@ export class DesignFiltersContainerComponent implements OnInit, OnDestroy {
     public selectedFontSize: string = "";
     /** Default image size */
     public defaultImageSize: string = 'S';
+    /** Stores the active company name */
+    public companyName: string;
 
     constructor(
         private _invoiceUiDataService: InvoiceUiDataService,
@@ -85,10 +87,11 @@ export class DesignFiltersContainerComponent implements OnInit, OnDestroy {
         let companies = null;
         let defaultTemplate = null;
 
-        this.store.pipe(select(s => s.session), take(1)).subscribe(ss => {
-            companyUniqueName = ss.companyUniqueName;
-            companies = ss.companies;
-            this.companyUniqueName = ss.companyUniqueName;
+        this.store.pipe(select(s => s.session), take(1)).subscribe(session => {
+            companyUniqueName = session.companyUniqueName;
+            companies = session.companies;
+            this.companyUniqueName = session.companyUniqueName;
+            this.companyName = session.companies.find((company) => company?.uniqueName === session.companyUniqueName)?.name ?? '';
         });
 
         this.store.pipe(select(s => s.invoiceTemplate), take(1)).subscribe(ss => {
@@ -196,6 +199,8 @@ export class DesignFiltersContainerComponent implements OnInit, OnDestroy {
         }
         template.copyFrom = cloneDeep(value);
         this.selectedTemplateUniqueName = value;
+        template.sections['header'].data['companyName'].label = this.companyName;
+        template.sections['footer'].data['companyName'].label = this.companyName;
         this._invoiceUiDataService.setCustomTemplate(cloneDeep(template));
     }
 
@@ -291,7 +296,6 @@ export class DesignFiltersContainerComponent implements OnInit, OnDestroy {
             data.updatedAt = null;
             data.updatedBy = null;
             data.sections['header'].data['pan'].label = '';
-            data.sections['header'].data['companyName'].label = '';
 
             data = this.newLineToBR(data);
 
