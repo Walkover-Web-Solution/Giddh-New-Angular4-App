7fcc1461c8edf614d692f6dbe798e66438f7f962
Divyanshu Shrivastava <divyanshushrivastava123@gmail.com>
2025-06-23 02:49:01 +0530

    conflict resolved

--- apps/web-giddh/src/app/financial-reports/components/profit-loss/components/profit-loss-grid/profit-loss-grid.component.ts
+++ apps/web-giddh/src/app/financial-reports/components/profit-loss/components/profit-loss-grid/profit-loss-grid.component.ts
@@ -105,7 +105,7 @@ export class ProfitLossGridComponent implements OnInit, OnChanges, OnDestroy {
                 this.listOfCheckGroupsAccounts = [];
                 setTimeout(() => {
                     this.refresh.emit();
-                }, 200);
+                }, 600);
             }
         });
     }
@@ -252,7 +252,8 @@ export class ProfitLossGridComponent implements OnInit, OnChanges, OnDestroy {
                     request: {
                         reportType: ReportType.ProfitLoss,
                         from: this.from,
-                        to: this.to
+                        to: this.to,
+                        branchUniqueName: this.generalService.currentBranchUniqueName
                     },
                     payload: this.listOfCheckGroupsAccounts
                 };
