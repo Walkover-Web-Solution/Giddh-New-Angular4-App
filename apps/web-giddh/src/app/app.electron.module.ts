3113bf5dbe85ffa6d608ba7347f59dbbe50e873e
Divyanshu <divyanshushrivastava123@gmail.com>
2025-06-24 14:38:27 +0530

    Fix: mat-accordion not works in electron

--- apps/web-giddh/src/app/app.electron.module.ts
+++ apps/web-giddh/src/app/app.electron.module.ts
@@ -5,11 +5,14 @@ import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { ROUTES } from './app.routes';
 import { CustomPreloadingStrategy } from './services/lazy-preloading.service';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
     imports: [
         AppModule,
+        BrowserAnimationsModule,
+        MatExpansionModule,
         RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: CustomPreloadingStrategy }),
     ],
     bootstrap: [AppComponent]
