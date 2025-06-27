381378ba9a3ed9cdb5a8503dd177465a811c760b
Divyanshu <divyanshushrivastava123@gmail.com>
2025-06-27 14:14:44 +0530

    fix: add loading state and delay when resetting voucher columns

--- apps/web-giddh/src/app/vouchers/list/list.component.ts
+++ apps/web-giddh/src/app/vouchers/list/list.component.ts
@@ -3264,6 +3264,7 @@ export class VoucherListComponent implements OnInit, OnDestroy {
             return;
         }
         this.getVouchersInProgress$.pipe(filter(inProgress => !inProgress), take(1)).subscribe(() => {
+            this.isColumnsLoading = true;
             this.dynamicCustomColumns = [];
             this.displayedColumns = [];
             this.dataSource = [];
@@ -3282,7 +3283,9 @@ export class VoucherListComponent implements OnInit, OnDestroy {
             this.setEInvoiceColumns();
             this.getVouchers(false);
         });
-        this.isColumnsLoading = false;
+       setTimeout(() => {
+            this.isColumnsLoading = false;
+       }, 400);
     }
 
     /**
