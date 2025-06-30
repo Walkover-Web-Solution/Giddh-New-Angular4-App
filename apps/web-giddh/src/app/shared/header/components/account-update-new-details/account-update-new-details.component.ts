ca176209eaac8f951a32ff658cb589130172f315
Ankit <ankitdubey@whozzat.com>
2025-06-30 17:01:55 +0530

    Prod - Assigned discount is not displaying on account

--- apps/web-giddh/src/app/shared/header/components/account-update-new-details/account-update-new-details.component.ts
+++ apps/web-giddh/src/app/shared/header/components/account-update-new-details/account-update-new-details.component.ts
@@ -158,7 +158,7 @@ export class AccountUpdateNewDetailsComponent implements OnInit, OnDestroy, OnCh
     public selectedAccountCallingCode: string = '';
     public isOtherSelectedTab: boolean = false;
     public selectedaccountForMerge: any = [];
-    public selectedDiscounts: string = null;
+    public selectedDiscounts: IOption = null
     public selectedDiscountList: any[] = [];
     public GSTIN_OR_TRN: string;
     public selectedCompanyCountryName: string;
@@ -1192,6 +1192,9 @@ export class AccountUpdateNewDetailsComponent implements OnInit, OnDestroy, OnCh
     public selectedState(gstForm: FormGroup, event: IOption): void {
         if (gstForm && event?.label) {
             gstForm.get('stateCode')?.patchValue(event?.value);
+            if (gstForm.get('state').get('code')?.value !== event?.value) {
+                gstForm.get('gstNumber')?.patchValue("");
+            }
             gstForm.get('state').get('code')?.patchValue(event?.value);
             const name = event.label.split(' - ')[1];
             gstForm.get('state').get('name')?.patchValue(name);
@@ -1697,9 +1700,10 @@ export class AccountUpdateNewDetailsComponent implements OnInit, OnDestroy, OnCh
         if (this.activeAccountName) {
             let assignDiscountObject: ApplyDiscountRequestV2 = new ApplyDiscountRequestV2();
             assignDiscountObject.uniqueName = this.activeAccountName;
-            assignDiscountObject.discounts = this.selectedDiscounts?.length ? [this.selectedDiscounts] : [];
+            assignDiscountObject.discounts = this.selectedDiscounts?.value ? [this.selectedDiscounts.value] : [];
             assignDiscountObject.isAccount = true;
             this.store.dispatch(this.accountsAction.applyAccountDiscountV2([assignDiscountObject]));
+            this.isDiscountSaveDisable$ = observableOf(true);
         }
     }
 
@@ -1812,10 +1816,18 @@ export class AccountUpdateNewDetailsComponent implements OnInit, OnDestroy, OnCh
 
     /**
      * To check discount list updated
-     *
+     * @param {*} event - event object containing selected discounts
      * @memberof AccountUpdateNewDetailsComponent
      */
-    public discountSelected(): void {
+    public discountSelected(event: any): void {
+        if (event) {
+            this.selectedDiscounts = {
+                value: event.value,
+                label: event.label
+            };
+        } else {
+            this.selectedDiscounts = null;
+        }
         this.isDiscountSaveDisable$ = observableOf(false);
     }
 
@@ -2200,8 +2212,10 @@ export class AccountUpdateNewDetailsComponent implements OnInit, OnDestroy, OnCh
                                 if (response.body[accountDetails?.uniqueName]) {
                                     let list = response.body[accountDetails?.uniqueName];
                                     Object.keys(list)?.forEach(key => {
-                                        let UniqueName = list[key]['discount']['uniqueName'];
-                                        this.selectedDiscounts = UniqueName;
+                                        this.selectedDiscounts = {
+                                            value: list[key]['discount']['uniqueName'],
+                                            label: list[key]['discount']['name']
+                                        };
                                     });
                                 }
                             }
