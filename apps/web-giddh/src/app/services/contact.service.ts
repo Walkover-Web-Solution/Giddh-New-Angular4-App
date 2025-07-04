7fa15fb0bab94237c561a5023c65fed167748aff
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-07-04 13:29:49 +0530

    Update contact.service.ts
    PR changes

--- apps/web-giddh/src/app/services/contact.service.ts
+++ apps/web-giddh/src/app/services/contact.service.ts
@@ -197,7 +197,7 @@ export class ContactService {
         this.companyUniqueName = this.generalService.companyUniqueName;
         const branchUniqueName = requestObjCopy.branchUniqueName;
         // Helper to build URL
-        const buildUrl = (isPost: boolean) => {
+        const buildUrl = () => {
             let url = this.config.apiUrl + ACCOUNT_STATEMENT_API.GET
                 .replace(':companyUniqueName', encodeURIComponent(this.companyUniqueName))
                 .replace(':accountUniqueName', encodeURIComponent(model.accountUniqueName))
@@ -222,9 +222,9 @@ export class ContactService {
         const handleError = (e: any) => this.errorHandler.HandleCatch<any, any>(e);
 
         if (requestObj.method === 'POST') {
-            return this.http.post(buildUrl(true), body).pipe(handleResponse, catchError(handleError));
+            return this.http.post(buildUrl(), body).pipe(handleResponse, catchError(handleError));
         } else {
-            return this.http.get(buildUrl(false)).pipe(handleResponse, catchError(handleError));
+            return this.http.get(buildUrl()).pipe(handleResponse, catchError(handleError));
         }
     }
 }
