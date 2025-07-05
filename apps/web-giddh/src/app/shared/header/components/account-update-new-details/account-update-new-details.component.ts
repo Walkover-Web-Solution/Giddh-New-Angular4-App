039dbdff2183fa59868df872fc31dc1ef4720e68
Ankit <ankitdubey@whozzat.com>
2025-07-05 12:43:40 +0530

    Prod - Mobile number is getting removed when updating account

--- apps/web-giddh/src/app/shared/header/components/account-update-new-details/account-update-new-details.component.ts
+++ apps/web-giddh/src/app/shared/header/components/account-update-new-details/account-update-new-details.component.ts
@@ -1066,6 +1066,7 @@ export class AccountUpdateNewDetailsComponent implements OnInit, OnDestroy, OnCh
             if (accountRequest.mobileCode && accountRequest.mobileNo) {
                 accountRequest.mobileNo = accountRequest.mobileNo;
             }
+            delete accountRequest['mobileCode'];
         }
 
         if (!this.showVirtualAccount) {
@@ -1090,9 +1091,10 @@ export class AccountUpdateNewDetailsComponent implements OnInit, OnDestroy, OnCh
         }
         if (this.intl) {
             let mobileNo = this.intl['init-contact-update']?.getNumber();
-            accountRequest['mobileNo'] = mobileNo;
+            if (mobileNo) {
+                accountRequest['mobileNo'] = mobileNo;
+            }
         }
-
         accountRequest['hsnNumber'] = (accountRequest["hsnOrSac"] === "hsn") ? accountRequest['hsnNumber'] : "";
         accountRequest['sacNumber'] = (accountRequest["hsnOrSac"] === "sac") ? accountRequest['sacNumber'] : "";
 
@@ -1355,7 +1357,7 @@ export class AccountUpdateNewDetailsComponent implements OnInit, OnDestroy, OnCh
                             this.addAccountForm.get('currency')?.patchValue(this.selectedCountryCurrency);
                             this.selectedCurrency = this.selectedCountryCurrency;
                         }
-                        if (!this.addAccountForm.get('mobileCode')?.value) {
+                        if (!this.addAccountForm.get('mobileCode')?.value && this.selectedAccountCallingCode) {
                             this.addAccountForm.get('mobileCode')?.patchValue(this.selectedAccountCallingCode);
                         }
                     }
