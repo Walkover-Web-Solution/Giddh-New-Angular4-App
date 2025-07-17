67e4c4506f175d4a8223f643cdf41782c60cc712
Divyanshu <divyanshushrivastava123@gmail.com>
2025-07-17 15:17:58 +0530

    pr changes

--- apps/web-giddh/src/app/services/general.service.ts
+++ apps/web-giddh/src/app/services/general.service.ts
@@ -2266,7 +2266,7 @@ export class GeneralService {
      * @returns {Array<{ label: string, value: string }>} An array of voucher type objects, each containing
      * @memberof GeneralService
      */
-    public getVoucherTypeList(commonLocaleData: any, onlyVouchers: string[] = ['sales', 'purchase', 'purchase order', 'receipt', 'payment', 'estimate', 'proforma', 'debit note', 'credit note']): IOption[] {
+    public getVoucherTypeList(commonLocaleData: any, onlyVouchers: string[] = []): IOption[] {
         const allVouchers = [{
             label: commonLocaleData?.app_voucher_types.sales,
             value: 'sales'
