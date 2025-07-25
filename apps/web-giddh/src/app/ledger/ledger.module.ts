ccebff9405d02b3c34b056e88def2556a9cd3ae3
Ankit <ankitdubey@whozzat.com>
2025-07-25 15:46:39 +0530

    conflict resolve

--- apps/web-giddh/src/app/ledger/ledger.module.ts
+++ apps/web-giddh/src/app/ledger/ledger.module.ts
@@ -65,6 +65,8 @@ import { CarouselComponent } from '../shared/carousel/carousel.component';
 import { AsideMenuCreateTaxModule } from '../shared/aside-menu-create-tax/aside-menu-create-tax.module';
 import { AsideMenuAccountModule } from '../shared/aside-menu-account/aside.menu.account.module';
 import { SalesPersonService } from '../shared/sales-person/utility/sales-person.service';
+import { TributeMentionModule } from '../shared/helpers/directives/tributeMention/tributeMention.module';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
     declarations: [
@@ -147,7 +149,9 @@ import { SalesPersonService } from '../shared/sales-person/utility/sales-person.
         BankIntegrationModule,
         CarouselComponent,
         AsideMenuCreateTaxModule,
-        AsideMenuAccountModule
+        AsideMenuAccountModule,
+        TributeMentionModule,
+        MatDividerModule
     ]
 })
 export class LedgerModule {
