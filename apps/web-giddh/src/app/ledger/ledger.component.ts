659798fcde71879a119e4a64d40f108c31d94305
Divyanshu <divyanshushrivastava123@gmail.com>
2025-07-30 17:42:49 +0530

    fix: show stock name with particular name in ledger transactions and hide duplicate button for company

--- apps/web-giddh/src/app/ledger/ledger.component.ts
+++ apps/web-giddh/src/app/ledger/ledger.component.ts
@@ -3905,7 +3905,11 @@ export class LedgerComponent implements OnInit, OnDestroy {
         let selectedAccountUniqueName = "";
 
         if (isActiveAccountAndParticularIsSame) {
-            selectedAccountName = transactionsParticular?.name;
+            if (transactionsParticular?.stock) {
+                selectedAccountName = `${transactionsParticular?.name} (${transactionsParticular?.stock?.name})`;
+            } else {
+                selectedAccountName = transactionsParticular?.name;
+            }
             selectedAccountUniqueName = transactionsParticular?.uniqueName;
         } else {
             selectedAccountName = `${res.particular?.name}${transactionsParticular?.stock ? ' (' + transactionsParticular?.stock?.name + ')': ''}`;
