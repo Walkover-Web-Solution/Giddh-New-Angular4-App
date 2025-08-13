9345b8bac676738f307227f9c4034450b2915757
Dilpreet Singh Dang <dilpreetsinghdang@Dilpreets-iMac.local>
2025-08-13 17:56:32 +0530

    PR changes

--- apps/web-giddh/src/app/vouchers/list/list.component.ts
+++ apps/web-giddh/src/app/vouchers/list/list.component.ts
@@ -3432,11 +3432,8 @@ export class VoucherListComponent implements OnInit, OnDestroy {
                 this.invoiceTemplatesService.deleteTemplate(template.uniqueName).pipe(takeUntil(this.destroyed$)).subscribe((res) => {
                     if (res?.status === 'success') {
                         this.toasterService.showSnackBar('success', 'Template deleted successfully');
-                        if (templateType) {
-                            this.fetchAllCreatedTemplates(templateType);
-                        } else {
-                            this.fetchTemplates(templateType);
-                        }
+                        this.fetchAllCreatedTemplates(templateType);
+                        this.fetchTemplates(templateType);
                     } else {
                         this.toasterService.showSnackBar('error', res?.message);
                     }
@@ -3446,10 +3443,11 @@ export class VoucherListComponent implements OnInit, OnDestroy {
     }
 
     /**
-   * Open template dialog
-   *
-   * @memberof VoucherListComponent
-   */
+     * Open template dialog
+     *
+     * @param {*} template
+     * @memberof VoucherListComponent
+     */
     public templateDialog(template: any): void {
         this.dialog.open(TemplatePreviewDialogComponent, {
             width: '980px',
@@ -3461,6 +3459,8 @@ export class VoucherListComponent implements OnInit, OnDestroy {
     /**
      * Open template edit dialog
      *
+     * @param {*} template
+     * @param {string} type
      * @memberof VoucherListComponent
      */
     public templateEdit(template: any, type: string): void {
