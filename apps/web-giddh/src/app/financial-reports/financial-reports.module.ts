0eae4fa8fd8f9c557757247bed437444f4a7f8c9
Ankit <ankitdubey@whozzat.com>
2025-08-21 11:50:49 +0530

    remove

--- apps/web-giddh/src/app/financial-reports/financial-reports.module.ts
+++ apps/web-giddh/src/app/financial-reports/financial-reports.module.ts
@@ -6,7 +6,6 @@ import { LaddaModule } from 'angular2-ladda';
 import { ClickOutsideModule } from 'ng-click-outside';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { PopoverModule } from 'ngx-bootstrap/popover';
 import { MatTabsModule } from '@angular/material/tabs';
 
 import { AmountFieldComponentModule } from '../shared/amount-field/amount-field.module';
@@ -112,7 +111,6 @@ import { ProjectAccountingService } from '../project-wise-accounting/project-wis
         GiddhPageLoaderModule,
         AmountFieldComponentModule,
         DatepickerWrapperModule,
-        PopoverModule.forRoot(),
         AsideMenuAccountModule,
         FinancialSearchPipe,
         MatButtonModule,
