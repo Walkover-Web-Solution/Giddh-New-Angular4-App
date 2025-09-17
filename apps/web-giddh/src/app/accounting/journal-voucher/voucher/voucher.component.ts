c915551d8a8ded37f50c9efe9496de7913e1f03d
Ankit <ankitdubey@whozzat.com>
2025-09-17 14:51:40 +0530

    Prod - Tab key navigation fails to show focus indicator in journal entry page

--- apps/web-giddh/src/app/accounting/journal-voucher/voucher/voucher.component.ts
+++ apps/web-giddh/src/app/accounting/journal-voucher/voucher/voucher.component.ts
@@ -1500,7 +1500,7 @@ export class AccountAsVoucherComponent implements OnInit, OnDestroy, AfterViewIn
      * @memberof AccountAsVoucherComponent
      */
     public handleEnterKeyPress(event: KeyboardEvent, submitButton: HTMLButtonElement): void {
-        if (event.key === 'Enter' && !this.showDiscountSidebar && !this.showTaxSidebar) {
+        if ((event.key === 'Enter' || event.key === 'Tab') && !this.showDiscountSidebar && !this.showTaxSidebar) {
             const descriptionControl = this.journalVoucherForm.get('description');
             if (!descriptionControl?.value || descriptionControl.value.trim() === '') {
                 event.preventDefault();
