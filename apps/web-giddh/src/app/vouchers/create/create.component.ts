1b821b75ca2f7786d892efe7fddeeb9ec688c9d1
Ankit <ankitdubey@whozzat.com>
2025-09-30 14:09:03 +0530

    check for tax

--- apps/web-giddh/src/app/vouchers/create/create.component.ts
+++ apps/web-giddh/src/app/vouchers/create/create.component.ts
@@ -4020,7 +4020,7 @@ export class VoucherCreateComponent implements OnInit, OnDestroy, AfterViewInit 
      */
     private calculateOtherTaxAmount(entry: FormGroup, isActiveEntry: boolean): void {
         let taxableValue = 0;
-        if (!entry.get("transactions").value[0]?.amount?.amountForAccount || isActiveEntry) {
+        if (!entry.get("otherTax").value || !entry.get("transactions").value[0]?.amount?.amountForAccount || isActiveEntry) {
             return;
         }
         const amountForAccount = Number(entry.get("transactions").value[0].amount.amountForAccount);
@@ -4078,6 +4078,7 @@ export class VoucherCreateComponent implements OnInit, OnDestroy, AfterViewInit 
         }
 
         this.calculateTotalTax();
+        this.calculateOtherTaxAmount(entryFormGroup, false);
     }
 
     /**
