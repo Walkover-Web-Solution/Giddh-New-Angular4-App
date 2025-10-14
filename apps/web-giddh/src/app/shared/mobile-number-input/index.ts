063486ea88223294dbb13405623dc0ac120d3435
Giddh's Black Tiger <divyanshushrivastava123@gmail.com>
2025-10-14 21:37:12 +0530

    docs: add mobile number input component with integration guide and documentation

---
+++ apps/web-giddh/src/app/shared/mobile-number-input/index.ts
@@ -0,0 +1,2 @@
+export { MobileNumberInputComponent, Country, mobileNumberValidator } from './mobile-number-input.component';
+export { GeolocationService, GeolocationResponse } from './geolocation.service';
