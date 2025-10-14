d913ae8bf537ba8296ed19b9e0c077d33328da84
Ankit <ankitdubey@whozzat.com>
2025-10-14 17:27:57 +0530

    separate service for bread crum

--- apps/web-giddh/src/app/vat-report/vat-report.component.ts
+++ apps/web-giddh/src/app/vat-report/vat-report.component.ts
@@ -11,6 +11,7 @@ import { VatService } from "../services/vat.service";
 import { saveAs } from "file-saver";
 import { SettingsFinancialYearService } from '../services/settings.financial-year.service';
 import { RestrictedModules } from '../app.constant';
+import { BreadCrumbService } from '../services/bread-crum.service';
 @Component({
     selector: 'app-vat-report',
     styleUrls: ['./vat-report.component.scss'],
@@ -78,6 +79,7 @@ export class VatReportComponent implements OnInit, OnDestroy {
         private toasty: ToasterService,
         private cdRef: ChangeDetectorRef,
         private route: Router,
+        private breadCrumbService: BreadCrumbService,
         public settingsFinancialYearService: SettingsFinancialYearService
     ) { }
 
@@ -199,9 +201,9 @@ export class VatReportComponent implements OnInit, OnDestroy {
     * @memberof VatReportComponent
     */
     public viewVatReportTransactions(section: string, description: string) {
-        this.generalService.setBreadCrumbPath([
-            { url: '/pages/vat-report', queryParams: { }, showBreadcrumb: 'VAT-Report' },
-            { url: `/pages/vat-report/transactions/section/${section}`, queryParams: { from: this.fromDate, to: this.toDate, taxNumber: this.taxNumber }, showBreadcrumb: description || 'Section' }
+        this.breadCrumbService.setBreadCrumbPath([
+            { url: '/pages/vat-report', queryParams: { }, currentPageName: 'VAT-Report' },
+            { url: `/pages/vat-report/transactions/section/${section}`, queryParams: { from: this.fromDate, to: this.toDate, taxNumber: this.taxNumber }, currentPageName: description }
         ]);
         this.route.navigate(['pages', 'vat-report', 'transactions', 'section', section], { queryParams: { from: this.fromDate, to: this.toDate, taxNumber: this.taxNumber } });
     }
