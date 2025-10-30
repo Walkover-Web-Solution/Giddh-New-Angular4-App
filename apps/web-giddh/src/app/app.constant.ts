61330749651a1fa13c466119ac17715e0a7d4708
Ankit <ankitdubey@whozzat.com>
2025-10-30 11:11:06 +0530

    conflict resolve

--- apps/web-giddh/src/app/app.constant.ts
+++ apps/web-giddh/src/app/app.constant.ts
@@ -114,6 +114,8 @@ export enum OnBoardingType {
 
 /** Pagination limit for every module */
 export const PAGINATION_LIMIT = 50;
+/** Pagination limit for bulk stock */
+export const PAGINATION_LIMIT_BULK_STOCK = 100;
 /** Pagination count options */
 export const PAGE_SIZE_OPTIONS = [10, 20, 50, 100];
 /** API default count limit */
