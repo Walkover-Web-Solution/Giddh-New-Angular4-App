31d271d745ba3a22571ef6c7349d321fe5af70c2
Giddh's Black Tiger <divyanshushrivastava123@gmail.com>
2025-11-12 15:18:42 +0530

    refactor: remove company initials avatar from sidebar

--- apps/web-giddh/src/app/shared/primary-sidebar/primary-sidebar.component.ts
+++ apps/web-giddh/src/app/shared/primary-sidebar/primary-sidebar.component.ts
@@ -59,8 +59,6 @@ export class PrimarySidebarComponent implements OnInit, OnChanges, OnDestroy {
     public isLedgerAccSelected: boolean = false;
     /** Holds the navigated accounts */
     public accountItemsFromIndexDB: any[] = [];
-    /** Company name initials (upto 2 characters) */
-    public companyInitials: any = '';
     /** Stores the total company list */
     public companyList: CompanyResponse[] = [];
     /** Stores all the menu items to be shown */
@@ -249,8 +247,6 @@ export class PrimarySidebarComponent implements OnInit, OnChanges, OnDestroy {
         this.store.pipe(select(state => state.session.activeCompany), takeUntil(this.destroyed$)).subscribe(selectedCmp => {
             if (selectedCmp && selectedCmp?.uniqueName === this.generalService.companyUniqueName) {
                 this.selectedCompanyDetails = selectedCmp;
-                this.companyInitials = this.generalService.getInitialsFromString(selectedCmp.name);
-
                 this.activeCompanyForDb = new CompAidataModel();
                 if (this.generalService.currentOrganizationType === OrganizationType.Branch) {
                     this.activeCompanyForDb.name = this.currentBranch ? this.currentBranch.name : '';
