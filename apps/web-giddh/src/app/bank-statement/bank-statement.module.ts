a84e36f0fb60139a9da960b6866dcfac2d279033
Divyanshu Shrivastava <divyanshushrivastava123@gmail.com>
2025-11-21 00:36:39 +0530

    refactor: remove unused Material modules and add localization support to bank statement data-list
    - Removed unused Material modules (Menu, Checkbox, Tabs, Ripple, Chips, Input, Dialog, SlideToggle, List, Scrolling, Sort, Paginator, Select, Datepicker, NativeDate, Divider) from BankStatementModule
    - Removed unused helper modules (ClickOutside, DigitsOnly, ElementViewChild, NgxMask, WatchVideo)
    - Added TranslateDirectiveModule and NewConfirmationModalModule imports
    - Wrapped data-list template with

--- apps/web-giddh/src/app/bank-statement/bank-statement.module.ts
+++ apps/web-giddh/src/app/bank-statement/bank-statement.module.ts
@@ -3,42 +3,22 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { LaddaModule } from 'angular2-ladda';
 import { SharedModule } from '../shared/shared.module';
-import { ClickOutsideModule } from 'ng-click-outside';
-import { DigitsOnlyModule } from '../shared/helpers/directives/digitsOnly/digitsOnly.module';
-import { ElementViewChildModule } from '../shared/helpers/directives/elementViewChild/elementViewChild.module';
-import { NgxMaskModule } from '../shared/helpers/directives/ngx-mask';
 import { NoDataModule } from '../shared/no-data/no-data.module';
 import { MatButtonModule } from "@angular/material/button";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatTabsModule } from "@angular/material/tabs";
-import { MatRippleModule } from "@angular/material/core";
 import { FormFieldsModule } from "../theme/form-fields/form-fields.module";
-import { MatChipsModule } from "@angular/material/chips";
 import { MatTableModule } from "@angular/material/table";
-import { MatInputModule } from "@angular/material/input";
 import { MatTooltipModule } from "@angular/material/tooltip";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatListModule } from '@angular/material/list';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatCardModule } from '@angular/material/card';
 import { MatStepperModule } from '@angular/material/stepper';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { GiddhPageLoaderModule } from '../shared/giddh-page-loader/giddh-page-loader.module';
-import { WatchVideoModule } from '../theme/watch-video/watch-video.module';
-
 import { BankStatementRoutingModule } from './bank-statement.routing.module';
 import { OnboardingComponent } from './components/onboarding/onboarding.component';
 import { DataListComponent } from './components/data-list/data-list.component';
 import { StepperFormComponent } from './components/stepper-form/stepper-form.component';
 import { EmailForwardingService } from './services/email-forwarding.service';
-import { MatDividerModule } from '@angular/material/divider';
+import { TranslateDirectiveModule } from '../theme/translate/translate.directive.module';
+import { NewConfirmationModalModule } from '../theme/new-confirmation-modal/confirmation-modal.module';
 
 @NgModule({
     declarations: [
@@ -56,36 +36,17 @@ import { MatDividerModule } from '@angular/material/divider';
             spinnerSize: 30
         }),
         SharedModule,
-        ClickOutsideModule,
-        DigitsOnlyModule,
-        ElementViewChildModule,
-        NgxMaskModule.forRoot(),
         NoDataModule,
         MatButtonModule,
-        MatMenuModule,
-        MatCheckboxModule,
-        MatTabsModule,
-        MatRippleModule,
         FormFieldsModule,
-        MatChipsModule,
         MatTableModule,
-        MatInputModule,
         MatTooltipModule,
-        MatDialogModule,
-        MatSlideToggleModule,
-        MatListModule,
-        ScrollingModule,
-        MatSortModule,
-        MatPaginatorModule,
         MatCardModule,
         MatStepperModule,
-        MatSelectModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
         MatProgressSpinnerModule,
         GiddhPageLoaderModule,
-        WatchVideoModule,
-        MatDividerModule
+        NewConfirmationModalModule,
+        TranslateDirectiveModule
     ],
     providers: [
         EmailForwardingService
