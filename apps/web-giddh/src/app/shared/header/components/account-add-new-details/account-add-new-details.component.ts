83a60348250db65eb4125a3bd28e22de0958d29a
Ankit <ankitdubey@whozzat.com>
2025-12-01 18:12:44 +0530

    Opening balance type should only be updated when opening balance or foreign opening balance value is greater than 0

--- apps/web-giddh/src/app/shared/header/components/account-add-new-details/account-add-new-details.component.ts
+++ apps/web-giddh/src/app/shared/header/components/account-add-new-details/account-add-new-details.component.ts
@@ -849,7 +849,9 @@ export class AccountAddNewDetailsComponent implements OnInit, OnChanges, AfterVi
      * @memberof AccountAddNewDetailsComponent
      */
     public openingBalanceTypeChanged(type: string): void {
-        this.addAccountForm.get('openingBalanceType')?.patchValue(type);
+        if (Number(this.addAccountForm.get('openingBalance')?.value) > 0 || Number(this.addAccountForm.get('foreignOpeningBalance')?.value) > 0) {
+            this.addAccountForm.get('openingBalanceType')?.patchValue(type);
+        }
     }
 
     public showLessGst() {
