e249913f868362bb4df0da9827650ee011d6eaaf
Giddh's Black Tiger <divyanshushrivastava123@gmail.com>
2025-12-16 12:40:43 +0530

    refactor: replace digit only directive with decimal and also remove from common components

--- apps/web-giddh/src/app/theme/form-fields/text-field/text-field.component.ts
+++ apps/web-giddh/src/app/theme/form-fields/text-field/text-field.component.ts
@@ -26,7 +26,6 @@ export class TextFieldComponent implements OnInit, OnChanges, OnDestroy, Control
     @Input() public min: number = null;
     @Input() public max: number = null;
     @Input() public allowDecimalDigitsOnly: boolean = false;
-    @Input() public allowDigitsOnly: boolean = false;
     @Input() public cssClass: string = "";
     @Input() public cssStyle: string = "";
     /** Taking placeholder as input */
