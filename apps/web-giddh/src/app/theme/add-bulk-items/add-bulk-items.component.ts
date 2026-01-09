f074de3b9bf8e8283fb3aa0fe9b534e8489425e4
Dilpreet Singh Dang <dilpreetsingh@Dilpreets-iMac.local>
2026-01-09 18:49:02 +0530

    remvoe console

--- apps/web-giddh/src/app/theme/add-bulk-items/add-bulk-items.component.ts
+++ apps/web-giddh/src/app/theme/add-bulk-items/add-bulk-items.component.ts
@@ -359,7 +359,6 @@ export class AddBulkItemsComponent implements OnInit, OnDestroy {
         const dataArray = this.addBulkForm.get('data') as FormArray;
         const isAlreadySelected = dataArray?.value?.filter(data => data?.additional?.value === item.uniqueName);
 
-        console.log(item, index, dataArray, isAlreadySelected, this.itemsInProcess[item.uniqueName]);
         if ((isAlreadySelected?.length && !isAlreadySelected[0].additional?.hasVariants) || this.itemsInProcess[item.uniqueName]) {
             this.toaster.showSnackBar('warning', this.localeData?.item_selected);
             return;
