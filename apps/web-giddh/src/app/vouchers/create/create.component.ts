675650fd50609447032081bbba745369317018be
Giddh's Black Tiger <divyanshushrivastava123@gmail.com>
2026-01-12 18:20:18 +0530

    fix(bug): Cursor is getting lost when closing the create tax/discount side bar with esc and cancel option 2

--- apps/web-giddh/src/app/vouchers/create/create.component.ts
+++ apps/web-giddh/src/app/vouchers/create/create.component.ts
@@ -3687,7 +3687,7 @@ export class VoucherCreateComponent implements OnInit, OnDestroy, AfterViewInit 
         });
 
         this.taxAsideMenuRef.afterClosed().subscribe(() => {
-            if (this.taxDropdown) {
+            if (this.lastInteraction === InteractionType.KEYBOARD && this.taxDropdown) {
                 this.taxDropdown.focusTaxDropdown();
             }
         });
@@ -3706,7 +3706,7 @@ export class VoucherCreateComponent implements OnInit, OnDestroy, AfterViewInit 
             if (response) {
                 this.componentStore.getDiscountsList();
             }
-            if (this.discountDropdown) {
+            if (this.lastInteraction === InteractionType.KEYBOARD && this.discountDropdown) {
                 this.discountDropdown.focusDiscountDropdown();
             }
         });
