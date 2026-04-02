f59bea7cb0eb5d4cb83575bc3dfd63b7af04bb38
Ankit <ankitdubey@whozzat.com>
2026-04-02 16:55:52 +0530

    fix(ledger): resolve advance receipt amount calculation and improve form focus handling

--- apps/web-giddh/src/app/ledger/ledger.component.ts
+++ apps/web-giddh/src/app/ledger/ledger.component.ts
@@ -1981,6 +1981,10 @@ export class LedgerComponent implements OnInit, OnDestroy {
                 blankTransactionObj = this.adjustmentUtilityService.getAdjustmentObject(blankTransactionObj);
             }
             const model = cloneDeep(blankTransactionObj);
+            if (model.transactions[0]?.subVoucher === "ADVANCE_RECEIPT" && !model.isOtherTaxesApplicable) {
+                /** Here key 'taxInclusiveAmount' represents the amount of the advance receipt, exclusive of tax (if tax is applied) */
+                model.transactions[0].amount = model.transactions[0].taxInclusiveAmount;
+            }
             if (eWayBillResponse && Object.keys(eWayBillResponse).length > 0) {
                 model.ewayBillDetails = eWayBillResponse;
             }
