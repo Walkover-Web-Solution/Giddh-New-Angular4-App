06a4d50a08fa76a4829a3ee0b5766892cdab0eaa
Giddh's Black Tiger <divyanshushrivastava123@gmail.com>
2026-09-22 17:55:46 +0530

    feat: implement button toggle for report nature selection in inventory settings
    - Replaced slide toggle with button toggle for selecting report nature (Books/Inventory) in inventory settings.
    - Updated the corresponding HTML and TypeScript files to handle the new button toggle functionality.
    - Enhanced the report nature handling logic to ensure proper state management across components.

--- apps/web-giddh/src/app/new-inventory/new-inventory.module.ts
+++ apps/web-giddh/src/app/new-inventory/new-inventory.module.ts
@@ -55,6 +55,7 @@ import { BulkStockEditComponent } from "./component/bulk-stock-edit/bulk-stock-e
 import { BulkStockAdvanceFilterComponent } from "./component/bulk-stock-advance-filter/bulk-stock-advance-filter.component";
 import { WatchVideoModule } from "../theme/watch-video/watch-video.module";
 import { ExportInventoryMasterComponent } from "./component/export-inventory-master/export-inventory-master.component";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { AdjustInventoryListComponent } from "./component/adjust-inventory-list/adjust-inventory-list.component";
@@ -136,6 +137,7 @@ import { AgeRangeEditorComponent } from "../theme/age-range-editor/age-range-edi
         DragDropModule,
         CustomPriceModule,
         WatchVideoModule,
+        MatButtonToggleModule,
         MatSlideToggleModule,
         MatExpansionModule,
         MatPaginatorModule,
